22eef27d7b5a55f40c5f3121220e82f840baf48d
pulokas <pulokas>
2002-04-16 18:39:08 +0000

    added ImageCanvas to install script

--- pyami/install.sh
+++ pyami/install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 install ImageViewer.py /usr/lib/python/site-packages
+install ImageCanvas.py /usr/lib/python/site-packages
 install NumericImage.py /usr/lib/python/site-packages
 install ScrolledCanvas.py /usr/lib/python/site-packages
