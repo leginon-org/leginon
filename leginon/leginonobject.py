03a454b38960b707d874419e92cdf0aaf8ad7506
pulokas <pulokas>
2002-10-24 19:43:45 +0000

    added location() method

--- leginon/leginonobject.py
+++ leginon/leginonobject.py
@@ -1,6 +1,14 @@
 
-
+import os, socket
 
 class LeginonObject(object):
 	def __init__(self):
 		pass
+
+	def location(self):
+		'return a dict describing the location of this object'
+		loc = {}
+		loc['hostname'] = socket.gethostname()
+                loc['pid'] = os.getpid()
+		loc['pythonid'] = id(self)
+		return loc
