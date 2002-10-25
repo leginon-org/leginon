c86d7c6cca805d4546c0beab47b3797e375de1e7
suloway <suloway>
2002-10-25 00:57:27 +0000

    initial revision

---
+++ leginon/datalocal.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+import leginonobject
+import copy
+
+class Server(leginonobject.LeginonObject):
+	def __init__(self, server):
+		leginonobject.LeginonObject.__init__(self)
+		self.server = server
+
+class PullServer(Server):
+	def datafromid(self, data_id):
+		return self.server.datafromid(data_id)
+
+class PushServer(Server):
+	def datatoid(self, data_id, data):
+		return self.server.datatoid(data_id, data)
+
+class Client(leginonobject.LeginonObject):
+	def __init__(self, server):
+		leginonobject.LeginonObject.__init__(self)
+		self.server = server
+
+class PullClient(Client):
+	def pull(self, data_id):
+		return copy.deepcopy(self.server.datafromid(data_id))
+
+class PushClient(Client):
+	def push(self, data_id, data):
+		# copy the data_id?
+		return self.server.datatoid(data_id, copy.deepcopy(data))
+
+if __name__ == '__main__':
+	import threading
+
+	class dummyServer:
+		def datafromid(self, data_id):
+			return {data_id : 'foo bar'}
+		def datatoid(self, data_id, data):
+			print `{data_id : data}`
+
+	pullserver = PullServer(dummyServer())
+	pushserver = PushServer(dummyServer())
+
+	pullclient = PullClient(pullserver)
+	pushclient = PushClient(pushserver)
+	print 'Test pulling:'
+	print pullclient.pull('my data id')
+	print 'Test pushing:'
+	print pushclient.push('my data id', [1,2,3,4])
+
