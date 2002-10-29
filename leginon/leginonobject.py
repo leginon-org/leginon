dd28a139cf321312eae6766124ce2cdeaecb6c1d
suloway <suloway>
2002-10-29 18:29:22 +0000

    ...

--- leginon/leginonobject.py
+++ leginon/leginonobject.py
@@ -3,12 +3,12 @@ import os, socket
 
 class LeginonObject(object):
 	def __init__(self):
-		pass
+		self.loc = {}
+		self.loc['hostname'] = socket.gethostname()
+		self.loc['pid'] = os.getpid()
+		self.loc['pythonid'] = id(self)
 
 	def location(self):
 		'return a dict describing the location of this object'
-		loc = {}
-		loc['hostname'] = socket.gethostname()
-                loc['pid'] = os.getpid()
-		loc['pythonid'] = id(self)
 		return loc
+
