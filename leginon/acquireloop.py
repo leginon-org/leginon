b5ba9c92e1d7383defc11b6ea733e14330183df2
pulokas <pulokas>
2002-11-09 01:15:08 +0000

    AcquireLoop is now derived from the node base class TimedLoop

--- leginon/acquireloop.py
+++ leginon/acquireloop.py
@@ -1,83 +1,27 @@
 #!/usr/bin/env python
 
-import node, event
-import threading, time
-import data
+import timedloop
+import time
 
-class AcquireLoop(node.Node):
+class AcquireLoop(timedloop.TimedLoop):
+	"""
+	A node that implements a timed image acquisition loop.
+	The default interval is 0 seconds, meaning it will acquire
+	images as fast as possible.
+	Event Inputs:
+		StartEvent - starts the acquisition loop
+		StopEvent - stops the acquisition loop
+		NumericControlEvent - modifies the loop interval
+	"""
 	def __init__(self, nodeid, managerlocation):
-		node.Node.__init__(self, nodeid, managerlocation)
+		timedloop.TimedLoop.__init__(self, nodeid, managerlocation)
 
-		self.addEventInput(event.StartEvent, self._handle_start)
-		self.addEventInput(event.StopEvent, self._handle_stop)
-		self.addEventInput(event.NumericControlEvent, self._handle_intervalchange)
-
-		self.interval = 0
-		self.acqevent = threading.Event()
-		self.stopevent = threading.Event()
-		self.mainlock = threading.RLock()
-
-		self.interact()
-
-	def main(self):
-		### main can only be run once
-		if not self.mainlock.acquire(blocking=0):
-			return
-
-		self.stopevent.clear()
-		while 1:
-			## check for a stop event
-			if self.stopevent.isSet():
-				break
-
-			## set a timer for the next acquire
-			self.acqevent.clear()
-			threading.Timer(self.interval,self._acqtimer).start()
-
-			## this acuqire
-			self._acquire()
-
-			## wait until the next acquire
-			self.acqevent.wait()
-
-		self.mainlock.release()
-
-	def _acqtimer(self):
-		"called to initiate the next acquire"
-		self.acqevent.set()
-
-	def _handle_start(self, startevent):
+	def action(self):
 		"""
-		start a new main thread
+		this is the real guts of this node
 		"""
-		t = threading.Thread(target=self.main)
-		t.setDaemon(1)
-		t.start()
-
-	def _handle_stop(self, stopevent):
-		self.stopevent.set()
-
-	def _handle_intervalchange(self, numcontrolevent):
-		print 'got control event %s' % numcontrolevent
-		new_interval = numcontrolevent.content
-		self._change_interval(new_interval)
-
-	def _change_interval(self, new_interval):
-		self.interval = new_interval
-
-	def _acquire(self):
+		## acquire image
 		print 'acquiring image %s' % time.asctime()
 
+		## publish image
 
-if __name__ == '__main__':
-	import signal, sys
-
-	manloc = {}
-	manloc['hostname'] = sys.argv[1]
-	manloc['TCP port'] = int(sys.argv[2])
-
-	m = MyNode(None, manloc)
-	try:
-		signal.pause()
-	except KeyboardInterrupt:
-		sys.exit()
---
+++ leginon/timedloop.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+
+import threading
+import node
+import event
+
+class TimedLoop(node.Node):
+	"""
+	A node that implements a timed action loop
+	The default interval is 0 seconds, meaning it will perform the 
+	action as fast as possible.
+	Event Inputs:
+		StartEvent - starts the loop
+		StopEvent - stops the loop
+		NumericControlEvent - modifies the loop interval
+	"""
+	def __init__(self, nodeid, managerlocation):
+		node.Node.__init__(self, nodeid, managerlocation)
+
+		self.addEventInput(event.StartEvent, self._handle_start)
+		self.addEventInput(event.StopEvent, self._handle_stop)
+		self.addEventInput(event.NumericControlEvent, self._handle_intervalchange)
+
+		self.interval = 0
+		self.nextevent = threading.Event()
+		self.stopevent = threading.Event()
+		self.mainlock = threading.RLock()
+
+		self.interact()
+
+	def main(self):
+		"""
+		this is the main loop, of which there can only
+		be one in this node
+		"""
+		### main can only be run once
+		if not self.mainlock.acquire(blocking=0):
+			return
+
+		self.stopevent.clear()
+		while 1:
+			## check for a stop event
+			if self.stopevent.isSet():
+				break
+
+			## set a timer for the next action
+			self.nextevent.clear()
+			threading.Timer(self.interval,self._nexttimer).start()
+
+			## this acuqire
+			self.action()
+
+			## wait for the duration of the interval timer
+			## or maybe if action() took too long, the
+			## timer already expired and this returns instantly
+			self.nextevent.wait()
+
+		self.mainlock.release()
+
+	def _nexttimer(self):
+		"called by a threading.Timer to initiate the next action"
+		self.nextevent.set()
+
+	def _handle_start(self, startevent):
+		"""
+		start a new main thread after receiving StartEvent
+		"""
+		print 'got start event %s' % startevent
+		t = threading.Thread(target=self.main)
+		t.setDaemon(1)
+		t.start()
+
+	def _handle_stop(self, stopevent):
+		"""
+		stop the main thread after receiving StopEvent
+		"""
+		print 'got stop event %s' % stopevent
+		self.stopevent.set()
+
+	def _handle_intervalchange(self, numcontrolevent):
+		"""
+		modify the loop interval after receiving NumericControlEvent
+		"""
+		print 'got control event %s' % numcontrolevent
+		new_interval = numcontrolevent.content
+		self._change_interval(new_interval)
+
+	def _change_interval(self, new_interval):
+		self.interval = new_interval
+
+	def action(self):
+		"""
+		this is the real guts of this node and must be 
+		defined in subclass
+		"""
+		raise NotImplementedError()
