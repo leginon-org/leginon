b025a2e287a76537fd03323d1426feaae3333da0
suloway <suloway>
2002-12-05 01:46:28 +0000

    ..

--- leginon/correlation.py
+++ leginon/correlation.py
@@ -1,7 +1,7 @@
 import Numeric
 import FFT
 
-def correlation(image1, image2, ccflag = True, pcflag = True):
+def correlation(image1, image2, ccflag=True, pcflag=True, subpixelflag=True):
 
 	val = {}
 
@@ -34,7 +34,16 @@ def correlation(image1, image2, ccflag = True, pcflag = True):
 		cc[center[0]:, :center[1]] = uncenteredcc[:center[0], center[1]:]
 		cc[:center[0], center[1]:] = uncenteredcc[center[0]:, :center[1]]
 
-		val['cross correlation'] = cc
+		if subpixelflag:
+			peak = quadraticPeak(cc)
+			val['cross correlation peak'] = peak
+			val['cross correlation shift'] = (peak[0] - center[0] + 0.5, \
+																				peak[1] - center[1] + 0.5)
+		else:
+			peak = quadraticPeak(cc)
+			val['cross correlation peak'] = matrixMax(cc)
+			val['cross correlation shift'] = (peak[0] - center[0], \
+																				peak[1] - center[1])
 
 	if pcflag:
 		pcfft = Numeric.zeros((len(ffts[0]), \
@@ -54,7 +63,47 @@ def correlation(image1, image2, ccflag = True, pcflag = True):
 		pc[center[0]:, :center[1]] = uncenteredpc[:center[0], center[1]:]
 		pc[:center[0], center[1]:] = uncenteredpc[center[0]:, :center[1]]
 
-		val['phase correlation'] = pc
+		if subpixelflag:
+			peak = quadraticPeak(pc)
+			val['phase correlation peak'] = peak
+			val['phase correlation shift'] = (peak[0] - center[0] + 0.5, \
+																				peak[1] - center[1] + 0.5)
+		else:
+			peak = quadraticPeak(pc)
+			val['phase correlation peak'] = matrixMax(pc)
+			val['phase correlation shift'] = (peak[0] - center[0], \
+																				peak[1] - center[1])
 
 	return val
+
+def matrixMax(m):
+	maxval = 0.
+	for rowindex in xrange(len(cc)):
+		rowmaxval = max(cc[rowindex])
+		if rowmaxval > maxval:
+			maxval = rowmaxval
+		maxindex = (rowindex, row.index(maxval))
+
+def quadraticPeak(m):
+	import Scientific
+
+	peak = matrixMax(m)
+
+	fcolumn = Scientific.Functions.polynomialLeastSquaresFit((0., 0., 0.),
+		[(-1, m[peak[0] - 1][peak[1]]),
+			(0, m[peak[0]][peak[1]]),
+			(1, m[peak[0] + 1][peak[1]])])
+
+	column = fcolumn[0][1] / 2*fcolumn[0][2]
+
+	frow = Scientific.Functions.polynomialLeastSquaresFit((0., 0., 0.),
+		[(-1, m[peak[0]][peak[1] - 1]),
+			(0, m[peak[0]][peak[1]]),
+			(1, m[peak[0]][peak[1] + 1])])
+
+	row = frow[0][1] / 2*frow[0][2]
+
+	npeak = (peak[0] + 0.5 + row, peak[1] + 0.5 + column)
+
+	return npeak
 
