d190a09c7f15a96c0796c9face06bf29b993fffb
pulokas <pulokas>
2002-12-11 01:38:59 +0000

    removed the part about reversing quadrants of the cc image

--- leginon/correlation.py
+++ leginon/correlation.py
@@ -3,6 +3,7 @@ import FFT
 
 #def correlation(image1, image2, ccflag=True, pcflag=True, subpixelflag=True):
 def correlation(image1, image2, ccflag=1, pcflag=1, subpixelflag=1):
+	centerzero = 0
 
 	val = {}
 
@@ -25,26 +26,20 @@ def correlation(image1, image2, ccflag=1, pcflag=1, subpixelflag=1):
 	Numeric.multiply(Numeric.conjugate(ffts[0]), ffts[1], ccfft)
 
 	if ccflag:
-		# invert correllation to use
-		uncenteredcc = FFT.inverse_real_fft2d(ccfft)
+		# invert correlation to use
+		cc = FFT.inverse_real_fft2d(ccfft)
 
-		cc = Numeric.zeros(dim, Numeric.Float)
-
-		cc[center[0]:, center[1]:] = uncenteredcc[:center[0], :center[1]]
-		cc[:center[0], :center[1]] = uncenteredcc[center[0]:, center[1]:]
-		cc[center[0]:, :center[1]] = uncenteredcc[:center[0], center[1]:]
-		cc[:center[0], center[1]:] = uncenteredcc[center[0]:, :center[1]]
-
-		if subpixelflag:
-			peak = quadraticPeak(cc)
+		try:
+			if subpixelflag:
+				peak = quadraticPeak(cc)
+			else:
+				peak = matrixMax(cc)
 			val['cross correlation peak'] = peak
-			val['cross correlation shift'] = (peak[0] - center[0] - 0.5, \
-																				peak[1] - center[1] - 0.5)
-		else:
-			peak = matrixMax(cc)
-			val['cross correlation peak'] = peak
-			val['cross correlation shift'] = (peak[0] - center[0], \
-																				peak[1] - center[1])
+		except:
+			val['cross correlation peak'] = None
+			print 'error finding peak'
+
+		val['cross correlation image'] = cc
 
 	if pcflag:
 		pcfft = Numeric.zeros((len(ffts[0]), \
@@ -55,69 +50,22 @@ def correlation(image1, image2, ccflag=1, pcflag=1, subpixelflag=1):
 		Numeric.divide(ccfft, Numeric.sqrt(Numeric.multiply(Numeric.add(ffts[0], \
 			ffts[1]), Numeric.conjugate(Numeric.add(ffts[0], ffts[1])))), pcfft)
 
-		uncenteredpc = FFT.inverse_real_fft2d(pcfft)
+		pc = FFT.inverse_real_fft2d(pcfft)
 
-		pc = Numeric.zeros(dim, Numeric.Float)
-
-		pc[center[0]:, center[1]:] = uncenteredpc[:center[0], :center[1]]
-		pc[:center[0], :center[1]] = uncenteredpc[center[0]:, center[1]:]
-		pc[center[0]:, :center[1]] = uncenteredpc[:center[0], center[1]:]
-		pc[:center[0], center[1]:] = uncenteredpc[center[0]:, :center[1]]
-
-		if subpixelflag:
-			peak = quadraticPeak(pc)
+		try:
+			if subpixelflag:
+				peak = quadraticPeak(pc)
+			else:
+				peak = matrixMax(pc)
 			val['phase correlation peak'] = peak
-			val['phase correlation shift'] = (peak[0] - center[0] - 0.5, \
-																				peak[1] - center[1] - 0.5)
-		else:
-			peak = matrixMax(pc)
-			val['phase correlation peak'] = peak
-			val['phase correlation shift'] = (peak[0] - center[0], \
-																				peak[1] - center[1])
+		except:
+			val['phase correlation peak'] = None
+			print 'error finding peak'
+		val['phase correlation image'] = pc
 
 	return val
 
 def matrixMax(m):
-	maxval = 0.
-	maxindex = (0, 0)
-	for rowindex in xrange(len(m)):
-		rowmaxval = max(m[rowindex])
-		if rowmaxval > maxval:
-			maxval = rowmaxval
-			maxindex = (rowindex, m[rowindex].tolist().index(maxval))
-	return maxindex
-
-def OLDquadraticPeak(m):
-	from Scientific.Functions.LeastSquares import polynomialLeastSquaresFit
-
-	peak = matrixMax(m)
-	if (peak[0] == 0) or (peak[0] == len(m) - 1):
-		raise ValueError
-	if (peak[1] == 0) or (peak[1] == len(m[0]) - 1):
-		raise ValueError
-
-	fcolumn = polynomialLeastSquaresFit((0., 0., 0.),
-		[(-1, m[peak[0] - 1][peak[1]]),
-			(0, m[peak[0]][peak[1]]),
-			(1, m[peak[0] + 1][peak[1]])])
-
-	column = fcolumn[0][1] / 2*fcolumn[0][2]
-
-	frow = polynomialLeastSquaresFit((0., 0., 0.),
-		[(-1, m[peak[0]][peak[1] - 1]),
-			(0, m[peak[0]][peak[1]]),
-			(1, m[peak[0]][peak[1] + 1])])
-
-	row = frow[0][1] / 2*frow[0][2]
-
-	npeak = (peak[0] + 0.5 + row, peak[1] + 0.5 + column)
-
-	return npeak
-
-def quadraticPeak(m):
-	from LinearAlgebra import linear_least_squares
-
-	## find the max pixel indices (row,col)
 	maxval = 0
 	peak = None
 	ind = 0
@@ -126,30 +74,53 @@ def quadraticPeak(m):
 			maxval = val
 			peak = ind
 		ind += 1
-	if not peak:
-		raise RuntimeError('no peak in image')
+	if peak is None:
+		raise RuntimeError('no peak in matrix')
 	# convert 1-d index to 2-d index
 	rows,cols = m.shape
 	peakrow = peak / rows
 	peakcol = peak % rows
+	return peakrow, peakcol
+
+def wrap(value, range):
+	if value < 0:
+		return range + value
+	else:
+		return value
+
+def quadraticPeak(m):
+	from LinearAlgebra import linear_least_squares
+
+	## find the max pixel indices (row,col)
+	peakrow,peakcol = matrixMax(m)
 	print 'peak', peakrow, peakcol
 
-	# reject edge pixel
-	if (peakrow == 0) or (peakrow == rows - 1):
-		raise RuntimeError('peak on edge')
-	if (peakcol == 0) or (peakcol == cols - 1):
-		raise RuntimeError('peak on edge')
+	rows,cols = m.shape
 
 	## fit quadratic to range of pixels centered on peak pixel
-	npix = 3  # could this be 5 for better accuracy?
+	npix = 3  # could this be 5 or 7 for better accuracy?
 
 	rowrange = (peakrow-npix/2, peakrow+npix/2+1)
 	rowinds = Numeric.arrayrange(rowrange[0], rowrange[1])
-	rowvals = m[rowrange[0]:rowrange[1], peakcol]
+	## fill in rowvals, wrap around array if necessary
+	rowvals = []
+	for row in rowinds:
+		if row < 0:
+			rowvals.append(m[rows + row, peakcol])
+		else:
+			rowvals.append(m[row, peakcol])
+	rowvals = Numeric.array(rowvals)
 
 	colrange = (peakcol-npix/2, peakcol+npix/2+1)
 	colinds = Numeric.arrayrange(colrange[0], colrange[1])
-	colvals = m[peakrow, colrange[0]:colrange[1]]
+	## fill in colvals, wrap around array if necessary
+	colvals = []
+	for col in colinds:
+		if col < 0:
+			colvals.append(m[peakrow, cols + col])
+		else:
+			colvals.append(m[peakrow, col])
+	colvals = Numeric.array(colvals)
 
 	print 'fit data indices', rowinds, colinds
 	print 'fit data values', rowvals, colvals
@@ -161,6 +132,7 @@ def quadraticPeak(m):
 	for row in rowinds:
 		row_dm[i] = (1,row,row*row)
 		i += 1
+	print 'row_dm', row_dm
 
 	## fit and find zero
 	rowfit = linear_least_squares(row_dm, rowvals)
