fc5173925fb01d9bf2401dc5743d2a62c0120c37
dfellman <dfellman>
2003-01-23 23:01:15 +0000

    ----------------------------------------------------------------------
    Modified Files:
     	nodegui.java
    ----------------------------------------------------------------------

--- leginon/gui/java/nodegui.java
+++ leginon/gui/java/nodegui.java
@@ -86,7 +86,7 @@ public class nodegui extends JFrame {
 	} else {
         nodegui window = new nodegui(args[0]);
 
-	window.setBounds(100,100,400,500);
+	window.setBounds(100,100,800,600);
         window.show();
 	}
     }
@@ -98,7 +98,7 @@ class GuiJIFContainer {
 	Vector content;
 	Container c;
 	String name, spectype;
-	int width=300, height=200;
+	int width=400, height=350;
 
 	public GuiJIFContainer(XmlRpcClient xmlrpcclient, Hashtable specWidget, Container c) throws Exception {
 		this.xmlrpcclient=xmlrpcclient;
@@ -224,6 +224,11 @@ class GuiMethod {
 			String xmlrpctype  = (String)o.get("xmlrpctype");
 			String id = (String)o.get("id");
 
+			Object defaultval = null;
+			if (spec.containsKey("default")) {
+				defaultval = spec.get("default");
+			}
+
 			if (o.containsKey("choices")) {
 				Hashtable choices = (Hashtable)o.get("choices");
 				String choices_type = (String)choices.get("type");
@@ -235,15 +240,11 @@ class GuiMethod {
 			} else
 
 			if (xmlrpctype.equals("string"))
-    				new AddTextField(name, 20, widgets, mainPanel);
+    				new AddTextField(name, 20, defaultval, widgets, mainPanel);
 
 			if (xmlrpctype.equals("boolean"))
 				new AddCheckBox(name, widgets, mainPanel);
 
-			Object defaultval = null;
-			if (spec.containsKey("default")) {
-				defaultval = spec.get("default");
-			}
 			if (xmlrpctype.equals("struct")) {
 				new TreeData(name, xmlrpcclient, id, (Hashtable)defaultval, widgets, mainPanel);
 			}
@@ -275,16 +276,22 @@ class GuiData {
 		xmlrpctype  = (String)spec.get("xmlrpctype");
 		String id = (String)spec.get("id");
 		Vector widgets = new Vector();
-		Vector args = new Vector();
-		args.add(id);
 
 		JPanel mainPanel =new JPanel();	
 		mainPanel.setBorder(new javax.swing.border.TitledBorder(name));
                 mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
 
 		if (spec.containsKey("permissions")) {
-			type=1;	
+			JPanel permPanel =new JPanel();	
+                	permPanel.setLayout(new BoxLayout(permPanel, BoxLayout.X_AXIS));
 			String permissions = (String)spec.get("permissions");
+<<<<<<< nodegui.java
+			if (permissions.matches("[r]"))
+				new AddButton ("Get", xmlrpcclient, "GET", id, widgets, permPanel);
+			if (permissions.matches("[w]")) 
+				new AddButton ("Set", xmlrpcclient, "SET", id, widgets, permPanel);
+			mainPanel.add(permPanel);
+=======
 			if (permissions.equals("r"))
 				new AddButton ("Get", xmlrpcclient, "GET", args, mainPanel);
 			if (permissions.equals("w")) 
@@ -294,16 +301,15 @@ class GuiData {
 				new AddButton ("Get", xmlrpcclient, "GET", args, mainPanel);
 				new AddButton ("Set", xmlrpcclient, "SET", args, mainPanel);
 			}
+>>>>>>> 1.2
 		}
 
 		Object defaultval = null;
 		if (spec.containsKey("default")) {
 			type=1;	
 			defaultval = spec.get("default");
-			System.out.println("def: "+defaultval +" class: "+defaultval.getClass());
 		} 
 
-		System.out.println("NAME: "+name+" SPEC: "+spec);
 		if (spec.containsKey("choices")) {
 				Hashtable choices = (Hashtable)spec.get("choices");
 				String choices_type = (String)choices.get("type");
@@ -314,7 +320,10 @@ class GuiData {
 					new TreeData(name, xmlrpcclient, choices_id, widgets, mainPanel);
 		} else
 		if (xmlrpctype.equals("string")) {
-    			new AddTextField(20, widgets, mainPanel);
+			new AddTextField(20, defaultval, widgets, mainPanel);
+		} else
+		if (xmlrpctype.equals("array")) {
+			new AddTextField(20, defaultval, widgets, mainPanel);
 		} else
 		if (xmlrpctype.equals("boolean")) {
 			new AddCheckBox(name, widgets, mainPanel);
@@ -330,17 +339,25 @@ class AddCheckBox {
 	String text;
 	Vector widgets;
 	Container c;
+	JCheckBox checkbox;
+
 	public AddCheckBox(String text, Vector widgets, Container c) {
 		this.text=text;
 		this.widgets=widgets;
 		this.c = c;
 		build();
 	}
+	
+	public Boolean getValue() {
+		Boolean b = Boolean.FALSE;
+		return b.valueOf(checkbox.isSelected());	
+	}
+
 	private void build() {
-		JCheckBox checkbox = new JCheckBox(text);
-		widgets.add(checkbox);
+		checkbox = new JCheckBox(text,false);
         	checkbox.setAlignmentX(Component.LEFT_ALIGNMENT);
         	c.add(checkbox);
+		widgets.add(this);
 	}
 }
 
@@ -348,8 +365,9 @@ class AddComboBox {
 	String text, id;
 	XmlRpcClient xmlrpcclient;
 	Container c;
-	Vector items;
+	Vector items = new Vector();
 	Vector widgets;
+	JComboBox cb;
 
 	public AddComboBox(String text, XmlRpcClient xmlrpcclient, String id, Vector widgets, Container c) throws Exception  {
 		this.text=text;
@@ -363,126 +381,204 @@ class AddComboBox {
 	public AddComboBox(XmlRpcClient xmlrpcclient, String id, Vector widgets, Container c) throws Exception  {
 		this(null, xmlrpcclient, id, widgets, c);
 	}
-	
+
+	public String getValue() {
+		return (String)cb.getSelectedItem();
+	}	
+
+	public void update() throws Exception {
+		getData(id);
+		DefaultComboBoxModel model = new DefaultComboBoxModel(items);
+        	cb.setModel(model);
+		cb.revalidate();
+	}
+
 	private void build() throws Exception {
-		Vector param = new Vector(1);
-		param.add(id);
-		Object items_obj = xmlrpcclient.execute("GET", param);
 		JPanel aPanel = new JPanel();
 		aPanel.setLayout(new BoxLayout(aPanel, BoxLayout.Y_AXIS));
 
-		if (items_obj instanceof Vector)
-			items=(Vector)items_obj;
-		else
-			items=new Vector();
-
-		JComboBox cb = new JComboBox(items);
-		widgets.add(cb);
+		getData(id);
+		cb = new JComboBox(items);
 		JLabel lbl = new JLabel(text);
 		aPanel.add(lbl);
 		aPanel.add(cb);
 		aPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
 		aPanel.setAlignmentY(Component.TOP_ALIGNMENT);
 		c.add(aPanel);
+		widgets.add(this);
+	}
+
+	private void getData(String id) throws Exception {
+			Vector param = new Vector(1);
+			param.add(id);
+			Object data = xmlrpcclient.execute("GET", param);
+
+			if (data instanceof Vector)
+				items=(Vector)data;
+
 	}
 }
 
 class AddTextField {
 	String text;
+	Object defaultval;
 	int size;
 	Vector widgets;
 	Container c;
+	JTextField textField;
 
-	public AddTextField(String text, int size, Vector widgets, Container c) {
+	public AddTextField(String text, int size, Object defaultval, Vector widgets, Container c) {
 		this.text=text;
 		this.size=size;
+		this.defaultval=defaultval;
 		this.widgets=widgets;
 		this.c = c;
 		build();
 	}
 
-	public AddTextField(int size, Vector widgets, Container c) {
-		this(null, size, widgets, c);
+	public AddTextField(int size, Object defaultval, Vector widgets, Container c) {
+		this(null, size, defaultval, widgets, c);
+	}
+	
+	public String getValue() {
+		return textField.getText();
+	}
+
+	public void update(String text) {
+		textField.setText(text);
 	}
 
 	private void build() {
 		JPanel aPanel = new JPanel();
 		aPanel.setLayout(new BoxLayout(aPanel, BoxLayout.Y_AXIS));
-		JTextField textField = new JTextField(size);
-		widgets.add(textField);
+		if (defaultval==null)
+			defaultval="";
+		textField = new JTextField(""+defaultval, size);
 		JLabel lbl = new JLabel(text);
         	aPanel.add(lbl);
         	aPanel.add(textField);
-        	aPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
+        	aPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
 		c.add(aPanel);
+		widgets.add(this);
 
 	}
 }
 
 class AddButton {
-	String name, id;
+	String name, id, cmd, cmdxmlrpc;
 	XmlRpcClient xmlrpcclient;
-	Vector args, widgets=new Vector();
+	Vector args, widgets;
 	Container c;
-	public AddButton (String name, XmlRpcClient xmlrpcclient, String id, Vector widgets, Container c) throws Exception  {
+
+	public AddButton (String name, XmlRpcClient xmlrpcclient, String cmd, String id, Vector widgets, Container c) throws Exception  {
 		this.name=name;
 		this.xmlrpcclient=xmlrpcclient;
+		this.cmd=cmd;
 		this.id=id;
 		this.widgets=widgets;
 		this.c = c;
 		build();
+	}
+
+	public AddButton (String name, XmlRpcClient xmlrpcclient, String id, Vector widgets, Container c) throws Exception  {
+		this(name,xmlrpcclient,null,id,widgets,c);
 	}
 	private void build() throws Exception {
         	JButton button = new JButton(name);
 		c.add(button);
                 button.addActionListener(new ActionListener() {
                         public void actionPerformed(ActionEvent event) {
-				args = getArgs(widgets);
-				System.out.println("("+widgets+");");
-				System.out.println("xmlrpcclient.execute("+id+", "+args+");");
 				try {
-					xmlrpcclient.execute(id, args);
+
+
+				if (cmd==null) {	
+					cmdxmlrpc=id;
+					args = getArgs(widgets);
+				} else {
+					cmdxmlrpc=cmd;
+					args = getData(widgets);
+				}
+					// Display xmlRPC command
+					System.out.println("xmlrpcclient.execute("+cmdxmlrpc+", "+args+");");
+					Object result = xmlrpcclient.execute(cmdxmlrpc, args);
+					
+					refresh(widgets, result);
+
+
 				} catch (Exception e) {
 					System.err.println("Exception: "+e);
 				}
 			}
 		});
 	}
-
-	private Vector getArgs(Vector widgets) {
-		Vector args = new Vector();
+	
+	private void refresh(Vector widgets, Object result) throws Exception {
 		for (Enumeration enumwidget = widgets.elements() ; enumwidget.hasMoreElements() ;) {
 			Object o = enumwidget.nextElement();
-			if (o instanceof JTextField) {
-				JTextField jtf = (JTextField)o;
-				args.add(jtf.getText());
-			} else if (o instanceof JCheckBox) {
-				JCheckBox jckb = (JCheckBox)o;
-				Boolean b = Boolean.FALSE;
-				args.add(b.valueOf(jckb.isSelected()));	
-			} else if (o instanceof Vector) {
-				Vector v = (Vector)o;
-				args.add(v);
-			} else if (o instanceof JComboBox) {
-				JComboBox jcb = (JComboBox)o;
-				args.add(jcb.getSelectedItem());
-			} else if (o instanceof String) {
-				args.add(o.toString());
+			if (o instanceof TreeData) {
+				TreeData td = (TreeData)o;
+				td.update();
+			} else if (o instanceof AddComboBox) {
+				AddComboBox c = (AddComboBox)o;
+				c.update();
+			} else if (o instanceof AddTextField) {
+				AddTextField t = (AddTextField)o;
+				if (result!=null)
+				t.update(""+result);
 			}
-				
+		}
+	}
+
+	private Vector getData(Vector widgets) throws Exception {
+		Vector args = new Vector();
+		args.add(id);
+		if (cmd.equals("SET"))
+		for (Enumeration enumwidget = widgets.elements() ; enumwidget.hasMoreElements() ;) {
+			Object o = enumwidget.nextElement();
+			if (o instanceof AddTextField) {
+				AddTextField t = (AddTextField)o;
+				args.add(t.getValue());
+			} else if (o instanceof TreeData) {
+				TreeData td = (TreeData)o;
+				args.add(td.getHashTree());
+			}
 		}
 		return args;
 	}
+
+	private Vector getArgs(Vector widgets) throws Exception {
+		Vector args = new Vector();
+		for (Enumeration enumwidget = widgets.elements() ; enumwidget.hasMoreElements() ;) {
+			Object o = enumwidget.nextElement();
+			if (o instanceof AddTextField) {
+				AddTextField t = (AddTextField)o;
+				args.add(t.getValue());
+			} else if (o instanceof AddCheckBox) {
+				AddCheckBox ckb = (AddCheckBox)o; 
+				args.add(ckb.getValue());
+			} else if (o instanceof TreeData) {
+				TreeData td = (TreeData)o;
+				args.add(td.getValue());
+			} else if (o instanceof AddComboBox) {
+				AddComboBox c = (AddComboBox)o;
+				args.add(c.getValue());
+			}
+		}
+		return args;
+	}
+
 }
 
 class TreeData {
 	String name, id;
 	XmlRpcClient xmlrpcclient;
 	Container c;
-	Hashtable hashtree;
-	Vector widgets = new Vector();
+	Vector widgets;
 	JTree tree;
 	TreeTableModel model;
+	JTreeTable treeTable;
+	JScrollPane hashPane;
+	Hashtable hashtree = new Hashtable();
 	Vector path = new Vector();
 
 	public TreeData(String name, XmlRpcClient xmlrpcclient, String id, Hashtable defaultval, Vector widgets, Container c) throws Exception  {
@@ -498,23 +594,35 @@ class TreeData {
 		this(name,xmlrpcclient,id,null,widgets,c);
 	}
 
-	private void build() throws Exception {
-		if (hashtree ==null) {
-			Vector param = new Vector(1);
-			param.add(id);
-			Object data = xmlrpcclient.execute("GET", param);
+	public Vector getValue() {
+		return path;
+	}
 
-			if (data instanceof Hashtable) {
-				hashtree=(Hashtable)data;
-			} else hashtree = new Hashtable();
-		} 
-
-		System.out.println("HASHTREE: "+hashtree);
+	public Hashtable getHashTree() {
+		return hashtree;
+	}
+	public void update() throws Exception {
+		getData(id);
 		model = new HashtableNodesModel(new HashtableNodes(hashtree,name).getRoot());
-		JTreeTable treeTable = new JTreeTable(model);
-                JScrollPane hashPane = new JScrollPane(treeTable);
-                hashPane.setPreferredSize(new Dimension(200,200));
+		tree.setModel(model);
+		treeTable.revalidate();
+	}
+
+
+	private void build() throws Exception {
+
+		if (hashtree==null || hashtree.isEmpty())
+			getData(id);
+
+		model = new HashtableNodesModel(new HashtableNodes(hashtree,name).getRoot());
+		treeTable = new JTreeTable(model);
+                hashPane = new JScrollPane(treeTable);
+        	hashPane.getViewport().setBackground(Color.white);
+
+		treeTable.setBackground(new Color(255, 255, 255));
+                hashPane.setPreferredSize(new Dimension(300,300));
 		c.add(hashPane);
+
 
 		tree = treeTable.getTree();
 		tree.addTreeSelectionListener(new TreeSelectionListener() {
@@ -529,13 +637,40 @@ class TreeData {
                                 		Object k = keys[i].toString();
                                 		path.add(k);
                         		}
+					if (value!=null && !(value.equals("")))
 						path.add(value);
-						path.remove(0);
+
+					// to remove the root of the tree...
+					path.remove(0);
 				}
         
 			}
 		});
-		widgets.add(path);
+		model.addTreeModelListener( new TreeModelListener() {
+			public void treeNodesChanged(TreeModelEvent e) {
+				Object key = e.getChildren()[0];
+				Object value = model.getValueAt(key,1);
+				TreePath tp = e.getTreePath().pathByAddingChild(key);
+				Object [] keys = tp.getPath();
 
+				// update the hashtable
+				new HashtableNodes.UpdateHashTree(hashtree, keys, value);
+			}
+			public void treeNodesInserted(TreeModelEvent e) {}
+			public void treeNodesRemoved(TreeModelEvent e) {}
+			public void treeStructureChanged(TreeModelEvent e) {}
+	});
+
+		widgets.add(this);
+
+	}
+
+	private void getData(String id) throws Exception {
+			Vector param = new Vector(1);
+			param.add(id);
+			Object data = xmlrpcclient.execute("GET", param);
+
+			if (data instanceof Hashtable)
+				hashtree=(Hashtable)data;
 	}
 }
