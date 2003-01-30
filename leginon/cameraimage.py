715cfc6277802b7746f162cfed8c5be251fa2c46
pulokas <pulokas>
2003-01-30 21:54:03 +0000

    this will contain utility functions for dealing with Numeric images

---
+++ leginon/cameraimage.py
@@ -0,0 +1,28 @@
+import Numeric
+
+def centerOffset(camerasize, imagesize):
+	'''determine offset to use image from camera center for 2k camera'''
+	r = (camerasize[0]/2 - imagesize[0]/2, camerasize[1]/2 - imagesize[1]/2)
+	return r
+
+def stdev(inputarray):
+	inlen = len(inputarray.flat)
+	divisor = Numeric.array(inlen, Numeric.Float32)
+	m = Numeric.sum(inputarray.flat) / divisor
+	stdev = Numeric.sqrt(Numeric.sum((Numeric.ravel(input) - m)**2) / len(Numeric.ravel(input)))
+	return stdev
+
+def mean(inputarray):
+	inlen = len(input.flat)
+	divisor = Numeric.array(inlen, Numeric.Float32)
+	m = Numeric.sum(Numeric.ravel(input)) / divisor
+	return m
+
+def averageSeries(self, series):
+	slen = len(series)
+	if slen == 0:
+		return None
+	sum = Numeric.sum(series)
+	divisor = Numeric.array(slen, Numeric.Float32)
+	avg = sum / divisor
+	return avg
