29e3e01ad62fcd91a046b6181a7f86e61b9fe863
pulokas <pulokas>
2003-02-14 01:40:01 +0000

    added another example of a return value

--- leginon/imagenode.py
+++ leginon/imagenode.py
@@ -16,9 +16,17 @@ class ImageNode(node.Node):
 		### Acquire Bright/Dark
 		ret = self.registerUIData('Image', 'binary')
 		getimage = self.registerUIMethod(self.uiGetImage, 'Get Image', (), returnspec=ret)
-		self.registerUISpec('Image Node', (getimage, nodespec))
+
+		arg = self.registerUIData('Your String', 'string', default='default string')
+		ret = self.registerUIData('My String', 'string')
+		changestr = self.registerUIMethod(self.uiChangeString, 'To Upper', (arg,), returnspec=ret)
+
+		self.registerUISpec('Image Node', (getimage, changestr, nodespec))
 
 	def uiGetImage(self):
 		numarray = Mrc.mrc_to_numeric('test1.mrc')
 		mrcstr = Mrc.numeric_to_mrcstr(numarray)
 		return xmlrpclib.Binary(mrcstr)
+
+	def uiChangeString(self, mystring=''):
+		return mystring.upper()
