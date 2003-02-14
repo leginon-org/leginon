e762c1bfc467813cda7d0137db2904294dbcd299
pulokas <pulokas>
2003-02-14 01:31:29 +0000

    major changes all around, untested

--- leginon/corrector.py
+++ leginon/corrector.py
@@ -4,11 +4,14 @@ import Numeric
 import cameraimage
 reload(cameraimage)
 import data, event
-import shelve
+import cPickle
+import string
 import threading
-from Mrc import mrc_to_numeric, numeric_to_mrc
+import Mrc
 import camerafuncs
 reload(camerafuncs)
+import xmlrpclib
+
 
 ### these should go in a stats node or module
 
@@ -23,10 +26,6 @@ class DataHandler(node.DataHandler):
 			result = self.node.acquireCorrectedImageData()
 			self.lock.release()
 			return result
-		#elif id == 'fake normalized image data':
-		#	result = self.node.acquireCorrectedFakeImageData()
-		#	self.lock.release()
-		#	return result
 		else:
 			self.lock.release()
 			return node.DataHandler.query(self, id)
@@ -34,11 +33,9 @@ class DataHandler(node.DataHandler):
 class Corrector(node.Node, camerafuncs.CameraFuncs):
 	def __init__(self, id, nodelocations):
 
-		self.plan = {}
+		self.plans = {}
 
 		node.Node.__init__(self, id, nodelocations, DataHandler, (self,))
-		self.addEventInput(event.ImageAcquireEvent, self.acquireCorrected)
-		self.addEventOutput(event.ImagePublishEvent)
 		self.addEventOutput(event.DarkImagePublishEvent)
 		self.addEventOutput(event.BrightImagePublishEvent)
 		self.addEventOutput(event.ListPublishEvent)
@@ -56,198 +53,187 @@ class Corrector(node.Node, camerafuncs.CameraFuncs):
 		nodespec = node.Node.defineUserInterface(self)
 
 		### Acquire Bright/Dark
-		acqdark = self.registerUIMethod(self.acquireDark, 'Dark', ())
-		acqbright = self.registerUIMethod(self.acquireBright, 'Bright', ())
-		acqcorr = self.registerUIMethod(self.acquireCorrected, 'Corrected', ())
-		acq = self.registerUIContainer('Acquire References', (acqdark, acqbright, acqcorr))
-
-		self.camdata = self.cameraConfigUISpec()
+		acqret = self.registerUIData('Image', 'binary')
+		acqdark = self.registerUIMethod(self.uiAcquireDark, 'Acquire Dark', (), returnspec=acqret)
+		acqbright = self.registerUIMethod(self.uiAcquireBright, 'Acquire Bright', (), returnspec=acqret)
+		acqcorr = self.registerUIMethod(self.uiAcquireCorrected, 'Acquire Corrected', (), returnspec=acqret)
 
 		self.navgdata = self.registerUIData('Frames to Average', 'integer', default=3, permissions='rw')
 
-		self.clipflag = self.registerUIData('Do Clipping', 'boolean', default=1, permissions='rw')
-		self.clipmin = self.registerUIData('Clip Min', 'float', default=0.0, permissions='rw')
-		self.clipmax = self.registerUIData('Clip Max', 'float', default=8000, permissions='rw')
+
 		self.fakeflag = self.registerUIData('Fake', 'boolean', default=False, permissions='rw')
 
-		prefs = self.registerUIContainer('Preferences', (self.camdata, self.navgdata, self.clipflag, self.clipmin, self.clipmax, self.fakeflag))
+		prefs = self.registerUIContainer('Preferences', (self.navgdata, self.fakeflag))
 
-		self.registerUISpec('Corrector', (acq, prefs, nodespec))
+		self.camconfigdata = self.cameraConfigUISpec()
+		argspec = (
+			self.registerUIData('Clip Limits', 'array', default=()),
+			self.registerUIData('Bad Rows', 'array', default=()),
+			self.registerUIData('Bad Cols', 'array', default=())
+		)
+		setplan = self.registerUIMethod(self.uiSetPlanParams, 'Set Plan', argspec)
+
+		plan = self.registerUIContainer('Plan', (self.camconfigdata, setplan))
+
+		self.registerUISpec('Corrector', (plan, acqdark, acqbright, acqcorr, prefs, nodespec))
+
+	def uiSetPlanParams(self, camstate, cliplimits, badrows, badcols):
+		key = self.newPlan(camstate)
+		newplan = self.plans[key]
+		newplan['clip limits'] = cliplimits
+		newplan['bad rows'] = badrows
+		newplan['bad cols'] = badcols
+		return ''
+
+	def uiAcquireDark(self):
+		dark = self.acquireReference(dark=True)
+		print 'Dark Stats: %s' % (self.stats(dark),)
+		mrcstr = Mrc.numeric_to_mrcstr(dark)
+		return xmlrpclib.Binary(mrcstr)
+
+	def uiAcquireBright(self):
+		bright = self.acquireReference(dark=False)
+		print 'Bright Stats: %s' % (self.stats(bright),)
+		mrcstr = Mrc.numeric_to_mrcstr(bright)
+		return xmlrpclib.Binary(mrcstr)
+
+	def uiAcquireCorrected(self):
+		camstate = self.camconfigdata.get()
+		self.cameraState(camstate)
+		imdata = self.acquireCorrectedArray()
+		print 'Corrected Stats: %s' % (self.stats(imdata),)
+		mrcstr = Mrc.numeric_to_mrcstr(imdata)
+		return xmlrpclib.Binary(mrcstr)
+
+	def newPlan(self, camstate):
+		key = self.planKey(camstate)
+		if key not in self.plans:
+			plan = CorrectorPlan(key)
+			self.plans[key] = plan
+		return key
+
+	def updatePlan(self, plankey, itemkey, value):
+		self.plans[plankey][itemkey] = value
 
 	def planKey(self, camstate):
 		'''
-		make a hash key from a camera state
+		make a string key from a camera state
 		'''
 		dim = camstate['dimension']
 		bin = camstate['binning']
 		off = camstate['offset']
 		## ignore exposure time for now
-		##camstate['exposure time']
-		key = (dim['x'],dim['y'],bin['x'],bin['y'],off['x'],off['y'])
-		return key
+		## exp = self.camstate['exposure time']
 
-	def savePlan(self, filename, key):
-		strkey = str(key)
-		s = shelve.open(filename)
-		try:
-			s[strkey] = self.plan[key]
-		except KeyError:
-			pass
-		s.close()
-
-	def loadPlan(self, filename, key):
-		strkey = str(key)
-		s = shelve.open(filename)
-		print 'trying to find %s in plan file' % strkey
-		try:
-			self.plan[key] = s[strkey]
-			print 'got %s in plan file' % (key,)
-		except KeyError:
-			pass
-		s.close()
+		tuplekey = (dim['x'],dim['y'],bin['x'],bin['y'],off['x'],off['y'])
+		strtuplekey = map( lambda a: str(int(a)), tuplekey)
+		strkey = string.join(strtuplekey, '_')
+		return strkey
 
 	def acquireSeries(self, n):
 		series = []
 		for i in range(n):
 			print 'acquiring %s of %s' % (i+1, n)
-			numimage = self.cameraAcquire()
+			numimage = self.cameraAcquireArray()
 			series.append(numimage)
 		return series
 
-	def acquireBright(self):
-		camstate = dict(self.camdata.get())
-		self.cameraState(camstate)
-		plankey = self.planKey(camstate)
-
-		navg = self.navgdata.get()
-		series = self.acquireSeries(navg)
-		bright = cameraimage.averageSeries(series)
-
-		self.plan[plankey]['bright'] = bright
-
-		if key not in self.plan:
-			self.plan[key] = {'dark':None,'bright':None,'norm':None}
-		self.plan[key]['bright'] = bright
-
-		imagedata = data.BrightImageData(self.ID(), bright)
-		self.publish(imagedata, event.BrightImagePublishEvent)
-
-		print 'bright stats', self.stats(bright)
-		self.calc_norm(key)
-		return ''
-
-	def acquireDark(self):
-		camstate = dict(self.camdata.get())
-		camstate['exposure time'] = 0.0
-		self.cameraState(camstate)
-
-		navg = self.navgdata.get()
-		series = self.acquireSeries(navg)
-		dark = cameraimage.averageSeries(series)
-
-		key = self.planKey(camstate)
-		if key not in self.plan:
-			self.plan[key] = {'dark':None,'bright':None,'norm':None}
-		self.plan[key]['dark'] = dark
-
-		imagedata = data.DarkImageData(self.ID(), dark)
-		self.publish(imagedata, event.DarkImagePublishEvent)
-
-		print 'dark stats', self.stats(dark)
-		self.calc_norm(key)
-		return ''
-
-	def removeBadPixels(self, image):
-		return image
-
-	def clip(self, image):
-		if self.clipflag.get():
-			minclip = self.clipmin.get()
-			maxclip = self.clipmax.get()
-			return Numeric.clip(image, minclip, maxclip)
+	def acquireReference(self, dark=False):
+		camstate = self.camconfigdata.get()
+		tempcamstate = self.camconfigdata.get()
+		if dark:
+			tempcamstate['exposure time'] = 0.0
+			typekey = 'dark'
+			datatype = data.DarkImageData
+			pubtype = event.DarkImagePublishEvent
 		else:
-			return image
+			typekey = 'bright'
+			datatype = data.BrightImageData
+			pubtype = event.BrightImagePublishEvent
 
+		self.cameraState(tempcamstate)
+
+		navg = self.navgdata.get()
+		series = self.acquireSeries(navg)
+		ref = cameraimage.averageSeries(series)
+
+		plankey = self.newPlan(camstate)
+
+		self.plans[plankey][typekey] = ref
+
+		imagedata = datatype(self.ID(), ref)
+		self.publish(imagedata, pubtype)
+
+		self.calc_norm(plankey)
+		return ref
+
+	def calc_norm(self, key):
+		if self.plans[key]['bright'] is None:
+			return
+		if self.plans[key]['dark'] is None:
+			return
+		bright = self.plans[key]['bright']
+		dark = self.plans[key]['dark']
+
+		norm = bright - dark
+
+		## there may be a better normavg than this
+		normavg = cameraimage.mean(norm)
+
+		# division may result infinity or zero division
+		# so make sure there are no zeros in norm
+		norm = Numeric.clip(norm, 1.0, cameraimage.inf)
+		norm = normavg / norm
+		self.plans[key]['norm'] = norm
+		
 	def acquireCorrectedImageData(self):
+		numdata = self.acquireCorrectedArray()
+		return data.ImageData(self.ID(), numdata)
+
+	def acquireCorrectedArray(self):
 		if self.fakeflag.get():
-			return self.__acquireCorrectedFakeImageData()
+			camstate = self.camconfigdata.get()
+			numimage = Mrc.mrc_to_numeric('test1.mrc')
 		else:
-			return self.__acquireCorrectedImageData()
+			camstate = self.cameraAcquireCamera()
+			numimage = camstate['image data']
+		corrected = self.correct(numimage, camstate)
+		return corrected
 
-	def __acquireCorrectedImageData(self):
-		camstate = self.cameraAcquireCamera()
-		numimage = camstate['image data']
-		#binning = self.researchByDataID('binning').content['binning']
-		key = self.planKey(camstate)
-		corrected = self.correct(numimage, key)
-		return data.ImageData(self.ID(), corrected)
-
-	def correct(self, original, key):
+	def correct(self, original, camstate):
 		'''
 		this puts an image through a pipeline of corrections
 		'''
+		key = self.newPlan(camstate)
 		normalized = self.normalize(original, key)
-		touchedup = self.removeBadPixels(normalized)
-		clipped = self.clip(touchedup)
+		touchedup = self.removeBadPixels(normalized, key)
+		clipped = self.clip(touchedup, key)
 		return clipped
 
-	def acquireCorrected(self, ievent=None):
-		correctdata = self.acquireCorrectedImageData()
-		self.publish(correctdata, event.ImagePublishEvent)
-		return ''
+	def removeBadPixels(self, image, key):
+		rows = self.plans[key]['bad rows']
+		cols = self.plans[key]['bad cols']
 
-	def __acquireCorrectedFakeImageData(self):
-		numimage = mrc_to_numeric('test1.mrc')
-		camstate = self.camdata.get()
-		print 'camstate', camstate
-		key = self.planKey(camstate)
-		print 'key', key
-		corrected = self.correct(numimage, key)
-		return data.ImageData(self.ID(), corrected)
+		print 'Removing bad rows %s and cols %s' % (rows, cols)
+		for row in rows:
+			cameraimage.zeroRow(image, row)
+		for col in cols:
+			cameraimage.zeroCol(image, col)
+		return image
 
-	def acquireCorrectedFake(self, ievent=None):
-		correctdata = self.acquireCorrectedFakeImageData()
-		self.publish(correctdata, event.ImagePublishEvent)
-		return ''
-
-	def stats(self, im):
-		mean = cameraimage.mean(im)
-		stdev = cameraimage.stdev(im)
-		mn = cameraimage.min(im)
-		mx = cameraimage.max(im)
-		return {'mean':mean,'stdev':stdev,'min':mn,'max':mx}
-
-	def calc_norm(self, key):
-		if key not in self.plan:
-			self.plan[key] = {'dark':None,'bright':None,'norm':None}
-		bright = self.plan[key]['bright']
-		dark = self.plan[key]['dark']
-		if bright is not None and dark is not None:
-			norm = bright - dark
-			## there may be a better norm than this
-			normavg = cameraimage.mean(norm)
-
-			# division may result infinity or zero division
-			# so make sure there are no zeros in norm
-			norm = Numeric.clip(norm, 1.0, cameraimage.inf)
-			norm = normavg / norm
-			self.plan[key]['norm'] = norm
-			
-			print 'saving plan'
-			self.saveRefs('correction_plan', key)
+	def clip(self, image, key):
+		minclip, maxclip = self.plans[key]['clip limits']
+		if minclip is None and maxclip is None:
+			return image
+		if minclip is None:
+			minclip = -cameraimage.inf
+		if maxclip is None:
+			maxclip = cameraimage.inf
+		return Numeric.clip(image, minclip, maxclip)
 
 	def normalize(self, raw, key):
-		if key not in self.plan:
-			print 'loading plan'
-			self.loadRefs('correction_plan', key)
-		if key not in self.plan:
-			print 'no plan, no correction'
-			return raw
-
-		plan = self.plan[key]
-		dark = plan['dark']
-		norm = plan['norm']
-
+		dark = self.plans[key]['dark']
+		norm = self.plans[key]['norm']
 		if dark is not None and norm is not None:
 			diff = raw - dark
 			## this may result in some infinity values
@@ -256,24 +242,104 @@ class Corrector(node.Node, camerafuncs.CameraFuncs):
 		else:
 			return raw
 
+	def stats(self, im):
+		mean = cameraimage.mean(im)
+		stdev = cameraimage.stdev(im)
+		mn = cameraimage.min(im)
+		mx = cameraimage.max(im)
+		return {'mean':mean,'stdev':stdev,'min':mn,'max':mx}
+
+class CorrectorPlan(object):
+	def __init__(self, id):
+		self.id = id
+		self.plan = {}
+		self.refs = {}
+		self.reftypes = ('dark','bright','norm')
+
+		## defaults
+		self.plan['bad rows'] = ()
+		self.plan['bad cols'] = ()
+		self.plan['clip limits'] = (None, None)
+
+		## load existing state
+		self.load()
+
+		## load existing reference images
+		for reftype in self.reftypes:
+			self.loadRef(reftype)
+
+	def __setitem__(self, key, value):
+		if key in self.reftypes:
+			self.refs[key] = value
+			self.saveRef(key)
+		else:
+			self.plan[key] = value
+			self.save()
+
+	def __getitem__(self, key):
+		if key in self.reftypes:
+			return self.refs[key]
+		else:
+			return self.plan[key]
+
+	def keys(self):
+		return self.plan.keys() + self.refs.keys()
+
+	def __hash__(self):
+		return hash(self.id)
+
+	def planFilename(self):
+		return self.id + '.plan'
+
+	def refFilename(self, reftype):
+		return self.id + '_' + reftype + '.mrc'
+
+	def save(self):
+		filename = self.planFilename()
+		f = open(filename, 'w')
+		cPickle.dump(self.plan, f, 1)
+		f.close()
+
+	def load(self):
+		filename = self.planFilename()
+		try:
+			f = open(filename, 'r')
+		except IOError:
+			print 'creating plan file: %s' % (filename,)
+			self.save()
+			return
+		self.plan = cPickle.load(f)
+		f.close()
+
+	def saveRef(self, reftype):
+		filename = self.refFilename(reftype)
+		if self.refs[reftype] is not None:
+			print 'REF', self.refs[reftype]
+			print 'saving %s' % (filename,)
+			Mrc.numeric_to_mrc(self.refs[reftype], filename)
+
+	def loadRef(self, reftype):
+		filename = self.refFilename(reftype)
+		self.refs[reftype] = Mrc.mrc_to_numeric(filename)
+
 
 if __name__ == '__main__':
 	from Numeric import *
-	from Mrc import *
+	import Mrc
 	from ImageViewer import ImageViewer
 
 	print 'reading darks'
-	dark1 = mrc_to_numeric('/home/pulokas/test_images/dark1.mrc')
-	dark2 = mrc_to_numeric('/home/pulokas/test_images/dark2.mrc')
+	dark1 = Mrc.mrc_to_numeric('/home/pulokas/test_images/dark1.mrc')
+	dark2 = Mrc.mrc_to_numeric('/home/pulokas/test_images/dark2.mrc')
 	print 'averaging darks'
 	dark = numeric_series_average( (dark1,dark2) )
 	print 'reading brights'
-	bright1 = mrc_to_numeric('/home/pulokas/test_images/bright1.mrc')
-	bright2 = mrc_to_numeric('/home/pulokas/test_images/bright2.mrc')
+	bright1 = Mrc.mrc_to_numeric('/home/pulokas/test_images/bright1.mrc')
+	bright2 = Mrc.mrc_to_numeric('/home/pulokas/test_images/bright2.mrc')
 	print 'averaging brights'
 	bright = numeric_series_average( (bright1,bright2) )
 	print 'reading raw'
-	raw = mrc_to_numeric('/home/pulokas/test_images/raw4.mrc')
+	raw = Mrc.mrc_to_numeric('/home/pulokas/test_images/raw4.mrc')
 
 	print 'setting up corrector'
 	corrector = FlatCorrector()
