7c8c4d613b06c96df91cba6279c9bf43144d6716
suloway <suloway>
2003-02-24 19:42:09 +0000

    runapp

--- leginon/runapplication.py
+++ leginon/runapplication.py
@@ -7,60 +7,58 @@ import time
 import os
 import socket
 
+def addNode(id, hostname, port):
+	while id not in m.clients:
+		try:
+			m.addNode(hostname, port)
+		except:
+			time.sleep(0.1)
+
+def createlauncher(id, hostname, port):
+	if socket.gethostname() != hostname:
+		return False
+	#launcherpath = 'c:\\dev\\pyleginon\\launcher.py'
+	launcherpath = '\\\\colossus\\amishare\\suloway\\pyleginon\\launcher.py'
+
+	print 'Attempting to spawn launcher process'
+	if sys.platform == 'win32':
+		os.spawnve(os.P_NOWAIT, 'c:\\Python22\\python.exe',
+				['launcher.py', launcherpath, str(port)], os.environ)
+	else:
+		os.spawnv(os.P_NOWAIT, 'launcher.py', ['launcher.py', str(port)])
+	addNode(id, hostname, port)
+	print 'Launcher process spawned'
+	return True
+
 if __name__ == '__main__':
 
+	print 'Creating manager'
 	m = manager.Manager(('manager',))
+	print 'Manager creation successful'
 
 	# quite hackish for now
 	try:
 		appfile = sys.argv[1]
+		print 'Attempting to load application: %s' % appfile
 		m.app.load(appfile)
+		print 'Load successful'
 		print m.app.getLaunchers()
 		for l in m.app.getLaunchers():
-	#		if l[0] == 'manager':
-	#			# if its in the manager's process, it must be started
-	#			m.newLauncher(l)
-	#			while l not in m.clients:
-	#				time.sleep(0.1)
-	#		else:
-				# if not, try to add it at a standard port
+			# try to add launcher at a standard port
+			hostname = l[-1]
+			port = 55555
 			try:
-				m.addNode(l[-1], 55555)
-				while l not in m.clients:
-					time.sleep(0.1)
+				print 'Trying to contact launcher %s at %s:%s' % (l, hostname, port)
+				addNode(l, hostname, port)
 			except:
-				# if its was on this machine, exec it
-				if socket.gethostname() == l[-1]:
-					print "Attempting to create local launcher process..."
-					if sys.platform == 'win32':
-						os.spawnve(os.P_NOWAIT, 'c:\\Python22\\python.exe',
-							#['launcher.py', 'c:\\dev\\pyleginon\\launcher.py', '55555'],
-							['launcher.py', '\\\\colossus\\amishare\\suloway\\pyleginon\\launcher.py', '55555'],
-							os.environ)
-					else:
-						os.spawnv(os.P_NOWAIT, 'launcher.py', ['launcher.py', '55555'])
-					while l not in m.clients:
-						try:
-							m.addNode(l[-1], 55555)
-						except:
-							time.sleep(0.25)
-				else:
-					print 'Unable to activate launcher %s' % l
+				print 'Contact launcher %s at %s:%s failed' % (l, hostname, port)
+				if not createlauncher(l, hostname, port):
+					print 'Error: launcher %s is not available' % l
 		m.app.launch()
 	except IndexError:
-		print "Attempting to create local launcher process..."
-		if sys.platform == 'win32':
-			os.spawnve(os.P_NOWAIT, 'c:\\Python22\\python.exe',
-#				['launcher.py', 'c:\\dev\\pyleginon\\launcher.py', '55555'], os.environ)
-				['launcher.py', '\\\\colossus\\amishare\\suloway\\pyleginon\\launcher.py', '55555'], os.environ)
-		else:
-			os.spawnv(os.P_NOWAIT, 'launcher.py', ['launcher.py', '55555'])
-		l = (socket.gethostname(),)
-		while l not in m.clients:
-			try:
-				m.addNode(l[-1], 55555)
-			except:
-				time.sleep(0.25)
+		hostname = socket.gethostname()
+		port = 55555
+		createlauncher((hostname,), hostname, port)
 
 	#m.start()
 
@@ -69,7 +67,7 @@ if __name__ == '__main__':
 
 	loc = m.location()
 	# jorg
-	time.sleep(1.0)
+	#time.sleep(1.0)
 	gui = nodegui.NodeUILauncher(root, loc['hostname'], loc['UI port'])
 	gui.pack(expand=Tkinter.YES, fill=Tkinter.BOTH)
 	root.mainloop()
