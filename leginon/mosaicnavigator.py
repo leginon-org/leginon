37a04a219d98ffb23816cd58c7ba05ad9105a12a
suloway <suloway>
2003-03-26 04:06:41 +0000

    more reorganizing

--- leginon/mosaicnavigator.py
+++ leginon/mosaicnavigator.py
@@ -38,8 +38,8 @@ class MosaicNavigator(navigator.Navigator):
 			offset = mosaicdata[tile]['offset']
 			offsetposition = (position[0] + offset[0], position[1] + offset[1])
 			shape = mosaicdata[tile]['shape']
-			deltaposition = (-(row - offsetposition[0] - shape[0] / 2),
-												-(column - offsetposition[1] - shape[1] / 2))
+			deltaposition = (-(row - offsetposition[0] - shape[0]/2),
+												-(column - offsetposition[1] - shape[1]/2))
 			magnitude = math.sqrt((deltaposition[0])**2 + (deltaposition[1])**2)
 			if magnitude < maxmagnitude:
 				maxmagnitude = magnitude
@@ -60,21 +60,4 @@ class MosaicNavigator(navigator.Navigator):
 
 		# hmm?
 		self.acquireImage()
-
-if __name__ == '__main__':
-	foo = MosaicNavigator(('fake id',), [])
-	foo.addStateMosaic(data.StateMosaicData(('data', 0),
-				{'image data ID': ('image', 0),
-				('tile', 0): {'position': (0, 0), 'state': 'state 0'},
-				('tile', 1): {'position': (5, 5), 'state': 'state 1'},
-				('tile', 2): {'position': (10, 10), 'state': 'state 2'},
-				('tile', 3): {'position': (15, 15), 'state': 'state 3'}}))
-
-	foo.handleImageClick(event.ImageClickEvent(('event', 0),
-		{'image id': ('image', 0),
-		'array row': 5,
-		'array column': 5,
-		'array shape': (256, 256)}))
-
-	print foo.statemosaic
 
