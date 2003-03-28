61a89ee9078e60dc3e30f5ca2b7fca4789427ec8
pulokas <pulokas>
2003-03-28 18:56:14 +0000

    big mess of deepcopies in attempt to fix a bug in reality it was probably just a problem with modifying a class that has instances in a pickle

--- leginon/presets.py
+++ leginon/presets.py
@@ -3,10 +3,11 @@ import data
 import event
 import datahandler
 import cPickle
+import copy
 
-class PresetDict(dict):
+class StrictDict(dict):
 	'''
-	PresetDict(keys)
+	StrictDict(keys)
 	   keys - sequence of keys that are allowed in the dict
 
 	Subclassed from dict, with the following differences:
@@ -36,47 +37,111 @@ class PresetDict(dict):
 	def update(self, obj):
 		for key in obj.keys(): self[key] = obj[key]
 
+	def diff(self, other):
+		return self.__diffValues(dict(self), dict(other))
+
+	def __sub__(self, other):
+		return self.diff(other)
+
+	def significant(self):
+		'''
+		return copy of self with only the significant items
+		'''
+		pass
+
+	def __equalKeys(self, keys1, keys2):
+		if len(keys1) != len(keys2):
+			return 0
+		for key in keys1:
+			if key not in keys2:
+				return 0
+		return 1
+
+	def __nonzeroValues(self, value):
+		'''
+		return true if at least one nonzero item in value
+		'''
+		if isinstance(value, dict):
+			#### dict objects
+			for key, value in value.items():
+				if self.__nonzeroValues(value):
+					return 1
+			return 0
+		else:
+			#### other objects
+			if value:
+				return 1
+			else:
+				return 0
+
+	def __diffValues(self, value1, value2):
+		try:
+			diff = value1 - value2
+		except TypeError:
+			if type(value1) == type(value2) == dict:
+				## dict subtraction
+				if not self.__equalKeys(value1.keys(), value2.keys()):
+					raise ValueError('keys are not same')
+				diff = {}
+				for key in value1:
+					diff[key] = self.__diffValues(value1[key], value2[key])
+			else:
+				## don't know what to do with this type
+				raise
+
+		return diff
 
 ### most common allowed keys for PresetDict
-STANDARD_KEYS = (
+PRESET_KEYS = (
 	'spot size',
 	'magnification',
 	'image shift',
 	'beam shift',
 	'intensity',
 	'defocus',
+
 	'dimension',
 	'binning',
+	'offset',
 	'exposure time'
 )
 
+class PresetDict(StrictDict):
+	def __init__(self):
+		StrictDict.__init__(self, PRESET_KEYS)
+
 
 class PresetsClient(object):
-	def __init__(self, node, allowed_keys):
+	def __init__(self, node):
 		self.node = node
-		self.allowed_keys = allowed_keys
 
 	def getPreset(self, key):
 		try:
 			presetdata = self.node.researchByDataID('presets')
+			print 'PRESETS', presetdata.content
 		except:
 			print 'PresetClient unable to use presets.  Is a PresetsManager node running?'
 			raise
 		try:
-			presetvalue = presetdata.content[key]
+			print 'presetkey', key
+			presetdict = presetdata.content[key]
+			dictcopy = copy.deepcopy(presetdict)
+			presetvalue = PresetDict()
+			presetvalue.update(dictcopy)
 		except KeyError:
 			print '%s is not in presets' % (key,)
 			raise
 		return presetvalue
 
 	def setPreset(self, key, presetdict):
-		newdict = {key: presetdict}
+		dictcopy = copy.deepcopy(dict(presetdict))
+		newdict = {key: dictcopy}
 		dat = data.PresetData('presets', newdict)
 		self.node.publishRemote(dat)
 
 	def toScope(self, presetdict):
 		d = dict(presetdict)
-		### this seems to work event if the preset contains camera keys
+		### this seems to work even if the preset contains camera keys
 		emdata = data.EMData('scope', d)
 		self.node.publishRemote(emdata)
 
@@ -84,7 +149,7 @@ class PresetsClient(object):
 		'''
 		return a new preset 
 		'''
-		p = PresetDict(self.allowed_keys)
+		p = PresetDict()
 		scope = self.node.researchByDataID('scope')
 		camera = self.node.researchByDataID('camera no image data')
 		p.update(scope.content)
@@ -100,13 +165,14 @@ class DataHandler(datahandler.DataBinder):
 	def query(self, id):
 		cal = self.node.getPresets()
 		result = data.PresetData(self.ID(), cal)
+		print 'RESULT', result.content
 		return result
 
 	def insert(self, idata):
 		if isinstance(idata, event.Event):
 			datahandler.DataBinder.insert(self, idata)
 		else:
-			self.node.setPresets(idata)
+			self.node.setPresets(idata.content)
 
 	# borrowed from NodeDataHandler
 	def setBinding(self, eventclass, func):
@@ -123,9 +189,9 @@ class PresetsManager(node.Node):
 		ids = ['presets',]
 		e = event.ListPublishEvent(self.ID(), ids)
 		self.outputEvent(e)
-
 		
-		self.presetsclient = PresetsClient(self, STANDARD_KEYS)
+		self.presetsclient = PresetsClient(self)
+		self.current = None
 
 		self.defineUserInterface()
 		self.start()
@@ -133,20 +199,28 @@ class PresetsManager(node.Node):
 	def getPresets(self):
 		try:
 			f = open('PRESETS', 'r')
-			presets = cPickle.load(f)
+			presetdict = cPickle.load(f)
+			presetdict = copy.deepcopy(presetdict)
 			f.close()
 		except IOError:
 			print 'unable to open PRESETS'
-			presets = {}
+			presetdict = {}
 		except EOFError:
 			print 'bad pickle in PRESETS'
-			presets = {}
-		return presets
+			presetdict = {}
+		return presetdict
 
-	def setPresets(self, idata):
+	def getPreset(self, name):
 		presets = self.getPresets()
-		newdict = idata.content
-		presets.update(newdict)
+		return copy.deepcopy(presets[name])
+
+	def setPreset(self, name, preset):
+		newdict = {name: preset}
+		self.setPresets(newdict)
+
+	def setPresets(self, preset):
+		presets = self.getPresets()
+		presets.update(preset)
 		## should make a backup before doing this
 		f = open('PRESETS', 'w')
 		cPickle.dump(presets, f, 1)
@@ -165,10 +239,12 @@ class PresetsManager(node.Node):
 
 	def uiStoreCurrent(self, name):
 		preset = self.presetsclient.fromScope()
-		self.presetsclient.setPreset(name, preset)
+		## is this the problem
+		presetdict = dict(preset)
+		self.setPreset(name, presetdict)
 		return ''
 
 	def uiRestore(self, name):
-		preset = self.presetsclient.getPreset(name)
+		preset = self.getPreset(name)
 		self.presetsclient.toScope(preset)
 		return ''
