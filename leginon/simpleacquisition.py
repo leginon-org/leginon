8f3a7907e4690406e501e04b09ce1872c095f509
pulokas <pulokas>
2003-04-03 20:42:35 +0000

    added looping image acquire

--- leginon/simpleacquisition.py
+++ leginon/simpleacquisition.py
@@ -7,6 +7,7 @@ import time
 import cameraimage
 import camerafuncs
 import presets
+import threading
 
 class SimpleAcquisition(node.Node):
 	def __init__(self, id, nodelocations, **kwargs):
@@ -15,19 +16,21 @@ class SimpleAcquisition(node.Node):
 		self.cam = camerafuncs.CameraFuncs(self)
 		self.presetsclient = presets.PresetsClient(self)
 
-		## default camera config
+		self.loopstop = threading.Event()
+		self.looplock = threading.Lock()
 
 		self.defineUserInterface()
 		self.start()
 
-	def acquireImage(self):
+	def setPreset(self):
 		presetname = self.presetname.get()
 		print 'going to preset %s' % (presetname,)
-		preset = self.presetsclient.getPreset(presetname)
-		print 'preset mag:', preset['magnification']
-		self.presetsclient.toScope(preset)
+		self.preset = self.presetsclient.getPreset(presetname)
+		print 'preset mag:', self.preset['magnification']
+		self.presetsclient.toScope(self.preset)
 		time.sleep(2)
 
+	def acquireImage(self):
 		print 'acquiring image'
 		acqtype = self.acqtype.get()
 		if acqtype == 'raw': imagedata = self.cam.acquireCameraImageData(None,0)
@@ -41,13 +44,47 @@ class SimpleAcquisition(node.Node):
 		if imagedata is None:
 			return
 		## attach preset to imagedata
-		imagedata.content['preset'] = dict(preset)
+		imagedata.content['preset'] = dict(self.preset)
 
 		print 'publishing image'
 		self.publish(imagedata, event.CameraImagePublishEvent)
 		print 'image published'
 
-		## for xmlrpc
+	def acquireImageOne(self):
+		self.setPreset()
+		self.acquireImage()
+		return ''
+
+	def acquireImageLoop(self, pausetime):
+		if not self.looplock.acquire(0):
+			return
+		try:
+			t = threading.Thread(target=self.loop,args=(pausetime,))
+			t.setDaemon(1)
+			t.start()
+		except:
+			try:
+				self.looplock.release()
+			except:
+				pass
+			raise
+		return ''
+
+	def loop(self, pausetime):
+		self.setPreset()
+		self.loopstop.clear()
+		while 1:
+			if self.loopstop.isSet():
+				break
+			self.acquireImage()
+			time.sleep(pausetime)
+		try:
+			self.looploock.release()
+		except:
+			pass
+
+	def acquireImageLoopStop(self):
+		self.loopstop.set()
 		return ''
 
 	def defineUserInterface(self):
@@ -60,7 +97,11 @@ class SimpleAcquisition(node.Node):
 
 		prefs = self.registerUIContainer('Preferences', (self.acqtype,self.presetname))
 
-		acq = self.registerUIMethod(self.acquireImage, 'Acquire', ())
+		acq = self.registerUIMethod(self.acquireImageOne, 'Acquire', ())
+		pausetime = self.registerUIData('Pause Time', 'float', default=0)
+		acqloop = self.registerUIMethod(self.acquireImageLoop, 'Acquire Loop', (pausetime,))
+		acqloopstop = self.registerUIMethod(self.acquireImageLoopStop, 'Stop', ())
+		acqcont = self.registerUIContainer('Acquire', (acq, acqloop, acqloopstop))
 
-		self.registerUISpec('Navigator', (acq, prefs, nodeui))
+		self.registerUISpec('Simple Acquisition', (acqcont, prefs, nodeui))
 
