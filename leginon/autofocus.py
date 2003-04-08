655ddddfb21305a875a3ca6e88c4d64bcdd27bb0
pulokas <pulokas>
2003-04-08 00:11:02 +0000

    changed this to be the new autofocus node, not done

--- leginon/autofocus.py
+++ leginon/autofocus.py
@@ -1,37 +1,6 @@
-import data
-import time
-import matrixcalibrator
-import node
 
-class AutoFocusCalibration(node.Node, matrixcalibrator.MatrixCalibrator):
-	def __init__(self, id, nodelocations, **kwargs):
-		calibration.Calibration.__init__(self, id, nodelocations, **kwargs)
-		self.axislist = ['x']
-		self.defocus = 0.0001
-		self.deltadefocus
+import acquisition
+import node, data, event
 
-	def state(self, value, axis):
-		return {'beam tilt': {axis: value}}
-
-	def calibrate(self):
-		emdata = data.EMData('defocus', {'defocus': self.defocus})
-		self.publishRemote(emdata)
-		time.sleep(1.0)
-
-		cal1 = calibration.Calibration.calibrate(self)
-
-		emdata = data.EMData('defocus',
-			{'defocus': self.defocus + self.deltadefocus})
-		self.publishRemote(emdata)
-		time.sleep(1.0)
-
-		cal2 = calibration.Calibration.calibrate(self)
-
-		cal = {'autofocus': {}}
-		cal['autofocus']['x shift'] = cal2['x shift']['x'] - cal1['x shift']['x'] / self.deltadefocus
-		cal['autofocus']['y shift'] = cal2['x shift']['y'] - cal1['x shift']['y'] / self.deltadefocus
-		# calibrate needs to take a specific value
-		cal['autofocus']['beam tilt'] = cal2['x shift']['value']
-
-		return cal
-
+class Focuser(acquisition.Acquisition):
+	pass
