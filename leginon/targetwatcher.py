c3920e37d2fabe0b89fae5a1f1b624498254ddf8
pulokas <pulokas>
2003-04-09 22:50:37 +0000

    general fixes and new way of inheriting defineUserInterface

--- leginon/targetwatcher.py
+++ leginon/targetwatcher.py
@@ -2,21 +2,36 @@
 
 import event, data
 import watcher
+import threading
 
 class TargetWatcher(watcher.Watcher):
 	def __init__(self, id, nodelocations, **kwargs):
 		watchfor = event.ImageTargetListPublishEvent
 		watcher.Watcher.__init__(self, id, nodelocations, watchfor, lockblocking=0, **kwargs)
+		self.abort = threading.Event()
 
 	def processData(self, newdata):
-		targetlist = newdata.content
-		for target in targetlist:
-			self.processTargetData(target)
+		'''
+		accepts either ImageTargetData or ImageTargetListData
+		'''
+		if isinstance(newdata, data.ImageTargetData):
+			self.processTargetData(newdata)
+		elif isinstance(newdata, data.ImageTargetListData):
+			targetlist = newdata.content
+			print 'TARGETLIST len', len(targetlist)
+			self.abort.clear()
+			for target in targetlist:
+				print 'targetid', target.id
+				self.processTargetData(target)
+				print 'checking abort'
+				if self.abort.isSet():
+					print 'breaking from targetlist loop'
+					break
+				print 'not aborted'
 
 	def processTargetData(self, targetdata):
 		raise NotImplementedError()
 
-	def defineUserInterface(self):
-		return watcher.Watcher.defineUserInterface(self)
-
-
+	def abortTargetListLoop(self):
+		print 'will abort target list loop after current target'
+		self.abort.set()
