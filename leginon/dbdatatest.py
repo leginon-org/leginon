5c656b504d5dd025308b425ca0837007ec3e1df7
pulokas <pulokas>
2003-04-28 22:44:29 +0000

    *** empty log message ***

--- leginon/dbdatatest.py
+++ leginon/dbdatatest.py
@@ -4,7 +4,8 @@ import data
 
 print 'ORIGINAL'
 
-############# create my original data ################
+###### create two instances of PresetImageData to insert ###########
+
 scopedict = {'magnification': 1501, 'beam tilt': {'x':1.1,'y':2.2}}
 cameradict = {'exposure time': 510, 'binning': {'x': 1, 'y':1}}
 ## PresetData
@@ -20,12 +21,11 @@ mydata = data.PresetImageData(('pidata', 1))
 mydata['preset'] = pdata
 mydata['scope'] = scopedict
 mydata['camera'] = cameradict
-
 print 'MYDATA'
 print mydata
 
 scopedict2 = {'magnification': 1801, 'beam tilt': {'x':5.1,'y':7.2}}
-cameradict2 = {'exposure time': 810, 'binning': {'x': 3, 'y':3}}
+cameradict2 = {'exposure time': 810, 'binning': {'x': 1, 'y':1}}
 ## PresetData
 pdata2 = data.PresetData(('pdata',2))
 pdata2['name'] = 'square1'
@@ -39,13 +39,34 @@ mydata2 = data.PresetImageData(('pidata', 2))
 mydata2['preset'] = pdata2
 mydata2['scope'] = scopedict2
 mydata2['camera'] = cameradict2
-
 print 'MYDATA2'
 print mydata2
+
+##### create another instance of PresetImageData for the query  #####
+
+cameradict3 = {'binning': {'x': 3, 'y':3}}
+## PresetData
+pdata3 = data.PresetData(('pdata',2))
+pdata3['id'] = None
+pdata2['magnification'] = 1900
+## PresetImageData: contains PresetData
+mydata3 = data.PresetImageData(('pidata', 2))
+mydata3['id'] = None
+mydata3['preset'] = pdata3
+mydata3['camera'] = cameradict3
+print 'MYDATA3'
+print mydata3
+
+### THIS IS THE FINAL FORMAT FOR HOW NODE GETS DATA
+#self.publish(idata, database=True)
+#mydata = self.research(qdata)
+
 
 mylist = []
 import random
 
+### this is the function is never called directly
+### It is called inside the depthFirst() call below
 def testfunc(mydata):
 	print 'TESTFUNC'
 	print mydata
@@ -54,22 +75,19 @@ def testfunc(mydata):
 	mylist.append(mydata)
 
 	### insert could be done here and retval should be last insert id
-	####### INSERT
+	####### INSERT HERE
 	rnum = random.randrange(100000)
 
 	retval = data.DataReference()
-	retval['id'] =rnum
+	retval['id'] = rnum
 	retval['classname'] = mydata.__class__.__name__
 	#print '    RETURNING',  retval
 	return retval
 
 # this executes testfunc on children of mydata
 # and then on mydata itself.  Children are replaced with a DataReference
+## this should be called either in dbdatakeeper or at a lower level
 funcinfo = mydata.depthFirst(testfunc)
 
 print 'MYLIST'
 print mylist
-
-#print ''
-#print 'IDATA3'
-#print idata3
