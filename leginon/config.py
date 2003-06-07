bbcf213728338e53747ab995b7a0d2338ba25dd1
pulokas <pulokas>
2003-06-07 00:18:37 +0000

    added image path

--- leginon/config.py
+++ leginon/config.py
@@ -1,13 +1,16 @@
 """
 config: Configuration file
 """
+import os
 
 #########################
 #	Database	#
 #########################
-
 DB_HOST		= 'cronus2'
 DB_NAME		= 'dbemdata'
 DB_USER		= 'usr_object'
 DB_PASS		= ''
 
+### where to store images
+### use os.getcwd() for current directory
+IMAGE_PATH	= os.getcwd()
