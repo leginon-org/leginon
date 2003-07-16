32d7144bb4e167ee67b31d082734170f06dba9ff
pulokas <pulokas>
2003-07-16 19:13:35 +0000

    fixed problem with publishing target list in loop switched to TargetListDone instead of TargetDone

--- leginon/targetwatcher.py
+++ leginon/targetwatcher.py
@@ -5,16 +5,23 @@ import watcher
 import threading
 
 class TargetWatcher(watcher.Watcher):
+	'''
+	TargetWatcher will watch for TargetLists
+	It is also initialized with a specific type of target that it can
+	process.  All other targets are republished in another target list.
+	'''
 	def __init__(self, id, session, nodelocations, targetclass=data.ImageTargetData, **kwargs):
 		watchfor = event.ImageTargetListPublishEvent
 		watcher.Watcher.__init__(self, id, session, nodelocations, watchfor, lockblocking=0, **kwargs)
 
 		## moved here from acquisition, hope this doesn't break it
 		self.addEventInput(event.TargetDoneEvent, self.handleTargetDone)
+		self.addEventInput(event.TargetListDoneEvent, self.handleTargetListDone)
 
 		self.abort = threading.Event()
 		self.targetclass = targetclass
-		self.targetevents = {}
+		#self.targetevents = {}
+		self.targetlistevents = {}
 
 	def processData(self, newdata):
 		'''
@@ -27,6 +34,7 @@ class TargetWatcher(watcher.Watcher):
 
 		### separate the good targets from the rejects
 		targetlist = newdata['targets']
+		print self.id, 'RECEIVED TARGET LIST', len(targetlist)
 		goodtargets = []
 		rejects = []
 		for target in targetlist:
@@ -37,16 +45,20 @@ class TargetWatcher(watcher.Watcher):
 
 		### republish the rejects
 		if rejects:
+			print self.id, 'PUBLISHING REJECTS', len(rejects)
 			newtargetlist = data.ImageTargetListData(id=self.ID(), targets=rejects)
 			self.passTargets(newtargetlist)
 
 		### process the good ones
 		self.abort.clear()
+		print self.id, 'PROCESSING GOOD', len(goodtargets)
 		for target in goodtargets:
+			print 'STARTING NEW TARGET', target['id']
 			print 'python id', id(target)
 			print 'target id', target['id']
 			print 'target id id', id(target['id'])
 			newstatus = self.processTargetData(target)
+			print 'NEWSTATUS', newstatus
 			e = event.TargetDoneEvent(id=self.ID(), targetid=target['id'], status=newstatus)
 			self.outputEvent(e)
 			print 'checking abort'
@@ -57,16 +69,19 @@ class TargetWatcher(watcher.Watcher):
 
 	def passTargets(self, targetlistdata):
 		## create an event watcher for each target we pass
-		for target in targetlistdata['targets']:
-			targetid = target['id']
-			## maybe should check if already waiting on this target?
-			self.targetevents[targetid] = {}
-			self.targetevents[targetid]['received'] = threading.Event()
-			self.targetevents[targetid]['status'] = 'waiting'
-			print 'publishing focustargetdata', targetid
-			self.publish(targetlistdata, pubevent=True)
+		#for target in targetlistdata['targets']:
+		#	targetid = target['id']
+		#	## maybe should check if already waiting on this target?
+		#	self.targetevents[targetid] = {}
+		#	self.targetevents[targetid]['received'] = threading.Event()
+		#	self.targetevents[targetid]['status'] = 'waiting'
 
-	def handleTargetDone(self, targetdoneevent):
+		self.targetlistevents[targetlistdata['id']['received'] = threading.Event()
+		self.targetlistevents[targetlistdata['id']]['stats'] = 'waiting'
+		print 'publishing focustargetdata', targetid
+		self.publish(targetlistdata, pubevent=True)
+
+	def OLDhandleTargetDone(self, targetdoneevent):
 		targetid = targetdoneevent['targetid']
 		status = targetdoneevent['status']
 		print 'got targetdone event, setting threading event', targetid
@@ -74,6 +89,15 @@ class TargetWatcher(watcher.Watcher):
 			self.targetevents[targetid]['status'] = status
 			self.targetevents[targetid]['received'].set()
 		self.confirmEvent(targetdoneevent)
+
+	def handleTargetListDone(self, targetlistdoneevent):
+		targetlistid = targetlistdoneevent['targetlistid']
+		status = targetlistdoneevent['status']
+		print 'got targetlistdone event, setting threading event', targetlistid
+		if targetlistid in self.targetlistevents:
+			self.targetlistevents[targetlistid]['status'] = status
+			self.targetlistevents[targetlistid]['received'].set()
+		self.confirmEvent(targetlistdoneevent)
 
 	def processTargetData(self, targetdata):
 		raise NotImplementedError()
