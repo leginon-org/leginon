5ceed915be845385df859e48c672525d8baa947f
pulokas <pulokas>
2003-07-24 00:13:11 +0000

    fixed

--- leginon/EM.py
+++ leginon/EM.py
@@ -195,6 +195,9 @@ class EM(node.Node):
 			'low dose',
 			'low dose mode',
 			'screen current',
+			'holder type',
+			'holder status',
+			'stage status'
 		)
 
 		for key in prunekeys:
@@ -398,13 +401,12 @@ class EM(node.Node):
 
 	def defineUserInterface(self):
 		node.Node.defineUserInterface(self)
-		self.twostep = uidata.Boolean('Two Step Stage Move', False, 'rw', persist=True)
 		self.statestruct = uidata.Struct('Instrument State', {},
 																				'rw', self.uiCallback)
 		self.statestruct.set(self.uistate, callback=False)
 		updatemethod = uidata.Method('Update', self.uiUpdate)
 		unlockmethod = uidata.Method('Unlock', self.uiUnlock)
 		container = uidata.MediumContainer('EM')
-		container.addObjects((self.twostep, self.statestruct, updatemethod, unlockmethod))
+		container.addObjects((self.statestruct, updatemethod, unlockmethod))
 		self.uiserver.addObject(container)
 
