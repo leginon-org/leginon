256420153aeb8fbef9f470b9f33ea6046532f954
suloway <suloway>
2003-08-27 17:18:12 +0000

    print back so know if there

--- leginon/timer.py
+++ leginon/timer.py
@@ -3,10 +3,10 @@ import time
 class Timer(object):
 	def __init__(self, message=''):
 		self.message = message
-#		print 'Timer Start: %s' % (self.message,)
+		print 'Timer Start: %s' % (self.message,)
 		self.t0 = time.time()
 
 	def stop(self):
 		t1 = time.time()
 		tdiff = t1 - self.t0
-#		print 'Timer Stop: %s, Time: %.2f' % (self.message, tdiff)
+		print 'Timer Stop: %s, Time: %.2f' % (self.message, tdiff)
