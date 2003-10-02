097cf04ff974f2ecb8c79175ceb60be7e4c02f30
suloway <suloway>
2003-10-02 22:36:43 +0000

    .

--- leginon/robot.py
+++ leginon/robot.py
@@ -2,34 +2,58 @@ import scopedict
 import tecnai
 import win32com.client
 import time
+import pywintypes
+import sys
+
+class Test(object):
+	def __init__(self):
+		self.Signal0 = 1
+		self.Signal1 = 1
+		self.Signal2 = 1
+		self.Signal3 = 1
+		self.Signal4 = 1
+		self.Signal5 = 1
+		self.Signal6 = 1
+		self.Signal7 = 1
 
 class RobotControl(object):
 	def __init__(self):
 		self.scope = scopedict.factory(tecnai.tecnai)()
-		self.communication = win32com.client.Dispatch('RobotCommunications.Signal')
+		try:
+#			self.communication = win32com.client.Dispatch('RobotCommunications.Signal')
+			self.communication = Test()
+		except pywintypes.com_error:
+			raise RuntimeError('Cannot initialized robot communications')
 
 	def __del__(self):
-		del self.communication
+		self.scope = None
+		self.communication = None
 
 	def insert(self):
 		# verify robot ready for insertion
-		print 'Verifying robot ready for insertion',
+		print 'Verifying robot ready for insertion...',
 		while not self.communication.Signal0:
 			time.sleep(0.5)
+			sys.stdout.softspace = 0
 			print '.',
 		self.communication.Signal0 = 0
+		sys.stdout.softspace = 0
 		print 'Done.'
 
 		# verify no holder in scope
 		print 'Verifying no holder in scope...',
 		if self.scope['holder status'] != 'not inserted':
 			raise RuntimeError('holder in already in scope')
+		sys.stdout.softspace = 0
 		print 'Done.'
 
 		# check if vacuum ready
 		print 'Waiting for vacuum to be ready...',
 		while self.scope['vacuum status'] != 'ready':
 			time.sleep(0.5)
+			sys.stdout.softspace = 0
+			print '.',
+		sys.stdout.softspace = 0
 		print 'Done.'
 
 		# close column valves
@@ -37,6 +61,9 @@ class RobotControl(object):
 		self.scope['column valves'] = 'closed'
 		while self.scope['column valves'] != 'closed':
 			time.sleep(0.5)
+			sys.stdout.softspace = 0
+			print '.',
+		sys.stdout.softspace = 0
 		print 'Done.'
 
 		# turn on turbo pump
@@ -44,6 +71,9 @@ class RobotControl(object):
 		self.scope['turbo pump'] = 'on'
 		while self.scope['turbo pump'] != 'on':
 			time.sleep(0.5)
+			sys.stdout.softspace = 0
+			print '.',
+		sys.stdout.softspace = 0
 		print 'Done.'
 	
 		# wait for led to turn off
@@ -51,15 +81,20 @@ class RobotControl(object):
 		time.sleep(1.0)
 		while self.scope['stage status'] == 'busy':
 			time.sleep(0.5)
+			sys.stdout.softspace = 0
+			print '.',
+		sys.stdout.softspace = 0
 		print 'Done.'
 
 		# robot: load probe first stage, wait to finish
-		print 'Robot, first stage',
+		print 'Robot, first stage...',
 		self.communication.Signal1 = 1
 		while not self.communication.Signal2:
 			time.sleep(0.5)
+			sys.stdout.softspace = 0
 			print '.',
 		self.communication.Signal2 = 0
+		sys.stdout.softspace = 0
 		print 'Done.'
 	
 		# set holder type
@@ -67,6 +102,9 @@ class RobotControl(object):
 		self.scope['holder type'] = 'single tilt'
 		while self.scope['holder type'] != 'single tilt':
 			time.sleep(0.5)
+			sys.stdout.softspace = 0
+			print '.',
+		sys.stdout.softspace = 0
 		print 'Done.'
 
 		# wait for led to turn off
@@ -74,6 +112,9 @@ class RobotControl(object):
 		time.sleep(1.0)
 		while self.scope['stage status'] == 'busy':
 			time.sleep(0.5)
+			sys.stdout.softspace = 0
+			print '.',
+		sys.stdout.softspace = 0
 		print 'Done.'
 
 		# robot: load probe second stage, retract
@@ -81,19 +122,23 @@ class RobotControl(object):
 		self.communication.Signal3 = 1
 		while not self.communication.Signal4:
 			time.sleep(0.5)
+			sys.stdout.softspace = 0
 			print '.',
 		self.communication.Signal4 = 0
+		sys.stdout.softspace = 0
 		print 'Done.'
 
 		# when done turbo off?
 
 	def extract(self):
 		# verify robot ready for extraction
-		print 'Verifying robot ready for extraction',
+		print 'Verifying robot ready for extraction...',
 		while not self.communication.Signal5:
 			time.sleep(0.5)
+			sys.stdout.softspace = 0
 			print '.',
 		self.communication.Signal5 = 0
+		sys.stdout.softspace = 0
 		print 'Done.'
 
 		# verify holder in scope
@@ -106,6 +151,9 @@ class RobotControl(object):
 		print 'Checking if vacuum is ready...',
 		while self.scope['vacuum status'] != 'ready':
 			time.sleep(0.5)
+			sys.stdout.softspace = 0
+			print '.',
+		sys.stdout.softspace = 0
 		print 'Done.'
 
 		# close column valves
@@ -120,15 +168,20 @@ class RobotControl(object):
 		time.sleep(1.0)
 		while self.scope['stage status'] == 'busy':
 			time.sleep(0.5)
+			sys.stdout.softspace = 0
+			print '.',
+		sys.stdout.softspace = 0
 		print 'Done.'
 
 		# robot: unload probe
-		print 'Robot, unload probe',
+		print 'Robot, unload probe...',
 		self.communication.Signal6 = 1
 		while not self.communication.Signal7:
 			time.sleep(0.5)
+			sys.stdout.softspace = 0
 			print '.',
 		self.communication.Signal7 = 0
+		sys.stdout.softspace = 0
 		print 'Done.'
 
 if __name__ == '__main__':
