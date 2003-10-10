3724a00c8364c28b75a1486b192096b4ad7970a2
pulokas <pulokas>
2003-10-10 20:45:36 +0000

    added convolver.py for convolution stuff

---
+++ leginon/convolver.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python
+
+import Numeric
+import fftengine
+
+## chose a FFT engine class
+if fftengine.fftFFTW is None:
+	fftclass = fftengine.fftNumeric
+else:
+	fftclass = fftengine.fftFFTW
+
+class Convolver(object):
+	'''
+	Provides an efficent convolution calculator.
+
+	Create an instance with two optional arguments:
+	     'kernel':  a convolution kernel as a 2-d Numeric array
+	     'image':  the subject image as a 2-d Numeric array
+	These values can also be set later using the setKernel and 
+	setImage methods.  The convolution is executed using the method
+	'convolve', which takes the same two optional arguments.
+
+	A convolver object is efficient at doing a series of convolutions
+	for a couple reasons:
+
+	- Individually convolving one kernel on multiple images:
+	When a new kernel is set, the convolver maintains a dictionary
+	of kernel image FFTs indexed by image shape.  If you want to use
+	the same kernel on a series of images, it only needs to compute
+	the kernel FFT once for every image shape it receives.
+
+	- Convolving a series of kernels on a single image:
+	When a convolution is performed, the FFT of the result is saved.
+	You can execute another convolution on this image using the same or
+	different kernel.  The FFT of the image does not have to be recomputed.
+	'''
+	def __init__(self, kernel=None, image=None):
+		self.fftengine = fftclass()
+
+		self.kernel = None
+		self.shape = None
+		self.image_fft = None
+		self.kernel_fft = {}
+
+		if kernel is not None:
+			self.setKernel(kernel)
+		if image is not None:
+			self.setImage(image)
+		
+	def setKernel(self, kernel):
+		self.kernel = kernel
+		self.kernel_fft = {}
+
+	def setImage(self, image):
+		self.shape = image.shape
+		self.image_fft = self.fftengine.transform(image)
+		self.makeKernelFFT()
+
+	def makeKernelFFT(self):
+		if self.kernel is None:
+			return None
+		if self.shape in self.kernel_fft:
+			return self.kernel_fft[self.shape]
+
+		kim = Numeric.zeros(self.shape, Numeric.Float32)
+		### center the kernel at 0,0 in the image
+		k = self.kernel.astype(Numeric.Float32)
+		kind = Numeric.indices(k.shape)
+		krows = kind[0]
+		kcols = kind[1]
+		kr,kc = self.kernel.shape
+		kr2 = int(kr/2)
+		kc2 = int(kc/2)
+		imrows = krows - kr2
+		imcols = kcols - kc2
+		for i in range(kr*kc):
+			ir = imrows.flat[i]
+			ic = imcols.flat[i]
+			kr = krows.flat[i]
+			kc = kcols.flat[i]
+			kim[ir,ic] = k[kr,kc]
+		kfft = self.fftengine.transform(kim)
+		self.kernel_fft[self.shape] = kfft
+		return kfft
+
+	def convolve(self, image=None, kernel=None, last_image=False):
+		if image is not None and last_image:
+			raise ValueError('Cannot use both a new image and the last image')
+		imfft = self.image_fft
+		if image is not None:
+			self.setImage(image)
+			imfft = self.image_fft
+		if kernel is not None:
+			self.setKernel(kernel)
+		if last_image:
+			imfft = self.result_fft
+
+		kfft = self.makeKernelFFT()
+		self.result_fft = Numeric.multiply(kfft, imfft)
+		result = self.fftengine.itransform(self.result_fft)
+		return result
+
+def laplacian():
+	'''
+	produces laplacian edge detector kernel
+	'''
+	k1 = Numeric.array((1,-1,1,-1,0,-1,1,-1,1),Numeric.Float32)
+	k2 = Numeric.array((0,1,0,1,-4,1,0,1,0), Numeric.Float32)
+	k = 1.0 / 2 * (k1 + k2)
+	k.shape = (3,3)
+	return k.astype(Numeric.Float32)
+
+def gaussian(n, sigma):
+	'''
+	produces gaussian smoothing kernel
+	'''
+	if not n % 2:
+		raise RuntimeError('guassian kernel must have odd size')
+	half = int(n / 2)
+	k1 = 1.0 / (2.0 * Numeric.pi * sigma**2)
+	def i(rows,cols):
+		rows = rows.astype(Numeric.Float32)
+		cols = cols.astype(Numeric.Float32)
+		rows = rows - half
+		cols = cols - half
+		k2 = Numeric.exp(-(rows**2+cols**2) / 2.0 / sigma**2)
+		return k1 * k2
+	k = Numeric.fromfunction(i, (n,n))
+	return k
+
+### smoothing and edge finder
+def laplacian_of_gaussian(n, sigma):
+	if not n % 2:
+		raise ValueError('kernel size must be odd')
+	half = (n - 1) / 2
+	def func(x,y):
+		f1 = (x**2 + y**2) / 2.0 / sigma**2
+		f2 = -1.0 / Numeric.pi / (sigma**4)
+		f3 = 1 - f1
+		f4 = Numeric.exp(-f1)
+		return f2 * f3 * f4
+	k = Numeric.zeros((n,n), Numeric.Float32)
+	for row in range(n):
+		x = row - half
+		for col in range(n):
+			y = col - half
+			k[row,col] = func(x,y)
+	return k
+
+### sobel row and column derivative
+sobel_row = Numeric.array((1,2,1,0,0,0,-1,-2,-1), Numeric.Float32)
+sobel_row.shape = (3,3)
+sobel_col = Numeric.array((1,0,-1,2,0,-2,1,0,-1), Numeric.Float32)
+sobel_col.shape = (3,3)
+
+if __name__ == '__main__':
+	import Mrc
+	import sys
+	import imagefun
+
+	filename = sys.argv[1]
+
+	sobel_row = Numeric.array((1,2,1,0,0,0,-1,-2,-1), Numeric.Float32)
+	sobel_row.shape = (3,3)
+	sobel_col = Numeric.array((1,0,-1,2,0,-2,1,0,-1), Numeric.Float32)
+	sobel_col.shape = (3,3)
+	gauss = imagefun.gaussian_kernel(9, 1.6)
+
+	c = Convolver()
+
+	im = Mrc.mrc_to_numeric(filename)
+
+	c.setImage(image=im)
+	s = c.convolve(kernel=gauss)
+	r = c.convolve(kernel=sobel_row, image=s)
+	c = c.convolve(kernel=sobel_col, image=s)
+	edge = Numeric.sqrt(r**2 + c**2)
+	Mrc.numeric_to_mrc(edge, 'edge.mrc')
