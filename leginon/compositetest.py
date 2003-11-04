4b8339917044db6dd4c8fe1e21add74df006fba3
suloway <suloway>
2003-11-04 00:35:13 +0000

    asdf

--- leginon/compositetest.py
+++ leginon/compositetest.py
@@ -17,11 +17,102 @@ class CompositeImageMaker(object):
 		print 'imageDiameter =', imagediameter
 		return imagediameter
 
+	def circlePoints(self, numericimage, cx, cy, x, y):
+		if x == 0:
+			numericimage[cy + y, cx] = 256
+			numericimage[cy + y, cx] = 256
+			numericimage[cy, cx + y] = 256
+			numericimage[cy, cx - y] = 256
+		elif x == y:
+			numericimage[cy + y, cx + x] = 256
+			numericimage[cy + y, cx - x] = 256
+			numericimage[cy - y, cx + x] = 256
+			numericimage[cy - y, cx - x] = 256
+		elif x < y:
+			numericimage[cy + y, cx + x] = 256
+			numericimage[cy + y, cx - x] = 256
+			numericimage[cy - y, cx + x] = 256
+			numericimage[cy - y, cx - x] = 256
+			numericimage[cy + x, cx + y] = 256
+			numericimage[cy + x, cx - y] = 256
+			numericimage[cy - x, cx + y] = 256
+			numericimage[cy - x, cx - y] = 256
+
+	def circle(self, numericimage):
+		x = 0
+		radius = numericimage.shape[0]/2
+		#radius = int(math.ceil(self.imageDiameter()/2))
+		y = radius
+		xcenter = radius - 1
+		ycenter = radius - 1
+		p = (5 - radius*4)/4
+		self.circlePoints(numericimage, xcenter, ycenter, x, y)
+		while x < y:
+			x += 1
+			if p < 0:
+				p += 2*x + 1
+			else:
+				y -= 1
+				p += 2*(x - y) + 1
+			self.circlePoints(numericimage, xcenter, ycenter, x, y)
+
+	def circlePoints2(self, array, cx, cy, x, y):
+		if x == 0:
+			array[cy + y, cx] = 1
+			array[cy + y, cx] = 1
+			array[cy, cx + y] = 1
+			array[cy, cx - y] = 1
+		elif x == y:
+			array[cy + y, cx + x] = 1
+			array[cy + y, cx - x] = 1
+			array[cy - y, cx + x] = 1
+			array[cy - y, cx - x] = 1
+		elif x < y:
+			array[cy + y, cx + x] = 1
+			array[cy + y, cx - x] = 1
+			array[cy - y, cx + x] = 1
+			array[cy - y, cx - x] = 1
+			array[cy + x, cx + y] = 1
+			array[cy + x, cx - y] = 1
+			array[cy - x, cx + y] = 1
+			array[cy - x, cx - y] = 1
+
+	def circle2(self, array=None):
+		x = 0
+		radius = int(math.ceil(self.imageDiameter()/2))
+		array = Numeric.zeros((radius*2,radius*2), Numeric.Int16)
+		y = radius
+		xcenter = 0
+		ycenter = 0
+		p = (5 - radius*4)/4
+		self.circlePoints2(array, xcenter, ycenter, x, y)
+		while x < y:
+			x += 1
+			if p < 0:
+				p += 2*x + 1
+			else:
+				y -= 1
+				p += 2*(x - y) + 1
+			self.circlePoints2(array, xcenter, ycenter, x, y)
+#		for i in range(array.shape[0]):
+#			set = 0
+#			for j in range(array.shape[1]):
+#				if array[i, j] == 0:
+#					array[i, j] = set
+#				else:
+#					if set == 0:
+#						set = 1
+#					else:
+#						set = 0
+		print array
+
 	def makeNumericImage(self):
 		imagediameter = math.ceil(self.imageDiameter())
 
 		size = math.ceil(imagediameter*self.imagesize/self.scale)
 		numericimage = Numeric.zeros((size, size), Numeric.Int16)
+
+		self.circle2()
 
 		imagecenters = Numeric.arrayrange(0, imagediameter).astype(Numeric.Int16)
 		imagecenters = imagecenters * self.imagesize
@@ -42,6 +133,8 @@ class CompositeImageMaker(object):
 				numericimage[bottom:top, right] = 256
 				numericimage[top, left:right] = 256
 				numericimage[bottom, left:right] = 256
+
+		self.circle(numericimage)
 
 		return numericimage
 
