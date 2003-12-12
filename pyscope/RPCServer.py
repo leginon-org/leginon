b5ae55a580d284dd3afa3424eb494bdaff5ecb08
suloway <suloway>
2003-12-12 22:50:23 +0000

    no more camera, cameradict

--- pyscope/RPCServer.py
+++ pyscope/RPCServer.py
@@ -5,26 +5,19 @@
 #       For terms of the license agreement
 #       see  http://ami.scripps.edu/software/leginon-license
 #
-import SimpleXMLRPCServer
-import threading
-import sys
-import socket
-sys.coinit_flags = 0
-import pythoncom
-import scopedict
-import tecnai
-import cameradict
-import tietz
-import gatan
 import emserver
-import client
+import gatan
+import scopedict
+import socket
+import tecnai
+import threading
+import tietz
 
 if __name__ == '__main__':
-#    pythoncom.CoInitializeEx(pythoncom.COINIT_MULTITHREADED)
     rpcscope = emserver.makeserver(scopedict.factory(tecnai.tecnai),
                               socket.gethostname(), 8000)
-    rpccamera = emserver.makeserver(cameradict.factory(gatan.gatan),
-#    rpccamera = emserver.makeserver(cameradict.factory(tietz.tietz),
+#    rpccamera = emserver.makeserver(methoddict.factory(tietz.Tietz),
+    rpccamera = emserver.makeserver(methoddict.factory(gatan.Gatan),
                               socket.gethostname(), 8001)
     scopethread = threading.Thread(None, rpcscope.serve_forever, None, (), {})
     camerathread = threading.Thread(None, rpccamera.serve_forever, None, (), {})
