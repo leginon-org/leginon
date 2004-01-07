4eb9688c2c9d59b00495fcb814568d3acd5d2bcc
suloway <suloway>
2004-01-07 02:23:35 +0000

    dsf

--- leginon/emailnotification.py
+++ leginon/emailnotification.py
@@ -4,25 +4,34 @@ import email.MIMEMultipart
 import email.MIMEImage
 import poplib
 import sha
+import Image
+import cStringIO
 
-def emailpicture(fromaddress, toaddress, subject, files):
+def emailImageString(fromaddress, toaddress, subject, imagestring):
 	msg = email.MIMEMultipart.MIMEMultipart()
 	msg['Subject'] = subject
 	msg['From'] = fromaddress
 	msg['To'] = toaddress
 
-	for filename in [files]:
-		f = open(filename, 'rb')
-		imagestring = f.read()
-		print sha.new(imagestring).hexdigest()
-		image = email.MIMEImage.MIMEImage(imagestring)
-		f.close()
-		msg.attach(image)
+	print sha.new(imagestring).hexdigest()
+	image = email.MIMEImage.MIMEImage(imagestring)
+	msg.attach(image)
 
 	s = smtplib.SMTP()
 	s.connect()
 	s.sendmail(fromaddress, [toaddress], msg.as_string())
 	s.close()
+
+def pilimage2jpgstring(pilimage):
+	stream = cStringIO.StringIO()
+	pilimage.save(stream, 'JPEG')
+	imagestring = stream.getvalue()
+	stream.close()
+	return imagestring
+
+def emailPILImage(fromaddress, toaddress, subject, pilimage):
+	emailImageString(fromaddress, toaddress, subject,
+										pilimage2jpgstring(pilimage))
 
 def get(hostname, username, password):
 	pop = poplib.POP3(hostname)
@@ -41,8 +50,10 @@ def get(hostname, username, password):
 
 if __name__ == '__main__':
 	import getpass
-	emailpicture('leginon2@scripps.edu', 'suloway@scripps.edu',
-								'testing email', 'robotgridtray.png')
+
+	pilimage = Image.open('test1.jpg')
+	emailPILImage('leginon2@scripps.edu', 'suloway@scripps.edu',
+								'testing email', pilimage)
 	username = getpass.getuser()
 	password = getpass.getpass()
 	messages = get('mail.scripps.edu', username, password)
