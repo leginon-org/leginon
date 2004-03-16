354b0ee236c8c5637260823ea0a30ab99608a236
suloway <suloway>
2004-03-16 22:47:26 +0000

    plugin squarefinder redux

--- leginon/squarefinder.py
+++ leginon/squarefinder.py
@@ -8,272 +8,337 @@
 #       see  http://ami.scripps.edu/software/leginon-license
 #
 
+import convolver
 import data
-from imagefun import TooManyBlobs
+import imagefun
 import Mrc
-import math
-import squarefinderback
+import Numeric
+import random
 import targetfinder
 import threading
 import uidata
-import random
+
+class Plugin(object):
+	name = 'Unamed'
+	def __init__(self, outputplugin=None):
+		self.outputplugin = outputplugin
+		self.userinterfaceitems = self.defineUserInterface()
+
+	def setOutputPlugin(self, outputplugin):
+		self.outputplugin = outputplugin
+
+	def _process(self):
+		raise NotImplementedError
+
+	def process(self, input):
+		return self._process(input)
+
+	def defineUserInterface(self):
+		return []
+
+class Image(object):
+	def __init__(self, image):
+		self.image = image
+
+class MaskedImage(Image):
+	def __init__(self, image, mask):
+		Image.__init__(self, image)
+		self.mask = mask
+
+class Targets(object):
+	def __init__(self, targets):
+		self.targets = targets
+
+class OffsetCenterTargets(Targets):
+	pass
+
+class ImageTargets(Image, Targets):
+	def __init__(self, image, targets):
+		Image.__init__(self, image)
+		Targets.__init__(self, targets)
+
+class PluginPipeline(object):
+	def __init__(self, inputclass, outputclass, pluginclasses=[],
+											imageviewer=None, targetviewer=None):
+		self.inputclass = inputclass
+		self.outputclass = outputclass
+		self.imageviewer = imageviewer
+		self.targetviewer = targetviewer
+		self.verifyevent = threading.Event()
+		self.plugins = []
+		for pluginclass in pluginclasses:
+			plugin = pluginclass()
+			self.plugins.append(plugin)
+		self.userinterfaceitems = self.defineUserInterface()
+
+	def _process(self, input):
+		return input
+
+	def process(self, input):
+		if not isinstance(input, self.inputclass):
+			raise TypeError('Wrong input class for pipeline process')
+		output = self._process(input)
+		for plugin in self.plugins:
+			output = plugin.process(output)
+			if isinstance(output, Image):
+				if isinstance(output, MaskedImage):
+					self.imageviewer.setImage(output.mask)
+				else:
+					self.imageviewer.setImage(output.image)
+			if isinstance(output, Targets):
+				if not isinstance(output, OffsetCenterTargets):
+					self.targetviewer.setTargetType('acquisition', output.targets)
+			if self.verify[plugin].get():
+				try:
+					self.containers[plugin].addObject(uidata.Message('Verify', 'info',
+																							'Waiting for user verification'))
+				except ValueError:
+					pass
+				self.verifyevent.clear()
+				self.verifyevent.wait()
+				try:
+					self.containers[plugin].deleteObject('Verify')
+				except ValueError:
+					pass
+		if not isinstance(output, self.outputclass):
+			output = None
+		return output
+
+	def setImageViewer(self, imageviewer):
+		self.imageviewer = imageviewer
+
+	def setTargetViewer(self, targetviewer):
+		self.targetviewer = targetviewer
+
+	def onProcess(self):
+		print 'process'
+
+	def onProceed(self):
+		self.verifyevent.set()
+
+	def defineUserInterface(self):
+		userinterfaceitems = []
+
+		self.verify = {}
+		self.containers = {}
+		for plugin in self.plugins:
+			self.containers[plugin] = uidata.Container(plugin.name)
+			self.verify[plugin] = uidata.Boolean('Wait for verification', False, 'rw',
+																						persist=True)
+			objects = []
+			objects.append(self.verify[plugin])
+			objects += plugin.userinterfaceitems
+			self.containers[plugin].addObjects(objects)
+			userinterfaceitems.append(self.containers[plugin])
+
+		self.processmethod = uidata.Method('Process', self.onProcess)
+		userinterfaceitems.append(self.processmethod)
+		self.proceedmethod = uidata.Method('Proceed', self.onProceed)
+		userinterfaceitems.append(self.proceedmethod)
+
+		return userinterfaceitems
+
+class LowPassFilterPlugin(Plugin):
+	name = 'Low Pass Filter'
+	inputclass = Image
+	outputclass = Image
+	def __init__(self):
+		Plugin.__init__(self)
+		self.convolver = convolver.Convolver()
+
+	def _process(self, input):
+		size = self.size.get()
+		sigma = self.sigma.get()
+		kernel = convolver.gaussian_kernel(size, sigma)
+		self.convolver.setKernel(kernel)
+		return self.outputclass(self.convolver.convolve(image=input.image))
+
+	def onSetSize(self, value):
+		if not value % 2:
+			value += 1
+		return value
+
+	def defineUserInterface(self):
+		self.size = uidata.Number('Size', 5, 'rw', callback=self.onSetSize,
+															persist=True)
+		self.sigma = uidata.Number('Sigma', 1.4, 'rw', persist=True)
+		return [self.size, self.sigma]
+
+class ThresholdPlugin(Plugin):
+	name = 'Threshold'
+	inputclass = Image
+	outputclass = MaskedImage
+
+	def _process(self, input):
+		min = imagefun.min(input.image)
+		max = imagefun.max(input.image)
+		cutoff = (max - min) / 10.0
+		return self.outputclass(input.image,
+														imagefun.threshold(input.image, cutoff))
+
+class BlobFinderPlugin(Plugin):
+	name = 'Blob Finder'
+	inputclass = MaskedImage
+	outputclass = ImageTargets
+
+	def _process(self, input):
+		border = self.border.get()
+		maxblobs = self.maxblobs.get()
+		minblobsize = self.minblobsize.get()
+		maxblobsize = self.maxblobsize.get()
+		try:
+			blobs = imagefun.find_blobs(input.image, input.mask, border, maxblobs,
+																	maxblobsize, minblobsize) 
+		except (ValueError, imagefun.TooManyBlobs):
+			blobs = []
+		targets = map(lambda b: (b.stats['center'][1], b.stats['center'][0]), blobs)
+		return self.outputclass(input.image, targets)
+
+	def defineUserInterface(self):
+		self.border = uidata.Number('Border', 0, 'rw', persist=True)
+		self.maxblobs = uidata.Number('Maximum Blobs', 100, 'rw', persist=True)
+		self.minblobsize = uidata.Number('Minimum Blob Size', 0, 'rw', persist=True)
+		self.maxblobsize = uidata.Number('Maximum Blob Size', 1000, 'rw',
+																			persist=True)
+		return [self.border, self.maxblobs, self.minblobsize, self.maxblobsize]
+
+class SquareBlobFinderPlugin(BlobFinderPlugin):
+	name = 'Square Blob Finder'
+
+	def _process(self, input):
+		squaredimension = self.squaredimension.get()
+		minblobsizetolerance = self.minblobsizetolerance.get()
+		maxblobsizetolerance = self.maxblobsizetolerance.get()
+		shape = input.image.shape
+		self.maxblobs.set(shape[0]*shape[1]/squaredimension**2)
+		self.minblobsize.set((squaredimension * (minblobsizetolerance))**2)
+		self.maxblobsize.set((squaredimension * (maxblobsizetolerance))**2)
+		self.border.set(squaredimension/10)
+		return BlobFinderPlugin._process(self, input)
+
+	def defineUserInterface(self):
+		self.squaredimension = uidata.Number('Square Dimension', 200, 'rw',
+																					persist=True)
+		self.minblobsizetolerance = uidata.Number('Minimum Blob Size Tolerance',
+																							0.6, 'rw', persist=True)
+		self.maxblobsizetolerance = uidata.Number('Maximum Blob Size Tolerance',
+																							1.25, 'rw', persist=True)
+		userinterfaceitems = []
+		userinterfaceitems.append(self.squaredimension)
+		userinterfaceitems.append(self.minblobsizetolerance)
+		userinterfaceitems.append(self.maxblobsizetolerance)
+		userinterfaceitems += BlobFinderPlugin.defineUserInterface(self)
+		return userinterfaceitems
+
+class TargetBorderPlugin(Plugin):
+	name = 'Target Border'
+	inputclass = ImageTargets
+	outputclass = ImageTargets
+
+	def _process(self, input):
+		targets = []
+		border = self.border.get()
+		shape = input.image.shape
+		for target in input.targets:
+			if target[0] < border:
+				continue
+			if target[1] < border:
+				continue
+			if target[0] > shape[0] - border:
+				continue
+			if target[1] > shape[1] - border:
+				continue
+			targets.append(target)
+		return self.outputclass(input.image, targets)
+
+	def defineUserInterface(self):
+		self.border = uidata.Number('Border', 0, 'rw', persist=True)
+		return [self.border]
+
+class RandomTargetPlugin(Plugin):
+	name = 'Random Target Selection'
+	inputclass = ImageTargets
+	outputclass = ImageTargets
+
+	def _process(self, input):
+		ntargets = self.ntargets.get()
+		try:
+			targets = random.sample(input.targets, ntargets)
+		except ValueError:
+			targets = input.targets
+		return self.outputclass(input.image, targets)
+
+	def defineUserInterface(self):
+		self.ntargets = uidata.Number('Number of targets', 1, 'rw', persist=True)
+		return [self.ntargets]
+
+class CenterOffsetTargetsPlugin(Plugin):
+	name = 'Center Offset Targets'
+	inputclass = ImageTargets
+	outputclass = OffsetCenterTargets
+
+	def _process(self, input):
+		rows, columns = input.image.shape
+		targets = []
+		for target in input.targets:
+			column, row = target
+			deltarow = row - rows/2
+			deltacolumn = column - columns/2
+			targets.append((deltarow, deltacolumn))
+		return self.outputclass(targets)
 
 class SquareFinder(targetfinder.TargetFinder):
 	def __init__(self, id, session, nodelocations, **kwargs):
 		targetfinder.TargetFinder.__init__(self, id, session, nodelocations,
 																				**kwargs)
-		self.squarefinder = squarefinderback.SquareFinder()
 		self.image = None
 		self.verifyevent = threading.Event()
+
+		pluginclasses = [LowPassFilterPlugin, ThresholdPlugin,
+											SquareBlobFinderPlugin, TargetBorderPlugin,
+											RandomTargetPlugin, CenterOffsetTargetsPlugin]
+		self.pluginpipeline = PluginPipeline(Image, Targets, pluginclasses)
+
 		self.defineUserInterface()
-		self.setImage(None)
+
+		self.pluginpipeline.setImageViewer(self.ui_image)
+		self.pluginpipeline.setTargetViewer(self.ui_image)
+
 		self.start()
 
-	def setImage(self, image):
-		self.image = image
-		self.ui_image.setImage(self.image)
-		if self.image is None:
-			self.lpfmethod.disable()
-		else:
-			dimension = self.uisquaredimension.get()
-			if dimension is not None:
-				self.updateMaxBlobs(self.image, dimension)
-			self.lpfmethod.enable()
-		self.setLPFImage(None)
-		self.blobs = None
-		self.targets = None
-		self.squaredimension = None
-
-	def setLPFImage(self, image):
-		self.lpfimage = image
-		if self.lpfimage is None:
-			self.thresholdmethod.disable()
-		else:
-			self.ui_image.setImage(self.lpfimage)
-			self.thresholdmethod.enable()
-		self.setThresholdImage(None)
-
-	def setThresholdImage(self, image):
-		self.thresholdimage = image
-		if self.thresholdimage is None:
-			self.findblobsmethod.disable()
-		else:
-			self.ui_image.setImage(self.thresholdimage)
-			self.findblobsmethod.enable()
-		self.setBlobs(None)
-
-	def setBlobs(self, blobs):
-		self.blobs = blobs
-		if blobs is None:
-			self.targetmethod.disable()
-		else:
-			self.targetmethod.enable()
-		self.setTargets(None)
-
-	def setTargets(self, targets):
-		self.targets = targets
-		if self.targets is None:
-			self.ui_image.setTargetType('acquisition', [])
-		else:
-			self.ui_image.setTargetType('acquisition', self.targets)
-		if self.targets is None:
-			self.verifymethod.disable()
-		else:
-			self.verifymethod.enable()
-
-	def lowPassFilter(self):
-		size = self.uilpfsize.get()
-		sigma = self.uilpfsigma.get()
+	def load(self, filename=None):
+		if filename is None:
+			filename = self.filename.get()
 		try:
-			image = self.squarefinder.lowPassFilter(size, sigma, self.image)
-		except (RuntimeError, OverflowError):
-			image = None
-			self.messagelog.error('Low pass filter failed')
-		self.setLPFImage(image)
-
-	def threshold(self):
-		self.setThresholdImage(self.squarefinder.threshold(self.lpfimage))
-
-	def findBlobs(self):
-		border = self.uiborder.get()
-		maxblobs = self.uimaxblobs.get()
-		maxblobsize = self.uimaxblobsize.get()
-		try:
-			blobs = self.squarefinder.findblobs(self.lpfimage, self.thresholdimage,
-																					border, maxblobs, maxblobsize)
-		except (TooManyBlobs, ValueError):
-			blobs = []
-			self.messagelog.error('Too many blobs found')
-		self.setBlobs(blobs)
-
-	def target(self):
-		targetborder = self.uitargetborder.get()
-		shape = self.image.shape
-		if targetborder < 0 or targetborder > shape[0]/2 \
-				or targetborder > shape[1]/2:
-			self.messagelog.error('Invalid border size specified')
-		targetmin = (targetborder, targetborder)
-		targetmax = (self.image.shape[0] - targetborder,
-									self.image.shape[1] - targetborder)
-		targets = []
-		for blob in self.blobs:
-			if blob[0] >= targetmin[0] and blob[0] <= targetmax[0] \
-					and blob[1] >= targetmin[1] and blob[1] <= targetmax[1]:
-				targets.append(blob)
-		if self.uilimittargets.get():
-			targetlimit = self.uitargetlimit.get()
-			try:
-				targets = random.sample(targets, targetlimit)
-			except ValueError:
-				pass
-		self.setTargets(targets)
-
-	def uiLoad(self):
-		filename = self.uitestfilename.get()
-		try:
-			image = Mrc.mrc_to_numeric(filename)
+			self.pluginpipeline.process(Image(Mrc.mrc_to_numeric(filename)))
 		except IOError:
-			image = None
 			self.messagelog.error('Load file "%s" failed' % filename)
-		self.setImage(image)
-
-	def uiLowPassFilter(self):
-		self.lowPassFilter()
-
-	def uiThreshold(self):
-		self.threshold()
-
-	def uiFindBlobs(self):
-		self.findBlobs()
-
-	def uiTarget(self):
-		self.target()
-
-	def uiVerify(self):
-		self.verifyevent.set()
-
-	def onSetSquareDimension(self, value):
-		if value is None:
-			return value
-		if self.image is not None:
-			self.updateMaxBlobs(self.image, value)
-		self.uimaxblobsize.set((value*1.1)**2)
-		self.uitargetborder.set(value*math.sqrt(2))
-		return value
-
-	def updateMaxBlobs(self, image, dimension):
-		if not self.uishowadvanced.get():
-			maxblobs = image.shape[0]/dimension * image.shape[1]/dimension
-			self.uimaxblobs.set(maxblobs*2)
-
-	def onShowAdvanced(self, value):
-		if value:
-			self.advancedcontainer.enable()
-		else:
-			self.advancedcontainer.disable()
-		return value
-
-	def findSquares(self, image):
-		self.setImage(image)
-		self.lowPassFilter()
-		self.threshold()
-		self.findBlobs()
-		self.target()
-
-	def getTargetList(self, imagedata):
-		targettypename = 'acquisition'
-		rows, columns = imagedata['image'].shape
-		targetlist = []
-		for target in self.targets:
-			column, row = target
-			deltarow = row - rows/2
-			deltacolumn = column - columns/2
-			targetlist.append(self.newTargetData(imagedata, targettypename,
-																						deltarow, deltacolumn))
-		return targetlist
 
 	def findTargets(self, imagedata):
-		self.findSquares(imagedata['image'])
-		if self.uiuserverify.get():
-			self.messagelog.warning('Waiting for user to verify picked squares')
-			self.verifyevent.clear()
-			self.verifyevent.wait()
-		self.targetlist = self.getTargetList(imagedata)
-		self.setImage(None)
+		output = self.pluginpipeline.process(Image(imagedata['image']))
+		self.targetlist = map(lambda t: self.newTargetData(imagedata, 'acquisition',
+																												t[0], t[1]),
+													output.targets)
 
 	def defineUserInterface(self):
 		targetfinder.TargetFinder.defineUserInterface(self)
 		self.uidataqueueflag.set(False)
 
 		self.messagelog = uidata.MessageLog('Message Log')
-
 		self.ui_image = uidata.TargetImage('Image', None, 'r')
-
-		self.uitestfilename = uidata.String('Test filename', None, 'rw',
-																				persist=True)
-		self.loadmethod = uidata.Method('Load', self.uiLoad)
-		testfilecontainer = uidata.Container('Test File')
-		testfilecontainer.addObjects((self.uitestfilename, self.loadmethod))
-
-		self.uilpfsize = uidata.Number('Size', 5, 'rw',
-																		persist=True)
-		self.uilpfsigma = uidata.Number('Sigma', 1.4, 'rw',
-																		persist=True)
-		self.lpfmethod = uidata.Method('Smooth', self.uiLowPassFilter)
-		self.lpfmethod.disable()
-		lpfcontainer = uidata.Container('Smoothing')
-		lpfcontainer.addObjects((self.uilpfsize, self.uilpfsigma))
-
-		self.thresholdmethod = uidata.Method('Threshold', self.uiThreshold)
-		self.thresholdmethod.disable()
-
 		self.ui_image.addTargetType('acquisition')
-		self.uiborder = uidata.Number('Border', 0, 'rw', persist=True)
-		self.uimaxblobs = uidata.Number('Maximum Blobs', 100, 'rw', persist=True)
-		self.uimaxblobsize = uidata.Number('Maximum Blob Size', 250, 'rw',
+
+		self.filename = uidata.String('Test filename', None, 'rw',
 																				persist=True)
-		self.findblobsmethod = uidata.Method('Find Blobs', self.uiFindBlobs)
-		self.findblobsmethod.disable()
-		findblobscontainer = uidata.Container('Find Blobs')
-		findblobscontainer.addObjects((self.uiborder, self.uimaxblobs,
-																		self.uimaxblobsize))
+		self.loadmethod = uidata.Method('Load', self.load)
+		filecontainer = uidata.Container('File')
+		filecontainer.addObjects((self.filename, self.loadmethod))
 
-		self.uitargetborder = uidata.Number('Border', 0, 'rw', persist=True)
-		self.targetmethod = uidata.Method('Target', self.uiTarget)
-		self.targetmethod.disable()
-		targetcontainer = uidata.Container('Targets')
-		targetcontainer.addObjects((self.uitargetborder,))
-
-		self.uiuserverify = uidata.Boolean('Allow user verification of targets',
-																				True, 'rw', persist=True)
-
-		self.uisquaredimension = uidata.Number('Square Dimension', None, 'rw',
-															callback=self.onSetSquareDimension, persist=True)
-
-		self.uilimittargets = uidata.Boolean('Limit number of targets', False,
-																					'rw', persist=True)
-		self.uitargetlimit = uidata.Number('Target Limit', 0, 'rw', persist=True)
-
-		self.advancedcontainer = uidata.Container('Advanced')
-		self.advancedcontainer.addObjects((lpfcontainer, findblobscontainer,
-																	targetcontainer))
-		self.uishowadvanced = uidata.Boolean('Edit advanced settings', False, 'rw',
-																					callback=self.onShowAdvanced,
-																					persist=True)
-		settingscontainer = uidata.Container('Settings')
-		settingscontainer.addObjects((self.uiuserverify, self.uisquaredimension,
-																	self.uilimittargets, self.uitargetlimit,
-																	self.uishowadvanced, self.advancedcontainer))
-
-		self.verifymethod = uidata.Method('Submit', self.uiVerify)
-		controlcontainer = uidata.Container('Control')
-		controlcontainer.addObjects((self.lpfmethod, self.thresholdmethod,
-																	self.findblobsmethod, self.targetmethod,
-																	self.verifymethod))
+		pipelinecontainer = uidata.Container('Pipeline')
+		pipelinecontainer.addObjects(self.pluginpipeline.userinterfaceitems)
 
 		container = uidata.LargeContainer('Square Finder')
-		container.addObjects((self.messagelog, testfilecontainer,
-													settingscontainer, controlcontainer,
-													self.ui_image,))
+		container.addObjects((self.messagelog, filecontainer,
+													pipelinecontainer, self.ui_image,))
 		self.uiserver.addObjects((container,))
 
