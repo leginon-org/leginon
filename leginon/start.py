84319b714a0938f961b29bb4bc42b3397f9169ed
suloway <suloway>
2004-04-05 23:13:37 +0000

    asdf

--- leginon/start.py
+++ leginon/start.py
@@ -18,32 +18,7 @@ import uiclient
 #gc.enable()
 #gc.set_debug(gc.DEBUG_LEAK)
 
-def startManager(location, event):
-	m = manager.Manager(('manager',), None)
-	location.update(m.location())
-	m.start()
-	event.set()
-
-def startLauncher(location, event):
-	launcher.Launcher((socket.gethostname(),),
-										nodelocations={'manager': location}).start()
-	event.set()
-
-location = {}
-event = threading.Event()
-thread = threading.Thread(target=startManager, args=(location, event))
-thread.setDaemon(True)
-thread.start()
-event.wait()
-thread = threading.Thread(target=startLauncher, args=(location, event))
-thread.setDaemon(True)
-thread.start()
-event.wait()
-
-'''
 location = manager.Manager(('manager',), None).location()
 launcher.Launcher((socket.gethostname(),), nodelocations={'manager': location})
-'''
-
 client = uiclient.UIApp(location['UI'], 'Leginon II')
 
