fa15b4606a5883c43f322d2a79b899ed037666f1
suloway <suloway>
2004-04-08 22:42:46 +0000

    prefs out, launchers out

--- leginon/leginonconfig.py
+++ leginon/leginonconfig.py
@@ -19,14 +19,8 @@ import os
 #   utility functions and exceptions used in this script    #
 #     (do not change any of this, skip to next section)     #
 #############################################################
-#pathmapping = {'Z:': '/ami/amishare',
-#								'Y:': '/ami/data04',
-#								'X:': '/ami/data03',
-#								'W:': '/ami/data02',
-#								'V:': '/ami/data01'}
 
 pathmapping = {}
-
 def mapPath(path):
 		if not pathmapping:
 			return path
@@ -48,18 +42,6 @@ def mkdirs(newdir, mode=0777):
 ### raise this if something is wrong in this config file
 class LeginonConfigError(Exception):
 	pass
-
-#########################
-#       Launchers       #
-#########################
-## This list is used by the Manager when launching nodes.
-## Add host names where you could potentially be running a launcher.
-## example:   LAUNCHERS = ['temhost', 'yourhost', 'myhost']
-LAUNCHERS = []
-
-# check if launchers configured
-if not LAUNCHERS:
-	raise LeginonConfigError('need launcher list in leginonconfig.py')
 
 #########################
 #	Database	#
@@ -90,7 +72,6 @@ DB_PROJECT_PASS = ''
 ## automatically be created when the first image is saved
 IMAGE_PATH	= ''
 HOME_PATH	= os.path.expanduser('~')
-PREFS_PATH	= os.path.join(HOME_PATH, '.leginon', 'prefs')
 ID_PATH		= os.path.join(HOME_PATH, '.leginon', 'ids')
 
 if not IMAGE_PATH:
@@ -101,21 +82,4 @@ try:
 	mkdirs(mapPath(IMAGE_PATH))
 except:
 	print 'error creating IMAGE_PATH %s' % (IMAGE_PATH,)
-try:
-	mkdirs(PREFS_PATH)
-except:
-	print 'error creating IMAGE_PATH %s' % (IMAGE_PATH,)
 
-###################################
-#       Default Camera Config     #
-###################################
-## this is likely to move in future versions, since it should be dependent
-## on which camera you are using.
-CAMERA_CONFIG = {}
-CAMERA_CONFIG['auto square'] = 1
-CAMERA_CONFIG['auto offset'] = 1
-CAMERA_CONFIG['correct'] = 1
-CAMERA_CONFIG['offset'] = {'x': 0, 'y': 0}
-CAMERA_CONFIG['dimension'] = {'x': 1024, 'y': 1024}
-CAMERA_CONFIG['binning'] = {'x': 2, 'y': 2}
-CAMERA_CONFIG['exposure time'] = 500.0
