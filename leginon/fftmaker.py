99fb2445010a3bd2e54bd9005a4b9225d63482ec
pulokas <pulokas>
2004-07-21 18:20:13 +0000

    added post processing feature

--- leginon/fftmaker.py
+++ leginon/fftmaker.py
@@ -19,6 +19,7 @@ class FFTMaker(imagewatcher.ImageWatcher):
 	def __init__(self, id, session, managerlocation, **kwargs):
 		imagewatcher.ImageWatcher.__init__(self, id, session, managerlocation, **kwargs)
 
+		self.stop_post = threading.Event()
 		self.defineUserInterface()
 		self.start()
 
@@ -44,6 +45,39 @@ class FFTMaker(imagewatcher.ImageWatcher):
 		self.publish(powdata, database=True)
 		self.logger.info('Published power spectrum for image')
 
+	def processByLabel(self, label):
+		'''
+		for each image in this session with the given label,
+		calculate the FFT, until we find one that is already done
+		'''
+		## find images in this session with the given label
+		iquery = data.AcquisitionImageData(session=self.session, label=label)
+		images = self.research(iquery, readimages=False)
+		# start with first chronologically
+		images.reverse()
+		for im in images:
+			if self.stop_post.isSet():
+				self.logger.info('stopping post processing')
+				break
+			## find if there is already an FFT
+			fquery = data.AcquisitionFFTData(source=im)
+			fft = self.research(fquery, readimages=False)
+			if fft:
+				continue
+			## read image
+			num = im['image'].read()
+			im.__setitem__('image', num, force=True)
+			self.publishPowerImage(im)
+
+	def uiStartPostProcess(self):
+		label = self.postlabel.get()
+		self.stop_post.clear()
+		self.processByLabel(label)
+
+	def uiStopPostProcess(self):
+		self.logger.info('will stop after next iteration')
+		self.stop_post.set()
+
 	def defineUserInterface(self):
 		imagewatcher.ImageWatcher.defineUserInterface(self)
 
@@ -53,8 +87,14 @@ class FFTMaker(imagewatcher.ImageWatcher):
 		self.ignore_images = uidata.Boolean('Ignore Images', True, 'rw', persist=True)
 		self.maskrad = uidata.Float('Mask Radius (% of imagewidth)', 0.01, 'rw', persist=True)
 
+		postproc = uidata.Container('Post Processing')
+		self.postlabel = uidata.String('Label', '', 'rw', persist=True)
+		poststart = uidata.Method('Start', self.uiStartPostProcess)
+		poststop = uidata.Method('Stop', self.uiStopPostProcess)
+		postproc.addObjects((self.postlabel, poststart, poststop))
+
 		container = uidata.LargeContainer('FFT Maker')
-		container.addObjects((self.ignore_images,self.maskrad))
+		container.addObjects((self.ignore_images,self.maskrad, postproc))
 
 		self.uicontainer.addObject(container)
 
