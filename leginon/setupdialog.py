79d02e92e340d16bef6238da1c4e35f1d253b7c9
suloway <suloway>
2004-08-06 17:56:17 +0000

    setup dialog

--- leginon/setupdialog.py
+++ leginon/setupdialog.py
@@ -7,12 +7,21 @@ class UserPage(wx.wizard.WizardPageSimple):
 		wx.wizard.WizardPageSimple.__init__(self, parent)
 		sizer = wx.GridBagSizer()
 
-		self.userchoice = wx.Choice(self, -1)
-		sizer.Add(self.userchoice, (0, 0), (1, 2), wx.ALIGN_CENTER|wx.ALL)
+		sizer.Add(wx.StaticText(self, -1,
+												'Please select your name and press the "Next" button.'),
+												(0, 0), (1, 2), wx.ALIGN_BOTTOM)
 
-		sizer.Add(wx.StaticText(self, -1, 'Group:'), (1, 0), (1, 1))
+		sizer.AddGrowableRow(0)
+		sizer.AddGrowableCol(0)
+		sizer.AddGrowableCol(1)
+
+		self.userchoice = wx.Choice(self, -1)
+		sizer.Add(self.userchoice, (1, 0), (1, 2), wx.ALIGN_CENTER|wx.ALL)
+
 		self.grouptext = wx.StaticText(self, -1, '')
-		sizer.Add(self.grouptext, (1, 1), (1, 1))
+		sizer.Add(self.grouptext, (2, 0), (1, 2), wx.ALIGN_CENTER|wx.ALL)
+
+		sizer.AddGrowableRow(2)
 
 		self.SetSizerAndFit(sizer)
 
@@ -21,18 +30,29 @@ class SessionPage(wx.wizard.WizardPageSimple):
 		wx.wizard.WizardPageSimple.__init__(self, parent)
 		sizer = wx.GridBagSizer()
 
-		self.userchoice = wx.Choice(self, -1)
-		sizer.Add(self.userchoice, (0, 0), (1, 2), wx.ALIGN_CENTER|wx.ALL)
+		choices = ['Create a new session', 'Return to an existing session']
+		self.sessiontyperadiobox = wx.RadioBox(self, -1, 'Session Type',
+																						choices=choices, majorDimension=1,
+																						style=wx.RA_SPECIFY_COLS)
+		sizer.Add(self.sessiontyperadiobox, (0, 0), (1, 2), wx.ALIGN_CENTER|wx.ALL)
+
+#		self.userchoice = wx.Choice(self, -1)
+#		sizer.Add(self.userchoice, (0, 0), (1, 2), wx.ALIGN_CENTER|wx.ALL)
+
+		sizer.AddGrowableCol(0)
+		sizer.AddGrowableCol(1)
 
 		self.descriptiontext = wx.StaticText(self, -1, '')
-		sizer.Add(self.descriptiontext, (1, 1), (1, 2))
+		sizer.Add(self.descriptiontext, (1, 0), (1, 2), wx.ALIGN_CENTER|wx.ALL)
+
+		sizer.AddGrowableRow(1)
 
 		sizer.Add(wx.StaticText(self, -1, 'Instrument:'), (2, 0), (1, 1))
 		self.instrumenttext = wx.StaticText(self, -1, '')
 		sizer.Add(self.instrumenttext, (2, 1), (1, 1))
 
 		self.connectcheckbox = wx.CheckBox(self, -1, 'Connect to instrument')
-		sizer.Add(self.connectcheckbox, (3, 1), (1, 2))
+		sizer.Add(self.connectcheckbox, (3, 0), (1, 2))
 
 		sizer.Add(wx.StaticText(self, -1, 'Image Directory:'), (4, 0), (1, 1))
 		self.imagedirectorytext = wx.StaticText(self, -1, '')
@@ -49,8 +69,6 @@ class SetupWizard(wx.wizard.Wizard):
 		wx.wizard.Wizard.__init__(self, parent, -1, 'Leginon Setup')
 		userpage = UserPage(self)
 		sessionpage = SessionPage(self)
-
-		self.FitToPage(userpage)
 
 		wx.wizard.WizardPageSimple_Chain(userpage, sessionpage)
 
