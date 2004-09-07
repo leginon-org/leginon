5a24b0ef5cba27d9dadd30ceac4a5c6d8376dfdc
suloway <suloway>
2004-09-07 22:26:28 +0000

    logging

--- leginon/extendedlogging.py
+++ leginon/extendedlogging.py
@@ -8,6 +8,12 @@ import uidata
 class Logger(logging.Logger):
 	def __init__(self, name):
 		logging.Logger.__init__(self, name)
+		self.format = '%(asctime)s' \
+									' %(name)s' \
+									' %(module)s:%(lineno)d' \
+									' %(levelname)s' \
+									' %(message)s'
+		self.dateformat = '%H:%M:%S'
 		self.formatter = logging.Formatter('%(asctime)s'\
 																				' %(name)s'\
 																				' %(module)s:%(lineno)d'\
@@ -20,6 +26,28 @@ class Logger(logging.Logger):
 		self.filehandler = None
 
 		self.defineUserInterface()
+
+	def hasPrintHandler(self):
+		if self.printhandler is None:
+			return False
+		else:
+			return True
+
+	def setPrintFormatter(self):
+		if self.hasPrintHandler():
+			formatter = logging.Formatter(self.format, self.dateformat)
+			self.printhandler.setFormatter(formatter)
+
+	def addPrintHandler(self):
+		if not self.hasPrintHandler():
+			self.printhandler = logging.StreamHandler(sys.stdout)
+			self.setPrintFormatter()
+			self.addHandler(self.printhandler)
+
+	def removePrintHandler(self):
+		if self.hasPrintHandler():
+			self.removeHandler(self.printhandler)
+			self.printhandler = None
 
 	def getLevelNames(self):
 		levelnames = []
