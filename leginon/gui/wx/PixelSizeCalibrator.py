927770d12fc8db380a2da5a36261ae7d09aebf09
suloway <suloway>
2004-10-04 20:10:45 +0000

    Measure framework

--- leginon/gui/wx/PixelSizeCalibrator.py
+++ leginon/gui/wx/PixelSizeCalibrator.py
@@ -152,6 +152,76 @@ class MeasureDialog(wx.Dialog):
 	def __init__(self, parent):
 		wx.Dialog.__init__(self, parent, -1, 'Measure Pixel Size')
 
+		self.stmag = wx.StaticText(self, -1, '')
+		self.stpd = wx.StaticText(self, -1, '')
+		self.stbinning = wx.StaticText(self, -1, '')
+		self.fedistance = FloatEntry(self, -1, chars=6)
+		self.tccomment = wx.TextCtrl(self, -1, style=wx.TE_MULTILINE)
+
+		szenter = wx.GridBagSizer(5, 5)
+		label = wx.StaticText(self, -1, 'Magnification:')
+		szenter.Add(label, (0, 0), (1, 1), wx.ALIGN_CENTER_VERTICAL)
+		szenter.Add(self.stmag, (0, 1), (1, 1),
+								wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_RIGHT)
+
+		label = wx.StaticText(self, -1, 'Pixel distance:')
+		szenter.Add(label, (1, 0), (1, 1), wx.ALIGN_CENTER_VERTICAL)
+		szenter.Add(self.stpd, (1, 1), (1, 1),
+								wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_RIGHT)
+
+		label = wx.StaticText(self, -1, 'Binning:')
+		szenter.Add(label, (2, 0), (1, 1), wx.ALIGN_CENTER_VERTICAL)
+		szenter.Add(self.stbinning, (2, 1), (1, 1),
+								wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_RIGHT)
+
+		label = wx.StaticText(self, -1, 'Distance:')
+		szenter.Add(label, (3, 0), (1, 1), wx.ALIGN_CENTER_VERTICAL)
+		szenter.Add(self.fedistance, (3, 1), (1, 1),
+								wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_RIGHT)
+
+		label = wx.StaticText(self, -1, 'Comment:')
+		szenter.Add(label, (4, 0), (1, 2), wx.ALIGN_CENTER_VERTICAL)
+		szenter.Add(self.tccomment, (5, 0), (1, 2), wx.EXPAND)
+
+		szenter.AddGrowableRow(3)
+		szenter.AddGrowableCol(1)
+
+		sb = wx.StaticBox(self, -1, 'Entry')
+		sbsz = wx.StaticBoxSizer(sb, wx.VERTICAL)
+		sbsz.Add(szenter, 1, wx.EXPAND|wx.ALL, 5)
+
+		self.bmeasure = wx.Button(self, -1, 'Calculate')
+		self.bsave = wx.Button(self, wx.ID_OK, 'Save')
+		self.bsave.Enable(False)
+		self.bcancel = wx.Button(self, wx.ID_CANCEL, 'Cancel')
+
+		szbutton = wx.GridBagSizer(5, 5)
+		szbutton.Add(self.bmeasure, (0, 0), (1, 1),
+									wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_RIGHT)
+		szbutton.Add(self.bsave, (0, 1), (1, 1), wx.ALIGN_CENTER)
+		szbutton.Add(self.bcancel, (0, 2), (1, 1), wx.ALIGN_CENTER)
+		szbutton.AddGrowableCol(0)
+
+		sz = wx.GridBagSizer(5, 5)
+		sz.Add(sbsz, (0, 0), (1, 1), wx.EXPAND|wx.ALL, 10)
+		sz.Add(szbutton, (1, 0), (1, 1), wx.EXPAND|wx.ALL, 10)
+		self.SetSizerAndFit(sz)
+
+		self.Bind(wx.EVT_BUTTON, self.onMeasureButton, self.bmeasure)
+
+	def onMeasureButton(self, evt):
+		distance = self.fedistance.GetValue()
+		if distance is None:
+			dialog = wx.MessageDialog(self, 'No distance entered',
+																'Error', wx.OK|wx.ICON_ERROR)
+			dialog.ShowModal()
+			dialog.Destroy()
+			return
+		else:
+			evt.Skip()
+
+		self.bsave.Enable(True)
+
 class PixelSizeListCtrl(wx.ListCtrl):
 	def __init__(self, parent, id):
 		wx.ListCtrl.__init__(self, parent, id, style=wx.LC_REPORT)
@@ -222,8 +292,7 @@ class PixelSizeCalibrationDialog(wx.Dialog):
 		dialog.Destroy()
 
 	def onExtrapolateButton(self, evt):
-		#dialog = ExtrapolateDialog(self, mags)
-		dialog = ExtrapolateDialog(self, [1.0, 100.0])
+		dialog = ExtrapolateDialog(self, mags)
 		dialog.ShowModal()
 		dialog.Destroy()
 
