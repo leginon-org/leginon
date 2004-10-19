2365a9631289cc1a50555ac4d35ef2792dd341b1
suloway <suloway>
2004-10-19 17:21:00 +0000

    extended event

---
+++ leginon/player.py
@@ -0,0 +1,71 @@
+import threading
+
+def Player(*args, **kwargs):
+	return _Player(*args, **kwargs)
+
+class _Player(threading._Verbose):
+	def __init__(self, verbose=None):
+		threading._Verbose.__init__(self, verbose)
+		self.__cond = Condition(Lock())
+		self.__flag = 'play'
+
+	def state(self):
+		return self.__flag
+
+	def play(self):
+		self.__cond.acquire()
+		try:
+			self.__flag = 'play'
+			self.__cond.notifyAll()
+		finally:
+			self.__cond.release()
+
+	def pause(self):
+		self.__cond.acquire()
+		try:
+			self.__flag = 'pause'
+		finally:
+			self.__cond.release()
+
+	def stop(self):
+		self.__cond.acquire()
+		try:
+			self.__flag = 'stop'
+			self.__cond.notifyAll()
+		finally:
+			self.__cond.release()
+
+	def wait(self, timeout=None):
+		self.__cond.acquire()
+		try:
+			if self.__flag == 'pause':
+				self.__cond.wait(timeout)
+			state = self.__flag
+		finally:
+			self.__cond.release()
+		return state
+
+if __name__ == '__main__':
+	import time
+
+	player = Player()
+
+	def _player():
+		while True:
+			command = player.wait(0.0)
+			print command
+			if command == 'stop':
+				break
+			time.sleep(1.0)
+
+	threading.Thread(target=_player).start()
+
+	while True:
+		command = raw_input()
+		if hasattr(player, command):
+			attr = getattr(player, command)
+			if callable(attr):
+				attr()
+			else:
+				print attr
+
