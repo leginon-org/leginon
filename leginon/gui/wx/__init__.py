23dd5bee2a9d317a1f995d955e5dda317570faa9
suloway <suloway>
2004-10-21 22:28:19 +0000

    double header removed

--- leginon/gui/wx/__init__.py
+++ leginon/gui/wx/__init__.py
@@ -4,22 +4,9 @@
 # see http://ami.scripps.edu/software/leginon-license
 #
 # $Source: /ami/sw/cvsroot/pyleginon/gui/wx/__init__.py,v $
-# $Revision: 1.10 $
+# $Revision: 1.11 $
 # $Name: not supported by cvs2svn $
-# $Date: 2004-10-21 22:27:06 $
-# $Author: suloway $
-# $State: Exp $
-# $Locker:  $
-
-# The Leginon software is Copyright 2004
-# The Scripps Research Institute, La Jolla, CA
-# For terms of the license agreement
-# see http://ami.scripps.edu/software/leginon-license
-#
-# $Source: /ami/sw/cvsroot/pyleginon/gui/wx/__init__.py,v $
-# $Revision: 1.10 $
-# $Name: not supported by cvs2svn $
-# $Date: 2004-10-21 22:27:06 $
+# $Date: 2004-10-21 22:28:19 $
 # $Author: suloway $
 # $State: Exp $
 # $Locker:  $
