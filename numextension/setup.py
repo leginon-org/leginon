80447c870a22861561e44fc1f69f51cb3729eae6
suloway <suloway>
2004-10-22 23:06:13 +0000

    tags

--- numextension/setup.py
+++ numextension/setup.py
@@ -1,10 +1,12 @@
 from distutils.core import setup, Extension
 
-module = Extension('numextension',
-                    sources = ['numextension.c'])
+module = Extension('numextension', sources = ['numextension.c'])
 
-setup (name = 'NumExtension',
-       version = '1.0',
-       description = 'Extensions to numpy',
-       ext_modules = [module])
+setup(
+	name='NumExtension',
+	version='1.0',
+	description='Extensions to numpy',
+	url='http://nramm.scripps.edu/',
+	ext_modules=[module]
+)
 
