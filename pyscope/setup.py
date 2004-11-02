e334bd1a8c201e88a9c6204761f119e21a2ffab4
suloway <suloway>
2004-11-02 00:09:07 +0000

    1.0.2

--- pyscope/setup.py
+++ pyscope/setup.py
@@ -20,7 +20,7 @@ else:
 
 setup(
 	name='pyScope',
-	version='1.0.1',
+	version='1.0.2',
 	description='Interface to Electron Microscopes and CCD Cameras',
 	packages=['pyScope'],
 	package_dir={'pyScope': ''},
