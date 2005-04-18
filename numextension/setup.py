8a64df9281e34e413f65be77025784d85a00f722
suloway <suloway>
2005-04-18 18:31:20 +0000

    clean up for 1.0.2

--- numextension/setup.py
+++ numextension/setup.py
@@ -5,7 +5,7 @@ module = NumarrayExtension('numextension', sources = ['numextension.c'])
 
 setup(
 	name='NumExtension',
-	version='1.0.1',
+	version='1.0.2',
 	description='Extensions to numpy',
 	url='http://nramm.scripps.edu/',
 	ext_modules=[module]
