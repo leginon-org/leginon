969a36d1f7e88dca7d04fc765bd0d1dbb6fb250a
Anchi Cheng <acheng>
2005-05-23 18:14:37 +0000

    test

--- leginon/gui/wx/MatlabTargetFinder.py
+++ leginon/gui/wx/MatlabTargetFinder.py
@@ -4,10 +4,10 @@
 # see http://ami.scripps.edu/software/leginon-license
 #
 # $Source: /ami/sw/cvsroot/pyleginon/gui/wx/MatlabTargetFinder.py,v $
-# $Revision: 1.5 $
+# $Revision: 1.6 $
 # $Name: not supported by cvs2svn $
-# $Date: 2005-05-11 23:49:31 $
-# $Author: pulokas $
+# $Date: 2005-05-23 18:14:37 $
+# $Author: acheng $
 # $State: Exp $
 # $Locker:  $
 
@@ -35,19 +35,35 @@ class Panel(gui.wx.TargetFinder.Panel):
 		self.toolbar.EnableTool(gui.wx.ToolBar.ID_REFRESH, False)
 
 		self.imagepanel = gui.wx.ImageViewer.TargetImagePanel(self, -1)
+		self.imagepanel.addTypeTool('Image', display=True, settings=True)
+		self.imagepanel.selectiontool.setDisplayed('Image', True)
+
 		self.imagepanel.addTargetTool('acquisition', wx.GREEN, target=True)
 		self.imagepanel.selectiontool.setDisplayed('acquisition', True)
 		self.imagepanel.addTargetTool('focus', wx.BLUE, target=True)
 		self.imagepanel.selectiontool.setDisplayed('focus', True)
 		#self.imagepanel.addTargetTool('done', wx.RED)
 		#self.imagepanel.selectiontool.setDisplayed('done', True)
-		self.imagepanel.addTypeTool('Image', display=True)
-		self.imagepanel.selectiontool.setDisplayed('Image', True)
+
 		self.szmain.Add(self.imagepanel, (1, 0), (1, 1), wx.EXPAND)
 		self.szmain.AddGrowableRow(1)
 		self.szmain.AddGrowableCol(0)
 
 		self.Bind(gui.wx.Events.EVT_FOUND_TARGETS, self.onFoundTargets)
+		self.Bind(gui.wx.ImageViewer.EVT_SETTINGS, self.onImageSettings)
+
+	def onImageSettings(self, evt):
+		if evt.name == 'Image':
+			dialog = OriginalSettingsDialog(self)
+			if dialog.ShowModal() == wx.ID_OK:
+				filename = self.node.settings['test image']
+				if filename:
+					self.node.readImage(filename)
+			dialog.Destroy()
+			return
+
+		dialog.ShowModal()
+		dialog.Destroy()
 
 	def onNodeInitialized(self):
 		gui.wx.TargetFinder.Panel.onNodeInitialized(self)
@@ -77,19 +93,37 @@ class Panel(gui.wx.TargetFinder.Panel):
 	def onTargetTestImage(self, evt):
 		threading.Thread(target=self.node.targetTestImage).start()
 
+class OriginalSettingsDialog(gui.wx.Settings.Dialog):
+	def initialize(self):
+		gui.wx.Settings.Dialog.initialize(self)
+
+		self.widgets['test image'] = filebrowse.FileBrowseButton(self,
+								labelText='Test Image:', fileMask='*.mrc')
+		self.bok.SetLabel('Load')
+
+		sz = wx.GridBagSizer(5, 5)
+		sz.Add(self.widgets['test image'], (0, 0), (1, 1),
+						wx.ALIGN_CENTER_VERTICAL)
+
+		sb = wx.StaticBox(self, -1, 'test image')
+		sbsz = wx.StaticBoxSizer(sb, wx.VERTICAL)
+		sbsz.Add(sz, 1, wx.EXPAND|wx.ALL, 5)
+
+		return [sbsz]
+
 class SettingsDialog(gui.wx.TargetFinder.SettingsDialog):
 	def initialize(self):
 		tfsbsz = gui.wx.TargetFinder.SettingsDialog.initialize(self)
 		#gui.wx.Settings.Dialog.initialize(self)
 
-		self.widgets['test image'] = filebrowse.FileBrowseButton(self,
-																			labelText='Test Image:', fileMask='*.mrc')
+#		self.widgets['test image'] = filebrowse.FileBrowseButton(self,
+#								labelText='Test Image:', fileMask='*.mrc')
 		self.widgets['module path'] = filebrowse.FileBrowseButton(self,
-																			labelText='Matlab File:', fileMask='*.m')
+								labelText='Matlab File:', fileMask='*.m')
 
 		sz = wx.GridBagSizer(5, 5)
-		sz.Add(self.widgets['test image'], (0, 0), (1, 1),
-						wx.ALIGN_CENTER_VERTICAL)
+#		sz.Add(self.widgets['test image'], (0, 0), (1, 1),
+#						wx.ALIGN_CENTER_VERTICAL)
 		sz.Add(self.widgets['module path'], (1, 0), (1, 1),
 						wx.ALIGN_CENTER_VERTICAL)
 
