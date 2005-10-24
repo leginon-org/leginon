f1e127058d6cc7955de3f9154ab344b439e50b57
suloway <suloway>
2005-10-24 21:02:51 +0000

    modifications for subclassing

--- leginon/gui/wx/Acquisition.py
+++ leginon/gui/wx/Acquisition.py
@@ -4,10 +4,10 @@
 # see http://ami.scripps.edu/software/leginon-license
 #
 # $Source: /ami/sw/cvsroot/pyleginon/gui/wx/Acquisition.py,v $
-# $Revision: 1.34 $
+# $Revision: 1.35 $
 # $Name: not supported by cvs2svn $
-# $Date: 2005-04-29 23:03:57 $
-# $Author: pulokas $
+# $Date: 2005-10-24 21:02:51 $
+# $Author: suloway $
 # $State: Exp $
 # $Locker:  $
 
@@ -22,154 +22,6 @@ import gui.wx.ImageViewer
 import gui.wx.ToolBar
 import threading
 from gui.wx.ImageBrowser import ImageBrowserPanel
-
-class Panel(gui.wx.Node.Panel):
-	icon = 'acquisition'
-	imagepanelclass = gui.wx.ImageViewer.ImagePanel
-	def __init__(self, parent, name):
-		gui.wx.Node.Panel.__init__(self, parent, -1)
-
-		self.toolbar.AddTool(gui.wx.ToolBar.ID_SETTINGS,
-													'settings',
-													shortHelpString='Settings')
-		self.toolbar.AddSeparator()
-		self.toolbar.AddTool(gui.wx.ToolBar.ID_PLAY,
-													'play',
-													shortHelpString='Process')
-		self.toolbar.AddTool(gui.wx.ToolBar.ID_PAUSE,
-													'pause',
-													shortHelpString='Pause')
-		self.toolbar.AddTool(gui.wx.ToolBar.ID_ABORT,
-													'stop',
-													shortHelpString='Abort')
-		self.toolbar.AddTool(gui.wx.ToolBar.ID_ABORT_QUEUE,
-													'stop_queue',
-													shortHelpString='Abort Queue')
-		self.toolbar.AddTool(gui.wx.ToolBar.ID_SIMULATE_TARGET,
-													'simulatetarget',
-													shortHelpString='Simulate Target')
-		self.toolbar.AddTool(gui.wx.ToolBar.ID_SIMULATE_TARGET_LOOP,
-													'simulatetargetloop',
-													shortHelpString='Simulate Target Loop')
-		self.toolbar.AddTool(gui.wx.ToolBar.ID_SIMULATE_TARGET_LOOP_STOP,
-													'simulatetargetloopstop',
-													shortHelpString='Stop Simulate Target Loop')
-		self.toolbar.AddTool(gui.wx.ToolBar.ID_BROWSE_IMAGES,
-													'imagebrowser',
-													shortHelpString='Browse Images')
-		self.toolbar.EnableTool(gui.wx.ToolBar.ID_PLAY, False)
-		self.toolbar.EnableTool(gui.wx.ToolBar.ID_PLAY, False)
-		self.toolbar.EnableTool(gui.wx.ToolBar.ID_PAUSE, False)
-		self.toolbar.EnableTool(gui.wx.ToolBar.ID_ABORT, False)
-		self.toolbar.EnableTool(gui.wx.ToolBar.ID_ABORT_QUEUE, False)
-		self.toolbar.Realize()
-
-		# image
-		self.imagepanel = self.imagepanelclass(self, -1)
-		self.imagepanel.addTypeTool('Image', display=True)
-		self.imagepanel.selectiontool.setDisplayed('Image', True)
-		self.szmain.Add(self.imagepanel, (1, 0), (1, 1), wx.EXPAND|wx.ALL, 3)
-
-		self.szmain.AddGrowableRow(1)
-		self.szmain.AddGrowableCol(0)
-
-		self.SetSizer(self.szmain)
-		self.SetAutoLayout(True)
-		self.SetupScrolling()
-
-		self.Bind(gui.wx.Events.EVT_PLAYER, self.onPlayer)
-
-	def onNodeInitialized(self):
-		self.toolbar.Bind(wx.EVT_TOOL, self.onSettingsTool,
-											id=gui.wx.ToolBar.ID_SETTINGS)
-		self.toolbar.Bind(wx.EVT_TOOL, self.onPlayTool,
-											id=gui.wx.ToolBar.ID_PLAY)
-		self.toolbar.Bind(wx.EVT_TOOL, self.onPauseTool,
-											id=gui.wx.ToolBar.ID_PAUSE)
-		self.toolbar.Bind(wx.EVT_TOOL, self.onStopTool,
-											id=gui.wx.ToolBar.ID_ABORT)
-		self.toolbar.Bind(wx.EVT_TOOL, self.onStopQueueTool,
-											id=gui.wx.ToolBar.ID_ABORT_QUEUE)
-		self.toolbar.Bind(wx.EVT_TOOL, self.onSimulateTargetTool,
-											id=gui.wx.ToolBar.ID_SIMULATE_TARGET)
-		self.toolbar.Bind(wx.EVT_TOOL, self.onSimulateTargetLoopTool,
-											id=gui.wx.ToolBar.ID_SIMULATE_TARGET_LOOP)
-		self.toolbar.Bind(wx.EVT_TOOL, self.onSimulateTargetLoopStopTool,
-											id=gui.wx.ToolBar.ID_SIMULATE_TARGET_LOOP_STOP)
-		self.toolbar.Bind(wx.EVT_TOOL, self.onBrowseImagesTool,
-											id=gui.wx.ToolBar.ID_BROWSE_IMAGES)
-
-	def onSimulateTargetTool(self, evt):
-		threading.Thread(target=self.node.simulateTarget).start()
-
-	def onSimulateTargetLoopTool(self, evt):
-		threading.Thread(target=self.node.simulateTargetLoop).start()
-
-	def onSimulateTargetLoopStopTool(self, evt):
-		threading.Thread(target=self.node.simulateTargetLoopStop).start()
-
-	def onSettingsTool(self, evt):
-		dialog = SettingsDialog(self)
-		dialog.ShowModal()
-		dialog.Destroy()
-
-	def onPlayTool(self, evt):
-		self.toolbar.EnableTool(gui.wx.ToolBar.ID_PLAY, False)
-		self.toolbar.EnableTool(gui.wx.ToolBar.ID_PAUSE, False)
-		self.toolbar.EnableTool(gui.wx.ToolBar.ID_ABORT, False)
-		self.toolbar.EnableTool(gui.wx.ToolBar.ID_ABORT_QUEUE, False)
-		self.node.player.play()
-
-	def onPauseTool(self, evt):
-		self.toolbar.EnableTool(gui.wx.ToolBar.ID_PLAY, False)
-		self.toolbar.EnableTool(gui.wx.ToolBar.ID_PAUSE, False)
-		self.toolbar.EnableTool(gui.wx.ToolBar.ID_ABORT, False)
-		self.toolbar.EnableTool(gui.wx.ToolBar.ID_ABORT_QUEUE, False)
-		self.node.player.pause()
-
-	def onStopTool(self, evt):
-		self.toolbar.EnableTool(gui.wx.ToolBar.ID_PLAY, False)
-		self.toolbar.EnableTool(gui.wx.ToolBar.ID_PAUSE, False)
-		self.toolbar.EnableTool(gui.wx.ToolBar.ID_ABORT, False)
-		self.toolbar.EnableTool(gui.wx.ToolBar.ID_ABORT_QUEUE, False)
-		self.node.player.stop()
-
-	def onStopQueueTool(self, evt):
-		self.toolbar.EnableTool(gui.wx.ToolBar.ID_PLAY, False)
-		self.toolbar.EnableTool(gui.wx.ToolBar.ID_PAUSE, False)
-		self.toolbar.EnableTool(gui.wx.ToolBar.ID_ABORT, False)
-		self.toolbar.EnableTool(gui.wx.ToolBar.ID_ABORT_QUEUE, False)
-		self.node.player.stopqueue()
-
-	def onPlayer(self, evt):
-		if evt.state == 'play':
-			self.toolbar.EnableTool(gui.wx.ToolBar.ID_PLAY, False)
-			self.toolbar.EnableTool(gui.wx.ToolBar.ID_PAUSE, True)
-			self.toolbar.EnableTool(gui.wx.ToolBar.ID_ABORT, True)
-			self.toolbar.EnableTool(gui.wx.ToolBar.ID_ABORT_QUEUE, True)
-		elif evt.state == 'pause':
-			self.toolbar.EnableTool(gui.wx.ToolBar.ID_PLAY, True)
-			self.toolbar.EnableTool(gui.wx.ToolBar.ID_PAUSE, False) 
-			self.toolbar.EnableTool(gui.wx.ToolBar.ID_ABORT, True)
-			self.toolbar.EnableTool(gui.wx.ToolBar.ID_ABORT_QUEUE, True)
-		elif evt.state == 'stop':
-			self.toolbar.EnableTool(gui.wx.ToolBar.ID_PLAY, True)
-			self.toolbar.EnableTool(gui.wx.ToolBar.ID_PAUSE, True) 
-			self.toolbar.EnableTool(gui.wx.ToolBar.ID_ABORT, False)
-			self.toolbar.EnableTool(gui.wx.ToolBar.ID_ABORT_QUEUE, True)
-		elif evt.state == 'stopqueue':
-			self.toolbar.EnableTool(gui.wx.ToolBar.ID_PLAY, True)
-			self.toolbar.EnableTool(gui.wx.ToolBar.ID_PAUSE, True) 
-			self.toolbar.EnableTool(gui.wx.ToolBar.ID_ABORT, True)
-			self.toolbar.EnableTool(gui.wx.ToolBar.ID_ABORT_QUEUE, False)
-
-	def onBrowseImagesTool(self, evt):
-		frame = wx.Frame(None, -1, 'Image Browser Test')
-		frame.node = self.node
-		panel = ImageBrowserPanel(frame)
-		frame.Fit()
-		#self.SetTopWindow(frame)
-		frame.Show()
 
 class SettingsDialog(gui.wx.Settings.Dialog):
 	def initialize(self):
@@ -286,6 +138,158 @@ class SettingsDialog(gui.wx.Settings.Dialog):
 		sbsz.Add(sz, 0, wx.ALIGN_CENTER|wx.ALL, 5)
 
 		return [sbsz]
+
+class Panel(gui.wx.Node.Panel):
+	icon = 'acquisition'
+	imagepanelclass = gui.wx.ImageViewer.ImagePanel
+	settingsdialogclass = SettingsDialog
+	def __init__(self, parent, name):
+		gui.wx.Node.Panel.__init__(self, parent, -1)
+
+		self.toolbar.AddTool(gui.wx.ToolBar.ID_SETTINGS,
+													'settings',
+													shortHelpString='Settings')
+		self.toolbar.AddSeparator()
+		self.toolbar.AddTool(gui.wx.ToolBar.ID_PLAY,
+													'play',
+													shortHelpString='Process')
+		self.toolbar.AddTool(gui.wx.ToolBar.ID_PAUSE,
+													'pause',
+													shortHelpString='Pause')
+		self.toolbar.AddTool(gui.wx.ToolBar.ID_ABORT,
+													'stop',
+													shortHelpString='Abort')
+		self.toolbar.AddTool(gui.wx.ToolBar.ID_ABORT_QUEUE,
+													'stop_queue',
+													shortHelpString='Abort Queue')
+		self.toolbar.AddTool(gui.wx.ToolBar.ID_SIMULATE_TARGET,
+													'simulatetarget',
+													shortHelpString='Simulate Target')
+		self.toolbar.AddTool(gui.wx.ToolBar.ID_SIMULATE_TARGET_LOOP,
+													'simulatetargetloop',
+													shortHelpString='Simulate Target Loop')
+		self.toolbar.AddTool(gui.wx.ToolBar.ID_SIMULATE_TARGET_LOOP_STOP,
+													'simulatetargetloopstop',
+													shortHelpString='Stop Simulate Target Loop')
+		self.toolbar.AddTool(gui.wx.ToolBar.ID_BROWSE_IMAGES,
+													'imagebrowser',
+													shortHelpString='Browse Images')
+		self.toolbar.EnableTool(gui.wx.ToolBar.ID_PLAY, False)
+		self.toolbar.EnableTool(gui.wx.ToolBar.ID_PLAY, False)
+		self.toolbar.EnableTool(gui.wx.ToolBar.ID_PAUSE, False)
+		self.toolbar.EnableTool(gui.wx.ToolBar.ID_ABORT, False)
+		self.toolbar.EnableTool(gui.wx.ToolBar.ID_ABORT_QUEUE, False)
+		self.toolbar.Realize()
+
+		self.addImagePanel()
+
+		self.szmain.AddGrowableRow(1)
+		self.szmain.AddGrowableCol(0)
+
+		self.SetSizer(self.szmain)
+		self.SetAutoLayout(True)
+		self.SetupScrolling()
+
+		self.Bind(gui.wx.Events.EVT_PLAYER, self.onPlayer)
+
+	def addImagePanel(self):
+		# image
+		self.imagepanel = self.imagepanelclass(self, -1)
+		self.imagepanel.addTypeTool('Image', display=True)
+		self.imagepanel.selectiontool.setDisplayed('Image', True)
+		self.szmain.Add(self.imagepanel, (1, 0), (1, 1), wx.EXPAND|wx.ALL, 3)
+
+	def onNodeInitialized(self):
+		self.toolbar.Bind(wx.EVT_TOOL, self.onSettingsTool,
+											id=gui.wx.ToolBar.ID_SETTINGS)
+		self.toolbar.Bind(wx.EVT_TOOL, self.onPlayTool,
+											id=gui.wx.ToolBar.ID_PLAY)
+		self.toolbar.Bind(wx.EVT_TOOL, self.onPauseTool,
+											id=gui.wx.ToolBar.ID_PAUSE)
+		self.toolbar.Bind(wx.EVT_TOOL, self.onStopTool,
+											id=gui.wx.ToolBar.ID_ABORT)
+		self.toolbar.Bind(wx.EVT_TOOL, self.onStopQueueTool,
+											id=gui.wx.ToolBar.ID_ABORT_QUEUE)
+		self.toolbar.Bind(wx.EVT_TOOL, self.onSimulateTargetTool,
+											id=gui.wx.ToolBar.ID_SIMULATE_TARGET)
+		self.toolbar.Bind(wx.EVT_TOOL, self.onSimulateTargetLoopTool,
+											id=gui.wx.ToolBar.ID_SIMULATE_TARGET_LOOP)
+		self.toolbar.Bind(wx.EVT_TOOL, self.onSimulateTargetLoopStopTool,
+											id=gui.wx.ToolBar.ID_SIMULATE_TARGET_LOOP_STOP)
+		self.toolbar.Bind(wx.EVT_TOOL, self.onBrowseImagesTool,
+											id=gui.wx.ToolBar.ID_BROWSE_IMAGES)
+
+	def onSimulateTargetTool(self, evt):
+		threading.Thread(target=self.node.simulateTarget).start()
+
+	def onSimulateTargetLoopTool(self, evt):
+		threading.Thread(target=self.node.simulateTargetLoop).start()
+
+	def onSimulateTargetLoopStopTool(self, evt):
+		threading.Thread(target=self.node.simulateTargetLoopStop).start()
+
+	def onSettingsTool(self, evt):
+		dialog = self.settingsdialogclass(self)
+		dialog.ShowModal()
+		dialog.Destroy()
+
+	def onPlayTool(self, evt):
+		self.toolbar.EnableTool(gui.wx.ToolBar.ID_PLAY, False)
+		self.toolbar.EnableTool(gui.wx.ToolBar.ID_PAUSE, False)
+		self.toolbar.EnableTool(gui.wx.ToolBar.ID_ABORT, False)
+		self.toolbar.EnableTool(gui.wx.ToolBar.ID_ABORT_QUEUE, False)
+		self.node.player.play()
+
+	def onPauseTool(self, evt):
+		self.toolbar.EnableTool(gui.wx.ToolBar.ID_PLAY, False)
+		self.toolbar.EnableTool(gui.wx.ToolBar.ID_PAUSE, False)
+		self.toolbar.EnableTool(gui.wx.ToolBar.ID_ABORT, False)
+		self.toolbar.EnableTool(gui.wx.ToolBar.ID_ABORT_QUEUE, False)
+		self.node.player.pause()
+
+	def onStopTool(self, evt):
+		self.toolbar.EnableTool(gui.wx.ToolBar.ID_PLAY, False)
+		self.toolbar.EnableTool(gui.wx.ToolBar.ID_PAUSE, False)
+		self.toolbar.EnableTool(gui.wx.ToolBar.ID_ABORT, False)
+		self.toolbar.EnableTool(gui.wx.ToolBar.ID_ABORT_QUEUE, False)
+		self.node.player.stop()
+
+	def onStopQueueTool(self, evt):
+		self.toolbar.EnableTool(gui.wx.ToolBar.ID_PLAY, False)
+		self.toolbar.EnableTool(gui.wx.ToolBar.ID_PAUSE, False)
+		self.toolbar.EnableTool(gui.wx.ToolBar.ID_ABORT, False)
+		self.toolbar.EnableTool(gui.wx.ToolBar.ID_ABORT_QUEUE, False)
+		self.node.player.stopqueue()
+
+	def onPlayer(self, evt):
+		if evt.state == 'play':
+			self.toolbar.EnableTool(gui.wx.ToolBar.ID_PLAY, False)
+			self.toolbar.EnableTool(gui.wx.ToolBar.ID_PAUSE, True)
+			self.toolbar.EnableTool(gui.wx.ToolBar.ID_ABORT, True)
+			self.toolbar.EnableTool(gui.wx.ToolBar.ID_ABORT_QUEUE, True)
+		elif evt.state == 'pause':
+			self.toolbar.EnableTool(gui.wx.ToolBar.ID_PLAY, True)
+			self.toolbar.EnableTool(gui.wx.ToolBar.ID_PAUSE, False) 
+			self.toolbar.EnableTool(gui.wx.ToolBar.ID_ABORT, True)
+			self.toolbar.EnableTool(gui.wx.ToolBar.ID_ABORT_QUEUE, True)
+		elif evt.state == 'stop':
+			self.toolbar.EnableTool(gui.wx.ToolBar.ID_PLAY, True)
+			self.toolbar.EnableTool(gui.wx.ToolBar.ID_PAUSE, True) 
+			self.toolbar.EnableTool(gui.wx.ToolBar.ID_ABORT, False)
+			self.toolbar.EnableTool(gui.wx.ToolBar.ID_ABORT_QUEUE, True)
+		elif evt.state == 'stopqueue':
+			self.toolbar.EnableTool(gui.wx.ToolBar.ID_PLAY, True)
+			self.toolbar.EnableTool(gui.wx.ToolBar.ID_PAUSE, True) 
+			self.toolbar.EnableTool(gui.wx.ToolBar.ID_ABORT, True)
+			self.toolbar.EnableTool(gui.wx.ToolBar.ID_ABORT_QUEUE, False)
+
+	def onBrowseImagesTool(self, evt):
+		frame = wx.Frame(None, -1, 'Image Browser Test')
+		frame.node = self.node
+		panel = ImageBrowserPanel(frame)
+		frame.Fit()
+		#self.SetTopWindow(frame)
+		frame.Show()
 
 if __name__ == '__main__':
 	class App(wx.App):
