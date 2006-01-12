a0052e36b01d8903f8d686c9a3d9d4354661ab22
pulokas <pulokas>
2006-01-12 23:07:07 +0000

    stig defoc limits

--- leginon/focuser.py
+++ leginon/focuser.py
@@ -244,8 +244,10 @@ class Focuser(acquisition.Acquisition):
         # stig is only valid in a certain defocus range
         if setting['stig correction']:
             self.logger.info('Stig. correction...')
-            stigdefocusmin = setting['stig defocus min']
-            stigdefocusmax = setting['stig defocus max']
+						stigdefocrange = [abs(setting['stig defocus min']),abs(setting['stig defocus max'])]
+						stigdefocrange.sort()
+            stigdefocusmin = stigdefocrange[0]
+            stigdefocusmax = stigdefocrange[1]
             if validdefocus and stigdefocusmin < abs(defoc) < stigdefocusmax:
                 self.correctStig(stigx, stigy)
                 resultdata['stig correction'] = 1
