60f0cd3af952f7eef3ce2356a6878620a338658d
Scott Stagg <sstagg>
2006-07-11 22:00:18 +0000

    Remove testing comment

--- appion/particle_manager/selexon.py
+++ appion/particle_manager/selexon.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python
 # Python wrapper for the selexon program
 # Will by default create a "jpgs" directory and save jpg images of selections & crudfinder results
-# testing CVS
 
 import os, re, sys
 import tempfile
