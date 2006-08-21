8a3b176272cfef54a9182285df89e13984512279
suloway <suloway>
2006-08-21 23:51:11 +0000

    gui updates

--- leginon/gui/wx/Reference.py
+++ leginon/gui/wx/Reference.py
@@ -1,13 +1,61 @@
 # $Source: /ami/sw/cvsroot/pyleginon/gui/wx/Reference.py,v $
-# $Revision: 1.1 $
+# $Revision: 1.2 $
 # $Name: not supported by cvs2svn $
-# $Date: 2006-08-21 23:10:18 $
+# $Date: 2006-08-21 23:51:11 $
 # $Author: suloway $
 # $State: Exp $
 # $Locker:  $
 
+import wx
+from gui.wx.Choice import Choice
+from gui.wx.Entry import FloatEntry
 import gui.wx.Node
+import gui.wx.Settings
+import gui.wx.ToolBar
 
-class AlignZeroLossPeakPanel(gui.wx.Node.Panel):
-	def __init__(self, parent, name):
-		gui.wx.Node.Panel.__init__(self, parent, -1)
+class SettingsDialog(gui.wx.Settings.Dialog):
+    def initialize(self):
+        gui.wx.Settings.Dialog.initialize(self)
+
+        move_types = self.node.calibration_clients.keys()
+        move_types.sort()
+        self.widgets['move type'] = Choice(self, -1, choices=move_types)
+        szmovetype = wx.GridBagSizer(5, 5)
+        szmovetype.Add(wx.StaticText(self, -1, 'Use'), (0, 0), (1, 1), wx.ALIGN_CENTER_VERTICAL)
+        szmovetype.Add(self.widgets['move type'], (0, 1), (1, 1), wx.ALIGN_CENTER_VERTICAL)
+        szmovetype.Add(wx.StaticText(self, -1, 'to move to the reference target'), (0, 2), (1, 1), wx.ALIGN_CENTER_VERTICAL)
+
+        self.widgets['pause time'] = FloatEntry(self, -1, min=0.0, allownone=False, chars=4, value='0.0')
+        szpausetime = wx.GridBagSizer(5, 5)
+        szpausetime.Add(wx.StaticText(self, -1, 'Wait'), (0, 0), (1, 1), wx.ALIGN_CENTER_VERTICAL)
+        szpausetime.Add(self.widgets['pause time'], (0, 1), (1, 1), wx.ALIGN_CENTER_VERTICAL|wx.FIXED_MINSIZE)
+        szpausetime.Add(wx.StaticText(self, -1, 'seconds before performing request'), (0, 2), (1, 1), wx.ALIGN_CENTER_VERTICAL)
+
+        sz = wx.GridBagSizer(5, 5)
+        sz.Add(szmovetype, (0, 0), (1, 1), wx.ALIGN_CENTER_VERTICAL)
+        sz.Add(szpausetime, (1, 0), (1, 1), wx.ALIGN_CENTER_VERTICAL)
+
+        sb = wx.StaticBox(self, -1, 'Reference Target')
+        sbsz = wx.StaticBoxSizer(sb, wx.VERTICAL)
+        sbsz.Add(sz, 0, wx.ALIGN_CENTER|wx.ALL, 5)
+
+        return [sbsz]
+
+class ReferencePanel(gui.wx.Node.Panel):
+    def __init__(self, parent, name):
+        gui.wx.Node.Panel.__init__(self, parent, -1)
+
+        self.toolbar.AddTool(gui.wx.ToolBar.ID_SETTINGS, 'settings', shortHelpString='Settings')
+
+    def onNodeInitialized(self):
+        self.toolbar.Bind(wx.EVT_TOOL, self.onSettingsTool,
+                                            id=gui.wx.ToolBar.ID_SETTINGS)
+
+    def onSettingsTool(self, evt):
+        dialog = SettingsDialog(self)
+        dialog.ShowModal()
+        dialog.Destroy()
+
+class AlignZeroLossPeakPanel(ReferencePanel):
+    icon = 'alignzlp'
+
