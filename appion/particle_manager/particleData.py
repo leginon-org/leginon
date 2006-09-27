8f9e8fc7a24ce36b0b99f4c3ddf3847db5765fb6
Gabriel Lander <glander>
2006-09-27 00:58:08 +0000

    - renamed some tables & fields - added "selexonParams" table to structure

--- appion/particle_manager/particleData.py
+++ appion/particle_manager/particleData.py
@@ -7,7 +7,7 @@
 import data
 import dbdatakeeper
 
-db=dbdatakeeper.DBDataKeeper()
+db=dbdatakeeper.DBDataKeeper(db='dbemparticles')
 
 class run(data.Data):
 	def typemap(cls):
@@ -31,12 +31,12 @@ data.image=image
 class shift(data.Data):
 	def typemap(cls):
 		return data.Data.typemap() + (
-			('run id', run),
+			('runId', run),
 			('dbemdata|AcquisitionImageData|image1', int),
 			('dbemdata|AcquisitionImageData|image2', int),
-			('shift x', float),
-			('shift y', float),
-			('correlation value', float),
+			('shiftx', float),
+			('shifty', float),
+			('correlation', float),
 		)
 	typemap = classmethod(typemap)
 data.shift=shift
@@ -44,13 +44,37 @@ data.shift=shift
 class particle(data.Data):
 	def typemap(cls):
 		return data.Data.typemap() + (
-			('run id', run),
-			('image id', image),
-			('shift id', shift),
-			('selexon params', selexon_params),
-			('x coord', int),
-			('y coord', int),
-			('correlation value', float),
+			('runId', run),
+			('imageId', image),
+			('shiftId', shift),
+			('selexonId', selexonParams),
+			('xcoord', int),
+			('ycoord', int),
+			('correlation', float),
 		)
 	typemap = classmethod(typemap)
 data.particle=particle
+
+class selexonParams(data.Data):
+	def typemap(cls):
+		return data.Data.typemap() + (
+			('runId', run),
+			('template', str),
+			('diam', int),
+			('bin', int),
+			('range_start', int),
+			('range_end', int),
+			('range_increment', int),
+			('manual_thresh', float),
+			('auto_thresh', int),
+			('lp_filt', int),
+			('hp_filt', int),
+			('crud_diameter', int),
+			('crud_blur', float),
+			('crud_low', float),
+			('crud_high', float),
+			('crud_std', float),
+		)
+	typemap = classmethod(typemap)
+data.selexonParams=selexonParams
+
