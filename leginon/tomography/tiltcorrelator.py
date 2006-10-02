623c214e43fc85907ddd02497d805b05269de0e0
suloway <suloway>
2006-10-02 21:12:06 +0000

    hanning window on padded image

--- leginon/tomography/tiltcorrelator.py
+++ leginon/tomography/tiltcorrelator.py
@@ -4,71 +4,11 @@ import peakfinder
 import numarray
 import scipy.ndimage
 
-class TiltCorrelator(object):
-    def __init__(self, binning, tilt_axis):
-        self.reset()
-        from UCSFTomo import ccf2d
-        self.correlation = ccf2d.Correlation()
-        self.setBinning(binning)
-        self.setTiltAxis(tilt_axis)
-
-    def setBinning(self, binning):
-        self.binning = binning
-        self.correlation.setBinning(self.binning)
-
-    def setTiltAxis(self, tilt_axis):
-        self.tilt_axis = tilt_axis
-        self.correlation.setTiltAxis(math.degrees(self.tilt_axis))
-
-    def reset(self):
-        self.shift = {'x':0, 'y':0}
-        self.image = None
-        self.tilt = None
-
-    def correlate(self, image, tilt):
-        if self.image is None and self.tilt is None:
-            self.image = image
-            self.tilt = tilt
-            return
-        self.correlation.setAngles(math.degrees(self.tilt), math.degrees(tilt))
-        correlation_image = self.correlation.doIt(self.image, image)
-        shift = {}
-        shift['x'] = self.shift['x'] + self.correlation.fShiftX
-        shift['y'] = self.shift['y'] - self.correlation.fShiftY
-        self.shift = self.unstretch(self.tilt, tilt, shift)
-
-        return correlation_image
-
-    def getShift(self, raw):
-        if raw:
-            shift = {}
-            shift['y'] = self.correlation.fShiftY
-            shift['x'] = self.correlation.fShiftX
-        else:
-            shift = self.shift.copy()
-        return shift
-
-    def unstretch(self, tilt1, tilt2, shift):
-        cos_tilt1 = math.cos(tilt1)
-        cos_tilt2 = math.cos(tilt2)
-        stretch = abs(cos_tilt2/cos_tilt1)
-        if stretch > 1.0:
-            stretch = 1.0/stretch
-
-        cos_tilt_axis = math.cos(self.tilt_axis)
-        sin_tilt_axis = math.sin(self.tilt_axis)
-
-        # TOOD: make matrix
-        a11 = 1 + (stretch - 1)*cos_tilt_axis*cos_tilt_axis
-        a22 = stretch + (1 - stretch)*cos_tilt_axis*cos_tilt_axis
-        a12 = (1 - stretch)*sin_tilt_axis*cos_tilt_axis
-        a21 = a12
-
-        result = {}
-        result['x'] = a11*shift['x'] + a12*shift['y']
-        result['y'] = a21*shift['x'] + a22*shift['y']
-
-        return result
+def hanning(N):
+    def function(m, n):
+        n = (numarray.hypot(m - N/2.0 + 0.5, n - N/2.0 + 0.5))/N + 0.5
+        return 0.5*(1 - numarray.cos(2*numarray.pi*n))
+    return numarray.fromfunction(function, (N, N))
 
 class Correlator(object):
     def __init__(self, binning, tilt_axis):
@@ -76,6 +16,7 @@ class Correlator(object):
         self.peakfinder = peakfinder.PeakFinder()
         self.reset()
         self.setBinning(binning)
+        self.hanning = None
 
     def setBinning(self, binning):
         pass
@@ -108,8 +49,16 @@ class Correlator(object):
 
     def correlate(self, image, tilt):
         # pad for now (not enough time to not be lazy)
+        if len(image.shape) != 2 or image.shape[0] != image.shape[1]:
+            raise ValueError
+
         padded_image = numarray.zeros(image.shape, image.type())
         image = scipy.ndimage.zoom(image, 0.5)
+
+        if self.hanning is None or image.shape[0] != self.hanning.shape[0]:
+            self.hanning = hanning(image.shape[0])
+        image *= self.hanning
+
         row = (padded_image.shape[0] - image.shape[0])/2
         column = (padded_image.shape[1] - image.shape[1])/2
         padded_image[row:row + image.shape[0], column:column + image.shape[1]] = image
@@ -148,14 +97,14 @@ if __name__ == '__main__':
 
     size = 16
 
-    offset = (512 - 64, 512 - 64)
+    offset = (400 + 64, 400 + 64)
     image = numarray.random_array.random((512, 512))
-    image[offset[0]:offset[0] + size, offset[1]:offset[1] + size] += 1
+    image[offset[0]:offset[0] + size, offset[1]:offset[1] + size] += 16
     _correlator.correlate(image, None)
 
-    offset = (64, 64)
+    offset = (50 + 64, 50 + 64)
     image = numarray.random_array.random((512, 512))
-    image[offset[0]:offset[0] + size, offset[1]:offset[1] + size] += 1
+    image[offset[0]:offset[0] + size, offset[1]:offset[1] + size] += 16
     _correlator.correlate(image, None)
 
     print _correlator.getShift(True)
