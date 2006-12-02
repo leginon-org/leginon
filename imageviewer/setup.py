257224a0004eb4e08d61ac8b52cb2ee9417c34e4
pulokas <pulokas>
2006-12-02 01:06:08 +0000

    install icons

--- imageviewer/setup.py
+++ imageviewer/setup.py
@@ -1,5 +1,12 @@
 from distutils.core import setup
+from distutils.command.install_data import install_data
 import glob
+
+class InstallData(install_data):
+	def run(self):
+		installcommand = self.get_finalized_command('install')
+		self.install_dir = installcommand.install_lib
+		return install_data.run(self)
 
 setup(
     name='Image Viewer',
@@ -12,8 +19,8 @@ setup(
     url='http://www.jensenlab.caltech.edu/',
     packages=['ImageViewer', 'ImageViewer.icons'],
     package_dir={'ImageViewer': ''},
-
-	data_files=[
+    cmdclass={'install_data': InstallData},
+    data_files=[
 		('ImageViewer/icons', glob.glob('icons/*.png'))
 	]
 )
