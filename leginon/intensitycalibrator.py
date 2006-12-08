0f72b6c9d31b651dbecb2b6f1daed5b926a761bb
pulokas <pulokas>
2006-12-08 22:56:40 +0000

    I don't know

--- leginon/intensitycalibrator.py
+++ leginon/intensitycalibrator.py
@@ -1,28 +1,100 @@
+# The Leginon software is Copyright 2004
+# The Scripps Research Institute, La Jolla, CA
+# For terms of the license agreement
+# see http://ami.scripps.edu/software/leginon-license
 #
-# COPYRIGHT:
-#       The Leginon software is Copyright 2003
-#       The Scripps Research Institute, La Jolla, CA
-#       For terms of the license agreement
-#       see  http://ami.scripps.edu/software/leginon-license
-#
+# $Source: /ami/sw/cvsroot/pyleginon/intensitycalibrator.py,v $
+# $Revision: 1.8 $
+# $Name: not supported by cvs2svn $
+# $Date: 2006-12-08 22:56:40 $
+# $Author: pulokas $
+# $State: Exp $
+# $Locker:  $
+
+import numarray
+import threading
 import calibrator
 import calibrationclient
+import data
+import gui.wx.IntensityCalibrator
+import imagefun
+
+class Abort(Exception):
+	pass
 
 class IntensityCalibrator(calibrator.Calibrator):
-	def __init__(self, id, session, managerlocation, **kwargs):
-		calibrator.Calibrator.__init__(self, id, session, managerlocation, **kwargs)
+	panelclass = gui.wx.IntensityCalibrator.Panel
+	settingsclass = data.IntensityCalibratorSettingsData
+	defaultsettings = calibrator.Calibrator.defaultsettings
+	defaultsettings.update({
+		'min': 0.3,
+		'max': 0.7,
+		'increment': 0.01,
+		'label': '',
+	})
 
+	def __init__(self, *args, **kwargs):
+		calibrator.Calibrator.__init__(self, *args, **kwargs)
 
-		self.calclient = calibrationclient.IntensityCalibrationClient(self)
+		self.abort = threading.Event()
 
-
-		self.defineUserInterface()
 		self.start()
 
+	def measureIntensity(self):
+		self.measureIntensityLoop()
 
-	def defineUserInterface(self):
-		calibrator.Calibrator.defineUserInterface(self)
+	def measureIntensityLoop(self):
+		minint = self.settings['min']
+		maxint = self.settings['max']
+		incr = self.settings['increment']
+		label = self.settings['label']
 
-		container = uidata.LargeContainer('Intensity Calibrator')
-		#container.addObjects((cameraconfig, calimage))
-		self.uicontainer.addObject(container)
+		for i in numarray.arange(minint, maxint, incr):
+			self.instrument.tem.Intensity = i
+			imdata = self.acquire(i)
+			stats = self.calcStats(imdata)
+			self.logger.info('%.4f: %s' % (i, stats,))
+			self.storeIntensityMeasurement(label, i, stats)
+			self.checkAbort()
+
+	def bringHome(self):
+		# start with a spread beam
+		self.instrument.tem.Intensity = 0.8
+		imdata0 = self.acquire()
+		mean0 = imagefun.mean(imdata0['image'])
+		
+		# test if we can even see the beam
+		self.instrument.tem.Intensity = 0.7
+		imdata1 = self.acquire()
+		mean1 = imagefun.mean(imdata1['image'])
+
+		print 'mean0,mean1', mean0, mean1
+
+	def acquire(self, intensity):
+		imagedata = self.instrument.getData(data.CorrectedCameraImageData)
+		self.setImage(imagedata['image'])
+		preset = self.presetsclient.getCurrentPreset()
+		acqimdata = data.AcquisitionImageData(initializer=imagedata, session=self.session, preset=preset)
+		acqimdata['filename'] = '%s_%s_%s_%04d' % (self.session['name'], preset['name'], self.settings['label'], int(intensity*1000))
+		self.publish(acqimdata, database=True)
+		return acqimdata
+
+	def calcStats(self, imdata):
+		stats = {}
+		stats['mean'] = imagefun.mean(imdata['image'])
+		stats['stdev'] = imagefun.stdev(imdata['image'])
+		return stats
+
+	def storeIntensityMeasurement(self, label, intensity, stats):
+		meas = data.IntensityMeasurementData(session=self.session, label=label, intensity=intensity)
+		meas.update(stats)
+		self.publish(meas, database=True)
+
+	def checkAbort(self):
+		if not self.abort.isSet():
+			return
+		self.abort.clear()
+		raise Abort('operation aborted')
+
+	def abortCalibration(self):
+		self.abort.set()
