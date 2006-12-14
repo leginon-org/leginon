8f3b8f0a8c1b040c81e7dfb64787e98e193135f0
pulokas <pulokas>
2006-12-14 01:38:58 +0000

    replaced numarray with numpy, much faster

--- leginon/ccdimage.py
+++ leginon/ccdimage.py
@@ -1,70 +1,30 @@
 #!/usr/bin/env python
-import numarray
-import numarray.nd_image
-import numarray.image
-import numarray.ma
 
+import numpy
 
 def combineImages(images, reject=None):
 	'''
 	Creates an average image from a stack of images.
 	reject - specifies outlier rejection threshold
 	'''
-	# if a list of images was give, convert to
-	if type(images) is not numarray.ArrayType:
-		images = imageSequenceToArray(images)
+	# if a list of images was give, convert to 3d array
+	images = numpy.asarray(images)
 	if reject is None:
 		# no mask
-		maskedimages = images
+		avg = numpy.mean(images, 0)
 	else:
 		# calculate std. dev. through stack
-		if len(images) > 10:
-			std = stdev1(images)
-		else:
-			std = stdev2(images)
+		std = numpy.std(images, 0)
 
 		# calculate median through stack
-		med = numarray.image.median(images)
+		med = numpy.median(images)
 
 		# outliers deviate from the median by too much
-		outliers = numarray.abs(images-med) > (reject * std)
-		maskedimages = numarray.ma.masked_array(images, mask=outliers)
+		outliers = numpy.abs(images-med) > (reject * std)
+		maskedimages = numpy.ma.masked_array(images, mask=outliers)
 
-	avg = numarray.ma.average(maskedimages)
-	return numarray.asarray(avg)
-
-def stdev1(images):
-	'''better for a large number of images'''
-	# calculate standard deviation through stack
-	# this is a lot of work but it's the only way I could think of
-	# without writing my own function in C print 'ccc'
-	shape = images[0].shape
-	labels = numarray.arange(shape[0]*shape[1])
-	labels.shape = shape
-	labels = imageSequenceToArray([labels for i in images])
-	std = numarray.nd_image.standard_deviation(images, labels, labels[0])
-	std = numarray.array(std,shape=shape, type=numarray.Float32)
-	return std
-
-def stdev2(images):
-	'''better for a small number of images'''
-	mean = numarray.image.average(images)
-	return numarray.sqrt(numarray.sum(numarray.power(images-mean,2))/(images.shape[0]-1))
-	#.astype(numarray.Float32)
-
-def imageSequenceToArray(images):
-	'''
-	Takes a sequence (list or tuple) of images and creates a 
-	3-dimensional image stack.
-	For some reason this is a lot faster than just calling
-	numarray.array(imagelist).  Maybe it uses more memory or something.
-	'''
-	shape = (len(images),) + images[0].shape
-	a = numarray.zeros(shape, type=images[0].type())
-	for i,im in enumerate(images):
-		a[i] = im
-	return a
-
+		avg = numpy.ma.average(maskedimages, 0)
+	return numpy.asarray(avg)
 
 class CCDImageCorrector(object):
 	def __init__(self):
@@ -152,13 +112,13 @@ class CCDImageCorrector(object):
 
 ################## SELF TEST ######################
 if __name__ == '__main__':
-	import numarray.random_array
+	import numpy
 
 	def randomImage(shape, mean, std):
-		im = numarray.random_array.normal(mean, std, shape) 
-		return im.astype(numarray.Float32)
+		im = numpy.random.normal(mean, std, shape) 
+		return im.astype(numpy.float32)
 
-	shape = 4,4
+	shape = 512,512
 
 	c = CCDImageCorrector()
 
@@ -179,12 +139,15 @@ if __name__ == '__main__':
 	# add fake bias into fake darks
 	bias = c.getReference('bias', (0,0), 2, shape)
 	dark = c.getReference('dark', (0,0), 2, shape)
-	gain = numarray.arange(shape[0]*shape[1], shape=shape,type=numarray.Float32)
-	gain = gain / numarray.nd_image.mean(gain)
+	gain = numpy.arange(shape[0]*shape[1], dtype=numpy.float32)
+	gain.shape = shape
+	gain = gain / gain.mean()
 	gains = [bias + exptime * dark + exptime * 1000 * gain for exptime in exptimes]
 	c.createGain(gains, exptimes, (0,0), 2)
 
-	print c.references
+	for key,value in c.references.items():
+		print key
+		print value
 
 	# fake raw image
 	im = randomImage(shape, 5000, 100)
