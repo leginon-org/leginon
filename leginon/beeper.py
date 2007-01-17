37556b56dc306199bc3cd0c4d1c55dbb160b2a62
pulokas <pulokas>
2007-01-17 00:01:00 +0000

    beeper

---
+++ leginon/beeper.py
@@ -0,0 +1,44 @@
+#
+# COPYRIGHT:
+#       The Leginon software is Copyright 2003
+#       The Scripps Research Institute, La Jolla, CA
+#       For terms of the license agreement
+#       see  http://ami.scripps.edu/software/leginon-license
+#
+
+'''
+'''
+
+import node
+import data
+import event
+import gui.wx.Beeper
+import threading
+
+class Beeper(node.Node):
+	panelclass = gui.wx.Beeper.Panel
+	settingsclass = data.BeeperSettingsData
+	eventinputs = node.Node.eventinputs + [event.TargetListDoneEvent]
+										
+	def __init__(self, id, session, managerlocation, **kwargs):
+		node.Node.__init__(self, id, session, managerlocation, **kwargs)
+
+		self.addEventInput(event.TargetListDoneEvent, self.handleTargetListDone)
+
+		self.stopevent = threading.Event()
+		self.start()
+
+	def handleTargetListDone(self, pubevent=None):
+		self.setStatus('user input')
+		while True:
+			self.beep()
+			time.sleep(0.5)
+			if self.stopevent.isSet():
+				break
+		self.setStatus('idle')
+
+	def stop(self):
+		self.stopevent.set()
+
+	def test(self):
+		self.handleTargetListDone()
---
+++ leginon/gui/wx/Beeper.py
@@ -0,0 +1,70 @@
+# The Leginon software is Copyright 2004
+# The Scripps Research Institute, La Jolla, CA
+# For terms of the license agreement
+# see http://ami.scripps.edu/software/leginon-license
+
+import wx
+import gui.wx.Node
+import gui.wx.Settings
+import gui.wx.ToolBar
+
+class Panel(gui.wx.Node.Panel):
+	icon = 'targetfilter'
+	def __init__(self, parent, name):
+		gui.wx.Node.Panel.__init__(self, parent, -1)
+
+		self.toolbar.AddTool(gui.wx.ToolBar.ID_SETTINGS,
+													'settings',
+													shortHelpString='Settings')
+		self.toolbar.AddSeparator()
+		self.toolbar.AddTool(gui.wx.ToolBar.ID_PLAY,
+													'play',
+													shortHelpString='Process')
+		self.toolbar.AddTool(gui.wx.ToolBar.ID_STOP,
+													'stop',
+													shortHelpString='Stop')
+		self.toolbar.EnableTool(gui.wx.ToolBar.ID_STOP, False)
+		self.toolbar.Realize()
+
+		self.szmain.AddGrowableCol(0)
+		self.SetSizer(self.szmain)
+		self.SetAutoLayout(True)
+		self.SetupScrolling()
+
+	def onNodeInitialized(self):
+		self.toolbar.Bind(wx.EVT_TOOL, self.onSettingsTool,
+											id=gui.wx.ToolBar.ID_SETTINGS)
+		self.toolbar.Bind(wx.EVT_TOOL, self.onPlayTool,
+											id=gui.wx.ToolBar.ID_PLAY)
+		self.toolbar.Bind(wx.EVT_TOOL, self.onStopTool,
+											id=gui.wx.ToolBar.ID_STOP)
+
+	def onSettingsTool(self, evt):
+		dialog = SettingsDialog(self)
+		dialog.ShowModal()
+		dialog.Destroy()
+
+	def onPlayTool(self, evt):
+		self.node.test()
+		self.toolbar.EnableTool(gui.wx.ToolBar.ID_PLAY, False)
+		self.toolbar.EnableTool(gui.wx.ToolBar.ID_STOP, True)
+
+	def onStopTool(self, evt):
+		self.node.stop()
+		self.toolbar.EnableTool(gui.wx.ToolBar.ID_STOP, False)
+		self.toolbar.EnableTool(gui.wx.ToolBar.ID_PLAY, True)
+
+class SettingsDialog(gui.wx.Settings.Dialog):
+	def initialize(self):
+		gui.wx.Settings.Dialog.initialize(self)
+
+		#self.widgets['bypass'] = wx.CheckBox(self, -1, 'Bypass Filter')
+
+		sz = wx.GridBagSizer(5, 10)
+		#sz.Add(self.widgets['bypass'], (0, 0), (1, 1), wx.ALIGN_CENTER_VERTICAL)
+
+		sb = wx.StaticBox(self, -1, 'Beeper')
+		sbsz = wx.StaticBoxSizer(sb, wx.VERTICAL)
+		sbsz.Add(sz, 0, wx.ALIGN_CENTER|wx.ALL, 5)
+
+		return [sbsz]
