9380b84a961f3edc753e6a84c054be06bff951ac
pulokas <pulokas>
2007-01-26 00:38:07 +0000

    some new stuff

--- leginon/raster.py
+++ leginon/raster.py
@@ -4,7 +4,7 @@ convenience functions for raster of points
 import numarray
 import numarray.linear_algebra
 
-def createRaster(shape, spacing, angle):
+def createRaster(shape, spacing, angle, indices=False, limit=None):
 	'''
 	raster across entire image
 	'''
@@ -30,14 +30,53 @@ def createRaster(shape, spacing, angle):
 			maxj = corner[1]
 		if corner[1] < minj:
 			minj = corner[1]
-	print 'min', mini, minj, 'max', maxi, maxj
 
 	# create full raster over whole image
 	rasterpoints = []
+
+	ind = []
 	for i in range(mini,maxi+1):
 		for j in range(minj,maxj+1):
 			p = numarray.matrixmultiply(E, numarray.array((i,j), numarray.Float32))
 			if (0 <= p[0] < shape[0]) and (0 <= p[1] < shape[1]):
 				rasterpoints.append(tuple(p))
+				ind.append((i,j))
+
+	if indices:
+		return ind
+	else:
+		return rasterpoints
+
+
+def createIndices(shape):
+	ind = numarray.indices(shape, numarray.Float32)
+	center0 = shape[0] / 2.0 - 0.5
+	center1 = shape[1] / 2.0 - 0.5
+	ind[0] = ind[0] - center0
+	ind[1] = ind[1] - center1
+	indices = zip(ind[0].flat, ind[1].flat)
+	return indices
+
+def createRaster2(spacing, angle, limit):
+	'''
+	raster across entire image
+	'''
+	co = spacing * numarray.cos(angle)
+	si = spacing * numarray.sin(angle)
+	E = numarray.array(((co,si),(-si,co)), numarray.Float32)
+	Einv = numarray.linear_algebra.inverse(E)
+
+	# create full raster over whole image
+	rasterpoints = []
+
+	shape = limit,limit
+
+	ind = createIndices(shape)
+
+	for i in ind:
+		p = numarray.matrixmultiply(E, numarray.array(i, numarray.Float32))
+		rasterpoints.append(tuple(p))
 
 	return rasterpoints
+
+
