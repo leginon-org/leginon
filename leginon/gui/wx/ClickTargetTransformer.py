888933c99aea5a1cf2a76ca498f1a100d89a8870
Anchi Cheng <acheng>
2007-04-10 22:21:40 +0000

    new derived from image assessor

---
+++ leginon/gui/wx/ClickTargetTransformer.py
@@ -0,0 +1,142 @@
+# The Leginon software is Copyright 2004
+# The Scripps Research Institute, La Jolla, CA
+# For terms of the license agreement
+# see http://ami.scripps.edu/software/leginon-license
+#
+# $Source: /ami/sw/cvsroot/pyleginon/gui/wx/ClickTargetTransformer.py,v $
+# $Revision: 1.1 $
+# $Name: not supported by cvs2svn $
+# $Date: 2007-04-10 22:21:40 $
+# $Author: acheng $
+# $State: Exp $
+# $Locker:  $
+
+import wx
+from gui.wx.Entry import Entry
+import gui.wx.Node
+import gui.wx.Settings
+import gui.wx.ToolBar
+import gui.wx.Choice
+import gui.wx.ClickTargetFinder
+from gui.wx.Presets import PresetChoice
+
+class Panel(gui.wx.ClickTargetFinder.Panel):
+	icon = 'check'
+	imagepanelclass = gui.wx.ImageViewer.TargetImagePanel
+	def __init__(self, parent, name):
+		gui.wx.Node.Panel.__init__(self, parent, -1)
+
+		self.toolbar.AddTool(gui.wx.ToolBar.ID_SETTINGS,
+													'settings',
+													shortHelpString='Settings')
+		self.toolbar.AddSeparator()
+
+		self.toolbar.AddTool(gui.wx.ToolBar.ID_PREVIOUS,
+													'up',
+													shortHelpString='Previous')
+		self.toolbar.AddTool(gui.wx.ToolBar.ID_NEXT,
+													'down',
+													shortHelpString='Next')
+		self.toolbar.AddTool(gui.wx.ToolBar.ID_PLAY,
+													'play',
+													shortHelpString='Transform')
+		self.toolbar.AddTool(gui.wx.ToolBar.ID_STOP,
+													'stop',
+													shortHelpString='Clear')
+
+		self.toolbar.Realize()
+
+		self.addImagePanel()
+
+		self.szmain.AddGrowableCol(0)
+		self.SetSizer(self.szmain)
+		self.SetAutoLayout(True)
+		self.SetupScrolling()
+
+	def addImagePanel(self):
+		# image
+		self.imagepanel = self.imagepanelclass(self, -1)
+
+		self.imagepanel.addTypeTool('Ancestor', display=True)
+		self.imagepanel.selectiontool.setDisplayed('Ancestor', True)
+
+		self.imagepanel.addTypeTool('Image', display=True)
+		self.imagepanel.selectiontool.setDisplayed('Image', True)
+
+		self.imagepanel.addTargetTool('acquisition', wx.GREEN, target=True, display=True)
+		self.imagepanel.selectiontool.setDisplayed('acquisition', True)
+		self.imagepanel.addTargetTool('focus', wx.BLUE, target=True, display=True)
+		self.imagepanel.selectiontool.setDisplayed('focus', True)
+		self.imagepanel.addTargetTool('Done', wx.RED, display=True)
+
+		self.szmain.Add(self.imagepanel, (1, 0), (1, 1), wx.EXPAND|wx.ALL, 3)
+
+
+	def onNodeInitialized(self):
+		self.toolbar.Bind(wx.EVT_TOOL, self.onNextTool,
+											id=gui.wx.ToolBar.ID_NEXT)
+		self.toolbar.Bind(wx.EVT_TOOL, self.onPreviousTool,
+											id=gui.wx.ToolBar.ID_PREVIOUS)
+		self.toolbar.Bind(wx.EVT_TOOL, self.onTransformTool,
+											id=gui.wx.ToolBar.ID_PLAY)
+		self.toolbar.Bind(wx.EVT_TOOL, self.onClearTool,
+											id=gui.wx.ToolBar.ID_STOP)
+		self.toolbar.Bind(wx.EVT_TOOL, self.onSettingsTool,
+											id=gui.wx.ToolBar.ID_SETTINGS)
+
+	def onSettingsTool(self, evt):
+		dialog = SettingsDialog(self)
+		dialog.ShowModal()
+		dialog.Destroy()
+
+	def onTransformTool(self, evt):
+		self.node.onTransform()
+
+	def onClearTool(self, evt):
+		self.node.onClear()
+
+	def onNextTool(self, evt):
+		self.node.onNext()
+
+	def onPreviousTool(self, evt):
+		self.node.onPrevious()
+
+class SettingsDialog(gui.wx.Settings.Dialog):
+	def initialize(self):
+		gui.wx.Settings.Dialog.initialize(self)
+
+		sz = wx.GridBagSizer(5, 10)
+
+		#self.widgets['image directory'] = filebrowse.FileBrowseButton(self, -1)
+		presets = self.node.presetsclient.getPresetNames()
+		self.widgets['child preset'] = PresetChoice(self, -1)
+		self.widgets['child preset'].setChoices(presets)
+		sz.Add(self.widgets['child preset'], (0, 1), (1, 1),
+						wx.ALIGN_CENTER_VERTICAL|wx.EXPAND)
+
+		self.widgets['ancestor preset'] = PresetChoice(self, -1)
+		self.widgets['ancestor preset'].setChoices(presets)
+		sz.Add(self.widgets['ancestor preset'], (1, 1), (1, 1),
+						wx.ALIGN_CENTER_VERTICAL|wx.EXPAND)
+
+
+
+		sb = wx.StaticBox(self, -1, 'Settings')
+		sbsz = wx.StaticBoxSizer(sb, wx.VERTICAL)
+		sbsz.Add(sz, 0, wx.ALIGN_CENTER|wx.ALL, 5)
+
+		return [sbsz,]
+
+if __name__ == '__main__':
+	class App(wx.App):
+		def OnInit(self):
+			frame = wx.Frame(None, -1, 'ClickTargetTransferor Test')
+			panel = Panel(frame, 'Test')
+			frame.Fit()
+			self.SetTopWindow(frame)
+			frame.Show()
+			return True
+
+	app = App(0)
+	app.MainLoop()
+
