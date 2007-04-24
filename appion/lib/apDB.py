674f307cd21547778100b282d57f97ce750cfc50
Neil Voss <vossman>
2007-04-24 20:13:11 +0000

    adding apDB.py; copied directly from sstagg home directory

---
+++ appion/lib/apDB.py
@@ -0,0 +1,8 @@
+#Part of the new appion
+
+import data
+import dbdatakeeper
+
+db=dbdatakeeper.DBDataKeeper()
+apdb=dbdatakeeper.DBDataKeeper(db='dbappiondata')
+projdb=dbdatakeeper.DBDataKeeper(db='project')
