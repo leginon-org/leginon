8962d65663b2af3546ca6244387c96b1c849b7ad
Neil Voss <vossman>
2007-04-29 23:00:11 +0000

    thresh = 0.40

--- appion/bin/runTmplCorrLocal.sh
+++ appion/bin/runTmplCorrLocal.sh
@@ -13,4 +13,4 @@ rm -frv selexlocal
   07jan05b_00012gr_00001sq_v01_00002sq_00_00006en_00.mrc \
   range=0,180,235 templateIds=57 \
   diam=140 lp=0 bin=8 overlapmult=2 \
-  runid=tmplcorrlocal thresh=0.35 method=updated outdir=.
+  runid=tmplcorrlocal thresh=0.40 method=updated outdir=.
