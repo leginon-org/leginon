22690641bc3174976f14098f9624a9946f8054b0
Neil Voss <vossman>
2007-04-29 22:59:46 +0000

    two new rm -fv

--- appion/clean.sh
+++ appion/clean.sh
@@ -1,4 +1,6 @@
 rm -vf `find . -name "*~"`
+rm -vf `find . -name ".#*"`
 rm -vf `find . -name "*.pyo"`
 rm -vf `find . -name "*.pyc"`
 rm -vf `find . -name "*.log"`
+rm -fv `find . -name ".DS_Store"`
