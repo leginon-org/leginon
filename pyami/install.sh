350cc0300af87bef3a5e1b6dd42cf59b4a5460ed
pulokas <pulokas>
2007-05-10 18:53:08 +0000

    cleaned out so I can reuse this cvs module

--- pyami/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-mkdir /ami/sw/lib/python/viewer
-install ImageViewer.py /ami/sw/lib/python/viewer
-install ImageCanvas.py /ami/sw/lib/python/viewer
-install NumericImage.py /ami/sw/lib/python/viewer
