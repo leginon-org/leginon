32656f7ac8d8c49b1a358f1aa523ce1f54aab8ab
Neil Voss <vossman>
2007-05-11 22:07:53 +0000

    run new dog

--- appion/test/runDogPickLocal.sh
+++ appion/test/runDogPickLocal.sh
@@ -3,11 +3,10 @@
 rm -fv `find .. -name "*.py[oc]"`
 rm -frv doglocal
 
-dogPicker.py \
+../bin/basicDogPicker.py \
   07jan05b_00012gr_00001sq_v01_00002sq_00_00002en_00.mrc \
   07jan05b_00012gr_00001sq_v01_00002sq_00_00003en_00.mrc \
   07jan05b_00012gr_00001sq_v01_00002sq_00_00004en_00.mrc \
   07jan05b_00012gr_00001sq_v01_00002sq_00_00005en_00.mrc \
   07jan05b_00012gr_00001sq_v01_00002sq_00_00006en_00.mrc \
-  diam=225 bin=16 outdir=. runid=doglocal \
-  numslices=1 sizerange=0 commit
+  diam=225 bin=16 outdir=. runid=doglocal thresh=0.6
