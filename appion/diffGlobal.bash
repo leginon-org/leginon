0eb6493768426b0ed664c9b2a18b129599a06a03
Neil Voss <vossman>
2007-05-19 18:51:20 +0000

    minor

--- appion/diffGlobal.bash
+++ appion/diffGlobal.bash
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 ./clean.sh
-for i in `find . -type f | grep -v CVS`
+for i in `find . -type f | egrep -v "(CVS|old)"`
 do
 	if [ -f /ami/sw/packages/pyappion/$i ]
 	then
-		echo $i
+		#echo $i
 		diff /ami/sw/packages/pyappion/$i $i
+	else
+		echo MISSING $i
 	fi
 done
