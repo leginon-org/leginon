e4a40217fa385673b6f96e92ffb44c152b191a3b
Neil Voss <vossman>
2007-05-23 18:12:46 +0000

    rm *.out

--- appion/clean.sh
+++ appion/clean.sh
@@ -3,4 +3,5 @@ rm -vf `find . -name ".#*"`
 rm -vf `find . -name "*.pyo"`
 rm -vf `find . -name "*.pyc"`
 rm -vf `find . -name "*.log"`
+rm -vf `find . -name "*.out"`
 rm -fv `find . -name ".DS_Store"`
