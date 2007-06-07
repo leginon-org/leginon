b910db39339b66e7dd93c8832b47dcdad00dad62
Anchi Cheng <acheng>
2007-06-07 01:38:33 +0000

    move mask related functions to the new apMask

--- appion/lib/apParticle.py
+++ appion/lib/apParticle.py
@@ -6,7 +6,10 @@ import os
 import sys
 import time
 #leginon
-import data
+try:
+	import sinedon.data as data
+except ImportError:
+	import data
 #appion
 import apDB
 import appionData
@@ -242,83 +245,6 @@ def convertBoxToPikFile(imgname,params):
 	pfile.writelines(piklist)
 	pfile.close()
 	return pikfilename
-
-def getMaskParamsByRunName(name,sessionname):
-	sessionq = data.SessionData(name=sessionname)
-	sessiondata = leginondb.query(sessionq)[0]
-	sessionid = sessiondata.dbid
-	maskRq=appionData.ApMaskMakerRunData()
-	maskRq['name']=name
-	maskRq['dbemdata|SessionData|session']=sessionid
-	# get corresponding makeMaskParams entry
-	result = appiondb.query(maskRq)
-	return result[0],result[0]['params']
-	
-		
-def insertMaskRegion(rundata,imgdata,regionInfo):
-
-	maskRq = createMaskRegionData(rundata,imgdata,regionInfo)
-	result=appiondb.query(maskRq)
-	if not (result):
-		appiondb.insert(maskRq)
-	
-	return
-
-def createMaskRegionData(rundata,imgdata,regionInfo):
-	maskRq=appionData.ApMaskRegionData()
-
-	maskRq['maskrun']=rundata
-	maskRq['dbemdata|AcquisitionImageData|image']=imgdata.dbid
-	maskRq['x']=regionInfo[4][1]
-	maskRq['y']=regionInfo[4][0]
-	maskRq['area']=regionInfo[0]
-	maskRq['perimeter']=regionInfo[3]
-	maskRq['mean']=regionInfo[1]
-	maskRq['stdev']=regionInfo[2]
-	maskRq['label']=regionInfo[5]
-
-	return maskRq
-
-def getMaskRegions(maskrun,imgid):
-	maskRq=appionData.ApMaskRegionData()
-
-	maskRq['maskrun']=maskrun
-	maskRq['dbemdata|AcquisitionImageData|image']=imgid
-	
-	results=appiondb.query(maskRq)
-	
-	return results
-
-def insertMaskAssessmentRun(sessiondata,maskrundata,name):
-	assessRdata=appionData.ApMaskAssessmentRunData()
-	assessRdata['dbemdata|SessionData|session'] = sessiondata.dbid
-	assessRdata['maskrun'] = maskrundata
-	assessRdata['name'] = name
-
-	result=appiondb.query(assessRdata)
-	if not (result):
-		appiondb.insert(assessRdata)
-		exist = False
-	else:
-		exist = True
-
-	return assessRdata,exist
-	
-def insertMaskAssessment(rundata,regiondata,keep):
-
-	assessMq = createMaskAssessmentData(rundata,regiondata,keep)
-	appiondb.insert(assessMq,force=True)
-	
-	return
-
-def createMaskAssessmentData(rundata,regiondata,keep):
-	assessMq=appionData.ApMaskAssessmentData()
-	
-	assessMq['run']=rundata
-	assessMq['region']=regiondata
-	assessMq['keep']=keep
-
-	return assessMq
 
 def pik2Box(params,file):
 	box=params["box"]
