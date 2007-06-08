4b71682a5cbff72ea4f23b23b6f175c37db299bc
Neil Voss <vossman>
2007-06-08 23:19:28 +0000

    makestack test

---
+++ appion/test/runMakeStack.sh
@@ -0,0 +1,6 @@
+../bin/makestack.py \
+ single=start.hed runid=test1 \
+ outdir=/ami/data10/appion/07jan05b/stacks/ prtlrunId=53 \
+ phaseflip boxsize=128 bin=4 ace=0.95 \
+ mindefocus=-0.25e-6 description="testing" \
+ limit=150 commit
