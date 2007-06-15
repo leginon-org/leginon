f5bef4cbeaf9b53437985d2ffb67ed7db9145f80
dfellman <dfellman>
2007-06-15 19:03:23 +0000

    ---------------------------------------------------------------------- timing report files Committing in . Added Files:  	timing.php timingstatsgraph.php
    ----------------------------------------------------------------------

---
+++ myamiweb/timing.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ *	The Leginon software is Copyright 2003 
+ *	The Scripps Research Institute, La Jolla, CA
+ *	For terms of the license agreement
+ *	see  http://ami.scripps.edu/software/leginon-license
+ */
+
+require ("inc/leginon.inc");
+
+$defaultId= 1445;
+$sessionId= ($_GET['Id']) ? $_GET['Id'] : $defaultId;
+
+// --- Set  experimentId
+// $lastId = $leginondata->getLastSessionId();
+// $sessionId = (empty($_GET[Id])) ? $lastId : $_GET[sessionId];
+$sessioninfo = $leginondata->getSessionInfo($sessionId);
+$title = $sessioninfo[Name];
+//$presets = $leginondata->getDatalabels($sessionId);
+$presets = $leginondata->getDatatypes($sessionId);
+$preset="hi3";
+$preset=$_GET['preset'];
+$timings = $leginondata->getTiming($sessionId, $preset);
+
+?>
+<html>
+<head>
+<title>Timing Statistics for session <?=$title; ?></title>
+<link rel="stylesheet" type="text/css" href="css/viewer.css"> 
+</head>
+
+<body>
+<table border="0" cellpadding=10>
+<tr>
+ <td>
+  <A class="header" HREF="index.php">&lt;index&gt;</A>
+ </td>
+ <td>
+  <A class="header" HREF="3wviewer.php?sessionId=<?=$sessionId; ?>">&lt;view <?=$title; ?>&gt;</A>
+ </td>
+</tr>
+</table>
+<table border="0" cellpadding=10>
+<tr valign="top">
+	<td colspan="2">
+	<?=divtitle("Timing Statistics for session $title "); ?>
+	</td>
+</tr>
+<?php
+$presets = array_reverse($presets);
+foreach ($presets as $preset) {
+//	$preset = $preset['label'];
+	if (!$preset)
+		continue;
+	echo "<tr>";
+	echo "<td>";
+	echo "- Acquisition Time for Preset $preset"; 
+	echo "</td>";
+	echo "</tr>";
+?>
+<tr>
+<td>
+<a href="timingstatsgraph.php?hg=1&vdata=1&Id=<?=$sessionId; ?>&preset=<?=$preset; ?>">[data]</a>
+<a href="timingstatsgraph.php?hg=1&vs=1&Id=<?=$sessionId; ?>&preset=<?=$preset; ?>">[sql]</a><br>
+<a href="timingstatsgraph.php?hg=1&Id=<?=$sessionId; ?>&preset=<?=$preset; ?>"><img border="0" src="timingstatsgraph.php?w=300&hg=1&Id=<?=$sessionId; ?>&preset=<?=$preset; ?>"></a>
+</td>
+<td>
+<a href="timingstatsgraph.php?vdata=1&Id=<?=$sessionId; ?>&preset=<?=$preset; ?>">[data]</a>
+<a href="timingstatsgraph.php?vs=1&Id=<?=$sessionId; ?>&preset=<?=$preset; ?>">[sql]</a><br>
+<a href="timingstatsgraph.php?Id=<?=$sessionId; ?>&preset=<?=$preset; ?>"><img border="0" src="timingstatsgraph.php?w=300&Id=<?=$sessionId; ?>&preset=<?=$preset; ?>"></a>
+</td>
+</tr>
+<?php } ?>
+</table>
+</td>
+</tr>
+</table>
+</body>
+</html>
---
+++ myamiweb/timingstatsgraph.php
@@ -0,0 +1,97 @@
+<?php
+
+/**
+ *	The Leginon software is Copyright 2003 
+ *	The Scripps Research Institute, La Jolla, CA
+ *	For terms of the license agreement
+ *	see  http://ami.scripps.edu/software/leginon-license
+ */
+
+include ("inc/jpgraph.php");
+include ("inc/jpgraph_line.php");
+include ("inc/jpgraph_scatter.php");
+include ("inc/jpgraph_bar.php");
+include ("inc/histogram.inc");
+include ("inc/image.inc");
+require_once ("inc/leginon.inc");
+
+$defaultId= 1445;
+$defaultpreset='hl';
+$histogram = ($_GET[hg]==1) ? true : false;
+$sessionId= ($_GET[Id]) ? $_GET[Id] : $defaultId;
+$preset = ($_GET[preset]) ? $_GET[preset] : $defaultpreset;
+$viewdata = ($_GET['vdata']==1) ? true : false;
+$viewsql = $_GET[vs];
+$data_name='acquisitiontime';
+
+$timingdata = $leginondata->getTiming($sessionId, $preset);
+foreach($timingdata as $t) {
+	$datax[] = $t['unix_timestamp'];
+	$datay[] = $t[$data_name];
+}
+if ($viewsql) {
+	$sql = $leginondata->mysql->getSQLQuery();
+	echo $sql;
+	exit;
+}
+if ($viewdata) {
+	$keys = array("timestamp", "filename", $data_name);
+	echo dumpData($timingdata, $keys);
+	exit;
+}
+
+function TimeCallback($aVal) {
+    return Date('H:i',$aVal);
+}
+
+$width = $_GET['w'];
+$height = $_GET['h'];
+if (!$datay) {
+	$width = 12;
+	$height = 12;
+	$source = blankimage($width,$height);
+} else {
+		$graph = new Graph(600,400,"auto");    
+	if ($histogram) {
+		$histogram = new histogram($datay);
+		$histogram->setBarsNumber(50);
+		$rdata = $histogram->getData();
+
+		$rdatax = $rdata['x'];
+		$rdatay = $rdata['y'];
+
+		$graph->SetScale("linlin");
+
+		$graph->img->SetMargin(40,30,20,40);
+
+		$bplot = new BarPlot($rdatay, $rdatax);
+		$graph->Add($bplot);
+
+		$graph->title->Set("$data_name histogram for preset $preset");
+		$graph->xaxis->title->Set("$data_name");
+
+		$graph->yaxis->title->Set("Frequency");
+	} else {
+		$graph->title->Set("$data_name for preset $preset");
+		$graph->SetAlphaBlending();
+		$graph->SetScale("intlin",0,'auto'); //,$datax[0],$datax[$n-1]);
+		$graph->xaxis->SetLabelFormatCallback('TimeCallback');
+		$graph->xaxis->SetLabelAngle(90);
+		$graph->xaxis->SetTitlemargin(25);
+		$graph->xaxis->title->Set("time");
+		$graph->yaxis->SetTitlemargin(35);
+		$graph->yaxis->title->Set("$data_name");
+
+		$sp1 = new ScatterPlot($datay,$datax);
+		$sp1->mark->SetType(MARK_CIRCLE);
+		$sp1->mark->SetColor('red');
+		$sp1->mark->SetWidth(4);
+		$graph->Add($sp1);
+		$p1 = new LinePlot($datay,$datax);
+	}
+	$source = $graph->Stroke(_IMG_HANDLER);
+}
+
+	resample($source, $width, $height);
+
+?>
