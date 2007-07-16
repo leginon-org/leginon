cde881e7199407888f8fc0ff79adf95f6ef55ede
Scott Stagg <sstagg>
2007-07-16 22:28:55 +0000

    added showinspected.php file

---
+++ myamiweb/showinspectdata.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ *	The Leginon software is Copyright 2003 
+ *	The Scripps Research Institute, La Jolla, CA
+ *	For terms of the license agreement
+ *	see  http://ami.scripps.edu/software/leginon-license
+ */
+require ("inc/particledata.inc");
+require_once ("inc/leginon.inc");
+
+$sessionId= $_GET[Id];
+$viewdata = ($_GET[vd]==1) ? true : false;
+$viewsql = ($_GET[vs]==1) ? true : false;
+
+$particle = new particledata();
+
+$assessmentId=$particle->getLastAssessmentRun($sessionId);
+$inspectinfo = $particle->getAssessmentDataForRun($assessmentId);
+if ($viewdata) {
+	echo dumpData($inspectinfo);
+}
+
+if ($viewsql) {
+	$sql = $particle->mysql->getSQLQuery();
+	echo $sql;
+	exit;
+}
+?>
