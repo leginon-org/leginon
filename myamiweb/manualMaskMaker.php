abed4f0822a1827e3a3ee689f99b19cca79da091
Anchi Cheng <acheng>
2007-07-20 21:17:03 +0000

    instruction

---
+++ myamiweb/manualMaskMaker.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ *      The Leginon software is Copyright 2003 
+ *      The Scripps Research Institute, La Jolla, CA
+ *      For terms of the license agreement
+ *      see  http://ami.scripps.edu/software/leginon-license
+ *
+ *      Simple viewer to view a image using mrcmodule
+ */
+
+//writeTop("Leginon Manual Mask Maker");
+
+	echo"
+<TABLE> <TR><TD ALIGN='LEFT'> <B> Web interaction NOT READY </B>\n </TD></TR>";
+
+	echo"<TR><TD ALIGN='LEFT'> run Leginon (1.4) and use the <B>CreateMask</B> node in the application <B>Masking</B>\n </TD></TR>";
+	echo"<TR><TD ALIGN='LEFT'><FONT COLOR='RED'><B>This only works on a 32-bit machine due to problem with reading/writing PNG</B></FONT>\n</TD></TR>";
+	echo"<TR><TD ALIGN='LEFT'><FONT COLOR='RED'><B>i.e., do not use defcon1 nor defcon2 </B></FONT>\n</TD></TR>";
+echo "</TABLE>";
+?>
