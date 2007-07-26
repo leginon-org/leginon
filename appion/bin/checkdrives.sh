3a91015bcef1044ed797b7111a1e4c4b960dcdba
Neil Voss <vossman>
2007-07-26 18:36:26 +0000

    update

--- appion/bin/checkdrives.sh
+++ appion/bin/checkdrives.sh
@@ -1,1 +1,8 @@
-df -h | grep /ami/data | sort -k 5
+#!/bin/bash
+
+for i in 02 03 04 05 06 07 08 09 10 11 12 13 14;
+do
+  cd /ami/data$i;
+done;
+
+df -h | sed 's/^Filesystem//' | sed 's/^ *//g' | egrep "(/ami/data|Size)" | sort -k 5
