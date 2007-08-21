5e0739ac23622dc9a92b936e05c9577b4e1d28df
Neil Voss <vossman>
2007-08-21 22:57:51 +0000

    more shapes

--- leginon/gui/wx/ImageViewer.py
+++ leginon/gui/wx/ImageViewer.py
@@ -1,14 +1,14 @@
-#!/usr/bin/env python
+#!/usr/bin/python -O
 # The Leginon software is Copyright 2004
 # The Scripps Research Institute, La Jolla, CA
 # For terms of the license agreement
 # see http://ami.scripps.edu/software/leginon-license
 #
 # $Source: /ami/sw/cvsroot/pyleginon/gui/wx/ImageViewer.py,v $
-# $Revision: 1.57 $
+# $Revision: 1.58 $
 # $Name: not supported by cvs2svn $
-# $Date: 2007-06-30 01:10:37 $
-# $Author: pulokas $
+# $Date: 2007-08-21 22:57:51 $
+# $Author: vossman $
 # $State: Exp $
 # $Locker:  $
 
@@ -130,6 +130,19 @@ def targetIcon(color, shape):
 			dc.DrawLine(8, 1, 8, 14)
 			dc.DrawLine(1, 8, 14, 8)
 			dc.DrawPoint(1, 7)
+		elif shape == 'x':
+			dc.DrawLine(1, 1, 14, 14)
+			dc.DrawLine(1, 14, 14, 1)
+			dc.DrawPoint(1, 7)
+		elif shape == '*':
+			dc.DrawLine(1, 1, 14, 14)
+			dc.DrawLine(1, 14, 14, 1)
+			dc.DrawLine(8, 1, 8, 14)
+			dc.DrawLine(1, 8, 14, 8)
+			dc.DrawPoint(1, 7)
+		elif shape == 'o':
+			dc.DrawCircle(8, 8, 7)
+			dc.DrawPoint(1, 7)
 		elif shape == 'polygon':
 			dc.DrawLine(3, 1, 13, 1)
 			dc.DrawLine(13, 1, 13, 13)
@@ -150,8 +163,14 @@ def getTargetBitmap(color, shape='+'):
 			bitmap = targetBitmap_plus(color)
 		elif shape == '.':
 			bitmap = targetBitmap_point(color)
+		elif shape == 'x':
+			bitmap = targetBitmap_cross(color)
+		elif shape == '*':
+			bitmap = targetBitmap_star(color)
+		elif shape == 'o':
+			bitmap = targetBitmap_circle(color)
 		else:
-			raise RuntimeError('invalid target shape')
+			raise RuntimeError('invalid target shape: '+shape)
 		targetbitmaps[color,shape] = bitmap
 	return bitmap
 
@@ -181,6 +200,58 @@ def targetBitmap_plus(color):
 	dc.SetPen(wx.Pen(color, penwidth))
 	dc.DrawLine(length/2, 0, length/2, length)
 	dc.DrawLine(0, length/2, length, length/2)
+	dc.EndDrawing()
+	dc.SelectObject(wx.NullBitmap)
+	bitmap.SetMask(wx.Mask(bitmap, wx.WHITE))
+	return bitmap
+
+def targetBitmap_cross(color):
+	penwidth = 2
+	length = 15
+	bitmap = wx.EmptyBitmap(length, length)
+	dc = wx.MemoryDC()
+	dc.SelectObject(bitmap)
+	dc.BeginDrawing()
+	dc.Clear()
+	dc.SetBrush(wx.Brush(color, wx.TRANSPARENT))
+	dc.SetPen(wx.Pen(color, penwidth))
+	dc.DrawLine(0, 0, length, length)
+	dc.DrawLine(0, length, length, 0)
+	dc.EndDrawing()
+	dc.SelectObject(wx.NullBitmap)
+	bitmap.SetMask(wx.Mask(bitmap, wx.WHITE))
+	return bitmap
+
+def targetBitmap_star(color):
+	penwidth = 2
+	length = 15
+	bitmap = wx.EmptyBitmap(length, length)
+	dc = wx.MemoryDC()
+	dc.SelectObject(bitmap)
+	dc.BeginDrawing()
+	dc.Clear()
+	dc.SetBrush(wx.Brush(color, wx.TRANSPARENT))
+	dc.SetPen(wx.Pen(color, penwidth))
+	dc.DrawLine(0, 0, length, length)
+	dc.DrawLine(0, length, length, 0)
+	dc.DrawLine(length/2, 0, length/2, length)
+	dc.DrawLine(0, length/2, length, length/2)
+	dc.EndDrawing()
+	dc.SelectObject(wx.NullBitmap)
+	bitmap.SetMask(wx.Mask(bitmap, wx.WHITE))
+	return bitmap
+
+def targetBitmap_circle(color):
+	penwidth = 2
+	length = 15
+	bitmap = wx.EmptyBitmap(length, length)
+	dc = wx.MemoryDC()
+	dc.SelectObject(bitmap)
+	dc.BeginDrawing()
+	dc.Clear()
+	dc.SetBrush(wx.Brush(color, wx.TRANSPARENT))
+	dc.SetPen(wx.Pen(color, penwidth))
+	dc.DrawCircle(length/2, length/2, length/2)
 	dc.EndDrawing()
 	dc.SelectObject(wx.NullBitmap)
 	bitmap.SetMask(wx.Mask(bitmap, wx.WHITE))
@@ -579,7 +650,7 @@ class ZoomTool(ImageTool):
 		self.zoom(selection, viewcenter)
 
 class ImagePanel(wx.Panel):
-	def __init__(self, parent, id, imagesize=(512, 512)):
+	def __init__(self, parent, id, imagesize=(512, 768)):
 		# initialize image variables
 		self.imagedata = None
 		self.bitmap = None
@@ -725,6 +796,7 @@ class ImagePanel(wx.Panel):
 		'''
 		if self.bitmap is not None:
 			width, height = self.bitmap.GetWidth(), self.bitmap.GetHeight()
+			
 			if self.scaleImage():
 				virtualsize = (width - 1, height - 1)
 			else:
@@ -1526,8 +1598,9 @@ class TargetType(object):
 		self.targets = []
 		for target in targets:
 			if isinstance(target, dict):
-				self.targets.append(StatsTarget(target['x'], target['y'],
-																				self, target['stats']))
+				self.targets.append(StatsTarget(target['x'], target['y'], self, target['stats']))
+			elif isinstance(target, Target):
+				self.targets.append(Target(target.x, target.y, self))
 			else:
 				self.targets.append(Target(target[0], target[1], self))
 
