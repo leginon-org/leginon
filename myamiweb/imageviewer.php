0bd6e56d88d082110c72cce87ef99055db472e21
Anchi Cheng <acheng>
2007-08-21 22:42:12 +0000

    first version

--- myamiweb/imageviewer.php
+++ myamiweb/imageviewer.php
@@ -68,6 +68,7 @@ viewer_header('image viewer', $javascript, 'initviewer()');
 ?>
 <a class="header" target="summary" href="summary.php?expId=<?php echo $sessionId ?>">[summary]</A>
 <a class="header" target="processing" href="processing.php?expId=<?php echo $sessionId; ?>">[processing]</A>
+<a class="header" target="make jpgs" href="runjpgmaker.php?expId=<?php echo $sessionId; ?>">[make jpgs]</A>
 <?php
 $viewer->display();
 viewer_footer();
