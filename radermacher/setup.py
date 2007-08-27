64c404e24611d8bd4f14b81b28cb0d99f754e729
Neil Voss <vossman>
2007-08-27 21:31:41 +0000

    fixes

--- radermacher/setup.py
+++ radermacher/setup.py
@@ -1,14 +1,20 @@
+#!/usr/bin/python
+
 from distutils.core import setup
 from numpy.distutils.extension import Extension
 import numpy
 
 numpyinc = numpy.get_include()
 
-module = Extension('radermacher', sources = ['radermacher.c'], include_dirs=[numpyinc,])
+module = Extension(
+	 'radermacher', 
+	 sources = ['main.c', 'tiltang.c', 'willsq.c'], 
+	 include_dirs=[numpyinc,]
+	)
 
 setup(
 	name='Radermacher',
-	version='0.9',
+	version='0.1',
 	description='Radermacher functions',
 	url='http://nramm.scripps.edu/',
 	ext_modules=[module]
