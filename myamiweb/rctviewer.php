1563f2af959025ec7795876037ed588aced4a40b
Neil Voss <vossman>
2007-08-30 22:07:55 +0000

    Add RCT Viewer

---
+++ myamiweb/rctviewer.php
@@ -0,0 +1,78 @@
+<?php
+require ('inc/leginon.inc');
+require ('inc/project.inc');
+require ('inc/viewer.inc');
+require ('inc/auth.inc');
+
+$sessionId = ($_POST[sessionId]) ? $_POST[sessionId] : $_GET[expId];
+$projectId = ($_POST[projectId]) ? $_POST[projectId] : 'all';
+$imageId = $_POST[imageId];
+$preset = $_POST[$_POST[controlpre]];
+
+// --- Set sessionId
+$lastId = $leginondata->getLastSessionId();
+$sessionId = (empty($sessionId)) ? $lastId : $sessionId;
+
+$projectdata = new project();
+$projectdb = $projectdata->checkDBConnection();
+
+if($projectdb)
+	$projects = $projectdata->getProjects('all');
+
+if(!$sessions)
+	$sessions = $leginondata->getSessions('description', $projectId);
+
+// --- update SessionId while a project is selected
+$sessionId_exists = $leginondata->sessionIdExists($sessions, $sessionId);
+if (!$sessionId_exists)
+	$sessionId=$sessions[0][id];
+$filenames = $leginondata->getFilenames($sessionId, $preset);
+// --- Get data type list
+$datatypes = $leginondata->getAllDatatypes($sessionId);
+
+$viewer = new viewer();
+if($projectdb) {
+	foreach($sessions as $s) {
+		if ($s['id']==$sessionId) {
+			$sessionname = $s['name_org'];
+			break;
+		}
+	}
+	$currentproject = $projectdata->getProjectFromSession($sessionname);
+	$viewer->setProjectId($projectId);
+	$viewer->addProjectSelector($projects, $currentproject);
+}
+$viewer->setSessionId($sessionId);
+$viewer->setImageId($imageId);
+$viewer->addSessionSelector($sessions);
+$viewer->addFileSelector($filenames);
+$viewer->setNbViewPerRow('2');
+$pl_refresh_time="2.0";
+$viewer->addPlaybackControl($pl_refresh_time);
+$playbackcontrol=$viewer->getPlaybackControl();
+$javascript = $viewer->getJavascript();
+
+$view1 = new view('Main View', 'v1');
+$view1->setControl();
+$view1->addMenuItems($playbackcontrol);
+$view1->setDataTypes($datatypes);
+$view1->setSize(350);
+$view1->setPresetScript("getpreset.php?tl=1");
+$viewer->add($view1);
+
+
+$view2 = new view('RCT', 'v3');
+$view2->setSize(350);
+$view2->setDataTypes(array('rct'=>'rct'));
+$view2->setPresetScript("getpreset.php?tl=1");
+$viewer->add($view2);
+
+
+$javascript .= $viewer->getJavascriptInit();
+viewer_header('image viewer', $javascript, 'initviewer()');
+?>
+<a class="header" target="summary" href="summary.php?expId=<?php echo $sessionId; ?>">[summary]</A>
+<?php
+$viewer->display();
+viewer_footer();
+?>
