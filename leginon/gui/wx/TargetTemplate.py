1f77ea6906bca77b97a5a5d12dcf35afc7fd5014
Neil Voss <vossman>
2007-09-06 18:27:10 +0000

    New 'Auto Fill...' feature

--- leginon/gui/wx/TargetTemplate.py
+++ leginon/gui/wx/TargetTemplate.py
@@ -4,14 +4,15 @@
 # see http://ami.scripps.edu/software/leginon-license
 #
 # $Source: /ami/sw/cvsroot/pyleginon/gui/wx/TargetTemplate.py,v $
-# $Revision: 1.6 $
+# $Revision: 1.7 $
 # $Name: not supported by cvs2svn $
-# $Date: 2007-02-27 21:07:00 $
-# $Author: acheng $
+# $Date: 2007-09-06 18:27:10 $
+# $Author: vossman $
 # $State: Exp $
 # $Locker:  $
 
 import wx
+import math
 from gui.wx.Entry import IntEntry
 
 TemplateUpdatedEventType = wx.NewEventType()
@@ -91,20 +92,23 @@ class Panel(wx.Panel):
 		self.badd = wx.Button(self, -1, 'Add...')
 		self.bedit = wx.Button(self, -1, 'Edit...')
 		self.bdelete = wx.Button(self, -1, 'Delete')
+		self.bautofill = wx.Button(self, -1, 'Auto Fill...')
 
 		sz = wx.GridBagSizer(5, 5)
 		label = wx.StaticText(self, -1, title)
 		sz.Add(label, (0, 0), (1, 2), wx.ALIGN_CENTER_VERTICAL)
-		sz.Add(self.lbtemplate, (1, 0), (2, 3), wx.EXPAND)
+		sz.Add(self.lbtemplate, (1, 0), (2, 4), wx.EXPAND)
 		sz.Add(self.badd, (3, 0), (1, 1), wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_RIGHT)
 		sz.Add(self.bedit, (3, 1), (1, 1), wx.ALIGN_CENTER)
 		sz.Add(self.bdelete, (3, 2), (1, 1), wx.ALIGN_CENTER)
+		sz.Add(self.bautofill, (3, 3), (1, 1), wx.ALIGN_CENTER)
 		sz.AddGrowableRow(1)
 		sz.AddGrowableCol(0)
 
 		self.Bind(wx.EVT_BUTTON, self.onAddButton, self.badd)
 		self.Bind(wx.EVT_BUTTON, self.onEditButton, self.bedit)
 		self.Bind(wx.EVT_BUTTON, self.onDeleteButton, self.bdelete)
+		self.Bind(wx.EVT_BUTTON, self.onAutoFillButton, self.bautofill)
 		self.Bind(wx.EVT_LISTBOX, self.onTemplateListBox, self.lbtemplate)
 
 		self._template = []
@@ -156,6 +160,15 @@ class Panel(wx.Panel):
 			self._updated(True)
 		dialog.Destroy()
 
+	def onAutoFillButton(self, evt):
+		n = self.lbtemplate.GetSelection()
+		dialog = AutoFillTargets(self, 'Auto Fill Targets')
+		if dialog.ShowModal() == wx.ID_OK:
+			for target in dialog.targets:
+				self._addTarget(target)
+			self._updated(True)
+		dialog.Destroy()
+
 	def validate(self, target):
 		if None in target:
 			return False
@@ -182,6 +195,63 @@ class Panel(wx.Panel):
 		del self._template[selection]
 		self.lbtemplate.Delete(selection)
 		self._updated(True)
+
+class AutoFillTargets(wx.Dialog):
+	def __init__(self, parent, title, targetname='Relative target'):
+		wx.Dialog.__init__(self, parent, -1, title)
+
+		self.targetname = targetname
+		self.numtargets = IntEntry(self, -1, chars=4, value='1')
+		self.radius = IntEntry(self, -1, chars=4, value='100')
+		self.angleoffset = IntEntry(self, -1, chars=4, value='0')
+		self.targets = []
+
+		valueentry = wx.GridBagSizer(5, 5)
+		label = wx.StaticText(self, -1, 'number of targets')
+		valueentry.Add(label, (0, 0), (1, 1), wx.ALIGN_LEFT|wx.ALL, 3)
+		valueentry.Add(self.numtargets, (0, 1), (1, 1), wx.ALIGN_CENTER|wx.FIXED_MINSIZE|wx.ALL, 3)
+
+		label = wx.StaticText(self, -1, 'radius (pixels)')
+		valueentry.Add(label, (1, 0), (1, 1), wx.ALIGN_LEFT|wx.ALL, 3)
+		valueentry.Add(self.radius, (1, 1), (1, 1), wx.ALIGN_CENTER|wx.FIXED_MINSIZE|wx.ALL, 3)
+
+		label = wx.StaticText(self, -1, 'angle offset (degrees)')
+		valueentry.Add(label, (2, 0), (1, 1), wx.ALIGN_LEFT|wx.ALL, 3)
+		valueentry.Add(self.angleoffset, (2, 1), (1, 1), wx.ALIGN_CENTER|wx.FIXED_MINSIZE|wx.ALL, 3)
+
+		self.bok = wx.Button(self, wx.ID_OK, 'OK')
+		self.Bind(wx.EVT_BUTTON, self.onOK, self.bok)
+		self.bcancel = wx.Button(self, wx.ID_CANCEL, 'Cancel')
+
+		szbutton = wx.GridBagSizer(5, 5)
+		szbutton.Add(self.bok, (0, 0), (1, 1), wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_RIGHT)
+		szbutton.Add(self.bcancel, (0, 1), (1, 1), wx.ALIGN_CENTER)
+		szbutton.AddGrowableCol(0)
+
+		sz = wx.GridBagSizer(5, 5)
+		sz.Add(valueentry, (0, 0), (1, 1), wx.EXPAND|wx.ALL, 10)
+		sz.Add(szbutton, (1, 0), (1, 1), wx.EXPAND|wx.ALL, 10)
+
+		self.SetSizerAndFit(sz)
+
+	def onOK(self, evt):
+		numtargets = float(self.numtargets.GetValue())
+		radius = float(self.radius.GetValue())
+		degoffset = float(self.angleoffset.GetValue())
+		deg = 0.0
+		degincr = 360.0/numtargets
+		while(deg < 359.9):
+			rad = (deg + degoffset) * math.pi / 180.0
+			x = int(math.sin(rad)*radius)
+			y = int(math.cos(rad)*radius)
+			target = (x, y)
+			if not self.GetParent().validate(target):
+				dialog = wx.MessageDialog(self, 'Invalid target dimensions (%d,%d)' % x,y, 'Error', wx.OK|wx.ICON_ERROR)
+				dialog.ShowModal()
+				dialog.Destroy()
+			self.targets.append(target)
+			deg += degincr
+		evt.Skip()
 
 if __name__ == '__main__':
 	class App(wx.App):
