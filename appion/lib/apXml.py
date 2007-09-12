a4881069c3de5c30bc9b6802104d9ee78faf8951
Neil Voss <vossman>
2007-09-12 18:05:37 +0000

    New features: write to file and list parsing

--- appion/lib/apXml.py
+++ appion/lib/apXml.py
@@ -1,12 +1,13 @@
 import sys
 import apDisplay
 import re
+import os
 import xml.dom.minidom
 
 #taken from http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/409899
 #Title: xmlreader2
 #Submitter: Peter Neish (other recipes) 
-#HEAVILY modified by Neil
+#**HEAVILY** modified by Neil
 
 def nodeToDict(node):
 	"""
@@ -34,18 +35,159 @@ def nodeToDict(node):
 			xmldict.update({str(n.nodeName): nodeToDict(n)})
 	return xmldict
 
-def readConfig(filename):
+def readDictFromXml(filename):
+	if not os.path.isfile(filename):
+		apDisplay.printWarning("Failed to open file "+filename+" for reading")
+		return None
 	dom = xml.dom.minidom.parse(filename)
 	xmldict = nodeToDict(dom.childNodes[0])
 	return xmldict
 
 ### END BORROWED CODE
 
+def readDictAndConvertFromXml(filename):
+	xmldict = readDictFromXml(filename)
+	convertDictData(xmldict)
+	return xmldict
+
+def convertDictData(xmldict):
+	newlist = []
+	for k,v in xmldict.items():
+		#convert data
+		xmldict[k] = convertValue(v)
+
+		#convert
+		if type(v) == type({}):
+			xmldict[k] = convertDictData(v)
+			v = xmldict[k]
+		if type(v) == type("") and ',' in v:
+			if v[-1:] == ',':
+				v = v[:-1]
+			mylist = v.split(',')
+			for i,val in enumerate(mylist):
+				mylist[i] = convertValue(val)
+			xmldict[k] = mylist
+
+		#make a list
+		if k[:4] == 'list':
+			newlist.append(xmldict[k])
+			del xmldict[k]
+
+	if len(newlist) > 1:
+		return newlist
+	return xmldict
+
+def convertValue(v):
+
+	if type(v) != type(""):
+		return v
+	if re.match("^[0-9]+$", v):
+		return int(v)
+	elif re.match("^[0-9]+\.[0-9]+$", v):
+		return round(float(v),2)
+	elif re.match("^False$", v):
+		return False
+	elif re.match("^True$", v):
+		return True
+	return v
+
+def dictToStr(xmldict, level=0):
+	#setup line prefix -- tab level
+	pre = ""
+	for i in range(level):
+		pre += "\t"
+	if pre == "":
+		pre = " "
+
+	mystr = ""
+	for k,v in xmldict.items():
+		#open dict key
+		mystr += pre+"<"+str(k)+">"
+
+		#insert values
+		if type(v) == type({}):
+			mystr += "\n"+pre+dictToStr(v, level=level+1)
+		elif type(v) == type([]):
+			mystr += listToStr(v, level=level+1)
+		elif False and ' ' in v:
+			mystr += '"'+str(v)+'"'
+		else:
+			mystr += str(v)
+
+		#close dict key
+		if mystr[-1:] == "\n":
+			mystr += pre+"</"+str(k)+">\n"
+		else:
+			mystr += "</"+str(k)+">\n"
+	return mystr
+
+def listToStr(xmllist, level=0):
+	#setup line prefix -- tab level
+	pre = ""
+	for i in range(level):
+		pre += "\t"
+	if pre == "":
+		pre = " "
+
+	mystr = ""
+	for index,item in enumerate(xmllist):
+		if type(item) == type([]):
+			#list of lists
+			#mystr += "\n"+pre+"<list><level>"+str(level)+"</level><index>"+str(index+1)+"</index><value>"
+			mystr += "\n"+pre+"<list"+str(index)+">"
+			mystr += listToStr(item, level=level+1)
+			if mystr[-1:] == "\n":
+				mystr += pre
+			mystr += "</list"+str(index)+">"
+			#mystr += "</value></list>"
+		elif type(item) == type({}):
+			#list of dicts
+			#mystr += "\n"+pre+"<list><level>"+str(level)+"</level><index>"+str(index+1)+"</index><value>"
+			mystr += "\n"+pre+"<list"+str(index)+">"
+			mystr += dictToStr(item, level=level+1)
+			if mystr[-1:] == "\n":
+				mystr += pre
+			mystr += "</list"+str(index)+">"
+			#mystr += "</value></list>"
+		else:
+			mystr += str(item)+","
+
+	#new line for item lists
+	if mystr[-1:] == "\n":
+		mystr += pre
+	elif mystr[-1:] == ">":
+		mystr += "\n"
+
+	return mystr
+
+def writeDictToXml(xmldict, filename, title=None):
+	#compile XML
+	mystr = dictToStr(xmldict)
+
+	#insert title
+	if title is not None:
+		mystr = "<"+str(title)+">\n"+mystr+"</"+str(title)+">\n"
+	else:
+		mystr = "<apxml>\n"+mystr+"</apxml>\n"
+
+	#write to file
+	f = open(filename,"w")
+	#f = sys.stderr
+	f.write("<?xml version='1.0' encoding='us-ascii'?>\n")
+	f.write(mystr)
+	f.close()
+
+	#check if it worked
+	if os.path.isfile(filename):
+		return True
+	apDisplay.printWarning("Failed to write XML data to file "+filename)
+	return False
+
 def readOneXmlFile(file1):
 	"""
 	reads two XML files and creates a dictionary
 	"""
-	xmldict  = readConfig(file1)
+	xmldict  = readDictFromXml(file1)
 
 	fillMissingInfo(xmldict)
 	updateXmlDict(xmldict)
@@ -56,8 +198,8 @@ def readTwoXmlFiles(file1,file2):
 	"""
 	reads two XML files and creates a dictionary
 	"""
-	xmldict  = readConfig(file1)
-	xmldict2 = readConfig(file2)
+	xmldict  = readDictFromXml(file1)
+	xmldict2 = readDictFromXml(file2)
 	xmldict = overWriteDict(xmldict,xmldict2)
 
 	fillMissingInfo(xmldict)
@@ -181,7 +323,7 @@ def str2bool(string):
 	else:
 		return True
 
-def printHelp(xmldict):
+def printHelp(xmldict, exit=True):
 	"""
 	print out help info for a function with XML file
 	"""
@@ -221,7 +363,8 @@ def printHelp(xmldict):
 			if 'example' in xmldict[param] and xmldict[param]['example'] != None:
 				outstr += " (example: "+str(xmldict[param]['example'])+")"
 			print outstr
-	sys.exit(1)
+	if exit is True:
+		sys.exit(1)
 
 
 def fancyPrintDict(pdict):
