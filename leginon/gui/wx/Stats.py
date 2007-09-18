c4607e167e88779510d99c0f63778a1d55ed1e9a
Neil Voss <vossman>
2007-09-18 17:25:26 +0000

    no change

--- leginon/gui/wx/Stats.py
+++ leginon/gui/wx/Stats.py
@@ -4,10 +4,10 @@
 # see http://ami.scripps.edu/software/leginon-license
 #
 # $Source: /ami/sw/cvsroot/pyleginon/gui/wx/Stats.py,v $
-# $Revision: 1.10 $
+# $Revision: 1.11 $
 # $Name: not supported by cvs2svn $
-# $Date: 2007-03-05 21:27:49 $
-# $Author: pulokas $
+# $Date: 2007-09-18 17:25:26 $
+# $Author: vossman $
 # $State: Exp $
 # $Locker:  $
 
@@ -17,7 +17,7 @@ class Panel(wx.Panel):
 	nonestr = ''
 	def __init__(self, parent, id, **kwargs):
 		wx.Panel.__init__(self, parent, id, **kwargs)
-		self.SetBackgroundColour(wx.Colour(255, 255, 220))
+		self.SetBackgroundColour(wx.Colour(255, 255, 210))
 
 		self.sz = wx.GridBagSizer(0, 0)
 
@@ -25,13 +25,20 @@ class Panel(wx.Panel):
 		self.values = {}
 		for i, label in enumerate(self.order):
 			self.labels[label] = wx.StaticText(self, -1, label + ':')
-			self.values[label] = wx.StaticText(self, -1, self.nonestr,
-																				style=wx.ALIGN_RIGHT)
+			self.values[label] = wx.StaticText(self, -1, self.nonestr, style=wx.ALIGN_RIGHT)
+			#smaller font size
+			#sfont = self.labels[label].GetFont()
+			#sfont.SetPointSize(sfont.GetPointSize() - 2)
+			#self.labels[label].SetFont(sfont)
+			#self.values[label].SetFont(sfont)
+
+			#add widgets to frame
 			self.sz.Add(self.labels[label], (i, 0), (1, 1),
-									wx.ALIGN_CENTER_VERTICAL|wx.ALL, 3)
+				wx.ALIGN_CENTER_VERTICAL|wx.ALL, 3)
 			self.sz.Add(self.values[label], (i, 1), (1, 1),
-									wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_RIGHT|wx.ALL, 3)
+				wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_RIGHT|wx.ALL, 3)
 			self.sz.AddGrowableRow(i)
+
 		self.sz.AddGrowableCol(1)
 
 		self.SetSizerAndFit(self.sz)
