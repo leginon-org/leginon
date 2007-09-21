ff10a18dc755801150ebf1365b53c91d4d070284
pulokas <pulokas>
2007-09-21 23:08:06 +0000

    script to get models for a given instrument

---
+++ leginon/modelinst.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import leginondata
+import sys
+from sinedon import newdict
+
+host = sys.argv[1]
+i = leginondata.InstrumentData(hostname=host)
+s = leginondata.StageModelCalibrationData(tem=i)
+models = s.query()
+
+labels = newdict.OrderedDict()
+for model in models:
+	label = model['label']
+	labels[label] = None
+for label in labels.keys():
+	print label
