4b0c8c5a5069de1eb5635c2ebd79b7c1cdd62760
Gabriel Lander <glander>
2007-09-25 16:44:20 +0000

    - if you want to use the average of the ctfs, pass ctfavg=True to the getBestCtfValue functions

--- appion/lib/apCtf.py
+++ appion/lib/apCtf.py
@@ -274,7 +274,7 @@ def getBestDefocusForImage(imgdata, display=False):
 
 	return -ctfvalue['defocus1']
 
-def getBestCtfValueForImage(imgdata):
+def getBestCtfValueForImage(imgdata, ctfavg=False):
 	"""
 	takes an image and get the best ctfvalues for that image
 	"""
@@ -295,42 +295,15 @@ def getBestCtfValueForImage(imgdata):
 		conf2 = ctfvalue['confidence_d']
 		if conf1 > 0 and conf2 > 0:
 			conf = max(conf1,conf2)
-#			conf = math.sqrt(conf1*conf2)
+			if ctfavg=True:
+				conf = math.sqrt(conf1*conf2)
 			if conf > bestconf:
 				bestconf = conf
 				bestctfvalue = ctfvalue
 
 	return bestctfvalue, bestconf
 
-def getBestAvgCtfValueForImage(imgdata):
-	"""
-	takes an image and get the best ctfvalues for that image
-	"""
-	### get all ctf values
-	ctfq = appionData.ApCtfData()
-	ctfq['dbemdata|AcquisitionImageData|image'] = imgdata.dbid
-	ctfvalues = appiondb.query(ctfq)
-
-	### check if it has values
-	if ctfvalues is None:
-		return None, None
-
-	### find the best values
-	bestconf = 0.0
-	bestctfvalue = None
-	for ctfvalue in ctfvalues:
-		conf1 = ctfvalue['confidence']
-		conf2 = ctfvalue['confidence_d']
-		if conf1 > 0 and conf2 > 0:
-#			conf = max(conf1,conf2)
-			conf = math.sqrt(conf1*conf2)
-			if conf > bestconf:
-				bestconf = conf
-				bestctfvalue = ctfvalue
-
-	return bestctfvalue, bestconf
-
-def getBestCtfValueForImageREFLEGINON(imgdata):
+def getBestCtfValueForImageREFLEGINON(imgdata, ctfavg=False):
 	"""
 	takes an image and get the best ctfvalues for that image
 	"""
@@ -351,7 +324,8 @@ def getBestCtfValueForImageREFLEGINON(imgdata):
 		conf2 = ctfvalue['confidence_d']
 		if conf1 > 0 and conf2 > 0:
 			conf = max(conf1,conf2)
-			#conf = math.sqrt(conf1*conf2)
+			if ctfavg=True:
+				conf = math.sqrt(conf1*conf2)
 			if conf > bestconf:
 				bestconf = conf
 				bestctfvalue = ctfvalue
