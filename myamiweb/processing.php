35294de65f9e5070e4ec767bf57445bb3cf95e95
Neil Voss <vossman>
2007-10-01 21:15:16 +0000

    tabs

--- myamiweb/processing.php
+++ myamiweb/processing.php
@@ -133,10 +133,10 @@ if ($sessionId) {
 
   // --- Get Reconstruction Data
   if ($stackruns>0) {
-          foreach ($stackIds as $stackid) {
-	          $reconIds = $particle->getReconIds($stackid['stackid']);
-		  if ($reconIds) {
-		          $reconruns+=count($reconIds);
+		foreach ($stackIds as $stackid) {
+			$reconIds = $particle->getReconIds($stackid['stackid']);
+			if ($reconIds) {
+				$reconruns+=count($reconIds);
 		  }
 	  }
   }
