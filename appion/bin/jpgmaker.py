bc0f858d3ad23f1bb9a027a42841f7ee769645a3
Anchi Cheng <acheng>
2007-10-03 02:00:38 +0000

    clip

--- appion/bin/jpgmaker.py
+++ appion/bin/jpgmaker.py
@@ -9,6 +9,20 @@ import apImage
 import NumericImage
 
 class MrcToJpgLoop(appionLoop.AppionLoop):
+	def specialDefaultParams(self):
+		self.params['min']= 0
+		self.params['max']= 65000
+	
+	def specialParseParams(self, args):
+		for arg in args:
+			elements=arg.split('=')
+			elements[0] = elements[0].lower()
+			if (elements[0]=='min'):
+				self.params['min']=int(elements[1])
+			elif (elements[0]=='max'):
+				self.params['max']=int(elements[1])
+
+
 	def processImage(self, imgdata):
 		quality = 80
 		image = imgdata['image']
@@ -16,7 +30,7 @@ class MrcToJpgLoop(appionLoop.AppionLoop):
 		self.params['bin'] = shape[0]/512
 		binnedimage = apImage.binImg(image, bin=self.params['bin'])
 		outfile = os.path.join(self.params['rundir'],imgdata['filename']+".jpg")
-		num_img = NumericImage.NumericImage(binnedimage)
+		num_img = NumericImage.NumericImage(binnedimage,(self.params['min'],self.params['max']))
 
 		num_img.jpeg(outfile,quality)
 
