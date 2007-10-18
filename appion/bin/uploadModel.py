f558712c8d93135202de2c301956f51fe1663e9e
Neil Voss <vossman>
2007-10-18 18:58:54 +0000

    detailed error

--- appion/bin/uploadModel.py
+++ appion/bin/uploadModel.py
@@ -28,7 +28,8 @@ if __name__ == '__main__':
 		apDisplay.printError("enter the pixel size of the model")
 	if params['session'] is None:
 		apDisplay.printError("enter a session ID")
-	if params['sym'] is None:
+	if params['sym'] is None or not re.match("[0-9]+", params['sym']):
+		apUpload.printSymmetries()
 		apDisplay.printError("enter a symmetry ID")
 	if params['description'] is None:
 		apDisplay.printError("enter a description of the initial model")
