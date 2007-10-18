eb37bd3ad672fde06e0baf4a9d519c9abccd63e1
Anchi Cheng <acheng>
2007-10-18 17:45:25 +0000

    make jpgs path

--- myamiweb/3wviewer.php
+++ myamiweb/3wviewer.php
@@ -70,7 +70,7 @@ viewer_header('image viewer', $javascript, 'initviewer()');
 ?>
 <a class="header" target="summary" href="summary.php?expId=<?php echo $sessionId; ?>">[summary]</A>
 <a class="header" target="processing" href="processing/processing.php?expId=<?php echo $sessionId; ?>">[processing]</A>
-<a class="header" target="make jpgs" href="runJpgMaker.php?expId=<?php echo $sessionId; ?>">[make jpgs]</A>
+<a class="header" target="make jpgs" href="processing/runJpgMaker.php?expId=<?php echo $sessionId; ?>">[make jpgs]</A>
 <?php
 $viewer->display();
 viewer_footer();
