c333401fea1b82e45283ff12d69210453bc970cd
Neil Voss <vossman>
2007-10-18 18:08:40 +0000

    new file for EMAN stuff

---
+++ appion/lib/apEMAN.py
@@ -0,0 +1,18 @@
+
+import apDisplay
+import EMAN
+
+def executeEmanCmd(emancmd, verbose=False):
+	sys.stderr.write("EMAN: "+emancmd+"\n")
+	try:
+		if verbose is False:
+			os.popen(emancmd)
+		else:
+			os.system(emancmd)
+	except:
+		apDisplay.printError("could not run eman command: "+emancmd)
+
+
+def getNumParticlesInStack(stackname):
+	numparticles = EMAN.fileCount(stackname)[0]
+	return numparticles
