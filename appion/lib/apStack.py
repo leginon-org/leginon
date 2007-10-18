7885daaa0e9121fbcb4d65a3574ad3e7583a27e2
Neil Voss <vossman>
2007-10-18 17:17:12 +0000

    new function

--- appion/lib/apStack.py
+++ appion/lib/apStack.py
@@ -1,46 +1,61 @@
 # stack functions
 
 import os, sys
+import time
 import apDB
 import apDisplay
 import appionData
 
-apdb=apDB.apdb
+appiondb=apDB.apdb
+
+def makeNewStack(lstfile,newstackname):
+	#first remove old imagic stack
+	if os.path.exists(newstackname):
+		apDisplay.printWarning("removing old stack: "+newstackname)
+		time.sleep(2)
+		prefix=newstackname.split('.')[0]
+		os.remove(prefix+'.hed')
+		os.remove(prefix+'.img')
+	command=('proc2d %s %s' % (lstfile, newstackname))
+	os.system(command)
+	return
 
 def getStackParticlesFromId(stackid):
 	print "Getting particles for stack", stackid
-	stackdata=apdb.direct_query(appionData.ApStackData, stackid)
+	stackdata=appiondb.direct_query(appionData.ApStackData, stackid)
 	stackq=appionData.ApStackParticlesData()
 	stackq['stack']=stackdata
-	stackparticledata=apdb.query(stackq)
+	stackparticledata=appiondb.query(stackq)
 	return(stackparticledata)
 
 def getOneParticleFromStackId(stackid):
 	print "Getting particles for stack", stackid
-	stackdata=apdb.direct_query(appionData.ApStackData, stackid)
+	stackdata=appiondb.direct_query(appionData.ApStackData, stackid)
 	stackq=appionData.ApStackParticlesData()
 	stackq['stack'] = stackdata
-	stackparticledata=apdb.query(stackq, results=1)
-	return stackparticledata
+	stackparticledata=appiondb.query(stackq, results=1)
+	return stackparticledata[0]
 
 def getOnlyStackData(stackid):
 	print "Getting stack data for stack", stackid
-	stackdata=apdb.direct_query(appionData.ApStackData,stackid)
+	stackdata=appiondb.direct_query(appionData.ApStackData,stackid)
 	return(stackdata)
 
 def getStackParticle(stackid, particlenumber):
-	stackdata=apdb.direct_query(appionData.ApStackData, stackid)
+	stackdata=appiondb.direct_query(appionData.ApStackData, stackid)
 	stackparticleq=appionData.ApStackParticlesData()
 	stackparticleq['stack']=stackdata
 	stackparticleq['particleNumber']=particlenumber
-	stackparticledata=apdb.query(stackparticleq)
+	stackparticledata=appiondb.query(stackparticleq)
 	if len(stackparticledata) > 1:
 		apDisplay.printError("There's a problem with this stack. More than one particle with the same number.")
 	return(stackparticledata[0])
 
 def getRunsInStack(stackid):
-	stackdata=apdb.direct_query(appionData.ApStackData, stackid)
+	stackdata=appiondb.direct_query(appionData.ApStackData, stackid)
 	runsinstackq=appionData.ApRunsInStackData()
 	runsinstackq['stack']=stackdata
-	runsinstackdata=apdb.query(runsinstackq)
+	runsinstackdata=appiondb.query(runsinstackq)
 	return(runsinstackdata)
+
+
