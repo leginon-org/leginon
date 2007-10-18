05eb51db43d068a065a75cc7070cd841190bef2a
Neil Voss <vossman>
2007-10-18 17:16:52 +0000

    print old stack info

--- appion/bin/uploadTemplate.py
+++ appion/bin/uploadTemplate.py
@@ -83,6 +83,9 @@ if __name__ == '__main__':
 		apDisplay.printMsg("Using stack to make template")
 		#get stack data (path, file)
 		stackdata = apStack.getOnlyStackData(params['stackid'])
+		sys.stderr.write("Old stack info: ")
+		apDisplay.printColor("'"+stackdata['description']+"'","cyan")
+		stackpath = os.path.join(stackdata['path']['path'], stackdata['name'])
 		#run proc2d with params['stackimgnum']
 		#Add file names to params['templatelist']
 	elif params['norefid'] is not None:
