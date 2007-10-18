fd54483d3b79c6a0efb900e174369136e6302b6e
Neil Voss <vossman>
2007-10-18 22:53:33 +0000

    multi-reference alignment

--- myamiweb/processing/refbasedali.php
+++ myamiweb/processing/refbasedali.php
@@ -20,7 +20,7 @@ if ($_POST['process']) {
 }
 
  // Create the form page
-elseif ($_POST['template']) {
+elseif ($_POST['templates']) {
   createAlignmentForm();
 }
 
@@ -30,85 +30,96 @@ else {
 }
 
 function createTemplateForm() {
-  // check if coming directly from a session
-  $expId = $_GET[expId];
-  $formAction=$_SERVER['PHP_SELF'];	
-  
-  // retrieve template info from database for this project
-  if ($expId){
-    $projectId=getProjectFromExpId($expId);
-    $formAction=$_SERVER['PHP_SELF']."?expId=$expId";
-  }
+	// check if coming directly from a session
+	$expId = $_GET[expId];
+	$formAction=$_SERVER['PHP_SELF'];	
 
-  // if user wants to use template from another project
-  if($_POST['projectId']) $projectId =$_POST[projectId];
-  
-  $projects=getProjectList();
+	// retrieve template info from database for this project
+	if ($expId){
+	$projectId=getProjectFromExpId($expId);
+		$formAction=$_SERVER['PHP_SELF']."?expId=$expId";
+	}
 
-  if (is_numeric($projectId)) {
-    $particle=new particleData;
-    $templateData=$particle->getTemplatesFromProject($projectId);
-  }
+	// if user wants to use templates from another project
+	if($_POST['projectId']) $projectId =$_POST[projectId];
 
-  // extract template info
-  if ($templateData) {
-    $i=1;
-    $templatetable="<TABLE BORDER='1' CELLPADDING='5' WIDTH='600'>\n";
-    $templatetable.="<style type='text/css'><!-- input { font-size: 14px; } --></style>";
-    $numtemplates=count($templateData);
+	$projects=getProjectList();
 
-    foreach($templateData as $templateinfo) { 
-      if (is_array($templateinfo)) {
-	$filename=$templateinfo[path] ."/".$templateinfo[templatename];
-	// create the image template table
-	$templatetable.="<TR><TD>\n";
-	$templatetable.="<IMG SRC='loadimg.php?filename=$filename&rescale=True' WIDTH='200'></TD>\n";
-	$templatetable.="<TD>\n";
-	$templatetable.="<INPUT TYPE='radio' NAME='templateid' VALUE='$templateinfo[DEF_id]'>\n";
-	$templatetable.="<B>Use This Template</B><BR>\n";
-	$templatetable.="<P>\n";
-	$templatetable.="<TABLE BORDER='0'>\n";
-	$templatetable.="<TR><TD><B>Template ID:</B></TD><TD>$templateinfo[DEF_id]</TD></TR>\n";
-	$templatetable.="<TR><TD><B>Diameter:</B></TD><TD>$templateinfo[diam]</TD></TR>\n";
-	$templatetable.="<TR><TD><B>Pixel Size:</B></TD><TD>$templateinfo[apix]</TD></TR>\n";
-	$templatetable.="</TABLE>\n";
-	$templatetable.="<B>Description:</B><BR>$templateinfo[description]\n";
-	$templatetable.="</TD></TR>\n";
-	$i++;
-      }
-    }
-    $templatetable.="</TABLE>\n";
-  }
-  $javafunctions="<script src='../js/viewer.js'></script>\n";
-  writeTop("Template Correlator Launcher","Automated Particle Selection with Template Correlator",$javafunctions);
-  echo"
+	if (is_numeric($projectId)) {
+		$particle=new particleData;
+		$templateData=$particle->getTemplatesFromProject($projectId);
+	}
+
+	// extract template info
+	if ($templateData) {
+		$i=1;
+		$templatetable="<TABLE CLASS='tableborder' BORDER='1' CELLPADDING='5' WIDTH='600'>\n";
+		$templatetable.="<style type='text/css'><!-- input { font-size: 14px; } --></style>";
+		$numtemplates=count($templateData);
+
+		foreach($templateData as $templateinfo) { 
+			if (is_array($templateinfo)) {
+				$filename = $templateinfo[path] ."/".$templateinfo[templatename];
+				$checkboxname='template'.$i;
+				// create the image template table
+				$templatetable.="<TR><TD>\n";
+				$templatetable.="<IMG SRC='loadimg.php?filename=$filename&rescale=True' WIDTH='200'></TD>\n";
+				$templatetable.="<TD>\n";
+				$templatetable.="<INPUT TYPE='hidden' NAME='templateId".$i."' VALUE='$templateinfo[DEF_id]'>\n";
+				$templatetable.="<INPUT TYPE='hidden' NAME='diam' VALUE='$templateinfo[diam]'>\n";
+				$templatetable.="<INPUT TYPE='checkbox' NAME='$checkboxname'>\n";
+				$templatetable.="<B>Use This Template</B><BR/>\n";
+				$templatetable.="<B>Template ID:</B>  $templateinfo[DEF_id]<BR/>\n";
+				$templatetable.="<B>Diameter:</B>  $templateinfo[diam]<BR/>\n";
+				$templatetable.="<B>Pixel Size:</B>  $templateinfo[apix]<BR/>\n";
+				$templatetable.="<B>File:</B><BR/>";
+				$templatetable.="<TABLE CLASS='tableborder' BORDER='1'><TR><TD CLASS='tablebg'>\n";
+				$templatetable.=$filename;
+				$templatetable.="</TD></TR></TABLE>\n";
+				$templatetable.="<B>Description:</B><BR/>";
+				$templatetable.="<TABLE CLASS='tableborder' BORDER='1'><TR><TD CLASS='tablebg'>\n";
+				$templatetable.=$templateinfo[description];
+				$templatetable.="</TD></TR></TABLE>\n";
+				$templatetable.="</TD></TR>\n";
+
+				$i++;
+			}
+		}
+		$templatetable.="</TABLE>\n";
+	}
+
+	writeTop("Template Correlator Launcher","Automated Particle Selection with Template Correlator","");
+	echo"
   <FORM NAME='viewerform' method='POST' ACTION='$formAction'>
   <B>Select Project:</B><BR>
   <SELECT NAME='projectId' onchange='newexp()'>\n";
 
-  foreach ($projects as $k=>$project) {
-    $sel = ($project['id']==$projectId) ? "selected" : '';
-    echo "<option value='".$project['id']."' ".$sel.">".$project['name']."</option>\n";
-  }
-  echo"
+	foreach ($projects as $k=>$project) {
+		$sel = ($project['id']==$projectId) ? "selected" : '';
+		echo "<option value='".$project['id']."' ".$sel.">".$project['name']."</option>\n";
+	}
+	echo"
   </select>
   <P>\n";
-  if ($templatetable) {
-    echo"
+	if ($templatetable) {
+		echo"
     <CENTER>
-    <INPUT TYPE='submit' NAME='template' value='Use This Template'>
+    <INPUT TYPE='submit' NAME='templates' value='Use These Templates'>
     </CENTER>\n
     $templatetable
     <CENTER>
-    <INPUT TYPE='submit' NAME='template' value='Use This Template'>
+    <INPUT TYPE='hidden' NAME='numtemplates' value='$numtemplates'>
+    <INPUT TYPE='submit' NAME='templates' value='Use These Templates'>
     </CENTER>\n";
-  }
-  else echo "<B>Project does not contain any templates.</B>\n";
-  echo"</FORM>\n";
+	}
+	else echo "<B>Project does not contain any templates.</B>\n";
+	echo"</FORM>\n";
 }
 
 function createAlignmentForm($extra=false, $title='refBasedAlignment.py Launcher', $heading='Perform a reference-based Alignment') {
   // check if coming directly from a session
+	//echo print_r($_POST);
+
   $expId=$_GET['expId'];
   if ($expId){
     $sessionId=$expId;
@@ -129,22 +140,12 @@ function createAlignmentForm($extra=false, $title='refBasedAlignment.py Launcher
   $refaliIds = $particle->getRefAliIds($sessionId);
   $refaliruns=count($refaliIds);
 
-  // --- find hosts to run refBasedAlignment.py
-  $hosts=getHosts();
-
-  // --- get list of users
-  $users[]=glander;
-  
-  $javascript="<script src='../js/viewer.js'></script>";
-
-  writeTop($title,$heading,$javascript);
+  writeTop($title,$heading,"");
   // write out errors, if any came up:
   if ($extra) {
     echo "<FONT COLOR='RED'>$extra</FONT>\n<HR>\n";
   }
-  
-  echo"
-       <FORM NAME='viewerform' method='POST' ACTION='$formAction'>\n";
+  echo"<FORM NAME='viewerform' method='POST' ACTION='$formAction'>\n";
   $sessiondata=displayExperimentForm($projectId,$sessionId,$expId);
   $sessioninfo=$sessiondata['info'];
   if (!empty($sessioninfo)) {
@@ -157,8 +158,8 @@ function createAlignmentForm($extra=false, $title='refBasedAlignment.py Launcher
   // Set any existing parameters in form
   $runidval = ($_POST['runid']) ? $_POST['runid'] : 'refali'.($refaliruns+1);
   $rundescrval = $_POST['description'];
-  $stackidval = $_POST['stackid'];
-  $imaskdiam = $_POST['imaskdiam'];
+  $stackidval =$_POST['stackid'];
+  $imaskdiam = $_POST['imaskdiam'] ? $_POST['imaskdiam'] : 5;
   $lp = $_POST['lp'];
   $csym = $_POST['csym'];
   $sessionpathval = ($_POST['outdir']) ? $_POST['outdir'] : $sessionpath;
@@ -167,10 +168,41 @@ function createAlignmentForm($extra=false, $title='refBasedAlignment.py Launcher
   $numpart = ($_POST['numpart']) ? $_POST['numpart'] : 3000;
   $iters = ($_POST['iters']) ? $_POST['iters'] : 5;
   $lp = ($_POST['lp']) ? $_POST['lp'] : 10;
-  $xysearch = ($_POST['xysearch']) ? $_POST['xysearch'] : ceil($templateinfo['diam']*0.1);
+	$diam = $_POST['diam'] ? $_POST['diam'] : 160;
+  $xysearch = ($_POST['xysearch']) ? $_POST['xysearch'] : ceil($diam*0.1);
   $csym = 1;
-  $maskdiam = ($_POST['maskdiam']) ? $_POST['maskdiam'] : $templateinfo['diam'];
-  $imaskdiam = ($_POST['imaskdiam']) ? $_POST['imaskdiam'] : ceil($templateinfo['diam']/16-2);
+  $maskdiam = ($_POST['maskdiam']) ? $_POST['maskdiam'] : $diam;
+  $imaskdiam = ($_POST['imaskdiam']) ? $_POST['imaskdiam'] : ceil($diam/16-2);
+
+
+	$templateCheck='';
+	$templateTable.="<TABLE><TR>\n";
+	for ($i=1; $i<=$_POST['numtemplates']; $i++) {
+		$templateimg = "template".$i;
+		if ($_POST[$templateimg]){
+			$templateTable.="<TD VALIGN='TOP'><TABLE CLASS='tableborder'>\n";
+			$templateIdName="templateId".$i;
+			$templateId=$_POST[$templateIdName];
+			$templateList.=$i.":".$templateId.",";
+			$templateinfo=$particle->getTemplatesFromId($templateId);
+			$filename=$templateinfo[path]."/".$templateinfo['templatename'];
+			$templateTable.="<TR><TD VALIGN='TOP'><IMG SRC='loadimg.php?filename=$filename&rescale=True' WIDTH='200'></TD></TR>\n";
+			$templateTable.="<TR><TD VALIGN='TOP'>".$templateinfo['templatename']."</TD></TR>\n";
+			$templateForm.="<INPUT TYPE='hidden' NAME='$templateIdName' VALUE='$templateId'>\n";
+			$templateForm.="<INPUT TYPE='hidden' NAME='$templateimg' VALUE='$templateId'>\n";
+			$templateTable.="</TABLE></TD>\n";
+		}
+	}
+	$templateTable.="</TR></TABLE>\n";
+	if ($templateList) { $templateList=substr($templateList,0,-1);}
+	else {
+		echo "<BR/><B>no templates chosen, go back and choose templates</B>\n";
+		exit;
+	}
+	echo "<INPUT TYPE='hidden' NAME='templateList' VALUE='$templateList'>\n";
+	echo "<INPUT TYPE='hidden' NAME='templates' VALUE='continue'>\n";
+	echo "<INPUT TYPE='hidden' NAME='numtemplates' VALUE='$numtemplates'>\n";
+
   echo"
 	<P>
 	<TABLE BORDER=0 CLASS=tableborder>
@@ -179,7 +211,7 @@ function createAlignmentForm($extra=false, $title='refBasedAlignment.py Launcher
 		<TABLE CELLPADDING='10' BORDER='0'>
 		<TR>
 			<TD VALIGN='TOP'>
-			<A HREF=\"javascript:infopopup('runid')\"><B>Alignment Run Name:</B></A>
+			<B>Alignment Run Name:</B>
 			<INPUT TYPE='text' NAME='runid' VALUE='$runidval'>
 			</TD>
 		</TR>\n";
@@ -215,7 +247,7 @@ function createAlignmentForm($extra=false, $title='refBasedAlignment.py Launcher
 			$totprtls=commafy($particle->getNumStackParticles($stack[stackid]));
 			echo "<OPTION VALUE='$stack[stackid]'";
 			// select previously set prtl on resubmit
-			if ($stackidval==$stackid) echo " SELECTED";
+			if ($stackidval == $stackid) echo " SELECTED";
 			echo">$runname ($totprtls prtls)</OPTION>\n";
 		}
 		echo "</SELECT>\n";
@@ -237,11 +269,11 @@ function createAlignmentForm($extra=false, $title='refBasedAlignment.py Launcher
 		<TD VALIGN='TOP'>
 		<B>Particle Params:</B></A><BR>
 		<INPUT TYPE='text' NAME='maskdiam' SIZE='5' VALUE='$maskdiam'>
-		Mask Diameter (in Angstroms)<BR>
+		Mask Diameter <FONT SIZE='-1'>(in &Aring;ngstroms)</FONT><BR>
 		<INPUT TYPE='text' NAME='imaskdiam' SIZE='5' VALUE='$imaskdiam'>
-		Inner Mask Diameter (in Angstroms)<BR>
+		Inner Mask Diameter <FONT SIZE='-1'>(in &Aring;ngstroms)</FONT><BR>
 		<INPUT TYPE='text' NAME='lp' SIZE='5' VALUE='$lp'>
-		Low Pass Filter (in Angstroms)<BR>
+		Low Pass Filter <FONT SIZE='-1'>(in &Aring;ngstroms)</FONT><BR>
 		</TD>
 	</TR>
 	<TR>
@@ -250,7 +282,7 @@ function createAlignmentForm($extra=false, $title='refBasedAlignment.py Launcher
 		<INPUT TYPE='text' NAME='iters' VALUE='$iters' SIZE='4'>
 		Iterations<BR>
 		<INPUT TYPE='text' NAME='xysearch' VALUE='$xysearch' SIZE='4'>
-		Search range from center (in Angstroms)<BR>
+		Search range from center <FONT SIZE='-1'>(in &Aring;ngstroms)</FONT><BR>
 		<INPUT TYPE='text' NAME='csym' VALUE='$csym' SIZE='4'>
 		C-symmetry to apply<BR>
 		<INPUT TYPE='text' NAME='numpart' VALUE='$numpart' SIZE='4'>
@@ -279,13 +311,18 @@ function createAlignmentForm($extra=false, $title='refBasedAlignment.py Launcher
 	  </select><BR>*/
 	  echo"
           <INPUT TYPE='hidden' NAME='refid' VALUE='$templateid'>
-	  <input type='submit' name='process' value='Align'><BR>
-	  <FONT COLOR='RED'>Submission will NOT start alignment, only output a command that you can copy and paste into a unix shell</FONT>
+	  <input type='submit' name='process' value='Show multi-reference alignment command'><BR>
+	  <FONT COLOR='RED'>Submission will NOT start alignment<BR/>
+		only output a command that you can copy and paste into a unix shell</FONT>
 	  </TD>
 	</TR>
 	</TABLE>
 	</FORM>
 	</CENTER>\n";
+
+	echo "$templateForm\n";
+	echo "$templateTable\n";
+
 	writeBottom();
 	exit;
 }
@@ -331,9 +368,10 @@ function runAlignment() {
 //	$command.="source /ami/sw/share/python/usepython.csh common32;";
 //	$command.="source /home/$user/pyappion/useappion.csh;";
 	$command.="refBasedAlignment.py ";
+	$command .= templateCommand();
 	$command.="runid=$runid ";
 	$command.="stackid=$stackid ";
-	$command.="refid=$refid ";
+	//$command.="refid=$refid ";
 	$command.="iter=$iters ";
 	if ($maskdiam) $command.="maskdiam=$maskdiam ";
 	if ($imaskdiam) $command.="imask=$imaskdiam ";
@@ -372,4 +410,28 @@ function runAlignment() {
 	echo"	</TABLE>\n";
 	writeBottom();
 }
+
+/*
+**
+**
+** GENERATE COMMANDLINE INFO FOR TEMPLATES
+**
+**
+*/
+
+function templateCommand () {
+	$command = "";
+	// get the list of templates
+	$templateList=$_POST['templateList'];
+	$templates=split(",", $templateList);
+	foreach ($templates as $tmplt) {
+		list($tmpltNum,$tmpltId)=split(":",$tmplt);
+		$templateIds.="$tmpltId,";
+	}
+	$templateIds=substr($templateIds,0,-1);
+
+	$command.="refids=$templateIds ";
+	return $command;
+}
+
 ?>
