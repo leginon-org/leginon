b994c2819ba23fa6ec3965c589d6c8ecee7aed82
Neil Voss <vossman>
2007-10-18 21:04:01 +0000

    expand functions

--- appion/lib/apStack.py
+++ appion/lib/apStack.py
@@ -1,12 +1,13 @@
 # stack functions
 
-import os, sys
+import os, sys, re
 import time
 import apDB
+import apEMAN
 import apDisplay
 import appionData
 
-appiondb=apDB.apdb
+appiondb = apDB.apdb
 
 def makeNewStack(oldstack, newstack, listfile):
 	if not os.path.isfile(oldstack):
@@ -18,10 +19,13 @@ def makeNewStack(oldstack, newstack, listfile):
 		#prefix=newstack.split('.')[0]
 		#os.remove(prefix+'.hed')
 		#os.remove(prefix+'.img')
+	apDisplay.printMsg("creating a newstack ("+newstack+
+		")\n\tfrom the oldstack ("+oldstack+")")
 	command=("proc2d "+oldstack+" "+newstack+" list="+listfile)
-	os.system(command)
+	apEMAN.executeEmanCmd(command, verbose=True)
 	return
 
+#--------
 def getStackParticlesFromId(stackid):
 	print "Getting particles for stack", stackid
 	stackdata=appiondb.direct_query(appionData.ApStackData, stackid)
@@ -30,6 +34,7 @@ def getStackParticlesFromId(stackid):
 	stackparticledata=appiondb.query(stackq)
 	return(stackparticledata)
 
+#--------
 def getOneParticleFromStackId(stackid):
 	print "Getting particles for stack", stackid
 	stackdata=appiondb.direct_query(appionData.ApStackData, stackid)
@@ -38,6 +43,7 @@ def getOneParticleFromStackId(stackid):
 	stackparticledata=appiondb.query(stackq, results=1)
 	return stackparticledata[0]
 
+#--------
 def getOnlyStackData(stackid):
 	apDisplay.printMsg("Getting stack data for stackid="+str(stackid))
 	stackdata=appiondb.direct_query(appionData.ApStackData,stackid)
@@ -48,21 +54,103 @@ def getOnlyStackData(stackid):
 	apDisplay.printColor("'"+stackdata['description']+"'","cyan")
 	return stackdata
 
+#--------
 def getStackParticle(stackid, particlenumber):
-	stackdata=appiondb.direct_query(appionData.ApStackData, stackid)
-	stackparticleq=appionData.ApStackParticlesData()
-	stackparticleq['stack']=stackdata
-	stackparticleq['particleNumber']=particlenumber
-	stackparticledata=appiondb.query(stackparticleq)
+	stackparticleq = appionData.ApStackParticlesData()
+	stackparticleq['stack'] = appiondb.direct_query(appionData.ApStackData, stackid)
+	stackparticleq['particleNumber'] = particlenumber
+	stackparticledata = appiondb.query(stackparticleq)
 	if len(stackparticledata) > 1:
 		apDisplay.printError("There's a problem with this stack. More than one particle with the same number.")
-	return(stackparticledata[0])
+	return stackparticledata[0]
 
+#--------
 def getRunsInStack(stackid):
-	stackdata=appiondb.direct_query(appionData.ApStackData, stackid)
-	runsinstackq=appionData.ApRunsInStackData()
-	runsinstackq['stack']=stackdata
-	runsinstackdata=appiondb.query(runsinstackq)
-	return(runsinstackdata)
+	stackdata = appiondb.direct_query(appionData.ApStackData, stackid)
+	runsinstackq = appionData.ApRunsInStackData()
+	runsinstackq['stack'] = stackdata
+	runsinstackdata = appiondb.query(runsinstackq)
+	return runsinstackdata
+
+#--------
+def getListFileParticle(line, linenum):
+	sline = line.strip()
+	if sline == "":
+		apDisplay.printWarning("Blank line "+str(linenum)+" in listfile")
+		return None
+	words = sline.split()
+	if len(words) < 1:
+		apDisplay.printWarning("Empty line "+str(linenum)+" in listfile")
+		return None
+	if not re.match("[0-9]+", words[0]):
+		apDisplay.printWarning("Line "+str(linenum)+" in listfile is not int: "+str(words[0]))
+		return None
+
+	#### Adding 1 to particle #: EMAN stacks start at 0 and appion starts at 1 ###
+	particlenum = int(words[0]) + 1
+
+	return particlenum
+
+#--------
+def commitSubStack(params):
+	"""
+	commit a substack to database
+	
+	required params:
+		stackid
+		description
+		commit
+		rundir
+		keepfile
+	"""
+
+	oldstackdata = getOnlyStackData(params['stackid'])
+
+	#create new stack data
+	stackq = appionData.ApStackData()
+	stackq['path'] = appionData.ApPathData(path=os.path.abspath(params['rundir']))
+	stackq['name'] = oldstackdata['name']
+	stackdata=appiondb.query(stackq, results=1)
+	if stackdata:
+		apDisplay.printWarning("A stack with these parameters already exists")
+		return
+	stackq['description'] = params['description']
+
+	partinserted = 0
+	#Insert particles
+	listfile = params['keepfile']
+	newparticlenum = 1
+	f=open(listfile,'r')
+	for line in f:
+		particlenum = getListFileParticle(line, newparticlenum)
+		if particlenum is None:
+			continue
+
+		# Find corresponding particle in old stack
+		oldstackpartdata = getStackParticle(params['stackid'], particlenum)
+
+		# Insert particle
+		newstackq = appionData.ApStackParticlesData()
+		newstackq['particleNumber'] = newparticlenum
+		newstackq['stack'] = stackq
+		newstackq['stackRun'] = oldstackpartdata['stackRun']
+		newstackq['particle'] = oldstackpartdata['particle']
+		if params['commit'] is True:
+			appiondb.insert(newstackq)
+		newparticlenum += 1
+	f.close()
+
+	if newparticlenum == 0:
+		apDisplay.printError("No particles were inserted for the stack")
+
+	apDisplay.printMsg("Inserted "+str(newparticlenum-1)+" stack particles into the database")
+	
+	runsinstack = getRunsInStack(params['stackid'])
+	for run in runsinstack:
+		newrunsq = appionData.ApRunsInStackData()
+		newrunsq['stack'] = stackq
+		newrunsq['stackRun'] = run['stackRun']
+		if params['commit'] is True:
+			appiondb.insert(newrunsq)
 
 
