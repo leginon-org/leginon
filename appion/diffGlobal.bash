5b504325bf1fedb105a238a268c75d0a6e8ff8ce
Neil Voss <vossman>
2007-10-18 00:46:30 +0000

    minor

--- appion/diffGlobal.bash
+++ appion/diffGlobal.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
  
 ./clean.sh
-for i in `find . -type f | egrep -v "(CVS|old|tilting|ctftest)"`
+for i in `find . -type f | egrep -v "(CVS|old|tilting|ctftest|data)"`
 do
 	if [ -f /ami/sw/packages/pyappion/$i ]
 	then
