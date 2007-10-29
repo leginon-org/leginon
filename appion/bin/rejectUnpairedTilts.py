7ad29f166efe58a9c21b0dd68073071ea0f8d8d3
Neil Voss <vossman>
2007-10-29 20:31:17 +0000

    sync images as well

--- appion/bin/rejectUnpairedTilts.py
+++ appion/bin/rejectUnpairedTilts.py
@@ -5,6 +5,7 @@ import sys
 import time
 import appionLoop
 import apDatabase
+from apTilt import apTiltPair
 import apDisplay
 
 ##################################
@@ -17,17 +18,35 @@ class rejectUnpairedTilts(appionLoop.AppionLoop):
 	##### START PRE-DEFINED APPION LOOP FUNCTIONS #####
 	#####################################################
 
+	def specialDefaultParams(self):
+		"""
+		put in any additional default parameters
+		"""
+		self.params['background'] = True
+		return
+
 	def processImage(self, imgdata):
-		time.sleep(0.2)
+		time.sleep(0.1)
 		return
 
 	def commitToDatabase(self, imgdata):
 		"""
 		Uses the appionLoop commit
 		"""
+		imgassess = apDatabase.getImgAssessmentStatus(imgdata)
 		tiltdata = apTiltPair.getTiltPair(imgdata)
 		if tiltdata is None:
-			apDisplay.printColor("rejecting image: "+apDisplay.short(imgdata['filename']), "magenta")
+			if imgassess is not False:
+				apDisplay.printColor("rejecting unpaired image: "+apDisplay.short(imgdata['filename']), "magenta")
+				apDatabase.insertImgAssessmentStatus(imgdata, self.params['runid'], False)
+			return
+
+		tiltassess = apDatabase.getImgAssessmentStatus(tiltdata)
+		if imgassess is False and tiltassess is not False:
+			apDisplay.printColor("rejecting bad tilt image: "+apDisplay.short(tiltdata['filename']), "magenta")
+			apDatabase.insertImgAssessmentStatus(tiltdata, self.params['runid'], False)
+		if tiltassess is False and imgassess is not False:
+			apDisplay.printColor("rejecting bad tilt image: "+apDisplay.short(imgdata['filename']), "magenta")
 			apDatabase.insertImgAssessmentStatus(imgdata, self.params['runid'], False)
 		return
 
