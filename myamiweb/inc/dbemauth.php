1571097ee35cdd0021eb07fb74ce49b6c90299c6
dfellman <dfellman>
2007-11-01 23:18:36 +0000

    *** empty log message ***

--- myamiweb/inc/dbemauth.php
+++ myamiweb/inc/dbemauth.php
@@ -1,12 +1,12 @@
 <?php
 require('inc/auth.php');
 $dbemauth = new authlib();
-$dbemauth->server="";
-$dbemauth->db_user="";
+$dbemauth->server="cronus4";
+$dbemauth->db_user="usr_project";
 $dbemauth->db_pass="";
-$dbemauth->database="";
+$dbemauth->database="project";
 $dbemauth->secret="8ca79c52f2eb411cfb1260b04bd8b605";
 $dbemauth->authcook="DBEMAUTH";
-$dbemauth->server_url = "/";
-$dbemauth->logout_url = "/";
+$dbemauth->server_url = "/dbemd";
+$dbemauth->logout_url = "/dbemd";
 ?>
