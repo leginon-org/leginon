bbff0534fa82ea697990f207c4c54410ba08af24
Anchi Cheng <acheng>
2007-11-14 00:02:28 +0000

    no import EMAN

--- appion/lib/apEMAN.py
+++ appion/lib/apEMAN.py
@@ -2,11 +2,11 @@
 import apDisplay
 import os
 import sys
-try:
-	import EMAN
-except ImportError:
-	apDisplay.printWarning("EMAN module did not get imported")
-	pass
+#try:
+#	import EMAN
+#except ImportError:
+#	apDisplay.printWarning("EMAN module did not get imported")
+#pass
 
 def executeEmanCmd(emancmd, verbose=False):
 	sys.stderr.write(apDisplay.colorString("EMAN: ","magenta")+emancmd+"\n")
@@ -16,8 +16,8 @@ def executeEmanCmd(emancmd, verbose=False):
 		else:
 			os.system(emancmd)
 	except:
-		apDisplay.printError("could not run eman command: "+emancmd)
-
+		apDisplay.printWarning("could not run eman command: "+emancmd)
+		raise
 
 def getNumParticlesInStack(stackname):
 	numparticles = EMAN.fileCount(stackname)[0]
