df6e429ef0c41416e4f22ce252a1e9d0b9c2f2c8
dfellman <dfellman>
2007-11-16 18:15:30 +0000

    *** empty log message ***

--- myamiweb/tomo/jpeg.php
+++ myamiweb/tomo/jpeg.php
@@ -2,25 +2,29 @@
 $paths = array('.', '..', get_include_path());
 set_include_path(implode(PATH_SEPARATOR, $paths));
 require_once "config.php";
+require_once "inc/leginon.inc";
 require_once "inc/image.inc";
 
 $binning = 16;
 $sigma = 3;
+$kernel = 3;
 $quality = 75;
+$densitymax = 255;
 
 $imageId = $_GET['imageId'];
 $path = $leginondata->getImagePathFromImageId($imageId);
 $filename = $leginondata->getFilenameFromId($imageId);
-$input = fopen($path.$filename, 'rb');
-if(!$input)
-    exit('cannot open file');
-$output = fopen('php://output', 'wb');
-if(!$output)
-    exit('cannot output');
 
+$src_mrc = mrcread($path.$filename);
+list($pmin, $pmax) = mrcstdevscale($src_mrc, $densitymax);
+mrcgaussianfilter($src_mrc, $binning, $kernel, $sigma);
+$img = mrctoimage($src_mrc,$pmin,$pmax);
+
+// --- create png image
 header("Content-type: image/jpeg");
-mrc2jpeg($input, $output, $binning, $sigma, $quality);
-fclose($input);
-fclose($output);
+imagejpeg($img, '', $quality);
+// --- destroy resources in memory
+mrcdestroy($src_mrc);
+imagedestroy($img);
 
 ?> 
