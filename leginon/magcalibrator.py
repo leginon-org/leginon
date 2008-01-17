c0504520e55bf27dc2bb18db4047d637b72a5fdf
pulokas <pulokas>
2008-01-17 21:38:13 +0000

    more stuff

--- leginon/magcalibrator.py
+++ leginon/magcalibrator.py
@@ -27,12 +27,18 @@ class MagCalibrator(calibrator.Calibrator):
 		'minsize': 50,
 		'maxsize': 500,
 		'pause': 1.0,
+		'label': '',
+		'maxthreshold': 60000,
+		'maxcount': 5000,
+		'cutoffpercent': 1.0,
+		'minbright': 100,
+		'maxbright': 5000,
 	})
 	def __init__(self, id, session, managerlocation, **kwargs):
 		calibrator.Calibrator.__init__(self, id, session, managerlocation, **kwargs)
 		self.start()
 
-	def uiStart(self):
+	def OLDuiStart(self):
 		mag = self.instrument.tem.Magnification
 		print 'MAG', mag
 		mags = self.getMags()
@@ -51,6 +57,52 @@ class MagCalibrator(calibrator.Calibrator):
 			print 'PREVIOUSMAGS', previousmags
 	
 		self.matchMags(previousmags)
+
+	def uiStart(self):
+		mag = self.instrument.tem.Magnification
+		print 'MAG', mag
+		mags = self.getMags()
+		print 'MAGS', mags
+		magindex = mags.index(mag)
+		print 'MAGINDEX', magindex
+
+		if magindex == 0:
+			print 'already at lowest mag'
+			return
+
+		steps = self.settings['magsteps']
+		maglist = mags[magindex-steps:magindex]
+		maglist.reverse()
+	
+		self.acquireMags(maglist)
+
+	def acquireAcquisitionImageData(self, range=None):
+		if range is None:
+			im = self.acquireImage()
+		else:
+			im = self.acquireWithinRange(*range)
+		im = leginondata.AcquisitionImageData(initializer=im)
+		im['session'] = self.session
+		mag = im['scope']['magnification']
+		magstring = '%06d' % (mag,)
+		label = self.settings['label']
+		im['filename'] = self.session['name'] + '-' + label + '-' + magstring
+		im['label'] = label
+		return im
+
+	def acquireMags(self, maglist):
+		firstim = self.acquireAcquisitionImageData()
+		firstim.insert(force=True)
+		print 'FIRST', firstim['image']
+		limitmin = self.settings['minbright']
+		limitmax = self.settings['maxbright']
+		for mag in maglist:
+			self.logger.info('mag: %s' % (mag,))
+			self.instrument.tem.Magnification = mag
+			self.pause()
+			im = self.acquireAcquisitionImageData(range=(limitmin,limitmax))
+			im.insert(force=True)
+			self.logger.info('inserted mag: %s' % (mag,))
 
 	def uiTest(self):
 		imdata = self.acquireImage()
@@ -111,34 +163,64 @@ class MagCalibrator(calibrator.Calibrator):
 		magdata['shiftcol'] = shift[1]
 		magdata.insert(force=True)
 
+	def isSaturated(self, im):
+		thresh = self.settings['threshold']
+		bins = (thresh,)
+		result = numpy.histogram(im, bins=bins)
+		count = result[0][0]
+		maxcount = self.settings['maxcount']
+		if count > maxcount:
+			self.logger.info('Overflow:  %s pixels above %s (max allowed: %s)' % (count, thresh, maxcount))
+			return True
+		else:
+			return False
+
+	def isUnderexposed(self, im):
+		thresh = self.settings['threshold']
+
+	def brightestStats(self, im, percent):
+		# look only at the brightest 1% of the pixels
+		sortedpixels = numpy.sort(im, axis=None)
+		npixels = len(sortedpixels)
+		nbrightest = int(percent / 100.0 * npixels)
+		brightest = sortedpixels[-nbrightest:]
+		stats = arraystats.all(brightest)
+		self.logger.info('Top %.1f%% stats: mean: %.1f, std: %.1f, min: %.1f, max: %.1f' % (percent, stats['mean'],stats['std'],stats['min'],stats['max']))
+		return stats
+
 	def acquireWithinRange(self, min, max):
 		imagedata = self.acquireImage()
-		mean = arraystats.mean(imagedata['image'])
-		self.logger.info('image mean:  %f' % (mean,))
+		stats = self.brightestStats(imagedata['image'], self.settings['cutoffpercent'])
 
-		while mean > max:
-			self.logger.info('too bright, spreading beam...')
-			# assuming we are greater than crossover, increase lens value
-			i = self.instrument.tem.Intensity
-			self.instrument.tem.Intensity = 1.1 * i
-			imagedata = self.acquireImage()
-			mean = arraystats.mean(imagedata['image'])
-			self.logger.info('image mean:  %f' % (mean,))
+		while not (min < stats['mean'] < max):
+			if stats['mean'] > max:
+				self.logger.info('too bright')
+				# assuming we are greater than crossover, increase lens value
+				i = self.instrument.tem.Intensity
+				if i < 1.0:
+					self.logger.info('spreading beam')
+					self.instrument.tem.Intensity = 1.02 * i
+				else:
+					self.logger.info('decreasing exposure time')
+					t = self.instrument.ccdcamera.ExposureTime
+					self.instrument.ccdcamera.ExposureTime = t / 2
 
-		while mean < min:
-			self.logger.info('not bright enough, condensing beam...')
-			# assuming we are greater than crossover, decrease lens value
-			i = self.instrument.tem.Intensity
-			self.instrument.tem.Intensity = 0.9 * i
-			imagedata = self.acquireImage()
-			mean = arraystats.mean(imagedata['image'])
-			self.logger.info('image mean:  %f' % (mean,))
+				imagedata = self.acquireImage()
+				stats = self.brightestStats(imagedata['image'], self.settings['cutoffpercent'])
+
+			if stats['mean'] < min:
+				self.logger.info('not bright enough, condensing beam...')
+				# assuming we are greater than crossover, decrease lens value
+				i = self.instrument.tem.Intensity
+				self.instrument.tem.Intensity = 0.98 * i
+				imagedata = self.acquireImage()
+				stats = self.brightestStats(imagedata['image'], self.settings['cutoffpercent'])
 
 		return imagedata
 
 	def acquireImage(self):
 		im = calibrator.Calibrator.acquireImage(self)
-		im['image'] = ndimage.gaussian_filter(im['image'], 1.2)
+		#im['image'] = ndimage.gaussian_filter(im['image'], 1.2)
 		return im
 
 	def matchMags(self, mags):
