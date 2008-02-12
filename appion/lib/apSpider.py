c1936d8a68431d9ffb0e92e3d23cc088a60bd8ee
Neil Voss <vossman>
2008-02-12 23:31:23 +0000

    more checks cleanup

--- appion/lib/apSpider.py
+++ appion/lib/apSpider.py
@@ -18,60 +18,67 @@ def spiderOutputLine(int1, int2, float1, float2, float3, float4, float5, float6=
 	return line
 
 def fermiLowPassFilter(imgarray, pixrad=2.0, dataext="spi"):
-	#save array to spider file
+	### save array to spider file
 	arrayToSpiderSingle(imgarray, "temp001."+dataext)
-
-	#run the filter
+	### run the filter
 	import spyder
 	spider_exe = os.popen("which spider").read().strip()
 	mySpider = spyder.SpiderSession(spiderexec=spider_exe, dataext=dataext)
-	#filter request: infile, outfile, filter-type, inv-radius, temperature
-	mySpider.toSpider("FQ NP", "temp001", "filt001", "5", str(1.0/pixrad), "0.02")
+	### filter request: infile, outfile, filter-type, inv-radius, temperature
+	print "lowpass pixrad=",pixrad
+	mySpider.toSpider("FQ NP", "temp001", "filt001", "5", str(1.0/pixrad), "0.04")
 	mySpider.close()
-	#this function does not wait for a results
+	### this function does not wait for a results
 	while(not os.path.isfile("filt001."+dataext)):
 		time.sleep(0.2)
-	#read array from spider file
+	time.sleep(0.5)
+	### read array from spider file
 	filtarray = spiderSingleToArray("filt001."+dataext)
-	#clean up
-	os.popen("rm -f temp001."+dataext+" filt001."+dataext)
+	### clean up
+	#os.popen("rm -f temp001."+dataext+" filt001."+dataext)
 	return filtarray
 
 def fermiHighPassFilter(imgarray, pixrad=200.0, dataext="spi"):
-	#save array to spider file
+	### save array to spider file
 	arrayToSpiderSingle(imgarray, "temp001."+dataext)
-
-	#run the filter
+	### run the filter
 	import spyder
 	spider_exe = os.popen("which spider").read().strip()
 	mySpider = spyder.SpiderSession(spiderexec=spider_exe, dataext=dataext)
-	#filter request: infile, outfile, filter-type, inv-radius, temperature
+	### filter request: infile, outfile, filter-type, inv-radius, temperature
 	mySpider.toSpider("FQ NP", "temp001", "filt001", "6", str(1.0/pixrad), "0.02")
 	mySpider.close()
-	#this function does not wait for a results
+	### this function does not wait for a results
 	while(not os.path.isfile("filt001."+dataext)):
 		time.sleep(0.2)
-	#read array from spider file
+	time.sleep(0.5)
+	### read array from spider file
 	filtarray = spiderSingleToArray("filt001."+dataext)
-	#clean up
+	### clean up
 	os.popen("rm -f temp001."+dataext+" filt001."+dataext)
 	return filtarray
 
 def arrayToSpiderSingle(imgarray, imgfile):
-	## temp hack
+	### temp hack
 	apImage.arrayToMrc(imgarray, "temp001.mrc")
-	apEMAN.executeEmanCmd("proc2d temp001.mrc "+imgfile+" spiderswap-single")
-	os.popen("rm -f temp001.mrc")
-	## better way to do it
+	apEMAN.executeEmanCmd("proc2d temp001.mrc "+imgfile+" spiderswap-single", verbose=False)
+	while(not os.path.isfile(imgfile)):
+		time.sleep(0.2)
+	#os.popen("rm -f temp001.mrc")
+	### better way to do it
 	#img = apImage.arrayToImage(imgarray)
 	#img.save(imgfile, format='SPIDER')
 
 def spiderSingleToArray(imgfile):
-	## temp hack
-	apEMAN.executeEmanCmd("proc2d "+imgfile+" temp001.mrc")
+	### temp hack
+	if not os.path.isfile(imgfile):
+		apDisplay.printError("File: "+imgfile+" does not exist")
+	apEMAN.executeEmanCmd("proc2d "+imgfile+" temp001.mrc", verbose=False)
+	while(not os.path.isfile("temp001.mrc")):
+		time.sleep(0.2)
 	imgarray = apImage.mrcToArray("temp001.mrc")
-	os.popen("rm -f temp001.mrc")
-	## better way to do it
+	#os.popen("rm -f temp001.mrc")
+	### better way to do it
 	#img = Image.open(imgfile)
 	#imgarray = apImage.imageToArray(img)
 	return imgarray
