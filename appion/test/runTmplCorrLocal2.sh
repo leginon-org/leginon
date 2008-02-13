eb9f93c9fe4727245f09f09d1e26042301e13676
Neil Voss <vossman>
2008-02-13 00:13:49 +0000

    playing around

--- appion/test/runTmplCorrLocal2.sh
+++ appion/test/runTmplCorrLocal2.sh
@@ -11,7 +11,7 @@ reset
   06nov29b_b_00031gr_00041sq_v01_00019hl_00003en.mrc \
   06nov29b_b_00031gr_00041sq_v01_00019hl_00004en.mrc \
   06nov29b_b_00031gr_00041sq_v01_00019hl_00005en.mrc \
-  templateIds=26,27,28 range1=0,51,10 range2=0,180,10 range3=0,180,10 \
-  diam=150 lp=15 bin=8 outdir=. maxpeaks=50 \
-  runid=${runid} thresh=0.45 background \
-  overlapmult=2 method=updated
+  templateIds=26,27 range1=0,51,10 range2=0,180,10 \
+  diam=160 lp=25 hp=400 median=2 bin=4 outdir=. maxpeaks=70 \
+  runid=${runid} thresh=0.5 keepall background \
+  overlapmult=1.5
