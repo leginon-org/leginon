ae1aeb072791802bde6cd55561c60135b0ff21b6
Neil Voss <vossman>
2008-02-22 17:35:10 +0000

    playing around with algorithm

--- appion/bin/numproj.py
+++ appion/bin/numproj.py
@@ -2,6 +2,9 @@
 
 import math
 import sys
+import random
+import numpy
+from scipy import ndimage
 
 """
 relavent EMAN code:
@@ -37,38 +40,75 @@ def arange(start, stop=None, step=None):
 
 
 def numProj(ang=5, sym='d7', with_mirror=False):
+	"""
+	for a given angular increment and symmetry calculate the projections
+	
+	approx. for C symm:
+		num_projections ~= 18000 * csym^-1 * ang^-2
+	approx. for D symm:
+		num_projections ~= 9000 * dsym^-1 * ang^-2
+	"""
 	csym = abs(float(sym[1:]))
 	ang = abs(float(ang))
+	if ang == 0.0:
+		return 0
 	angrad = ang*math.pi/180.0
 	maxalt = math.pi/2.0 + angrad/1.99
 	maxaz = 2.0*math.pi/csym
-	if sym[0] == 'd':
+	if sym[0].lower() == 'd':
 		maxaz /= 2.0
 	numproj = 0
-	for i,alt in enumerate(arange(0.0, maxalt, angrad)):
+	for alt in arange(0.0, maxalt, angrad):
+		if alt < 1.0e-6:
+			### only one for top projection
+			numproj+=1
+			#print "%d\t%.2f\t%.2f\t0.00" % (numproj, alt*180.0/math.pi, 0.0)
+			continue
+		### calculate number of steps
 		numsteps = math.floor(360.0/(ang*1.1547));
 		numsteps = math.floor(numsteps * math.sin(alt) + 0.5)
 		if numsteps < 1.0e-3:
+			### only valid for c1, d1, c2 and d2
 			numsteps = 1.0
-		numsteps = csym * math.floor(numsteps/csym + 0.5)
-		if numsteps < 1.0e-3:
-			numsteps = 1.0e-3
+		numsteps = csym * math.floor(numsteps/csym + 0.5) + 1.0e-6
+		### calculate azimuthal step size
 		azstep = 2.0*math.pi/numsteps
-		if alt < 1.0e-6:
-			azstep = maxaz
-		elif (maxaz/azstep) < 2.8:
+		if (maxaz/azstep) < 2.8:
 			### if less than 2.8 steps, use 2 steps
 			azstep = maxaz/2.1
 		for az in arange(0.0, maxaz-azstep/4.0, azstep):
 			if not with_mirror and az > math.pi-1.0e-3 and abs(alt-math.pi/2.0) < 1.0e-3:
 				### ignore half of the equtor
-				#print "skip mirror"
 				continue
 			numproj+=1
-			print "%d\t%.2f\t%.2f\t0.00" % (numproj, alt*180.0/math.pi, az*180.0/math.pi)
+			#print "%d\t%.2f\t%.2f\t0.00" % (numproj, alt*180.0/math.pi, az*180.0/math.pi)
 
-	print "Number of Projections: ",numproj
+	#print "Number of Projections: ",numproj
+	#sys.stderr.write(str(ang)+"\t"+str(numproj)+"\n")
+	#sys.stderr.write(str(math.log(ang))+"\t"+str(math.log(numproj))+"\n")
 	return numproj
 
 if __name__ == '__main__':
-	numProj(sys.argv[1], sys.argv[2])
+	#numProj(sys.argv[1], sys.argv[2])
+	#numProj(1.0, sys.argv[2])
+	errors = []
+	for i in range(50000):
+		if i % 500 == 0:
+			sys.stderr.write(".")
+		ang = random.random()*5.0+0.5
+		nsym = math.ceil(random.random()*10.0)
+		sym = "c"+str(int(nsym))
+		nproj = numProj(ang,sym)
+		dproj = 18000.0/nsym/ang**2
+		#dproj = 9000.0/nsym/ang**2
+		#sys.stdout.write(str(dproj)+"\t"+str(nproj)+"\n")
+		#print round(100.0*abs(nproj-dproj)/float(nproj),3),nproj,dproj,round(ang,3),sym
+		errors.append(100.0*abs(nproj-dproj)/float(nproj))
+		#numProj(1000.0/float(i+10)**2, sys.argv[2])
+		#print math.log(i+1), math.log(numProj(1.0, "d"+str(i+1)))
+	earray = numpy.asarray(errors, dtype=numpy.float32)
+	mean = ndimage.mean(earray)
+	std = ndimage.standard_deviation(earray)
+	sys.stderr.write("\nmean error: "+str(mean)+" +- "+str(std)+"\n")
+
+
