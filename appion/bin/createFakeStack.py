5fe50485fc72bc45dd1ee41fa8a61d3a1a08e6c5
Neil Voss <vossman>
2008-02-26 01:41:44 +0000

    it works

--- appion/bin/createFakeStack.py
+++ appion/bin/createFakeStack.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python -O
 
+import os
+import sys
+import time
+import random
+import cPickle
 import MySQLdb
 import sinedon
 import apEMAN
 import apDisplay
-import time
+
 
 dbconf = sinedon.getConfig('appionData')
 db     = MySQLdb.connect(**dbconf)
@@ -39,14 +44,16 @@ def getParticles(stackid):
 			+"      stpart1.`REF|ApStackData|stack` = "+str(stackid)+" \n" 
 			+"  AND stpart2.`REF|ApStackData|stack` = "+str(stackid)+" \n" 
 			+"ORDER BY stpart1.`particleNumber` ASC \n"
-			+"LIMIT 10 \n"
+			#+"LIMIT 21 \n"
 		)
 	cursor.execute(query)
 	results = cursor.fetchall()
 	#apDisplay.printMsg("Fetched data in "+apDisplay.timeString(time.time()-t0))
 	if not results:
 		apDisplay.printError("Failed to get stack particles")
-	return convertSQLtoTree(results)
+	parttree = convertSQLtoTree(results)
+	parttree.sort(compPart)
+	return parttree
 
 #=====================
 def convertSQLtoTree(results):
@@ -72,17 +79,84 @@ def convertSQLtoTree(results):
 	return parttree
 
 #=====================
-def genProj(alt, az, phi, tilt=False):
+def compPart(a, b):
+	if a['part1'] > b['part1']:
+		return 1
+	else:
+		return -1
+
+#=====================
+def randomEuler():
+	"""
+	alt = int(round(random.random()*180.0,0))
+	az = int(round(random.random()*360.0,0))
+	phi = int(round(random.random()*360.0,0))
+	"""
+	alt = int(round(random.random()*90.0,0))
+	az = int(round(random.random()*51.43,0))
+	phi = int(round(random.random()*360.0,0))
+	return (alt, az, phi)
+
+#=====================
+def genProj(euler, tilt=False):
+	(alt, az, phi) = euler
 	rotcmd = ( "proc3d groel.mrc rotated.mrc rot="
 		+str(alt)+","+str(az)+","+str(phi) )
+	apEMAN.executeEmanCmd(rotcmd, verbose=False, showcmd=False)
 	if tilt is True:
 		cmd = "project3d rotated.mrc euler=15,90,-90"
 	else:
 		cmd = "project3d rotated.mrc euler=0,0,0"
+	apEMAN.executeEmanCmd(cmd, verbose=False, showcmd=False)
 
-
-if __name__ == '__main__':
-	parttree = getParticles(773)
+#=====================
+def generateProjections(parttree):
+	t0 = time.time()
+	datafile = "parttree_data-"+str(stackid)+".txt"
+	dataf = open(datafile, 'w', 0666)
+	count = 0.0
+	total = float(len(parttree))
+	mult = 0.05
 	for part in parttree:
-		print part
+		count += 1.0
+		### generate euler
+		if not 'euler' in part:
+			part['euler'] = randomEuler()
+			### set euler for pair
+			if part['part2'] < len(parttree) and parttree[part['part2']-1]['part1'] == part['part2']:
+				parttree[part['part2']-1]['euler'] = part['euler']
+			else:
+				apDisplay.printWarning("using slow method for part: "+str(part))
+				for pair in parttree:
+					if pair['part1'] == part['part2']:
+						pair['euler'] = part['euler']
+		### save to file
+		dataf.write(str(part)+"\n")
+		### generate projection
+		if count > mult*total:
+			sys.stderr.write("\b\b\b\b\b\b\b\b\b\b\b\b"+str(int(100*mult))+"% complete")
+			mult += 0.05
+			#sys.stderr.write(".")
+		genProj(part['euler'], tilt=part['tilt'])
+	dataf.close()
+	sys.stderr.write("\nwriting to cache file")
+	cachefile = "parttree_cache-"+str(stackid)+".pickle"
+	cachef = open(cachefile, 'w', 0666)
+	cPickle.dump(parttree, cachef)
+	cachef.close()
+	apDisplay.printMsg("Projected "+str(len(parttree))+" particles in "+apDisplay.timeString(time.time()-t0))
+
+#=====================
+#=====================
+if __name__ == '__main__':
+	stackid = 773
+	if os.path.isfile("proj.hed"):
+		apDisplay.printWarning("Removing proj.hed and proj.img files")
+		time.sleep(2)
+		os.remove("proj.hed")
+		os.remove("proj.img")
+	parttree = getParticles(stackid)
+	generateProjections(parttree)
+
+
 
