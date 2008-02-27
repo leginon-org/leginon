cb286ce5a95da9784ec50cec7adfb61a5f06e163
Neil Voss <vossman>
2008-02-27 19:28:33 +0000

    new subStack that uses appionScript()

--- appion/bin/subStack.py
+++ appion/bin/subStack.py
@@ -2,100 +2,84 @@
 
 #python
 import os
-import sys
-from optparse import OptionParser
-#appion
-import apStack
 import shutil
+#appion
+import appionScript
+import apStack
 import apDisplay
-import apParam
-import apUpload
-import apDB
-import appionData
 
-appiondb=apDB.apdb
+class subStackScript(appionScript.AppionScript):
+	#=====================
+	def setupParserOptions(self):
+		self.parser.set_usage("Usage: %prog --old-stack-id=ID --keep-file=FILE [options]")
+		self.parser.add_option("-s", "--old-stack-id", dest="stackid", type="int",
+			help="Stack database id", metavar="ID")
+		self.parser.add_option("-k", "--keep-file", dest="keepfile",
+			help="File listing which particles to keep", metavar="FILE")
+		self.parser.add_option("-C", "--commit", dest="commit", default=True,
+			action="store_true", help="Commit stack to database")
+		self.parser.add_option("--no-commit", dest="commit", default=True,
+			action="store_false", help="Do not commit stack to database")
+		self.parser.add_option("-o", "--outdir", dest="outdir",
+			help="Output directory", metavar="PATH")
+		self.parser.add_option("-d", "--description", dest="description", default="",
+			help="Stack description", metavar="TEXT")
+		self.parser.add_option("-n", "--new-stack-name", dest="runname",
+			help="Run id name", metavar="STR")
 
+	#=====================
+	def checkConflicts(self):
+		if self.params['stackid'] is None:
+			apDisplay.printError("stackid was not defined")
+		if self.params['description'] is None:
+			apDisplay.printError("stack description was not defined")
+		if self.params['runname'] is None:
+			apDisplay.printError("new stack name was not defined")
+		if self.params['keepfile'] is None:
+			apDisplay.printError("keep file was not defined")
+		self.params['keepfile'] = os.path.abspath(self.params['keepfile'])
+		if not os.path.isfile(self.params['keepfile']):
+			apDisplay.printError("Could not find keep file: "+params['keepfile'])
 
-def parseCommandLine():
-	usage = "Usage: %prog --old-stack-id=ID --keep-file=FILE [options]"
-	parser = OptionParser(usage=usage)
-
-	parser.add_option("-s", "--old-stack-id", dest="stackid", type="int",
-		help="Stack database id", metavar="ID")
-	parser.add_option("-k", "--keep-file", dest="keepfile",
-		help="File listing which particles to keep", metavar="FILE")
-	parser.add_option("--commit", dest="commit", default=True,
-		action="store_true", help="Commit stack to database")
-	parser.add_option("--no-commit", dest="commit", default=True,
-		action="store_false", help="Do not commit stack to database")
-	parser.add_option("-o", "--outdir", dest="outdir",
-		help="Output directory", metavar="PATH")
-	parser.add_option("-d", "--description", dest="description", default="",
-		help="Stack description", metavar="TEXT")
-	parser.add_option("-n", "--new-stack-name", dest="runname",
-		help="Run id name", metavar="STR")
-
-	params = apParam.convertParserToParams(parser)
-	return params
-
-#--------
-def checkConflicts(params):
-	if params['stackid'] is None:
-		apDisplay.printError("stackid was not defined")
-	if params['description'] is None:
-		apDisplay.printError("stack description was not defined")
-	if params['runname'] is None:
-		apDisplay.printError("new stack name was not defined")
-	if params['keepfile'] is None:
-		apDisplay.printError("keep file was not defined")
-	params['keepfile'] = os.path.abspath(params['keepfile'])
-	if not os.path.isfile(params['keepfile']):
-		apDisplay.printError("Could not find keep file: "+params['keepfile'])
-
-
-#-----------------------------------------------------------------------
-
-if __name__ == '__main__':
-	#read command line
-	params = parseCommandLine()
-	checkConflicts(params)
-
-	#get stack data
-	stackdata = apStack.getOnlyStackData(params['stackid'])
-	oldstack = os.path.join(stackdata['path']['path'], stackdata['name'])
-
-	#create run directory
-	if params['outdir'] is None:
+	#=====================
+	def setOutDir(self):
+		stackdata = apStack.getOnlyStackData(self.params['stackid'])
 		path = stackdata['path']['path']
-		path = os.path.split(os.path.abspath(path))[0]
-		params['outdir'] = path
-	params['rundir'] = os.path.join(params['outdir'], params['runname'])
-	apParam.createDirectory(params['rundir'])
-	apDisplay.printMsg("Run directory: "+params['rundir'])
-	os.chdir(params['rundir'])
-	if not os.path.isfile(os.path.basename(params['keepfile'])):
-		shutil.copy(params['keepfile'], os.path.basename(params['keepfile']))
+		uppath = os.path.dirname(os.path.abspath(path))
+		self.params['outdir'] = os.path.join(uppath, self.params['runname'])
 
-	#new stack path
-	newstack = os.path.join(params['rundir'], stackdata['name'])
-	apStack.checkForPreviousStack(newstack)
+	#=====================
+	def start(self):
+		#copy keep file
+		if not os.path.isfile(os.path.basename(self.params['keepfile'])):
+			shutil.copy(self.params['keepfile'], os.path.basename(self.params['keepfile']))
 
-	#get number of particles
-	f = open(params['keepfile'], "r")
-	numparticles = len(f.readlines())
-	f.close()
-	params['description'] += (
-		(" ... substack of %d particles from original stackid=%d" 
-		% (numparticles, params['stackid']))
-	)
-	
-	#create the new sub stack
-	apStack.makeNewStack(oldstack, newstack, params['keepfile'])
-	if not os.path.isfile(newstack):
-		apDisplay.printError("No stack was created")
+		#new stack path
+		stackdata = apStack.getOnlyStackData(self.params['stackid'])
+		oldstack = os.path.join(stackdata['path']['path'], stackdata['name'])
+		newstack = os.path.join(self.params['outdir'], stackdata['name'])
+		apStack.checkForPreviousStack(newstack)
 
-	apStack.commitSubStack(params)
-	apStack.averageStack(stack=newstack)
+		#get number of particles
+		f = open(self.params['keepfile'], "r")
+		numparticles = len(f.readlines())
+		f.close()
+		self.params['description'] += (
+			(" ... %d particle substack of stackid %d" 
+			% (numparticles, self.params['stackid']))
+		)
+		
+		#create the new sub stack
+		apStack.makeNewStack(oldstack, newstack, self.params['keepfile'])
+		if not os.path.isfile(newstack):
+			apDisplay.printError("No stack was created")
 
+		apStack.commitSubStack(self.params)
+		apStack.averageStack(stack=newstack)
 
+#=====================
+if __name__ == "__main__":
+	subStack = subStackScript()
+	subStack.start()
+	subStack.close()
 
