22a8ab2a08012bf933b9ed18862a2ed493683bcf
Neil Voss <vossman>
2008-02-27 23:30:46 +0000

    silly tool for copying ACE images

---
+++ appion/bin/assembleBestCTFImages.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python -O
+
+#pythonlib
+import os
+import sys
+import re
+import math
+import cPickle
+import time
+import shutil
+#appion
+import appionLoop
+import apImage
+import apDisplay
+import apDatabase
+import apCtf
+import apParam
+
+class aceLoop(appionLoop.AppionLoop):
+	def setProcessingDirName(self):
+		self.processdirname = "bestace"
+		
+
+	def postLoopFunctions(self):
+		apCtf.printCtfSummary(self.params)
+
+	def reprocessImage(self, imgdata):
+		"""
+		Returns 
+		True, if an image should be reprocessed
+		False, if an image was processed and should NOT be reprocessed
+		None, if image has not yet been processed 
+		e.g. a confidence less than 80%
+		"""
+		if self.params['reprocess'] is None:
+			return None
+		ctfvalue, conf = apCtf.getBestCtfValueForImage(imgdata)
+		if ctfvalue is None:
+			return None
+
+		if conf > self.params['reprocess']:
+			return False
+		else:
+			return True
+
+	def processImage(self, imgdata):
+		ctfvalue, conf = apCtf.getBestCtfValueForImage(imgdata)
+		if ctfvalue is None:
+			return None
+		acepath = ctfvalue['acerun']['path']['path']
+		opimgpath = os.path.join(acepath,"opimages")
+		#print opimgpath
+		#for i,v in ctfvalue.items():
+		#	print i,v
+
+		if conf < 0.5:
+			confdir = "doubtful"
+		elif conf > 0.8:
+			confdir = "probable"
+		else:
+			confdir = "questionable"
+
+		#copy first file
+		opfile = os.path.join(opimgpath, ctfvalue['graph1'])
+		if os.path.isfile(opfile):
+			shutil.copyfile(opfile, os.path.join(self.params['opimage1'], "all", ctfvalue['graph1']))
+			shutil.copyfile(opfile, os.path.join(self.params['opimage1'], confdir, ctfvalue['graph1']))
+		else:
+			apDisplay.printWarning("could not find opimage: "+opfile)
+
+		#copy second file
+		opfile = os.path.join(opimgpath, ctfvalue['graph2'])
+		if os.path.isfile(opfile):
+			shutil.copyfile(opfile, os.path.join(self.params['opimage2'], "all", ctfvalue['graph2']))
+			shutil.copyfile(opfile, os.path.join(self.params['opimage2'], confdir, ctfvalue['graph2']))
+		else:
+			apDisplay.printWarning("could not find opimage: "+opfile)
+
+	def commitToDatabase(self, imgdata):
+		return
+
+	def specialDefaultParams(self):
+		self.params['nowait'] = True
+		self.params['background'] = True
+		return
+
+	def specialCreateOutputDirs(self):
+		self.params['opimage1'] = os.path.join(self.params['rundir'],"opimages1")
+		self.params['opimage2'] = os.path.join(self.params['rundir'],"opimages2")
+		apParam.createDirectory(self.params['opimage1'], warning=False)
+		apParam.createDirectory(self.params['opimage2'], warning=False)
+		for p in ("doubtful", "questionable", "probable", "all"):
+			path = os.path.join(self.params['opimage1'], p)
+			apParam.createDirectory(path, warning=False)
+			path = os.path.join(self.params['opimage2'], p)
+			apParam.createDirectory(path, warning=False)
+
+	def specialParseParams(self,args):
+		for arg in args:
+			elements=arg.split('=')
+			elements[0] = elements[0].lower()
+			if (elements[0]=='tempdir'):
+				self.params['tempdir']=os.path.abspath(elements[1]+"/")
+			else:
+				apDisplay.printError(str(elements[0])+" is not recognized as a valid parameter")
+
+	def specialParamConflicts(self):
+		return
+
+
+if __name__ == '__main__':
+	imgLoop = aceLoop()
+	imgLoop.run()
+
