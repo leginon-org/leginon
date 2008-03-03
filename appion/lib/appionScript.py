e0a6b14d9d751c6e39a1759b9da1705dd0ea78e7
Neil Voss <vossman>
2008-03-03 22:41:51 +0000

    umask

--- appion/lib/appionScript.py
+++ appion/lib/appionScript.py
@@ -25,7 +25,10 @@ class AppionScript(object):
 		self.t0 = time.time()
 		self.timestamp = time.strftime("%y%b%d").lower()+lowercase[time.localtime()[4]%26]
 		self.functionname = apParam.getFunctionName(sys.argv[0])
-		os.umask(000)
+		if os.getgid() == 773:
+			os.umask(002)
+		else:
+			os.umask(000)
 
 		### setup default parser: output directory, etc.
 		self.parser = OptionParser()
