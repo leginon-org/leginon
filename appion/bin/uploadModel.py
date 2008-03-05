2db338cf24f776a1ff964309c6d5ad493c1d8ad5
Neil Voss <vossman>
2008-03-05 19:33:31 +0000

    uploadModel now uses appionScript()

--- appion/bin/uploadModel.py
+++ appion/bin/uploadModel.py
@@ -13,99 +13,238 @@ import apDisplay
 import apDatabase
 import apRecon
 import apVolume
+import appionScript
 
-if __name__ == '__main__':
-	# record command line
-	#apParam.writeFunctionLog(sys.argv)
+#=====================
+#=====================
+class UploadModelScript(appionScript.AppionScript):
+	#=====================
+	def setupParserOptions(self):
+		"""
+		self.params['rundir']=os.path.abspath('.')
+		self.params['abspath']=os.path.abspath('.')
+		self.params['outdir']=None
+		self.params['scale']=None
+		self.params['rescale']=False
+		if (elements[0]=='outdir'):
+			self.params['outdir']=elements[1]
+		elif (arg=='chimeraonly'):
+			self.params['chimeraonly']=True
+			self.params['commit']=False
+		elif (elements[0]=='rescale'):
+			modinfo=elements[1].split(',')
+			if len(modinfo) == 2:
+				self.params['origmodel']=modinfo[0]
+				self.params['newapix']=float(modinfo[1])
+				self.params['rescale']=True
+			else:
+				apDisplay.printError("rescale must include both the original model id and a scale factor")
 
-	# create params dictionary & set defaults
-	params = apUpload.createDefaults()
+		# if not rescaling, make sure that the input model exists
+		if (os.path.isfile(mrcfile) or self.params['rescale'] is True):
+			(self.params['path'], self.params['name']) = os.path.split(mrcfile)
+			self.params['path'] = os.path.abspath(self.params['path'])
+			if not self.params['path']:
+				self.params['path']=self.params['abspath']
+		else:
+			apDisplay.printError("file '"+mrcfile+"' does not exist\n")
+		"""
 
-	# parse command line input
-	apUpload.parseModelUploadInput(sys.argv, params)
+		self.parser.set_usage("Usage: %prog --file=<filename> --session=<name> --symm=<#> --apix=<#> \n\t "
+			+" --res=<#> --description='text' [--contour=<#>] [--zoom=<#>] \n\t "
+			+" [--rescale=<model ID,scale factor> --boxsize=<#>] ")
+		self.parser.add_option("-f", "--file", dest="file", 
+			help="MRC file to upload", metavar="FILE")
+		self.parser.add_option("-s", "--session", dest="session",
+			help="Session name associated with template (e.g. 06mar12a)", metavar="SESSION")
+		self.parser.add_option("--sym", "--symm", "--symmetry", dest="sym", type="int",
+			help="Symmetry id in the database", metavar="INT")
+		self.parser.add_option("--old-apix", dest="oldapix", type="float",
+			help="Original pixel size in Angstroms if oldapix != apix, it will be rescaled", metavar="FLOAT")
+		self.parser.add_option("-a", "--apix", dest="newapix", type="float",
+			help="Pixel size in Angstroms", metavar="FLOAT")
+		self.parser.add_option("--res", "--resolution", dest="res", type="float",
+			help="Map resolution in Angstroms", metavar="FLOAT")
+		self.parser.add_option("-d", "--description", dest="description",
+			help="Description of the reconstruction (must be in quotes)", metavar="'TEXT'")
+		self.parser.add_option("-z", "--zoom", dest="zoom", type="float", default=1.75,
+			help="Zoom factor for snapshot rendering (1.75 by default)", metavar="FLOAT")
+		self.parser.add_option("-c", "--contour", dest="contour", type="float", default=1.5,
+			help="Sigma level at which snapshot of density will be contoured (1.5 by default)", metavar="FLOAT")
+		self.parser.add_option("-C", "--commit", dest="commit", default=True, action="store_true", 
+			help="Commit reconstruction to database")
+		self.parser.add_option("--no-commit", dest="commit", default=True, action="store_false", 
+			help="Do not commit reconstruction to database")
+		self.parser.add_option("--chimera-only", dest="chimeraonly", default=False, action="store_true",
+			help="Do not do any reconstruction calculations only run chimera")
+		self.parser.add_option("-o", "--outdir", dest="outdir",
+			help="Location to store uploaded model", metavar="PATH")
+		self.parser.add_option("-b", "--boxsize", "--newbox", dest="newbox", type="int",
+			help="Boxsize of new model", metavar="INT")
+		self.parser.add_option("-m", "--modelid", "--old-model-id", dest="oldmodelid", type="int",
+			help="Initial model id in the database to rescale", metavar="INT")
+		self.parser.add_option("-n", "--name", dest="name",
+			help="File name for new model, automatically set")
 
-	# check to make sure that incompatible parameters are not set
-	if params['apix'] is None:
-		apDisplay.printError("enter the pixel size of the model")
-	if params['session'] is None:
-		apDisplay.printError("enter a session ID")
-	if params['sym'] is None:
-		apUpload.printSymmetries()
-		apDisplay.printError("enter a symmetry ID")
-	if params['description'] is None:
-		apDisplay.printError("enter a description of the initial model")
-	if params['res'] is None:
-		apDisplay.printError("enter the resolution of the initial model")
-	if params['newbox'] and params['rescale'] is not True:
-		apDisplay.printError("you must provide a reference model to resize the box")
-	if params['outdir'] is None:
-		#auto set the output directory
-		sessiondata = apDatabase.getSessionDataFromSessionName(params['session'])
+	#=====================
+	def checkConflicts(self):
+		if self.params['session'] is None:
+			apDisplay.printError("Enter a session ID")
+		if self.params['description'] is None:
+			apDisplay.printError("Enter a description of the initial model")
+		if self.params['chimeraonly'] is True:
+			self.params['commit'] = False
+		if self.params['newbox'] is not None and self.params['newbox'] % 16 != 0:
+			apDisplay.printWarning("Box size is not a multiple of 16")
+		if self.params['newbox'] is None:
+			self.params['rescale'] = False
+		else:
+			self.params['rescale'] = True
+		### program requires either a model id  or filename
+		if self.params['oldmodelid'] is not None and self.params['file'] is not None:
+			apDisplay.printError("Either provide a modelid or file, but not both")
+		elif self.params['oldmodelid'] is not None and self.params['rescale'] is False:
+			apDisplay.printError("Please specify either a new boxsize or scale for your model")
+		elif self.params['oldmodelid'] is not None:
+			self.getModelParams()
+		elif self.params['file'] is not None:
+			if not os.path.isfile(self.params['file']):
+				apDisplay.printError("could not find file: "+self.params['file'])
+			if self.params['file'][-4:] != ".mrc":
+				apDisplay.printError("uploadModel.py only supports MRC files")
+			self.params['file'] = os.path.abspath(self.params['file'])
+		else:
+			apDisplay.printError("Please provide either a --modelid=112 or --file=initmodel.mrc")
+		### required only if now model id is provided
+		if self.params['newapix'] is None:
+			apDisplay.printError("Enter the pixel size of the model")
+		if self.params['sym'] is None:
+			apUpload.printSymmetries()
+			apDisplay.printError("Enter a symmetry ID, e.g. --symm=19")
+		if self.params['res'] is None:
+			apDisplay.printError("Enter the resolution of the initial model")
+
+
+	#=====================
+	def setOutDir(self):
+		sessiondata = apDatabase.getSessionDataFromSessionName(self.params['session'])
 		path = os.path.abspath(sessiondata['image path'])
 		path = re.sub("leginon","appion",path)
 		path = re.sub("/rawdata","",path)
-		params['outdir'] = os.path.join(path,"models")
+		self.params['outdir'] = os.path.join(path,"models")
 
-	#create the output directory, if needed
-	apParam.createDirectory(params['outdir'])
+	#=====================
+	def setNewFileName(self):
+		#clean up old name
+		basename = os.path.basename(self.params['file'])
+		basename = re.sub(".mrc", "", basename)
+		basename = re.sub("-[0-9]_[0-9]+(apix)?", "", basename)
+		basename = re.sub("-[0-9]+(box)?", "", basename)
+		basename = re.sub("[0-9]+", "", basename)
+		basename = re.sub("-", "", basename)
+		# set apix, box, and foldname
+		foldname = os.path.basename(os.path.dirname(self.params['file']))
+		apixname = re.sub("\.", "_", str(round(self.params['newapix'],2)))+"apix"
+		boxsizename = str(int(self.params['newbox']))+"box"
+		if self.params['oldmodelid'] is not None:
 
-	os.chdir(params['outdir'])
-	apParam.writeFunctionLog(sys.argv)
+			self.params['name'] = basename+"-"+apixname+"-"+boxsizename+".mrc"
+		else:
+			self.params['name'] = foldname+"-"+basename+"-"+apixname+"-"+boxsizename+".mrc"
 
-	apUpload.checkSymInfo(params)
 
-	new = os.path.join(params['outdir'], params['name'])
-	old = os.path.join(params['path'], params['name'])
-	if os.path.isfile(new):
-		mdnew = apFile.md5sumfile(new)
-		mdold = apFile.md5sumfile(old)
+	#=====================
+	def getModelParams(self):
+		modeldata = apVolume.getModelFromId(self.params['oldmodelid'])
+		self.params['oldapix'] = float(modeldata['pixelsize'])
+		self.params['sym'] = int(modeldata['symmetry'].dbid)
+		self.params['res'] = float(modeldata['resolution'])
+		self.params['file'] = os.path.join(modeldata['path']['path'], modeldata['name'])
+
+	#=====================
+	def rescaleModel(self):
+		### rescale old model to a new size
+		modeldata = apVolume.getModelFromId(self.params['origmodel'])
+		old = os.path.join(modeldata['path']['path'], modeldata['name'])
+		apDisplay.printMsg("rescaling model "+origmodelpath+" to "+newmodelpath)
+		apVolume.rescaleModel(old, new, float(oldmod['pixelsize']), self.params['newapix'], self.params['newbox'])
+		# set new apix of the rescaled model
+		self.params['apix'] = self.params['newapix']
+
+	#=====================
+	def checkExistingFile(self):
+		newmodelpath = os.path.join(self.params['outdir'], self.params['name'])
+		origmodelpath = self.params['file']
+		apDisplay.printWarning("a model by the same filename already exists: '"+newmodelpath+"'")
+		### a model by the same name already exists
+		mdnew = apFile.md5sumfile(newmodelpath)
+		mdold = apFile.md5sumfile(origmodelpath)
 		if mdnew != mdold:
-			apDisplay.printError("a different model with name \'"+new+"\' already exists!")
+			### they are different
+			apDisplay.printError("the models are different, cannot overwrite")
 		elif apDatabase.isModelInDB(mdnew):
-			apDisplay.printWarning("same model with md5sum \'"+mdnew+"\' already exists in the DB!")
-			apDisplay.printMsg("skipping model \'"+old+"\'")
-			params['commit'] = False
+			### they are the same and its in the database already
+			apDisplay.printWarning("same model with md5sum '"+mdnew+"' already exists in the DB!")
+			apDisplay.printWarning("creating new images, but skipping upload for file: '"+newmodelpath+"'")
+			self.params['commit'] = False
+			self.params['chimeraonly'] = True
 		else:
-			apDisplay.printWarning("the same model with name \'"+new+"\' already exists!")
-			apDisplay.printMsg("skipping model \'"+old+"\'")
-	else:
-		# if rescaling an old template:
-		if params['rescale'] is True:
-			# get model to be rescaled
-			oldmod = apVolume.getModelFromId(params['origmodel'])
-			old = os.path.join(oldmod['path']['path'], oldmod['name'])
-			apDisplay.printMsg("rescaling model "+old+" to "+new)
-			apVolume.rescaleModel(old, new, float(oldmod['pixelsize']), params['newapix'], params['newbox'])
-			# set new ang/pix of the rescaled model
-			params['apix'] = params['newapix']
-		# otherwise, copy templates to final location
+			### they are the same, but its not in the database
+			apDisplay.printWarning("the same model with name '"+newmodelpath+"' already exists, but is not uploaded!")
+			if self.params['commit'] is True:
+				apDisplay.printMsg("inserting model into database")
+		if self.params['rescale'] is True:
+			apDisplay.printError("cannot rescale an existing model")	
+
+	#=====================
+	def start(self):
+		self.params['syminfo'] = apUpload.getSymmetryData(self.params['sym'])
+		self.params['oldbox'] = apVolume.getModelDimensions(self.params['file'])
+		if self.params['newbox'] is None:
+			self.params['newbox'] = self.params['oldbox']
+		if self.params['oldapix'] is None:
+			self.params['oldapix'] = self.params['newapix']
+		self.params['scale'] =  self.params['oldapix']/self.params['newapix']
+		if self.params['name'] is None:
+			self.setNewFileName()
+		apDisplay.printColor("Naming initial model as: "+self.params['name'], "cyan")
+
+		newmodelpath = os.path.join(self.params['outdir'], self.params['name'])
+		origmodelpath = self.params['file']
+		if os.path.isfile(newmodelpath):
+			### rescale old model to a new size
+			self.checkExistingFile()
+		elif (abs(self.params['oldapix'] - self.params['newapix']) > 1.0e-2 or 
+			abs(self.params['oldbox'] - self.params['newbox']) > 1.0e-1):
+			### rescale old model to a new size
+			apDisplay.printWarning("rescaling original model to a new size")
+			apDisplay.printMsg("rescaling model "+origmodelpath+" by "+str(round(self.params['scale']*100.0,2))+"%")
+			apVolume.rescaleModel(origmodelpath, newmodelpath, 
+				self.params['oldapix'], self.params['newapix'], self.params['newbox'])
 		else:
-			old = os.path.join(params['path'], params['name'])
-			apDisplay.printMsg("copying file "+old+" to "+new)
-			shutil.copyfile(old, new)
+			### simple upload, just copy file to models folder
+			apDisplay.printMsg("copying original model to a new location: "+newmodelpath)
+			shutil.copyfile(origmodelpath, newmodelpath)
 
-	# only allow user read access just so they don't get deleted
-	os.chmod(new, 0666)
+		### upload Initial Model
+		apUpload.getProjectId(self.params)
 
-	modelname = os.path.join(params['outdir'], params['name'])
+		### render chimera images of model
+		initmodel={}
+		initmodel['pixelsize'] = self.params['newapix']
+		initmodel['boxsize']   = self.params['oldbox']
+		initmodel['symmetry']  = self.params['syminfo']
+		apRecon.renderSnapshots(newmodelpath, self.params['res'], initmodel, self.params['contour'], self.params['zoom'])
 
-	params['box'] = apVolume.getModelDimensions(modelname)
-	
-	# upload Initial Model
-	apUpload.getProjectId(params)
 
-	# render images of initial model
-	initmodel={}
-	initmodel['pixelsize'] = params['apix']
-	initmodel['boxsize']   = params['box']
-	initmodel['symmetry']  = params['syminfo']
-	apRecon.renderSnapshots(modelname, params['res'], initmodel, params['contour'], params['zoom'])
+		apUpload.insertModel(self.params)
 
-	apDisplay.printMsg("commiting model to database")
-	if params['commit'] is True:
-		apUpload.insertModel(params)
-	else:
-		apDisplay.printWarning("not commiting model to database")
 
+#=====================
+#=====================
+if __name__ == '__main__':
+	uploadModel = UploadModelScript()
+	uploadModel.start()
+	uploadModel.close()
 
 
--- appion/lib/apUpload.py
+++ appion/lib/apUpload.py
@@ -25,11 +25,6 @@ def printPrtlUploadHelp():
 	print "\n"
 	sys.exit(1)
 
-def printModelUploadHelp():
-	print "\nUsage:\nuploadModel.py <filename> session=<session> symmetry=<sym id> apix=<pixel> res=<resolution> [contour=<n>] [zoom=<n>] description=<\"text\"> [rescale=<model ID,scale factor>] [boxsize=<n>]\n"
-	print "uploadModel.py lambda.mrc symmetry=1 apix=2.02 res=25 description=\"CCMV in EMAN orientation\"\n"
-	sys.exit(1)
-
 def printMiscUploadHelp():
 	print "\nUsage:\nuploadMisc.py <filename> reconid=<n> session=<session> description=<\"text\">\n"
 	print "uploadMisc.py cpmv_cross_section.png reconid=311 description=\"cpmv cross section with pdb docked in\"\n"
@@ -71,57 +66,6 @@ def parsePrtlUploadInput(args,params):
 		else:
 			apDisplay.printError("undefined parameter \'"+arg+"\'\n")
 
-def parseModelUploadInput(args,params):
-	# check that there are enough input parameters
-	if (len(args)<2 or args[1]=='help') :
-		printModelUploadHelp()
-	# get MRC file
-	mrcfile=args[1]
-	# save the input parameters into the "params" dictionary
-	for arg in args[2:]:
-		elements=arg.split('=')
-		if (elements[0]=='apix'):
-			params['apix']=float(elements[1])
-		elif (elements[0]=='session'):
-			params['session']=elements[1]
-		elif (elements[0]=='outdir'):
-			params['outdir']=elements[1]
-		elif (elements[0]=='nocommit'):
-			params['commit']=False
-		elif (elements[0]=='symmetry'):
-			params['sym']=int(elements[1])
-		elif (elements[0]=='res'):
-			params['res']=float(elements[1])
-		elif (elements[0]=='contour'):
-			params['contour']=float(elements[1])
-		elif (elements[0]=='zoom'):
-			params['zoom']=float(elements[1])
-		elif (elements[0]=='description'):
-			params['description']=elements[1]
-		elif (elements[0]=='boxsize'):
-			params['newbox']=int(elements[1])
-		elif (arg=='chimeraonly'):
-			params['chimeraonly']=True
-			params['commit']=False
-		elif (elements[0]=='rescale'):
-			modinfo=elements[1].split(',')
-			if len(modinfo) == 2:
-				params['origmodel']=modinfo[0]
-				params['newapix']=float(modinfo[1])
-				params['rescale']=True
-			else:
-				apDisplay.printError("rescale must include both the original model id and a scale factor")
-		else:
-			apDisplay.printError("undefined parameter \'"+arg+"\'\n")
-	# if not rescaling, make sure that the input model exists
-	if (os.path.isfile(mrcfile) or params['rescale'] is True):
-		(params['path'], params['name']) = os.path.split(mrcfile)
-		params['path'] = os.path.abspath(params['path'])
-		if not params['path']:
-			params['path']=params['abspath']
-	else:
-		apDisplay.printError("file '"+mrcfile+"' does not exist\n")
-	
 def parseMiscUploadInput(args,params):
 	# check that there are enough input parameters
 	if (len(args)<2 or args[1]=='help') :
@@ -170,13 +114,15 @@ def createDefaults():
 	params['newbox']=None
 	return params
 
-def checkSymInfo(params):
-	symid = appiondb.direct_query(appionData.ApSymmetryData, params['sym'])
-	if not symid:
+def getSymmetryData(symid, msg=True):
+	symdata = appiondb.direct_query(appionData.ApSymmetryData, symid)
+	if not symdata:
 		printSymmetries()
-		apDisplay.printError("no symmetry associated with this id\n")
-	apDisplay.printMsg("Selected symmetry group: "+str(symid['symmetry']))
-	params['syminfo'] = symid
+		apDisplay.printError("no symmetry associated with this id: "+str(symid))
+	if msg is True:
+		apDisplay.printMsg("Selected symmetry group: "
+			+apDisplay.colorString(str(symdata['symmetry']), "cyan"))
+	return symdata
 
 def getProjectId(params):
 	projectdata = project.ProjectData()
@@ -184,7 +130,7 @@ def getProjectId(params):
 	for i in projects.getall():
 		if i['name'] == params['session']:
 			params['projectId'] = i['projectId']
-	print params['session'],params['projectId']
+	print params['session'], params['projectId']
 	if not params['projectId']:
 		apDisplay.printError("no project associated with this session\n")
 	return
@@ -211,25 +157,27 @@ def printSymmetries():
 		)
 
 def insertModel(params):
-	print "inserting into database"
-	symid=appiondb.direct_query(appionData.ApSymmetryData,params['sym'])
-	if not symid:
+	apDisplay.printMsg("commiting model to database")
+	symdata=appiondb.direct_query(appionData.ApSymmetryData, params['sym'])
+	if not symdata:
 		apDisplay.printError("no symmetry associated with this id\n")		
-	params['syminfo']=symid
+	params['syminfo'] = symdata
 	modq=appionData.ApInitialModelData()
-	modq['project|projects|project']=params['projectId']
-	modq['path']= appionData.ApPathData(path=os.path.abspath(params['outdir']))
-	modq['name']=params['name']
-	modq['symmetry']=symid
-	modq['pixelsize']=params['apix']
-	modq['boxsize']=params['box']
-	modq['resolution']=params['res']
-	modq['hidden']=False
+	modq['project|projects|project'] = params['projectId']
+	modq['path'] = appionData.ApPathData(path=os.path.abspath(params['outdir']))
+	modq['name'] = params['name']
+	modq['symmetry'] = symdata
+	modq['pixelsize'] = params['newapix']
+	modq['boxsize'] = params['oldbox']
+	modq['resolution'] = params['res']
+	modq['hidden'] = False
 	filepath = os.path.join(params['outdir'], params['name'])
-	modq['md5sum']=apFile.md5sumfile(filepath)
-	modq['description']=params['description']
+	modq['md5sum'] = apFile.md5sumfile(filepath)
+	modq['description'] = params['description']
 	if params['commit'] is True:
 		appiondb.insert(modq)
+	else:
+		apDisplay.printWarning("not commiting model to database")
 
 def checkReconId(params):
 	reconinfo=appiondb.direct_query(appionData.ApRefinementRunData, params['reconid'])
