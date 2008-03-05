6cad573c7d553ff84b9e7ee1e5e751afdf8e165f
pulokas <pulokas>
2008-03-05 22:14:58 +0000

    PIL <-> numpy I/O functions

---
+++ pyami/pilio.py
@@ -0,0 +1,9 @@
+
+def readPILgrey(imfile):
+	im = Image.open(imfile)
+	im = im.convert('L')
+	width,height = im.size
+	s = im.tostring()
+	im = numpy.fromstring(s, numpy.uint8)
+	im.shape = height,width
+	return im
