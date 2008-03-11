ed8c651c4faadc4f4356581b9bf028285cb2777c
Neil Voss <vossman>
2008-03-11 21:23:04 +0000

    num func: numImagesInStack()

--- appion/lib/apFile.py
+++ appion/lib/apFile.py
@@ -18,3 +18,19 @@ def md5sumfile(fname):
 		m.update(d)
 	f.close()
 	return m.hexdigest()
+
+
+def numImagesInStack(imgfile):
+	"""
+	Find the number of images in an 
+	IMAGIC stack based on the filesize
+	"""
+	if imgfile[-4:] == '.hed':
+		numimg = int('%d' % (os.stat(imgfile)[6]/1024))
+	elif imgfile[-4:] == '.img':
+		numimg = int('%d' % (os.stat(imgfile)[6]/102400))
+	elif os.path.isfile(imgfile+'.hed'):
+		numimg = int('%d' % (os.stat(imgfile+'.hed')[6]/1024))
+	else:
+		apDisplay.printError("numImagesInStack requires an IMAGIC stack")
+	return numimg
