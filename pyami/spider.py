7dc61fd10bf36b6d0fbdd163bc09ec58c093bb54
Neil Voss <vossman>
2008-03-19 17:28:09 +0000

    add copyright info

--- pyami/spider.py
+++ pyami/spider.py
@@ -1,20 +1,39 @@
-# Spider Python Library: Spiderarray.py
-# Copyright (C) 2006  Health Research Inc.
-#
-# HEALTH RESEARCH INCORPORATED (HRI),
-# ONE UNIVERSITY PLACE, RENSSELAER, NY 12144-3455
-#
-# Email:  spider@wadsworth.org
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License as
-# published by the Free Software Foundation; either version 2 of the
-# License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
+"""
+Source material taken from the spipylib package at Wadsworth
+
+* Spiderarray.py
+http://www.wadsworth.org/spider_doc/spider/docs/python/spipylib/array.html
+
+AND
+
+* Spiderutils.py
+http://www.wadsworth.org/spider_doc/spider/docs/python/spipylib/library.html
+
+	# Spider Python Library: Spiderarray.py 
+	# Copyright (C) 2006  Health Research Inc.
+	#
+	# HEALTH RESEARCH INCORPORATED (HRI),
+	# ONE UNIVERSITY PLACE, RENSSELAER, NY 12144-3455
+	#
+	# Email:  spider@wadsworth.org
+	#
+	# This program is free software; you can redistribute it and/or
+	# modify it under the terms of the GNU General Public License as
+	# published by the Free Software Foundation; either version 2 of the
+	# License, or (at your option) any later version.
+	#
+	# This program is distributed in the hope that it will be useful,
+	# but WITHOUT ANY WARRANTY; without even the implied warranty of
+	# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	# General Public License for more details.
+
+Modified on March 14, 2008 by Neil Voss:
+* Merged Spiderutils.py and Spiderarray.py
+* Removed unnecessary DOC file stuff
+* Changed Numeric to numpy
+* Allowed read first image of a stack
+
+"""
 
 import sys, struct
 import numpy
