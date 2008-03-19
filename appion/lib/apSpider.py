8f3a3a8621ecd16954c1e49493d8addddbe94593
Neil Voss <vossman>
2008-03-19 17:54:01 +0000

    temporary commit

--- appion/lib/apSpider.py
+++ appion/lib/apSpider.py
@@ -128,13 +128,14 @@ def refFreeAlignParticles(stackfile, template, numpart, pixrad,
 	if dataext in stackfile:
 		stackfile = stackfile[:-4]
 	t0 = time.time()
-	apParam.createDirectory("alignment")
+	rundir = "alignment"
+	apParam.createDirectory(rundir)
 
 	### remove previous iterations
 	numiter = 0
-	while os.path.isfile("alignment/avgimg%03d%s" % (numiter+1, dataext)):
-		os.remove("alignment/avgimg%03d%s" % (numiter+1, dataext))
-		pngfile = "alignment/avgimg%03d%s" % (numiter+1, ".png")
+	while os.path.isfile(rundir+"/avgimg%03d%s" % (numiter+1, dataext)):
+		os.remove(rundir+"/avgimg%03d%s" % (numiter+1, dataext))
+		pngfile = rundir+"/avgimg%03d%s" % (numiter+1, ".png")
 		if os.path.isfile(pngfile):
 			os.remove(pngfile)
 		numiter += 1
@@ -146,15 +147,15 @@ def refFreeAlignParticles(stackfile, template, numpart, pixrad,
 	mySpider.toSpider("AP SR", 
 		stackfile+"@*****", "1-"+str(numpart), 
 		str(int(pixrad)), str(int(firstring))+","+str(int(lastring)), 
-		"_9", "alignment/avgimg***", "alignment/paramdoc***")
+		"_9", rundir+"/avgimg***", rundir+"/paramdoc***")
 	mySpider.close()
 
 	### find number of iterations
 	numiter = 0
-	while os.path.isfile("alignment/avgimg%03d%s" % (numiter+1, dataext)):
+	while os.path.isfile(rundir+"/avgimg%03d%s" % (numiter+1, dataext)):
 		emancmd = ("proc2d "
-			+" alignment/avgimg"+("%03d%s" % (numiter+1, dataext))
-			+" alignment/avgimg"+("%03d%s" % (numiter+1, ".png"))
+			+" "+rundir+"/avgimg"+("%03d%s" % (numiter+1, dataext))
+			+" "+rundir+"/avgimg"+("%03d%s" % (numiter+1, ".png"))
 		)
 		apEMAN.executeEmanCmd(emancmd, verbose=False, showcmd=False)
 		numiter += 1
@@ -162,13 +163,78 @@ def refFreeAlignParticles(stackfile, template, numpart, pixrad,
 		apDisplay.printError("alignment failed")
 	apDisplay.printMsg(str(numiter)+" alignment iterations were run by spider")
 
+	### convert spider rotation, shift data to python
+
 	### write aligned stack -- with python loop
 	### I tried this loop in both spider and python: python was faster?!? -neil
 	mySpider = spyder.SpiderSession(dataext=dataext, logo=False)
 	for p in range(1,numpart+1):
 		mySpider.toSpiderQuiet(
 			"UD IC,"+str(p)+",x21,x22,x23",
-			("alignment/paramdoc%03d" % (numiter)),
+			(rundir+"/paramdoc%03d" % (numiter)),
+			"RT SQ",
+			stackfile+"@"+("%05d" % (p)),
+			"alignedstack@"+("%05d" % (p)),
+			"x21", "x22,x23")
+	mySpider.close()
+	td1 = time.time()-t0
+
+	apDisplay.printMsg("completed alignment of "+str(numpart)
+		+" particles in "+apDisplay.timeString(td1))
+
+	return "alignedstack.spi"
+
+#===============================
+def refBasedAlignParticles(stackfile, templatestack, 
+		xysearch, xystep, 
+		numpart, numtemplate,
+		firstring=2, lastring=100, 
+		dataext=".spi"):
+	"""
+	inputs:
+		stack
+		template
+		search params
+	outputs:
+		aligned stack
+		rotation/shift params
+	"""
+	### setup
+	if dataext in template:
+		template = template[:-4]
+	if dataext in stackfile:
+		stackfile = stackfile[:-4]
+	t0 = time.time()
+	rundir = "alignment"
+	apParam.createDirectory(rundir)
+
+	### remove previous iterations
+
+	### perform alignment
+	mySpider = spyder.SpiderSession(dataext=dataext)
+	# copy template to memory
+	mySpider.toSpider("AP MQ", 
+		templatestack+"@***",                       # reference image series
+		"1-"+str(numtemplate),                      # enter number of templates of doc file
+		str(int(xysearch))+","+str(int(xystep)),    # translation search range, step size
+		str(int(firstring))+","+str(int(lastring)), # first and last ring for rotational correlation
+		stackfile+"@*****",                         # unaligned image series
+		"1-"+str(numpart),                          # enter number of particles of doc file
+		rundir+"/paramdoc***",                      # output angles document file
+	)
+	mySpider.close()
+
+	### find number of iterations
+
+	### convert spider rotation, shift data to python
+
+	### write aligned stack -- with python loop
+	### I tried this loop in both spider and python: python was faster?!? -neil
+	mySpider = spyder.SpiderSession(dataext=dataext, logo=False)
+	for p in range(1,numpart+1):
+		mySpider.toSpiderQuiet(
+			"UD IC,"+str(p)+",x21,x22,x23",
+			(rundir+"/paramdoc%03d" % (numiter)),
 			"RT SQ",
 			stackfile+"@"+("%05d" % (p)),
 			"alignedstack@"+("%05d" % (p)),
@@ -196,7 +262,8 @@ def correspondenceAnalysis(alignedstack, boxsize, maskrad, numpart, numfactors=2
 	if dataext in alignedstack:
 		alignedstack = alignedstack[:-4]
 	t0 = time.time()
-	apParam.createDirectory("coran")
+	rundir = "coran"
+	apParam.createDirectory(rundir)
 
 	### make template in memory
 	mySpider = spyder.SpiderSession(dataext=dataext)
@@ -208,13 +275,13 @@ def correspondenceAnalysis(alignedstack, boxsize, maskrad, numpart, numfactors=2
 		"CA S",
 		alignedstack+"@*****", "1-"+str(numpart),
 		"_9", str(numfactors), "C", "10",
-		"coran/corandata")
+		rundir+"/corandata")
 
 	### generate eigen images
 	for fact in range(1,numfactors+1):
 		mySpider.toSpiderQuiet(
-			"CA SRE", "coran/corandata", str(fact), 
-			"coran/eigenimg@"+("%03d" % (fact)), )
+			"CA SRE", rundir+"/corandata", str(fact), 
+			rundir+"/eigenimg@"+("%03d" % (fact)), )
 	mySpider.close()
 
 	"""
@@ -235,10 +302,10 @@ def correspondenceAnalysis(alignedstack, boxsize, maskrad, numpart, numfactors=2
 	"""
 	### remove worthless temporary files
 	for tail in ["_SEQ", "_SET", "_MAS", "_PIX",]:
-		if os.path.isfile("coran/corandata"+tail+dataext):
-			os.remove("coran/corandata"+tail+dataext)
+		if os.path.isfile(rundir+"/corandata"+tail+dataext):
+			os.remove(rundir+"/corandata"+tail+dataext)
 
-	eigf = open("coran/corandata_EIG"+dataext, "r")
+	eigf = open(rundir+"/corandata_EIG"+dataext, "r")
 	count = 0
 	for line in eigf:
 		bits = line.strip().split()
@@ -248,17 +315,17 @@ def correspondenceAnalysis(alignedstack, boxsize, maskrad, numpart, numfactors=2
 
 	### make nice pngs for webpage
 	for fact in range(1,numfactors+1):
-		emancmd = ("proc2d coran/eigenimg.spi "
-			+"coran/eigenimg"+("%03d" % (fact))+".png "
+		emancmd = ("proc2d "+rundir+"/eigenimg.spi "
+			+rundir+"/eigenimg"+("%03d" % (fact))+".png "
 			+" first="+str(fact-1)+" last="+str(fact-1))
 		apEMAN.executeEmanCmd(emancmd, verbose=False, showcmd=False)
 
 	### convert SPIDER image to IMAGIC for webpage
-	eigenimg = "coran/eigenimg.hed"
+	eigenimg = rundir+"/eigenimg.hed"
 	if os.path.isfile(eigenimg):
 		os.remove(eigenimg)
 		os.remove(eigenimg[:-4]+".img")
-	emancmd = "proc2d coran/eigenimg.spi "+eigenimg
+	emancmd = "proc2d "+rundir+"/eigenimg.spi "+eigenimg
 	apEMAN.executeEmanCmd(emancmd, verbose=False, showcmd=True)
 
 	td1 = time.time()-t0
