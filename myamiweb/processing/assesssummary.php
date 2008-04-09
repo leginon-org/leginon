6a8a1635ff2eabd2af604e259fdf35cf8e7d248a
Neil Voss <vossman>
2008-04-09 19:30:36 +0000

    find actual hidden/exemplar images not only those assess

--- myamiweb/processing/assesssummary.php
+++ myamiweb/processing/assesssummary.php
@@ -39,7 +39,9 @@ $particle = new particledata();
 
 echo"<P>\n";
 $totimgs = $particle->getNumImgsFromSessionId($sessionId);
+$viewerimgs = $particle->getNumViewerPrefImages($sessionId);
 $assessedimgs = $particle->getNumAssessedImages($sessionId);
+$totalassessimgs = $particle->getNumTotalAssessImages($sessionId);
 
 $rejectimgs = $particle->getNumRejectAssessedImages($sessionId);
 $hiddenimgs = $particle->getNumHiddenImages($sessionId);
@@ -52,15 +54,18 @@ $totalkeepimgs = $particle->getNumTotalKeepImages($sessionId);
 
 echo "<table class='tableborder' border='1' cellspacing='1' cellpadding='5'>\n";
 echo "<TR><TD>Particle Picked Images: </TD><TD>".$totimgs."</TD></TR>\n";
-echo "<TR><TD>Assessed Images: </TD><TD>".$assessedimgs."</TD></TR>\n";
 echo "<TR><TD COLSPAN='2' BGCOLOR='#CCCCCC'></TD></TR>";
-echo "<TR><TD>Rejected Images: </TD><TD>".$rejectimgs." (".round(100.0*$rejectimgs/$assessedimgs,1)."%)</TD></TR>\n";
-echo "<TR><TD>Hidden Images: </TD><TD>".$hiddenimgs." (".round(100.0*$hiddenimgs/$assessedimgs,1)."%)</TD></TR>\n";
-echo "<TR><TD><B>Total Rejected Images:</B> </TD><TD>".$totalrejectimgs." (".round(100.0*$totalrejectimgs/$assessedimgs,1)."%)</TD></TR>\n";
+echo "<TR><TD>Assessed Images: </TD><TD>".$assessedimgs." (".round(100.0*$assessedimgs/$totimgs,1)."%)</TD></TR>\n";
+echo "<TR><TD>Hidden/Exemplar Images: </TD><TD>".$viewerimgs." (".round(100.0*$viewerimgs/$totimgs,1)."%)</TD></TR>\n";
+echo "<TR><TD><B>Total Assess Images:</B> </TD><TD>".$totalassessimgs." (".round(100.0*$totalassessimgs/$totimgs,1)."%)</TD></TR>\n";
 echo "<TR><TD COLSPAN='2' BGCOLOR='#CCCCCC'></TD></TR>";
-echo "<TR><TD>Keep Images: </TD><TD>".$keepimgs." (".round(100.0*$keepimgs/$assessedimgs,1)."%)</TD></TR>\n";
-echo "<TR><TD>Exemplar Images: </TD><TD>".$exemplarimgs." (".round(100.0*$exemplarimgs/$assessedimgs,1)."%)</TD></TR>\n";
-echo "<TR><TD><B>Total Keep Images:</B> </TD><TD>".$totalkeepimgs." (".round(100.0*$totalkeepimgs/$assessedimgs,1)."%)</TD></TR>\n";
+echo "<TR><TD>Rejected Images: </TD><TD>".$rejectimgs." (".round(100.0*$rejectimgs/$totimgs,1)."%)</TD></TR>\n";
+echo "<TR><TD>Hidden Images: </TD><TD>".$hiddenimgs." (".round(100.0*$hiddenimgs/$totimgs,1)."%)</TD></TR>\n";
+echo "<TR><TD><B>Total Rejected Images:</B> </TD><TD>".$totalrejectimgs." (".round(100.0*$totalrejectimgs/$totimgs,1)."%)</TD></TR>\n";
+echo "<TR><TD COLSPAN='2' BGCOLOR='#CCCCCC'></TD></TR>";
+echo "<TR><TD>Keep Images: </TD><TD>".$keepimgs." (".round(100.0*$keepimgs/$totimgs,1)."%)</TD></TR>\n";
+echo "<TR><TD>Exemplar Images: </TD><TD>".$exemplarimgs." (".round(100.0*$exemplarimgs/$totimgs,1)."%)</TD></TR>\n";
+echo "<TR><TD><B>Total Keep Images:</B> </TD><TD>".$totalkeepimgs." (".round(100.0*$totalkeepimgs/$totimgs,1)."%)</TD></TR>\n";
 
 echo "</TABLE>";
 
