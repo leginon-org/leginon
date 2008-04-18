f84ec0715cb3b5195a6bfb440dbacfa0b161668f
pulokas <pulokas>
2008-04-18 00:27:53 +0000

    script to count particles

---
+++ appion/bin/pcount.py
@@ -0,0 +1,50 @@
+'''
+This will count the number of particles and tell you how many are from
+reject images versus other images.
+usage:    pcount.py <session name> <particle selection run name>
+'''
+#!/usr/bin/env python
+
+import appionData
+import leginondata
+import sys
+
+ses_name = sys.argv[1]
+run_name = sys.argv[2]
+
+# query objects
+qsession = leginondata.SessionData(name=ses_name)
+qrun = appionData.ApSelectionRunData(session=qsession, name=run_name)
+qpart = appionData.ApParticleData(selectionrun=qrun)
+
+# all particles
+particles = qpart.query()
+
+# Find assessment of image that each particle is from.
+# This is messy because we want to query images without reading MRCs.
+assessments = {}
+akeep = []
+areject = []
+anone = []
+for particle in particles:
+	imgref = particle.special_getitem('image', dereference=False)
+	imgid = imgref.dbid
+	if imgid not in assessments:
+		img = leginondata.AcquisitionImageData.direct_query(imgref.dbid, readimages=False)
+		qassess = appionData.ApAssessmentData(image=img)
+		assessment = qassess.query(results=1)
+		if assessment:
+			assessments[imgid] = assessment[0]['selectionkeep']
+		else:
+			assessments[imgid] = None
+	assessment = assessments[imgid]
+	if assessment is None:
+		anone.append(particle)
+	elif assessment:
+		akeep.append(particle)
+	else:
+		areject.append(particle)
+
+print ' Total Particles:', len(particles)
+print '         Rejected', len(areject)
+print '  Keep or Unknown', len(akeep) + len(anone)
