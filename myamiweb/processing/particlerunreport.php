e7fe92395cce2ff60f0126c0fbac3dd63a710a37
Gabriel Lander <glander>
2008-05-23 23:29:05 +0000

    - incorporated header

--- myamiweb/processing/particlerunreport.php
+++ myamiweb/processing/particlerunreport.php
@@ -1,38 +1,31 @@
 <?php
 require "inc/particledata.inc";
-require "inc/util.inc";
+require "inc/viewer.inc";
+require "inc/processing.inc";
+//require "inc/util.inc";
 require "inc/leginon.inc";
+require "inc/project.inc";
 
-$sessionId= $_GET['Id'];
+$expId = $_GET['expId'];
 $runId = $_GET['rId'];
 
 $particle = new particledata();
 
-?>
-
-<html>
-<head>
-<title>Particle Run Report</title>
-<link rel="stylesheet" type="text/css" href="../css/viewer.css">
-</head>
-<body>
-
-<?php
 list($runparams) = $particle->getSelectionParams($runId);
 $templateparams = $particle->getTemplateRunParams($runId);
-echo divtitle("Particle report for $runparams[name]");
+
+processing_header("Particle run report","Particle report for $runparams[name]");
 
 $particlestats = $particle->getStats($runId);
-echo "<br><table cellspacing='1' cellpadding='2'><tr><td><span class='datafield0'>Total particles for $runparams[name]: </span></td><td>$particlestats[totparticles]</td></tr></table>\n";
+echo "<table cellspacing='1' cellpadding='2'><tr><td><span class='datafield0'>Total particles for $runparams[name]: </span></td><td>$particlestats[totparticles]</td></tr></table>\n";
 //Report template run parameters
 if (count($templateparams) > 0) {
 	$template_fields = array('id', 'apix', 'description', 'range_start', 'range_end', 'range_incr');
 	echo "<h4>Template images and parameters</h4>\n";
 	echo "<table>";
-	echo "<tr>";
 	foreach($templateparams as $template){
 		$templatepath=$template[path]."/".$template[tname];
-		echo '<td>';
+		echo '<tr><td>';
 			echo "<table class='tableborder' border='1' cellspacing='1' cellpadding='2'>";
 			echo "<tr>\n";
 				echo "<td colspan='2'><IMG alt='$templatepath' SRC='loadimg.php?filename=$templatepath&rescale=True' WIDTH='100'></td>\n";
@@ -46,9 +39,8 @@ if (count($templateparams) > 0) {
 				}
 			}
 			echo "</table>\n";
-		echo "</td>";
+		echo "</td></tr>";
 	}
-	echo "</tr>";
 	echo "</table>";
 }
 
@@ -57,6 +49,4 @@ $title = "Selection parameters";
 $exclude_fields = array('DEF_timestamp');
 $particle->displayParameters($title,$runparams,$exclude_fields);
 
-?>
-</body>
-</html>
+processing_footer();
