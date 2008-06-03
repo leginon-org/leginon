c88f293afe0416349bc971aacaa14be7370a59e5
Gabriel Lander <glander>
2008-06-03 00:39:33 +0000

    - edit description

--- myamiweb/processing/viewmodels.php
+++ myamiweb/processing/viewmodels.php
@@ -1,45 +1,65 @@
 <?php
 require"inc/particledata.inc";
-require"inc/viewer.inc";
-require"inc/processing.inc";
 require"inc/leginon.inc";
 require"inc/project.inc";
+require"inc/viewer.inc";
+require"inc/processing.inc";
 
 $expId= $_GET['expId'];
 $particle = new particledata();
 $projectId=getProjectFromExpId($expId);
+$formAction=$_SERVER['PHP_SELF']."?expId=$expId";
 
 $models = $particle->getModelsFromProject($projectId);
 
-processing_header("Initial Models","Initial Models");
+$javascript = editTextJava();
+
+processing_header("Initial Models","Initial Models",$javascript);
+
+// edit description form
+echo "<form name='modelform' method='post' action='$formAction'>\n";
 
 foreach ($models as $model) {
-  echo "<TABLE CLASS='tableborder' BORDER='1' CELLSPACING='1' CELLPADDING='2'>\n";
-  # get list of png files in directory
-  $pngfiles=array();
-  $modeldir= opendir($model['path']);
-  while ($f = readdir($modeldir)) {
-    if (eregi($model['name'].'.*\.png$',$f)) $pngfiles[] = $f;
-  }
-  sort($pngfiles);
+	$modelid = $model['DEF_id'];
+	// get updated description
+	if ($_POST['updateDesc'.$modelid]) {
+		updateDescription('ApInitialModelData', $modelid, $_POST['newdescription'.$modelid]);
+		$model['description']=$_POST['newdescription'.$modelid];
+	}
+
+	echo "<table class='tableborder' border='1' cellspacing='1' cellpadding='2'>\n";
+	# get list of png files in directory
+	$pngfiles=array();
+	$modeldir= opendir($model['path']);
+	while ($f = readdir($modeldir)) {
+		if (eregi($model['name'].'.*\.png$',$f)) $pngfiles[] = $f;
+	}
+	sort($pngfiles);
   
-  # display starting model
-  echo "<TR><TD COLSPAN=2>\n";
-  echo "<B>Model ID: ".$model['DEF_id']."</B><BR>\n";
-  foreach ($pngfiles as $snapshot) {
-    $snapfile = $model['path'].'/'.$snapshot;
-    echo "<A HREF='loadimg.php?filename=$snapfile' target='snapshot'><IMG SRC='loadimg.php?filename=$snapfile' HEIGHT='80'>\n";
-  }
-  echo "</TD>\n";
-  echo "</TR>\n";
-  $sym=$particle->getSymInfo($model['REF|ApSymmetryData|symmetry']);
-  echo"<TR><TD COLSPAN=2>$model[description]</TD></TR>\n";
-  echo"<TR><TD COLSPAN=2>$model[path]/$model[name]</TD></TR>\n";
-  echo"<TR><TD>pixel size:</TD><TD>$model[pixelsize]</TD></TR>\n";
-  echo"<TR><TD>box size:</TD><TD>$model[boxsize]</TD></TR>\n";
-  echo"<TR><TD>symmetry:</TD><TD>$sym[symmetry]</TD></TR>\n";
-  echo"<TR><TD>resolution:</TD><TD>$model[resolution]</TD></TR>\n";
-  echo "</TABLE>\n";
+# display starting model
+	echo "<TR><TD COLSPAN=2>\n";
+	echo "<B>Model ID: $modelid</b><br />\n";
+	foreach ($pngfiles as $snapshot) {
+		$snapfile = $model['path'].'/'.$snapshot;
+		echo "<A HREF='loadimg.php?filename=$snapfile' target='snapshot'><IMG SRC='loadimg.php?filename=$snapfile' HEIGHT='80'>\n";
+	}
+	echo "</TD>\n";
+	echo "</TR>\n";
+	$sym=$particle->getSymInfo($model['REF|ApSymmetryData|symmetry']);
+
+	# add edit button to description if logged in
+	$descDiv = ($_SESSION['username']) ? editButton($modelid,$model['description']) : $model['description'];
+	
+	echo"<TR><TD COLSPAN=2>$descDiv</TD></TR>\n";
+	echo"<TR><TD COLSPAN=2>$model[path]/$model[name]</TD></TR>\n";
+	echo"<TR><TD>pixel size:</TD><TD>$model[pixelsize]</TD></TR>\n";
+	echo"<TR><TD>box size:</TD><TD>$model[boxsize]</TD></TR>\n";
+	echo"<TR><TD>symmetry:</TD><TD>$sym[symmetry]</TD></TR>\n";
+	echo"<TR><TD>resolution:</TD><TD>$model[resolution]</TD></TR>\n";
+	echo "</table>\n";
 }
 
+echo "</form>\n";
 processing_footer();
+
+?>
