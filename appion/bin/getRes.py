077681e53e59f98aa49fe0f765f5e3254aa8ea7d
Neil Voss <vossman>
2008-06-27 21:05:49 +0000

    same as method perl script but in python

---
+++ appion/bin/getRes.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python -O
+
+import appionScript
+import apDisplay
+import os
+import apRecon
+
+class getResScript(appionScript.AppionScript):
+
+	#=====================
+	def setupParserOptions(self):
+		self.quiet = True
+		self.parser.set_usage("Usage: %prog --fscfile=FILE --boxsize=INT --apix=FLOAT")
+		self.parser.add_option("-f", "--fscfile", dest="fscfile",
+			help="FSC file from EMAN", metavar="FILE")
+		self.parser.add_option("-o", "--outdir", dest="outdir",
+			help="Output directory", metavar="PATH")
+		self.parser.add_option("-b", "--boxsize", dest="boxsize", type="int",
+			help="Box size in pixels", metavar="INT")
+		self.parser.add_option("-a", "--apix", dest="apix", type="float",
+			help="Angstroms per pixels", metavar="FLOAT")
+
+
+	#=====================
+	def checkConflicts(self):
+		if self.params['fscfile'] is None:
+			apDisplay.printError("fscfile was not defined")
+		if not os.path.isfile(self.params['fscfile']):
+			apDisplay.printError("fscfile "+self.params['fscfile']+" was not found")
+		if self.params['boxsize'] is None:
+			apDisplay.printError("Box size was not defined")
+		if self.params['apix'] is None:
+			apDisplay.printError("Angstroms per pixels was not defined")
+
+	#=====================
+	def setOutDir(self):
+		self.params['outdir'] = os.getcwd()
+
+	#=====================
+	def start(self):
+		res = apRecon.getResolutionFromFSCFile(self.params['fscfile'], self.params['boxsize'], self.params['apix'])
+		apDisplay.printColor( ("resolution: %.5f" % (res)), "cyan")
+
+
+#=====================
+if __name__ == "__main__":
+	getRes = getResScript()
+	getRes.start()
+	getRes.close()
+
