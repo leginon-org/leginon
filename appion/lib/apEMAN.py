1de1d5bc29625fbc401cd1894ce7218c7148844f
Neil Voss <vossman>
2008-06-29 23:28:44 +0000

    add waittime

--- appion/lib/apEMAN.py
+++ appion/lib/apEMAN.py
@@ -26,13 +26,56 @@ def executeEmanCmd(emancmd, verbose=False, showcmd=True, logfile=None):
 			emanproc = subprocess.Popen(emancmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 		else:
 			emanproc = subprocess.Popen(emancmd, shell=True)
-		emanproc.wait()
+		if verbose is True:
+			emanproc.wait()
+		else:
+			### continuous check
+			waittime = 15.0
+			while emanproc.poll() is None:
+				sys.stderr.write(".")
+				waittime *= 1.1
+				time.sleep(waittime)
 	except:
 		apDisplay.printWarning("could not run eman command: "+emancmd)
 		raise
 	tdiff = time.time() - t0
 	if tdiff > 20:
 		apDisplay.printMsg("completed in "+apDisplay.timeString(tdiff))
+
+
+
+	def wait(self):
+		### waits until spider quits
+
+		### set wait times
+		if self.logo is True:
+			waittime = 15.0
+		else:
+			waittime = 2.0
+		self.logf.flush()
+		### check number 1
+		if self.spiderproc.poll() is None:
+			waiting = True
+			time.sleep(2.0)
+		else:
+			self.spiderproc.wait()
+			return
+		### check number 2
+		if self.spiderproc.poll() is None:
+			waiting = True
+			sys.stderr.write("waiting for spider")
+		else:
+			self.spiderproc.wait()
+			return
+		### continuous check
+		while self.spiderproc.poll() is None:
+			sys.stderr.write(".")
+			time.sleep(waittime)
+			waittime *= 1.1
+			self.logf.flush()
+		if waiting is True:
+			sys.stderr.write("\n")
+		self.spiderproc.wait()
 
 #=====================
 def getNumParticlesInStack(stackname):
