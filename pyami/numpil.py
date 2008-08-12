e6140f347a1a9bb459e33783241b38f35f0d778c
pulokas <pulokas>
2008-08-12 22:24:30 +0000

    improved mrc2any

--- pyami/numpil.py
+++ pyami/numpil.py
@@ -4,47 +4,20 @@ import imagefun
 import arraystats
 import sys
 
-pilformats = '''
-BMP
-BUFR (identify only)
-CUR (read only)
-DCX (read only)
-EPS (write-only)
-FITS (identify only)
-FLI, FLC (read only)
-FPX (read only)
-GBR (read only)
-GD (read only)
-GIF
-GRIB (identify only)
-HDF5 (identify only)
-ICO (read only)
-IM
-IMT (read only)
-IPTC/NAA (read only)
-JPEG
-MCIDAS (read only)
-MIC (read only)
-MPEG (identify only)
-MSP
-PALM (write only)
-PCD (read only)
-PCX
-PDF (write only)
-PIXAR (read only)
-PNG
-PPM
-PSD (read only)
-SGI (read only)
-SPIDER
-TGA (read only)
-TIFF
-WAL (read only)
-WMF (identify only)
-XBM
-XPM (read only)
-XV Thumbnails
-'''
+pilformats = [
+	'BMP',
+	'GIF',
+	'IM',
+	'JPEG',
+	'MSP',
+	'PCX',
+	'PDF',
+	'PNG',
+	'PPM',
+	'SPIDER',
+	'TIFF',
+	'XBM',
+]
 
 def read(imfile):
 	'''
@@ -58,15 +31,16 @@ def read(imfile):
 	im.shape = height,width
 	return im
 
-def write(a, imfile=None, format=None):
+def write(a, imfile=None, format=None, limits=None):
 	'''
 	Convert array to 8 bit gray scale and save to filename.
 	Format is determined from filename extension by PIL.
 	'''
-	mean = arraystats.mean(a)
-	std = arraystats.std(a)
-	limit = (mean-3*std, mean+3*std)
-	a = imagefun.linearscale(a, limit, (0,255))
+	if limits is None:
+		mean = arraystats.mean(a)
+		std = arraystats.std(a)
+		limits = (mean-3*std, mean+3*std)
+	a = imagefun.linearscale(a, limits, (0,255))
 	a = a.clip(0,255)
 	a = numpy.asarray(a, numpy.uint8)
 	size = a.shape[1], a.shape[0]
@@ -77,4 +51,4 @@ def write(a, imfile=None, format=None):
 		im.save(imfile, format=format)
 	except KeyError:
 		## bad file format
-		sys.stderr.write('Bad PIL image format.  Try one of these: %s' % (pilformats,))
+		sys.stderr.write('Bad PIL image format.  Try one of these: %s\n' % (pilformats,))
