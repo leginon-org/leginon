2c1260b47bdbe6fab274428e25304cc8a90446ef
Neil Voss <vossman>
2008-09-17 20:15:21 +0000

    removing unused stuff

--- appion/lib/apSpider/backproject.py
+++ appion/lib/apSpider/backproject.py
@@ -118,6 +118,7 @@ def projectVolume(volfile, eulerdocfile, projstackfile, numpart, pixrad, dataext
 	if dataext in projstackfile:
 		projstackfile = projstackfile[:-4]
 
+	apFile.removeFile(projstackfile)
 	mySpider = spyder.SpiderSession(dataext=dataext, logo=True)
 	mySpider.toSpider("PJ 3Q", 
 		volfile, #input vol file
@@ -132,61 +133,6 @@ def projectVolume(volfile, eulerdocfile, projstackfile, numpart, pixrad, dataext
 
 #===============================
 def crossCorrelateAndShift(infile, reffile, alignfile, ccdocfile, partnum, dataext=".spi"):
-	### rewrite this and do the whole thing in memory in SPIDER, it'll be faster
-
-	if dataext in infile:
-		infile = infile[:-4]
-	if dataext in reffile:
-		reffile = reffile[:-4]
-	if dataext in alignfile:
-		alignfile = alignfile[:-4]
-	tempccfile = "tempccfile"
-	### cross correlate images
-	mySpider = spyder.SpiderSession(dataext=dataext, logo=False)
-	mySpider.toSpiderQuiet("CC N", 
-		infile+("@%05d"%(partnum)), #picture
-		reffile+("@%05d"%(partnum)), #reference
-		tempccfile, #output file
-	)
-	mySpider.close()
-
-	### find pixel peak from cc map
-	if not os.path.isfile(tempccfile+dataext):
-		mySpider = spyder.SpiderSession(dataext=dataext, logo=False)
-		mySpider.toSpiderQuiet("CP", 
-			infile+("@%05d"%(partnum)), #old stack
-			alignfile+("@%05d"%(partnum)), #new stack
-		)
-		mySpider.close()
-		return
-	ccmap = spider.read(tempccfile+dataext)
-	peak = peakfinder.findSubpixelPeak(ccmap, lpf=1.5)
-	subpixpeak = numpy.asarray(peak['subpixel peak'], dtype=numpy.float32)
-	shift = subpixpeak - numpy.asarray(ccmap.shape, dtype=numpy.float32)/2.0
-	### cannot shift more the 1/4 size of the image
-	if abs(shift[0]) > ccmap.shape[0]/4.0:
-		shift[0] = ccmap.shape[0]/4.0*shift[0]/abs(shift[0])
-	if abs(shift[1]) > ccmap.shape[1]/4.0:
-		shift[1] = ccmap.shape[1]/4.0*shift[1]/abs(shift[1])
-	apFile.removeFile(tempccfile+dataext)
-	f = open(ccdocfile, "a")
-	spiline = operations.spiderOutputLine3(partnum, shift[0], shift[1], 0.0)
-	f.write(spiline)
-	f.close()
-
-	### shift the images images
-	mySpider = spyder.SpiderSession(dataext=dataext, logo=False)
-	mySpider.toSpiderQuiet("SH", 
-		infile+("@%05d"%(partnum)), #old stack
-		alignfile+("@%05d"%(partnum)), #new stack
-		"%.3f,%.3f"%(shift[0], shift[1]), #output file
-	)
-	mySpider.close()
-	return
-
-
-#===============================
-def crossCorrelateAndShift2(infile, reffile, alignfile, ccdocfile, partnum, dataext=".spi"):
 	### rewriten to do the whole thing in memory in SPIDER, it should be faster
 	if dataext in infile:
 		infile = infile[:-4]
@@ -237,7 +183,6 @@ def crossCorrelateAndShift2(infile, reffile, alignfile, ccdocfile, partnum, data
 	mySpider.close()
 	return
 
-
 #===============================
 def rctParticleShift(volfile, origstackfile, eulerdocfile, iternum, numpart, pixrad, dataext=".spi"):
 	"""
@@ -251,11 +196,13 @@ def rctParticleShift(volfile, origstackfile, eulerdocfile, iternum, numpart, pix
 	projstackfile = "projstack%03d.spi"%(iternum)
 	projectVolume(volfile, eulerdocfile, projstackfile, numpart, pixrad, dataext)
 
-	### align particles to projection
+	### clean up files
 	ccdocfile = "ccdocfile%03d.spi"%(iternum)
 	apFile.removeFile(ccdocfile)
 	alignstackfile = "alignstack%03d.spi"%(iternum)
 	apFile.removeFile(alignstackfile)
+
+	### align particles to projection
 	apDisplay.printMsg("Shifting particles")
 	starttime = time.time()
 	partnum = 0
@@ -264,7 +211,7 @@ def rctParticleShift(volfile, origstackfile, eulerdocfile, iternum, numpart, pix
 		if partnum%50 == 0:
 			esttime = float(time.time()-starttime)/float(partnum)*float(numpart-partnum)
 			print "partnum=", partnum, "--", apDisplay.timeString(esttime), "remain"
-		crossCorrelateAndShift2(origstackfile, projstackfile, alignstackfile, ccdocfile, partnum)
+		crossCorrelateAndShift(origstackfile, projstackfile, alignstackfile, ccdocfile, partnum)
 	apDisplay.printColor("finished correlations in "+apDisplay.timeString(time.time()-starttime), "cyan")
 	return alignstackfile
 
