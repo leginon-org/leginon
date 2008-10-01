26a24f5f0685ee063cdffe0e83843998870182a0
pulokas <pulokas>
2008-10-01 23:09:28 +0000

    now using wxpython

--- pyami/plot.py
+++ pyami/plot.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
 
-import pylab
+import wxmpl
 
-def plot(*args, **kwargs):
-	pylab.plot(*args, **kwargs)
-	pylab.show()
+class PlotPanel(wxmpl.PlotPanel):
+	def plot(self, *args):
+		if len(args) == 1:
+			x = range(len(args[0]))
+			y = args[0]
+		elif len(args) == 2:
+			x = args[0]
+			y = args[1]
+		fig = self.get_figure()
+		axes = fig.gca()
+		axes.plot(x,y)
 
 if __name__ == '__main__':
 	import mrc
@@ -12,6 +20,7 @@ if __name__ == '__main__':
 	import numextension
 	import imagefun
 	import numpil
+	import wx
 	a = mrc.read(sys.argv[1])
 	low = float(sys.argv[2])
 	high = float(sys.argv[3])
@@ -20,5 +29,10 @@ if __name__ == '__main__':
 	#pow = imagefun.power(a)
 	#numpil.write(pow, 'pow.png')
 	b = numextension.radialPower(a, low, high)
-	plot(b)
-	raw_input('enter to quit')
+
+	app = wx.PySimpleApp()
+	frame = wx.Frame(None, -1, title='My Plot')
+	plotpanel = PlotPanel(frame, -1)
+	plotpanel.plot(b)
+	frame.Show()
+	app.MainLoop()
