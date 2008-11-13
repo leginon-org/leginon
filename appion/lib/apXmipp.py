e5abf1b64c66086dac2aa25716b214ef6b1f673e
Neil Voss <vossman>
2008-11-13 20:44:43 +0000

    new file for Xmipp stuff

---
+++ appion/lib/apXmipp.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+#
+import os
+import time
+import sys
+import random
+import math
+import shutil
+#appion
+import apDisplay
+import apFile
+import apParam
+import apEMAN
+import apTemplate
+import apStack
+from pyami import spider
+
+def breakupStackIntoSingleFiles(stackfile, partdir="partfiles"):
+	"""
+	takes the stack file and creates single spider files ready for processing
+	"""
+	apDisplay.printColor("Breaking up spider stack into single files, this can take a while", "cyan")
+
+	starttime = time.time()
+	filesperdir = 892
+	numpart = apFile.numImagesInStack(stackfile)
+	apParam.createDirectory(partdir)
+	if numpart > filesperdir:
+		numdir = createSubFolders(partdir, numpart, filesperdir)
+		filesperdir = int(math.ceil(numpart/float(numdir)))
+		apDisplay.printMsg("Splitting "+str(numpart)+" particles into "+str(numdir)+" folders with "
+			+str(filesperdir)+" particles per folder")
+		
+		subdir = 1
+	else:
+		subdir = "."
+
+	if not os.path.isfile(stackfile):
+		apDisplay.printError("stackfile does not exist: "+stackfile)
+
+	### make particle files
+	partlistdocfile = "partlist.doc"
+	f = open(partlistdocfile, "w")
+	i = 0
+
+	curdir = os.path.join(partdir,str(subdir))
+	stackimages = apStack.readImagic(stackfile)
+
+	t0 = time.time()
+	while i < numpart:
+		### messaging
+		if (i+1) % filesperdir == 0:
+			subdir += 1
+			curdir = os.path.join(partdir,str(subdir))
+			esttime = (time.time()-t0)/float(i+1)*float(numpart-i)
+			apDisplay.printMsg("new directory: '"+curdir+"' at particle "+str(i)+" of "+str(numpart)
+				+", "+apDisplay.timeString(esttime)+" remain")
+				
+		"""
+		### using EMAN, 16 hours for 9000 particles
+		### pull out particle
+		emancmd = ("proc2d "+stackfile+" badpart spiderswap-single first=%d last=%d"%(i,i))
+		apEMAN.executeEmanCmd(emancmd, verbose=False, showcmd=False)
+
+		### rename particle and add to doc file
+		if i == 0:
+			singlefile = "badpart"
+		else:
+			singlefile = "badpart"+str(i)+".spi"
+		if os.path.isfile(singlefile):	
+			goodfile = os.path.join(partdir,str(subdir),"part%06d.spi"%(i))
+			shutil.move(singlefile, goodfile)
+			f.write(os.path.abspath(goodfile)+" 1\n")
+		else:
+			apDisplay.printWarning("missing particle "+str(i)+" of "+str(numpart))
+		"""
+
+		### Scott's imagic reader and Neil's spidersingle writer, 38 sec for 9000 particles
+		partfile = os.path.join(partdir,str(subdir),"part%06d.spi"%(i))
+		partimg = stackimages['images'][i]
+		spider.write(partimg, partfile)
+		f.write(os.path.abspath(partfile)+" 1\n")
+
+		i += 1
+	f.close()
+
+	apDisplay.printColor("finished breaking stack in "+apDisplay.timeString(time.time()-starttime), "cyan")
+	return partlistdocfile
+
+
+def createSubFolders(partdir, numpart, filesperdir):
+	i = 0
+	dirnum = 0
+	while i < numpart:
+		dirnum += 1
+		apParam.createDirectory(os.path.join(partdir, str(dirnum)))
+		i += filesperdir
+	return dirnum
+
+
+
+
+
+
+
