79aa609680cdc2c3e5a4b0836838a2619e70b4b4
Anchi Cheng <acheng>
2008-11-14 00:42:59 +0000

    track images for deletion

---
+++ myamiweb/deletionlist.php
@@ -0,0 +1,74 @@
+<?php
+require_once 'inc/leginon.inc';
+?>
+<html>
+<head>
+<title>Image Deletion List</title>
+<link rel="stylesheet" href="../css/viewer.css" type="text/css" /> 
+</head>
+	<body>
+	<div class="header">
+	<form name="deletionlist" method="POST" action="deletionlist.php">
+		<table>
+			<tr><td>
+			<table class='tableborder' border='1' cellspacing='1' cellpadding='5' width=100%>
+				<tr><td>
+					<b>Files marked for deletion but still exist:</b>
+				</td></tr>
+				<tr><td>
+<?php
+$filecount = 0;
+$filearray = $leginondata->getDeletionList(1);
+foreach ($filearray as $file) {
+	echo $file['filepath'];
+	$filecount += 1;
+	?><br><?
+}
+if ($filecount == 0) 
+	echo 'NONE';
+?>
+			</td></tr>
+		</table>
+		<br>
+		</td></tr><tr><td>
+<table class='tableborder' border='1' cellspacing='1' cellpadding='5' width=100%>
+<tr><td>
+<b>Files marked for deletion that are removed:</b>
+</td></tr>
+<tr><td>
+<?php
+$filearray = $leginondata->getDeletionList(0);
+$filecount = 0;
+foreach ($filearray as $file) {
+	if ($_POST['markasdeleted']=='Mark as Deleted') {
+		$imageId = $file['imageId'];
+		$sessionId = $file['sessionId'];
+		$leginondata-> setImageDeletionStatus($imageId,$sessionId, 'deleted');
+		$status = '';
+	} else {
+		echo $file['filepath'];
+		$status = 'Mark as Deleted';
+		$filecount += 1;
+		?>
+		<br>
+		<?
+	}
+}
+if ($filecount > 0) {
+?>
+</td></tr>
+<tr><td>
+	<input type="submit" name="markasdeleted" value = "<?$status?>" >
+<?
+} else {
+echo 'NONE';
+}
+?>
+</td></tr>
+</table>
+</td></tr></table>
+</form>
+</body>
+
+</html>
+
