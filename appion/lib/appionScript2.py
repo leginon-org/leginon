e7ca448c5e8705baf1460b373bb48e6888ed4177
Neil Voss <vossman>
2008-12-04 19:54:14 +0000

    getting ready for project transition

---
+++ appion/lib/appionScript2.py
@@ -0,0 +1,218 @@
+#!/usr/bin/python -O
+
+import pyami.quietscipy
+
+#builtin
+import sys
+import os
+import re
+import time
+import math
+import random
+import cPickle
+from optparse import OptionParser
+#appion
+import apDisplay
+import apDatabase
+import apParam
+import apDB
+import apFile
+#leginon
+from pyami import mem
+
+
+class AppionScript(object):
+
+	#=====================
+	def __init__(self):
+		self.quiet = False
+		self.appiondb  = apDB.apdb
+		self.leginondb = apDB.db
+		self.startmem = mem.active()
+		### clean up any preliminary warnings
+		sys.stderr.write("\n\n")
+		#set the name of the function; needed for param setup
+		self.t0 = time.time()
+
+		self.timestamp = apParam.makeTimestamp()
+		self.functionname = apParam.getFunctionName(sys.argv[0])
+		apDisplay.printMsg("Function name: "+self.functionname)
+		self.appiondir = apParam.getAppionDirectory()
+		apParam.setUmask()
+		self._parsePythonPath()
+
+		### setup default parser: output directory, etc.
+		self.parser = OptionParser()
+		self._setupGlobalParserOptions()
+		self.setupParserOptions()
+		self.params = apParam.convertParserToParams(self.parser)
+
+		### check if user wants to print help message
+		if 'commit' in self.params:
+			if self.params['commit'] is False:
+				apDisplay.printWarning("Not committing data to database")
+			else:
+				apDisplay.printMsg("Committing data to database")
+		self.checkConflicts()
+
+		### setup output directory
+		self.setProcessingDirName()
+		self.setupOutputDirectory()
+		self.params['rundir'] = self.params['outdir']
+		if apDatabase.queryDirectory(self.params['outdir']):
+			self.preExistingDirectoryError()
+
+		### write function log
+		self.logfile = apParam.writeFunctionLog(sys.argv, msg=(not self.quiet))
+
+		### any custom init functions go here
+		self.onInit()
+		self.appiondb.dbd.ping()
+
+	#=====================
+	def setupOutputDirectory(self):
+		#IF NO OUTDIR IS SET
+		if self.params['outdir'] is None:
+			self.setOutDir()
+		#create the output directory, if needed
+		if self.quiet is False:
+			apDisplay.printMsg("Run directory: "+self.params['outdir'])
+		apParam.createDirectory(self.params['outdir'], warning=(not self.quiet))
+		os.chdir(self.params['outdir'])
+
+	#=====================
+	def close(self):
+		self.onClose()
+		apParam.closeFunctionLog(params=self.params, logfile=self.logfile, msg=(not self.quiet))
+		apFile.removeFile("spider.log")
+		if self.quiet is False:
+			apDisplay.printMsg("ended at "+time.strftime("%a, %d %b %Y %H:%M:%S"))
+			apDisplay.printMsg("memory increase during run: %.3f MB"%((mem.active()-self.startmem)/1024.0))
+			apDisplay.printMsg("outdir:\n "+self.params['outdir'])
+			apDisplay.printColor("COMPLETE SCRIPT:\t"+apDisplay.timeString(time.time()-self.t0),"green")
+		apParam.killVirtualFrameBuffer()
+
+	#=====================
+	def _setupGlobalParserOptions(self):
+		"""
+		set the input parameters
+		"""
+		self.parser.add_option("-r", "--runname", dest="runname", default=self.timestamp,
+			help="Name for processing run, e.g. --runname=run1", metavar="NAME")
+		self.parser.add_option("-d", "--description", dest="description",
+			help="Description of the processing run (must be in quotes)", metavar="TEXT")
+		self.parser.add_option("-p", "--projectid", dest="session", type="int",
+			help="Project id associated with processing run, e.g. --projectid=159", metavar="#")
+		self.parser.add_option("-o", "--rundir", "--outdir", dest="rundir",
+			help="Run directory for storing output, e.g. --rundir=/ami/data00/appion/runs/run1", metavar="PATH")
+		self.parser.add_option("-C", "--commit", dest="commit", default=True,
+			action="store_true", help="Commit processing run to database")
+		self.parser.add_option("--no-commit", dest="commit", default=True,
+			action="store_false", help="Do not commit processing run to database")
+
+	#=====================
+	def _parsePythonPath(self):
+		pythonpath = os.environ.get("PYTHONPATH")
+		paths = pythonpath.split(":")
+		leginons = []
+		appions = []
+		for p in paths:
+			if "appion" in p:
+				appions.append(p)
+			if "leginon" in p:
+				leginons.append(p)
+		if len(appions) > 1:
+			apDisplay.printWarning("There is more than one appion directory in your PYTHONPATH")
+			print appions
+		if len(leginons) > 1:
+			apDisplay.printWarning("There is more than one leginon directory in your PYTHONPATH")
+			print leginons
+
+	#######################################################
+	#### ITEMS BELOW CAN BE SPECIFIED IN A NEW PROGRAM ####
+	#######################################################
+
+	#=====================
+	def preExistingDirectoryError(self):
+		apDisplay.printWarning("Output directory already exists in the database")
+
+	#=====================
+	def setupParserOptions(self):
+		"""
+		set the input parameters
+		"""
+		self.parser.set_usage("Usage: %prog --commit --description='<text>' [options]")
+		self.parser.add_option("--stackid", dest="stackid", type="int",
+			help="ID for particle stack (optional)", metavar="INT")
+
+	#=====================
+	def checkConflicts(self):
+		"""
+		make sure the necessary parameters are set correctly
+		"""
+		if self.params['session'] is None:
+			apDisplay.printError("enter a session ID, e.g. --session=07jun06a")
+		if self.params['description'] is None:
+			apDisplay.printError("enter a description, e.g. --description='awesome data'")
+
+	#=====================
+	def setProcessingDirName(self):
+		self.processdirname = self.functionname
+
+	#=====================
+	def setOutDir(self):
+		import apStack
+		if ( self.params['outdir'] is None 
+		and 'session' in self.params 
+		and self.params['session'] is not None ):
+			#auto set the output directory
+			sessiondata = apDatabase.getSessionDataFromSessionName(self.params['session'])
+			path = os.path.abspath(sessiondata['image path'])
+			path = re.sub("leginon","appion",path)
+			path = re.sub("/rawdata","",path)
+			path = os.path.join(path, self.processdirname)
+			self.params['outdir'] = path
+		if ( self.params['outdir'] is None 
+		and 'reconid' in self.params 
+		and self.params['reconid'] is not None ):
+			self.params['stackid'] = apStack.getStackIdFromRecon(self.params['reconid'], msg=False)
+		if ( self.params['outdir'] is None 
+		and 'stackid' in self.params
+		and self.params['stackid'] is not None ):
+			#auto set the output directory
+			stackdata = apStack.getOnlyStackData(self.params['stackid'], msg=False)
+			path = os.path.abspath(stackdata['path']['path'])
+			path = os.path.dirname(path)
+			path = os.path.dirname(path)
+			self.params['outdir'] = os.path.join(path, self.processdirname)
+
+	#=====================
+	def start(self):
+		"""
+		this is the main component of the script
+		where all the processing is done
+		"""
+		raise NotImplementedError()
+
+	#=====================
+	def onInit(self):
+		return
+
+	#=====================
+	def onClose(self):
+		return
+
+class TestScript(AppionScript):
+	def start(self):
+		apDisplay.printMsg("Hey this works")
+
+if __name__ == '__main__':
+	print "__init__"
+	testscript = TestScript()
+	print "start"
+	testscript.start()
+	print "close"
+	testscript.close()
+	
+
+
