9f9b370d0b28ec795c05fd1fc3e428f4d1096caa
Neil Voss <vossman>
2008-12-05 01:11:13 +0000

    deleting this file

--- appion/lib/apDB.py
+++
@@ -1,7 +0,0 @@
-#Part of the new appion
-
-import sinedon
-
-db     = sinedon.getConnection('leginondata')
-apdb   = sinedon.getConnection('appionData')
-projdb = sinedon.getConnection('projectData')
