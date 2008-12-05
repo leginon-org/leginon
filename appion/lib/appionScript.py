c1051c9cdba8ce8c1e4d6f719732367dd4be38a9
Neil Voss <vossman>
2008-12-05 01:35:53 +0000

    moving towards a common setup

--- appion/lib/appionScript.py
+++ appion/lib/appionScript.py
@@ -21,39 +21,49 @@ from pyami import mem
 
 
 class AppionScript(object):
-
 	#=====================
 	def __init__(self):
+		### setup some expected values
+		sys.stderr.write("\n\n")
 		self.quiet = False
 		self.startmem = mem.active()
-		### clean up any preliminary warnings
-		sys.stderr.write("\n\n")
-		#set the name of the function; needed for param setup
 		self.t0 = time.time()
-
+		self.createDefaultStats()
 		self.timestamp = apParam.makeTimestamp()
+		apDisplay.printMsg("Time stamp: "+self.timestamp)
 		self.functionname = apParam.getFunctionName(sys.argv[0])
+		apDisplay.printMsg("Function name: "+self.functionname)
 		self.appiondir = apParam.getAppionDirectory()
+		apDisplay.printMsg("Appion directory: "+self.appiondir)
 		apParam.setUmask()
+		self.parsePythonPath()
 
-		### setup default parser: output directory, etc.
+		### setup default parser: run directory, etc.
 		self.parser = OptionParser()
+		#self.setupGlobalParserOptions()
 		self.setupParserOptions()
 		self.params = apParam.convertParserToParams(self.parser)
+		self.checkForDuplicateCommandLineInputs()
+		self.params['rundir'] = self.params['outdir']
+
+		### setup correct database after we have read the project id
+		if False and self.params['projectid'] is not None:
+			apDisplay.printWarning("Using split database")
+			# use a project database
+			newdbname = "ap"+str(self.params['projectid'])
+			sinedon.setConfig('appionData', db=newdbname)
 
 		### check if user wants to print help message
-		if 'commit' in self.params:
-			if self.params['commit'] is False:
-				apDisplay.printWarning("Not committing data to database")
-			else:
-				apDisplay.printMsg("Committing data to database")
+		if self.params['commit'] is False:
+			apDisplay.printWarning("Not committing data to database")
+		else:
+			apDisplay.printMsg("Committing data to database")
 		self.checkConflicts()
 
-		### setup output directory
+		### setup run directory
 		self.setProcessingDirName()
-		self.setupOutputDirectory()
-		self.params['rundir'] = self.params['outdir']
-		if apDatabase.queryDirectory(self.params['outdir']):
+		self.setupRunDirectory()
+		if apDatabase.queryDirectory(self.params['rundir']):
 			self.preExistingDirectoryError()
 
 		### write function log
@@ -63,13 +73,45 @@ class AppionScript(object):
 		self.onInit()
 
 	#=====================
-	def setupOutputDirectory(self):
-		#IF NO OUTDIR IS SET
+	def checkForDuplicateCommandLineInputs(self):
+		args = sys.argv[1:]
+		argdict = {}
+		for arg in args:
+			elements=arg.split('=')
+			opt = elements[0].lower()
+			if opt in argdict:
+				apDisplay.printError("Multiple arguments were supplied for argument: "+str(opt))
+			else:
+				argdict[opt] = True
+
+	#=====================
+	def createDefaultStats(self):
+		self.stats = {}
+		self.stats['startTime']=time.time()
+		self.stats['count'] = 1
+		self.stats['lastcount'] = 0
+		self.stats['startmem'] = mem.active()
+		self.stats['memleak'] = False
+		self.stats['peaksum'] = 0
+		self.stats['lastpeaks'] = None
+		self.stats['imagesleft'] = 1
+		self.stats['peaksumsq'] = 0
+		self.stats['timesum'] = 0
+		self.stats['timesumsq'] = 0
+		self.stats['skipcount'] = 0
+		self.stats['waittime'] = 0
+		self.stats['lastimageskipped'] = False
+		self.stats['notpair'] = 0
+		self.stats['memlist'] = [mem.active()]
+
+	#=====================
+	def setupRunDirectory(self):
+		#IF NO RUNDIR IS SET
 		if self.params['outdir'] is None:
 			self.setOutDir()
-		#create the output directory, if needed
+		#create the run directory, if needed
 		if self.quiet is False:
-			apDisplay.printMsg("Output directory: "+self.params['outdir'])
+			apDisplay.printMsg("Run directory: "+self.params['outdir'])
 		apParam.createDirectory(self.params['outdir'], warning=(not self.quiet))
 		os.chdir(self.params['outdir'])
 
@@ -84,6 +126,23 @@ class AppionScript(object):
 			apDisplay.printColor("COMPLETE SCRIPT:\t"+apDisplay.timeString(time.time()-self.t0),"green")
 		apParam.killVirtualFrameBuffer()
 
+	#=====================
+	def parsePythonPath(self):
+		pythonpath = os.environ.get("PYTHONPATH")
+		paths = pythonpath.split(":")
+		leginons = []
+		appions = []
+		for p in paths:
+			if "appion" in p:
+				appions.append(p)
+			if "leginon" in p:
+				leginons.append(p)
+		if len(appions) > 1:
+			apDisplay.printWarning("There is more than one appion directory in your PYTHONPATH")
+			print appions
+		if len(leginons) > 1:
+			apDisplay.printWarning("There is more than one leginon directory in your PYTHONPATH")
+			print leginons
 
 	#######################################################
 	#### ITEMS BELOW CAN BE SPECIFIED IN A NEW PROGRAM ####
@@ -91,7 +150,7 @@ class AppionScript(object):
 
 	#=====================
 	def preExistingDirectoryError(self):
-		apDisplay.printWarning("Output directory already exists in the database")
+		apDisplay.printWarning("Run directory already exists in the database")
 
 	#=====================
 	def setupParserOptions(self):
@@ -134,7 +193,7 @@ class AppionScript(object):
 		if ( self.params['outdir'] is None 
 		and 'session' in self.params 
 		and self.params['session'] is not None ):
-			#auto set the output directory
+			#auto set the run directory
 			sessiondata = apDatabase.getSessionDataFromSessionName(self.params['session'])
 			path = os.path.abspath(sessiondata['image path'])
 			path = re.sub("leginon","appion",path)
@@ -148,7 +207,7 @@ class AppionScript(object):
 		if ( self.params['outdir'] is None 
 		and 'stackid' in self.params
 		and self.params['stackid'] is not None ):
-			#auto set the output directory
+			#auto set the run directory
 			stackdata = apStack.getOnlyStackData(self.params['stackid'], msg=False)
 			path = os.path.abspath(stackdata['path']['path'])
 			path = os.path.dirname(path)
