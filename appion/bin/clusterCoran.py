b29b79993fbcf169938d5065eb214ea103c16ae4
Neil Voss <vossman>
2008-12-05 18:04:57 +0000

    naming mix up

---
+++ appion/bin/clusterCoran.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python
+
+import os
+import time
+import sys
+import appionScript
+import apDisplay
+import apAlignment
+import apFile
+import apStack
+import apEMAN
+from apSpider import alignment
+import appionData
+
+
+#=====================
+#=====================
+class NoRefClassScript(appionScript.AppionScript):
+
+	#=====================
+	def setupParserOptions(self):
+		self.parser.set_usage("Usage: %prog --noref=ID [ --num-part=# ]")
+
+		#required
+		self.parser.add_option("-a",  "--analysisid", dest="analysisid", type="int",
+			help="Coran database id", metavar="ID#")
+
+		#with defaults
+		self.parser.add_option("-f", "--factor-list", dest="factorstr", type="str", default="1,2,3",
+			help="List of factors to use in classification", metavar="#")
+		self.parser.add_option("-N", "--num-class", dest="numclass", type="int", default=40,
+			help="Number of classes to make", metavar="#")
+		self.parser.add_option("-C", "--commit", dest="commit", default=True,
+			action="store_true", help="Commit noref class to database")
+		self.parser.add_option("--no-commit", dest="commit", default=True,
+			action="store_false", help="Do not commit noref class to database")
+		self.parser.add_option("--method", dest="method", default="hierarch",
+			help="Method to use for classification: 'hierarch' or 'kmeans'")
+
+
+		self.parser.add_option("-o", "--rundir", dest="rundir",
+			help="Run directory", metavar="PATH")
+
+	#=====================
+	def checkConflicts(self):
+		if self.params['analysisid'] is None:
+			apDisplay.printError("analysis id was not defined")
+		if self.params['numclass'] > 900:
+			apDisplay.printError("too many classes defined: "+str(self.params['numclass']))
+		if self.params['method'] not in ['hierarch','kmeans']:
+			apDisplay.printError("--method must be either 'hierarch' or 'kmeans', e.g. --method=hierarch")
+		self.analysisdata = appionData.ApAlignAnalysisData.direct_query(self.params['analysisid'])
+
+	#=====================
+	def setRunDir(self):
+		self.params['rundir'] = self.analysisdata['path']['path']
+
+	#=====================
+	def readClassDocFile(self, docfile):
+		if not os.path.isfile(docfile):
+			apDisplay.printError("could not read doc file, "+docfile)
+		partlist = []
+		f = open(docfile, 'r')
+		for line in f:
+			if line.strip()[0] == ';':
+				continue
+			bits = line.split()
+			partnum = int(float(bits[2]))
+			partlist.append(partnum)
+		if not partlist:
+			apDisplay.printError("reading class doc file did not work: "+docfile)
+		partlist.sort()
+		return partlist
+
+	#=====================
+	def getAlignParticlesData(self, partnum):
+		alignpartq = appionData.ApAlignParticlesData()
+		alignpartq['alignstack'] = self.analysisdata['alignstack']
+		alignpartq['partnum'] = partnum
+		alignparts = alignpartq.query(results=1)
+		return alignparts[0]
+
+	#=====================
+	def insertClusterRun(self, classavg=None, classvar=None, insert=False):
+		### broken
+		sys.exit(1)
+		# create a norefParam object
+		classq = appionData.ApNoRefClassRunData()
+		classq['num_classes'] = self.params['numclass']
+		classq['norefRun'] = self.norefdata
+		uniqueclass = classq.query(results=1)
+		if uniqueclass:
+			apDisplay.printWarning("Classification of "+str(classq['num_classes'])+" classes for norefid="+\
+				str(self.params['norefid'])+"\nis already in the database")
+
+		classq['factor_list'] = self.params['factorstr']
+		if classavg is None:
+			classq['classFile'] = ("cluster/classavgstack%03d_%s" % (self.params['numclass'], self.timestamp))
+		else:
+			classq['classFile'] = classavg
+		if classvar is None:
+			classq['varFile'] = ("cluster/classvarstack%03d_%s" % (self.params['numclass'], self.timestamp))
+		else:
+			classq['varFile'] = classvar
+
+		classq['method'] = self.params['method']
+
+		apDisplay.printMsg("inserting classification parameters into database")
+		if insert is True:
+			classq.insert()
+
+		### particle class data
+		apDisplay.printColor("Inserting particle classification data, please wait", "cyan")
+		for i in range(self.params['numclass']):
+			classnum = i+1
+			classdocfile = os.path.join(self.params['outdir'], "cluster/classdoc%04d.spi" % (classnum))
+			partlist = self.readClassDocFile(classdocfile)
+			sys.stderr.write(".")
+			for partnum in partlist:
+				norefpart = self.getNoRefPart(partnum)
+				cpartq = appionData.ApNoRefClassParticlesData()
+				cpartq['classRun'] = classq
+				cpartq['noref_particle'] = norefpart
+				cpartq['classNumber'] = classnum
+				# actual parameters
+				if insert is True:
+					cpartq.insert()
+
+		return
+
+	#=====================
+	def start(self):
+		### get database information
+		alignedstack = os.path.join(self.analysisdata['alignstack']['path']['path'], self.analysisdata['alignstack']['spiderstack'])
+		numpart = self.analysisdata['alignstack']['num_particles']
+		corandata = os.path.join(self.analysisdata['path']['path'],"coran/corandata")
+
+		### parse factor list
+		factorlist = self.params['factorstr'].split(",")
+		apDisplay.printMsg("using factorlist "+str(factorlist))
+		if len(factorlist) > self.analysisdata['coranrun']['num_factors']:
+			apDisplay.printError("Requested factor list is longer than available factors")
+
+		#run the classification
+		if self.params['method'] == "kmeans":
+			apDisplay.printMsg("Using the k-means clustering method")
+			classavg,classvar = alignment.kmeansCluster(alignedstack, numpart, numclasses=self.params['numclass'], 
+				timestamp=self.timestamp, factorlist=factorlist, corandata=corandata, dataext=".spi")
+		else:
+			apDisplay.printMsg("Using the hierarch clustering method")
+			classavg,classvar = alignment.hierarchCluster(alignedstack, numpart, numclasses=self.params['numclass'], 
+				timestamp=self.timestamp, factorlist=factorlist, corandata=corandata, dataext=".spi")
+
+		if self.params['commit'] is True:
+			self.insertClusterRun(classavg, classvar, insert=True)
+		else:
+			apDisplay.printWarning("not committing results to DB")
+
+#=====================
+if __name__ == "__main__":
+	noRefClass = NoRefClassScript()
+	noRefClass.start()
+	noRefClass.close()
+
