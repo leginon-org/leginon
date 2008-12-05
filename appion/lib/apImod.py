c40047f4aac6beb855a8d845b346866c81d8a5ba
Anchi Cheng <acheng>
2008-12-05 22:37:15 +0000

    tomography

---
+++ appion/lib/apImod.py
@@ -0,0 +1,224 @@
+import os
+import subprocess
+import apDisplay
+import apFile
+import time
+import apTomo
+
+def writeRawtltFile(path,seriesname,tilts):
+	rawtltname = os.path.join(path,seriesname+'.rawtlt')
+	f = open(rawtltname, 'w')
+	for tilt in tilts:
+		f.write('%6.2f\n' % (tilt,))
+	f.close()
+
+def writePrexfFile(path, seriesname,xpeaks):
+	rawtltname = os.path.join(path,seriesname+'.prexf')
+	f = open(rawtltname, 'w')
+	tilts = xpeaks.keys()
+	tilts.sort()
+	f.write('%11.7f %11.7f %11.7f %11.7f %11.3f %11.3f\n' % (1.0,0.0,0.0,1.0,0.0,0.0))
+	for tilt in tilts:
+		if xpeaks[tilt] is not None:
+			f.write('%11.7f %11.7f %11.7f %11.7f %11.3f %11.3f\n' % (1.0,0.0,0.0,1.0,xpeaks[tilt][0],xpeaks[tilt][1]))
+	f.close()
+
+def coarseAlignment(processpath, seriesname, commit=False):
+		"""
+#
+# TO RUN TILTXCORR
+#
+$tiltxcorr -StandardInput
+InputFile       08aug14f_008.st
+OutputFile      08aug14f_008.prexf
+TiltFile        08aug14f_008.rawtlt
+RotationAngle   0.0
+FilterSigma1    0.03
+FilterRadius2   0.25
+FilterSigma2    0.05
+
+		"""
+
+		inputparams = {
+			'imagestack': os.path.join(processpath, seriesname+".st"),
+			'output': os.path.join(processpath, seriesname+".prexf"),
+			'tilts': os.path.join(processpath, seriesname+".rawtlt"),
+		}
+		commands = [
+			"$tiltxcorr -StandardInput",
+			"InputFile	"+inputparams['imagestack'],
+			"OutputFile "+inputparams['output'],
+			"TiltFile "+inputparams['tilts'],
+			"TiltFile "+inputparams['tilts'],
+			"RotationAngle   0.0",
+			"FilterSigma1    0.03",
+			"FilterRadius2   0.25",
+			"FilterSigma2    0.05",
+		]
+		writeCommandAndRun(processpath,'xcorr',commands,[inputparams['output'],'xcorr.log'])
+		if commit:
+			xcorrdata = apTomo.insertImodXcorr(0.0,0.03,0.25,0.05)
+			return xcorrdata
+
+def convertToGlobalAlignment(processpath, seriesname):
+		"""
+$xftoxg
+# global fit
+0
+08aug14f_008.prexf
+08aug14f_008.prexg
+		"""
+		inputparams = {
+			'input': os.path.join(processpath, seriesname+".prexf"),
+			'output': os.path.join(processpath, seriesname+".prexg"),
+		}
+		commands = [
+			"$xftoxg",
+			"# global fit",
+			"0",
+			inputparams['input'],
+			inputparams['output'],
+		]
+		writeCommandAndRun(processpath,'gcorr',commands,[inputparams['output'],'gcorr.log'])
+		return readShifts(inputparams['output'])
+
+def readShifts(filepath):
+	file = open(filepath,'r')
+	lines = file.readlines()
+	shifts = []
+	for line in lines:
+		cleanlines = line.split('\n')
+		items = cleanlines[0].split()
+		shifts.append((float(items[-2]),float(items[-1])))
+	return shifts
+
+def createAlignedStack(processpath, seriesname):
+		"""
+# THIS IS A COMMAND FILE TO MAKE AN ALIGNED STACK FROM THE ORIGINAL STACK
+#
+####CreatedVersion#### 3.4.4
+#
+# It assumes that the views are in order in the image stack
+#
+# The -size argument should be ,, for the full area or specify the desired
+# size (e.g.: ,10)
+#
+# The -offset argument should be 0,0 for no offset, 0,300 to take an area
+# 300 pixels above the center, etc.
+#
+$newstack -input 08aug14f_008.st -output 08aug14f_008.ali -offset 0,0 -xform 08aug14f_
+008.xf
+$mrctaper 08aug14f_008.ali
+		"""
+		inputparams = {
+			'alignedstack': os.path.join(processpath, seriesname+".ali"),
+			'alignment': os.path.join(processpath, seriesname+".prexg"),
+			'imagestack': os.path.join(processpath, seriesname+".st"),
+		}
+		commands = [
+			"$newstack -input "+inputparams['imagestack']+" -output "+inputparams['alignedstack']+" -offset 0,0 -xform "+inputparams['alignment'],
+			"$mrctaper "+inputparams['alignedstack'],
+		]
+		writeCommandAndRun(processpath,'newst',commands,[inputparams['alignedstack'],'newst.log'])
+
+
+def recon3D(processpath, seriesname,invert=False):
+		"""
+# Command file to run Tilt
+#
+####CreatedVersion#### 3.4.4
+#
+# RADIAL specifies the frequency at which the Gaussian low pass filter begins
+#   followed by the standard deviation of the Gaussian roll-off
+#
+# LOG takes the logarithm of tilt data after adding the given value
+#
+$tilt
+08aug14f_008.ali
+08aug14f_008_full.rec
+FULLIMAGE 512 512
+LOG 0.0
+MODE 2
+PERPENDICULAR
+RADIAL 0.35 0.05
+SCALE 1.39 500.0
+SUBSETSTART 0 0
+THICKNESS 100
+TILTFILE 08aug14f_008.tlt
+XAXISTILT 0.0
+DONE
+
+		"""
+		inputparams = {
+			'alignedstack': os.path.join(processpath, seriesname+".ali"),
+			'tilts': os.path.join(processpath, seriesname+".rawtlt"),
+			'recon': os.path.join(processpath, seriesname+"_full.rec"),
+			'scale': 500.0,
+		}
+		if invert:
+			inputparams['scale'] = -inputparams['scale']
+		commands = [
+			"$tilt",
+			inputparams['alignedstack'],
+			inputparams['recon'],
+			"FULLIMAGE 512 512",
+			"LOG 0.0",
+			"MODE 2",
+			"PERPENDICULAR",
+			"RADIAL 0.35 0.05",
+			"SCALE 1.39 -500.0",
+			"SUBSETSTART 0 0",
+			"THICKNESS 100",
+			"TILTFILE "+inputparams['tilts'],
+			"XAXISTILT 0.0",
+		]
+		writeCommandAndRun(processpath,'tilt',commands,[inputparams['recon'],'tilt.log'])
+
+def trimVolume(processpath, seriesname, volumename, center, size):
+		"""
+#	Command for triming reconstructed volume
+#
+# Tomography reconstruction y and z are flipped
+trimvol -x 390,460 -z 477,537 -yz 08aug14f_008_full.rec test.rec
+		"""	
+		inputparams = {
+			'recon': os.path.join(processpath, seriesname+"_full.rec"),
+			'subvolume': os.path.join(processpath, volumename+"/",seriesname+"_"+volumename+".rec"),
+			'xrange0': center[0] - size[0]/2,
+			'zrange0': center[1] - size[1]/2,
+		}
+		inputparams['xrange1'] = inputparams['xrange0'] + size[0]
+		inputparams['zrange1'] = inputparams['zrange0'] + size[1]
+
+		commands = [
+			"$trimvol -x %d,%d -z %d,%d -yz %s %s"
+				% (inputparams['xrange0'],inputparams['xrange1'],
+				inputparams['zrange0'],inputparams['zrange1'],
+				inputparams['recon'],
+				inputparams['subvolume'],
+				),
+		]
+		writeCommandAndRun(processpath,'trimvol',commands,[inputparams['subvolume'],'trimvol.log'])
+
+def writeCommandAndRun(path,comname, commands, outputlist):		
+		### make standard input for ctftilt
+		commandlines = map((lambda x: x+"\n"), commands)
+		comname = os.path.join(path,comname)
+		comfile = open(comname+".com", "w")
+		comfile.writelines(commandlines)
+		comfile.close()
+		os.system('chmod 755 '+comname+'.com')
+		comfile = open(comname+".com", "r")
+		for output in outputlist:
+			if os.path.isfile(output):
+				# clean up output before running
+				apFile.removeFile(output)
+
+		t0 = time.time()
+		apDisplay.printMsg("running "+comname+" at "+time.asctime())
+		proc = subprocess.Popen("submfg "+comname+".com", shell=True)
+		proc.wait()
+		comfile.close()
+
+		apDisplay.printMsg(comname+" completed in "+apDisplay.timeString(time.time()-t0))
+
---
+++ appion/lib/apTomo.py
@@ -0,0 +1,227 @@
+import os
+from tomography import tiltcorrelator
+import leginondata
+import appionData
+import node
+import apDisplay
+import apFile
+import apDB
+
+def getFilename(tiltseriesdata):
+	# determine param filename
+	session_name = tiltseriesdata['session']['name']
+	seriesnumber = tiltseriesdata['number']
+	numberstr = '%03d' % (seriesnumber,)
+	seriesname = session_name + '_' + numberstr
+	return seriesname
+
+def getImageList(tiltseriesdata):
+	imquery = leginondata.AcquisitionImageData()
+	imquery['tilt series'] = tiltseriesdata
+
+	## query, but don't read image files yet, or else run out of memory
+	imagelist = imquery.query(readimages=False)
+	## list is reverse chronological, so reverse it
+	imagelist.reverse()
+	return imagelist
+	
+def orderImageList(imagelist):
+	if not imagelist:
+		apDisplay.warning('No images in image list.')
+		return
+	mrc_files = []
+	imagepath = imagelist[0]['session']['image path']
+	tiltseries = imagelist[0]['tilt series']
+	tiltangledict = {}
+	second_group = False 
+	for imagedata in imagelist:
+		tilt = imagedata['scope']['stage position']['a']*180/3.14159
+		if tilt < tiltseries['tilt start']+0.01 and tilt > tiltseries['tilt start']-0.01:
+			if second_group:
+				direction=-tiltseries['tilt step']
+			else:
+				direction= tiltseries['tilt step']
+				# switch group in getCorrelationPeak not here
+				second_group = False
+			tilt = tilt+0.001*direction
+		tiltangledict[tilt] = imagedata
+	tiltkeys = tiltangledict.keys()
+	tiltkeys.sort()
+	ordered_imagelist = []
+	for key in tiltkeys:
+		imagedata = tiltangledict[key]
+		mrc_name = imagedata['filename'] + '.mrc'
+		fullname = os.path.join(imagepath, mrc_name)
+		mrc_files.append(fullname)
+		ordered_imagelist.append(imagedata)
+	return tiltkeys,ordered_imagelist,mrc_files
+
+def writeOrderedImageListCorrelation(imagelist, bin):
+	fakenode = node.Node('fake',imagelist[0]['session'])
+	correlator = tiltcorrelator.Correlator(fakenode, 0, 4,lpf=1.5)
+	allpeaks = [{'x':0.0,'y':0.0}]
+	tiltseries = imagelist[0]['tilt series']
+	tiltangledict = {}
+	correlationpeak = {}
+	second_group = False 
+	for imagedata in imagelist:
+		tilt = imagedata['scope']['stage position']['a']*180/3.14159
+		if tilt < tiltseries['tilt start']+0.01 and tilt > tiltseries['tilt start']-0.01:
+			if second_group:
+				direction=-tiltseries['tilt step']
+			else:
+				direction= tiltseries['tilt step']
+				# switch group in getCorrelationPeak not here
+				second_group = False
+			tilt = tilt+0.001*direction
+		try:
+			correlationpeak[tilt],allpeaks,second_group = getCorrelationPeak(correlator,bin, tiltseries, tilt, imagedata,allpeaks,second_group)
+		except:
+			raise
+			break
+	fakenode.die()
+	return correlationpeak
+
+def getCorrelationPeak(correlator, bin, tiltseries, tilt, imagedata,allpeaks,second_group):
+	q = leginondata.TomographyPredictionData(image=imagedata)
+	results = q.query()
+	if len(results) > 0:
+		peak = results[0]['correlation']
+	else:
+		raise ValueError
+	start = tiltseries['tilt start']
+	if tilt < start:
+		peak['x']=-peak['x']
+		peak['y']=-peak['y']
+	if tilt < tiltseries['tilt start']+0.01 and tilt > tiltseries['tilt start']-0.01:
+		correlator.correlate(imagedata, tiltcorrection=True, channel=None)
+		if second_group:
+			allpeaks = [{'x':0.0,'y':0.0}]
+			peak = correlator.getShift(False)
+			if tiltseries['tilt step'] > 0:
+				return (-peak['x']/bin,peak['y']/bin),allpeaks,second_group
+			else:
+				return (peak['x']/bin,-peak['y']/bin),allpeaks,second_group
+		else:
+			second_group = True
+			return None,allpeaks,second_group
+	allpeaks.append(peak)
+	return ((peak['x']-allpeaks[-2]['x'])/bin,-(peak['y']-allpeaks[-2]['y'])/bin),allpeaks,second_group
+
+def getTomographySettings(sessiondata,tiltdata):
+	'''
+	search for the last tomography node settings in the order of
+	from the same session, from the same user, from default
+	''' 
+	timestamp = tiltdata.timestamp
+	qtomo = leginondata.TomographySettingsData(session=sessiondata)
+	tomosettingslist = qtomo.query()
+	settingsdata = None
+	for settings in tomosettingslist:
+		if settings.timestamp < timestamp:
+			settingsdata = dict(settings)
+			break
+	if settingsdata is None:
+		sessionq = leginondata.SessionData(user=sessiondata['user'])
+		qtomo = leginondata.TomographySettingsData(session=sessiondata)
+		tomosettingslist = qtomo.query()
+		for settings in tomosettingslist:
+			if settings.timestamp < timestamp:
+				settingsdata = dict(settings)
+				break
+	if settingsdata is None:
+		qtomo = leginondata.TomographySettingsData(isdefault=True)
+		tomosettingslist = qtomo.query()
+		for settings in tomosettingslist:
+			if settings.timestamp < timestamp:
+				settingsdata = dict(settings)
+				break
+	if settingsdata is None:
+		if tomosettingslist:
+			settingsdata = dict(tomosettingslist[0])
+		else:
+			settingsdata = None
+	return settingsdata
+
+def getTomoPixelSize(imagedata):
+	predq = leginondata.TomographyPredictionData(image=imagedata)
+	results = predq.query(readimages=False)
+	if results:
+		print results[0]['pixel size']
+		return results[0]['pixel size']
+
+def	insertImodXcorr(rotation,filtersigma1,filterradius,filtersigma2):
+			paramsq = appionData.ApImodXcorrParamsData()		
+			paramsq['RotationAngle'] = rotation
+			paramsq['FilterSigma1'] = filtersigma1
+			paramsq['FilterRadius2'] = filterradius
+			paramsq['FilterSigma2'] = filtersigma2
+			results = paramsq.query()
+			if not results:
+				paramsq.insert()
+			results = paramsq.query()
+			return results[0]
+
+def insertTomoAlignmentRun(sessiondata,tiltdata,leginoncorrdata,imodxcorrdata,bin):
+	qalign = appionData.ApTomoAlignmentRunData(session=sessiondata,tiltseries=tiltdata,
+		coarseLeginonParams=leginoncorrdata,coarseImodParams=imodxcorrdata,bin=bin)
+	results = qalign.query()
+	if not results:
+		qalign.insert()
+		results = qalign.query()
+	return results[0]
+
+def checkExistingFullTomoData(path,name):
+	filepath = os.path.join(path,name+".rec")
+	md5sum = apFile.md5sumfile(filepath)
+	tomoq = appionData.ApFullTomogramData(md5sum = md5sum)
+	results = tomoq.query()
+	if not results:
+		return None
+	else:
+		return results[0]
+	
+def insertFullTomogram(sessiondata,tiltdata,alignrun,path,name,description):
+	tomoq = appionData.ApFullTomogramData()
+	tomoq['session'] = sessiondata
+	tomoq['tiltseries'] = tiltdata
+	tomoq['alignment'] = alignrun
+	tomoq['path'] = appionData.ApPathData(path=os.path.abspath(path))
+	tomoq['name'] = name
+	tomoq['description'] = description
+	filepath = os.path.join(path,name+".rec")
+	tomoq['md5sum'] = apFile.md5sumfile(filepath)
+	tomoq.query()
+	results = tomoq.query()
+	if not results:
+		tomoq.insert()
+		return tomoq
+	return results[0]
+
+def getLastVolumeIndex(fulltomodata):
+	tomoq = appionData.ApTomogramData(fulltomogram=fulltomodata)
+	results = tomoq.query()
+	if results:
+		return results[0]['number']
+	else:
+		return 0
+
+def insertSubTomogram(fulltomogram,center,dimension,path,name,index,pixelsize,description):
+	tomoq = appionData.ApTomogramData()
+	tomoq['session'] = fulltomogram['session']
+	tomoq['tiltseries'] = fulltomogram['tiltseries']
+	tomoq['fulltomogram'] = fulltomogram
+	tomoq['path'] = appionData.ApPathData(path=os.path.abspath(path))
+	tomoq['name'] = name
+	tomoq['number'] = index
+	tomoq['center'] = center
+	tomoq['dimension'] = dimension
+	tomoq['pixelsize'] = pixelsize
+	tomoq['description'] = description
+	filepath = os.path.join(path,name+".rec")
+	tomoq['md5sum'] = apFile.md5sumfile(filepath)
+	results = tomoq.query()	
+	if not results:
+		tomoq.insert()
+		return tomoq
+	return results[0]
