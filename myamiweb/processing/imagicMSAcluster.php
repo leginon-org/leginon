dc5e0bebee0ad27389e59c969d8c9698603cc7aa
Dmitry Lyumkis <dlyumkis>
2009-01-10 23:19:25 +0000

    compatibility with new alignment pipeline

--- myamiweb/processing/imagicMSAcluster.php
+++ myamiweb/processing/imagicMSAcluster.php
@@ -16,7 +16,7 @@ require "inc/project.inc";
 
 // check for errors in submission form
 if ($_POST['process']) {
-	if (!is_numeric($_POST['num_classes'])) jobform("error: number of classes not specified");
+	if (!$_POST['num_classes']) jobform("error: number of classes not specified");
 	runImagicMSAcluster();
 }
 else jobform();
@@ -33,12 +33,13 @@ function jobform($extra=false)	{
 	echo "<form name='viewerform' method='POST' action='$formaction'>\n";
 	$expId=$_GET['expId'];
 	$projectId=getProjectFromExpId($expId);
+	$analysisId=$_GET['analysisId'];
+	$analysisdata=$particle->getImagicAnalysisParams($analysisId);
+	$alignId=$_GET['alignId'];
 	$sessiondata=getSessionList($projectId,$expId);
 	$sessioninfo=$sessiondata['info'];
 	if (!empty($sessioninfo)) {
-		$sessionpath=$sessioninfo['Image path'];
-		$sessionpath=ereg_replace("leginon","appion",$sessionpath);
-		$sessionpath=ereg_replace("rawdata","align",$sessionpath);
+		$sessionpath=$analysisdata['path'];
 		$sessionname=$sessioninfo['Name'];
 	}
 
@@ -47,20 +48,18 @@ function jobform($extra=false)	{
 #	$imagicAnalysisIds = $particle->getImagicClassIds($expId);
 	
 	processing_header("IMAGIC Class Clustering","IMAGIC Class Clustering",$javafunc);
-
 	// write out errors, if any came up:
 	if ($extra) echo "<FONT COLOR='RED'>$extra</FONT>\n<HR>\n";
 	
 	// set commit on by default when first loading page, else set
 	$commitcheck = ($_POST['commit']=='on' || !$_POST['process']) ? 'checked' : '';
 	// Set any existing parameters in form
-	$runid = ($_POST['runid']) ? $_POST['runid'] : 'cluster'.($imagicnorefruns+1);
 	$description = $_POST['description'];
 	$classidval = $_GET['imagicAnalysisId'];
 	$outdir = ($_POST['outdir']) ? $_POST['outdir'] : $sessionpath;
 	$ignore_images = ($_POST['ignore_images']) ? $_POST['ignore_images'] : '0';
 	$ignore_members = ($_POST['ignore_members']) ? $_POST['ignore_members'] : '0';
-	$num_classes = ($_POST['num_classes']) ? $_POST['num_classes'] : '';
+	$num_classes = ($_POST['num_classes']) ? $_POST['num_classes'] : '4,16,64';
 	
 	echo"
 	<table border='0' class='tableborder'>
@@ -69,13 +68,10 @@ function jobform($extra=false)	{
 	echo "<table border='0' cellpadding='5'>\n";
 	echo "<TR><TD>\n";
 	echo openRoundBorder();
-	echo docpop('runid','<b>Cluster Run Name:</b>');
-	echo "<input type='text' name='runid' value='$runid'>\n";
-	echo "<BR/>\n";
 	echo "<BR/>\n";
 	echo docpop('outdir','<b>Output Directory:</b>');
 	echo "<BR/>\n";
-	echo "<input type='text' name='outdir' value='$outdir' size='38'>\n";
+	echo "<input type='text' name='outdir' value='$outdir' size='42'>\n";
 	echo "<BR/>\n";
 	echo "<BR/>\n";
 	echo docpop('descr','<b>Description of IMAGIC Clustering run:</b>');
@@ -84,38 +80,8 @@ function jobform($extra=false)	{
 	echo closeRoundBorder();
 	echo "</TD></TR><TR>\n";
 	echo "<TD VALIGN='TOP'>\n";
-/*	if (!$imagicAnalysisIds) {
-		echo"
-		<FONT COLOR='RED'><B>No Stacks for this Session</B></FONT>\n";
-	}
-	else {
-		echo "
-		<select name='classid'>\n";
-		foreach ($imagicAnalysisIds as $class) {
-			$stackparams=$particle->getStackParams($stack[stackid]);
 
-			// get pixel size and box size
-			$mpix=$particle->getStackPixelSizeFromStackId($stack['stackid']);
-			if ($mpix) {
-				$apix = $mpix*1E10;
-				$apixtxt=format_angstrom_number($mpix)."/pixel";
-			}
-			$boxsize=($stackparams['bin']) ? $stackparams['boxSize']/$stackparams['bin'] : $stackparams['boxSize'];
-
-			//handle multiple runs in stack
-			$runname=$stackparams[shownstackname];
-			$totprtls=commafy($particle->getNumStackParticles($stack[stackid]));
-			$stackid = $stack['stackid'];
-			echo "<OPTION VALUE='$stackid|--|$apix|--|$boxsize|--|$totprtls'";
-			// select previously set prtl on resubmit
-			if ($stackidval==$stackid) echo " SELECTED";
-			echo ">$runname ($totprtls prtls,";
-			if ($mpix) echo " $apixtxt,";
-			echo " $boxsize pixels)</OPTION>\n";
-		}
-		echo "</SELECT>\n";
-	}
-*/	echo "</TD></TR><TR>\n";
+	echo "</TD></TR><TR>\n";
 	echo "<TD VALIGN='TOP'>\n";
 
 	echo "</TD></TR>\n";
@@ -131,11 +97,7 @@ function jobform($extra=false)	{
 	echo "  <TR><TD VALIGN='TOP'>\n";
 	echo "<b>Clustering Parameters</b>\n";
 	echo "<BR/>\n";
-/*	if  (!$apix) {
-        	echo "<font color='#DD3333' size='-2'>WARNING: These values will not be checked!<br />\n";
-		echo "Make sure you are within the limitations of the box size</font><br />\n";
-	}
-*/	echo "<INPUT TYPE='text' NAME='num_classes' SIZE='4' VALUE='$num_classes'>\n";
+	echo "<INPUT TYPE='text' NAME='num_classes' SIZE='8' VALUE='$num_classes'>\n";
 	echo docpop('num_classes','Number of Classes');
 	echo "<BR/>\n";
 
@@ -177,7 +139,8 @@ function jobform($extra=false)	{
 function runImagicMSAcluster($extra=false)	{
 	$expId=$_GET['expId'];
 	$projectId=getProjectFromExpId($expId);
-	$runid=$_POST['runid'];
+	$analysisId=$_GET['analysisId'];
+	$runname=date("yMd").random_letters();
 	$outdir=$_POST['outdir'];
 	$classvalues=$_POST['imagicAnalysisId'];
 	$ignore_images=$_POST['ignore_images'];
@@ -191,31 +154,17 @@ function runImagicMSAcluster($extra=false)	{
 	list($stackid,$apix,$boxsize,$totpartls) = split('\|--\|',$stackvalues);
 
 	// create python command for executing imagic job file	
-	$pythoncmd = "imagicMSAcluster.py";
-	$pythoncmd.= " --projectId=$projectId --imagicAnalysisId=$imagicAnalysisId --runname=$runid --rundir=$outdir/cluster";
-	$pythoncmd.= " --ignore_images=$ignore_images --num_classes=$num_classes --ignore_members=$ignore_members";
-	$pythoncmd.= " --description=\"$description\"";
-	if ($commit) $pythoncmd.= " --commit\n";
-	else $pythoncmd.=" --no-commit\n";
-/*
-	// write to jobfile
-	$jobfile = "{$runid}_imagicMSAcluster.job";
-	$tmpjobfile = "/tmp/$jobfile";
-	$f = fopen($tmpjobfile,'w');
-	fwrite($f,$pythoncmd);
-	fclose($f);	
+	$cmd = "imagicMSAcluster.py";
+	$cmd.= " --projectid=$projectId --imagicAnalysisId=$analysisId --runname=$runname --rundir=$outdir";
+	$cmd.= " --ignore_images=$ignore_images --num_classes=$num_classes --ignore_members=$ignore_members";
+	$cmd.= " --description=\"$description\"";
+	if ($commit) $cmd.= " --commit";
+	else $cmd.=" --no-commit";
 
-	// create appion directory & copy job & batch files
-	$cmd = "mkdir -p $outdir/$runid\n";
-	$cmd.= "cp $tmpjobfile $outdir/$runid/$jobfile\n";
-	$cmd.= "cd $outdir/$runid\n";
-	$cmd.= "chmod 777 $jobfile\n";
-	exec_over_ssh($_SERVER['HTTP_HOST'], $user, $pass, $cmd, True);
-*/
 	if ($_POST['process']=="run imagic") {
 		if (!($user && $pass)) jobform("<B>ERROR:</B> Enter a user name and password");
 
-		$sub = submitAppionJob($pythoncmd,$outdir,$runid,$expId,'runImagicMSAcluster');
+		$sub = submitAppionJob($cmd,$outdir,$runname,$expId,'runImagicMSAcluster');
 		// if errors:
 		if ($sub) jobform("<b>ERROR:</b> $sub");
 	}
@@ -223,7 +172,7 @@ function runImagicMSAcluster($extra=false)	{
 	processing_header("IMAGIC Clustering","IMAGIC Clustering",$javafunc);
 
 	echo "<pre>";
-	echo htmlspecialchars($pythoncmd);
+	echo htmlspecialchars($cmd);
 	echo "</pre>";
 
 	processing_footer();
@@ -232,3 +181,14 @@ function runImagicMSAcluster($extra=false)	{
 
 
 }
+
+function random_letters($length = 2, $letters = 'abcdefghijklmnopqrstuvwxyz') {
+      $s = '';
+      $lettersLength = strlen($letters)-1;
+     
+      for($i = 0 ; $i < $length ; $i++)	{
+	      $s .= $letters[rand(0,$lettersLength)];
+      }
+
+      return $s;
+ } 
