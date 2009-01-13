602c6bf890e4d52a440fd6aa604f21d83c1da439
Neil Voss <vossman>
2009-01-13 17:28:49 +0000

    ready for testing

--- appion/bin/alignSubStack.py
+++ appion/bin/alignSubStack.py
@@ -16,32 +16,47 @@ import apEMAN
 class subStackScript(appionScript.AppionScript):
 	#=====================
 	def setupParserOptions(self):
-		self.parser.set_usage("Usage: %prog --norefclass=ID --exclude=0,1,... [options]")
-		self.parser.add_option("--norefclass", dest="norefclassid", type="int",
-			help="noref class id", metavar="ID")
-		self.parser.add_option("--exclude", dest="exclude",
-			help="EMAN style classes to EXCLUDE in the new stack (0,5,8)", metavar="0,1,...")
-		self.parser.add_option("--include", dest="include",
-			help="EMAN style classes to INCLUDE in the new stack (0,2,7)", metavar="0,1,...")
+		self.parser.set_usage("Usage: %prog [options]")
+
+		self.parser.add_option("--cluster-id", dest="clusterid", type="int",
+			help="clustering stack id", metavar="ID")
+		self.parser.add_option("--align-id", dest="alignid", type="int",
+			help="alignment stack id", metavar="ID")
+
+		self.parser.add_option("--class-list-keep", dest="keepclasslist",
+			help="list of EMAN style class numbers to include in sub-stack, e.g. --class-list-keep=0,5,3", metavar="#,#")
+		self.parser.add_option("--class-list-drop", dest="dropclasslist",
+			help="list of EMAN style class numbers to exclude in sub-stack, e.g. --class-list-drop=0,5,3", metavar="#,#")
+
 
 	#=====================
 	def checkConflicts(self):
 		if self.params['runname'] is None:
-			apDisplay.printError("new stack name was not defined")
-		if self.params['norefclassid'] is None:
-			apDisplay.printError("noref class ID was not defined")
-		
-		#get the stack ID from the noref class ID
-		norefclassdata = appionData.ApNoRefClassRunData.direct_query(self.params['norefclassid'])
-		norefRun=norefclassdata['norefRun']
-		self.params['stackid'] = norefRun['stack'].dbid
+			apDisplay.printError("New stack name was not defined, e.g. --runname=newstack1")
 
+		### check for missing and duplicate entries
+		if self.params['alignid'] is None and self.params['clusterid'] is None:
+			apDisplay.printError("Please provide either --cluster-id or --align-id")
+		if self.params['alignid'] is not None and self.params['clusterid'] is not None:
+			apDisplay.printError("Please provide only one of either --cluster-id or --align-id")		
+
+		### get the stack ID from the other IDs
+		if self.params['alignid'] is not None:
+			self.alignstackdata = appionData.ApAlignStackData.direct_query(self.params['alignid'])
+			self.params['stackid'] = self.alignstackdata['stack'].dbid
+		elif self.params['clusterid'] is not None:
+			self.clusterstackdata = appionData.ApClusteringStackData.direct_query(self.params['clusterid'])
+			self.alignstackdata = clusterstackdata['clusterrun']['alignstack']
+			self.params['stackid'] = self.alignstackdata['stack'].dbid
+
+		### check and make sure we got the stack id
 		if self.params['stackid'] is None:
 			apDisplay.printError("stackid was not defined")
-		if self.params['exclude'] is None and self.params['include'] is None:
-			apDisplay.printError("noref classes to be included/excluded was not defined")
-		if self.params['exclude'] is not None and self.params['include'] is not None:
-			apDisplay.printError("both include and exclude were defined, only one is allowed")
+
+		if self.params['keepclasslist'] is None and self.params['dropclasslist'] is None:
+			apDisplay.printError("class numbers to be included/excluded was not defined")
+		if self.params['keepclasslist'] is not None and self.params['dropclasslist'] is not None:
+			apDisplay.printError("both --class-list-keep and --class-list-drop were defined, only one is allowed")
 
 	#=====================
 	def setRunDir(self):
@@ -52,7 +67,7 @@ class subStackScript(appionScript.AppionScript):
 
 	#=====================
 	def start(self):
-		#new stack path
+		### new stack path
 		stackdata = apStack.getOnlyStackData(self.params['stackid'])
 		oldstack = os.path.join(stackdata['path']['path'], stackdata['name'])
 		newstack = os.path.join(self.params['rundir'], stackdata['name'])
@@ -60,32 +75,42 @@ class subStackScript(appionScript.AppionScript):
 
 		### list of classes to be excluded
 		excludelist = []
-		if self.params['exclude'] is not None:
-			excludestrlist = self.params['exclude'].split(",")
-			for excld in excludestrlist:
-				excludelist.append(int(excld.strip()))
+		if self.params['dropclasslist'] is not None:
+			excludestrlist = self.params['dropclasslist'].split(",")
+			for excludeitem in excludestrlist:
+				excludelist.append(int(excludeitem.strip()))
 		apDisplay.printMsg("Exclude list: "+str(excludelist))
 
 		### list of classes to be included
 		includelist = []
-		if self.params['include'] is not None:
-			includestrlist = self.params['include'].split(",")
-			for incld in includestrlist:
-				includelist.append(int(incld.strip()))		
+		if self.params['keepclasslist'] is not None:
+			includestrlist = self.params['keepclasslist'].split(",")
+			for includeitem in includestrlist:
+				includelist.append(int(includeitem.strip()))		
 		apDisplay.printMsg("Include list: "+str(includelist))
 
-		#get particles from noref class run
+		### get particles from align or cluster stack
+		if self.params['alignid'] is not None:
+			alignpartq =  appionData.ApAlignParticlesData()
+			alignpartq['alignstack'] = self.alignstackdata
+			particles = alignpartq.query()
 		norefclassdata = appionData.ApNoRefClassRunData.direct_query(self.params['norefclassid'])
 		classpartq = appionData.ApNoRefClassParticlesData()
 		classpartq['classRun'] = norefclassdata
 		classpartdatas = classpartq.query()
 
+		### write included particles to text file
 		includeParticle = []
 		excludeParticle = 0
-		for classpart in classpartdatas:
-			#write to text file
-			classnum = classpart['classNumber']-1
-			emanstackpartnum = classpart['noref_particle']['particle']['particleNumber']-1
+		for part in particles:
+			classnum = part['partnum']-1
+			if 'alignparticle' in part:
+				alignpart = part['alignparticle']
+				classnum = int(part['refnum'])
+			else:
+				alignpart = part
+				classnum = int(part['ref']['refnum'])
+			emanstackpartnum = alignpart['stackpart']['particleNumber']-1
 			if includelist and classnum in includelist:
 				includeParticle.append(emanstackpartnum)
 			elif excludelist and not classnum in excludelist:
@@ -95,31 +120,34 @@ class subStackScript(appionScript.AppionScript):
 		includeParticle.sort()
 		apDisplay.printMsg("Keeping "+str(len(includeParticle))+" and excluding "+str(excludeParticle)+" particles")
 
-		#print includeParticle
+		print includeParticle
 
 		### write kept particles to file
-		self.params['keepfile'] = os.path.join(norefclassdata['norefRun']['path']['path'], "keepfile-"+self.timestamp+".list")
+		self.params['keepfile'] = os.path.join(self.params['rundir'], "keepfile-"+self.timestamp+".list")
 		apDisplay.printMsg("writing to keepfile "+self.params['keepfile'])
 		kf = open(self.params['keepfile'], "w")
 		for partnum in includeParticle:
 			kf.write(str(partnum)+"\n")
 		kf.close()
 
-		#get number of particles
+		### get number of particles
 		numparticles = len(includeParticle)
 		if excludelist:
-			self.params['description'] += ( " ... %d particle substack of norefclassid %d with %s classes excluded" 
-				% (numparticles, self.params['norefclassid'], self.params['exclude']))
+			self.params['description'] += ( " ... %d particle substack with %s classes excluded" 
+				% (numparticles, self.params['dropclasslist']))
 		elif includelist:
-			self.params['description'] += ( " ... %d particle substack of norefclassid %d with %s classes included" 
-				% (numparticles, self.params['norefclassid'], self.params['include']))	
-		#create the new sub stack
+			self.params['description'] += ( " ... %d particle substack with %s classes included" 
+				% (numparticles, self.params['keepclasslist']))	
+
+		### create the new sub stack
 		apStack.makeNewStack(oldstack, newstack, self.params['keepfile'])
 		if not os.path.isfile(newstack):
 			apDisplay.printError("No stack was created")
 
 		apStack.commitSubStack(self.params)
 		apStack.averageStack(stack=newstack)
+		newstackid = apStack.getStackIdFromPath(newstack)
+		apStackMeanPlot.makeStackMeanPlot(newstackid)
 
 #=====================
 if __name__ == "__main__":
