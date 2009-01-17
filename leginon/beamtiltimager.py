0cdfdcd2283e78ea4503258bcd5adc75e641490a
Anchi Cheng <acheng>
2009-01-17 03:28:38 +0000

    ace2 in this node and print astig on tableau

--- leginon/beamtiltimager.py
+++ leginon/beamtiltimager.py
@@ -14,12 +14,15 @@ import threading
 import event
 import time
 import math
-from pyami import correlator, peakfinder, imagefun
+from pyami import correlator, peakfinder, imagefun, numpil,arraystats
 import numpy
 import copy
 import gui.wx.BeamTiltImager
 import player
 import tableau
+import subprocess
+import re
+import os
 
 class BeamTiltImager(acquisition.Acquisition):
 	panelclass = gui.wx.BeamTiltImager.Panel
@@ -65,6 +68,7 @@ class BeamTiltImager(acquisition.Acquisition):
 		self.imageshiftcalclient = calibrationclient.ImageShiftCalibrationClient(self)
 		self.euclient = calibrationclient.EucentricFocusClient(self)
 		self.corclient = corrector.CorrectorClient(self)
+		self.ace2exe = self.getACE2Path()
 
 	def alignRotationCenter(self, defocus1, defocus2):
 		try:
@@ -117,10 +121,18 @@ class BeamTiltImager(acquisition.Acquisition):
 		self.tabscale = None
 		self.displayTableau(self.tabimage)
 
-	def insertTableau(self, image, angle, rad):
+	def insertTableau(self, imagedata, angle, rad):
+		image = imagedata['image']
 		pow = imagefun.power(image)
 		binning = self.settings['tableau binning']
 		binned = imagefun.bin(pow, binning)
+		ctfdata = self.estimateCTF(imagedata)
+		s = '%.1f' % (ctfdata['astig'],)
+		t = numpil.textArray(s)
+		min = arraystats.min(binned)
+		max = arraystats.max(binned)
+		t = min + t * (max-min)
+		imagefun.pasteInto(t, binned, (20,20))
 		self.tableauimages.append(binned)
 		self.tableauangles.append(angle)
 		self.tableaurads.append(rad)
@@ -189,10 +201,11 @@ class BeamTiltImager(acquisition.Acquisition):
 			self.instrument.tem.BeamTilt = oldbt
 			angle = anglelist[i]
 			rad = radlist[i]
+
 			if self.settings['tableau type'] == 'split image':
 				self.splitTableau(imagedata['image'])
 			elif self.settings['tableau type'] == 'beam tilt series':
-				self.insertTableau(imagedata['image'], angle, rad)
+				self.insertTableau(imagedata, angle, rad)
 			try:
 				shiftinfo = self.correlateOriginal(i,imagedata)
 			except Exception, e:
@@ -320,12 +333,95 @@ class BeamTiltImager(acquisition.Acquisition):
 		bt = {}
 		if self.tabscale is not None:
 			bt['x'] = deltacol * self.settings['beam tilt']/self.tabscale
-			bt['y'] = deltarow * self.settings['beam tilt']/self.tabscale
+			bt['y'] = -deltarow * self.settings['beam tilt']/self.tabscale
 			oldbt = self.instrument.tem.BeamTilt
 			self.logger.info('Old beam tilt: %.4f, %.4f' % (oldbt['x'],oldbt['y'],))
 			newbt = {'x': oldbt['x'] + bt['x'], 'y': oldbt['y'] + bt['y']}
 			self.instrument.tem.BeamTilt = newbt
 			self.logger.info('New beam tilt: %.4f, %.4f' % (newbt['x'],newbt['y'],))
 			self.simulateTarget()
+			self.logger.info('New beam tilt: %.4f, %.4f' % (newbt['x'],newbt['y'],))
 		else:
 			self.logger.warning('need more than one beam tilt images in tableau to navigate')
+
+	def getACE2Path(self):
+		exename = 'ace2.exe'
+		ace2exe = subprocess.Popen("which "+exename, shell=True, stdout=subprocess.PIPE).stdout.read().strip()
+		if not os.path.isfile(ace2exe):
+			self.logger.error(exename+" was not found in path ")
+		return ace2exe
+
+	def estimateCTF(self, imagedata):
+		mag = imagedata['scope']['magnification']
+		tem = imagedata['scope']['tem']
+		cam = imagedata['camera']['ccdcamera']
+		pixelsize = self.btcalclient.getPixelSize(mag, tem, cam)
+		inputparams = {
+			'input': os.path.join(imagedata['session']['image path'],imagedata['filename']+".mrc"),
+			'cs': 2.0,
+			'kv': imagedata['scope']['high tension']/1000.0,
+			'apix': pixelsize*1e10,
+			'binby': 1,
+		}
+
+		### make standard input for ACE 2
+		commandline = ( self.ace2exe
+			+ " -i " + str(inputparams['input'])
+			+ " -b " + str(inputparams['binby'])
+			+ " -c " + str(inputparams['cs'])
+			+ " -k " + str(inputparams['kv'])
+			+ " -a " + str(inputparams['apix']) + "\n" )
+
+		### run ace2
+		self.logger.info("run ace2 on %s" % (imagedata['filename']))
+		#aceoutf = open("ace2.out", "a")
+		#aceerrf = open("ace2.err", "a")
+		t0 = time.time()
+		#ace2proc = subprocess.Popen(commandline, shell=True, stdout=aceoutf, stderr=aceerrf)
+		ace2proc = subprocess.Popen(commandline, shell=True)
+		ace2proc.wait()
+
+		### check if ace2 worked
+		imagelog = imagedata['filename']+".mrc"+".ctf.txt"
+		if not os.path.isfile(imagelog):
+			### ace2 always crashes on first image??? .fft_wisdom file??
+			time.sleep(1)
+			#ace2proc = subprocess.Popen(commandline, shell=True, stdout=aceoutf, stderr=aceerrf)
+			ace2proc = subprocess.Popen(commandline, shell=True)
+			ace2proc.wait()
+		#aceoutf.close()
+		#aceerrf.close()
+		if not os.path.isfile(imagelog):
+			self.logger.warning("ace2 did not run")
+
+		### parse log file
+		self.ctfvalues = {}
+		logf = open(imagelog, "r")
+		for line in logf:
+			sline = line.strip()
+			if re.search("^Final Defocus:", sline):
+				parts = sline.split()
+				self.ctfvalues['defocus1'] = float(parts[2])
+				self.ctfvalues['defocus2'] = float(parts[3])
+				### convert to degrees
+				self.ctfvalues['angle_astigmatism'] = math.degrees(float(parts[4]))
+			elif re.search("^Amplitude Contrast:",sline):
+				parts = sline.split()
+				self.ctfvalues['amplitude_contrast'] = float(parts[2])
+			elif re.search("^Confidence:",sline):
+				parts = sline.split()
+				self.ctfvalues['confidence'] = float(parts[1])
+				self.ctfvalues['confidence_d'] = float(parts[1])
+		logf.close()
+
+		### summary stats
+		avgdf = (self.ctfvalues['defocus1']+self.ctfvalues['defocus2'])/2.0
+		ampconst = 100.0*self.ctfvalues['amplitude_contrast']
+		pererror = 100.0 * (self.ctfvalues['defocus1']-self.ctfvalues['defocus2']) / avgdf
+		self.ctfvalues['astig'] = pererror
+		self.logger.info("Amplitude contrast: %.2f percent"%(ampconst))
+		self.logger.info("Final confidence: %.3f"%(self.ctfvalues['confidence']))
+		self.logger.info("Defocus: %.3f x %.3f um, angle %.2f degress (%.2f %% astigmatism)"%
+			(self.ctfvalues['defocus1']*1.0e6, self.ctfvalues['defocus2']*1.0e6, self.ctfvalues['angle_astigmatism'],pererror ))
+
+		return self.ctfvalues
