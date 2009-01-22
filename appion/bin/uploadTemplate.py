b7930588431769942f0d576dddf4a3e08ce094f3
Neil Voss <vossman>
2009-01-22 23:18:03 +0000

    add align and cluster links

--- appion/bin/uploadTemplate.py
+++ appion/bin/uploadTemplate.py
@@ -16,6 +16,7 @@ import apDisplay
 import apDatabase
 import appionData
 import apEMAN
+import apFile
 import apProject
 
 #=====================
@@ -24,40 +25,76 @@ class uploadTemplateScript(appionScript.AppionScript):
 	def setupParserOptions(self):
 		self.parser.set_usage( "Usage: %prog --template=<name> --apix=<pixel> --session=<session> --diam=<int> "
 			+"--description='<text>' [options]")
+
+		### required info
 		self.parser.add_option("--apix", dest="apix", type="float",
 			help="Template pixel size in Angstroms per pixel", metavar="FLOAT")
 		self.parser.add_option("--diam", dest="diam", type="int",
 			help="Approximate diameter of particle (in Angstroms)", metavar="INT")
-		self.parser.add_option("-t", "--template", dest="template",
-			help="Filename of the template (wild cards accepted)", metavar="FILE")
 		self.parser.add_option("-s", "--session", dest="session",
 			help="Session name associated with template (e.g. 06mar12a)", metavar="INT")
-		self.parser.add_option("--norefid", dest="norefid", type="int",
-			help="ID for reference-free alignment (optional)", metavar="INT")
+
+		### optional input methods
+		self.parser.add_option("-t", "--template", dest="template",
+			help="Filename of the template (wild cards accepted)", metavar="FILE")
+		self.parser.add_option("--alignid", dest="alignid", type="int",
+			help="ID for particle alignment (optional)", metavar="INT")
+		self.parser.add_option("--clusterid", dest="clusterid", type="int",
+			help="ID for particle clustering (optional)", metavar="INT")
 		self.parser.add_option("--stackid", dest="stackid", type="int",
 			help="ID for particle stack (optional)", metavar="INT")
-		self.parser.add_option("--stackimgnum", dest="stackimgnum", type="int",
-			help="Particle number in stack", metavar="INT")
+
+		### img number list
+		self.parser.add_option("--imgnums", dest="imgnums",
+			help="Particle numbers in stack, e.g. 0,15,3", metavar="LIST")
 		self.parser.add_option("--avgstack", dest="avgstack", default=False,
 			action="store_true", help="Average all particles in stack for template")
 
 	#=====================
 	def checkConflicts(self):
-		# make sure the necessary parameters are set
-		if self.params['stackid'] is None and self.params['apix'] is None:
-			apDisplay.printError("enter a pixel size")
-		if self.params['diam'] is None:
-			apDisplay.printError("enter the particle diameter in Angstroms")
-		if self.params['stackid'] is not None and self.params['norefid'] is not None:
-			apDisplay.printError("only one of either stackid or norefid can be used, NOT both")
-		if self.params['template'] is None and self.params['stackid'] is None and self.params['norefid'] is None:
-			apDisplay.printError("enter a template root name (wild cards are valid)")
-		if self.params['template'] is not None:
-			self.params['template'] = os.path.abspath(self.params['template'])
-		if self.params['session'] is None:
-			apDisplay.printError("enter a session ID")
+		### make sure the necessary parameters are set
 		if self.params['description'] is None:
 			apDisplay.printError("enter a template description")
+		if self.params['diam'] is None:
+			apDisplay.printError("enter the particle diameter in Angstroms")
+
+		### make sure we have something
+		if (self.params['template'] is None 
+		  and self.params['stackid'] is None
+		  and self.params['clusterid'] is None
+		  and self.params['alignid'] is None
+		):
+			apDisplay.printError("enter a template root name (wild cards are valid)")
+
+		### check if apix is needed
+		if (self.params['apix'] is None
+		  and self.params['stackid'] is None
+		  and self.params['clusterid'] is None
+		  and self.params['alignid'] is None
+		):
+			apDisplay.printError("enter a pixel size")
+
+		### check for session
+		if self.params['session'] is None:
+			if self.params['alignid'] is not None:
+				alignstackdata = appionData.ApAlignStackData.direct_query(self.params['alignid'])
+				stackid = alignstackdata['stack'].dbid
+				sessiondata = apStack.getSessionDataFromStackId(stackid)
+				self.params['session'] = sessiondata['name']
+			elif self.params['stackid'] is not None:
+				stackid = self.params['stackid']
+				sessiondata = apStack.getSessionDataFromStackId(stackid)
+				self.params['session'] = sessiondata['name']
+			elif self.params['alignid'] is not None:
+				clusterstackdata = appionData.ApClusteringStackData.direct_query(self.params['clusterid'])
+				stackid = clusterstackdata['clusterrun']['alignstack']['stack'].dbid
+				sessiondata = apStack.getSessionDataFromStackId(stackid)
+				self.params['session'] = sessiondata['name']
+		if self.params['session'] is  None:
+			apDisplay.printError("Could not find session")
+
+		if self.params['template'] is not None:
+			self.params['template'] = os.path.abspath(self.params['template'])
 
 	#=====================
 	def setRunDir(self):
@@ -70,96 +107,117 @@ class uploadTemplateScript(appionScript.AppionScript):
 
 	#=====================
 	def useStackForTemplate(self):
-		apDisplay.printMsg("Using stack to make template")
-		### get stack data (path, file)
+		apDisplay.printMsg("Using stack to make templates")
+		sessiondata = apStack.getSessionDataFromStackId(self.params['stackid'])
+		self.params['session'] = sessiondata['name']
+		self.params['apix'] = apStack.getStackPixelSizeFromStackId(self.params['stackid'])
 		stackdata = apStack.getOnlyStackData(self.params['stackid'])
-		if self.params['apix'] is None:
-			self.params['apix'] = apStack.getStackPixelSizeFromStackId(self.params['stackid'])
-		stackpath = stackdata['path']['path']
-		stackname = stackdata['name']
-		absstackpath = os.path.join(stackpath, stackname)
-		abstemplatepath = None
-
-		### averaging all files together
-		if self.params['avgstack'] is True:
-			apDisplay.printMsg("Averaging all images in stack")
-			templatename = "template"+str(self.params['stackid'])+"avg.mrc"
-			abstemplatepath = os.path.join(stackpath,templatename)
-			### average all images using proc2d
-			emancmd = "proc2d %s %s average" % (absstackpath, abstemplatepath)
-		elif self.params['stackimgnum'] is not None:
-			### check to see if stackimagenum is within the boundary of the stack
-			# there has to be a faster way of doing this... -neil
-			stacksize = len(apStack.getStackParticlesFromId(self.params['stackid']))
-			num = self.params['stackimgnum']
-			if num > stacksize or num < 0:
-				apDisplay.printError("'stackimagenum' is NOT within the boundary of the stack: 0->"+str(stacksize))
-			apDisplay.printMsg("Extracting image %i from stack" % self.params['stackimgnum'])
-			templatename = "template"+str(self.params['stackimgnum'])+".mrc"
-			abstemplatepath= os.path.join(stackpath,templatename)
-			### run proc2d with params['stackimgnum']
-			emancmd = "proc2d %s %s first=%i last=%i" % (absstackpath, abstemplatepath, num, num)
-		else:
-			apDisplay.printError("could not create template from stack specify either 'stackimgnum' or 'avgstack'")
-
-		###  create template
-		apDisplay.printMsg("creating "+templatename+" in "+stackpath+"...\n")
-		apEMAN.executeEmanCmd(emancmd)
+		stackfile = os.path.join(stackdata['path']['path'], stackdata['name'])
 		self.params['templatelist'] = []
-		self.params['templatelist'].append(abstemplatepath)
+
+		if self.params['avgstack'] is True:
+			### averaging all particles together
+			apDisplay.printMsg("Averaging all images in stack")
+			templatename = "stack%d-average.mrc"%(self.params['stackid'])
+			abstemplatepath = os.path.join(stackdata['path']['path'], templatename)
+			### average all images using proc2d
+			emancmd = "proc2d %s %s average" % (stackfile, abstemplatepath)
+			if os.path.isfile(abstemplatepath):
+				self.params['templatelist'] = [abstemplatepath]
+
+		elif self.params['imgnums'] is not None:
+			### check to see if stackimagenum is within the boundary of the stack
+			numpart = apFile.numImagesInStack(stackfile)
+			for i in self.params['imgnums'].split(","):
+				partnum = int(i)
+				if partnum > numpart or partnum < 0:
+					apDisplay.printError("'imgnums' is NOT within the boundary of the stack: %d > %d"%(partnum,numpart))
+				apDisplay.printMsg("Extracting image %d from stack" % partnum)
+				templatename = "stack%d-particle%d.mrc"%(self.params['stackid'],partnum)
+				abstemplatepath= os.path.join(stackdata['path']['path'], templatename)
+				### run proc2d with params['stackimgnum']
+				emancmd = "proc2d %s %s first=%d last=%d" % (stackfile, abstemplatepath, partnum, partnum)
+				###  create template
+				apDisplay.printMsg("creating "+templatename)
+				apEMAN.executeEmanCmd(emancmd)
+				if os.path.isfile(abstemplatepath):
+					self.params['templatelist'].append(abstemplatepath)
 
 	#=====================
-	def useRefFreeForTemplate(self):
-		apDisplay.printMsg("Using reference-free class to make template")
-		norefClassdata = appionData.ApNoRefClassRunData.direct_query(self.params['norefid'])
-		
-		#Get class average file path through ApNoRefRunData
-		norefRun=norefClassdata['norefRun']
-		norefpath = norefRun['path']['path']
-		norefname = norefRun['name']
-		#norefpath = os.path.join(norefpath, norefname)
-				
-		#Get class average file name
-		norefClassFile = norefClassdata['classFile']
-		norefClassFile+=".img"
-
-		#complete path of the class average file
-		absnorefpath = os.path.join(norefpath, norefClassFile)
-		apDisplay.printMsg("noref class file: "+absnorefpath)
-
-		#get the num of classes
-		classnum = norefClassdata['num_classes']
-		imgnum = self.params['stackimgnum']	
-
-		#make sure that params['stackimgnum'] is less than the num of classes
-		if imgnum > classnum or imgnum < 0:
-				apDisplay.printError("'stackimagenum' is NOT within the boundary of the stack: 0->"+str(classnum))
-
-		templatename = "template"+str(imgnum)+".mrc"
-		abstemplatepath= os.path.join(norefpath, templatename)
-
-		#run proc2d with params['stackimgnum']
-		apDisplay.printMsg("creating "+templatename+" in "+norefpath+"...\n")
-		emancmd = "proc2d %s %s first=%i last=%i" % (absnorefpath, abstemplatepath, imgnum, imgnum)
-		apEMAN.executeEmanCmd(emancmd)
-		
-		#Add file names to params['templatelist']
+	def useAlignForTemplate(self):
+		apDisplay.printMsg("Using alignment stack to make templates")
+		alignstackdata = appionData.ApAlignStackData.direct_query(self.params['alignid'])
+		self.params['apix'] = alignstackdata['pixelsize']
+		stackfile = os.path.join(alignstackdata['path']['path'], alignstackdata['imagicfile'])
 		self.params['templatelist'] = []
-		self.params['templatelist'].append(abstemplatepath)
+		stackid = alignstackdata['stack'].dbid
+		sessiondata = apStack.getSessionDataFromStackId(stackid)
+		self.params['session'] = sessiondata['name']
+
+		### check to see if stackimagenum is within the boundary of the stack
+		numpart = apFile.numImagesInStack(stackfile)
+		for i in self.params['imgnums'].split(","):
+			partnum = int(i)
+			if partnum > numpart or partnum < 0:
+				apDisplay.printError("'imgnums' is NOT within the boundary of the stack: %d > %d"%(partnum,numpart))
+			apDisplay.printMsg("Extracting image %d from align stack" % partnum)
+			templatename = "align%d-average%d.mrc"%(self.params['alignid'],partnum)
+			abstemplatepath= os.path.join(alignstackdata['path']['path'], templatename)
+			### run proc2d with params['stackimgnum']
+			emancmd = "proc2d %s %s first=%d last=%d" % (stackfile, abstemplatepath, partnum, partnum)
+			###  create template
+			apDisplay.printMsg("creating "+templatename)
+			apEMAN.executeEmanCmd(emancmd)
+			if os.path.isfile(abstemplatepath):
+				self.params['templatelist'].append(abstemplatepath)
+
+	#=====================
+	def useClusterForTemplate(self):
+		apDisplay.printMsg("Using clustering stack to make templates")
+		clusterstackdata = appionData.ApClusteringStackData.direct_query(self.params['clusterid'])
+		self.params['apix'] = clusterstackdata['pixelsize']
+		stackfile = os.path.join(clusterstackdata['path']['path'], clusterstackdata['imagicfile'])
+		self.params['templatelist'] = []
+		stackid = clusterstackdata['clusterrun']['alignstack']['stack'].dbid
+		sessiondata = apStack.getSessionDataFromStackId(stackid)
+		self.params['session'] = sessiondata['name']
+
+		### check to see if stackimagenum is within the boundary of the stack
+		numpart = apFile.numImagesInStack(stackfile)
+		for i in self.params['imgnums'].split(","):
+			partnum = int(i)
+			if partnum > numpart or partnum < 0:
+				apDisplay.printError("'imgnums' is NOT within the boundary of the stack: %d > %d"%(partnum,numpart))
+			apDisplay.printMsg("Extracting image %d from cluster stack" % partnum)
+			templatename = "cluster%d-average%d.mrc"%(self.params['clusterid'],partnum)
+			abstemplatepath= os.path.join(clusterstackdata['path']['path'], templatename)
+			### run proc2d with params['stackimgnum']
+			emancmd = "proc2d %s %s first=%d last=%d" % (stackfile, abstemplatepath, partnum, partnum)
+			###  create template
+			apDisplay.printMsg("creating "+templatename)
+			apEMAN.executeEmanCmd(emancmd)
+			if os.path.isfile(abstemplatepath):
+				self.params['templatelist'].append(abstemplatepath)
 
 	#=====================
 	def start(self):
 		if self.params['stackid'] is not None:
 			self.useStackForTemplate()
-		elif self.params['norefid'] is not None:
-			self.useRefFreeForTemplate()
+		elif self.params['alignid'] is not None:
+			self.useAlignForTemplate()
+		elif self.params['clusterid'] is not None:
+			self.useClusterForTemplate()
 		else:
 			apDisplay.printMsg("Using local file: '"+str(self.params['template'])+"' to upload template")
 			#find the number of template files
 			apTemplate.findTemplates(self.params)
 
+		### check for templates
 		apDisplay.printColor("Template List:","green")
 		pprint.pprint(self.params['templatelist'])
+		if not self.params['templatelist']:
+			apDisplay.printError("Could not find templates")
+
 		time.sleep(2)
 
 		# copy templates to final location
