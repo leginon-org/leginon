fe1b5b75ef92d6d1b1efb31383303f1998e80a7c
Anchi Cheng <acheng>
2009-01-24 03:00:51 +0000

    link to report

--- myamiweb/processing/tomosummary.php
+++ myamiweb/processing/tomosummary.php
@@ -28,7 +28,7 @@ $projectId=$_POST['projectId'];
 $javascript = "<script src='../js/viewer.js'></script>\n";
 $javascript.= editTextJava();
 
-processing_header("Tomographic Reconstruction Summary","Reconstruction Summary Page", $javascript);
+processing_header("Tomographic Reconstruction Summary","Tomogram Summary Page", $javascript);
 
 // edit description form
 echo "<form name='templateform' method='post' action='$formAction'>\n";
@@ -39,49 +39,55 @@ $particle = new particledata();
 // --- Get Reconstruction Data
 $tomograms = $particle->getTomogramsFromSession($sessionId);
 if ($tomograms) {
-
 	$html = "<table class='tableborder' border='1' cellspacing='1' cellpadding='5'>\n";
 	$html .= "<TR>\n";
-	$display_keys = array ( 'tiltseries','snapshot','description','download');
+	$display_keys = array ( 'tiltseries','full','volume','snapshot','description');
 	foreach($display_keys as $key) {
 		$html .= "<TD><span class='datafield0'>".$key."</span> </TD> ";
 	}
-
-	foreach ($tomograms as $reconrun) {
-		$reconid = $reconrun['DEF_id'];
-		$tiltseriesinfo = $particle->getTiltSeriesInfo($reconrun['tiltseries']);
+	foreach ($tomograms as $tomo) {
+		$tomogramid = $tomo['DEF_id'];
+		$tomogram = $particle->getTomogramInfo($tomogramid);
+		if ($tomogram['prtlimage']) {
+			$number = $tomogram['number'];
+			$center0 = $particle->getTomoCenter($tomogram['centerx'],
+					$tomogram['centery'],$tomogram['prtlimage'],$tomogram['tiltseries']);
+		} else {
+			$number = '';
+			$center0 = Null;
+		}
+		$centerprint = ($center0) ? '('.floor($center0['x']).','.floor($center0['y']).')' : 'Full';
 		// update description
-		if ($_POST['updateDesc'.$reconid]) {
-			updateDescription('ApTomogramData', $reconid, $_POST['newdescription'.$reconid]);
+		if ($_POST['updateDesc'.$tomogramid]) {
+			updateDescription('ApTomogramData', $tomogramid, $_POST['newdescription'.$tomogramid]);
 
-			$reconrun['description']=$_POST['newdescription'.$reconid];
+			$tomogram['description']=$_POST['newdescription'.$tomogramid];
 
 		}
-		$tiltseriesnumber = $tiltseriesinfo[0]['number'];
+		$tiltseriesnumber = $tomogram['tiltnumber'];
 		// PRINT INFO
 		$html .= "<TR>\n";
 		$html .= "<TD>$tiltseriesnumber</TD>\n";
-#		$html .= "<td>$reconrun[name]</td>\n";
-#		$html .= "<TD><A HREF='reconreport.php?expId=$expId&reconId=$reconrun[DEF_id]'>$reconrun[name]</A></TD>\n";
+		$html .= "<TD>".$tomogram['full']."</TD>\n";
+		$html .= "<TD><A HREF='tomoreport.php?expId=$expId&tomoId=$tomogramid'>$number<br/>$centerprint</A></TD>\n";
 		$html .= "<td>";
-    $snapfile = $reconrun['path'].'/snapshot.png';
+    $snapfile = $tomogram['path'].'/snapshot.png';
     $html .= "<A HREF='loadimg.php?filename=$snapfile' target='snapshot'><IMG SRC='loadimg.php?filename=$snapfile' HEIGHT='80'>\n";
 		$html .= "</td>\n";
 
 		# add edit button to description if logged in
-		$descDiv = ($_SESSION['username']) ? editButton($reconid,$reconrun['description']) : $reconrun['description'];
+		$descDiv = ($_SESSION['username']) ? editButton($tomogramid,$tomogram['description']) : $tomogram['description'];
 
 		$html .= "<td>$descDiv</td>\n";
-$downloadDiv = "<a href=downloadtomo.php?tomogramId=$reconrun[DEF_id]>[Download Tomogram]</a><br>";
-		$html .= "<td>$downloadDiv</td>\n";
+$downloadDiv = "<a href=downloadtomo.php?tomogramId=$tomogramid>[Download Tomogram]</a><br>";
+#		$html .= "<td>$downloadDiv</td>\n";
 		$html .= "</TR>\n";
 	}
-
 	$html .= "</table>\n";
 	echo $html;
 //	echo $particle->displayParticleStats($particleruns, $display_keys, $inspectcheck, $mselexval);
 } else {
-	echo "no reconstruction information available";
+	echo "no tomograms available";
 }
 
 
