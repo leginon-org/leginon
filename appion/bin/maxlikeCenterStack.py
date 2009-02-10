36f83b9e70edcaf562b941ac0852dd5e097bd4c4
Neil Voss <vossman>
2009-02-10 18:39:02 +0000

    should work max stack size of 1.2 GB

--- appion/bin/maxlikeCenterStack.py
+++ appion/bin/maxlikeCenterStack.py
@@ -6,6 +6,9 @@ import shutil
 #appion
 import appionScript
 import apStack
+import apFile
+import apXmipp
+import apImagicFile
 import apDisplay
 import apEMAN
 
@@ -15,12 +18,31 @@ class centerStackScript(appionScript.AppionScript):
 		self.parser.set_usage("Usage: %prog --stack-id=ID [options]")
 		self.parser.add_option("-s", "--stack-id", dest="stackid", type="int",
 			help="Stack database id", metavar="ID")
-		self.parser.add_option("-m", "--mask", dest="mask", type="int",
-			help="Outer mask")
-		self.parser.add_option("-x", "--maxshift", dest="maxshift", type="int",
-			help="Maximum shift")
+		self.parser.add_option("--maxshift", dest="maxshift", type="int",
+			help="Maximum shift", metavar="#")
 		self.parser.add_option("--new-stack-name", dest="runname",
 			help="Run name", metavar="STR")
+
+		self.parser.add_option("--lowpass", "--lp", dest="lowpass", type="int", default=10,
+			help="Low pass filter radius (in Angstroms) not applied to final particles", metavar="#")
+		self.parser.add_option("--highpass", "--hp", dest="highpass", type="int", default=400,
+			help="High pass filter radius (in Angstroms) not applied to final particles", metavar="#")
+
+		### true/false
+		self.parser.add_option("-M", "--mirror", dest="mirror", default=False,
+			action="store_true", help="Use mirror method")
+		self.parser.add_option("--no-mirror", dest="mirror", default=False,
+			action="store_false", help="Do NOT use rotate method")
+		self.parser.add_option("-R", "--rotate", dest="rotate", default=False,
+			action="store_true", help="Use rotate method")
+		self.parser.add_option("--no-rotate", dest="rotate", default=False,
+			action="store_false", help="Do NOT use rotate method")
+
+		### choices
+		self.convergemodes = ( "normal", "fast", "slow" )
+		self.parser.add_option("--converge", dest="converge",
+			help="Convergence criteria mode", metavar="MODE", 
+			type="choice", choices=self.convergemodes, default="normal" )
 
 	#=====================
 	def checkConflicts(self):
@@ -30,54 +52,191 @@ class centerStackScript(appionScript.AppionScript):
 			apDisplay.printError("substack description was not defined")
 		if self.params['runname'] is None:
 			apDisplay.printError("new stack name was not defined")
-		
+		stackdata = apStack.getOnlyStackData(self.params['stackid'])
+		oldstack = os.path.join(stackdata['path']['path'], stackdata['name'])
+		stacksize = apFile.fileSize(datafilename)/1024.0/1024.0
+		if stacksize > 1200:
+			apDisplay.printError("Stack is too large to read "+str(round(stacksize,1))+" MB")
 
 	#=====================
 	def setRunDir(self):
 		stackdata = apStack.getOnlyStackData(self.params['stackid'], msg=False)
 		path = stackdata['path']['path']
 		uppath = os.path.dirname(os.path.abspath(path))
-		# add mask & maxshift to rundir if specifie
-		if self.params['mask'] is not None:
-			self.params['runname'] = self.params['runname']+"_"+str(self.params['mask'])
-		if self.params['maxshift'] is not None:
-			self.params['runname'] = self.params['runname']+"_"+str(self.params['maxshift'])
 		self.params['rundir'] = os.path.join(uppath, self.params['runname'])
-		
+
+	#=====================
+	def writeXmippLog(self, text):
+		f = open("xmipp.log", "a")
+		f.write(apParam.getLogHeader())
+		f.write(text+"\n")
+		f.close()
+
+	#=====================
+	def runMaxlike(self):
+		stackdata = apStack.getOnlyStackData(self.params['stackid'])
+		apix = apStack.getStackPixelSizeFromStackId(self.params['stackid'])
+		stackfile = os.path.join(stackdata['path']['path'], stackdata['name'])
+
+		### process stack to local file
+		self.params['localstack'] = os.path.join(self.params['rundir'], self.timestamp+".hed")
+		proccmd = "proc2d "+stackfile+" "+self.params['localstack']+" apix="+str(apix)
+		if self.params['highpass'] > 1:
+			proccmd += " hp="+str(self.params['highpass'])
+		if self.params['lowpass'] > 1:
+			proccmd += " lp="+str(self.params['lowpass'])
+		proccmd += " last="+str(self.params['numpart'])
+		apEMAN.executeEmanCmd(proccmd, verbose=True)
+
+		### convert stack into single spider files
+		self.partlistdocfile = apXmipp.breakupStackIntoSingleFiles(self.params['localstack'])
+
+		### setup Xmipp command
+		aligntime = time.time()
+		xmippopts = ( " "
+			+" -i "+os.path.join(self.params['rundir'], self.partlistdocfile)
+			+" -nref 1 "
+			+" -iter 100 "
+			+" -o "+os.path.join(self.params['rundir'], "part"+self.timestamp)
+			+" -fast -C 1e-18 "
+		)
+		### angle step
+		if self.params['rotate'] is True:
+			xmippopts += " -psi_step 90 "
+		else:
+			xmippopts += " -psi_step 360 "
+		### convergence criteria
+		if self.params['converge'] == "fast":
+			xmippopts += " -eps 5e-3 "
+		elif self.params['converge'] == "slow":
+			xmippopts += " -eps 5e-8 "
+		else:
+			xmippopts += " -eps 5e-5 "
+		### mirrors
+		if self.params['mirror'] is True:
+			xmippopts += " -mirror "
+		if self.params['maxshift'] is not None:
+			xmippopts += " -max_shift=%d "%(self.params['maxshift'])
+
+
+		### use single processor
+		xmippexe = apParam.getExecPath("xmipp_ml_align2d", die=True)
+		xmippcmd = xmippexe+" "+xmippopts
+		self.writeXmippLog(xmippcmd)
+		apEMAN.executeEmanCmd(xmippcmd, verbose=True, showcmd=True)
+		aligntime = time.time() - aligntime
+		apDisplay.printMsg("Alignment time: "+apDisplay.timeString(aligntime))
+
+		### create a quick mrc
+		emancmd = "proc2d part"+self.timestamp+"_ref000001.xmp average.mrc"
+		apEMAN.executeEmanCmd(emancmd, verbose=True)
+		apFile.removeStack(self.params['localstack'])
+		apFile.removeFilePattern("partfiles/*")
+
+
+	#=====================
+	def writeFakeKeepFile(self, numpart):
+		"""
+		This method keeps all particles
+		"""
+		self.params['keepfile'] = os.path.join(self.params['rundir'], "allparticles.lst")
+		f = open(self.params['keepfile'], "w")
+		for partnum in range(numpart):
+			f.write("%d\n"%(partnum))
+		f.close()
+			
+	#=====================
+	def readPartDocFile(self):
+		partlist = []
+		docfile = "part"+self.params['timestamp']+".doc"
+		if not os.path.isfile(docfile):
+			apDisplay.printError("could not find doc file "+docfile+" to read particle angles")
+		f = open(docfile, "r")
+		mininplane = 360.0
+		for line in f:
+			if line[:2] == ' ;':
+				continue
+			spidict = operations.spiderInLine(line)
+			partdict = self.spidict2partdict(spidict)
+			if partdict['inplane'] < mininplane:
+				mininplane = partdict['inplane']
+			partlist.append(partdict)
+		apDisplay.printMsg("minimum inplane: "+str(mininplane))
+		for partdict in partlist:
+			partdict['inplane'] = partdict['inplane']-mininplane
+		apDisplay.printMsg("read rotation and shift parameters for "+str(len(partlist))+" particles")
+		return partlist
+
+	#=====================
+	def spidict2partdict(self, spidict):
+		partdict = {
+			'partnum': int(spidict['row']),
+			'inplane': float(spidict['floatlist'][2]),
+			'xshift': float(spidict['floatlist'][3]),
+			'yshift': float(spidict['floatlist'][4]),
+			'refnum': int(spidict['floatlist'][5]),
+			'mirror': bool(spidict['floatlist'][6]),
+			'spread': float(spidict['floatlist'][7]),
+		}
+		return partdict
+
+	#=====================
+	def createAlignedStacks(self, partlist):
+		stackid = self.params['stackid']
+		stackdata = apStack.getOnlyStackData(stackid)
+		origstackfile = os.path.join(stackdata['path']['path'], stackdata['name'])
+		imagesdict = apImagicFile.readImagic(origstackfile)
+		i = 0
+		t0 = time.time()
+		apDisplay.printMsg("rotating and shifting particles at "+time.asctime())
+		alignstack = []
+		while i < len(partlist):
+			partimg = imagesdict['images'][i]
+			partdict = partlist[i]
+			partnum = i+1
+			#print partnum, partdict, partimg.shape
+			if partdict['partnum'] != partnum:
+				apDisplay.printError("particle shifting "+str(partnum)+" != "+str(partdict))
+			xyshift = (partdict['xshift'], partdict['yshift'])
+			alignpartimg = apImage.rotateThenShift(partimg, rot=partdict['inplane'], 
+				shift=xyshift, mirror=partdict['mirror'])
+			alignstack.append(alignpartimg)
+			i += 1
+		apDisplay.printMsg("rotate then shift %d particles in %s"%(i,apDisplay.timeString(time.time()-t0)))
+		alignstackarray = numpy.asarray(alignstack)
+		self.alignimagicfile = "alignstack.hed"
+		apImagicFile.writeImagic(alignstackarray, self.alignimagicfile)
 
 	#=====================
 	def start(self):
-		#new stack path
+		### new stack path
 		stackdata = apStack.getOnlyStackData(self.params['stackid'])
 		oldstack = os.path.join(stackdata['path']['path'], stackdata['name'])
 
-		#make sure that old stack is numbered
+		### make sure that old stack is numbered
 		apEMAN.checkStackNumbering(oldstack)
 
-		alignedstack = os.path.join(self.params['rundir'], 'ali.img')
-		badstack = os.path.join(self.params['rundir'], 'bad.img')
+		alignedstack = os.path.join(self.params['rundir'], 'alignstack.hed')
 		apStack.checkForPreviousStack(alignedstack)
 
-		#run centering algorithm
-		apStack.centerParticles(oldstack, self.params['mask'], self.params['maxshift'])
-		self.params['keepfile'] = os.path.join(self.params['rundir'],'keepfile.txt')
-		apEMAN.writeStackParticlesToFile(alignedstack, self.params['keepfile'])
-		if not os.path.isfile(alignedstack, ):
+		### run centering algorithm
+		self.runMaxlike()
+
+		### create aligned stacks
+		partlist = self.readPartDocFile()
+		stackfile = self.createAlignedStacks(partlist)
+		if not os.path.isfile(alignedstack):
 			apDisplay.printError("No stack was created")
 
-		#get number of particles
-		f = open(self.params['keepfile'], "r")
-		numparticles = len(f.readlines())
-		f.close()
+		### get number of particles
+		numpart = apStack.getNumberStackParticlesFromId(self.params['stackid'])
+		self.writeFakeKeepFile(numpart)
 		self.params['description'] += (
-			(" ... %d centered particle substack of stackid %d" 
+			" ... %d maxlike centered substack id %d" 
 			% (numparticles, self.params['stackid']))
-		)
 		
-		apStack.commitSubStack(self.params, newname='ali.hed', centered=True)
+		apStack.commitSubStack(self.params, newname='alignstack.hed', centered=True)
 		apStack.averageStack(stack=alignedstack)
-		if (os.path.exists(badstack)):
-			apStack.averageStack(stack=badstack, outfile='badaverage.mrc')
 
 #=====================
 if __name__ == "__main__":
