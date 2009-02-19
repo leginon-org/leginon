b98b35e843e3a1c0d6d6597182b3fe68ca8df9b1
Dmitry Lyumkis <dlyumkis>
2009-02-19 03:44:10 +0000

    change back

--- myamiweb/processing/selectTemplateStack.php
+++ myamiweb/processing/selectTemplateStack.php
@@ -23,7 +23,7 @@ echo "<table border='1' class='tableborder' width='640'>\n";
 echo "<tr><td width='100' align='center'>\n";
 echo "  <img src='img/template_stack_clsavgs.png' width='96'>\n";
 echo "</td><td>\n";
-echo "  <h3><a href='templateStackSummary.php?expId=$expId'>Template Stacks from Class Averages</a></h3>\n";
+echo "  <h3><a href='templateStackSummary.php?expId=$expId&type=clsavg'>Template Stacks from Class Averages</a></h3>\n";
 echo "  Summary of the template stacks created from the alignment pipeline. These can be used to run "
 	."IMAGIC common lines, or for IMAGIC Multi Reference Alignment."
 	."<br/><br/>\n";
@@ -32,7 +32,7 @@ echo "</td></tr>\n";
 echo "<tr><td width='100' align='center'>\n";
 echo "  <img src='img/template_stack_forwardproj.png' width='96'>\n";
 echo "</td><td>\n";
-echo "  <h3><a href='templateStackSummary.php?expId=$expId'>Template Stacks from Forward Projections</a></h3>\n";
+echo "  <h3><a href='templateStackSummary.php?expId=$expId&type=forward'>Template Stacks from Forward Projections</a></h3>\n";
 echo "  Summary of the template stacks created from forward projections of an already existing map. These "
 	."are particularly useful for running IMAGIC Multi Reference Alignment, but can also be used for other "
 	."template-based protocols. "
