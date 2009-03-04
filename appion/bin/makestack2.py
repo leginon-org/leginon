bcf44804c1481580c83473606e93f24aee599a4f
Neil Voss <vossman>
2009-03-04 21:21:31 +0000

    better, faster prime checking

--- appion/bin/makestack2.py
+++ appion/bin/makestack2.py
@@ -23,6 +23,7 @@ import apParticle
 import apStackMeanPlot
 import apEMAN
 import apProject
+import apPrimeFactor
 import apFile
 import apParam
 import apImagicFile
@@ -756,19 +757,9 @@ class Makestack2Loop(appionLoop2.AppionLoop):
 	def checkConflicts(self):
 		if self.params['boxsize'] is None:
 			apDisplay.printError("A boxsize has to be specified")
-		goodboxes = [2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 
-			24, 25, 27, 28, 30, 32, 35, 36, 40, 42, 45, 48, 49, 50, 54, 56, 60, 
-			63, 64, 70, 72, 75, 80, 81, 84, 90, 96, 98, 100, 105, 108, 112, 120, 
-			125, 126, 128, 135, 140, 144, 147, 150, 160, 162, 168, 175, 180, 189, 
-			192, 196, 200, 210, 216, 224, 225, 240, 243, 245, 250, 252, 256, 270, 
-			280, 288, 294, 300, 315, 320, 324, 336, 343, 350, 360, 375, 378, 384, 392, 400]
-		if not self.params['boxsize'] in goodboxes and self.params['boxsize'] <= 400:
+		if not apPrimeFactor.isGoodPrime(self.params['boxsize']):
 			apDisplay.printWarning("Boxsize does not contain recommended prime numbers")
-			for box in goodboxes:
-				if box > self.params['boxsize']:
-					bigbox = box
-					break
-				smallbox = box
+			smallbox,bigbox = apPrimeFactor.getPrimeLimits(self.params['boxsize'])
 			apDisplay.printWarning("You should use %d or %d for a boxsize instead"%(smallbox,bigbox))
 		if self.params['description'] is None:
 			apDisplay.printError("A description has to be specified")
--- appion/lib/apPrimeFactor.py
+++ appion/lib/apPrimeFactor.py
@@ -2,6 +2,7 @@
 
 import sys
 
+#====================
 def prime_factors(n):
 	""" Return the prime factors of the given number. """
 	# < 1 is a special case
@@ -21,18 +22,58 @@ def prime_factors(n):
 		lastresult /= c
 	return factors
 
+#====================
+def getAllPrimes(maxn=400):
+	goodones = []
+	n = 2
+	while True:
+		factors = prime_factors(n)
+		if max(factors) < 13:
+			print n, factors
+			goodones.append(n)
+			if (n > maxn):
+				break
+		n += 1
+	return goodones
+
+#====================
+def getNextPrime(num=400):
+	goodones = []
+	n = num
+	while not isGoodPrime(n) and n < 10000:
+		n += 1
+	return n
+
+#====================
+def getPrevPrime(num=400):
+	goodones = []
+	n = num
+	while not isGoodPrime(n) and n > 1:
+		n -= 1
+	return n
+
+#====================
+def getPrimeLimits(num=4):
+	prev = getPrevPrime(num)
+	next = getNextPrime(num)
+	return (prev, next)
+
+#====================
+def isGoodPrime(num=4):
+	print num
+	factors = prime_factors(num)
+	if max(factors) < 13:
+		return True
+	return False
+
+#====================
 if __name__ == "__main__":
 	if len(sys.argv) > 1:
 		n = int(sys.argv[1])
 		factors = prime_factors(n)
 		print n, factors
+		prev, next = getPrimeLimits(n)
+		print "Use %d or %d instead"%(prev,next)
 	else:
-		goodones = []
-		for i in range(400):
-			n = i+2
-			factors = prime_factors(n)
-			if max(factors) < 13:
-				print n, factors
-				goodones.append(n)
-		print goodones
+		print getAllPrimes()
 			
