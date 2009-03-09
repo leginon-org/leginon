ca14b2b3033a123f052e888debaa0cc010eb0b2c
Neil Voss <vossman>
2009-03-09 22:24:35 +0000

    added type

--- appion/bin/makeSnapshot.py
+++ appion/bin/makeSnapshot.py
@@ -13,16 +13,21 @@ class MakeSnapshotScript(appionScript.AppionScript):
 	#=====================
 	def setupParserOptions(self):
 		self.parser.set_usage("Usage: %prog --file=<filename> --sym=<c1,icos,d7> \n\t "
-			+" [--contour=<#>] [--zoom=<#>] ")
+			+" [--contour=<#>] [--zoom=<#>] [--type=<snapshot>] ")
 		self.parser.add_option("-f", "--file", dest="file", 
 			help="3d MRC file to snapshot", metavar="FILE")
 		self.parser.add_option("-z", "--zoom", dest="zoom", type="float", default=1.5,
 			help="Zoom factor for snapshot rendering (1.5 by default)", metavar="#")
 		self.parser.add_option("-c", "--contour", dest="contour", type="float", default=2.0,
 			help="Sigma level at which snapshot of density will be contoured (2.0 by default)", metavar="FLOAT")
-		self.parser.add_option("-s", "--sym", "--symmetry", dest="sym",
+		self.parser.add_option("-s", "--sym", "--symmetry", dest="sym", default='c1',
 			help="Symmetry name, e.g. d7, c1, c4, or icos", metavar="")
 
+		### choices
+		self.typemodes = ( "snapshot", "animate", "both" )
+		self.parser.add_option("-t", "--type", dest="type",
+			help="Snapshot mode: "+str(self.typemodes), metavar="MODE", 
+			type="choice", choices=self.typemodes, default="snapshot" )
 
 	#=====================
 	def checkConflicts(self):
@@ -41,10 +46,14 @@ class MakeSnapshotScript(appionScript.AppionScript):
 	#=====================
 	def start(self):
 		box = apVolume.getModelDimensions(self.params['file'])
-		apChimera.renderSnapshots(self.params['file'], res=30, contour=self.params['contour'], zoom=self.params['zoom'],
-			apix=None, sym=self.params['sym'], box=box, lpfilter=False)
-
-
+		if self.params['type'] != "snapshot":
+			apDisplay.printMsg("Creating animation")
+			apChimera.renderAnimation(self.params['file'], res=30, contour=self.params['contour'], zoom=self.params['zoom'],
+				apix=None, sym=self.params['sym'], box=box, lpfilter=False)
+		if self.params['type'] != "animate":
+			apDisplay.printMsg("Creating snapshots")
+			apChimera.renderSnapshots(self.params['file'], res=30, contour=self.params['contour'], zoom=self.params['zoom'],
+				apix=None, sym=self.params['sym'], box=box, lpfilter=False)
 
 #=====================
 #=====================
