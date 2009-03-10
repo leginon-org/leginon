1ce8fba8e820a3f61db327e3637e889c182a55da
pulokas <pulokas>
2009-03-10 22:46:43 +0000

    idle timer

--- pyscope/tem.py
+++ pyscope/tem.py
@@ -12,9 +12,35 @@
 # $Locker:  $
 
 import time
+import threading
 
 class TEM(object):
 	name = None
+	def __init__(self):
+		self.idletimers = {}
 
 	def getSystemTime(self):
 		return time.time()
+
+	def __getattr__(self, name):
+		self.resetIdleTimers()
+		#object.__getattr__(self, name)
+
+	def registerIdleTimer(self, name, interval, function, args=(), kwargs={}):
+		timer_args = (interval, function, args, kwargs)
+		self.ideltimers[name] = {}
+		self.idletimers[name]['args'] = timer_args
+		self.idletimers[name]['timer'] = None
+		self.initIdleTimer(name)
+
+	def initIdleTimer(self, name):
+		if self.ideltimers[name]['timer'] is not None:
+			self.idletimers[name]['timer'].cancel()
+		timer_args = self.idletimers[name]['args']
+		timer = threading.Timer(*timer_args)
+		self.idletimers[name]['timer'] = timer
+		timer.start()
+
+	def resetIdleTimers(self):
+		for name in self.idletimers.keys():
+			self.initIdleTimer(name)
