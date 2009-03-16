e44e334715136d6398a67347e5b51b6e3dd20351
pulokas <pulokas>
2009-03-16 18:53:12 +0000

    exception handling

--- pyscope/config.py
+++ pyscope/config.py
@@ -8,27 +8,40 @@ import tem
 import ccdcamera
 
 configparser = ConfigParser.SafeConfigParser()
+
+# use the path of this module
 modpath = os.path.dirname(__file__)
 
-# look in the same directory as this module
+# read instruments.cfg
 filename = os.path.join(modpath, 'instruments.cfg')
+if not os.path.exists(filename):
+	print 'please configure %s' % (filename,)
+	sys.exit()
 try:
 	configparser.read([filename])
-except IOError:
-	print 'instrument.cfg must be configured for pyScope'
+except:
+	print 'error reading %s' % (filename,)
 	sys.exit()
 
+# parse
 names = configparser.sections()
 temclasses = []
 cameraclasses = []
 for name in names:
 	cls_str = configparser.get(name, 'class')
 	modname,clsname = cls_str.split('.')
-	args = imp.find_module(modname, [modpath])
 	fullmodname = 'pyScope.' + modname
-	mod = imp.load_module(fullmodname, *args)
+	args = imp.find_module(modname, [modpath])
+	try:
+		mod = imp.load_module(fullmodname, *args)
+	finally:
+		if args[0] is not None:
+			args[0].close()
 	cls = getattr(mod, clsname)
 	if issubclass(cls, tem.TEM):
 		temclasses.append(cls)
 	if issubclass(cls, ccdcamera.CCDCamera):
 		cameraclasses.append(cls)
+
+print temclasses
+print cameraclasses
