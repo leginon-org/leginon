2785a1f0ce9350e7ac2f027644ce6ba059bb0fdd
Neil Voss <vossman>
2009-03-31 18:09:10 +0000

    added rescaleModelId()

--- appion/lib/apVolume.py
+++ appion/lib/apVolume.py
@@ -3,6 +3,10 @@
 #pythonlib
 import math
 import time
+import shutil
+import os
+import re
+import string
 #numpy
 import pyami.quietscipy
 import numpy
@@ -10,15 +14,11 @@ from scipy import ndimage
 from numpy import linalg
 from numpy import ma
 #appion
-import os
-import re
-import string
 import apDisplay
 import appionData
 import apAlignment
 import apParam
 import apEMAN
-import shutil
 import spyder
 #pyami
 from pyami import mrc
@@ -26,6 +26,7 @@ from pyami import imagefun
 from pyami import convolver
 from random import choice
 
+#================
 def getModelDimensions(mrcfile):
 	print "calculating dimensions..."
 	vol=mrc.read(mrcfile)
@@ -34,9 +35,11 @@ def getModelDimensions(mrcfile):
 		apDisplay.printError("starting model is not a cube")
 	return x
 
+#================
 def getModelFromId(modelid):
 	return appionData.ApInitialModelData.direct_query(modelid)
-	
+
+#================
 def rescaleModel(infile,outfile,inapix,outapix,newbox=None):
 	# scale an existing model - provide an input model & output (strings)
 	# an input a/pix & output a/pix (floats)
@@ -44,16 +47,30 @@ def rescaleModel(infile,outfile,inapix,outapix,newbox=None):
 	# currently uses EMAN's proc3d to do this
 	origbox=getModelDimensions(infile)
 	if newbox is None:
-		newbox=origbox
+		newbox = origbox
 	scalefactor = float(inapix/outapix)
-	print "rescaling",infile,"with boxsize:",origbox
-	print "by a factor of",scalefactor
-	print "saving to",outfile,"with a boxsize:",newbox
+	apDisplay.printMsg("rescaling %s with boxsize %d by a factor of %.3f\n"
+		+"\tand saving to %s with a boxsize %d"
+		%(infile, origbox, scalefactor, outfile, newbox))
 	emancmd = "proc3d %s %s " % (infile, outfile)
-	emancmd += "scale=%s " % scalefactor
+	if abs(scalefactor-1.0) > 0.2:
+		emancmd += "scale=%.3f " % scalefactor
 	emancmd += "clip=%i,%i,%i norm=0,1" % (newbox, newbox, newbox)
 	apEMAN.executeEmanCmd(emancmd, verbose=True)
+	return
 
+#================
+def rescaleModelId(modelid, outfile, newapix, newbox=None):
+	"""
+	take an existing model id and rescale it
+	"""
+	modeldata = getModelFromId(modelid)
+	modelapix = modeldata['pixelsize']
+	modelfile = os.path.join(modeldata['path']['path'], modeldata['name'])
+	rescaleModel(modelfile, outfile, modelapix, newapix, newbox)
+	return
+
+#================
 def MRCtoSPI(infile,rundir):
 	# convert file to spider file
 	tmpspifile = randomfilename(8)+".spi"
@@ -62,6 +79,7 @@ def MRCtoSPI(infile,rundir):
 	apEMAN.executeEmanCmd(emancmd, verbose=True)
 	return tmpspifile
 
+#================
 def createAmpcorBatchFile(infile,params):
 	localinfile = spyder.fileFilter(infile)
 
@@ -116,6 +134,7 @@ def createAmpcorBatchFile(infile,params):
 				outf.write(line)
 	return tmpfile
 
+#================
 def runAmpcor():
 	spidercmd = "spider bat/spi @enhance_edit"
 	starttime = time.time()
@@ -125,7 +144,9 @@ def runAmpcor():
 	if os.path.isfile('enhance_edit.bat'):
 		os.remove('enhance_edit.bat')
 	apDisplay.printColor("finished spider in "+apDisplay.timeString(time.time()-starttime),"cyan")
-	
+	return
+
+#================	
 def randomfilename(num):
 	# return a string of random letters and numbers of 'num' length
 	f=''
@@ -133,3 +154,10 @@ def randomfilename(num):
 	for i in range(num):
 		f+=choice(chars)
 	return f
+
+
+
+
+
+
+
