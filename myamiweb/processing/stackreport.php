22a2b2debd6f7e9e14fe83bac0378f94c9560c60
Anchi Cheng <acheng>
2009-04-01 02:47:14 +0000

    stack report display with parents and combination

--- myamiweb/processing/stackreport.php
+++ myamiweb/processing/stackreport.php
@@ -16,28 +16,28 @@ require "inc/processing.inc";
 
 $expId = $_GET['expId'];
 $sessionId= $_GET['Id'];
-$stackid = $_GET['sId'];
+$stackId = $_GET['sId'];
 
 processing_header('Particle Stack Report','Particle Stack Report');
 
 $particle = new particledata();
-
-	$s=$particle->getStackParams($stackid);
+	echo $stackId;
+	$s=$particle->getStackParams($stackId);
 	# get list of stack parameters from database
-	$nump=commafy($particle->getNumStackParticles($stackid));
+	$nump=commafy($particle->getNumStackParticles($stackId));
 	# get pixel size of stack
-	$mpix=($particle->getStackPixelSizeFromStackId($stackid));
+	$mpix=($particle->getStackPixelSizeFromStackId($stackId));
 	$apix=format_angstrom_number($mpix)."/pixel";
 	$s['pixelsize']=$apix;
 	$boxsize= $s[boxSize]/$s[bin];
 	$s['boxsize']=$boxsize;
 
 	echo apdivtitle("Stack: <FONT class='aptitle'>".$s['shownstackname']
-		."</FONT> (ID: <FONT class='aptitle'>".$stackid."</FONT>)");
+		."</FONT> (ID: <FONT class='aptitle'>".$stackId."</FONT>)");
 
 	echo "<table cellspacing='1' cellpadding='2'><tr><td><span class='datafield0'>Total particles for $runparams[stackRunName]: </span></td><td>$nump</td></tr></table>\n";
 
-	$stackparts = $particle->getStackParticles($stackid);
+	$stackparts = $particle->getStackParticles($stackId);
 
 	echo "<table border='0'><tr>\n";
 	$stackavg = $s['path']."/average.mrc";
@@ -49,12 +49,12 @@ $particle = new particledata();
 	}	
 	if (!is_null($stackparts[0]['mean'])) {
 		echo "<td align='center'>\n";
-		echo "<a href='stack_mean_stdev.php?sId=$stackid&expId=$expId'>\n";
-		echo "<img border='0' src='stack_mean_stdev.php?w=256&sId=$stackid'><br/>\n";
-		echo "<i><a href='subStack.php?expId=$expId&sId=$stackid&mean=1'>Filter Stack by Mean & Stdev</a></i>\n";
+		echo "<a href='stack_mean_stdev.php?sId=$stackId&expId=$expId'>\n";
+		echo "<img border='0' src='stack_mean_stdev.php?w=256&sId=$stackId'><br/>\n";
+		echo "<i><a href='subStack.php?expId=$expId&sId=$stackId&mean=1'>Filter Stack by Mean & Stdev</a></i>\n";
 		echo "</td>\n";
 	}
-	$montage = $s['path']."/montage$stackid.png";
+	$montage = $s['path']."/montage$stackId.png";
 	if (file_exists($montage)) {
 		echo "<td align='center'>\n";
 		echo "<a href='loadimg.php?filename=$montage'>";
@@ -67,20 +67,43 @@ $particle = new particledata();
 	$stackfile=$s['path']."/".$s['name'];
 	echo "View Stack: <A TARGET='stackview' HREF='viewstack.php?stackId=$stackId&file=$stackfile'>$s[name]</A><br>\n";
 
-//Report stack run parameters
-	$exclude_fields = array('DEF_id','DEF_timestamp','count','REF|ApPathData|path');
+//Report stack parameters
+	$exclude_fields = array('DEF_id','DEF_timestamp','count','REF|ApPathData|path','boxSize','selectionstacks');
 	for ($i=1;$i < $s[count]; $i++) $exclude_fields[]=$i;
 	$title = "stack parameters";
 	$particle->displayParameters($title,$s,$exclude_fields,$expId);
 
-//Report stack run parameters
+//Report parent (old) stack run parameters if a substack
+	while ($s['REF|ApStackData|oldstack']) {
+		$s0 = $s;
+		$stackId = $s['REF|ApStackData|oldstack'];
+		$s=$particle->getStackParams($stackId);
+		#for ($i=1;$i < $s[count]; $i++) $exclude_fields[]=$i;
+		$title = "parent stack parameters";
+		$particle->displayParameters($title,$s,$exclude_fields,$expId);
+	}
+//Report individual stack run parameters if combined
+//This need rework along with getStackParams to handle multiple layers----AC
+	if ($s['count'] > 1) {
+		for ($i=0; $i < $s[count]; $i++) {
+			$selectionstackId = $s['selectionstacks'][$i];
+			$sr=$particle->getStackParams($selectionstackId);
+			#for ($i=1;$i < $sr[count]; $i++) $exclude_fields[]=$i;
+			$title = "combining stack run parameters";
+			$particle->displayParameters($title,$sr,$exclude_fields,$expId);
+		}
+	}
+//Report stack run particle picking parameters
 	echo "<table><tr><td>";
 	for ($i=0; $i < $s[count]; $i++) {
-		$selectionruninfo=$particle->getStackSelectionRun($s[$i][stackId]);
-		$s[$i]['particleSelection']=array('display'=>$selectionruninfo['name'], 'link'=>$selectionruninfo['selectionid']);
-		$exclude_fields = array('DEF_id','DEF_timestamp','REF|ApPathData|path');
-		$title = "run parameters";
-		$particle->displayParameters($title,$s[$i],$exclude_fields,$expId);
+		$selectionstackId = $s['selectionstacks'][$i];
+		$selectionruninfo=$particle->getStackSelectionRun($selectionstackId);
+		$a = array('stackId'=>$selectionstackId);
+		$a = array_merge($a,$selectionruninfo);
+		$a['particleSelection']=array('display'=>$selectionruninfo['name'], 'link'=>$selectionruninfo['selectionid']);
+		$exclude_fields = array('DEF_id','DEF_timestamp','REF|ApPathData|path','name');
+		$title = "particle selection";
+		$particle->displayParameters($title,$a,$exclude_fields,$a['sessionId']);
 		echo "</td><td>";
 	}
 	echo "</td><tr></table>";
