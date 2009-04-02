d45f7d58c5e60e93b92490150911ff6c1a2beb9c
pulokas <pulokas>
2009-04-02 19:53:20 +0000

    other new name for "Tecnai Scripting" is "TEM Scripting"

--- pyscope/updatecom.py
+++ pyscope/updatecom.py
@@ -4,6 +4,7 @@ from win32com.client import makepy
 
 info = [
 	('Tecnai Scripting', 'tecnaicom.py', 'Tecnai Scripting'),
+	('TEM Scripting', 'tecnaicom.py', 'Tecnai Scripting'),
 	('Low Dose Server Library', 'ldcom.py', 'Tecnai Low Dose Kit'),
 	('adaExp Library', 'adacom.py', 'Tecnai Exposure Adaptor'),
 	('TecnaiCCD 1.0 Type Library', 'gatancom.py', 'Gatan CCD Camera'),
