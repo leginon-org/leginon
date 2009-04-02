31a0e819f054cc414747abfb485ff4f51594874e
Neil Voss <vossman>
2009-04-02 20:47:46 +0000

    major changes

--- appion/bin/createModel.py
+++ appion/bin/createModel.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
-### From Pick-wei:
-###   create model from EMAN startAny function and automatically calls uploadModel.py
+
+### create model from EMAN startAny function and automatically calls uploadModel.py
 
 import os
-import sys
 import re
+import sys
 import time
 import shutil
 ### appion
@@ -13,7 +13,7 @@ import apEMAN
 import apDisplay
 import apChimera
 import apStack
-import apUpload
+import apSymmetry
 import apFile
 import apParam
 import apDatabase
@@ -25,89 +25,85 @@ class createModelScript(appionScript.AppionScript):
 
 	#=====================
 	def setupParserOptions(self):
-		self.parser.set_usage("Usage: %prog --template=<name> --apix=<pixel> --session=<session> --diam=<int> "
-			+"--description='<text>' [options]")
+		self.parser.set_usage("Usage: %prog [options]")
 
-		self.parser.add_option("--session", dest="session",
-			help="Session name associated with model (e.g. 06mar12a)", metavar="TEXT")
-		self.parser.add_option("--norefClass", dest="norefclass", type="int",
-			help="ID for the classes of the reference-free alignment", metavar="INT")
-		self.parser.add_option("--exclude", dest="exclude",
-			help="Class indices to be excluded e.g. 1,0,10", metavar="TEXT")
-		self.parser.add_option("--method", dest="method",
-			help="EMAN method for commonline backprojection: startIcos, startCSym, startAny, startOct", metavar="TEXT")
-		self.parser.add_option("--apix", dest="apix", type="float",
-			help="Angstrom per pixel of the images in the class average file", metavar="FLOAT")
+		### Ints
+		self.parser.add_option("--cluster-id", dest="clusterid", type="int",
+			help="clustering stack id", metavar="ID")
+		self.parser.add_option("--align-id", dest="alignid", type="int",
+			help="alignment stack id", metavar="ID")
 
-		#===================
-		#Common Parameters
-		#===================
+		### Strings
+		self.parser.add_option("--class-list-keep", dest="keepclasslist",
+			help="list of EMAN style class numbers to include in sub-stack, e.g. --class-list-keep=0,5,3", metavar="#,#")
+		self.parser.add_option("--class-list-drop", dest="dropclasslist",
+			help="list of EMAN style class numbers to exclude in sub-stack, e.g. --class-list-drop=0,5,3", metavar="#,#")
 		self.parser.add_option("--symm", dest="symm", default="c1",
-			help="startAny, startCSym & startIcos: Cn symmetry if any, e.g. --symm=4,c3", metavar="TEXT")
+			help="symmetry id or name, e.g. --symm=c3 or --symm=25", metavar="TEXT")
 
-		#===================
-		#StartAny Parameters
-		#===================
+		### Choices
+		self.startmethods = ( "any", "csym", "oct", "icos" )
+		self.parser.add_option("--method", dest="method",
+			help="EMAN common lines method: startIcos, startCSym, startAny, startOct", metavar="TEXT", 
+			type="choice", choices=self.startmethods, default="normal" )
+
+		### Program specific parameters
+		"""
+		startAny  <imagefile> [sym=<c?>] [proc=<num proc>] [rounds=<2-5>] [mask=<rad>] [lp=<radius>]
+		startcsym  <centered particles> <# to keep> [sym=<c?>] [imask=<rad>] [nosym] [fixrot=<side angle>]
+		startoct  <centered particles> <# to keep> [nosym]
+		starticos  <centered particles> <# to keep> [imask=<rad>] [nosym]
+		"""
 		self.parser.add_option("--mask", dest="mask", type="int",
-			help="startAny: Mask radius", metavar="INT")
-		self.parser.add_option("--lp", dest="lp", type="float",
-			help="startAny: Lowpass filter radius in Fourier pixels", metavar="INT")
-		self.parser.add_option("--rounds", dest="rounds", type="int",
-			help="startAny: Rounds of Euler angle determination to use", metavar="INT")
-
-		#===================
-		#StartCSym Parameters
-		#===================
-		self.parser.add_option("--partnum", dest="partnum",
-			help="startCSym & startIcos & starOct: Number of particles to use for each view. ~10% of the total particle", metavar="INT")
+			help="any: Mask radius", metavar="INT")
+		self.parser.add_option("--rounds", dest="rounds", type="int", default=5,
+			help="any: Rounds of Euler angle determination to use", metavar="INT")
+		self.parser.add_option("--numkeep", dest="numkeep",
+			help="csym/icos/oct: Number of particles per view. ~10% of the total particle", metavar="INT")
 		self.parser.add_option("--imask", dest="imask",
-			help="startCSym & startIcos: Inside mask used to exclude inside regions", metavar="INT")
-
-		#===================
-		#StartOct Parameters
-		#===================
+			help="csym/icos: Inside mask used to exclude inside regions", metavar="INT")
 
 	#=====================
 	def checkConflicts(self):
-		# make sure the necessary parameters are set
-		if self.params['session'] is None:
-			apDisplay.printError("enter a session ID")
-		else:
-			self.sessiondata = apDatabase.getSessionDataFromSessionName(self.params['session'])
-
-		if self.params['description'] is None:
-			apDisplay.printError("enter a template description")
-		if self.params['norefclass'] is None:
-			apDisplay.printError("enter the ID for the classes of the reference-free alignment")
-		if self.params['apix'] is None:
-			apDisplay.printError("enter the apix for the images of the class average file")
+		### check the method
 		if self.params['method'] is None:
-			apDisplay.printError("enter the EMAN commonline method")
+			apDisplay.printError("Please enter the EMAN commonline method")
+		if self.params['method'] != 'any' and self.params['numkeep'] is None:
+			apDisplay.printError("Please enter the number of particles per view, e.g. --numkeep=120")
 
-		print self.params['method']
+		### get the symmetry data
+		if self.params['symm'] is None:
+			apDisplay.printError("Symmetry was not defined")			
+		else:
+			self.symmdata = apSymmetry.parseSymmetry(self.params['symm'])
+			self.params['symm_id'] = self.symmdata.dbid
+			self.params['symm_name'] = self.symmdata['eman_name']
+			apDisplay.printMsg("Selected symmetry %s with id %s"%(self.symmdata['eman_name'], self.symmdata.dbid))
 
-		# make sure each of the method has its required options
-		if self.params['method'] != 'startAny' and self.params['method'] != 'startCSym' and self.params['method'] != 'startOct' and self.params['method'] != 'startIcos':
-			apDisplay.printError("enter a correct EMAN commonline method: startAny, startCSym, startOct, startIcos")
+		### check for missing and duplicate entries
+		if self.params['alignid'] is None and self.params['clusterid'] is None:
+			apDisplay.printError("Please provide either --cluster-id or --align-id")
+		if self.params['alignid'] is not None and self.params['clusterid'] is not None:
+			apDisplay.printError("Please provide only one of either --cluster-id or --align-id")		
 
-		if self.params['method'] == 'startAny' and (self.params['lp'] is None or self.params['symm'] is None):
-			apDisplay.printError("Make sure options lp, mask, rounds and symm are provided")
+		### get the stack ID from the other IDs
+		if self.params['alignid'] is not None:
+			self.alignstackdata = appionData.ApAlignStackData.direct_query(self.params['alignid'])
+			self.params['stackid'] = self.alignstackdata['stack'].dbid
+		elif self.params['clusterid'] is not None:
+			self.clusterstackdata = appionData.ApClusteringStackData.direct_query(self.params['clusterid'])
+			self.alignstackdata = self.clusterstackdata['clusterrun']['alignstack']
+			self.params['stackid'] = self.alignstackdata['stack'].dbid
 
-		if self.params['method'] == 'startCSym'and (self.params['partnum'] is None or self.params['symm'] is None):
-			apDisplay.printError("Make sure options partnum and symm are provided")
+		### check and make sure we got the stack id
+		if self.params['stackid'] is None:
+			apDisplay.printError("stackid was not defined")
 
-		if self.params['method'] == 'startIcos' and (self.params['partnum'] is None):
-			apDisplay.printError("Make sure options partnum is provided")
-
-		if self.params['method'] == 'startAny' or self.params['method'] == 'startCSym':
-			self.symmdata   = apSymmetry.parseSymmetry(self.params['symm'])
-			self.params['symm_id'] = symmdata.dbid
-			self.params['symm_name'] = symmdata['eman_name']
-			apDisplay.printMsg("Selected symmetry: "+str(self.params['symm_id'])+" named: "+self.params['symm_name'])
-			except:
-				apSymmetry.printSymmetries()
-				apDisplay.printError("Could not parse symmetry, should be of the form"
-					+" --symm=4,c3 NOT --symm="+str( self.params['symm']))
+		### check that we have a keep or drop list and not both
+		if self.params['keepclasslist'] is None and self.params['dropclasslist'] is None:
+			apDisplay.printError("class numbers to be included/excluded was not defined")
+		if self.params['keepclasslist'] is not None and self.params['dropclasslist'] is not None:
+			apDisplay.printError("both --class-list-keep and --class-list-drop were defined, only one is allowed")
 
 	#=====================
 	def cleanup(self, norefpath, norefclassid, method):
@@ -143,28 +139,14 @@ class createModelScript(appionScript.AppionScript):
 				newexcludepath = os.path.join(norefpath, "exclude-"+str(norefclassid)+".mrc")
 				apDisplay.printWarning("Moving "+oldexcludepath+" to "+newexcludepath)
 				shutil.copy(oldexcludepath, newexcludepath)
-
 		return modelpath
 
 	#=====================
-	def changeapix(self, mrcpath, apix):
-		"""
-		this doesn't do anything, but copy the file,
-		you need to use shrink=## to change pixel size
-			--neil
-		"""
-		cmd = "proc3d "+mrcpath+" "+mrcpath+" apix="+str(apix)
-		print "\nChanging the apix value of "+mrcpath+"...\n"+cmd
-		apEMAN.executeEmanCmd(cmd, verbose=True)
-
-	#=====================
 	def setRunDir(self):
-		#auto set the output directory
-		self.sessiondata = apDatabase.getSessionDataFromSessionName(self.params['session'])
-		path = os.path.abspath(sessiondata['image path'])
-		path = re.sub("leginon","appion",path)
-		path = re.sub("/rawdata","",path)
-		self.params['rundir'] = os.path.join(path, "createmodel", self.timestamp)
+		stackdata = apStack.getOnlyStackData(self.params['stackid'], msg=False)
+		path = stackdata['path']['path']
+		uppath = os.path.dirname(os.path.abspath(path))
+		self.params['rundir'] = os.path.join(uppath, self.params['runname'])
 
 	#=====================
 	def excludedClasses(self, origclassfile, norefpath):
@@ -203,8 +185,8 @@ class createModelScript(appionScript.AppionScript):
 		densq['name'] = os.path.basename(volfile)
 		densq['hidden'] = False
 		densq['norm'] = True
-		#densq['symmetry'] = appionData.ApSymmetryData.direct_query(25)
-		densq['pixelsize'] = self.params['apix']
+		densq['symmetry'] = self.symmdata
+		densq['pixelsize'] = apix
 		densq['boxsize'] = self.params['box']
 		densq['lowpass'] = self.params['lp']
 		#densq['highpass'] = self.params['highpasspart']
@@ -217,6 +199,116 @@ class createModelScript(appionScript.AppionScript):
 		if self.params['commit'] is True:
 			densq.insert()
 		return
+
+	#=====================
+	def start(self):
+		### new stack path
+		stackdata = apStack.getOnlyStackData(self.params['stackid'])
+		oldstack = os.path.join(stackdata['path']['path'], stackdata['name'])
+		newstack = os.path.join(self.params['rundir'], stackdata['name'])
+		apStack.checkForPreviousStack(newstack)
+
+		### list of classes to be excluded
+		excludelist = []
+		if self.params['dropclasslist'] is not None:
+			excludestrlist = self.params['dropclasslist'].split(",")
+			for excludeitem in excludestrlist:
+				excludelist.append(int(excludeitem.strip()))
+		apDisplay.printMsg("Exclude list: "+str(excludelist))
+
+		### list of classes to be included
+		includelist = []
+		if self.params['keepclasslist'] is not None:
+			includestrlist = self.params['keepclasslist'].split(",")
+			for includeitem in includestrlist:
+				includelist.append(int(includeitem.strip()))		
+		apDisplay.printMsg("Include list: "+str(includelist))
+
+		### get particles from align or cluster stack
+		if self.params['alignid'] is not None:
+			alignpartq =  appionData.ApAlignParticlesData()
+			alignpartq['alignstack'] = self.alignstackdata
+			particles = alignpartq.query()
+		elif self.params['clusterid'] is not None:
+			clusterpartq = appionData.ApClusteringParticlesData()
+			clusterpartq['clusterstack'] = self.clusterstackdata
+			particles = clusterpartq.query()
+
+		### write included particles to text file
+		includeParticle = []
+		excludeParticle = 0
+		f = open("test.log", "w")
+		count = 0
+		for part in particles:
+			count += 1
+			#partnum = part['partnum']-1
+			if 'alignparticle' in part:
+				alignpart = part['alignparticle']
+				classnum = int(part['refnum'])-1
+			else:
+				alignpart = part
+				classnum = int(part['ref']['refnum'])-1
+			emanstackpartnum = alignpart['stackpart']['particleNumber']-1
+
+			### check score
+			if ( self.params['minscore'] is not None 
+			 and alignpart['score'] is not None 
+			 and alignpart['score'] < self.params['minscore'] ):
+				excludeParticle += 1
+				f.write("%d\t%d\t%d\texclude\n"%(count, emanstackpartnum, classnum))
+
+			### check spread
+			elif ( self.params['minspread'] is not None 
+			 and alignpart['spread'] is not None 
+			 and alignpart['spread'] < self.params['minspread'] ):
+				excludeParticle += 1
+				f.write("%d\t%d\t%d\texclude\n"%(count, emanstackpartnum, classnum))
+
+			elif includelist and classnum in includelist:
+				includeParticle.append(emanstackpartnum)
+				f.write("%d\t%d\t%d\tinclude\n"%(count, emanstackpartnum, classnum))
+			elif excludelist and not classnum in excludelist:
+				includeParticle.append(emanstackpartnum)
+				f.write("%d\t%d\t%d\tinclude\n"%(count, emanstackpartnum, classnum))
+			else:
+				excludeParticle += 1
+				f.write("%d\t%d\t%d\texclude\n"%(count, emanstackpartnum, classnum))
+
+		f.close()
+		includeParticle.sort()
+		apDisplay.printMsg("Keeping "+str(len(includeParticle))+" and excluding "+str(excludeParticle)+" particles")
+
+		#print includeParticle
+
+		### write kept particles to file
+		self.params['keepfile'] = os.path.join(self.params['rundir'], "keepfile-"+self.timestamp+".list")
+		apDisplay.printMsg("writing to keepfile "+self.params['keepfile'])
+		kf = open(self.params['keepfile'], "w")
+		for partnum in includeParticle:
+			kf.write(str(partnum)+"\n")
+		kf.close()
+
+		### get number of particles
+		numparticles = len(includeParticle)
+		if excludelist:
+			self.params['description'] += ( " ... %d particle substack with %s classes excluded" 
+				% (numparticles, self.params['dropclasslist']))
+		elif includelist:
+			self.params['description'] += ( " ... %d particle substack with %s classes included" 
+				% (numparticles, self.params['keepclasslist']))	
+
+		### create the new sub stack
+		apStack.makeNewStack(oldstack, newstack, self.params['keepfile'])
+
+		if not os.path.isfile(newstack):
+			apDisplay.printError("No stack was created")
+
+		apStack.averageStack(stack=newstack)
+		if self.params['commit'] is True:
+			apStack.commitSubStack(self.params)
+			newstackid = apStack.getStackIdFromPath(newstack)
+			apStackMeanPlot.makeStackMeanPlot(newstackid, gridpoints=4)
+
 
 	#=====================
 	def start(self):
@@ -293,28 +385,15 @@ class createModelScript(appionScript.AppionScript):
 
 		#cleanup the extra files, move the created model to the same folder as the class average and rename it as startAny.mrc
 		modelpath = self.cleanup(norefpath, self.params['norefclass'], self.params['method'])
-		#change its apix back to be the same as the class average file
-		self.changeapix(modelpath, self.params['apix'])
+
+		### upload it
+		self.uploadDensity(modelpath)
 
 		### chimera imaging
 		apChimera.renderSnapshots(modelpath, contour=1.5, zoom=1.0, sym='c1')
 		apChimera.renderAnimation(modelpath, contour=1.5, zoom=1.0, sym='c1')
 
-		### upload it
-		self.uploadDensity(modelpath)
 
-#		#call uploadModel
-#		upload = ("uploadModel.py --file=%s --session=%s --apix=%.3f --res=%i --symmetry=%i --contour=1.5 --zoom=1.5 --description=\"%s\"" %
-#			(modelpath, self.params['session'], self.params['apix'],
-#			int(self.params['lp']), int(self.params['symm_id']), self.params['description']) )
-
-#		print "\n############################################"
-#		print "\nReady to upload model "+modelpath+" into the database...\n"
-#		if not self.params['commit']:
-#			apDisplay.printWarning("Commit flag is not turned on... Model will not be uploaded!")
-#		time.sleep(10)
-#		if self.params['commit']:
-#			apEMAN.executeEmanCmd(upload, verbose=True)
 
 
 #=====================
