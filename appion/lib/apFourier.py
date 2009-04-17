e6b1d3428c0dc6aaa1c311377273a5955765f17e
Neil Voss <vossman>
2009-04-17 21:32:02 +0000

    advancements

--- appion/lib/apFourier.py
+++ appion/lib/apFourier.py
@@ -3,6 +3,7 @@
 import sys
 import glob
 import numpy
+import time
 import math
 from pyami import quietscipy
 from scipy import fftpack, ndimage
@@ -16,6 +17,237 @@ def savePower(fft, fname="power.mrc"):
 	half = numpy.asarray(image.shape)/2
 	imagecent = ndimage.shift(image, half, mode='wrap', order=0)
 	mrc.write(imagecent, fname)
+
+#===========
+def real_fft2d(image, *args, **kwargs):
+	padshape = numpy.asarray(image.shape)*2
+	padimage = apImage.frame_constant(image, padshape, image.mean())
+	fft = fftpack.fft2(padimage, *args, **kwargs)
+	#normfft = (fft - fft.mean())/fft.std()
+	return fft
+
+#===========
+def inverse_real_fft2d(image, *args, **kwargs):
+	return fftpack.ifft2(image, *args, **kwargs).real
+
+#===========
+def normImage(image):
+	return (image - image.mean())/image.std()
+
+#===========
+def fourierRingCorrelation(fftim1, fftim2):
+	"""
+	Formula taken from:
+		http://www.imagescience.de/fsc/index.htm
+	"""
+	t0 = time.time()
+	### initialization
+	if fftim1.shape != fftim2.shape:
+		apDisplay.printError("Cannot calculate the FRC for images of different sizes")
+	if len(fftim1.shape) != 2 or len(fftim2.shape) != 2:
+		apDisplay.printError("Cannot calculate the FRC non-2D images")
+	fftshape = numpy.asarray(fftim1.shape, dtype=numpy.float32)
+	fftcenter = fftshape/2.0
+	#print fftshape, fftshape
+	#print fftcenter, fftcenter
+	length = int(max(fftshape)/2.0)
+	#print "linear length", length
+	linear = numpy.zeros((length), dtype=numpy.float32)
+	linear[0] = 1.0
+
+	### figure out which pixels go with which ring
+	lineardict = getLinearIndices(fftshape)
+
+	### for each ring calculate the FRC
+	keys = lineardict.keys()
+	keys.sort()
+	lastfrc = 1.0
+	K = float(2)
+	for key in keys:
+		indexlist = lineardict[key]
+		numer = 0.0
+		f1sum = 0.0
+		f2sum = 0.0
+		for indextuple in indexlist:
+			i,j = indextuple
+			F1 = fftim1[i,j]
+			F2 = fftim2[i,j]
+			#print (i,j), abs(F1), abs(F2), math.atan2(F1.imag, F1.real), math.atan2(F2.imag, F2.real)
+			numer += F1*F2.conjugate()
+			f1sum += abs(F1)**2
+			f2sum += abs(F2)**2
+		frc = numer / math.sqrt(f1sum*f2sum)
+		#print "*** %02d %03d %.3f (%.4f / %.4f %.4f)"%(key, K, frc, numer/K, f1sum/K, f2sum/K)
+		#print key, frc
+		linear[key] = frc
+		#if key > 2:
+		#	sys.exit(1)
+		if frc < 0 and lastfrc < 0:
+			break
+		lastfrc = frc
+
+	### output
+	writeFrcPlot("frc.dat", linear)
+	apDisplay.printMsg("Finished FRC in "+apDisplay.timeString(time.time()-t0))
+	return
+
+#===========
+def spectralSNRfft(fftimlist):
+	"""
+	Compute the Spectral Signal-to-Noise Ratio (SSNR) of a given series of transforms. 
+	"""
+	t0 = time.time()
+	### initialization
+	fftim1 = fftimlist[0]
+	for fftim in fftimlist:
+		if fftim1.shape != fftim.shape:
+			apDisplay.printError("Cannot calculate the SSNR for images of different sizes")
+		if len(fftim1.shape) != 2 or len(fftim.shape) != 2:
+			apDisplay.printError("Cannot calculate the SSNR non-2D images")
+	fftshape = numpy.asarray(fftim1.shape, dtype=numpy.float32)
+	fftcenter = fftshape/2.0
+	#print fftshape, fftshape
+	#print fftcenter, fftcenter
+	length = int(max(fftshape)/2.0)
+	#print "linear length", length
+	linear = numpy.zeros((length), dtype=numpy.float32)
+	linear[0] = 1.0
+
+	### figure out which pixels go with which ring
+	lineardict = getLinearIndices(fftshape)
+
+	### for each ring calculate the FRC
+	keys = lineardict.keys()
+	keys.sort()
+	K = float(len(fftimlist))
+	for key in keys:
+		sys.stderr.write(".")
+		indexlist = lineardict[key]
+		numer = 0.0
+		denom = 0.0
+		for indextuple in indexlist:
+			i,j = indextuple
+			fsum = 0.0
+			for fftim in fftimlist:
+				F = fftim[i,j]
+				fsum += F
+			fmean = fsum/K
+			n1 = abs(fsum)**2
+			d1 = 0.0
+			for fftim in fftimlist:
+				F = fftim[i,j]
+				d1 += abs(F - fmean)**2
+			#if i == 5 and j == 5:
+			#print ("%d,%d (%.3f / %.3f) %.3f"%(indextuple[0], indextuple[1], n1, d1, n1/d1))
+			#return
+			numer += n1
+			denom += d1
+		ssnr = numer / ( K/(K-1.0) * denom ) - 1.0
+		frc = ssnr / (ssnr + 1)
+		#print "%02d %.3f %.3f (%.3f / %.3f)"%(key, ssnr, frc, numer/K, denom/K)
+		#print key, frc
+		linear[key] = frc
+	sys.stderr.write("\n")
+
+	### output
+	writeFrcPlot("ssnrfft.dat", linear)
+
+	apDisplay.printMsg("Finished SSNR (fft) in "+apDisplay.timeString(time.time()-t0))
+
+	return
+
+#===========
+def spectralSNR(partarray):
+	"""
+	Compute the Spectral Signal-to-Noise Ratio (SSNR) of a given series of images. 
+	"""
+	t0 = time.time()
+	### initialization
+	part0 = partarray[0]
+	for partimg in partarray:
+		if part0.shape != partimg.shape:
+			apDisplay.printError("Cannot calculate the SSNR for images of different sizes")
+		if len(partimg.shape) != 2:
+			apDisplay.printError("Cannot calculate the SSNR non-2D images")
+
+	### get fft
+	fftlist = []
+	for partimg in partarray:
+		fftim = real_fft2d(partimg)
+		fftlist.append(fftim)
+
+	### dimension init
+	fftim0 = real_fft2d(partarray[0])
+	fftshape = numpy.asarray(fftim0.shape, dtype=numpy.float32)
+	fftcenter = fftshape/2.0
+	length = int(max(fftshape)/2.0)
+	linear = numpy.zeros((length), dtype=numpy.float32)
+	linear[0] = 1.0
+
+	### figure out which pixels go with which ring
+	lineardict = getLinearIndices(fftshape)
+
+	### for each ring calculate the FRC
+	keys = lineardict.keys()
+	keys.sort()
+	K = float(len(partarray))
+	for key in keys:
+		sys.stderr.write(".")
+		indexlist = lineardict[key]
+		numer = 0.0
+		denom = 0.0
+		for indextuple in indexlist:
+			n1, d1 = mini_ssnr1fft(fftlist, indextuple)
+			#n1, d1 = mini_ssnr1(partarray, indextuple)
+			#n2, d2 = mini_ssnr2(partarray, indextuple)
+			#if indextuple[0] == 5 and indextuple[1] == 5:
+			#print "%d,%d (%.3f / %.3f) vs (%.3f / %.3f) %.3f"%(indextuple[0], indextuple[1], n1, d1, n2, d2, n1/d1)
+			#return
+			numer += n1
+			denom += d1
+		ssnr = numer / ( K/(K-1.0) * denom ) - 2.0
+		frc = ssnr / (ssnr + 1)
+		#print "%02d %.3f %.3f (%.3f / %.3f)"%(key, ssnr, frc, numer/K, denom/K)
+		#print key, frc
+		linear[key] = frc
+	sys.stderr.write("\n")
+
+	### output
+	writeFrcPlot("ssnr.dat", linear)
+
+	apDisplay.printMsg("Finished SSNR in "+apDisplay.timeString(time.time()-t0))
+
+
+#===========
+def writeFrcPlot(fname, linear, apix=1.0):
+	length = linear.shape[0]
+	f = open(fname, "w")
+	#f.write("0\t1.0\n")
+	for i in range(1, length):
+		f.write("%.1f\t%.5f\n"%(length/float(i), linear[i]))
+		if linear[i] < 0 and linear[i-1] < 0:
+			break
+	#f.write("%d\t0.0\n&\n"%(length+1))
+	f.close()
+	apDisplay.printMsg("wrote data to: "+fname)
+
+#===========
+def getLinearIndices(fftshape):
+	### figure out which pixels go with which ring
+	length = int(max(fftshape)/2.0)
+	lineardict = {}
+	for i in range(length):
+		for j in range(length):
+			k, m = wrap_coord((i,j), fftshape)
+			r, a = cartToPolar(k, m)
+			index = int(r*1.0)
+			if index < 1 or index >= length:
+				continue
+			if not index in lineardict:
+				lineardict[index] = []
+			lineardict[index].append((i,j))
+	apDisplay.printMsg("Number of rings: "+str(len(lineardict)))
+	return lineardict
 
 #===========
 def wrap_coord(coord, shape):
@@ -32,29 +264,6 @@ def wrap_coord(coord, shape):
 	return (wrapped0, wrapped1)
 
 #===========
-def real_fft2d(image, *args, **kwargs):
-	padshape = numpy.asarray(image.shape)*1
-	padimage = apImage.frame_constant(image, padshape, image.mean())
-	fft = fftpack.fft2(padimage, *args, **kwargs)
-	#normfft = (fft - fft.mean())/fft.std()
-	return fft
-
-#===========
-def inverse_real_fft2d(image, *args, **kwargs):
-	return fftpack.ifft2(image, *args, **kwargs).real
-
-#===========
-def normImage(image):
-	return (image - image.mean())/image.std()
-
-#===========
-def polarToCart(rad, thrad):
-	#thrad = thdeg*math.pi/180.0
-	x = rad*math.cos(thrad)
-	y = rad*math.sin(thrad)
-	return x,y
-
-#===========
 def cartToPolar(x, y):
 	x1 = float(x)
 	y1 = float(y)
@@ -63,173 +272,62 @@ def cartToPolar(x, y):
 	return r, th*180.0/math.pi
 
 #===========
-def linearizeFourierShell(fftim):
-	fftshape = numpy.asarray(fftim.shape, dtype=numpy.float32)
-	fftcenter = fftshape/2.0
-	#print fftshape
-	#print fftcenter
-	length = int(math.ceil(max(fftshape)/math.sqrt(2.0)/2.0))
-	#print length
-	linear = numpy.zeros((length), dtype=numpy.uint8)
-
-	for i in range(fftshape[0]):
-		for j in range(fftshape[1]):
-			x = float(i - fftcenter[0])
-			y = float(j - fftcenter[1])
-			r, a = cartToPolar(x, y)
-			linear[int(r*0.5)] += 1
-	#print linear 
+def mini_ssnr1fft(fftlist, indextuple):
+	"""
+	this function works and is fast
+	"""
+	i,j = indextuple
+	fsum = 0.0
+	K = float(len(fftlist))
+	for fftim in fftlist:
+		F = fftim[i,j]
+		fsum += F
+	fmean = fsum/K
+	numer = abs(fsum)**2
+	denom = 0.0
+	for fftim in fftlist:
+		F = fftim[i,j]
+		denom += abs(F - fmean)**2
+	return numer, denom
 
 #===========
-def fourierRingCorrelation(fftim1, fftim2):
+def mini_ssnr1(partarray, indextuple):
 	"""
-	Formula taken from:
-		http://www.imagescience.de/fsc/index.htm
+	this function works but is slow
 	"""
-	### initialization
-	if fftim1.shape != fftim2.shape:
-		apDisplay.printError("Cannot calculate the FRC for images of different sizes")
-	if len(fftim1.shape) != 2 or len(fftim2.shape) != 2:
-		apDisplay.printError("Cannot calculate the FRC non-2D images")
-	fftshape = numpy.asarray(fftim1.shape, dtype=numpy.float32)
-	fftcenter = fftshape/2.0
-	#print fftshape, fftshape
-	#print fftcenter, fftcenter
-	length = int(max(fftshape)/2.0)
-	#print "linear length", length
-	linear = numpy.zeros((length), dtype=numpy.float32)
-	linear[0] = 1.0
-
-	### figure out which pixels go with which ring
-	lineardict = {}
-	for i in range(fftshape[0]):
-		for j in range(fftshape[1]):
-			k, m = wrap_coord((i,j), fftshape)
-			r = math.hypot(float(k),float(m))
-			index = int(r*1.0)
-			#print (i,j),(k,m),(index,)
-			if index < 1 or index >= length:
-				continue
-			if not index in lineardict:
-				lineardict[index] = []
-			lineardict[index].append((i,j))
-	#print lineardict[1]
-
-	### for each ring calculate the FRC
-	keys = lineardict.keys()
-	keys.sort()
-	lastfrc = 1.0
-	for key in keys:
-		indexlist = lineardict[key]
-		numer = 0.0
-		f1sum = 0.0
-		f2sum = 0.0
-		count = 0
-		for indextuple in indexlist:
-			i,j = indextuple
-			F1 = fftim1[i,j]
-			F2 = fftim2[i,j]
-			#print (i,j), abs(F1), abs(F2), math.atan2(F1.imag, F1.real), math.atan2(F2.imag, F2.real)
-			numer += F1*F2.conjugate()
-			f1sum += abs(F1)**2
-			f2sum += abs(F2)**2
-			count += 1
-		frc = numer / math.sqrt(f1sum*f2sum)
-		#print "*** %02d %03d %.3f (%.4f / %.4f %.4f)"%(key, count, frc, numer/count, f1sum/count, f2sum/count)
-		#print key, frc
-		linear[key] = frc
-		#if key > 2:
-		#	sys.exit(1)
-		if frc < 0 and lastfrc < 0:
-			break
-		lastfrc = frc
-
-	### output
-	f = open("frc.dat", "w")
-	#f.write("0\t1.0\n")
-	apix = 1.55
-	for i in range(1, length):
-		f.write("%.1f\t%.5f\n"%(length/float(i), linear[i]))
-		if linear[i] < 0 and linear[i-1] < 0:
-			break
-	#f.write("%d\t0.0\n&\n"%(length+1))
-	f.close()
-	#print linear
-
+	i,j = indextuple
+	fsum = 0.0
+	K = float(len(partarray))
+	for partimg in partarray:
+		fftim = real_fft2d(partimg)
+		F = fftim[i,j]
+		fsum += F
+	fmean = fsum/K
+	numer = abs(fsum)**2
+	denom = 0.0
+	for partimg in partarray:
+		fftim = real_fft2d(partimg)
+		F = fftim[i,j]
+		denom += abs(F - fmean)**2
+	return numer, denom
 
 #===========
-def spectralSNR(fftimlist):
+def mini_ssnr2(partarray, indextuple):
 	"""
-	Formula taken from:
-		http://www.imagescience.de/fsc/index.htm
+	this function is slow and does not work
 	"""
-	### initialization
-	fftim1 = fftimlist[0]
-	for fftim in fftimlist:
-		if fftim1.shape != fftim.shape:
-			apDisplay.printError("Cannot calculate the FRC for images of different sizes")
-		if len(fftim1.shape) != 2 or len(fftim.shape) != 2:
-			apDisplay.printError("Cannot calculate the FRC non-2D images")
-	fftshape = numpy.asarray(fftim1.shape, dtype=numpy.float32)
-	fftcenter = fftshape/2.0
-	#print fftshape, fftshape
-	#print fftcenter, fftcenter
-	length = int(max(fftshape)/2.0)
-	#print "linear length", length
-	linear = numpy.zeros((length), dtype=numpy.float32)
-	linear[0] = 1.0
-
-	### figure out which pixels go with which ring
-	lineardict = {}
-	for i in range(fftshape[0]):
-		for j in range(fftshape[1]):
-			k, m = wrap_coord((i,j), fftshape)
-			r, a = cartToPolar(k, m)
-			index = int(r*1.0)
-			if index < 1 or index >= length:
-				continue
-			if not index in lineardict:
-				lineardict[index] = []
-			lineardict[index].append((i,j))
-
-	### for each ring calculate the FRC
-	keys = lineardict.keys()
-	keys.sort()
-	for key in keys:
-		indexlist = lineardict[key]
-		count = 0
-		numer = 0.0
-		denom = 0.0
-		for indextuple in indexlist:
-			count += 1
-			i,j = indextuple
-			fsum = 0.0
-			K = float(len(fftimlist))
-			for fftim in fftimlist:
-				F = fftim[i,j]
-				fsum += F
-			fmean = fsum/K
-			numer += abs(fsum)**2
-			for fftim in fftimlist:
-				F = fftim[i,j]
-				denom += abs(F - fmean)**2
-		ssnr = numer / ( K/(K-1.0) * denom ) - 1.0
-		frc = ssnr / (ssnr + 1)
-		#print "%02d %03d %.3f %.3f (%.3f / %.3f)"%(key, count, ssnr, frc, numer/count, denom/count)
-		#print key, frc
-		linear[key] = frc
-
-	### output
-	f = open("ssnr.dat", "w")
-	#f.write("0\t1.0\n")
-	apix = 1.55
-	for i in range(1, length):
-		f.write("%.1f\t%.5f\n"%(length/float(i), linear[i]))
-		if linear[i] < 0 and linear[i-1] < 0:
-			break
-	#f.write("%d\t0.0\n&\n"%(length+1))
-	f.close()
-	#print linear
+	i,j = indextuple
+	fsum = 0.0
+	fsumsq = 0.0
+	K = float(len(partarray))
+	for partimg in partarray:
+		fftim = real_fft2d(partimg)
+		F = fftim[i,j]
+		fsum += F
+		fsumsq += (F**2)
+	numer = abs(fsum)**2
+	denom = abs(fsumsq - fsum**2/K)
+	return numer, denom
 
 #===========
 def printImageInfo(image):
@@ -240,13 +338,40 @@ def printImageInfo(image):
 	print image.min(), image.max()
 	print "============="
 
+def sumComp(clist):
+	sumc = 0.0
+	for c in clist:
+		sumc += c
+	return sumc
+
+def sumsqComp(clist):
+	sumsq = 0.0
+	for c in clist:
+		sumsq += (c**2)
+	return sumsq
+
+def varComp(clist):
+	meanComp = sumComp(clist)/float(len(clist))
+	diffsum = 0.0
+	for c in clist:
+		diffsum += abs(c-meanComp)**2
+	return diffsum
+
+def runComp(clist):
+	sumsq = sumsqComp(clist)
+	sumc = sumComp(clist)
+	N = float(len(clist))
+	return abs(sumsq - sumc**2/N)
+
+
 #===========
 if __name__ == "__main__":
 	bin = 1
 	### read image 1
 	#a = mrc.read("/home/vossman/appion/lib/test01.mrc")
-	a = mrc.read("/home/vossman/appion/lib/waylon2.mrc")
+	#a = mrc.read("/home/vossman/appion/lib/waylon2.mrc")
 	#a = mrc.read("/home/vossman/leginon/holetemplate.mrc")
+	a = mrc.read("/ami/data00/appion/09mar04b/align/kerden11/09apr13q11.7.mrc")
 	a = normImage(a)
 	a = imagefun.bin2(a, bin)
 	#printImageInfo(a)
@@ -254,27 +379,40 @@ if __name__ == "__main__":
 	#printImageInfo(afft)
 
 	### read image 2
+	b = mrc.read("/ami/data00/appion/09mar04b/align/kerden11/09apr13q11.8.mrc")
 	#b = mrc.read("/home/vossman/appion/lib/test02.mrc")
 	#b = mrc.read("/home/vossman/leginon/holetemplate2.mrc")
-	b = mrc.read("/home/vossman/appion/lib/pickwei2.mrc")
+	#b = mrc.read("/home/vossman/appion/lib/pickwei2.mrc")
 	b = normImage(b)
 	b = imagefun.bin2(b, bin)
 	bfft = real_fft2d(b)
 	#printImageInfo(bfft)
 
-	afft = afft/afft.std()
-	bfft = bfft/bfft.std()
-	savePower(afft, "power1.mrc")
-	savePower(bfft, "power2.mrc")
+	#afft = afft/afft.std()
+	#bfft = bfft/bfft.std()
+	#savePower(afft, "power1.mrc")
+	#savePower(bfft, "power2.mrc")
+	spectralSNR([a, b])
 	fourierRingCorrelation(afft, bfft)
-	spectralSNR([afft, bfft])
+	spectralSNRfft([afft, bfft])
 
-	files = glob.glob("/home/vossman/appion/lib/test*.mrc")
+	sys.exit(1)
+
+	files = glob.glob("/ami/data00/appion/09mar04b/align/kerden11/09apr13q11*.mrc")
 	fftlist = []
+	imlist = []
+	t0 = time.time()
 	for fname in files:
 		c = mrc.read(fname)
 		c = normImage(c)
 		c = imagefun.bin2(c, bin)
 		cfft = real_fft2d(c)
+		imlist.append(c)
 		fftlist.append(cfft)
-	spectralSNR(fftlist)
+	apDisplay.printMsg("Finished conversion in "+apDisplay.timeString(time.time()-t0))
+	spectralSNR(imlist)
+	spectralSNRfft(fftlist)
+
+
+
+
