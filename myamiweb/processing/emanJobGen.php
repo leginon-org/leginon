a4a71b684d335cb0e8b6e4d49975d79a3442a45a
Dmitry Lyumkis <dlyumkis>
2009-04-25 02:08:06 +0000

    redid $line.="rm cls*.lst\n";

--- myamiweb/processing/emanJobGen.php
+++ myamiweb/processing/emanJobGen.php
@@ -869,9 +869,7 @@ function writeJobFile ($extra=False) {
 			if ($msgp_minptcls) $line .= " minNumOfPtcls=$msgp_minptcls";
 			$line .= "\n";
 		}
-		$line.="foreach file (cls*.lst)\n";
-		$line.="\trm \$file\n";
-		$line.="end\n";
+		$line.="rm cls*.lst\n";
 		$ejob.= $line;
 	}
 	
