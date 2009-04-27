88e8ef2d6df49193f376c1aced2bbf678cbd9596
Neil Voss <vossman>
2009-04-27 22:44:31 +0000

    subprocess fix

--- appion/lib/apIMAGIC.py
+++ appion/lib/apIMAGIC.py
@@ -6,41 +6,42 @@ import apDisplay
 import subprocess
 
 def executeImagicBatchFile(filename, verbose=False, logfile=None):
-        """
-        executes an IMAGIC batch file in a controlled fashion
-        """
-        subprocess.Popen("chmod 775 "+filename, shell=True)
+	"""
+	executes an IMAGIC batch file in a controlled fashion
+	"""
+	proc = subprocess.Popen("chmod 775 "+filename, shell=True)
+	proc.wait()
 	path = os.path.dirname(filename)
 	os.chdir(path)
 	waited = False
-        t0 = time.time()
-        try:
-                if logfile is not None:
-                        logf = open(logfile, 'a')
-                        process = subprocess.Popen(filename, shell=True, stdout=logf, stderr=logf)
-                elif verbose is False:
-                        process = subprocess.Popen(filename, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                else:
-                        process = subprocess.Popen(filename, shell=True)
-                if verbose is True:
-                        process.wait()
-                else:
-                        ### continuous check
-                        waittime = 0.01
-                        while process.poll() is None:
-                                if waittime > 0.05:
-                                        waited = True
-                                        sys.stderr.write(".")
-                                waittime *= 1.02
-                                time.sleep(waittime)
-        except:
-                apDisplay.printWarning("could not run IMAGIC batchfile: "+filename)
-                raise
-        tdiff = time.time() - t0
-        if tdiff > 20:
-                apDisplay.printMsg("completed in "+apDisplay.timeString(tdiff))
-        elif waited is True:
-                print ""
+	t0 = time.time()
+	try:
+		if logfile is not None:
+			logf = open(logfile, 'a')
+			process = subprocess.Popen(filename, shell=True, stdout=logf, stderr=logf)
+		elif verbose is False:
+			process = subprocess.Popen(filename, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		else:
+			process = subprocess.Popen(filename, shell=True)
+		if verbose is True:
+			process.wait()
+		else:
+			### continuous check
+			waittime = 0.01
+			while process.poll() is None:
+				if waittime > 0.05:
+					waited = True
+					sys.stderr.write(".")
+				waittime *= 1.02
+				time.sleep(waittime)
+	except:
+		apDisplay.printWarning("could not run IMAGIC batchfile: "+filename)
+		raise
+	tdiff = time.time() - t0
+	if tdiff > 20:
+		apDisplay.printMsg("completed in "+apDisplay.timeString(tdiff))
+	elif waited is True:
+		print ""
 
 
 def copyFile(path, file, headers=False):
@@ -53,17 +54,17 @@ def copyFile(path, file, headers=False):
 	batchfile = os.path.join(path, 'copyImage.batch')
 
 	if file[-4:] == ".img" or file[-4:] == ".hed":
-                stripped_file = file[:-4]
+		stripped_file = file[:-4]
 	else:
 		 stripped_file = file
 
 	f = open(batchfile, 'w')
-        f.write("#!/bin/csh -f\n")
-        f.write("setenv IMAGIC_BATCH 1\n")	 
-        f.write("/usr/local/IMAGIC/stand/copyim.e <<EOF \n")
-        f.write(stripped_file+"\n")
-        f.write(stripped_file+"_copy\n")
-        f.write("EOF\n")
+	f.write("#!/bin/csh -f\n")
+	f.write("setenv IMAGIC_BATCH 1\n")	 
+	f.write("/usr/local/IMAGIC/stand/copyim.e <<EOF \n")
+	f.write(stripped_file+"\n")
+	f.write(stripped_file+"_copy\n")
+	f.write("EOF\n")
 	f.write("/usr/local/IMAGIC/stand/imdel.e <<EOF \n")
 	f.write(stripped_file+"\n")
 	f.write("EOF\n")
@@ -73,11 +74,11 @@ def copyFile(path, file, headers=False):
 	f.write("EOF\n")
 	if headers is True:
 		f.write("/usr/local/IMAGIC/stand/headers.e <<EOF \n")
-        	f.write(stripped_file+"\n")
-        	f.write("write\n")
-        	f.write("wipe\n")
-        	f.write("all\n")
-        	f.write("EOF\n")
+		f.write(stripped_file+"\n")
+		f.write("write\n")
+		f.write("wipe\n")
+		f.write("all\n")
+		f.write("EOF\n")
 	f.close()
 
 	executeImagicBatchFile(batchfile)
@@ -86,13 +87,13 @@ def convertFilteringParameters(hpfilt, lpfilt, apix):
 	### CONVERT FILTERING PARAMETERS TO IMAGIC FORMAT BETWEEN 0-1
 	if lpfilt is not "" and apix is not "":
 		lpfilt_imagic = 2 * float(apix) / int(lpfilt)
-        else:
+	else:
 		lpfilt_imagic = False
-        if float(lpfilt_imagic) > 1:
-		lpfilt_imagic = 1        # imagic cannot perform job when lowpass > 1
-        if hpfilt is not "" and apix is not "":
+	if float(lpfilt_imagic) > 1:
+		lpfilt_imagic = 1	# imagic cannot perform job when lowpass > 1
+	if hpfilt is not "" and apix is not "":
 		hpfilt_imagic = 2 * float(apix) / int(hpfilt)
-        else:
+	else:
 		hpfilt_imagic = False
 
 	return hpfilt_imagic, lpfilt_imagic
