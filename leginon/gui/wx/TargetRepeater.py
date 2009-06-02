52627eafdc64700f84d224e221435068bd7bba67
pulokas <pulokas>
2009-06-02 20:17:11 +0000

    gui for target repeater

---
+++ leginon/gui/wx/TargetRepeater.py
@@ -0,0 +1,50 @@
+# The Leginon software is Copyright 2004
+# The Scripps Research Institute, La Jolla, CA
+# For terms of the license agreement
+# see http://ami.scripps.edu/software/leginon-license
+
+import wx
+import gui.wx.Node
+import gui.wx.Settings
+import gui.wx.ToolBar
+
+class Panel(gui.wx.Node.Panel):
+	icon = 'targetrepeater'
+	def __init__(self, parent, name):
+		gui.wx.Node.Panel.__init__(self, parent, -1)
+
+		self.toolbar.AddTool(gui.wx.ToolBar.ID_SETTINGS,
+													'settings',
+													shortHelpString='Settings')
+		self.toolbar.AddSeparator()
+
+		self.toolbar.Realize()
+
+		self.SetSizer(self.szmain)
+		self.SetAutoLayout(True)
+		self.SetupScrolling()
+
+	def onNodeInitialized(self):
+		self.toolbar.Bind(wx.EVT_TOOL, self.onSettingsTool, id=gui.wx.ToolBar.ID_SETTINGS)
+
+	def onSettingsTool(self, evt):
+		dialog = SettingsDialog(self)
+		dialog.ShowModal()
+		dialog.Destroy()
+
+class SettingsDialog(gui.wx.Settings.Dialog):
+	def initialize(self):
+		return ScrolledSettings(self,self.scrsize,False)
+
+class ScrolledSettings(gui.wx.Settings.ScrolledDialog):
+	def initialize(self):
+		gui.wx.Settings.ScrolledDialog.initialize(self)
+		sb = wx.StaticBox(self, -1, 'Target Repeater')
+		sbsz = wx.StaticBoxSizer(sb, wx.VERTICAL)
+
+		self.widgets['bypass'] = wx.CheckBox(self, -1, 'Bypass Filter')
+		sz = wx.GridBagSizer(5, 10)
+		sz.Add(self.widgets['bypass'], (0, 0), (1, 1), wx.ALIGN_CENTER_VERTICAL)
+		sbsz.Add(sz, 0, wx.ALIGN_CENTER|wx.ALL, 5)
+
+		return [sbsz]
