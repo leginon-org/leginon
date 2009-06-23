7abc18025c5817d32c49d34ea76ee9570ea62e97
Neil Voss <vossman>
2009-06-23 19:27:26 +0000

    added alignSubStack and jumperSubStack

--- myamiweb/processing/moreStackTools.php
+++ myamiweb/processing/moreStackTools.php
@@ -37,6 +37,17 @@ echo "  view a list of the available stacks where you can center the particles"
 	." or filter the stack based on the mean and standard deviation";
 echo "</td></tr>";
 
+echo "<tr><td>";
+echo "  <h3><a href='alignSubStack.php?expId=$expId'>Alignment SubStack</a></h3>";
+echo "  make a substack based on an alignment or classification. "
+	."It is easier to select from stack viewer of class averages";
+echo "</td></tr>";
+
+echo "<tr><td>";
+echo "  <h3><a href='jumpSubStack.php?expId=$expId'>Jumpers SubStack</a></h3>";
+echo "  make a substack based on particle 'jumpers' from a reconstruction.";
+echo "</td></tr>";
+
 echo "</table>";
 processing_footer();
 exit;
