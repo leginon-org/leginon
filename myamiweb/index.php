019bdae34232fbe28aa9ab91cbfb41b513fe9043
dfellman <dfellman>
2009-07-13 19:58:05 +0000

    M    index.php

--- myamiweb/index.php
+++ myamiweb/index.php
@@ -27,55 +27,11 @@ $link->addlink('/phpMyAdmin/','phpMyAdmin','', 'phpMyAdmin');
 $link->addlink('template.php', 'Hole Template viewer','', '');
 
 
-$ip = $_SERVER['REMOTE_ADDR'];
-$host = $_SERVER['REMOTE_HOST'];
-
-/*
-$title = "Viewed sessions";
-$R = $leginondata->getviewerlog('last');
-$content="<b>Lastest:</b><table>";
-if ($R)
-foreach ($R as $r) {
-	$url = "3wviewer.php?expId=".$r['sessionId'];
-	$content .= "<tr><td><a href='".$url."'>".$r['name']."</a> ".$r['comment']."</td></tr>";
-}
-$content.="</table>";
-
-$content.="<b>Most:</b><table>";
-$R = $leginondata->getviewerlog('most');
-if ($R)
-foreach ($R as $r) {
-	$url = "3wviewer.php?expId=".$r['sessionId'];
-	$content .= "<tr><td><a href='".$url."'>".$r['name']."</a> ".$r['comment']."</td></tr>";
-}
-$content.="</table>";
-$log = display_table($title, $content);
-
-$title = "Viewed sessions from : $host";
-
-$R = $leginondata->getviewerlog('last', $host, $ip);
-$content="<b>Lastest:</b><table>";
-foreach ($R as $r) {
-	$url = "3wviewer.php?expId=".$r['sessionId'];
-	$content .= "<tr><td><a href='".$url."'>".$r['name']."</a> ".$r['comment']."</td></tr>";
-}
-$content.="</table>";
-$R = $leginondata->getviewerlog('most', $host, $ip);
-$content.="<b>Most:</b><table>";
-foreach ($R as $r) {
-	$url = "3wviewer.php?expId=".$r['sessionId'];
-	$content .= "<tr><td><a href='".$url."'>".$r['name']."</a> ".$r['comment']."</td></tr>";
-}
-$content.="</table>";
-$log_from = display_table($title, $content);
-*/
-
-
 $title = "Leginon II database Tools";
 viewer_header($title);
 ?>
 <style>
-	BODY {background-image:url('img/background.jpg')}
+	body {background-image:url('img/background.jpg')}
 </style>
 
 <script src="js/prototype.js"></script>
@@ -101,19 +57,9 @@ viewer_header($title);
 <noscript>
 <?php echo divtitle("<center>Please enable Javascript in you Browser</center>"); ?>
 </noscript>
-<table align="center">
-<tr valign="top">
-<? /*
-	<td>
-		<?php echo $log_from; ?>
-		<?php echo $log; ?>
-	</td>
-*/ ?>
-	<td>
+<p>
 		<?php echo $link->Display(); ?>
-	</td>
-</tr>
-</table>
+</p>
 <label for="query"><strong>Session finder:</strong>&nbsp;
 <input style="border: 1px solid #bdcebb;" type="text" name="search" autocomplete="off" id="query" onKeyUp="preSearch()" />
 </label>
