bcea6477aa3c386bb71d625d4a46902fd9b4667d
Anchi Cheng <acheng>
2009-07-22 21:39:08 +0000

    apix

--- appion/lib/appionLoop2.py
+++ appion/lib/appionLoop2.py
@@ -45,6 +45,7 @@ class AppionLoop(appionScript.AppionScript):
 		### get images from database
 		self._getAllImages()
 		os.chdir(self.params['rundir'])
+		self.params['apix'] = apDatabase.getPixelSize(self.imgtree[0])
 		self.preLoopFunctions()
 		### start the loop
 		self.notdone=True
