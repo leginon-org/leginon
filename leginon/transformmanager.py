04f008fa756deb926f48c4c4ecea9c3c56df0e3a
pulokas <pulokas>
2009-09-01 22:17:53 +0000

    fixed debugging output

--- leginon/transformmanager.py
+++ leginon/transformmanager.py
@@ -161,13 +161,18 @@ class TargetTransformer(targethandler.TargetHandler):
 
 	def calculateMatrix(self, image1, image2):
 		## Debugging...
-		fileref = image1.special_getitem('image', dereference=False)
 		print 'IMAGE DBID', image1.dbid
 		print 'IMAGE DMID', image1.dmid
 		print 'IMAGE PATH', image1.getpath()
 		print 'IMAGE FILENAME', image1.filename()
-		print 'FILEREF PATH', fileref.path
-		print 'FILEREF FILENAME', fileref.filename
+		fileref = image1.special_getitem('image', dereference=False)
+		import sinedon.newdict
+		import numpy
+		if isinstance(fileref, sinedon.newdict.FileReference):
+			print 'FILEREF PATH', fileref.path
+			print 'FILEREF FILENAME', fileref.filename
+		elif isinstance(fileref, numpy.ndarray):
+			print 'FILEREF ALREADY READ'
 		## End debugging
 		array1 = image1['image']
 		array2 = image2['image']
