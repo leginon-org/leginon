6f1ce2ee7d45b36f2a391eacefa531fc740fbd5e
pulokas <pulokas>
2009-09-03 03:50:22 +0000

    trying to phase out dmid

--- leginon/acquisition.py
+++ leginon/acquisition.py
@@ -47,14 +47,9 @@ def setImageFilename(imagedata):
 	if imagedata['filename'] is not None:
 		return
 	listlabel = ''
-	## use either data id or target number
-	if imagedata['target'] is None or imagedata['target']['number'] is None:
-		print 'This image does not have a target number, it would be nice to have an alternative to target number, like an image number.  for now we will use dmid'
-		numberstr = '%05d' % (imagedata.dmid[-1],)
-	else:
-		numberstr = '%05d' % (imagedata['target']['number'],)
-		if imagedata['target']['list'] is not None:
-			listlabel = imagedata['target']['list']['label']
+	numberstr = '%05d' % (imagedata['target']['number'],)
+	if imagedata['target']['list'] is not None:
+		listlabel = imagedata['target']['list']['label']
 	if imagedata['preset'] is None:
 		presetstr = ''
 	else:
