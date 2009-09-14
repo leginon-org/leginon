b5d452fd4e974d3512a5a1a8f8bd3d53bd275d30
pulokas <pulokas>
2009-09-14 21:49:15 +0000

    args changed in new version of numpy

--- pyami/imagefun.py
+++ pyami/imagefun.py
@@ -39,10 +39,16 @@ def despike(image, size=11, sigma=3.5, debug=0):
 	numextension.despike(image, size, sigma, debug)
 
 def medianSeries(series):
-	return numpy.median(series, 0)
+	try:
+		return numpy.median(series, 0)
+	except:
+		return numpy.median(series)
 
 def averageSeries(series):
-	return numpy.mean(series, 0)
+	try:
+		return numpy.mean(series, 0)
+	except:
+		return numpy.mean(series)
 
 def scale(a, scale):
 	if scale == 1.0:
