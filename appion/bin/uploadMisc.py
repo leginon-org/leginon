a3fd3953cb1c0513ae848ba5c2bf6a82b32a729f
Anchi Cheng <acheng>
2009-09-15 20:07:34 +0000

    fulltomo misc

--- appion/bin/uploadMisc.py
+++ appion/bin/uploadMisc.py
@@ -12,6 +12,7 @@ import apDisplay
 import appiondata
 import apFile
 import apProject
+import apTomo
 
 #=====================
 #=====================
@@ -26,6 +27,8 @@ class UploadMiscScript(appionScript.AppionScript):
 			help="Session name associated with file (e.g. 06mar12a)", metavar="SESSION")
 		self.parser.add_option("-r", "--reconid", dest="reconid", type='int',
 			help="ReconID associated with file (e.g. --reconid=311)", metavar="RECONID")
+		self.parser.add_option("-t", "--fulltomoid", dest="fulltomoid", type='int',
+			help="Full tomogram ID associated with file (e.g. --fulltomoid=311)", metavar="FULLTOMOID")
 
 	#=====================
 	def checkConflicts(self):
@@ -37,8 +40,24 @@ class UploadMiscScript(appionScript.AppionScript):
 			apDisplay.printError("file does not exist")
 		if self.params['description'] is None:
 			apDisplay.printError("enter a file description")
-		if self.params['session'] is None and self.params['reconid'] is None:
-			apDisplay.printError("please enter either session or reconID")
+		print self.params
+		if self.params['session'] is None and self.params['reconid'] is None and self.params['fulltomoid'] is None:
+			apDisplay.printError("please enter either session or reconID or fulltomoid")
+
+	#=====================
+	def setRunDir(self):
+		if self.params['reconid'] is not None:
+			self.recondata = apRecon.getRefineRunDataFromID(self.params['reconid'])
+			path = self.recondata['path']['path']
+		if self.params['fulltomoid'] is not None:
+			self.tomodata = apTomo.getFullTomoData(self.params['fulltomoid'])
+			path = self.tomodata['path']['path']
+		if self.params['session'] is not None:
+			sessiondata = apDatabase.getSessionDataFromSessionName(self.params['session'])
+			path = os.path.abspath(sessiondata['image path'])
+			path = re.sub("leginon","appion",path)
+			path = re.sub("/rawdata","",path)
+		self.params['rundir'] = os.path.join(path,"misc")
 
 	#=====================
 	def setProcessingDirName(self):
@@ -51,6 +70,12 @@ class UploadMiscScript(appionScript.AppionScript):
 		if self.params['reconid'] is not None:
 			miscq['refinementRun'] = self.recondata
 			sessiondata = apRecon.getSessionDataFromReconId(self.params['reconid'])
+			miscq['session'] = sessiondata
+			projectid = apProject.getProjectIdFromSessionName(sessiondata['name'])
+			miscq['project|projects|project'] = projectid
+		elif self.params['fulltomoid'] is not None:
+			miscq['fulltomogram'] = self.tomodata
+			sessiondata = self.tomodata['session']
 			miscq['session'] = sessiondata
 			projectid = apProject.getProjectIdFromSessionName(sessiondata['name'])
 			miscq['project|projects|project'] = projectid
@@ -69,13 +94,15 @@ class UploadMiscScript(appionScript.AppionScript):
 		else:
 			apDisplay.printWarning("not committing to DB")
 
-
 	#=====================
 	def start(self):
 
 		if self.params['reconid'] is not None:
 			self.recondata = apRecon.getRefineRunDataFromID(self.params['reconid'])
 			print "Associated with",self.recondata['name'],":",self.recondata['path']
+		if self.params['fulltomoid'] is not None:
+			self.tomodata = apTomo.getFullTomoData(self.params['fulltomoid'])
+			print "Associated with",self.tomodata['name'],":",self.tomodata['path']['path']
 		if self.params['session'] is not None:
 			self.sessiondata = apDatabase.getSessionDataFromSessionName(self.params['session'])
 			self.params['projectId'] = apProject.getProjectIdFromSessionName(self.params['session'])
