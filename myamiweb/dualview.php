4ba6a53ffd973252e787c2deba145d893990fdc7
Gabriel Lander <glander>
2009-09-24 03:41:42 +0000

    added dualview to dbem

---
+++ myamiweb/dualview.php
@@ -0,0 +1,7 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd">
+<html>
+<frameset cols="50%,50%" bordercolor="#a0a0a0">
+    <frame name="frv1" src="imageviewer.php" frameborder="yes">
+    <frame name="frv2" src="imageviewer.php" frameborder="yes">
+</frameset>
+</html>
