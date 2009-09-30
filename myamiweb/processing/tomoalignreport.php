5ca24685394ae3b681fb297f0932317e4e349d93
Anchi Cheng <acheng>
2009-09-30 01:40:49 +0000

    first version of aligner report

---
+++ myamiweb/processing/tomoalignreport.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ *	The Leginon software is Copyright 2003 
+ *	The Scripps Research Institute, La Jolla, CA
+ *	For terms of the license agreement
+ *	see  http://ami.scripps.edu/software/leginon-license
+ *
+ */
+
+require "inc/particledata.inc";
+require "inc/leginon.inc";
+require "inc/project.inc";
+require "inc/viewer.inc";
+require "inc/processing.inc";
+  
+// check if coming directly from a session
+$expId = $_GET['expId'];
+if ($expId) {
+        $sessionId=$expId;
+        $formAction=$_SERVER['PHP_SELF']."?expId=$expId";
+}
+else {
+        $sessionId=$_POST['sessionId'];
+        $formAction=$_SERVER['PHP_SELF'];
+}
+
+if ($_GET['showHidden']) {
+	$showhidden = True;
+	$formAction.="&showHidden=1";
+}
+$alignId = $_GET['alignId'];
+$projectId = (int) getProjectFromExpId($expId);
+
+$javascript = "<script src='../js/viewer.js'></script>\n";
+$javascript.= editTextJava();
+
+processing_header("Tomographic Alignment Run Report","Tilt Series Alignment Report Page", $javascript);
+
+// edit description form
+echo "<form name='templateform' method='post' action='$formAction'>\n";
+
+$particle = new particledata();
+$runinfo = $particle->getTomoAlignmentInfo($alignId);
+$rundir = $runinfo['path'];
+if ($runinfo['method'] == 'protomo')
+	$allcycles = $particle->getProtomoAlignerInfoFromAlignmentRun($alignId,False);
+if ($_POST) {
+	foreach ($allcycles as $t)
+		$particle->updateTableDescriptionAndHiding($_POST,'ApProtomoAlignerParamsData',$t['talignerid']);
+}
+
+// --- Get Averaged tomograms
+if (!$showhidden) {
+	$showncycles = $particle->getProtomoAlignerInfoFromAlignmentRun($alignId,False);
+	$allcycles = $particle->getProtomoAlignerInfoFromAlignmentRun($alignId,True);
+} else {
+	$showncycles = $particle->getProtomoAlignerInfoFromAlignmentRun($alignId,True);
+	$allcycles = $allcycles;
+}
+echo $particle->displayHidingOption($expId,$allcycles,$showncycles,$showhidden);
+if ($showncycles) {
+	$html = "<h4>Protomo Alignment Cycles</h4>";
+	$html .= "<table class='tableborder' border='1' cellspacing='1' cellpadding='5'>\n";
+	$html .= "<TR>\n";
+	$selected_keys = array ( 'refine cycle','alignerid','reset cycle','align sampling','align box size','description');
+	$display_keys = $selected_keys;
+	$display_keys[2] = "reset cycle<br>[image range]";
+	foreach($display_keys as $key) {
+		$html .= "<td><span class='datafield0'>".$key."</span> </TD> ";
+	}
+	$html .= "</TR>\n";
+	foreach ($showncycles as $t) {
+		if ($t['good cycle'])
+			$t['reset cycle'] = $t['good cycle'].'<br>['.$t['good start'].' : '.$t['good end'].']';
+		$t['align box size'] = '('.$t['align box x']*$t['align sampling'].','.$t['align box y']*$t['align sampling'].')';
+		$html .= $particle->displayParametersInSummary($t,$selected_keys,$expId,$hide_button_field='alignerid');
+	}
+	$html .= "</table>\n";
+	$html .= "<br>\n";
+	echo $html;
+} else {
+	$html = "<p>no tilt series alignment available</p>";
+	echo $html;
+}
+echo $particle->displayHidingOption($expId,$allcycles,$showncycles,$showhidden);
+// --- 
+
+processing_footer();
+?>
