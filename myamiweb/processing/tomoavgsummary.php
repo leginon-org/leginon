52c907f736e58d85e37b3741f583e59293259cdb
Anchi Cheng <acheng>
2009-09-30 01:39:41 +0000

    move hide option function to particle.inc

--- myamiweb/processing/tomoavgsummary.php
+++ myamiweb/processing/tomoavgsummary.php
@@ -26,6 +26,7 @@ else {
 
 if ($_GET['showHidden'])
 	$formAction.="&showHidden=1";
+	$showhidden = True;
 $projectId = (int) getProjectFromExpId($expId);
 
 $javascript = "<script src='../js/viewer.js'></script>\n";
@@ -37,6 +38,11 @@ processing_header("Tomographic Reconstruction Summary","Tomogram Summary Page", 
 echo "<form name='templateform' method='post' action='$formAction'>\n";
 
 $particle = new particledata();
+$allavgtomos = $particle->getAveragedTomogramsFromSession($sessionId, True);
+if ($_POST) {
+	foreach ($allavgtomos as $t)
+		$particle->updateTableDescriptionAndHiding($_POST,'ApTomoAverageRunData',$t['avgid']);
+}
 // --- Get Averaged tomograms
 if (!$_GET['showHidden']) {
 	$shownavgtomos = $particle->getAveragedTomogramsFromSession($sessionId, False);
@@ -45,55 +51,22 @@ if (!$_GET['showHidden']) {
 	$shownavgtomos = $particle->getAveragedTomogramsFromSession($sessionId, True);
 	$allavgtomos = $allavgtomos;
 }
-if (!$_GET['showHidden'] && count($allavgtomos) != count($shownavgtomos)) {
-	$numhidden = count($allavgtomos) - count($shownavgtomos);
-	echo "<a href='".$_SERVER['PHP_SELF']."?expId=$expId&showHidden=1'>[Show ".$numhidden." hidden runs]</a><br/><br/>\n";
-} elseif ($_GET['showHidden']) {
-	echo "<a href='".$_SERVER['PHP_SELF']."?expId=$expId&showHidden=0'>[Hide hidden runs]</a><br/><br/>\n";
-}
+echo $particle->displayHidingOption($expId,$allavgtomos,$shownavgtomos,$showhidden);
 if ($shownavgtomos) {
 	$html = "<h4>Averaged SubTomograms</h4>";
 	$html .= "<table class='tableborder' border='1' cellspacing='1' cellpadding='5'>\n";
 	$html .= "<TR>\n";
-	$display_keys = array ( 'id','runname','stack','description');
+	$display_keys = array ( 'avgid','runname','stack','description');
 	foreach($display_keys as $key) {
 		$html .= "<td><span class='datafield0'>".$key."</span> </TD> ";
 	}
 	$html .= "</TR>\n";
 	foreach ($shownavgtomos as $t) {
-		$avgid = $t['avgid'];
-		// update description
-		if ($_POST['updateDesc'.$avgid]) {
-			updateDescription('ApTomoAverageRunData', $avgid, $_POST['newdescription'.$avgid]);
-			$t['description']=$_POST['newdescription'.$avgid];
-		}
-
-		if ($_POST['hideStack'.$avgid] == 'hide') {
-			$particle->updateHide('ApTomoAverageRunData', $avgid, '1');
-			$t['hidden']=1;
-		} elseif ($_POST['unhideStack'.$avgid] == 'unhide') {
-			$particle->updateHide('ApTomoAverageRunData', $avgid, '0');
-			$t['hidden']=0;
-		}
-
 		$stackid = $t['stackid'];
 		$stackparams = $particle->getStackParams($stackid);
 		$stackname = $stackparams['shownstackname'];
-		$html .= "<TR>\n";
-		// runid and hide
-		$html .= "<TD valign='center' align='center'>$avgid\n";
-		if ($t['hidden'] == 1) {
-			$html.= "<br/><font color='#cc0000'>HIDDEN</font>\n";
-			$html.= " <input class='edit' type='submit' name='unhideStack".$avgid."' value='unhide'>\n";
-		} else $html .= "<br/><input class='edit' type='submit' name='hideStack".$avgid."' value='hide'>\n";
-		$html .= "</td>\n";
-		$html .= "<td><A HREF='tomoavgreport.php?expId=$expId&avgId=$avgid'>".$t['runname']."</A></TD>\n";
-		$html .= "<td><A HREF='stackreport.php?expId=$expId&sId=$stackid']'>$stackname</A></TD>\n";
-
-		# add edit button to description if logged in
-		$descDiv = ($_SESSION['username']) ? editButton($avgid,$t['description']) : $t['description'];
-		$html .= "<td>$descDiv</td>\n";
-		$html .= "</tr>\n";
+		$t['stack'] = array('link'=>$stackid,'display'=>$stackname);
+		$html .= $particle->displayParametersInSummary($t,$display_keys,$expId,$hide_button_field='avgid');
 	}
 	$html .= "</table>\n";
 	$html .= "<br>\n";
@@ -102,13 +75,7 @@ if ($shownavgtomos) {
 	$html = "<p>no averaged tomograms available</p>";
 	echo $html;
 }
-
-if (!$_GET['showHidden'] && count($allavgtomos) != count($shownavgtomos)) {
-	$numhidden = count($allavgtomos) - count($shownavgtomos);
-	echo "<a href='".$formAction."&showHidden=1'>[Show ".$numhidden." hidden runs]</a><br/><br/>\n";
-} elseif ($_GET['showHidden']) {
-	echo "<a href='".$formAction."&showHidden=0'>[Hide hidden runs]</a><br/><br/>\n";
-}
+echo $particle->displayHidingOption($expId,$alltomoaligns,$showntomoaligns,$showhidden);
 // --- 
 
 processing_footer();
