36b8610270814a2936d35262978288b1d11dfc21
Anchi Cheng <acheng>
2009-10-10 01:12:57 +0000

    aligner report for other than protomo

--- myamiweb/processing/tomoalignreport.php
+++ myamiweb/processing/tomoalignreport.php
@@ -44,58 +44,60 @@ echo "<form name='templateform' method='post' action='$formAction'>\n";
 $particle = new particledata();
 $runinfo = $particle->getTomoAlignmentInfo($alignId);
 $rundir = $runinfo['path'];
-if ($runinfo['method'] == 'protomo') {
+if (!$runinfo['protomoid']) {
+	$aligners = $particle->getTomoAlignersFromAlignmentRun($alignId,False);
+	echo "<a href='protomoreport.php?expId=".$expId."&aId=".$aligners[0]['alignerid']."'><b>[Redirect to Aligner Report]</b></a>";
+} else {
 	$allcycles = $particle->getProtomoAlignerInfoFromAlignmentRun($alignId,False);
-}
-if ($_POST && $allcycles) {
-	foreach ($allcycles as $t)
-		$particle->updateTableDescriptionAndHiding($_POST,'ApProtomoAlignerParamsData',$t['alignerid']);
-}
+	if ($_POST && $allcycles) {
+		foreach ($allcycles as $t)
+			$particle->updateTableDescriptionAndHiding($_POST,'ApProtomoAlignerParamsData',$t['alignerid']);
+	}
 
-// --- Get Aligner cycles
-if (!$showhidden) {
-	$showncycles = $particle->getProtomoAlignerInfoFromAlignmentRun($alignId,False);
-	$allcycles = $particle->getProtomoAlignerInfoFromAlignmentRun($alignId,True);
-} else {
-	$showncycles = $particle->getProtomoAlignerInfoFromAlignmentRun($alignId,True);
-	$allcycles = $allcycles;
-}
-echo $particle->displayHidingOption($expId,$allcycles,$showncycles,$showhidden);
-if ($showncycles) {
-	$html = "<h4>Protomo Alignment Cycles</h4>";
-	$html .= "<table class='tableborder' border='1' cellspacing='1' cellpadding='5'>\n";
-	$html .= "<TR>\n";
-	$selected_keys = array ( 'refine cycle','alignerid','reset cycle','align sampling','align box size','description');
-	$display_keys = $selected_keys;
-	$display_keys[2] = "reset cycle<br>[accept range]";
-	$display_keys[3] = "align<br>sampling";
-	foreach($display_keys as $key) {
-		$html .= "<td><span class='datafield0'>".$key."</span> </TD> ";
-	}
-	$html .= "</TR>\n";
-	foreach ($showncycles as $t) {
-		if ($t['good cycle'])
-			$t['reset cycle'] = $t['good cycle'].'<br>['.$t['good start'].' : '.$t['good end'].']';
-		$t['align box size'] = '('.$t['align box x']*$t['align sampling'].','.$t['align box y']*$t['align sampling'].')';
-	$t['refine cycle'] = array('display'=>$t['refine cycle'],'link'=>$t['alignerid']);
-		$html .= $particle->displayParametersInSummary($t,$selected_keys,$expId,$hide_button_field='alignerid');
-	}
-	$html .= "</table>\n";
-	$html .= "<br>\n";
-	$total = count($showncycles);
-	if ($total >= 2) {
-		$html .= "<a href='runTomoAligner.php?expId=".$expId."&lastaId=".$showncycles[$total-2]['alignerid']."'><b>[Repeat Last Aligner Cycle]</b></a>";
+	// --- Get Aligner cycles
+	if (!$showhidden) {
+		$showncycles = $particle->getProtomoAlignerInfoFromAlignmentRun($alignId,False);
+		$allcycles = $particle->getProtomoAlignerInfoFromAlignmentRun($alignId,True);
 	} else {
-		$html .= "<a href='runTomoAligner.php?expId=".$expId."'>[Repeat Last Aligner Cycle]</b></a>";
+		$showncycles = $particle->getProtomoAlignerInfoFromAlignmentRun($alignId,True);
+		$allcycles = $allcycles;
 	}
-	$html .= "<a href='runTomoAligner.php?expId=".$expId."&lastaId=".$t['alignerid']."'><b>[Set up Next Aligner Cycle]</b></a>";
-	echo $html;
-} else {
-	$html = "<p>no alignment information available</p>";
-	echo $html;
+	echo $particle->displayHidingOption($expId,$allcycles,$showncycles,$showhidden);
+	if ($showncycles) {
+		$html = "<h4>Protomo Alignment Cycles</h4>";
+		$html .= "<table class='tableborder' border='1' cellspacing='1' cellpadding='5'>\n";
+		$html .= "<TR>\n";
+		$selected_keys = array ( 'refine cycle','alignerid','reset cycle','align sampling','align box size','description');
+		$display_keys = $selected_keys;
+		$display_keys[2] = "reset cycle<br>[accept range]";
+		$display_keys[3] = "align<br>sampling";
+		foreach($display_keys as $key) {
+			$html .= "<td><span class='datafield0'>".$key."</span> </TD> ";
+		}
+		$html .= "</TR>\n";
+		foreach ($showncycles as $t) {
+			if ($t['good cycle'])
+				$t['reset cycle'] = $t['good cycle'].'<br>['.$t['good start'].' : '.$t['good end'].']';
+			$t['align box size'] = '('.$t['align box x']*$t['align sampling'].','.$t['align box y']*$t['align sampling'].')';
+		$t['refine cycle'] = array('display'=>$t['refine cycle'],'link'=>$t['alignerid']);
+			$html .= $particle->displayParametersInSummary($t,$selected_keys,$expId,$hide_button_field='alignerid');
+		}
+		$html .= "</table>\n";
+		$html .= "<br>\n";
+		$total = count($showncycles);
+		if ($total >= 2) {
+			$html .= "<a href='runTomoAligner.php?expId=".$expId."&lastaId=".$showncycles[$total-2]['alignerid']."'><b>[Repeat Last Aligner Cycle]</b></a>";
+		} else {
+			$html .= "<a href='runTomoAligner.php?expId=".$expId."'>[Repeat Last Aligner Cycle]</b></a>";
+		}
+		$html .= "<a href='runTomoAligner.php?expId=".$expId."&lastaId=".$t['alignerid']."'><b>[Set up Next Aligner Cycle]</b></a>";
+		echo $html;
+	} else {
+		$html = "<p>no alignment information available</p>";
+		echo $html;
+	}
+	echo $particle->displayHidingOption($expId,$allcycles,$showncycles,$showhidden);
+	// --- 
 }
-echo $particle->displayHidingOption($expId,$allcycles,$showncycles,$showhidden);
-// --- 
-
 processing_footer();
 ?>
