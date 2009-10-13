b3c89d8d8fcf09ccdc6915de72d435d855e52fe3
pulokas <pulokas>
2009-10-13 19:40:37 +0000

    more changes

--- leginon/caustic.py
+++ leginon/caustic.py
@@ -23,6 +23,7 @@ def saveMRC(image, name):
 	mrc.write(image, name)
 
 def gradient(a):
+	a = ndimage.gaussian_filter(a, 1.5)
 	a = ndimage.generic_gradient_magnitude(a, ndimage.sobel)
 	a = numpy.abs(a)
 	return a
@@ -103,9 +104,7 @@ def findCaustic(input, smallrange, bigrange, mask, binning=None):
 		bin_radii_big = numpy.arange(bigmin, bigmax)
 		print 'BINRADIIBIG', len(bin_radii_big), bin_radii_big
 
-		bin_mask = mask / binning
-
-		small_circle, big_circle = __findCaustic(bin_input, bin_radii_small, bin_radii_big, bin_mask)
+		small_circle, big_circle = __findCaustic(bin_input, bin_radii_small, bin_radii_big, mask)
 		print 'BINCIRCLES', small_circle, big_circle
 
 		# set up ranges for full size image
@@ -113,24 +112,24 @@ def findCaustic(input, smallrange, bigrange, mask, binning=None):
 
 		### XXX need to make sure new radii do not include more than original
 		rsmall = small_circle['radius']
-		smallmin = int(numpy.floor(rsmall * binning - 4 * binning))
-		smallmax = int(numpy.ceil(rsmall * binning + 4 * binning))
+		smallmin = int(numpy.floor(rsmall * binning - binning/2.0))
+		smallmax = int(numpy.ceil(rsmall * binning + binning/2.0))
 		smallrange = smallmin, smallmax
 		rbig = big_circle['radius']
-		bigmin = int(numpy.floor(rbig * binning - 4 * binning))
-		bigmax = int(numpy.ceil(rbig * binning + 4 * binning))
+		bigmin = int(numpy.floor(rbig * binning - binning/2.0))
+		bigmax = int(numpy.ceil(rbig * binning + binning/2.0))
 		bigrange = bigmin, bigmax
 		print 'NEWRANGES', smallrange, bigrange
 
-		small_row0 = binning * (small_circle['center'][0] - 2)
-		small_row1 = binning * (small_circle['center'][0] + 2)
-		small_col0 = binning * (small_circle['center'][1] - 2)
-		small_col1 = binning * (small_circle['center'][1] + 2)
+		small_row0 = binning * (small_circle['center'][0] - 1)
+		small_row1 = binning * (small_circle['center'][0] + 1)
+		small_col0 = binning * (small_circle['center'][1] - 1)
+		small_col1 = binning * (small_circle['center'][1] + 1)
 		small_limit = small_row0, small_row1, small_col0, small_col1
-		big_row0 = binning * (big_circle['center'][0] - 2)
-		big_row1 = binning * (big_circle['center'][0] + 2)
-		big_col0 = binning * (big_circle['center'][1] - 2)
-		big_col1 = binning * (big_circle['center'][1] + 2)
+		big_row0 = binning * (big_circle['center'][0] - 1)
+		big_row1 = binning * (big_circle['center'][0] + 1)
+		big_col0 = binning * (big_circle['center'][1] - 1)
+		big_col1 = binning * (big_circle['center'][1] + 1)
 		big_limit = big_row0, big_row1, big_col0, big_col1
 
 	radii_small = numpy.arange(smallrange[0], smallrange[1]+1, dtype=numpy.int)
@@ -169,6 +168,7 @@ def __findCaustic(input, radii_small, radii_big, mask, small_limit=None, big_lim
 	print 'finding caustic curve'
 	if big_limit is None:
 		big_limit = 0, input.shape[0], 0, input.shape[1]
+	saveMRC(newgrad, 'newgrad.mrc')
 	circle_big = findBestCircle2(newgrad, radii_big, big_limit)
 
 	return circle_small, circle_big
@@ -181,15 +181,15 @@ if __name__ == '__main__':
 	radii_small = (6,40)
 	radii_big = (80,100)
 
-	input = mrc.read('input8.mrc')
-	radii_small = numpy.multiply((6,40), 2)
-	radii_big = numpy.multiply((80,100), 2)
+	#input = mrc.read('input8.mrc')
+	radii_small = (23, 35)
+	radii_big = (179, 192)
 
-	#input = mrc.read('09jul22caustic_00007ma_0.mrc')
-	#radii_small = numpy.multiply((6,40), 16)
-	#radii_big = numpy.multiply((80,100), 16)
+	input = mrc.read('09jul22caustic_00007ma_0.mrc')
+	radii_small = numpy.multiply((6,40), 16)
+	radii_big = numpy.multiply((80,100), 16)
 
 	t0 = time.time()
-	findCaustic(input, radii_small, radii_big, 1.8, 2)
+	findCaustic(input, radii_small, radii_big, 2.0, 16)
 	t1 = time.time()
 	print 'TIME', t1-t0
