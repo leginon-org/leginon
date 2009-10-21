06cdc713b43ad0dccddc4446aad127caf909ae16
Neil Voss <vossman>
2009-10-21 20:26:13 +0000

    allow low tilts

--- myamiweb/processing/runMakeStack2.php
+++ myamiweb/processing/runMakeStack2.php
@@ -506,7 +506,7 @@ function runMakestack() {
 	$maxang = $particle->getMaxTiltAngle($expId);
 	if ($maxang > 5) {
 		$tiltangle = $_POST['tiltangle'];
-		if ($_POST['ctfcorrect']=='on' && $_POST['ctfcorrecttype']!='emantilt' && $tiltangle!='notilt') {
+		if ($_POST['ctfcorrect']=='on' && $_POST['ctfcorrecttype']!='emantilt' && ($tiltangle!='notilt' || $tiltangle!='lowtilt')) {
 			createMakestackForm("CtfCorrect does not work on tilted images");
 			exit;
 		}
