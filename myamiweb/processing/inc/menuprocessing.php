3d5b5d9ebb0ed31ef49aafd786c06b491f91adb9
Gabriel Lander <glander>
2009-11-24 17:56:26 +0000

    - look for pyace2 runs in the database

--- myamiweb/processing/inc/menuprocessing.php
+++ myamiweb/processing/inc/menuprocessing.php
@@ -241,6 +241,9 @@ if ($expId) {
 	$ace2done = count($subclusterjobs['ace2']['done']);
 	$ace2run = count($subclusterjobs['ace2']['running']);
 	$ace2q = count($subclusterjobs['ace2']['queued']);
+	$ace2done+= count($subclusterjobs['pyace2']['done']);
+	$ace2run = count($subclusterjobs['pyace2']['running']);
+	$ace2q = count($subclusterjobs['pyace2']['queued']);
 
 	$ace2results[] = ($ace2done==0) ? "" : "<a href='ctfreport.php?expId=$sessionId'>$ace2done complete</a>";
 	$ace2results[] = ($ace2run==0) ? "" : "<a href='listAppionJobs.php?expId=$sessionId&jobtype=ace2'>$ace2run running</a>";
