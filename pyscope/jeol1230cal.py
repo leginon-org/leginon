ba47f13094a12a50c58d603473370e4bd055e10f
Anchi Cheng <acheng>
2010-02-17 21:25:23 +0000

    remove files that should have been gone in skipped pyScope revision 319

--- pyscope/jeol1230cal.py
+++
@@ -1,9 +0,0 @@
-# jeol1230cal contains calibration table
-# Author: Minghui Hu, mhu@nysbc.org, New York Structural Biology Center
-
-from pyscope import jeol1230mags
-
-f_defocus_H_rate = 1.0     	# The measured defocus and indicated defocus has a factor of 1.65,
-
-screenup_mag = jeol1230mags.screenup
-screendown_mag = jeol1230mags.screendown
--- pyscope/jeol1230data.py
+++
@@ -1,5 +0,0 @@
-# jeol1230data.py defines jeol1230 data structure
-# Author: Minghui Hu, mhu@nysbc.org, New York Structural Biology Center
-
-
-
