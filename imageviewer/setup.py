5db03df28c87ae3c709e059991771ccdbb6ac3e7
pulokas <pulokas>
2010-03-05 19:27:34 +0000

    ImageViewer -> imageviewer

--- imageviewer/setup.py
+++ imageviewer/setup.py
@@ -9,7 +9,7 @@ class InstallData(install_data):
 		return install_data.run(self)
 
 setup(
-    name='ImageViewer',
+    name='imageviewer',
     version='0.1',
     description='An image viewer package for wxPython',
     author='Christian Suloway',
@@ -17,10 +17,10 @@ setup(
     maintainer='Christian Suloway',
     maintainer_email='suloway@caltech.edu',
     url='http://www.jensenlab.caltech.edu/',
-    packages=['ImageViewer', 'ImageViewer.icons'],
-    package_dir={'ImageViewer': ''},
+    packages=['imageviewer', 'imageviewer.icons'],
+    package_dir={'imageviewer': ''},
     cmdclass={'install_data': InstallData},
     data_files=[
-		('ImageViewer/icons', glob.glob('icons/*.png'))
+		('imageviewer/icons', glob.glob('icons/*.png'))
 	]
 )
