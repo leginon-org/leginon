44e13b8b906a0816f06ba6ba117ac83320df3ae1
Neil Voss <vossman>
2010-03-09 19:36:51 +0000

    starting new script to upload frealign, refs #192

---
+++ appion/bin/uploadFrealign.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+#python
+import os
+import re
+import sys
+import time
+import math
+#appion
+from appionlib import appionScript
+
+class UploadFrealign(AppionScript):
+	#=====================
+	def setupParserOptions(self):
+		self.parser.set_usage("Usage: %prog --commit --description='<text>' [options]")
+		self.parser.add_option("--stackid", dest="stackid", type="int",
+			help="ID for particle stack (optional)", metavar="INT")
+
+	#=====================
+	def checkConflicts(self):
+		apDisplay.printMsg("Conflicts")
+
+	#=====================
+	def setRunDir(self):
+		self.params['rundir'] = os.getcwd()
+
+	#=====================
+	def start(self):
+		"""
+		this is the main component of the script
+		where all the processing is done
+		"""
+		apDisplay.printMsg("Hey this works")
+
+if __name__ == '__main__':
+	upfrealign = UploadFrealign()
+	upfrealign.start()
+	upfrealign.close()
