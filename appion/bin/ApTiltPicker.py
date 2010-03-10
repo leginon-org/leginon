8c94253e7bf70429a57a4c25c0f9d5218546d60f
Neil Voss <vossman>
2010-03-10 22:28:56 +0000

    add logo for TiltPicker

--- appion/bin/ApTiltPicker.py
+++ appion/bin/ApTiltPicker.py
@@ -1610,7 +1610,8 @@ if __name__ == '__main__':
 	releasedate = "March 5, 2010"
 	logoimage = os.path.join(os.path.abspath(os.path.dirname(__file__)), "data/tplogo.png")
 	if not os.path.isfile(logoimage):
-		logoimage = os.path.join(apParam.getAppionDirectory(), "data/tplogo.png")
+		logoimage = os.path.join(apParam.getAppionDirectory(), "appionlib/data/tplogo.png")
+	print logoimage
 	citationlogo = """
 ####
   # ###    Voss NR, Yoshioka CK, Radermacher M, Potter CS, and Carragher B.
