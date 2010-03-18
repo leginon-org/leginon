f2dd71809bfbfe5d44661631fa40e4913133fadd
Neil Voss <vossman>
2010-03-18 20:24:59 +0000

    reduce memory footprint, highpass as an option, refs #218

--- appion/bin/powerSpectra.py
+++ appion/bin/powerSpectra.py
@@ -18,20 +18,24 @@ class powerSpectraLoop(appionLoop2.AppionLoop):
 	def processImage(self, imgdata):
 		### make the power spectra
 		powerspectra = imagefun.power(imgdata['image'], mask_radius=1.0, thresh=4)
-		powerspectra = imagefun.bin2(powerspectra, self.params['bin'])
-		powerspectra = apImage.fermiHighPassFilter(powerspectra, apix=4.0, radius=2000.0)
-		powerspectra = apImage.normStdevMed(powerspectra, size=5) 
-		powerspectra = apImage.pixelLimitFilter(powerspectra, pixlimit=4)
-		powerspectra = apImage.normRange(powerspectra)
+		binpowerspectra = imagefun.bin2(powerspectra, self.params['bin'])
+		del powerspectra
+		if self.params['hp'] is True:
+			binpowerspectra = apImage.fermiHighPassFilter(binpowerspectra, apix=4.0, radius=2000.0)
+		binpowerspectra = apImage.normStdevMed(binpowerspectra, size=5) 
+		binpowerspectra = apImage.pixelLimitFilter(binpowerspectra, pixlimit=4)
+		binpowerspectra = apImage.normRange(binpowerspectra)
 
 		### filter the image
 		imagedata = imagefun.bin2(imgdata['image'], self.params['bin'])
+		del imgdata['image']
 		imagedata = apImage.normStdevMed(imagedata, size=5) 
 		imagedata = apImage.pixelLimitFilter(imagedata, pixlimit=2)
 		imagedata = apImage.normRange(imagedata)
 
 		### write to file
-		stacked = numpy.hstack([imagedata, powerspectra])
+		stacked = numpy.hstack([imagedata, binpowerspectra])
+		del imagedata, binpowerspectra
 		stackedname = os.path.join(self.params['rundir'], imgdata['filename']+"power.jpg")
 		apImage.arrayToJpeg(stacked, stackedname)
 
@@ -39,6 +43,8 @@ class powerSpectraLoop(appionLoop2.AppionLoop):
 	def setupParserOptions(self):
 		self.parser.add_option("--bin", dest="bin", type="int", default=4,
 			help="Bin the images by X")
+		self.parser.add_option("--no-hp", dest="hp", default=True,
+			action="store_false", help="Do not use high pass filter")
 		return
 
 	#======================
