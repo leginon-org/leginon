5c7622fff7351657871299a8ade34b77c6e8b0b9
pulokas <pulokas>
2010-03-19 23:41:35 +0000

    put TEM Scripting first so if there is Tecnai Scripting it will overwrite

--- pyscope/updatecom.py
+++ pyscope/updatecom.py
@@ -3,8 +3,8 @@ from win32com.client import selecttlb
 from win32com.client import makepy
 
 info = [
-	('Tecnai Scripting', 'tecnaicom.py', 'Tecnai Scripting'),
 	('TEM Scripting', 'tecnaicom.py', 'Tecnai Scripting'),
+	('Tecnai Scripting', 'tecnaicom.py', 'Tecnai Scripting'),
         ('TOMMoniker 1.0 Type Library', 'tomcom.py', 'TOM Moniker'),
 	('Low Dose Server Library', 'ldcom.py', 'Tecnai Low Dose Kit'),
 	('adaExp Library', 'adacom.py', 'Tecnai Exposure Adaptor'),
