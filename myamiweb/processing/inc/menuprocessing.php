92a5848d0bb57e8d676993f6031c40ae43e659b2
Dmitry Lyumkis <dlyumkis>
2010-04-21 22:12:26 +0000

    added hide_imagic variable to angular reconstitution stuff

--- myamiweb/processing/inc/menuprocessing.php
+++ myamiweb/processing/inc/menuprocessing.php
@@ -458,18 +458,19 @@ if ($expId) {
 	}
 
 	/* IMAGIC Angular Reconstitution */
-	if ($aligndone >= 1 && $clusterdone >=1) {
-		$angrecondone = $particle->getAngularReconstitutionRuns($sessionId);
-		$angreconqueue = count($subclusterjobs['angrecon']['queued']);
-		$angreconrun = count($subclusterjobs['angrecon']['running']);
-		$angreconresults[] = ($angrecondone > 0) ? "<a href='angreconsummary.php?expId=$sessionId'>$angrecondone complete</a>" : '';
-		$angreconresults[] = ($angreconrun==0) ? "" : "<a href='listAppionJobs.php?expId=$sessionId&jobtype=angrecon'>$angreconrun running</a>";
-		$angreconresults[] = ($angreconqueue==0) ? "" : "$angreconqueue queued";
-		$nruns[] = array(
-			'name'=>"<a href='bootstrappedAngularReconstitution.php?expId=$sessionId'>IMAGIC Angular Reconstitution</a>",
-			'result'=>$angreconresults,
-		);
-
+	if (!HIDE_IMAGIC) {
+		if (($aligndone >= 1 && $clusterdone >=1) || ($tsdone >= 1)) {
+			$angrecondone = count($particle->getAngularReconstitutionRuns($sessionId));
+			$angreconqueue = count($subclusterjobs['angrecon']['queued']);
+			$angreconrun = count($subclusterjobs['angrecon']['running']);
+			$angreconresults[] = ($angrecondone > 0) ? "<a href='angreconsummary.php?expId=$sessionId'>$angrecondone complete</a>" : '';
+			$angreconresults[] = ($angreconrun==0) ? "" : "<a href='listAppionJobs.php?expId=$sessionId&jobtype=angrecon'>$angreconrun running</a>";
+			$angreconresults[] = ($angreconqueue==0) ? "" : "$angreconqueue queued";
+			$nruns[] = array(
+				'name'=>"<a href='bootstrappedAngularReconstitution.php?expId=$sessionId'>IMAGIC Angular Reconstitution</a>",
+				'result'=>$angreconresults,
+			);
+		}
 	}
 
 	if ( (array)$nruns ) {
