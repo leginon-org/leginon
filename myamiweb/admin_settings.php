b7a33e16092d79494959c70269c6cb75d4dfd640
Anchi Cheng <acheng>
2010-04-24 05:12:57 +0000

    creates inc/setdefault.php

--- myamiweb/admin_settings.php
+++ myamiweb/admin_settings.php
@@ -1,4 +1,3 @@
-<html><head><title>Administrator Settings</title>
 <?php
 
 /**
@@ -8,27 +7,80 @@
  *	see  http://ami.scripps.edu/software/leginon-license
  */
 
-include ("inc/jpgraph.php");
-include ("inc/jpgraph_line.php");
-include ("inc/jpgraph_scatter.php");
-include ("inc/jpgraph_bar.php");
-require ("inc/leginon.inc");
+require_once ("inc/leginon.inc");
 
-function displayResult($result,$title) {
+function displayResult($result,$table,$name) {
+	$dlinebreak = '<br/>';
+	$linebreak = "\n";
+	$filearray = array();
 	if ($result) {
-		?><p><? echo $title;
-		?></p> <p><? print_r($result);
-		?></p><p>-------------------------------------</p><?
-	} else {
-		?><p><? echo $title;
-		?></p> <p><? echo "empty";
-		?></p><p>-------------------------------------</p><?
-	} 
-	return;
+		$id = $result['DEF_id'];
+		$removekeys = array('DEF_id'=>1,'DEF_timestamp'=>2,'REF|SessionData|session'=>null);
+		$result = array_diff_key($result,$removekeys);
+		echo '$table = "'.$table.'";'.$dlinebreak.'';
+		echo '$data=array();'.$dlinebreak.''; 
+		$filearray[] = '$table = "'.$table.'";'.$linebreak.'';
+		$filearray[] = '$data=array();'.$linebreak.''; 
+		foreach ($result as $key=>$r) {
+			if (substr_count($r,'$') == 0) {
+				echo '$data["'.$key.'"] = "'.$r.'";'.$dlinebreak.''; 
+				$filearray[] = '$data["'.$key.'"] = "'.$r.'";'.$linebreak.''; 
+			} else {
+				echo '$data["'.$key.'"] = '.$r.';'.$dlinebreak.''; 
+				$filearray[] = '$data["'.$key.'"] = '.$r.';'.$linebreak.''; 
+			}
+		}
+		$t = strtolower(substr($table,0,1));
+		echo '$data["REF|SessionData|session"]= $sessionId;'.$dlinebreak.'';
+		echo '$'.$t.'id'.$id.'=$dbc->SQLInsert($table, $data);'.$dlinebreak.'';
+		$filearray[] = '$data["REF|SessionData|session"]= $sessionId;'.$linebreak.'';
+		$filearray[] = '$'.$t.'id'.$id.'=$dbc->SQLInsert($table, $data);'.$linebreak.'';
+		echo $dlinebreak;
+		$filearray[] = $linebreak;
+	}
+	return writeFile($filearray);
 }
 
-function displayFirstResult($results,$title) {
-		displayResult($results[0],$title);
+function writeFile($array) {
+	global $filename;
+	static $state = 'w';
+	$fd = fopen($filename, $state);
+	foreach($array as $content) {
+		fwrite($fd, $content);
+	}
+	fclose($fd);
+	$state = 'a';
+}
+
+function fileHeader() {
+	global $filename;
+	$array = array();
+	$linebreak = "\n";
+	$array[] = '<?php'.$linebreak;
+	$array[] = '$user_id = $leginondata->getAdminUserId();'.$linebreak;
+	$array[] = 'if ($user_id < 1) {'.$linebreak;
+	$array[] = '	echo "<h3> Error: Create administrator user first </h3>";'.$linebreak;
+	$array[] = '	exit();'.$linebreak;
+	$array[] = '}'.$linebreak;
+	$array[] = '$dbc=$leginondata->mysql;'.$linebreak;
+	$array[] = '$q = "insert into `SessionData` (`name`,`REF|UserData|user` ) "'.$linebreak;
+	$array[] = '      . " VALUES "'.$linebreak;
+	$array[] = '      . " ( concat(\'importsettings\', DATE_FORMAT(now(), \'%Y%m%d%H%i%s\')), "'.$linebreak;
+	$array[] = '			. " ".$user_id." ) ";'.$linebreak;
+	$array[] = '$sessionId = $dbc->SQLQuery($q, true);'.$linebreak;
+	$array[] = $linebreak;
+	writeFile($array);
+}
+
+function fileFooter() {
+	$linebreak = "\n";
+	$array = array();
+	$array[] = '?>'.$linebreak;
+	writeFile($array);
+}
+
+function displayFirstResult($results,$table,$name) {
+	displayResult($results[0],$table,$name);
 }
 		
 function getSettingsById($table,$id) {
@@ -36,7 +88,7 @@ function getSettingsById($table,$id) {
 	 $sql = 'SELECT * from `'.$table.'` '
 			. 'where `DEF_id`='.$id.' ';
 	$results = $leginondata->mysql->getSQLResult($sql);
-	displayFirstResult($results,$table.': '.$id);
+	displayFirstResult($results,$table,$id);
 	return $results[0];
 }
 
@@ -81,13 +133,6 @@ function getSettingsData($user_id,$table,$name) {
 	return $results[0];
 }
 
-function getAdminUserId(){
-  global $leginondata;
-	$sql = 'SELECT `DEF_id` from `UserData` where `name` like "Administrator"';
-	$results = $leginondata->mysql->getSQLResult($sql);
-	return $results[0]['DEF_id'];
-}
-
 $nodenames = array(
 	'AcquisitionSettingsData'=>array('Grid','Square','Hole','Preview','Exposure','Square Q','Hole Q','Tomography Preview','Final Section','Subsquare','Centered Square','Rough Tissue','Final Raster','Grid Survey','Mid Mag Survey','Reacquisition','High Mag Acquisition'),
 	'FocuserSettingsData'=>array('Focus','Z Focus','Tomo Focus','Tomo Z Focus','RCT Focus','Section Z Focus','Grid Focus','Section Focus','Screen Z Focus'),
@@ -105,51 +150,64 @@ $nodenames = array(
 	'CorrectorSettingsData'=>array('Correction'),
 	'BeamFixerSettingsData'=>array('Fix Beam'),
 );
-
+$show_tables = false;
+$filename = 'test.php';
 $lpfids = array();
 $bfids = array();
+fileHeader();
 $aliases = array('edge lpf','template lpf','lpf');
-
-$user_id = getAdminUserId();
+$user_id = $leginondata->getAdminUserId();
 $extratables = array('LowPassFilterSettingsData','BlobFinderSettingsData','FocusSequenceData');
 //Tables
-?><p><?;
-foreach ($extratables as $table) {
-	echo $table?></br><?;
+if ($show_tables) {
+	foreach ($extratables as $table) {
+		echo $table?></br><?;
+	}
+	foreach (array_keys($nodenames) as $table) {
+		echo $table?></br><?;
+	}
 }
-foreach (array_keys($nodenames) as $table) {
-	echo $table?></br><?;
-}
-?><p><?;
-
 //Default values
+$lpfids = array();
+$blobids = array();
 foreach (array_keys($nodenames) as $table) {
 	foreach ($nodenames[$table] as $name) { 
 		$sqldata1 = getSettingsData($user_id,$table,$name);
 		if ($table == 'MosaicClickTargetFinderSettingsData' or 'HoleFinderSettingsData' or 'JAHCFinderSettingsData' or 'DTFinderSettingsData') {
 			foreach ($aliases as $alias) {
-				$id = $sqldata1['REF|LowPassFilterSettingsData|'.$alias]; 
-				if ($id != 0 and !in_array($id,$lpfids)) {
-					array_push($lpfids,$sqldata1['REF|LowPassFilterSettingsData|'.$alias]);
-					getSettingsById('LowPassFilterSettingsData',$id);
+				$lid = $sqldata1['REF|LowPassFilterSettingsData|'.$alias]; 
+				if ($lid != 0 ) {
+					if (!in_array($lid,$lpfids)) {
+						array_push($lpfids,$sqldata1['REF|LowPassFilterSettingsData|'.$alias]);
+						$lpfids[$table.'.'.$name.'.'.$alias] = getSettingsById('LowPassFilterSettingsData',$lid);
+					}
+					$sqldata1['REF|LowPassFilterSettingsData|'.$alias] = '$lid'.$lid; 
+				} else {
+					$sqldata1 = array_diff_key($sqldata1,array('REF|LowPassFilterSettingsData|'.$alias=>$lid)); 
 				}
 			}
 		}
+
 		if ($table == 'MosaicClickTargetFinderSettingsData') {
 			$id = $sqldata1['REF|BlobFinderSettingsData|blobs']; 
-			if ($id != 0 and !in_array($id,$bfids)) {
-				array_push($bfids,$sqldata1['REF|BlobFinderSettingsData|blobs']);
-				getSettingsById('BlobFinderSettingsData',$id);
-			}
+			if ($id != 0) {
+				if (!in_array($id,$bfids)) {
+					array_push($bfids,$sqldata1['REF|BlobFinderSettingsData|blobs']);
+					getSettingsById('BlobFinderSettingsData',$id);
+					$blobids[$table.'.'.$name.'.'.'blobs'] = $id;
+				}
+				$sqldata1['REF|BlobFinderSettingsData|blobs'] = '$bid'.$id;
+			} else {
+					$sqldata1 = array_diff_key($sqldata1,array('REF|BlobFinderSettingsData|blobs'=>$id));
+			} 
 		}
-	displayResult($sqldata1,$table.": ".$name);
+	displayResult($sqldata1,$table,$name);
 	}
 }
-
 $focusernodenames = $nodenames['FocuserSettingsData'];
 $table = 'FocusSequenceData';
 foreach ($focusernodenames as $name) { 
 	$sqldata1 = getFocusSequenceData($user_id,$table,$name);
 }
+fileFooter();
 ?>
-</body></html>
