a62ed4b8c5ab0e760b1df8bf46f386454cdf3a55
Anchi Cheng <acheng>
2010-04-24 03:45:07 +0000

    Rick's first wish list item, reset x,y to zero when done refs Feature #376

--- leginon/targethandler.py
+++ leginon/targethandler.py
@@ -180,10 +180,10 @@ class TargetHandler(object):
 				self.publish(donetargetlist, database=True)
 			self.player.play()
 			if self.settings['reset tilt']:
-				zerostage = {'a':0.0}
+				zerostage = {'a':0.0,'x':0.0,'y':0.0}
 				self.instrument.tem.setStagePosition(zerostage)
 				stageposition = self.instrument.tem.getStagePosition()
-				self.logger.info('return alhpa tilt to %.1f' % stageposition['a'])
+				self.logger.info('return x,y, and alhpa tilt to %.1f um,%.1f um,%.1f deg' % (stageposition['x']*1e6,stageposition['y'],stageposition['a']))
 
 	def queueStatus(self, queuedata):
 		active = self.getListsInQueue(queuedata)
