7e7576de9b5c0fb72f1c99e62019cf150502abba
Neil Voss <vossman>
2010-05-12 21:29:27 +0000

    include subdir and data files in setup.py, refs #473

--- appion/setup.py
+++ appion/setup.py
@@ -1,12 +1,26 @@
 # For more info on expanding this script, see:
 #     http://docs.python.org/distutils/setupscript.html
 
-import distutils.core
+import os
 import glob
+import distutils.core
+
+#--install-scripts=/usr/local/bin
+
+version = None
+verfile = 'appionlib/version.txt'
+if os.path.isfile(verfile):
+	f = open(verfile, "r")
+	line = f.readline()
+	f.close()
+	version = line.strip()
 
 distutils.core.setup(
 	name='Appion',
-	packages=['appionlib'],
-	scripts=glob.glob('bin/*.py'), 
+	version=version,
+	author_email='appion@scripps.edu',
+	packages=['appionlib', 'appionlib.apSpider', 'appionlib.apTilt', 'appionlib.apImage'],
+	package_data={'appionlib': ['data/*.*', 'version.txt']},
+	scripts=glob.glob('bin/*.py'),
 )
 
