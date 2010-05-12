da55e92750802d491abf4c6cfa04491f244ece44
Anchi Cheng <acheng>
2010-05-12 06:43:09 +0000

    for feature #465 and upgrading

---
+++ myamiweb/setup/updateDataReport.php
@@ -0,0 +1,54 @@
+<?php
+
+require_once('template.inc');
+require_once('setupUtils.inc');
+require_once("../inc/mysql.inc");
+
+	$template = new template;
+	$template->wizardHeader("Step 4 : Data Insertion for Update Report", DB_INITIALIZATION);
+	
+	if(file_exists(CONFIG_FILE)){
+
+		require_once(CONFIG_FILE);
+		require_once("../inc/leginon.inc");
+		require_once("../project/inc/project.inc.php");
+
+		$project = new project();
+		$project->install('../xml/projectUpdateValues.xml');	
+/*
+		$leginondata->updateAdmin('../xml/leginonddDefaultValues.xml');		
+		
+		$data = array('username' => $_POST['username'], 
+					  'password' => md5($_POST['password']), 
+					  'firstname' => 'Admin',
+					  'lastname' => 'Admin',
+					  'email' => $_POST['email'], 
+					  'REF|GroupData|group' => 1);
+		
+		$mysqld = new mysql(DB_HOST, DB_USER, DB_PASS);
+		$dbLink = $mysqld->connect_db();
+		
+		$mysqld->select_db(DB_LEGINON, $dbLink);	
+		
+		$mysqld->SQLInsert('UserData', $data);
+*/
+	}
+	else{
+		$has_error[] = "Config file does not exist. Please create it first.";
+	}
+
+?>
+		
+		<h3>Data Insertion Sucess:</h3>
+		<p>Web tools wizard has successfully insert all the required tables and values to your databases.<br />
+		   You can now start using web tools.<br /><br />
+		   If you have enabled the login system, use your administrator's password to login,<br />
+           Otherwise to start using the web tools, please click 
+           <a href="http://<?php echo $_SERVER['SERVER_NAME'].BASE_URL; ?>">here</a>.<br /><br />
+           To go back to the wizard, please click <a href="index.php">here</a><br /><br />
+			Thanks you for using this wizard.</p>	
+
+<?php 
+		
+	$template->wizardFooter();
+?>
