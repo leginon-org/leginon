a75aa7c373af9b513dc9e7e4bb114f81ca9fd294
erichou <erichou>
2010-05-14 17:04:53 +0000

    print out the config file if web user does not have privillege to edit the web directory

--- myamiweb/setup/review.php
+++ myamiweb/setup/review.php
@@ -11,22 +11,33 @@ require_once('setupUtils.inc');
 		header("Location: index.php");
 		exit();
 	}
-	
+
 	$template = new template;
 	$template->wizardHeader("Step 6 : Review your setup", SETUP_CONFIG);
-	
+
 	$setupUtils = new SetupUtils();
-	$setupUtils->copyFiles(CONFIG_TEMPLATE, CONFIG_FILE) or die("Can't copy file");		
-	$result = $setupUtils->editConfigFile(CONFIG_FILE, $_POST);
-	
-	if($result){
-		$fileContents = $setupUtils->fileToArray(CONFIG_FILE);
+	$fileContents = $setupUtils->editConfigFile(CONFIG_TEMPLATE, $_POST);
+
+	$copyResult = $setupUtils->copyFiles(CONFIG_TEMPLATE, CONFIG_FILE);
+
+	if($copyResult){
+			$result = $setupUtils->arrayToFile(CONFIG_FILE, $fileContents);
+
+		if($result)
+			$fileContents = $setupUtils->fileToArray(CONFIG_FILE);
 	}
+	else{
+
+		$cpErrorMsg = "Apache User does not have permission to create configure file.<br />
+				You can change your \"myamiweb\" folder's permission or <br />
+				you can use the code below and create a config.php under the myamiweb folder.";
+
+        }
 
 ?>
 
 	<form name='wizard_form' method='POST' action='initDBTables.php'>
-		
+
 		<h3>Initializing database table</h3>
 		<p>Web tools require default tables to be created in both databases.<br />
 		   If this is your first time setting up these web tools, please click the following button:</p>
@@ -34,18 +45,19 @@ require_once('setupUtils.inc');
 		<br /><br />
 		<h3>This is what your config file looks like based on the settings you provided: </h3>
 		<p>You can change the settings later by returning to this <a href="index.php">wizard</a></p>
-		
+		<?php if(!empty($cpErrorMsg)) echo "<p><font color='red'>".$cpErrorMsg."</font></p>"; ?>
 		<p>
-	<?php 
+	<?php
 		foreach ($fileContents as $eachLineOfFile){
-			echo $eachLineOfFile . "<br />";
+                    if(trim($eachLineOfFile) == "<?php") echo "&lt;?php <br />";
+                    else echo $eachLineOfFile . "<br />";
 		}
-	
+
 	?>
 		</p>
 	</form>
-	
-<?php 
-		
+
+<?php
+
 	$template->wizardFooter();
 ?>
