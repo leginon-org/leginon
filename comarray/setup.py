9aeca2beae0ec6f8cf64af50f9325c19172b5516
pulokas <pulokas>
2010-05-28 23:04:18 +0000

    setup.py that will actually compile it

--- comarray/setup.py
+++ comarray/setup.py
@@ -1,11 +1,25 @@
-from distutils.core import setup
+from distutils.core import setup, Extension
+from distutils.sysconfig import get_python_lib
+import sys
+
+pl = get_python_lib(True)
+
+numpysafearray = Extension(
+	'NumpySafeArray',
+	include_dirs=['%s/win32/include' % pl, '%s/win32com/include' % pl, '%s/numpy/core/include' % pl],
+	library_dirs=['%s/win32/libs' % pl, '%s/win32com/libs' % pl],
+	sources=['NumpySafeArray.cpp']
+)
+ext_modules = [numpysafearray]
+ext_package = 'NumpySafeArray'
 
 setup(
-	name='comarray',
+	name='NumpySafeArray',
 	version='1.0.0',
 	description='get numpy array from COM object',
-	packages=['comarray'],
-	package_dir={'comarray': 'py'},
-	package_data={'comarray': ['NumpySafeArray.pyd']}
+	packages=['NumpySafeArray'],
+	package_dir={'NumpySafeArray': ''},
+	ext_package=ext_package,
+	ext_modules=ext_modules,
 )
 
