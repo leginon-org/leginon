6d3e72df7006a683f5ac59bfb7bf5622ff3f6683
Amber Herold <amber>
2010-06-24 23:44:47 +0000

    Merge from Trunk to Branch r14412 and r14414, Remove $ from HIDE_IMAGIC and make eman default. refs #693 and refs #694

--- myamiweb/processing/selectParticleAlignment.php
+++ myamiweb/processing/selectParticleAlignment.php
@@ -74,7 +74,7 @@ echo " <p> first you select template and then this method uses the "
 //echo "  <img src='img/align-rsm.png' width='125'><br/>\n";
 echo "</td></tr>\n";
 
-if (!$HIDE_IMAGIC && !HIDE_FEATURE) {
+if (!HIDE_IMAGIC && !HIDE_FEATURE) {
 	/*
 	** IMAGIC Reference Based Alignment
 	*/
