3c805281aed2e0986182cf57ef1d217532a3085f
pulokas <pulokas>
2010-07-29 18:02:38 +0000

    more stashing reorganization

--- pyami/fft/calc_base.py
+++ pyami/fft/calc_base.py
@@ -11,7 +11,7 @@ import pyami.weakattr
 
 class Calculator(object):
 	def __init__(self):
-		self.stashing_on = False
+		self.stashing_on = True
 
 	def enable_stashing(self):
 		self.stashing_on = True
@@ -20,17 +20,37 @@ class Calculator(object):
 		# should we delete all stashed ffts?
 		self.stashing_on = False
 
-	def forward_if_necessary(self, image_array):
-		## check if fft already associated with this image array
+	def stash(self, image_array, fft_array):
+		## prevent modification to both image and fft to ensure
+		## that stashed fft corresponds to image forever
+		image_array.setflags(write=False)
+		fft_array.setflags(write=False)
+		pyami.weakattr.set(image_array, 'fft', fft_array)
+
+	def getStashedFFT(self, image_array):
 		try:
 			f = pyami.weakattr.get(image_array, 'fft')
-		except:
+		except AttributeError:
 			f = None
+		return f
+
+	def unstash(self, image_array):
+		fft_array = self.getStashedFFT(image_array)
+		if fft_array is None:
+			# already unstashed
+			return
+		## no longer associated with each other, so make them writable
+		image_array.setflags(write=True)
+		fft_array.setflags(write=True)
+		pyami.weakattr.set(image_array, 'fft', None)
+
+	def forward_if_necessary(self, image_array):
+		## check if fft already associated with this image array
+		f = self.getStashedFFT(image_array)
 		if f is None:
 			f = self._forward(image_array)
 			if self.stashing_on:
-				image_array.setflags(write=False)  # prevent modification
-				pyami.weakattr.set(image_array, 'fft', f)
+				self.stash(image_array, f)
 		else:
 			return f
 
