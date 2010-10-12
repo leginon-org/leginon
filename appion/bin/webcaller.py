85be1364e9ecbb0043d2cb09b49e25fe18b49a56
Gabriel Lander <glander>
2010-10-12 22:22:03 +0000

    use "mkdir -p" from a subprocess instead of "os.mkdir", since this will create necessary subdirectories

--- appion/bin/webcaller.py
+++ appion/bin/webcaller.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
 	time.sleep(0.5)
 	dirname = os.path.dirname(outf)
 	if not os.path.isdir(dirname):
-		os.mkdir(dirname)
+		subprocess.Popen("mkdir -p %s"%dirname,shell=True).wait()
 
 	## run command and write to log file
 	f = open(outf, "w")
