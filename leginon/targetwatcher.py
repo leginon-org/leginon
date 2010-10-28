7adb5b550b9689dab6b5bc6e3e01321e1ee894e6
pulokas <pulokas>
2010-10-28 20:51:57 +0000

    removed "z" from park method

--- leginon/targetwatcher.py
+++ leginon/targetwatcher.py
@@ -292,7 +292,7 @@ class TargetWatcher(watcher.Watcher, targethandler.TargetHandler):
 	def park(self):
 		self.logger.info('parking...')
 		self.instrument.tem.ColumnValvePosition = 'closed'
-		self.instrument.tem.StagePosition = {'x': 0, 'y': 0, 'z': 0, 'a': 0}
+		self.instrument.tem.StagePosition = {'x': 0, 'y': 0, 'a': 0}
 		self.logger.warning('column valves closed and stage reset')
 
 	def waitForRejects(self):
