c69c32d708ef316885994e2f5337c812545ea7d0
Anchi Cheng <acheng>
2010-10-29 21:52:03 +0000

    merge r14967 start.py change to branch 2.1 to check requirement refs #980

--- leginon/start.py
+++ leginon/start.py
@@ -12,9 +12,21 @@ import pyami.quietscipy
 import sinedon.data as data
 import leginon.gui.wx.Manager
 import leginon.version
+import leginon.project
 print 'Leginon version:  ', leginon.version.getVersion()
 
+def checkRequirements():
+
+	try:
+		projectdata = leginon.project.ProjectData()
+		projects = projectdata.getProjects()
+	except:
+		projects = None
+	if not projects:
+		raise RuntimeError('Must create at least one project before starting Leginon')
+
 def start(options=None):
+	checkRequirements()
 	m = leginon.gui.wx.Manager.App(None, options=options)
 	if not m.abort:
 		m.MainLoop()
