dd8e490c6116894422bdbb0c24a5455eeadca313
Anchi Cheng <acheng>
2010-12-07 20:19:23 +0000

    same change as in branch 2.1 r15120 refs #1067

--- myamiweb/imgreport.php
+++ myamiweb/imgreport.php
@@ -177,6 +177,7 @@ echo formatHtmlRow('Acquired', $imageinfo[timestamp]);
 echo formatHtmlRow('Path', $path);
 echo formatHtmlRow('Session', "$sessioninfo[Name] - $sessioninfo[Purpose]");
 echo formatHtmlRow('Instrument', $imageinfo['scope'].' - '.$imageinfo['camera']);
+echo formatHtmlRow('Scope Host', $sessioninfo['Scope Host']);
 echo "</table>";
 ?>
 	</td>
