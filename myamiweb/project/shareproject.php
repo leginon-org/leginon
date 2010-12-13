c08305e14e68215b0a86c8b30ee11a1442e7fee2
Anchi Cheng <acheng>
2010-12-13 20:39:03 +0000

    merge r15124 from trunk into branch 2.0 to retire incompatible updateuser.php refs #1070 --1his line, and those below, will be ignored--
    M    myamiweb/project/shareproject.php
    M    myamiweb/project/share.php
    M    myamiweb/project/user.php
    D    myamiweb/project/updateuser.php

--- myamiweb/project/shareproject.php
+++ myamiweb/project/shareproject.php
@@ -47,7 +47,7 @@ if ($is_admin) {
 ?>
 <p>
 <img src="img/info.png"> Users with no password set won't be listed; go
-to <a class="header" href="user.php">[user]</a> to update user's profile.
+to <a class="header" href="<? echo BASE_URL.'user.php'; ?>">[user]</a> to update user's profile.
 </p>
 <?
 	};
