51c1b782f2725a310cc019d181573afebf4cb98a
Anchi Cheng <acheng>
2011-03-02 20:38:56 +0000

    organize by package refs #1211

--- appion/test/check3rdPartyPackages.py
+++ appion/test/check3rdPartyPackages.py
@@ -3,41 +3,58 @@
 from appionlib import apParam
 from appionlib import apDisplay
 
-
+package = "Unknown"
 packageDict = {
-               "xmipp_normalize" : "",
-               "embfactor64.exe" : "",
-               "proc2d" : "",
-               "mpirun" : "For Alignments and Refinements.",
-               "xmipp_mpi_ml_align2d" : "",
-               "xmipp_ml_align2d" : "",
-               "xmipp_mpi_angular_project_library" : "",
-               "xmipp_mpi_angular_projection_matching" : "",
-               "xmipp_mpi_angular_class_average" : "",
-               "xmipp_mpi_reconstruct_wbp" : "",
-               "xmipp_mpi_ml_refine3d" : "",
-               "can64.exe" : "",
-               "ctffind64.exe" : "",
-               "ctftilt64.exe" : "",
-               "spider" : "",
-               "ace2correct.exe" : "",
-               "ace2.exe" : "",
-               "xmipp_protocols" : "",
-               "apChimSnapshot.py" : "Not required.",
-               "tomo-refine.sh" : "",
-               "tomo-fit.sh" : "",
-               "acetilt.py" : "",
-               "findem64.exe" : "Also check for which findem32.exe.",
-               "rmeasure64.exe" : "Also check for which rmeasure32.exe, rmeasure.exe, or rmeasure.",
-               "signature64.exe" : "For Signature Particle Picking. Also check which signature32.exe.",
-               }
+               "embfactor64.exe" : (package,""),
+               "mpirun" : (package,"For Alignments and Refinements."),
+	}
+package = "Appion"
+packageDict.update({
+               "can64.exe" : (package,""),
+               "ace2correct.exe" : (package,"For CTF correction"),
+               "ace2.exe" : (package,"For CTF estimation"),
+               "acetilt.py" : (package,""),
+               "findem64.exe" : (package,"Also check for which findem32.exe."),
+	})
+package = "EMAN"
+packageDict.update({
+							 "proc2d" : (package,"For Image Manipulation"),
+	})
+package = "SPIDER"
+packageDict.update({
+							 "spider" : (package,"For Alignments and Refinements."),
+	})
+package = "XMIPP"
+packageDict.update({
+               "xmipp_normalize" : (package,""),
+               "xmipp_mpi_ml_align2d" : (package,""),
+               "xmipp_ml_align2d" : (package,""),
+               "xmipp_mpi_angular_project_library" : (package,""),
+               "xmipp_mpi_angular_projection_matching" : (package,""),
+               "xmipp_mpi_angular_class_average" : (package,""),
+               "xmipp_mpi_reconstruct_wbp" : (package,""),
+               "xmipp_mpi_ml_refine3d" : (package,""),
+               "xmipp_protocols" : (package,""),
+	})
+package = "Gregoriff Lab"
+packageDict.update({
+               "ctffind64.exe" : (package,""),
+               "ctftilt64.exe" : (package,""),
+               "rmeasure64.exe" : (package,"Also check for which rmeasure32.exe, rmeasure.exe, or rmeasure."),
+               "signature64.exe" : (package,"For Signature Particle Picking. Also check which signature32.exe."),
+	})
+package = "PROTOMO"
+packageDict.update({
+               "tomo-refine.sh" : (package,"For Tomography alignment"),
+               "tomo-fit.sh" : (package,"For Tomography alignment"),
+	})
 
-outString = "The following third party processing packages could not be found...\n"
+outString = "The following third party processing package could not be found...\n"
 
 for nameKey, desc in packageDict.iteritems():
     pathValue = apParam.getExecPath(nameKey, die=False)
     if pathValue is None:
-        outString += "%s - %s\n"%(nameKey, desc)
+        outString += "%s - From %s - %s\n"%(nameKey, desc[0], desc[1])
 
 apDisplay.printColor(outString,"cyan")
 
