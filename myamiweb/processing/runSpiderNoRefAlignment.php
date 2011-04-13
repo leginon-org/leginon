797be50f606e221047eab471f4e97980e518fc7c
erichou <erichou>
2011-04-13 17:51:25 +0000

    change require to require_once

--- myamiweb/processing/runSpiderNoRefAlignment.php
+++ myamiweb/processing/runSpiderNoRefAlignment.php
@@ -8,11 +8,11 @@
  *      Simple viewer to view a image using mrcmodule
  */
 
-require "inc/particledata.inc";
-require "inc/leginon.inc";
-require "inc/project.inc";
-require "inc/viewer.inc";
-require "inc/processing.inc";
+require_once "inc/particledata.inc";
+require_once "inc/leginon.inc";
+require_once "inc/project.inc";
+require_once "inc/viewer.inc";
+require_once "inc/processing.inc";
 
 // IF VALUES SUBMITTED, EVALUATE DATA
 if ($_POST) {
