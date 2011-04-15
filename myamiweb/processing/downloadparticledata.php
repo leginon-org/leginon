4cd1c86827293a29b4d61c15f8077c1bb480aaaa
erichou <erichou>
2011-04-15 22:05:07 +0000

    added defocus pair information to the download file when the image type is ef. refs #1264

--- myamiweb/processing/downloadparticledata.php
+++ myamiweb/processing/downloadparticledata.php
@@ -8,30 +8,50 @@ require "inc/processing.inc";
 
 //This file dumps the best CTF parameters for all images in the session
 
-$expId = $_GET['expId'];
-checkExptAccessPrivilege($expId,'data');
-$selectionId = $_GET['selectionId'];
+$sessionId = $_GET['expId'];
+checkExptAccessPrivilege($sessionId,'data');
+$particleSelectionId = $_GET['pSelectionId'];
+$preset = $_GET['preset'];
 $appiondb = new particledata();
 
-if (!$appiondb->hasParticleData($expId)) {
+if (!$appiondb->hasParticleData($sessionId)) {
 	echo "No particle information available<br/>\n";
 	exit;
 }
 
-$partdatas = $appiondb->getParticles($selectionId);
+if ($preset == 'ef'){
+	$data[] = "particle#\tx_coord\ty_coord\timage#\tpImage#\tshiftX\tshiftY\n";
+	$partdatas = $appiondb->getParticlesDataWithDeforcusPair($particleSelectionId);
+} 
+else{ 
+	$data[] = "particle#\tx_coord\ty_coord\timage#\n";
+	$partdatas = $appiondb->getParticles($particleSelectionId);
+}
 
-$data[] = "particle #\tx_coord\ty_coord\timage_#\n";
 //echo "</br>\n";
 
 foreach ($partdatas as $partdata) {
-	//$filename = $appiondb->getImageNameFromId($partdata['REF|leginondata|AcquisitionImageData|image']);
-	$data[] = sprintf("%d\t%d\t%d\t%d\n",
-		$partdata['DEF_id'],
-		$partdata['xcoord'],
-		$partdata['ycoord'],
-		$partdata['REF|leginondata|AcquisitionImageData|image']);
-}
 
+	switch($preset){
+		case 'ef':
+			$data[] = sprintf("%d\t%d\t%d\t%d\t%d\t%d\t%d\n",
+				$partdata['DEF_id'],
+				$partdata['xcoord'],
+				$partdata['ycoord'],
+				$partdata['imageNum'],
+				$partdata['pImage'],
+				$partdata['shiftx'],
+				$partdata['shifty']);			
+			break;
+		default:		
+			$data[] = sprintf("%d\t%d\t%d\t%d\n",
+				$partdata['DEF_id'],
+				$partdata['xcoord'],
+				$partdata['ycoord'],
+				$partdata['REF|leginondata|AcquisitionImageData|image']);			
+			break;
+	}
+}
 $size = 0;
 foreach ($data as $line) {
 	$size += strlen($line);
@@ -43,7 +63,7 @@ header("Content-Type: application/force-download");
 header("Content-Type: application/download");
 header("Content-Transfer-Encoding: binary");
 header("Content-Length: $size");
-$downname = sprintf("particledata-%04d_%04d.dat", $expId, $selectionId);
+$downname = sprintf("particledata-%04d_%04d.dat", $sessionId, $particleSelectionId);
 header("Content-Disposition: attachment; filename=$downname;");
 foreach ($data as $line) {
 	echo $line;
