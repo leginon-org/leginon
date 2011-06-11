47a34c45dd39134045966da5c5ff71fccfbd1b70
Dmitry Lyumkis <dlyumkis>
2011-06-11 22:07:06 +0000

    multi-model refinement documentation, refs #1324

--- myamiweb/processing/selectMultiModelRefine.php
+++ myamiweb/processing/selectMultiModelRefine.php
@@ -16,21 +16,22 @@ require "inc/processing.inc";
 $expId = $_GET['expId'];
 $formAction=$_SERVER['PHP_SELF']."?expId=$expId";
 
-processing_header("Multi-Model Reconstruction Refinement Software Selection","Multi-Model Refinement Software Selection Page", $javascript,False);
+processing_header("Multi-Model Classification and Refinement Software Selection","Multi-Model Classification and Refinement Software Selection Page", $javascript,False);
 
 // Selection Header
 echo "<table border='0' width='640'>\n";
 echo "<tr><td>\n";
-echo "  <h2>Multi-Model Reconstruction Refinement Procedures</h2>\n";
+echo "  <h2>Multi-Model Classification and Refinement Procedures</h2>\n";
 echo "  <h4>\n";
-echo "    Most initial models establish a preliminary sense of the overall shape of " 
-	."the biological specimen. To reveal structural information that can answer specific "
-	."biological questions, the model requires refining. In single particle analysis, a refinement "
-	."is an iterative procedure, which sequentially aligns the raw particles, assign to them appropriate "
-	."spatial orientations (Euler angles) by comparing them against the a model, and then back-projects "
-	."them into 3D space to form a new model. Effectively, a full refinement takes as input a raw particle "
-	."stack and an initial model and is usually carried out until no further improvement of the structure can "
-	."be observed.\n";
+echo "    Many molecular complexes appear to exhibit at least a small degree of intrinsic conformational variability that becomes "
+	."important for performing their biological function. Alternatively, complexes may exist in variable compositional states, "
+	."whereby a cofactor may be binding with substoichiometric affinity. This sort of conformational and / or compositional "
+	."heterogeneity is often found within a single data set, and can be dealt with computationally. The algorithm(s) below "
+	."address this issue by classifying raw particles into a predefined number of classes, using both supervised and unsupervised "
+	."approaches. After classification, the particles are refined within their particular class and a model is reconstructed in a similar "
+	."manner to single-model refinement protocols. Effectively, rather than limiting the final resolution of a reconstruction, "
+	."conformational and / or compositional heterogeneity becomes a factor that can be exploited to uncover novel " 
+	."states of the macromolecular complex.\n";
 echo "  </h4>\n";
 echo "</td></tr>\n";
 echo "</table>\n";
@@ -41,22 +42,23 @@ echo "<table border='1' class='tableborder' width='640'>\n";
 
 
 /*
-** Xmipp Maximum Likelihood Reference Free Alignment
+** Xmipp 3D Maximum Likelihood Classification 
 */
 
 echo "<tr><td width='100' align='center'>\n";
 echo "  <img src='img/xmipp_logo.png' width='64'>\n";
 echo "</td><td>\n";
-echo "  <h3><a href='selectStackForm.php?expId=$expId&method=xmippml3d&type=multi'>Xmipp 3DML Refinement</a></h3>\n";
-echo " <p> this method is the most robust, but takes some time to complete."
-	." It uses the "
-	."<a href='http://xmipp.cnb.csic.es/twiki/bin/view/Xmipp/MLalign2D'>Xmipp ml_align2d</a>"
-	."&nbsp;<img src='img/external.png'>"
-	." program to perform alignments. "
-	."</p><p>\n"
-	."This method is unbiased and very thorough, but also the slowest of the methods (~days). "
-	."Maximum likelihood also does a course search, integer pixels shifts and ~5 degree angle increments, "
-	."so it is best to get templates with this method and use ref-based alignment to get better alignment parameters" 
+echo "  <h3><a href='selectStackForm.php?expId=$expId&method=xmippml3d&type=multi'>Xmipp Maximum-Likelihood 3D Classification</a></h3>\n";
+echo " <p>. This is the <a href='http://xmipp.cnb.csic.es/twiki/bin/view/Xmipp/MLrefine3D'>Xmipp ML3D protocol</a>&nbsp;<img src='img/external.png'>&nbsp"
+	."The classification technique is the key operation behind the all of the procedures in the "
+	."<a href='http://xmipp.cnb.csic.es/twiki/bin/view/Xmipp/MLrefine3D'>full maximum-likelihood refinement procedure</a>&nbsp;<img src='img/external.png'>&nbsp;"
+	."Through optimization of the log-likelihood function, it aims to find the most likely set of parameterers to construct a model describing the "
+	."structural heterogeneity present in the data. One of the biggest advantages of this approach is that distinct classes are identified in an usupervised manner. "
+	."The input to this procedure can either be a single, highly low-pass filtered reference, or several references. In the former case, the user is given the "
+	."option of specifying the number of classes to uncover in an unsupervised manner. In the latter case, the number of resulting classes is identical "
+	."to the number of input references, and the procedure becomes more supervised in nature. The downside to the maximum-likelihood approach is the large "
+	."computational burden. For example, 25 iterations of a 100,000-particle stack with a boxsize of 80 pixels at an angular sampling increment of 10 "
+	."degrees takes ~ 6 cpu months to complete. Therefore choose your cluster parameters wisely." 
 	."</p>\n";
 //echo "  <img src='img/align-smr.png' width='250'><br/>\n";
 echo "</td></tr>\n";
