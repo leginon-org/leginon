9b327872fa24893cf0266624ccb481e46402e6b6
pulokas <pulokas>
2011-07-11 18:33:52 +0000

    changes

--- pyscope/stagespeedtest.py
+++ pyscope/stagespeedtest.py
@@ -3,23 +3,42 @@
 #scope = 'SimTEM'
 scope = 'Tecnai'
 
-n = 3
-
-holders = ['single tilt', 'cryo']
+n = 1
 
 mags = [
-	50,
-	5000,
-	50000,
+	190,
+	6500,
+	52000,
+]
+
+positions1 = [
+	(0, 0),
+	(0, 0),
+	(5e-6, 0),
+	(0, 0),
+	(100e-6, 0),
+	(0, 0),
+	(0, 5e-6),
+	(0, 0),
+	(0, 100e-6),
+	(0, 0),
+	(5e-6, 5e-6),
+	(0, 0),
+	(100e-6, 100e-6),
+	(0, 0),
 ]
 
 positions = [
 	(0, 0),
 	(0, 0),
-	(0, 10e-6),
+	(5e-6, 0),
+	(0, 0),
+	(100e-6, 0),
+	(0, 0),
+	(100e-6, 100e-6),
+	(0, 0),
 ]
 
-import itertools
 import time
 #import pyscope.tecnai
 import pyscope.registry
@@ -29,6 +48,7 @@ log_file = 'speedtest%s.log' % (int(time.time()))
 t = pyscope.registry.getClass(scope)()
 
 t.setCorrectedStagePosition(False)
+t.findMagnifications()
 
 def write_log(fields):
 	line = '\t'.join(map(str, fields)) + '\n'
@@ -36,19 +56,25 @@ def write_log(fields):
 	f.write(line)
 	f.close()
 
-for i, holder, mag, (x1,y1) in itertools.product(range(n), holders, mags, positions):
-	t.setHolderType(holder)
-	t.setMagnification(mag)
-	time.sleep(0.25)
-	pos0 = t.getStagePosition()
-	x0,y0 = pos0['x'], pos0['y']
-	t0 = time.time()
+for i in range(n):
+	print 'iteration', i
+	for mag in mags:
+		print 'mag', mag
+		for (x1,y1) in positions:
+			print 'position', x1,y1
+			t.setMagnification(mag)
+			time.sleep(0.25)
+			pos0 = t.getStagePosition()
+			x0,y0 = pos0['x'], pos0['y']
+			t0 = time.time()
+		
+			t.setStagePosition({'x':x1, 'y':y1})
+		
+			t1 = time.time()
+			pos2 = t.getStagePosition()
+			x2,y2 = pos2['x'],pos2['y']
+		
+			fields = [i,mag,x0,y0,x1,y1,x2,y2,t0,t1]
+			write_log(fields)
 
-	t.setStagePosition({'x':x1, 'y':y1})
-
-	t1 = time.time()
-	pos2 = t.getStagePosition()
-	x2,y2 = pos2['x'],pos2['y']
-
-	fields = [i,holder,mag,x0,y0,x1,y1,x2,y2,t0,t1]
-	write_log(fields)
+del t
