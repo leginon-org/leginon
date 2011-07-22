ca98c66d683ff235aa3cccccc69e8bc661333713
Gabriel Lander <glander>
2011-07-22 20:10:26 +0000

    updated to also handle aligned stack deletion

--- myamiweb/processing/dropstack.php
+++ myamiweb/processing/dropstack.php
@@ -28,16 +28,27 @@ function createDeleteStackForm($extra=false, $title='Delete Stack', $heading='De
 	$expId = $_GET['expId'];
 	$projectId = getProjectId();
 	$stackid=$_GET['sId'];
+	$alignstackid=$_GET['alignId'];
+
+	$formAction=$_SERVER['PHP_SELF']."?expId=$expId";
+	if ($stackid) {
+		$formAction.="&sId=$stackid";
+		$type="stack";
+	}
+	elseif ($alignstackid) {
+		$formAction.="&alignId=$alignstackid";
+		$type="alignment";
+	}
 
 	$javascript = "<script type='text/javascript'>\n";
 	$javascript.= "function doubleCheck(parts) {\n";
-	$javascript.= "  return confirm(\"Are you sure you want to delete this stack of \"+parts+\" particles?\");\n";
+	$javascript.= "  return confirm(\"Are you sure you want to delete this $type of \"+parts+\" particles?\");\n";
 	$javascript.= "}\n";
 	$javascript.= "</script>\n";
 
 	processing_header($title,$heading, $javascript, False);
 
-	if (!$stackid) {
+	if (!$stackid && !$alignstackid) {
 		echo "<B>No Stack Selected for deletion</B>\n";
 		processing_footer();
 		exit;
@@ -48,20 +59,25 @@ function createDeleteStackForm($extra=false, $title='Delete Stack', $heading='De
 		echo "<font color='#cc3333' size='+2'>$extra</font>\n<hr/>\n";
 	}
   
-	$formAction=$_SERVER['PHP_SELF']."?expId=$expId&sId=$stackid";
-
 	// --- Get Stack Data --- //
 	$particle = new particledata();
 
-	echo "<font color='#990000' size='+2'>WARNING: This stack will be deleted, along with all associated metadata.<br />\n";
+	echo "<font color='#990000' size='+2'>WARNING: This $type will be deleted, along with all associated metadata.<br />\n";
 	echo "This action cannot be undone!<br /></font>\n";
-	echo stacksummarytable($stackid, $mini=False,$tiny=False,$showOptions=False);
-	$numpart = $particle->getNumStackParticles($stackid);
+	if ($stackid) {
+		echo stacksummarytable($stackid, $mini=False,$tiny=False,$showOptions=False);
+		$numpart = $particle->getNumStackParticles($stackid);
+	}
+	elseif ($alignstackid) {
+		echo alignstacksummarytable($alignstackid, $mini=False, $showOptions=False);
+		$numpart = $particle->getNumAlignStackParticles($alignstackid);
+	}
+
 	$numpart = commafy($numpart);
 	echo "<hr />\n";
 	echo "<center>\n";
 	echo "<form name='viewerform' method='post' action='$formAction' onSubmit='return doubleCheck(\"$numpart\")'>\n";
-	echo "<input type='submit' name='process' value='Delete Stack'>\n";
+	echo "<input type='submit' name='process' value='Delete $type'>\n";
 	echo "</form>\n";
 	echo "</center>\n";
 	processing_footer();
@@ -75,13 +91,20 @@ function runDeleteStack() {
 	$expId = $_GET['expId'];
 	$projectId = getProjectId();
 	$stackid=$_GET['sId'];
+	$alignstackid=$_GET['alignId'];
 
 	/* *******************
 	PART 2: Get stack info, then remove it from database 
 	******************** */
 	$particle = new particledata();
-	$stackdata = $particle->getStackParams($stackid);
-	$deleteInfo = $particle->removeStackById($stackid);
+	if ($stackid) {
+		$stackdata = $particle->getStackParams($stackid);
+		$deleteInfo = $particle->removeStackById($stackid);
+	}
+	elseif($alignstackid) {
+		$stackdata = $particle->getAlignStackParams($alignstackid);
+		$deleteInfo = $particle->removeAlignStackById($alignstackid);
+	}
 	
 	/* *******************
 	PART 3: Show Additional Commands
