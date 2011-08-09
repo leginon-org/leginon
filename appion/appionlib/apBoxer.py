144004f07655d7b7cb1326eb8036438f126be735
Neil Voss <vossman>
2011-08-09 16:59:34 +0000

    debugging through subversion

--- appion/appionlib/apBoxer.py
+++ appion/appionlib/apBoxer.py
@@ -118,7 +118,7 @@ def boxerRotate(imgfile, parttree, outstack, boxsize):
 		### add 90 degrees because database angle is from x-axis not y-axis
 		angle = partdict['angle']+90.0
 		rotatepart = ndimage.rotate(bigboxpart, angle=angle, reshape=False, order=1)
-		boxpart = imagefilter.frame_cut(rotatepart, boxshape)
+		#boxpart = imagefilter.frame_cut(rotatepart, boxshape)
 		boxedparticles.append(boxpart)
 	sys.stderr.write("done\n")
 	apImagicFile.writeImagic(boxedparticles, outstack)
