7621dfcde3eb753a48e7087c03e2befd4bd54ff2
Neil Voss <vossman>
2011-08-10 23:47:13 +0000

    playing around

--- myamiweb/simpleviewer.py
+++ myamiweb/simpleviewer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
 import os
-#import leginondata
+import time
+from leginon import leginondata
 from leginon import projectdata
 
 def getProjectList():
@@ -9,12 +10,22 @@ def getProjectList():
 	projectlist = projectsdata.query()
 	return projectlist
 
+def getLatestSession(projectdata):
+	sessionq = leginondata.SessionData()
+	sessionq['project'] = projectdata
+	sessiondatas = sessionq.query(results=1)
+	return sessiondatas[0]
+
 def writeProjectDropList():
 	projectlist = getProjectList()
 	print "<select name='projectid' onChange='select_project()'>"
+	currentyear = time.localtime()[0]
 	for project in projectlist:
-		print ("  <option value='%d'>%s</option>"
-			%( project.dbid, project['name'].strip() ))
+		sessiondata = getLatestSession(project)
+		year = int(str(sessiondata.timestamp)[:4])
+		if year > currentyear-1:
+			print ("  <option value='%d'>%s (%d)</option>"
+				%( project.dbid, project['name'].strip(), project.dbid ))
 	print "</select>"
 
 if __name__ == "__main__":
