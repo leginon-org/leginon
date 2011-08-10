ac4028aa1ee06a896fc6985b72bf63d4a94c8dc9
Neil Voss <vossman>
2011-08-10 23:28:58 +0000

    more changes

--- myamiweb/simpleviewer.py
+++ myamiweb/simpleviewer.py
@@ -1,21 +1,31 @@
 #!/usr/bin/env python
 
 import os
-import leginondata
-import projectdata
+#import leginondata
+from leginon import projectdata
 
 def getProjectList():
-	projectlist = projectdata.projects()
-
+	projectsdata = projectdata.projects()
+	projectlist = projectsdata.query()
+	return projectlist
 
 def writeProjectDropList():
 	projectlist = getProjectList()
-	print "<select>"
+	print "<select name='projectid' onChange='select_project()'>"
 	for project in projectlist:
-		print ("<option value='%s'>%s</option>"
-			%(value, name))
+		print ("  <option value='%d'>%s</option>"
+			%( project.dbid, project['name'].strip() ))
 	print "</select>"
 
-
 if __name__ == "__main__":
+	print "<html>"
+	print "<head>"
+	print "  <title>Simple Viewer</title>"
+	print "</head>"
+	print "<body>"
+
+	### Project Drop Down List
 	writeProjectDropList()
+
+	print "</body>"
+	print "</html>"
