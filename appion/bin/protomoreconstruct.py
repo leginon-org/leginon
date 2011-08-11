cb1968c8006777392900d81dc161f648741bed12
Scott Stagg <sstagg>
2011-08-11 17:33:17 +0000

    completed simple appion-free reconstruction script. Depends on completed protomo refinement.

--- appion/bin/protomoreconstruct.py
+++ appion/bin/protomoreconstruct.py
@@ -8,3 +8,26 @@ import sys
 import glob
 import os
 
+def parseOptions():
+	parser=optparse.OptionParser()
+	parser.add_option('--param', dest='param', help= 'Param file')
+	parser.add_option('--map', dest='map', help= 'Output map name, e.g. series1.img')
+
+	options, args=parser.parse_args()
+	
+	if len(args) != 0 or len(sys.argv) == 1:
+		parser.print_help()
+		sys.exit()
+	
+	return options
+
+
+if __name__=='__main__':
+
+	options=parseOptions()
+	
+	param=protomo.param(options.param)
+	series=protomo.series(param)
+	series.mapfile(options.map)
+	
+	print "Done!"
