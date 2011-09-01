7c1844ca25ef802b8e0fa155297fdcc51da80365
Amber Herold <amber>
2011-09-01 22:45:18 +0000

    Make sure selectstackform does not include a row for stacks with no particles. refs #1407

--- myamiweb/processing/selectStackForm.php
+++ myamiweb/processing/selectStackForm.php
@@ -15,32 +15,29 @@ require_once "inc/viewer.inc";
 require_once "inc/project.inc";
 require_once "inc/summarytables.inc";
 
+// check if session provided
+$expId 	= $_GET['expId'];
+$method = $_GET['method'];
+$type	= $_GET['type'];
 
+if (!$expId) {
+	exit;
+}
 
-	// check if session provided
-	$expId 	= $_GET['expId'];
-	$method = $_GET['method'];
-	$type	= $_GET['type'];
-	$projectId = getProjectId();
+$particle = new particledata();
 
+// find each stack entry in database
+$stackIds = $particle->getStackIds($expId);
 
-	if (!$expId) {
-		exit;
+// remove stacks with no particles
+$filteredStackIds = array();
+foreach ($stackIds as $stackId) {
+	$numpart = $particle->getNumStackParticles($stackId['stackid']);
+	if ($numpart != 0) { 
+		$filteredStackIds[] = $stackId['stackid'];
 	}
+}
 
-	$particle = new particledata();
-
-	// find each stack entry in database
-	$stackIds 	= $particle->getStackIds($expId);
-	$stackinfo 	= explode('|--|', $_POST['stackval']);
-	$stackidval = $stackinfo[0];
-	$apix 		= $stackinfo[1];
-	$box 		= $stackinfo[2];
-
-	// write out errors, if any came up:
-	if ($extra) echo "<font color='#cc3333' size='+2'>$extra</font>\n<hr/>\n";
-
-	$javafunc="<script src='../js/viewer.js'></script>\n";
 ?>
 
 <?php processing_header("Appion: Recon Refinement","Select Refinement Stack",$javafunc); ?>
@@ -54,15 +51,14 @@ require_once "inc/summarytables.inc";
 	<br /><br />
 <?php 
 	echo "<table class='tableborder' border='1'>\n";
-	foreach ($stackIds as $stackId) {
+	foreach ($filteredStackIds as $stackId) {
 		echo "<tr><td>\n";
-		$stackid = $stackId['stackid'];
-		echo "<input type='radio' NAME='stackval' value='$stackid' ";		
+		echo "<input type='radio' NAME='stackval' value='$stackId' ";		
 		echo ">\n";
 		echo "Use<br/>Stack\n";
 
 		echo "</td><td>\n";	
-		echo stacksummarytable( $stackId["stackid"], True, False, False );
+		echo stacksummarytable( $stackId, True, False, False );
 		echo "</td></tr>\n";
 	}
 	echo "</table>\n\n";	
