27bd4541ae1cad556fb33c2e0c3675d1ba74efa5
Dmitry Lyumkis <dlyumkis>
2011-10-07 19:44:56 +0000

    added readers for selfiles and docfiles

--- appion/appionlib/apXmipp.py
+++ appion/appionlib/apXmipp.py
@@ -498,3 +498,30 @@ def calculate_equivalent_Eulers_without_flip(m):
 	### this was assessed empirically, works on synthetic data projected with xmipp_project
 	newpsi = -newpsi
 	return newphi, newtheta, newpsi
+
+#=======================
+#=======================
+def readSelfile(selfile):
+	''' returns a list of filenames '''
+	f = open(selfile, "r")
+	lines = f.readlines()
+	f.close()
+	split = [l.strip().split() for l in lines]
+	filenames = [s[0] for s in split]
+	return filenames
+
+#=======================
+#=======================
+def readDocfile(docfile):
+	''' returns a nested dictionary: key=particle number (starts with 0), value=dictionary: {'values':[allvals], 'filename':'name'} '''
+	f = open(docfile, "r")
+	lines = f.readlines()[1:]
+	f.close()
+	splitl = [l.strip().split() for l in lines]
+	d = {}
+	for i in range(len(splitl)/2):
+		filename = splitl[i*2][1]
+		partnum = int(float(filename[-10:-4]))
+		vals = splitl[i*2+1]
+		d[partnum] = {'filename':filename, 'values':vals}
+	return d
