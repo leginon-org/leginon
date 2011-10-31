945d10d5d6cb79ea8e761cd3d615c4f0e22fb188
pulokas <pulokas>
2011-10-31 20:58:21 +0000

    refs #1271 start of a module dedicated to imagic functions, imagic2mrc.py downsized  since some stuff moved into imagic.py

--- pyami/imagic2mrc.py
+++ pyami/imagic2mrc.py
@@ -1,43 +1,24 @@
 #!/usr/bin/env python
 
-import mrc
+import pyami.mrc
 import numpy
 import shutil
-
-# FIX:  only handling REAL for now
-code_to_dtype = {
-	numpy.frombuffer('REAL', numpy.int32)[0]:  numpy.float32,
-	# ...
-}
-
-def readImagicHeader(filename):
-	hed_array_int = numpy.fromfile(filename, numpy.int32)
-	hed_array_float = hed_array_int.view(numpy.float32)
-	hed_dict = {}
-	hed_dict['ny'] = hed_array_int[12]
-	hed_dict['nx'] = hed_array_int[13]
-	hed_dict['nz'] = len(hed_array_int.data) / 1024
-	hed_dict['dtype'] = code_to_dtype[hed_array_int[14]]
-	hed_dict['amin'] = hed_array_float[22]
-	hed_dict['amax'] = hed_array_float[21]
-	hed_dict['amean'] = hed_array_float[17]
-	hed_dict['rms'] = hed_array_float[18]
-	return hed_dict
+import pyami.imagic
 
 def imagic_to_mrc(imagic_name, mrc_name):
 	# read imagic header
 	hed_name = imagic_name + '.hed'
 	dat_name = imagic_name + '.img'
-	imagic_header = readImagicHeader(hed_name)
+	imagic_header = pyami.imagic.readImagicHeader(hed_name)
 
 	## create generic mrc header
-	mrc_header = mrc.newHeader()
-	mrc.updateHeaderDefaults(mrc_header)
+	mrc_header = pyami.mrc.newHeader()
+	pyami.mrc.updateHeaderDefaults(mrc_header)
 	## copy some values from imagic header to mrc header
 	for key in imagic_header.keys():
 		mrc_header[key] = imagic_header[key]
-	mrc_header['mode'] = mrc.numpy2mrc[mrc_header['dtype']]
-	mrc_header_data = mrc.makeHeaderData(mrc_header)
+	mrc_header['mode'] = pyami.mrc.numpy2mrc[mrc_header['dtype']]
+	mrc_header_data = pyami.mrc.makeHeaderData(mrc_header)
 
 	## write mrc header to file
 	fmrc = open(mrc_name, 'w')
