645a246cdee33ea0164ee7842e10143ebfa255dd
Anchi Cheng <acheng>
2012-02-16 19:05:51 +0000

    refs #1555 let appionScript decide the rundir

--- appion/appionlib/appionTiltSeriesLoop.py
+++ appion/appionlib/appionTiltSeriesLoop.py
@@ -132,18 +132,6 @@ class AppionTiltSeriesLoop(appionScript.AppionScript):
 	#######################################################
 
 	#=====================
-	def setRunDir(self):
-		if self.params['sessionname'] is not None:
-			#auto set the output directory
-			sessiondata = apDatabase.getSessionDataFromSessionName(self.params['sessionname'])
-			path = os.path.abspath(sessiondata['image path'])
-			pieces = path.split('leginon')
-			path = 'leginon'.join(pieces[:-1]) + 'appion' + pieces[-1]
-			path = re.sub("/rawdata","",path)
-			path = os.path.join(path, self.processdirname, self.params['runname'])
-			self.params['rundir'] = path
-
-	#=====================
 	def setupParserOptions(self):
 		"""
 		put in any additional parser options
