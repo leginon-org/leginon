582e7e5c7b82af912489f1706eb7df3560845f53
Anchi Cheng <acheng>
2012-03-23 00:49:42 +0000

    refs #1677 offset number by one

--- myamiweb/processing/tomoaligngraph.php
+++ myamiweb/processing/tomoaligngraph.php
@@ -49,7 +49,7 @@ $title = $titles[$type];
 $miny = 100000;
 $maxy = -100000;
 foreach ($refinedata as $data) {
-	$datax[] = $data['number'];
+	$datax[] = $data['number']+1;
 	$datay[] = $data[$key];
 	$miny = ($miny > $data[$key] && $data['number'] <= $maxx && $data['number'] >= $minx) ? 
 		$data[$key]:$miny;
