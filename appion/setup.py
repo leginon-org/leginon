374efede16001b31fc899737e8f5c502254f3536
pulokas <pulokas>
2012-04-09 20:22:34 +0000

    refs 1699, user will be warned that they should specify a script destination.

--- appion/setup.py
+++ appion/setup.py
@@ -1,6 +1,7 @@
 # For more info on expanding this script, see:
 #     http://docs.python.org/distutils/setupscript.html
 
+import sys
 import os
 import glob
 import distutils.core
@@ -52,12 +53,50 @@ def getVersion():
 
 version=getVersion()
 
+# script files to be installed in bin dir
+binfiles=glob.glob('bin/*.*')
+
+# determine if script destination has been properly specified
+arg_cmd = None
+arg_install_scripts = False
+arg_install_dir = False
+arg_help = False
+for arg in sys.argv:
+	if arg == 'install':
+		arg_cmd = 'install'
+	if arg == 'install_scripts':
+		arg_cmd = 'install_scripts'
+	if 'install-scripts' in arg:
+		arg_install_scripts = True
+	if 'install-dir' in arg:
+		arg_install_dir = True
+	if 'help' in arg:
+		arg_help = True
+warn_user = ''
+if not arg_help:
+	if arg_cmd == 'install' and not arg_install_scripts:
+		warn_user = '--install-scripts'
+	if arg_cmd == 'install_scripts' and not arg_install_dir:
+		warn_user = '--install_dir'
+if warn_user:
+	message = '''   *** WARNING ***
+  You have not specified the option:  %s=<scriptpath>
+  You are about to install %d scripts into the default script directory.
+  The default could be /usr/bin, /usr/local/bin, or some other location
+  you do not wish to clutter up with these scripts.
+
+  Are you sure you want to continue? (y/n): ''' % (warn_user, len(binfiles))
+	answer = raw_input(message)
+	if answer != 'y':
+		print 'Installation aborted.'
+		sys.exit()
+
 distutils.core.setup(
 	name='Appion',
 	version=version,
 	author_email='appion@scripps.edu',
 	packages=['appionlib', 'appionlib.apSpider', 'appionlib.apTilt', 'appionlib.apImage', 'appionlib.apImagic'],
 	package_data={'appionlib': ['data/*.*', 'version.txt']},
-	scripts=glob.glob('bin/*.*'),
+	scripts=binfiles,
 )
 
