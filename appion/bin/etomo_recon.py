e55e789657ca7bd986970c4b3f4c725051babfc7
Anchi Cheng <acheng>
2012-04-09 19:43:20 +0000

    remove to make executable one

--- appion/bin/etomo_recon.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-from appionlib import apTomoFullRecon
-
-if __name__ == '__main__':
-	app.apTomoFullRecon.ETomoMaker()
-	app = apTomoSample.SampleMaker()
-	app.start()
-	app.close()
