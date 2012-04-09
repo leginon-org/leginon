d810fbf4d29c70c75add978014692dfe65e1ab7e
Anchi Cheng <acheng>
2012-04-09 20:00:56 +0000

    refs #1689 debug

--- appion/bin/etomo_recon.py
+++ appion/bin/etomo_recon.py
@@ -2,7 +2,6 @@
 from appionlib import apTomoFullRecon
 
 if __name__ == '__main__':
-	app.apTomoFullRecon.ETomoMaker()
-	app = apTomoSample.SampleMaker()
+	app = apTomoFullRecon.ETomoMaker()
 	app.start()
 	app.close()
