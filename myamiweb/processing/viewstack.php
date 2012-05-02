841c448ae4f9975f708172e37ee979f79563d2ed
Gabriel Lander <glander>
2012-05-02 23:40:21 +0000

    web tools for examining results of 2D alignment with helical data refs #1776

--- myamiweb/processing/viewstack.php
+++ myamiweb/processing/viewstack.php
@@ -65,6 +65,7 @@ if ($reconId) {
 }
 if ($alignId) {
 	$classnumber=$particle->getAlignParticleNumber($alignId);
+	$helical = $particle->hasHelicalInfo($alignId);
 } elseif ($clusterId) {
 	$classnumber=$particle->getClusteringParticleNumber($clusterId);
 }
@@ -183,7 +184,9 @@ if ($alignId || $clusterId) {
 	while ($classindex < $numclass && $i < $n_images) {
 		if ($classnumber[$classindex]['classNumber'] == $i+1) {
 			if (array_key_exists('resolution', $classnumber[$classindex]))
-				$c[]= sprintf("'%d, %.1fA'", $classnumber[$classindex]['number'], $classnumber[$classindex]['resolution']);
+				if ($classnumber[$classindex]['resolution'])
+					$c[]= sprintf("'%d, %.1fA'", $classnumber[$classindex]['number'], $classnumber[$classindex]['resolution']);
+				else $c[]= sprintf("'%d'", $classnumber[$classindex]['number']);
 			else
 				$c[]= sprintf("'%d'", $classnumber[$classindex]['number']);
 			$classindex++;
@@ -348,6 +351,13 @@ function viewAlignedSubstack() {
 	}
 }
 
+function showHelicalInfo() {
+	var index = $('selectedIndex').value
+	if (alignId!="") {
+		window.open("showHelicalInfo.php?expId="+expId+"&alignId="+alignId+"&include="+index,'height=250,width=400');
+	}
+}
+
 </script>
 </head>
 <body onload='load()'>
@@ -399,6 +409,8 @@ if ($stackId || $clusterId || $alignId)
 if ($clusterId || $alignId) {
 	$includebuttons .= "<input type='button' value='Create Template Stack' onClick='createTemplateStackIncluded()'>\n";
 	$includebuttons .= "<input type='button' value='Run Common Lines' onClick='runCommonLines()'>\n";
+if ($helical && $alignId)
+	$includebuttons .= "<input type='button' value='Helical Info' onClick='showHelicalInfo()'>\n";
 }
 //if ($clusterId || $templateStackId)
 //	$includebuttons .= "<input type='button' value='Run Imagic 3d0' onClick='create3d0();' id='3d0button'>\n";
@@ -433,7 +445,6 @@ echo "</td></tr><tr><td>\n";
 if ($stackId)
 	echo "<input id='uploadavg' type='button' value='Average images as template' onClick='uploadavg();'>\n";
 echo "</td></tr></table>\n";
-
 ?>
 
 
