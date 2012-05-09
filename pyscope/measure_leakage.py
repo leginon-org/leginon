99c96c37fc79b953a1d59ea863ddbce4324dedc3
pulokas <pulokas>
2012-05-09 01:10:37 +0000

    fixed some things

--- pyscope/measure_leakage.py
+++ pyscope/measure_leakage.py
@@ -12,29 +12,34 @@ for i in (0,1):
 	cam.setUseFrames((0,))
 	cam.setExposureType('dark')
 	im = cam.getImage()
-	images.append(im)
+	dark_images.append(im)
 diff = dark_images[1] - dark_images[0]
-std = numextension.allstats(diff, std=True)['std']
+std = diff.std()
 print 'Dark Noise: ', std
 
 # Measure Leakage Current
 # acquire two dark frames, one at 5 fps, on at 20 fps
 dark_images = []
 exposure_times = []
-for fps in (5,20):
-	exp_time = 1000 * int(1.0 / fps)
-	cam.setFramesPerSecond(fps)
-	cam.setExposureTime(exp_time)  # only one frame
+for fps in (20,5):
+	set_exp_time = 1000 * int(1.0 / fps)
+	cam.setFrameRate(fps)
+	cam.setExposureTime(set_exp_time)  # only one frame
+	exp_time = cam.getExposureTime()
+	print 'EXPTIME', exp_time
 	cam.setUseFrames((0,))   # just to be sure there is only one frame
 	cam.setExposureType('dark')
 	im = cam.getImage()
-	images.append(im)
+	print 'IM MEAN', im.mean()
+	dark_images.append(im)
 	exposure_times.append(exp_time)
 
 # calculate dark noise
 imdiff = dark_images[1] - dark_images[0]
-timediff = exposure_times[1] - exposure_times[0]
-mean = numextension.allstats(diff, mean=True)['mean']
+timediff = (exposure_times[1] - exposure_times[0]) / 1000.0
+print 
+mean = imdiff.mean()
+print 'DIFF MEAN', mean
 leakage = mean / timediff
 print 'Leakage: ', leakage
 
