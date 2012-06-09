5bbde25dfcfe4ff0460c428f7068717d68a40bca
Anchi Cheng <acheng>
2012-06-09 01:41:46 +0000

    refs #1844 merge r16754:r16759 from trunk to branch 2.2

--- pyscope/measure_leakage.py
+++ pyscope/measure_leakage.py
@@ -2,20 +2,24 @@
 
 import pyscope.registry
 import numextension
+import numpy
+import time
 
 cam = pyscope.registry.getClass('DE12')()
 
 # Measure Dark Noise
+cam.setFrameRate(24.57)
 cam.setExposureTime(0)
 dark_images = []
 for i in (0,1):
 	cam.setUseFrames((0,))
 	cam.setExposureType('dark')
 	im = cam.getImage()
+	im = im.astype(numpy.float32)
 	dark_images.append(im)
 diff = dark_images[1] - dark_images[0]
-std = diff.std()
-print 'Dark Noise: ', std
+dark_noise = diff.std()
+print 'Dark Noise: ', dark_noise
 
 # Measure Leakage Current
 # acquire two dark frames, one at 5 fps, on at 20 fps
@@ -26,22 +30,25 @@ for fps in (20,5):
 	cam.setFrameRate(fps)
 	cam.setExposureTime(set_exp_time)  # only one frame
 	exp_time = cam.getExposureTime()
-	print 'EXPTIME', exp_time
 	cam.setUseFrames((0,))   # just to be sure there is only one frame
 	cam.setExposureType('dark')
 	im = cam.getImage()
-	print 'IM MEAN', im.mean()
+	im = im.astype(numpy.float32)
 	dark_images.append(im)
 	exposure_times.append(exp_time)
 
 # calculate dark noise
 imdiff = dark_images[1] - dark_images[0]
 timediff = (exposure_times[1] - exposure_times[0]) / 1000.0
-print 
 mean = imdiff.mean()
-print 'DIFF MEAN', mean
 leakage = mean / timediff
 print 'Leakage: ', leakage
+
+logname = 'leakage.log'
+f = open(logname, 'a+')
+timestamp = time.asctime()
+f.write('%s\t%s\t%s\n' % (timestamp, dark_noise, leakage))
+print 'Values appended to %s' % (logname,)
 
 print ''
 raw_input('Enter to quit.')
