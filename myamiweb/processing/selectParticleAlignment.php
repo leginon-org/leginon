9d1b9fc498beadeee2d16694eced8a42606f9c6b
Neil Voss <vossman>
2012-06-11 03:03:15 +0000

    change order, refs #1846

--- myamiweb/processing/selectParticleAlignment.php
+++ myamiweb/processing/selectParticleAlignment.php
@@ -36,28 +36,6 @@ echo "</table>\n";
 echo "<br/>\n";
 echo "<table border='1' class='tableborder' width='640'>\n";
 
-
-/*
-** Xmipp Clustering 2D Reference Free Alignment
-*/
-
-echo "<tr><td width='100' align='center'>\n";
-echo "  <img src='img/xmipp_logo.png' width='64'>\n";
-echo "</td><td>\n";
-echo "  <h3><a href='runCL2DAlign.php?expId=$expId'>Xmipp Clustering 2D Alignment</a></h3>\n";
-echo " <p> this method builds a hierarchical classification of particles"
-	." It uses the "
-	."<a href='http://xmipp.cnb.csic.es/twiki/bin/view/Xmipp/ClassAverages'>Xmipp cl2d</a>"
-	."&nbsp;<img src='img/external.png'>"
-	." program to perform alignments. "
-	."It is a relatively fast method that aligns and classify the images at the same time. "
-	."The method starts by estimating a few classes that are further subdivided till the desired number of classes is reached. "
-	."Every time an image is compared to the class averages it is aligned before-hand. <b> NOTE: in Xmipp 2.4 the alignment "
-	."parameters are not saved in the database, and therefore this method cannot be used for RCT / OTR reconstructions.</b>"
-	."</p>\n";
-//echo "  <img src='img/align-smr.png' width='250'><br/>\n";
-echo "</td></tr>\n";
-
 /*
 ** Xmipp Maximum Likelihood Reference Free Alignment
 */
@@ -95,6 +73,28 @@ echo " <p> first you select template and then this method uses the "
 //echo "  <img src='img/align-rsm.png' width='125'><br/>\n";
 echo "</td></tr>\n";
 
+/*
+** Xmipp Clustering 2D Reference Free Alignment
+*/
+
+echo "<tr><td width='100' align='center'>\n";
+echo "  <img src='img/xmipp_logo.png' width='64'>\n";
+echo "</td><td>\n";
+echo "  <h3><a href='runCL2DAlign.php?expId=$expId'>Xmipp Clustering 2D Alignment</a></h3>\n";
+echo " <p> this method builds a hierarchical classification of particles"
+	." It uses the "
+	."<a href='http://xmipp.cnb.csic.es/twiki/bin/view/Xmipp/ClassAverages'>Xmipp cl2d</a>"
+	."&nbsp;<img src='img/external.png'>"
+	." program to perform alignments. "
+	."It is a relatively fast method that aligns and classify the images at the same time. "
+	."The method starts by estimating a few classes that are further subdivided till the desired number of classes is reached. "
+	."Every time an image is compared to the class averages it is aligned before-hand. <b> NOTE: in Xmipp 2.4 the alignment "
+	."parameters are not saved in the database, and therefore this method cannot be used for RCT / OTR reconstructions.</b>"
+	."</p>\n";
+//echo "  <img src='img/align-smr.png' width='250'><br/>\n";
+echo "</td></tr>\n";
+
+
 if (!HIDE_IMAGIC && !HIDE_FEATURE) {
 	/*
 	** IMAGIC Reference Based Alignment
@@ -108,6 +108,45 @@ if (!HIDE_IMAGIC && !HIDE_FEATURE) {
 		."<a href='http://www.imagescience.de/smi/index.htm'>IMAGIC m-r-a</a>"
 		."&nbsp;<img src='img/external.png'>"
 		." command to align your particles to the templates within a specified template stack"
+		."</p>\n";
+	echo "</td></tr>\n";
+}
+
+/*
+** SPIDER Reference Free Alignment
+*/
+
+echo "<tr><td width='100' align='center'>\n";
+echo "  <img src='img/spider_logo.png' width='64'>\n";
+echo "</td><td>\n";
+echo "  <h3><a href='runSpiderNoRefAlignment.php?expId=$expId'>Spider Reference-free Alignment</a></h3>\n";
+echo " <p> this method uses the "
+	."<a href='http://www.wadsworth.org/spider_doc/spider/docs/man/apsr.html'>Spider AP SR</a>"
+	."&nbsp;<img src='img/external.png'>"
+	." command to align your particles. "
+	."</p><p>\n"
+	."<font color='#aa2222'>WARNING:</font> this method is very quick (~few minutes), "
+	."but also very sloppy and does not always do a great job. "
+	."The only way to obtain decent results is to run several times and compare the results.</p>"
+	."</p>\n";
+//echo "  <img src='img/align-rsm.png' width='125'><br/>\n";
+echo "</td></tr>\n";
+
+if (!HIDE_FEATURE)
+{
+	/*
+	** Topology representing network alignment
+	*/
+	echo "<tr><td width='100' align='center'>\n";
+	echo "  <img src='img/canimg.png' width='64'>\n";
+	echo "</td><td>\n";
+	echo "  <h3><a href='runTopolAlign.php?expId=$expId'>Iterative MSA/MRA</a></h3>\n";
+	echo "<p>Fast & easy 2D ref-free alignment by iterative MSA/MRA.  Classification "
+		." can be performed using a topology-representing network or IMAGIC MSA."
+		." Multi-reference alignment can be performed using IMAGIC or EMAN. </a>" 
+#		."The classification is performed by Vince Ramey's implementation of "
+#		."<a target='blank' href='http://www.ncbi.nlm.nih.gov/pubmed/14572474'>"
+#		."Ogura et al. JSB (2003)</a>"
 		."</p>\n";
 	echo "</td></tr>\n";
 }
@@ -136,26 +175,21 @@ if (!HIDE_FEATURE)
 	echo "</td></tr>\n";
 }
 
-/*
-** SPIDER Reference Free Alignment
-*/
-
-echo "<tr><td width='100' align='center'>\n";
-echo "  <img src='img/spider_logo.png' width='64'>\n";
-echo "</td><td>\n";
-echo "  <h3><a href='runSpiderNoRefAlignment.php?expId=$expId'>Spider Reference-free Alignment</a></h3>\n";
-echo " <p> this method uses the "
-	."<a href='http://www.wadsworth.org/spider_doc/spider/docs/man/apsr.html'>Spider AP SR</a>"
-	."&nbsp;<img src='img/external.png'>"
-	." command to align your particles. "
-	."</p><p>\n"
-	."<font color='#aa2222'>WARNING:</font> this method is very quick (~few minutes), "
-	."but also very sloppy and does not always do a great job. "
-	."The only way to obtain decent results is to run several times and compare the results.</p>"
-	."</p>\n";
-//echo "  <img src='img/align-rsm.png' width='125'><br/>\n";
-echo "</td></tr>\n";
-
+if (!HIDE_FEATURE)
+{
+	/*
+	** EMAN reference-free alignment
+	*/
+	echo "<tr><td width='100' align='center'>\n";
+	echo "  <img src='img/eman_logo.png' width='64'>\n";
+	echo "</td><td>\n";
+	echo "  <h3><a href='runEmanRefine2d.php?expId=$expId'>EMAN Refine 2d Reference-free Alignment</a></h3>\n";
+	echo "<p>Fast and easy 2D ref-free alignment using EMAN's "
+		."<a href='http://blake.bcm.tmc.edu/eman/eman1/progs/refine2d.py.html'>"
+		."refine2d.py</a> program"
+		."</p>\n";
+	echo "</td></tr>\n";
+}
 
 /*
 ** Xmipp Maximum Likelihood Reference Based Alignment
@@ -176,43 +210,6 @@ echo " <p> similar to reference-free but you select templates first."
 	."</p>\n";
 //echo "  <img src='img/align-smr.png' width='250'><br/>\n";
 echo "</td></tr>\n";
-
-
-if (!HIDE_FEATURE)
-{
-	/*
-	** Topology representing network alignment
-	*/
-	echo "<tr><td width='100' align='center'>\n";
-	echo "  <img src='img/canimg.png' width='64'>\n";
-	echo "</td><td>\n";
-	echo "  <h3><a href='runTopolAlign.php?expId=$expId'>Iterative MSA/MRA</a></h3>\n";
-	echo "<p>Fast & easy 2D ref-free alignment by iterative MSA/MRA.  Classification "
-		." can be performed using a topology-representing network or IMAGIC MSA."
-		." Multi-reference alignment can be performed using IMAGIC or EMAN. </a>" 
-#		."The classification is performed by Vince Ramey's implementation of "
-#		."<a target='blank' href='http://www.ncbi.nlm.nih.gov/pubmed/14572474'>"
-#		."Ogura et al. JSB (2003)</a>"
-		."</p>\n";
-	echo "</td></tr>\n";
-}
-
-
-if (!HIDE_FEATURE)
-{
-	/*
-	** EMAN reference-free alignment
-	*/
-	echo "<tr><td width='100' align='center'>\n";
-	echo "  <img src='img/eman_logo.png' width='64'>\n";
-	echo "</td><td>\n";
-	echo "  <h3><a href='runEmanRefine2d.php?expId=$expId'>EMAN Refine 2d Reference-free Alignment</a></h3>\n";
-	echo "<p>Fast and easy 2D ref-free alignment using EMAN's "
-		."<a href='http://blake.bcm.tmc.edu/eman/eman1/progs/refine2d.py.html'>"
-		."refine2d.py</a> program"
-		."</p>\n";
-	echo "</td></tr>\n";
-}
 
 echo "</table>\n";
 processing_footer();
