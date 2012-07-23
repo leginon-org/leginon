95697161c990de05d000b38ff5bd1df9415b2643
Neil Voss <vossman>
2012-07-23 20:39:35 +0000

    new file

---
+++ appion/appionlib/apCtf/ctfpower.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import time
+import math
+import numpy
+from pyami import imagefun
+from appionlib import apDisplay
+from appionlib.apCtf import ctftools
+
+debug = False
+
+
+
+#===================
+#===================
+#===================
+if __name__ == "__main__":
+	print "xxx"
