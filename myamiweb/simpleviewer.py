a2bb7657a721fe08bbe130cb31d041da322e88d0
Neil Voss <vossman>
2012-08-28 13:10:12 +0000

    this file is not used

--- myamiweb/simpleviewer.py
+++ myamiweb/simpleviewer.py
@@ -5,10 +5,20 @@ import time
 from leginon import leginondata
 from leginon import projectdata
 
-def getProjectList():
+def getProjectList(showall=False):
 	projectsdata = projectdata.projects()
 	projectlist = projectsdata.query()
-	return projectlist
+	if showall is True:
+		return projectlist
+	### select only projects that have recently collected data
+	selectedprojects = []
+	currentyear = time.localtime()[0]
+	for project in projectlist:
+		sessiondata = getLatestSession(project)
+		year = int(str(sessiondata.timestamp)[:4])
+		if year > currentyear-1:
+			selectedprojects.append(project)
+	return selectedprojects
 
 def getLatestSession(projectdata):
 	sessionq = leginondata.SessionData()
@@ -19,12 +29,8 @@ def getLatestSession(projectdata):
 def writeProjectDropList():
 	projectlist = getProjectList()
 	print "<select name='projectid' onChange='select_project()'>"
-	currentyear = time.localtime()[0]
 	for project in projectlist:
-		sessiondata = getLatestSession(project)
-		year = int(str(sessiondata.timestamp)[:4])
-		if year > currentyear-1:
-			print ("  <option value='%d'>%s (%d)</option>"
+		print ("  <option value='%d'>%s (%d)</option>"
 				%( project.dbid, project['name'].strip(), project.dbid ))
 	print "</select>"
 
