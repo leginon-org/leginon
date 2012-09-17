e9fa7741fba68ff1183c7613ad26c7609256c7f0
Amber Herold <amber>
2012-09-17 21:34:10 +0000

    refs #1996, more progress on maskiton

--- myamiweb/processing/maskiton.php
+++ myamiweb/processing/maskiton.php
@@ -8,16 +8,20 @@
  *	Simple viewer to view a image using mrcmodule
  */
 
-require "inc/particledata.inc";
-require "inc/leginon.inc";
-require "inc/project.inc";
-require "inc/viewer.inc";
-require "inc/processing.inc";
-require "inc/appionloop.inc";
- 
-createForm();
+require_once "inc/particledata.inc";
+require_once "inc/processing.inc";
+require_once "inc/leginon.inc";
+require_once "inc/viewer.inc";
+require_once "inc/project.inc";
+require_once "inc/summarytables.inc";
 
-function createForm($extra=false, $title='Maskiton Launcher', $heading='Masks with Maskiton') {
+if ($_POST) {
+	createMaskitonForm();
+} else {
+	createSelectStackForm();
+}
+
+function createMaskitonForm($extra=false, $title='Maskiton Launcher', $heading='Masks with Maskiton') {
 	// check if coming directly from a session
 	$expId = $_GET['expId'];
 	if ( $expId ) {
@@ -28,15 +32,73 @@ function createForm($extra=false, $title='Maskiton Launcher', $heading='Masks wi
 		$formAction = $_SERVER['PHP_SELF'];	
 	}
 	$projectId = getProjectId();
+	$stackval 	= $_POST['stackval'];
 
-	processing_header($title,$heading,$javascript);
+	processing_header($title,$heading,$javascript, $pleaseWait=false, $showmenu=true, $printDiv=false, 
+						$guideURL="http://ami.scripps.edu/redmine/projects/appion/wiki/");
 	// write out errors, if any came up:
 	if ($extra) {
 		echo "<font color='#cc3333' size='+2'>$extra</font>\n<hr/>\n";
 	}
 	
-	echo "<Iframe src='http://ami.scripps.edu/group/forum/index.php?view=1' width='600' height='600'></Iframe><br /><br />\n";
+	echo "<Iframe src='http://maskiton.scripps.edu/masking.html?projectid=$projectId&stackid=$stackval' width='800' height='800'></Iframe><br /><br />\n";
 
+	processing_footer();
+}
+
+
+function createSelectStackForm($extra=false, $title='Appion: Select Maskiton Stack', $heading='Select Maskiton Stack') 
+{
+	// check if coming directly from a session
+   $expId = $_GET['expId'];
+	if ($expId) {
+		$sessionId=$expId;
+		$formAction=$_SERVER['PHP_SELF']."?expId=$expId";
+	}
+	else {
+		$sessionId=$_POST['sessionId'];
+		$formAction=$_SERVER['PHP_SELF'];	
+	}
+		
+	$particle = new particledata();
+	
+	// find each stack entry in database
+	$stackIds = $particle->getStackIds($expId);
+	
+	// remove stacks with no particles
+	$filteredStackIds = array();
+	foreach ($stackIds as $stackId) {
+		$numpart = $particle->getNumStackParticles($stackId['stackid']);
+		if ($numpart != 0) { 
+			$filteredStackIds[] = $stackId['stackid'];
+		}
+	}
+
+	processing_header($title,$heading,$javafunc);
+
+	echo "<form name='maskiton' method='POST' ACTION='$formAction'>";
+	echo "<b>Select a stack:</b><br>";
+	echo "<P><input type='SUBMIT' NAME='submitstack' VALUE='Use selected stack'>";
+
+	echo "<br /><br />";
+ 
+	echo "<table class='tableborder' border='1'>\n";
+	foreach ($filteredStackIds as $stackId) {
+		echo "<tr><td>\n";
+		echo "<input type='radio' NAME='stackval' value='$stackId' ";		
+		echo ">\n";
+		echo "Use<br/>Stack\n";
+
+		echo "</td><td>\n";	
+		echo stacksummarytable( $stackId, True, False, False );
+		echo "</td></tr>\n";
+	}
+	echo "</table>\n\n";	
+
+	echo "<P><input type='SUBMIT' NAME='submitstack' VALUE='Use selected stack'>";
+	echo "</form>";
+
+	echo showReference( "appion" );
 	processing_footer();
 }
 
