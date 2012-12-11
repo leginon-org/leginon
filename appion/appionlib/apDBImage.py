760f504cabcbf39fe379d863ef566224df01724f
Anchi Cheng <acheng>
2012-12-11 22:33:08 +0000

    refs #2160 allow version to go more than 1

--- appion/appionlib/apDBImage.py
+++ appion/appionlib/apDBImage.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
+import os
 from leginon import leginondata
 #appion
-from appionlib import apDatabase
+from appionlib import apDatabase, apDisplay
 
 #=========================
 def correctImage(imgdata):
@@ -24,9 +25,17 @@ def makeUniqueImageFilename(old_imagedata,old_presetname,new_presetname):
 		bits = old_imagefilename.split(old_presetname)
 		before_string = old_presetname.join(bits[:-1])
 		new_imagefilename = new_presetname.join((before_string,bits[-1]))
-		existing_images = leginondata.AcquisitionImageData(session=old_imagedata['session'],filename=new_imagefilename).query()
-		if len(existing_images) == 0:
-			return new_imagefilename
-		else:
-			version = '_v%02d' % len(existing_images)
-			return new_imagefilename+version
+		version_number = 0
+		while True:
+			if version_number == 0:
+				version_text = ''
+			else:
+				version_text = '_v%02d' % version_number
+			new_name = new_imagefilename+version_text
+			new_image_path = os.path.join(old_imagedata['session']['image path'],new_name+'.mrc')
+			if not os.path.isfile(new_image_path):
+				break
+			else:
+				version_number += 1
+		apDisplay.printColor('New image filename is: %s' % new_name,'magenta')
+		return new_name
