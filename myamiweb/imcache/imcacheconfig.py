3362bd1357ebebb35b120a310a8d9b9d963dc212
pulokas <pulokas>
2013-01-14 21:55:44 +0000

    refs #2203, completed the redux thread, should be good for a real test now

--- myamiweb/imcache/imcacheconfig.py
+++ myamiweb/imcache/imcacheconfig.py
@@ -3,10 +3,10 @@
 # time in seconds to wait between consecutive queries
 query_interval = 5
 
-# limit query to later than this timestamp (mysql style)
-min_timestamp = '201301111450'
+# limit query to later than this timestamp (mysql style: yyyymmddhhmmss)
+min_timestamp = '20130114120000'
 
-# limit query to start at this id
+# limit query to start at this image id
 start_id = 0
 
 # root dir of cache.  session subdirs will be added automatically
@@ -18,14 +18,10 @@ max_size = 1024
 # redux args for image conversion
 # filename and shape will be determined per image
 redux_args = {
-	'pipes': (
-		('read','Read'),
-		('shape','Shape'),
-		('scale','Scale'),
-		('format','Format')
-	),
+	'pipes': 'read:Read,shape:Shape,scale:Scale,format:Format',
+	'cache': False,
 	'oformat': 'JPEG',
-	'scaletype': 'std',
+	'scaletype': 'stdev',
 	'scalemin': -5,
 	'scalemax': 5,
 }
