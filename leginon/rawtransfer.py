de781d597e02e2b2cfc32814647b2bd42cad6bca
pulokas <pulokas>
2013-04-19 22:42:09 +0000

    refs #2334, recent changes for where to store frames

--- leginon/rawtransfer.py
+++ leginon/rawtransfer.py
@@ -7,6 +7,7 @@ import subprocess
 import time
 import leginon.leginondata
 import leginon.ddinfo
+import pyami.fileutil
 
 check_interval = 30  # seconds between checking for new frames
 
@@ -40,6 +41,8 @@ def copy_and_delete(src, dst):
 	## ensure a trailing / on src
 	if src[-1] != '/':
 		src = src + '/'
+	## make dirs
+	pyami.fileutil.mkdirs(dst)
 	cmd = 'rsync -av --remove-sent-files %s %s' % (src, dst)
 	print cmd
 	p = subprocess.Popen(cmd, shell=True)
@@ -54,17 +57,53 @@ def copy_and_delete(src, dst):
 	p = subprocess.Popen(cmd, shell=True, cwd=dirpath)
 	p.wait()
 
+next_time_start = 0
+mtime = 0
+time_expire = 300  # ignore anything older than 5 minutes
+expired_names = {} # directories that have no db record after 5 minutes
+
 def run_once(parent_src_path):
+	global next_time_start
+	global time_expire
+	global mtime
 	names = os.listdir(parent_src_path)
 	time.sleep(4)  # wait for any current writes to finish
+	time_start = next_time_start
 	for name in names:
+		src_path = os.path.join(parent_src_path, name)
+		## skip empty directories
+		if os.path.isdir(src_path) and not os.listdir(src_path):
+			# maybe delete empty dir too?
+			continue
+
+		# skip expired dirctories
+		if name in expired_names:
+			continue
+
+		# adjust next expiration timer to most recent time
+		if mtime > next_time_start:
+			next_time_start = mtime
+
+		print '**running', src_path
+
+		# query for Leginon image
 		imdata = query_image_by_frames_name(name)
 		if imdata is None:
 			continue
 		image_path = imdata['session']['image path']
+		path_parts = image_path.split('/')
+		print 'PATHPARTS', path_parts
+		root_parts = path_parts[:-3]
+		leg_part = path_parts[-3]
+		ses_part = path_parts[-2]
+		raw_part = path_parts[-1]
+		frames_parts = root_parts + ['frames',ses_part,raw_part]
+		frames_path = '/'.join(frames_parts)
 		imname = imdata['filename'] + '.frames'
-		src_path = os.path.join(parent_src_path, name)
-		dst_path = os.path.join(image_path, imname)
+		dst_path = os.path.join(frames_path, imname)
+		print 'DEST', dst_path
+
+		# do actual copy and delete
 		copy_and_delete(src_path, dst_path)
 		leginon.ddinfo.saveImageDDinfoToDatabase(imdata,os.path.join(dst_path,'info.txt'))
 
@@ -72,6 +111,7 @@ def run():
 	src_path = get_source_path()
 	print 'Source path:  %s' % (src_path,)
 	while True:
+		print 'Iterating...'
 		run_once(src_path)
 		time.sleep(check_interval)
 
