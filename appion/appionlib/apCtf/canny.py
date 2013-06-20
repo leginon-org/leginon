bc1e97f674a3f45e97cd068da57ddd5b90c01a22
Neil Voss <vossman>
2013-06-20 18:43:19 +0000

    working methods, refs #2400

--- appion/appionlib/apCtf/canny.py
+++ appion/appionlib/apCtf/canny.py
@@ -3,6 +3,7 @@
 import math
 import time
 import numpy
+import random
 from scipy import ndimage
 #from appionlib.apImage import imagefile
 
@@ -76,7 +77,7 @@ def non_maximal_edge_suppresion(mag, orient):
 	edge_map = numpy.where(outermask, edge_map, False)
 	edge_map = numpy.where(innermask, edge_map, False)
 
-	print time.time() - t0
+	#print time.time() - t0
 	return edge_map
 
 
@@ -89,32 +90,48 @@ def canny_edges(image, sigma=1.0, low_thresh=50, high_thresh=100):
 
 	mag = numpy.hypot(dx, dy)
 	mag = mag / mag.max()
+
 	ort = numpy.arctan2(dy, dx)
 
 	edge_map = non_maximal_edge_suppresion(mag, ort)
 
 	edge_map = numpy.logical_and(edge_map, mag > low_thresh)
 
-	labels, num = ndimage.measurements.label(edge_map, numpy.ones((3,3)))
+	labels, numlabels = ndimage.measurements.label(edge_map, numpy.ones((3,3)))
 	#print "labels", len(labels)
 
 	#print maxs
 
-	maxs = ndimage.measurements.maximum(mag, labels, range(1,num+1))
-	high_thresh = numpy.array(maxs).mean()
+	maxs = ndimage.measurements.maximum(mag, labels, range(1,numlabels+1))
+	maxs = numpy.array(maxs, dtype=numpy.float64)
+	high_thresh = maxs.mean()
+	minThresh = maxs.min()
 
-	print time.time() - t0
+	#print time.time() - t0
 	minedges = 1500
-	maxedges = 15000
+	maxedges = 8000
 	edge_count = edge_map.sum()
 	count = 0
-	while count < 10:
-		count += 1
-		maxs = ndimage.measurements.maximum(mag, labels, range(1,num+1))
-
+	while count < 25:
 		t0 = time.time()
-		good_label = numpy.zeros((num+1,), bool)
-		good_label[1:] = maxs > high_thresh
+		count += 1
+		maxs = ndimage.measurements.maximum(mag, labels, range(1,numlabels+1))
+		maxs = numpy.array(maxs, dtype=numpy.float64)
+
+		good_label = (maxs > high_thresh)
+		good_label = numpy.append([False, ], good_label)
+		numgood = good_label.sum()
+		if numgood == numlabels and high_thresh > minThresh:
+			print "ERROR"
+			maxs.sort()
+			print high_thresh
+			print maxs[:3], maxs[-3:]
+			print maxs[0], ">", high_thresh, "=", maxs[0] > high_thresh
+			good_label = numpy.zeros((numlabels+1,), dtype=numpy.bool)
+			good_label[1:] = maxs > high_thresh
+			print good_label[:3], good_label[-3:]
+			time.sleep(10)
+
 		newedge_map = good_label[labels]
 		#for i in range(len(maxs)):
 		#	#if max(mag[labels==i]) < high_thresh:
@@ -122,15 +139,26 @@ def canny_edges(image, sigma=1.0, low_thresh=50, high_thresh=100):
 		#		edge_map[labels==i] = False
 		edge_count = newedge_map.sum()
 
-		print "num edges=%d, (thresh=%.3f) time=%.6f"%(edge_count, high_thresh, time.time() - t0)
-		if (edge_count > maxedges):
-			high_thresh *= 1.5
-		elif (edge_count < minedges):
-			high_thresh /= 1.5
+		print "canny edges=%d, (thresh=%.3f) time=%.6f"%(edge_count, high_thresh, time.time() - t0)
+
+		if edge_count > maxedges:
+			rand = math.sqrt(random.random())
+			new_thresh = high_thresh / rand
+			# fix for too large values
+			#print rand, new_thresh
+			if new_thresh < 1.0:
+				high_thresh = new_thresh
+			else:
+				high_thresh = math.sqrt(high_thresh)
+		elif edge_count < minedges and high_thresh > minThresh:
+			rand = math.sqrt(random.random())
+			new_thresh = high_thresh * rand
+			#print rand, new_thresh, minThresh
+			high_thresh = new_thresh
 		else:
 			break
 
-	print time.time() - t0
+	#print time.time() - t0
 	return newedge_map
 
 
@@ -141,7 +169,7 @@ if __name__ == "__main__":
 	
 	high_thresh = 0.5
 	low_thresh = 0.1*high_thresh
-	blur = 3
+	blur = 6
 	e = canny_edges(lena, blur, low_thresh, high_thresh)
 
 	pyplot.imshow(e)
--- appion/appionlib/apCtf/ransac.py
+++ appion/appionlib/apCtf/ransac.py
@@ -5,54 +5,125 @@ import time
 import math
 import numpy
 import random
-from appionlib.apCtf import ctftools
 from pyami import ellipse
+from scipy.ndimage import filters
+from appionlib.apCtf import ctftools
+from matplotlib import pyplot
 
-def ellipseRANSAC(edgeMap, ellipseThresh=2.0, minPercentGoodPoints=0.001, certainProb=0.9, maxiter=10000):
+#=========================
+def trimZeroEdges(oldarray):
+	"""
+	takes a 2D array an cuts out the center
+	that is removes zero filled edges
+
+	this should help speed up the ellipse RANSAC search
+	"""
+	columnvals = oldarray.sum(0)
+	rowvals = oldarray.sum(0)
+	## reverse and add
+	columnvals = columnvals + columnvals[::-1]
+	rowvals = rowvals + rowvals[::-1]
+	## find first non-zero value
+	firstColumnValue = numpy.nonzero(rowvals)[0][0]
+	firstRowValue = numpy.nonzero(rowvals)[0][0]
+	cutsize = min(firstColumnValue,firstRowValue)-4
+	newshape = numpy.array(oldarray.shape, dtype=numpy.uint16) - 2*cutsize
+	#print "resize", oldarray.shape, newshape
+	cutarray = frame_cut(oldarray, newshape)
+	return cutarray
+
+
+#=================
+def printParams(params):
+	a = params['a']
+	b = params['b']
+	alpha = params['alpha']
+	print ("ellip: %d x %d < %.1f deg"%
+		(a, b, alpha*180/math.pi))
+	return
+
+#=========================
+def frame_cut(a, newshape):
+	"""
+	clips image, similar to EMAN1's proc2d clip=X,Y
+	
+	>>> a = num.arange(16, shape=(4,4))
+	>>> frame_cut(a, (2,2))
+	array(
+			[[5,  6],
+		   [9, 10]])
+	"""
+	mindimx = int( (a.shape[0] / 2.0) - (newshape[0] / 2.0) )
+	maxdimx = int( (a.shape[0] / 2.0) + (newshape[0] / 2.0) )
+	mindimy = int( (a.shape[1] / 2.0) - (newshape[1] / 2.0) )
+	maxdimy = int( (a.shape[1] / 2.0) + (newshape[1] / 2.0) )
+	return a[mindimx:maxdimx, mindimy:maxdimy]
+
+#=========================
+def ellipseRANSAC(edgeMap, ellipseThresh=3, minPercentGoodPoints=0.001, certainProb=0.9, maxiter=5000):
 	"""
 	takes 2D edge map from image and trys to find a good ellipse in the data
 	"""
+	shrinkEdgeMap = numpy.float64(trimZeroEdges(edgeMap))
+	#shrinkEdgeMap = numpy.float64(edgeMap)
+	shape = shrinkEdgeMap.shape
+
 	## make a list of edges, with x, y radii
-	bottomEdgeMap = numpy.copy(edgeMap)
-	bottomEdgeMap[:edgeMap.shape[0]/2,:] = 0
-	topEdgeMap = numpy.copy(edgeMap)
-	topEdgeMap[edgeMap.shape[0]/2:,:] = 0
-	rightEdgeMap = numpy.copy(edgeMap)
-	rightEdgeMap[:,:edgeMap.shape[1]/2] = 0
-	leftEdgeMap = numpy.copy(edgeMap)
-	leftEdgeMap[:,edgeMap.shape[1]/2:] = 0
-	
-	center = numpy.array(edgeMap.shape, dtype=numpy.float64)/2.0 - 0.5
-	edgeList = numpy.array(numpy.where(edgeMap), dtype=numpy.float64).transpose() - center
+	bottomEdgeMap = numpy.copy(shrinkEdgeMap)
+	bottomEdgeMap[:shape[0]/2,:] = 0
+	topEdgeMap = numpy.copy(shrinkEdgeMap)
+	topEdgeMap[shape[0]/2:,:] = 0
+	rightEdgeMap = numpy.copy(shrinkEdgeMap)
+	rightEdgeMap[:,:shape[1]/2] = 0
+	leftEdgeMap = numpy.copy(shrinkEdgeMap)
+	leftEdgeMap[:,shape[1]/2:] = 0
+
+	#sort edges into quadrants and choose that way
+	center = numpy.array(shape, dtype=numpy.float64)/2.0 - 0.5
+	edgeList = numpy.array(numpy.where(shrinkEdgeMap), dtype=numpy.float64).transpose() - center
 	bottomEdgeList = numpy.array(numpy.where(bottomEdgeMap), dtype=numpy.float64).transpose() - center
 	topEdgeList = numpy.array(numpy.where(topEdgeMap), dtype=numpy.float64).transpose() - center
 	rightEdgeList = numpy.array(numpy.where(rightEdgeMap), dtype=numpy.float64).transpose() - center
 	leftEdgeList = numpy.array(numpy.where(leftEdgeMap), dtype=numpy.float64).transpose() - center
 
-
-
-	#might be better to sort edges into quadrants and choose that way
-
-	numEdges = edgeMap.sum()
+	numEdges = shrinkEdgeMap.sum()
 	if numEdges != len(edgeList):
 		print "something weird in array sizes"
 		return None
 
 	numSamples = 4
-	mostGoodPoints = 0
-	maxDistFromCenter = math.hypot(edgeMap.shape[0], edgeMap.shape[1])/10.0
-	inscribedCircleArea = edgeMap.shape[0] * edgeMap.shape[1] / 4
-	iternum = 0
-	bestEllipseParams = None # {'center':(x,y), 'a':a, 'b':b, 'alpha':radians}
+
+	## count rejects
 	areaReject = 0
 	distReject = 0
+	ratioReject = 0
+	sizeReject = 0
+	validEllipse = 0
+
+	## reject criteria
+	maxDistFromCenter = math.hypot(shape[0], shape[1])/10.0
+	squareArea = max(edgeMap.shape)**2/4 #quarter of the image, little less than indcribed circle
+	maxShrinkRadius = int(max(shape)*0.7) #to the corner of shrink image
+	maxNormRadius = max(edgeMap.shape)/2 #to the side of full image
+	maxRadius = min(maxShrinkRadius, maxNormRadius)
 	minGoodPoints = minPercentGoodPoints*numEdges
+	finalMinEdges = numEdges*(1.0 - math.exp( math.log(1.0-certainProb)/maxiter ) )**(1.0/numSamples)
+
+	mostGoodPoints = 0
+	bestEllipseParams = None # {'center':(x,y), 'a':a, 'b':b, 'alpha':radians}
+	iternum = 0
 	t0 = time.time()
 	while iternum <= maxiter:
 		iternum += 1
 
 		currentProb = 1.0 - math.exp( math.log(1.0-certainProb)/iternum )
 		currentProb = currentProb**(1.0/numSamples)
+
+		if iternum % 500 == 0:
+			print ("RANSAC iter=%d/%d, points=%d, need=%d->%d, bestProb=%.1f, timePer=%.4f"
+				%(iternum, maxiter, mostGoodPoints, currentProb*numEdges, 
+					finalMinEdges, mostGoodPoints/float(numEdges)*100, 
+					(time.time()-t0)/float(iternum)))
 
 		## check to see if we can stop
 		if mostGoodPoints > currentProb*numEdges:
@@ -76,82 +147,163 @@ def ellipseRANSAC(edgeMap, ellipseThresh=2.0, minPercentGoodPoints=0.001, certai
 		currentEdges.append(random.choice(topEdgeList))
 		currentEdges.append(random.choice(leftEdgeList))
 		currentEdges.append(random.choice(rightEdgeList))
-		currentEdges = numpy.array(currentEdges)
+		currentEdges = numpy.array(currentEdges, dtype=numpy.int16)
 
 		# solve centered ellipse, fixed center
 		centeredParams = ellipse.solveEllipseOLS(currentEdges)
+		#print centeredParams
 		if centeredParams is None:
-			sys.stderr.write("c")
+			#sys.stderr.write("c")
 			continue
 		## check to see if ellipse has a area smaller than circle inscribed in image
 		area = centeredParams['a'] * centeredParams['b']
-		if area > inscribedCircleArea:
+		if area > squareArea:
 			areaReject += 1
-			sys.stderr.write("A%d"%(areaReject))
+			#sys.stderr.write("A%d "%(areaReject))
 			continue
+		radius = max(centeredParams['a'],centeredParams['b'])
+		if radius > maxRadius:
+			sizeReject += 1
+			#sys.stderr.write("S%d "%(sizeReject))
+			continue	
 		ratio = centeredParams['a']/float(centeredParams['b'])
-		if ratio > 6 or ratio < 0.16:
-			sys.stderr.write("R%d"%(areaReject))
+		if ratio > 2.5 or ratio < 0.4:
+			ratioReject += 1
+			#sys.stderr.write("R%d "%(ratioReject))
 			continue	
 
 		## check to see if ellipse has a circumference smaller than something
 
 		# solve general ellipse, floating center
-		## only three points, can use other Ellipse fit
+		## only a few points, can use solveEllipseGander fit
 		#generalParams = ellipse.solveEllipseB2AC(currentEdges)
 		generalParams = ellipse.solveEllipseGander(currentEdges)
-		if generalParams is None:
-			sys.stderr.write("g")
-		else:
+		if generalParams is not None:
 			## check center to see if its reasonably close to center
 			distFromCenter = math.hypot(generalParams['center'][0], generalParams['center'][1])
 			if distFromCenter > maxDistFromCenter:
 				distReject += 1
-				sys.stderr.write("D%d"%(distReject))
+				#sys.stderr.write("D%d "%(distReject))
 				continue
 			## check to see if ellipse has a area smaller than circle inscribed in image
 			area = math.pi * centeredParams['a'] * centeredParams['b']
-			if area > inscribedCircleArea:
+			if area > squareArea:
 				areaReject += 1
-				sys.stderr.write("A%d"%(areaReject))
+				#sys.stderr.write("a%d "%(areaReject))
 				continue
-		
-		## create a larger and smaller ellipse intersect them to create a fit area
-		ellipseMap = generateEllipseRangeMap(centeredParams, ellipseThresh, edgeMap.shape)
+			radius = max(centeredParams['a'],centeredParams['b'])
+			if radius > maxRadius:
+				sizeReject += 1
+				#sys.stderr.write("s%d "%(sizeReject))
+				continue	
+			ratio = centeredParams['a']/float(centeredParams['b'])
+			if ratio > 6 or ratio < 0.16:
+				ratioReject += 1
+				#sys.stderr.write("r%d "%(ratioReject))
+				continue	
+
+		## create an outline of the ellipse
+		ellipseMap = generateEllipseRangeMap2(centeredParams, ellipseThresh, shape)
 
 		## take overlap of edges and fit area to determine number of good points
-		goodPoints = numpy.logical_and(edgeMap, ellipseMap).sum()
+		goodPoints = (shrinkEdgeMap*ellipseMap).sum()
 
-		if goodPoints > mostGoodPoints or iternum%100 == 0:
-			print ("\ngood points=%d (best=%d; need=%d, bestProb=%.1f, iter=%d, timePer=%.3f)"
-				%(goodPoints, mostGoodPoints, currentProb*numEdges, 
-					mostGoodPoints/float(numEdges)*100, iternum, (time.time()-t0)/float(iternum)))
+		#if goodPoints > mostGoodPoints or iternum%100 == 0:
+		#	print ("\ngood points=%d (best=%d; need=%d->%d, bestProb=%.1f, iter=%d/%d, timePer=%.4f)"
+		#		%(goodPoints, mostGoodPoints, currentProb*numEdges, finalMinEdges,
+		#			mostGoodPoints/float(numEdges)*100, iternum, maxiter, 
+		#			(time.time()-t0)/float(iternum)))
+
+		validEllipse += 1
 
 		if goodPoints < minGoodPoints or goodPoints < mostGoodPoints:
-			sys.stderr.write("F")
+			#sys.stderr.write(".")
 			continue
 
 		if goodPoints > mostGoodPoints:
-			bestEllipseParams = centeredParams
-			mostGoodPoints = goodPoints
+			### refine the ellipse parameters to see if we can do better
+			centeredEllipseMap1 = generateEllipseRangeMap2(centeredParams, ellipseThresh, shape)
+			centeredEllipseList1 = numpy.array(numpy.where(centeredEllipseMap1), dtype=numpy.float64).transpose() - center
+			betterEllipseParams1 = ellipse.solveEllipseOLS(centeredEllipseList1)
+			betterEllipseMap1 = generateEllipseRangeMap2(betterEllipseParams1, ellipseThresh, shape)
+			betterGoodPoints1 = (shrinkEdgeMap*betterEllipseMap1).sum()
+			centeredEllipseMap2 = generateEllipseRangeMap2(centeredParams, ellipseThresh*8, shape)
+			centeredEllipseList2 = numpy.array(numpy.where(centeredEllipseMap2), dtype=numpy.float64).transpose() - center
+			betterEllipseParams2 = ellipse.solveEllipseOLS(centeredEllipseList2)
+			betterEllipseMap2 = generateEllipseRangeMap2(betterEllipseParams2, ellipseThresh, shape)
+			betterGoodPoints2 = (shrinkEdgeMap*betterEllipseMap2).sum()
+			#print goodPoints, betterGoodPoints1, betterGoodPoints2
+
+			if betterGoodPoints1 > goodPoints and betterGoodPoints1 > betterGoodPoints2:
+				bestEllipseParams = betterEllipseParams1
+				mostGoodPoints = betterGoodPoints1
+			elif betterGoodPoints2 > goodPoints:
+				bestEllipseParams = betterEllipseParams2
+				mostGoodPoints = betterGoodPoints2
+			else:
+				bestEllipseParams = centeredParams
+				mostGoodPoints = goodPoints
 
 	### end loop and do stuff
+	#bestEllipseMap = generateEllipseRangeMap2(bestEllipseParams, ellipseThresh*1.5, edgeMap.shape)
+	#bestEllipseList = numpy.array(numpy.where(bestEllipseMap), dtype=numpy.float64).transpose() - center
+	#betterEllipseParams = ellipse.solveEllipseOLS(bestEllipseList)
+
+	print "total iterations", iternum
+	print "\tpercent good points %.1f"%(mostGoodPoints/float(numEdges)*100)
+	print "\ttime per iter %.2f ms"%(1000.*(time.time()-t0)/float(iternum))
+	print "\tvalid ellipses", validEllipse
+	print "\toff-center rejects", distReject
+	print "\tlarge area rejects", areaReject
+	print "\tlarge radius rejects", sizeReject
+	print "\toblong ratio rejects", ratioReject
+	printParams(bestEllipseParams)
+
+
 	return bestEllipseParams
 
-#=================
-def drawFilledEllipse(shape, a, b, alpha):
-	'''
-	Generate a zero initialized image array with a filled ellipse
-	drawn by setting pixels to 1.
 
-	see also imagefun.filled_circle
+#=================
+def generateEllipseRangeMap2(ellipseParams, ellipseThresh, shape):
 	'''
-	ellipratio = a/float(b)
-	### this step is TOO SLOW
-	radial = ctftools.getEllipticalDistanceArray(ellipratio, math.degrees(alpha), shape)
-	meanradius = math.sqrt(a*b)
-	filledEllipse = numpy.where(radial > meanradius, False, True)
-	return filledEllipse
+	make an elliptical ring of width 1 based on ellipseParams
+	'''
+	a = ellipseParams['a']
+	b = ellipseParams['b']
+	alpha = ellipseParams['alpha']
+
+	maxradius = max(a,b)
+	numpoints = int(math.ceil(6.28*maxradius))
+
+	ellipseRange = numpy.zeros(shape, dtype=numpy.float64)
+
+	center = numpy.array(shape, dtype=numpy.float64)/2.0
+	points = ellipse.generate_ellipse(a, b, alpha, center=center, numpoints=numpoints, integers=True)
+
+	#remove negative coordinates, off screen
+	if points.min() < 0:
+		mins = points.min(1)
+		abovezero = numpy.where(mins > 0)
+		points = points[abovezero]
+
+	#remove too large coordinates, off screen
+	if points.max() > min(shape)-1:
+		maxs = points.max(1)
+		belowedge = numpy.where(maxs < min(shape)-1)
+		points = points[belowedge]
+
+	ellipseRange[points[:,0],points[:,1]] = 1.0
+	ellipseRange = filters.maximum_filter(ellipseRange, size=ellipseThresh)
+	ellipseRange2 = filters.maximum_filter(ellipseRange, size=ellipseThresh*2)
+	ellipseRange = ellipseRange + ellipseRange2*0.1
+
+	#pyplot.clf()
+	#pyplot.imshow(ellipseRange)
+	#pyplot.gray()
+	#pyplot.show()
+	#time.sleep(1)
+
+	return ellipseRange
 
 #=================
 def generateEllipseRangeMap(ellipseParams, ellipseThresh, shape):
@@ -166,6 +318,22 @@ def generateEllipseRangeMap(ellipseParams, ellipseThresh, shape):
 	return ellipseRange
 
 #=================
+def drawFilledEllipse(shape, a, b, alpha):
+	'''
+	Generate a zero initialized image array with a filled ellipse
+	drawn by setting pixels to 1.
+
+	see also imagefun.filled_circle
+	'''
+	raise DeprecationWarning
+	ellipratio = a/float(b)
+	### this step is TOO SLOW
+	radial = ctftools.getEllipticalDistanceArray(ellipratio, math.degrees(alpha), shape)
+	meanradius = math.sqrt(a*b)
+	filledEllipse = numpy.where(radial > meanradius, False, True)
+	return filledEllipse
+
+#=================
 #=================
 if __name__ == "__main__":
 	from scipy.misc import lena
@@ -177,12 +345,18 @@ if __name__ == "__main__":
 	edgeMapInv = numpy.flipud(numpy.fliplr(edgeMap))
 	edgeMap = numpy.logical_or(edgeMap,edgeMapInv)
 
+	thresh = 3
+
 	t0 = time.time()
 	ellipseParams = ellipseRANSAC(edgeMap)
 	print time.time()-t0, "seconds"
 
-	filled = generateEllipseRangeMap(ellipseParams, 2, edgeMap.shape)
-	filled = 2*edgeMap + filled
+	if ellipseParams is None:
+		raise
+
+	filled1 = generateEllipseRangeMap2(ellipseParams, thresh, edgeMap.shape)
+	filled2 = generateEllipseRangeMap2(ellipseParams, thresh*3, edgeMap.shape)
+	filled = edgeMap + filled2 - filled1
 	pyplot.imshow(filled)
 	pyplot.gray()
 	pyplot.show()
