699b1678dfa76e06f701578f44a450b9a25790b9
Dmitry Lyumkis <dlyumkis>
2013-08-10 22:58:21 +0000

    added function

--- appion/appionlib/apFrealign.py
+++ appion/appionlib/apFrealign.py
@@ -958,3 +958,42 @@ def combine_frealign8_parfiles(outparfile, *parfiles):
 				% (total, psi, theta, phi, shx, shy, mag, micnum, dx, dy, ast))
 	ff.close()
 
+def split_frealign9_parfile(inparfile, *outparfiles):
+	''' takes as input a frealign9 parameter file, and splits up the occupancies into however many
+	output parameter files one specifies (e.g. 2). Keeps all other input the same '''
+
+	if len(outparfiles) < 1:
+		apDisplay.printError("must specify at least one output parameter file")
+
+	### read params
+	params = parseFrealign9ParamFile(inparfile)
+
+	for outfile in outparfiles:
+		### output file
+		ff = open(outfile, "w")
+		ff.write("%s%8s%8s%8s%10s%10s%8s%6s%9s%9s%8s%8s%13s%8s%8s\n" \
+			% ("C      ","PSI","THETA","PHI","SHX","SHY","MAG","FILM","DF1","DF2","ANGAST","OCC","-LogP","SCORE","CHANGE"))
+		### read & write params
+		for i,p in params.iteritems():	
+			if i % 1000 == 0:
+				print "finished %d particles" % i
+			partnum = float(p['partnum'])
+			psi = float(p['psi'])
+			theta = float(p['theta'])
+			phi = float(p['phi'])
+			shx = float(p['shiftx'])
+			shy = float(p['shifty'])
+			mag = float(p['mag'])
+			film = float(p['micn'])
+			dx = float(p['defx'])
+			dy = float(p['defy'])
+			ast = float(p['astig'])
+			occ = float(p['occ']) / len(outparfiles)
+			logp = float(p['logp']) 
+			score = float(p['score'])
+			change = float(p['change'])
+
+			ff.write("%7d%8.2f%8.2f%8.2f%10.2f%10.2f%8d%6d%9.1f%9.1f%8.2f%8.2f%13d%8.2f%8.2f\n" \
+				% (partnum, psi, theta, phi, shx, shy, mag, film, dx, dy, ast, occ, logp, score, change))
+		ff.close()
+		
