3dc830a0a633385fda5cde36d067cb9d3331ca78
Anchi Cheng <acheng>
2014-04-15 17:34:22 +0000

    refs #2681 frame configfile making module

---
+++ pyscope/falconframe.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+import math
+import sys
+import xml.dom.minidom
+import xml.etree.ElementTree as et
+
+class FalconFrameConfigXmlMaker(object):
+	def __init__(self):
+		self.frame_time = 0.055771
+		self.frame_path = 'E:\\frames'
+
+	def setFrameTime(self,second):
+		sefl.frame_time = second
+
+	def setFramePath(self,path):
+		self.frame_path = path
+
+	def setFrameRange(self,exposure_time,frame_offset=1):
+		'''
+		frame number range to output.  Frame 0  is the first frame with
+		shutter roll-in.
+		'''
+		total_frames = 7
+		end_frames = []
+
+		nframes = int(exposure_time / self.frame_time)
+		# avoid roll-in
+		equal_step = (nframes-frame_offset) / total_frames
+		spare_frames = nframes - equal_step * total_frames - frame_offset
+		frames_in_step = []
+		print nframes,spare_frames
+		if equal_step > 0:
+			frames_in_step = total_frames * [equal_step,]
+			for i in range(1,spare_frames+1):
+				frames_in_step[-1*i] += 1
+		else:
+			frames_in_step = nframes * [1,]
+		print frames_in_step
+		end_frames = map((lambda x:sum(frames_in_step[:x+1])),range(len(frames_in_step)))
+		start_frames = map((lambda x:end_frames[x]+1),range(len(end_frames)-1))
+		start_frames.insert(0,frame_offset)
+		return start_frames, end_frames
+
+	def writeConfigXml(self,start_frames,end_frames):
+		rt = et.Element('IntermediateConfig')
+		sp = et.SubElement(rt,'StoragePath')
+		sp.text = self.frame_path
+		ifb = {}
+		for i in range(len(start_frames)):
+			ifb[i] = et.SubElement(rt,'InterFrameBoundary')
+			start = self.frame_time * (start_frames[i]+0.5)
+			end = self.frame_time * (end_frames[i]+0.5)
+			ifb[i].text = '%.3f - %.3f' % (start, end)
+		# pretty print
+		roughstr =  et.tostring(rt,'utf-8')
+		reparsed = xml.dom.minidom.parseString(roughstr)
+		xmlstr = reparsed.toprettyxml(indent="\t",newl='\n',encoding="utf-8")
+		print xmlstr
+		f = open('IntermediateConfig.xml','w')
+		f.write(xmlstr)
+		f.close()
+
+	def getFrames(self):
+		return range(1,8,1)
+
+	def makeConfigFromExposureTime(self,second):
+		start_frames,end_frames = self.setFrameRange(second,1)
+		app.writeConfigXml(start_frames,end_frames)
+
+if __name__ == '__main__':
+		if len(sys.argv) != 2:
+			print 'usage: falconframe.py exposure_time_in_second'
+			sys.exit()
+		app = FalconFrameConfigXmlMaker()
+		exposure_second = float(sys.argv[1])
+		app.makeConfigFromExposureTime(exposure_second)
