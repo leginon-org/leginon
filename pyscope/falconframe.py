94455b7c7021facd31124b771614bc63d6efd14d
Anchi Cheng <acheng>
2014-04-18 06:05:18 +0000

    refs #2681 handle exposure time frame binning validation

--- pyscope/falconframe.py
+++ pyscope/falconframe.py
@@ -6,7 +6,15 @@ import xml.dom.minidom
 import xml.etree.ElementTree as et
 
 class FalconFrameConfigXmlMaker(object):
+	'''
+	Create Falcon IntermediateConfig.xml for given exposure time
+	either when frames to be saved and not.
+	The raw frames are put in limited number of bins.
+	'''
 	def __init__(self):
+		'''
+		i/o time unit is second
+		'''
 		self.base_frame_time = 0.055771
 		self.frame_time = 0.055771
 		self.format_version = 1.0
@@ -14,7 +22,14 @@ class FalconFrameConfigXmlMaker(object):
 		self.base_frame_path = 'E:\\frames'
 		self.frame_path = 'E:\\frames'
 		self.configxml_path = 'C:\Titan\Data\Falcon'
-		self.total_output_frames = 7
+		# Falcon 2 software can save frames in at most 7 bins
+		self.output_bin_limit = 7
+		self.output_bins = self.output_bin_limit + 0
+		# Readout delay of one means frame 0 (shutter roll-in) is  not readout
+		self.frame_readout_delay = 1
+
+	def getNumberOfBinLimit(self):
+		return self.output_bin_limit
 
 	def setFrameTime(self,second):
 		self.frame_time = second
@@ -28,35 +43,93 @@ class FalconFrameConfigXmlMaker(object):
 	def getFramePath(self):
 		return self.frame_path
 
-	def setNFrames(self,nframes):
-		self.total_output_frames = nframes
+	def validateExposureTime(self,second):
+		'''
+		Exposure time in second is valid only if it allows one frame to be
+		saved after readout delay.
+		'''
+		if second < self.base_frame_time * (self.frame_readout_delay - 0.5):
+			return False
+		return True
 
-	def getNFrames(self):
-		return self.total_output_frames
+	def setExposureTime(self,second):
+		'''
+		Set Exposure Time in second.  The time must be validated first.
+		'''
+		self.exposure_time = second
 
-	def setFrameRange(self,exposure_time,frame_offset=1):
+	def setMaxNumberOfFrameBins(self,nframes):
+		'''
+		Set maximal bins to output frames
+		'''
+		self.output_bins = min(self.output_bin_limit,nframes)
+
+	def getNumberOfFrameBins(self):
+		'''
+		Get number of frame bins saved without running self.distributeFramesInBins()
+		Exposure time must be set first.
+		'''
+		available_nframes = self.getNumberOfAvailableFrames()
+		usable_nframes = available_nframes -self.frame_readout_delay
+		return min(self.output_bins,max(usable_nframes,1))
+
+	def setFrameReadOutDelay(self,value=1):
+		self.frame_readout_delay = value
+
+	def getFrameReadOutDelay(self):
+		return self.frame_readout_delay
+
+	def getNumberOfAvailableFrames(self):
+		'''
+		Available frames include the offset frames since exposure time does not include that.
+		'''
+		half_frame_time = self.base_frame_time / 2.0
+		return int((self.exposure_time + half_frame_time) / self.base_frame_time) + self.getFrameReadOutDelay()
+
+	def distributeFramesInBins(self):
+		'''
+		return list of number of frames in each output bin.
+		'''
+		output_bins = self.output_bins
+
+		available_nframes = self.getNumberOfAvailableFrames()
+		# usable number of frames does not include those not read
+		usable_nframes = available_nframes -self.frame_readout_delay
+		return self._distributeItemsInBins(usable_nframes,output_bins)
+
+	def _distributeItemsInBins(self,n_items,bins):
+		'''
+		Distribute items in bins as evenly as possible.
+		Fill spare items from the end
+		'''
+		# keep bin as equally occupied as possible
+		equal_step = n_items / bins
+		spare_frames = n_items - equal_step * bins
+		n_items_in_bins = []
+		if equal_step > 0:
+			n_items_in_bins = bins * [equal_step,]
+			# spare items are filled-in from the last bins
+			for i in range(1,spare_frames+1):
+				n_items_in_bins[-1*i] += 1
+		else:
+			# not enough items to fill all bins
+			n_items_in_bins = n_items * [1,]
+		return n_items_in_bins
+
+	def setFrameRange(self):
 		'''
 		frame number range to output.  Frame 0  is the first frame with
 		shutter roll-in.
 		'''
-		total_output_frames = self.total_output_frames
+		nframe_in_bins = self.distributeFramesInBins()
 		end_frames = []
-
-		# available number of frames
-		available_nframes = int(exposure_time / self.base_frame_time)
-		# avoid roll-in
-		equal_step = (available_nframes-frame_offset) / total_output_frames
-		spare_frames = available_nframes - equal_step * total_output_frames - frame_offset
-		frames_in_step = []
-		if equal_step > 0:
-			frames_in_step = total_output_frames * [equal_step,]
-			for i in range(1,spare_frames+1):
-				frames_in_step[-1*i] += 1
+		if self.frame_readout_delay == 0:
+			return [0],[0]
 		else:
-			frames_in_step = total_output_frames * [1,]
-		end_frames = map((lambda x:sum(frames_in_step[:x+1])),range(len(frames_in_step)))
-		start_frames = map((lambda x:end_frames[x]+1),range(len(end_frames)-1))
-		start_frames.insert(0,frame_offset)
+			end_frames = map((lambda x:sum(nframe_in_bins[:x+1])),range(len(nframe_in_bins)))
+			start_frames = map((lambda x:end_frames[x]+1),range(len(end_frames)-1))
+			start_frames.insert(0,self.frame_readout_delay)
+		self.output_bins = len(start_frames)
 		return start_frames, end_frames
 
 	def writeConfigXml(self,start_frames,end_frames):
@@ -82,27 +155,43 @@ class FalconFrameConfigXmlMaker(object):
 	def getFrames(self):
 		return range(1,8,1)
 
-	def makeConfigFromExposureTime(self,second):
-		start_frames,end_frames = self.setFrameRange(second,1)
+	def makeConfigXML(self):
+		start_frames,end_frames = self.setFrameRange()
 		self.writeConfigXml(start_frames,end_frames)
 
 	def makeRealConfigFromExposureTime(self,second):
-		self.setNFrames(7)
+		'''
+		Make Useful Frame saving config.xml.
+		Minimal is 2 base_frame_time
+		'''
+		self.setMaxNumberOfFrameBins(7)
+		self.setFrameReadOutDelay(1)
+		status = self.validateExposureTime(second)
+		if status is False:
+			return False
+		self.setExposureTime(second)
 		self.setFramePath(self.base_frame_path)
-		self.makeConfigFromExposureTime(second)
+		self.makeConfigXML()
+		return True
 
-	def makeDummyConfig(self):
+	def makeDummyConfig(self,second):
 		'''
 		Non-frame saving can be achieved by saving
 		the frames to a directory under a non-existing
 		directory
 		'''
-		self.setNFrames(1)
+		self.setMaxNumberOfFrameBins(1)
+		self.setFrameReadOutDelay(1)
+		status = self.validateExposureTime(second)
+		if status is False:
+			return status
+		self.setExposureTime(self.base_frame_time)
 		# self.no_save_frame_path needs to be on not existing
 		# since FalconIntermediateImage program is capable of
 		# making a directory below existing ones
 		self.setFramePath(os.path.join(self.no_save_frame_path,'dummy'))
-		self.makeConfigFromExposureTime(self.base_frame_time)
+		self.makeConfigXML()
+		return True
 
 if __name__ == '__main__':
 		if len(sys.argv) != 2:
@@ -112,6 +201,6 @@ if __name__ == '__main__':
 		else:
 			exposure_second = float(sys.argv[1])
 		app = FalconFrameConfigXmlMaker()
-		#app.makeDummyConfig()
-		app.setNFrames(7)
-		app.makeConfigFromExposureTime(exposure_second)
+		#is_success = app.makeDummyConfig(exposure_second)
+		is_success = app.makeRealConfigFromExposureTime(exposure_second)
+		print is_success
