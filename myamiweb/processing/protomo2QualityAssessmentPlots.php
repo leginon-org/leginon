f97b019993b7ffd8f744b6bf44d90059f5839131
ajn10d <ajn10d>
2015-07-18 18:01:08 +0000

    Updated plots, refs #2834

---
+++ myamiweb/processing/protomo2QualityAssessmentPlots.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ *	The Leginon software is Copyright 2003 
+ *	The Scripps Research Institute, La Jolla, CA
+ *	For terms of the license agreement
+ *	see  http://ami.scripps.edu/software/leginon-license
+ */
+
+require_once dirname(__FILE__).'/../config.php';
+require_once "inc/path.inc";
+require_once "inc/leginon.inc";
+require_once "inc/processing.inc";
+require_once "inc/viewer.inc";
+require_once "inc/project.inc";
+require_once "inc/appionloop.inc";
+require_once "inc/particledata.inc";
+
+$outdir=$_GET['outdir'];
+$runname=$_GET['runname'];
+$tiltseries=$_GET['tiltseries'];
+
+$qa_gif_file = "$outdir/$runname/media/quality_assessment/series".sprintf('%04d',$tiltseries)."_quality_assessment.gif";
+$azimuth_gif_files = "$outdir/$runname/media/angle_refinement/series".sprintf('%04d',$tiltseries)."_azimuth.gif";
+$theta_gif_files = "$outdir/$runname/media/angle_refinement/series".sprintf('%04d',$tiltseries)."_theta.gif";
+
+$qa_gif = "loadimg.php?rawgif=1&filename=".$qa_gif_file;
+$azimuth_gif = "loadimg.php?rawgif=1&filename=".$azimuth_gif_files;
+$theta_gif = "loadimg.php?rawgif=1&filename=".$theta_gif_files;
+
+$html .= "
+	<center><H3><b>Tilt-Series #$tiltseries</b></H3></center>
+	<hr />";
+
+$html .= "
+	<H4><center><b>CCMS Plot</b></center></H4>";
+        
+if (isset($qa_gif_file)) {
+	$html .= '<center><img src="'.$qa_gif.'" alt="qa" /></center>
+	<hr />';
+} else {
+        $html .= "<center><b>CCMS Plot for Tilt-Series ".$tiltseries." either failed to generate or is processing</b></center>";
+}
+
+$html .= "
+	<H4><center><b>Tilt Azimuth Plot</b></center></H4>";
+        
+if (isset($azimuth_gif_files)) {
+	$html .= '<center><img src="'.$azimuth_gif.'" alt="azimuth" /></center>
+	<hr />';
+} else {
+        $html .= "<center><b>Tilt Azimuth Plot for Tilt-Series ".$tiltseries." either failed to generate or is processing</b></center>";
+}
+
+$html .= "
+	<H4><center><b>Tilt Angle Plot for Theta</b></center></H4>";
+        
+if (isset($theta_gif_files)) {
+	$html .= '<center><img src="'.$theta_gif.'" alt="theta" /></center>';
+} else {
+        $html .= "<center><b>Theta Plot for Tilt-Series ".$tiltseries." either failed to generate or is processing</b></center>";
+}
+
+
+echo $html
+?>
+</body>
+</html>
