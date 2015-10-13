fc964e47c74dc13f1a066dbd15430ccfd15980b5
Neil Voss <vossman>
2015-10-13 22:00:58 +0000

    additions and completions

--- appion/docker/centos6/startup.sh
+++ appion/docker/centos6/startup.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+/etc/init.d/mysqld start &
 vncserver -autokill :1 -name vnc -geometry 1200x800 &
 updatedb &
 /etc/init.d/httpd start &
 /etc/init.d/reduxd start &
-/etc/init.d/mysqld start &
-sleep 3
-mysql -u root < /emg/sw/docker.sql
+sleep 7
+#mysql -u root < /emg/sw/docker.sql
+#mysql_upgrade
+su appionuser
 tail -f .vnc/*:1.log
