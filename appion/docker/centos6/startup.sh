87e930d849b30e88fe95cb25e0f83bba23781daf
Neil Voss <vossman>
2015-10-19 17:55:38 +0000

    changes to allow saving of results in docker, refs #3549

--- appion/docker/centos6/startup.sh
+++ appion/docker/centos6/startup.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-/etc/init.d/mysqld start &
-vncserver -autokill :1 -name vnc -geometry 1200x800 &
-updatedb &
-/etc/init.d/httpd start &
-/etc/init.d/reduxd start &
-sleep 7
-#mysql -u root < /emg/sw/docker.sql
-#mysql_upgrade
-su appionuser
-tail -f .vnc/*:1.log
+/etc/init.d/reduxd start && echo 'reduxd' >> /var/log/startup.log
+/etc/init.d/mysqld start && echo 'mysqld' >> /var/log/startup.log
+vncserver -autokill :1 -name vnc -geometry 1200x800 && echo 'vncserver' >> /var/log/startup.log
+updatedb && echo 'updatedb' >> /var/log/startup.log
+/etc/init.d/httpd start && echo 'http' >> /var/log/startup.log
+#sleep 2s && echo 'sleep' >> /var/log/startup.log
+if [ ! -d "/emg/data/appion" ]; then
+	mysql -u root < /emg/sw/docker.sql && echo 'mysqldump' >> /var/log/startup.log
+fi
+
+#need a command that does not end to keep container alive
+tail -f .vnc/*:1.log 
