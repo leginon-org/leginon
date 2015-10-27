3e480ec60e14807fcf418c36b403ad3cfb452ca7
Anchi Cheng <acheng>
2015-10-27 03:38:14 +0000

    debug r19235 and add Ed's lastname

--- myamiweb/processing/selectParticleAlignment.php
+++ myamiweb/processing/selectParticleAlignment.php
@@ -193,7 +193,7 @@ if (!HIDE_FEATURE)
 		."&nbsp;<img src='img/external.png'> "
 		." commands to align your particles through multiple iterations of ref-free and ref-based alignments. "
 		."</p><p>\n"
-		."<font color='#aa2222'>WARNING:</font> report all problems to Ed"
+		."<font color='#aa2222'>WARNING:</font> report all problems to Ed Brignole"
 		."</p>\n";
 	//echo "  <img src='img/align-rsm.png' width='125'><br/>\n";
 	echo "</td></tr>\n";
@@ -212,6 +212,7 @@ if (!HIDE_FEATURE)
 		."refine2d.py</a> program"
 		."</p>\n";
 	echo "</td></tr>\n";
+}
 
 /*
 ** Xmipp Maximum Likelihood Reference Based Alignment
