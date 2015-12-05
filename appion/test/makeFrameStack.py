99d0cd23108197d1157f37a3943bf47de0b06d2c
Anchi Cheng <acheng>
2015-12-05 04:08:30 +0000

    refs #3810 a case that does not works

--- appion/test/makeFrameStack.py
+++ appion/test/makeFrameStack.py
@@ -3,21 +3,31 @@
 Makes a small and simple stack for testing frame alignment program
 It writes to local directory
 """
+# parameters to change
+size = (2048,2048)
+drift = 3.0
+number_of_frames = 20
+feature_size = 2
+
+
+
 import numpy
 from pyami import mrc
-size = (512,512)
-zeros = numpy.zeros(size)
-drift = 10
 
-for i in range(7):
+zeros = numpy.zeros(size)
+center_frame = number_of_frames / 2
+start_center = (size[0]/2 - center_frame, size[1]/2 - center_frame)
+
+for i in range(number_of_frames):
 	print 'writing frame %d' % (i,)
 	a = zeros * 1.0
-	a[(size[0]/2+i*drift,size[1]/2+i*drift)] = 1.0
-	a[(size[0]/2+1+i*drift,size[1]/2+i*drift)] = 1.0
-	a[(size[0]/2+i*drift,size[1]/2+1+i*drift)] = 1.0
-	a[(size[0]/2+1+i*drift,size[1]/2+1+i*drift)] = 1.0
+	for p in range(feature_size):
+		for q in range(feature_size):
+			position = (int(round(start_center[0]+p+i*drift)),int(round(start_center[1]+q+i*drift)))
+			a[position] = 5.0
+	print position
 	if i == 0:
-		mrc.write(a,'frames.mrc')
+			mrc.write(a,'frames.mrc')
 	else:
 		mrc.append(a,'frames.mrc')
 
