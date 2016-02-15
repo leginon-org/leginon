1f98835c27b37912e6f5d8cd3464b8b7d9520bea
Neil Voss <vossman>
2016-02-15 16:54:49 +0000

    Add EMAN2 to Docker install, precompiled Xmipp, refs #3549

--- appion/docker/centos6/build.sh
+++ appion/docker/centos6/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+mkdir MRC
+cd MRC
 wget -nc -c 'http://emg.nysbc.org/redmine/attachments/download/112/06jul12a_00015gr_00028sq_00004hl_00002en.mrc'
 wget -nc -c 'http://emg.nysbc.org/redmine/attachments/download/113/06jul12a_00015gr_00028sq_00023hl_00002en.mrc'
 wget -nc -c 'http://emg.nysbc.org/redmine/attachments/download/114/06jul12a_00015gr_00028sq_00023hl_00004en.mrc'
@@ -8,12 +10,18 @@ wget -nc -c 'http://emg.nysbc.org/redmine/attachments/download/116/06jul12a_0002
 wget -nc -c 'http://emg.nysbc.org/redmine/attachments/download/109/06jul12a_00022gr_00037sq_00025hl_00004en.mrc'
 wget -nc -c 'http://emg.nysbc.org/redmine/attachments/download/110/06jul12a_00022gr_00037sq_00025hl_00005en.mrc'
 wget -nc -c 'http://emg.nysbc.org/redmine/attachments/download/111/06jul12a_00035gr_00063sq_00012hl_00004en.mrc'
+cd ..
 
+mkdir TGZ
+cd TGZ
 wget -nc -c 'http://emg.nysbc.org/redmine/attachments/download/631/eman-linux-x86_64-cluster-1.9.tar.gz'
 #wget -nc -c 'http://emg.nysbc.org/redmine/attachments/download/638/spidersmall.18.10.tar.gz'
 wget -nc -c 'http://emg.nysbc.org/redmine/attachments/download/4485/spidersmall.13.00.tgz'
-wget -nc -c 'http://emg.nysbc.org/redmine/attachments/download/636/Xmipp-2.4-src.tar.gz'
+#wget -nc -c 'http://emg.nysbc.org/redmine/attachments/download/636/Xmipp-2.4-src.tar.gz'
 wget -nc -c 'http://emg.nysbc.org/redmine/attachments/download/4484/ctf_140609.tar.gz'
 wget -nc -c 'http://emg.nysbc.org/redmine/attachments/download/4483/ctffind-4.0.16-linux64.tar.gz'
+wget -nc -c 'http://emg.nysbc.org/redmine/attachments/download/5035/eman2_centos6_docker.tgz'
+wget -nc -c 'http://emg.nysbc.org/redmine/attachments/download/5034/xmipp_centos6_docker.tgz'
+cd ..
 
 docker build -t vosslab/appion .
