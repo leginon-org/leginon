5c356c6321a9e7f0adc45a802ccec4f02f968678
Neil Voss <vossman>
2016-02-19 22:27:45 +0000

    neil specific change

--- appion/docker/centos6/run.sh
+++ appion/docker/centos6/run.sh
@@ -2,6 +2,7 @@
 
 docker run -d -t \
   -v /Users/vosslab/myami/appion:/emg/sw/myami/appion \
+  -v /Users/vosslab/myami/myamiweb/processing:/emg/sw/myami/myamiweb/processing \
   -w /emg/sw/myami/appion \
   -p 80:80 -p 5901:5901 \
   vosslab/appion
