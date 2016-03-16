d6b8f78a121a82d82430aec10c6444bcbb54418e
Neil Voss <vossman>
2016-03-16 21:45:05 +0000

    starting implementation, refs #4014

--- appion/appionlib/StackClass/mrcClass.py
+++ appion/appionlib/StackClass/mrcClass.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
 import os
+from pyami import mrc
+from appionlib import apDisplay
+from appionlib.StackClass import baseClass
 
-
-class StackClass(object):
+class MrcClass(baseClass.StackClass):
 	################################################
 	# Must be implemented in new Stack subClass
 	################################################
@@ -11,10 +13,11 @@ class StackClass(object):
 		# read the header information
 		#  or initialize empty stack
 		# required variables to set are below
-		self.boxsize = None
-		self.apix = None
-		self.originalNumberOfParticles = None
-		raise NotImplementedError
+		if os.path.isfile(self.filename) and self.fileSize() > 1:
+			self.mrcheader = mrc.readHeaderFromFile(self.filename)
+			self.boxsize = self.mrcheader['nx']
+			self.apix = self.getPixelSize()
+			self.originalNumberOfParticles = self.mrcheader['nz']
 	def newFile(self):
 		# create new file to append to
 		raise NotImplementedError
@@ -27,49 +30,45 @@ class StackClass(object):
 	def appendParticlesToFile(self, particleDataTree):
 		# takes a list of 2D numpy arrays
 		#  and wrtie them to a file
-		raise NotImplementedError
+		if os.path.exists(self.filename):
+			partarray = numpy.array(particleDataTree)
+			mrc.append(partarray, self.filename)
+		else:
+			f = open(self.filename, "wb+")
+			partarray = numpy.array(particleDataTree)
+			mrc.write(partarray, f)
+			f.close()
+			apix = self.apix
+			pixeldict = {'x': apix, 'y': apix, 'z': apix, }
+			mrc.updateFilePixelSize(self.filename, pixeldict)
 	def close(self):
 		# close out file
 		# write total particles to header, etc.
-		raise NotImplementedError
+		return
 
-	################################################
-	# These functions are general should not be copied to subClasses
-	################################################
-	def __init__(self, filename):
-		self.filename = filename
-		self.readHeader()
-		self.particlesWritten = 0
-		self.particlesRead = 0
-		self.boxsize = None
-		self.apix = 1.0 # assume 1.0 apix unless specified
-		self.originalNumberOfParticles = 0
-		self.currentParticles = 0
-		self.readonly = False
-	def setPixelSize(self, apix):
-		self.apix = apix
-	def fileSize(self):
-		return int(os.stat(self.filename)[6])
 	################################################
 	# Unique functions for this class
 	################################################
-
-
+	def getPixelSize(self):
+			pixeldict = mrc.readFilePixelSize(self.filename)
+			if pixeldict['x'] == pixeldict['y'] and pixeldict['x'] == pixeldict['z']:
+				return pixeldict['x']
+			else:
+				apDisplay.printWarning("Image Stack has unknown pixel size, using 1.0 A/pixel")
+				return 1.0
 
 if __name__ == '__main__':
 	import numpy
 	# create a random stack of 4 particles with 16x16 dimensions
 	a = numpy.random.random((16,16,4))
 	# create new stack file
-	f1 = StackClass("temp.mrc")
-	# set the pixel size
-	f1.setPixelSize(1.0)
+	f1 = MrcClass("temp.mrc")
 	# save particles to file
 	f1.appendParticlesToFile(a)
 	# close stack
 	f1.close()
 	# open created stack
-	f2 = StackClass("temp.mrc")
+	f2 = MrcClass("temp.mrc")
 	# read particles in stack
 	b = f2.readParticles()
 	# create new particles from old ones
