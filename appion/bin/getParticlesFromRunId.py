fd4896ae8a1a695c593a53d61640d30d347fff27
Scott Stagg <sstagg>
2016-03-19 02:08:54 +0000

    little python tool for making a list of particle coordinates

---
+++ appion/bin/getParticlesFromRunId.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from appionlib import appiondata
+import sinedon
+import optparse
+
+def parseOptions():
+	parser=optparse.OptionParser()
+	parser.add_option('--projectid', '-p', dest='projectid', type='int', help='project id')
+	parser.add_option('--selectionrunid', dest='runid', type='int', help='run id')
+	parser.add_option('--outlist', dest='outlist', type='str', help='output file with particle coordinates')
+	options, args=parser.parse_args()
+
+	if len(args) != 0 or len(sys.argv) == 1:
+		parser.print_help()
+		sys.exit()
+	
+	return options
+
+if __name__ == "__main__":
+	options=parseOptions()
+	sinedon.setConfig('appiondata',db=('ap%d' % options.projectid))
+	q=appiondata.ApParticleData()
+	rundata=appiondata.ApSelectionRunData.direct_query(options.runid)
+	q['selectionrun']=rundata
+	ptlcs=q.query()
+	print len(ptcls)
