ec9ed2e187659047dec8adb67a9437729353fb45
Scott Stagg <sstagg>
2016-03-19 03:21:05 +0000

    final version

--- appion/bin/getParticlesFromRunId.py
+++ appion/bin/getParticlesFromRunId.py
@@ -3,6 +3,7 @@
 from appionlib import appiondata
 import sinedon
 import optparse
+import sys
 
 def parseOptions():
 	parser=optparse.OptionParser()
@@ -23,5 +24,17 @@ if __name__ == "__main__":
 	q=appiondata.ApParticleData()
 	rundata=appiondata.ApSelectionRunData.direct_query(options.runid)
 	q['selectionrun']=rundata
-	ptlcs=q.query()
-	print len(ptcls)
+	print "Querying for particles"
+	ptcls=q.query()
+	print len(ptcls),'returned'
+	f=open(options.outlist,'w')
+	f.write('particle#\tx_coord\ty_coord\timage_name\n')
+	for ptcl in ptcls:
+		n=ptcl.dbid
+		x=ptcl['xcoord']
+		y=ptcl['ycoord']
+		name=ptcl['image']['filename']
+		f.write('%d\t%d\t%d\t%s\n' % (n, x, y,name))
+	f.close()
+	print 'Done!'
+	
