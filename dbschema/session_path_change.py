436c6c085ac0f6091543115800be35a2b3fbd99b
Anchi Cheng <acheng>
2016-04-21 18:03:00 +0000

    refs #2995 add option to change frame path, too

--- dbschema/session_path_change.py
+++ dbschema/session_path_change.py
@@ -5,9 +5,12 @@ from leginon import leginondata
 from sinedon import directq
 
 class SessionPathChange(object):
-	def __init__(self, sessionname, new_path):
+	def __init__(self, sessionname, new_path, path_type='image'):
 		if not os.path.isdir(new_path):
 			raise ValueError('New image path must exists')
+
+		if path_type not in ('image','frame'):
+			raise ValueError ('Path type must be "image" or "frame"')
 
 		# find session(s) that is named as sessionname.  It should be unique
 		results = leginondata.SessionData(name=sessionname).query()
@@ -15,30 +18,35 @@ class SessionPathChange(object):
 			raise ValueError('Found %d session data record for %s' % (len(results),sessionname))
 		self.sessiondata = results[0]
 		self.new_path = new_path
+		self.path_type = path_type
 
 	def run(self):
 		# DEF_id field in SessionData table is used to choose which row of data to update
 		sessionid = self.sessiondata.dbid
 
 		# update
-		query = "update `SessionData` set `image path` = '%s' where `SessionData`.`DEF_id`=%d" % (self.new_path, sessionid)
+		query = "update `SessionData` set `%s path` = '%s' where `SessionData`.`DEF_id`=%d" % (self.path_type, self.new_path, sessionid)
 
 		# send the query to the database linked to sinedon module named leginondata
 		directq.complexMysqlQuery('leginondata',query)
 
 if __name__=='__main__':
 
-	if len(sys.argv) != 3:
-		print 'usage: python session_path_change.py sessionname new_image_path'
+	if len(sys.argv) <= 3:
+		print 'usage: python session_path_change.py sessionname new_image_path path_type'
 		print 'new_image_path needs to lead to where the images are stored'
 		print 'For example, /data/leginon/14oct01a/rawdata, and exists'
+		print "path_type can be either image or frame"
 		sys.exit()
 
 	sessionname = sys.argv[1]
 	new_path = sys.argv[2]
-
+	if len(sys.argv) == 4:
+		path_type = sys.argv[3]
+	else:
+		path_type = 'image'
 	try:
-		app = SessionPathChange(sessionname, new_path)
+		app = SessionPathChange(sessionname, new_path, path_type)
 		app.run()
 	except ValueError, e:
 		print 'Value Error: %s' % (e.message)
