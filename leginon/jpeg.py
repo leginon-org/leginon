c2e5d08c171133d1bd2c40d21460c071b8e1c280
Anchi Cheng <acheng>
2016-06-09 21:40:52 +0000

    refs #4237 check for pil

--- leginon/jpeg.py
+++ leginon/jpeg.py
@@ -3,6 +3,16 @@
 from PIL import Image
 import numpy
 
+def getPilFromStringFuncName():
+	# PIL function name changes #4237
+	# same as in numpil but do not want to import that
+	if hasattr(Image,'frombytes'):
+		func_name = 'frombytes'
+	else:
+		func_name = 'fromstring'
+	return func_name
+
+
 def save(a, min, max, filename, quality):
 	a = numpy.clip(a, min, max)
 	scale = 255.0 / (max - min)
@@ -11,5 +21,5 @@ def save(a, min, max, filename, quality):
 	a = a.astype(numpy.uint8)
 	imsize = a.shape[1], a.shape[0]
 	nstr = a.tostring()
-	image = Image.fromstring('L', imsize, nstr, 'raw', 'L', 0, 1)
+	image = getattr(Image,getPilFromStringFuncName())('L', imsize, nstr, 'raw', 'L', 0, 1)
 	image.convert('L').save(filename, "JPEG", quality=quality)
