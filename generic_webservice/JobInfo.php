557e16de570c2035edb511e4e5ebe5a3a4b4964e
Donny <dcshrum@fsu.edu>
2016-08-09 13:16:39 -0400

    creating a generic webservice framework

--- generic_webservice/JobInfo.php
+++ generic_webservice/JobInfo.php
@@ -1,23 +1,20 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
-  The json content sent with this command:
-  { "session-data":{"processingdb":"ap461","expId":"9681","projectId":"461","processinghost":"localhost","advanced_user":"0","username":"dcshrum","password":"dcshrum","loggedin":"1" }, "command":"qstat -au dcshrum | egrep '^[0-9]'" }
+ * The json content sent with this command:
+ * {    "session-data":{"processingdb":"ap461","expId":"9681","projectId":"461",
+ *      "processinghost":"localhost","advanced_user":"0","username":"dcshrum","password":"dcshrum","loggedin":"1" }, 
+ * "command":"qstat -au dcshrum | egrep '^[0-9]'" }
  */
 class JobInfo {
 
-// processing.inc (1015):  
-// processing.inc: checkcluster jobs runs: $cmd = "qstat -u ".$user." | egrep '^[0-9]'"; returns result
-    /* */
+    // processing.inc (1015):  
+    // processing.inc: checkcluster jobs runs: $cmd = "qstat -u ".$user." | egrep '^[0-9]'"; returns result
+
     public function checkQueue($username) {
 
-        $runCmd = "/usr/bin/sudo /var/www/webservice/apache_squeue.sh \"" . $username . "\""; // &> /testing/result.txt";
+        # a script that allows apache to run an squeue command as a different user
+        $runCmd = "/usr/bin/sudo /var/www/webservice/apache_squeue.sh \"" . $username . "\""; 
         exec($runCmd, $output, $return);
 
         $value = "";
