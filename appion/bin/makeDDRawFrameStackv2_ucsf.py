0ec42468850d2e7e1124a90ab4723d4bbbfdd2c0
dlyumkis <dlyumkis@bpho-bladerunner-1.(none)>
2016-08-12 09:50:18 -0700

    for motioncorr2

--- appion/bin/makeDDRawFrameStackv2_ucsf.py
+++ appion/bin/makeDDRawFrameStackv2_ucsf.py
@@ -13,7 +13,8 @@ from leginon import ddinfo
 #appion
 from appionlib import apDDLoop
 from appionlib import apDisplay
-from appionlib import apDDprocessv2
+from appionlib import apDDprocess
+from appionlib import apDDFrameAligner
 from appionlib import apDatabase
 from appionlib import apFile
 from appionlib import apStack
@@ -23,7 +24,9 @@ class MakeFrameStackLoop(apDDLoop.DDStackLoop):
 	#=======================
 	def setupParserOptions(self):
 		super(MakeFrameStackLoop,self).setupParserOptions()
-		# Boolean
+
+		# Gain reference generation
+
 		self.parser.add_option("--rawarea", dest="rawarea", default=False,
 			action="store_true", help="use full area of the raw frame, not leginon image area")
 		self.parser.add_option("--useGS", dest="useGS", default=False,
@@ -32,71 +35,76 @@ class MakeFrameStackLoop(apDDLoop.DDStackLoop):
 			action="store_true", help="Make Aligned frame stack")
 		self.parser.add_option("--square", dest="square", default=False,
 			action="store_true", help="Output square images")
-
-#		self.parser.add_option("--defergpu", dest="defergpu", default=False,
-#			action="store_true", help="Make unaligned frame stack first on computer without gpu alignment program")
-
+		self.parser.add_option("--defergpu", dest="defergpu", default=False,
+			action="store_true", help="Make unaligned frame stack first on computer without gpu alignment program")
 		self.parser.add_option("--no-cyclechannels", dest="cyclechannels", default=True,
 			action="store_false", help="Use only one reference channel for gain/dark correction")
-		# String
-		# Integer
 		self.parser.add_option("--refimgid", dest="refimgid", type="int",
 			help="Specify a corrected image to do gain/dark correction with", metavar="INT")
-
-
 		self.parser.add_option("--trim", dest="trim", type="int", default=0,
 			help="Trim edge off after frame stack gain/dark correction", metavar="INT")
 
-		self.parser.add_option("--Group", dest="Group", type="int", default=1,
-			help="Number of frames in running average window. Default=1, can be odd or even.", metavar="INT")
-
-#		self.parser.add_option("--Bft", dest="Bft",type="float",default=100.0,
-#			help="B factor in A^2", metavar="#")
-
+		### MOTIONCORR2 PARAMS ###
+		
+		self.parser.add_option("--nrw", dest="nrw", type="int", default=1,
+			help="Number (1, 3, 5, ...) of frames in running average window (motioncorr2 parameter -Group). 0 = disabled", metavar="INT")
 
 		self.parser.add_option("--FmRef", dest="FmRef",type="int",default=0,
 			help="Specify which frame to be the reference to which all other frames are aligned. Default 0 is aligned to the first frame, other values aligns to the central frame.", metavar="#")
 
-
-
-		self.parser.add_option("--FtBin", dest="FtBin",type="float",default=1.0,
-			help="Binning performed in Fourier space, default is 1.0", metavar="#")
-
-
-
-
-		self.parser.add_option("--Iter", dest="Iter",type="int",default=7,
+		self.parser.add_option("--Iter", dest="Iter",type="int",default=5,
 			help="Maximum iterations for iterative alignment, default is 7.")
 
                 self.parser.add_option("--Tol", dest="Tol",type="float",default=0.5,
                         help="Tolerance for iterative alignment, in pixels", metavar="#")
 
-		self.parser.add_option("--Patchrows",dest="Patchrows",metavar="#",type=int,default=0,
-			help="Number of rows to be used for patch-based alignment. Default 0 corresponds to full frame alignment.")
+		self.parser.add_option("--Patch",dest="Patch",metavar="#,#",type=str,default=0,
+			help="Number of patches to be used for patch based alignment. Default 0,0 corresponds to full frame alignment.")
 
-                self.parser.add_option("--Patchcols",dest="Patchcols",metavar="#",type=int,default=0,
-                        help="Number of columns to be used for patch-based alignment. Default 0 corresponds to full frame alignment.")
+		self.parser.add_option("--MaskCent",dest="MaskCent",metavar="#,#",type=str,default="0,0",
+			help="Coordinates for center of subarea that will be used for alignment. Default 0,0 corresponds to center coordinate.")
 
-                self.parser.add_option("--MaskCentrows",dest="MaskCentrows",metavar="#",type=int,default=0,
-                        help="Row coordinate for center of subarea that will be used for alignment. Default 0 corresponds to center coordinate.")
+		self.parser.add_option("--MaskSize",dest="MaskSize",metavar="#,#",type=str,default="0,0",
+			help="The size of subarea that will be used for alignment, default 1.0 1.0 corresponding full size.")
 
-                self.parser.add_option("--MaskCentcols",dest="MaskCentcols",metavar="#",type=int,default=0,
-                        help="Column coordinate for center of subarea that will be used for alignment. Default 0 corresponds to center coordinate.")
+		self.parser.add_option("--Throw",dest="Throw",metavar="#",type=int,default=0,
+                        help="Throw initial number of frames")
 
-                self.parser.add_option("--MaskSizerows",dest="MaskSizerows",metavar="#",type=float,default=1.0,
-                        help="Row scaling value for size of subarea that will be used for alignment, default is 1.0")
+		self.parser.add_option("--Trunc",dest="Trunc",metavar="#",type=int,default=0,
+                        help="Truncate last number of frames")
 
-                self.parser.add_option("--MaskSizecols",dest="MaskSizecols",metavar="#",type=float,default=1.0,
-                        help="Column scaling value for size of subarea that will be used for alignment, default is 1.0")
+		self.parser.add_option("--doseweight",dest="doseweight",metavar="bool", default=False, 
+			action="store_true", help="dose weight the frame stack, according to Tim / Niko's curves")
+
+		self.parser.add_option("--FmDose",dest="FmDose",metavar="float",type=float,
+                        help="Frame dose in e/A^2. If not specified, will get value from database")
 
 
+#		self.parser.add_option("--FtBin", dest="FtBin",type="float",default=1.0,
+#			help="Binning performed in Fourier space, default is 1.0", metavar="#")
 
-                self.parser.add_option("--Bft", dest="Bft",type="float",default=100.0,
-                        help="B factor in A^2", metavar="#")
 
-		self.parser.add_option("--defergpu", dest="defergpu", default=False,
-                        action="store_true", help="Make unaligned frame stack first on computer without gpu alignment program")
+#		self.parser.add_option("--Bft", dest="Bft",type="float",default=100.0,
+#			help="B factor in A^2", metavar="#")
 
+
+#		self.parser.add_option("--Patchrows",dest="Patchrows",metavar="#",type=int,default=0,
+#			help="Number of rows to be used for patch-based alignment. Default 0 corresponds to full frame alignment.")
+
+#                self.parser.add_option("--Patchcols",dest="Patchcols",metavar="#",type=int,default=0,
+#                        help="Number of columns to be used for patch-based alignment. Default 0 corresponds to full frame alignment.")
+
+#                self.parser.add_option("--MaskCentX",dest="MaskCentX",metavar="#",type=int,default=0,
+#                        help="Row coordinate for center of subarea that will be used for alignment. Default 0 corresponds to center coordinate.")
+
+#                self.parser.add_option("--MaskCentY",dest="MaskCentY",metavar="#",type=int,default=0,
+#                        help="Column coordinate for center of subarea that will be used for alignment. Default 0 corresponds to center coordinate.")
+
+#                self.parser.add_option("--MaskSizerows",dest="MaskSizerows",metavar="#",type=float,default=1.0,
+#                        help="Row scaling value for size of subarea that will be used for alignment, default is 1.0")
+
+#                self.parser.add_option("--MaskSizecols",dest="MaskSizecols",metavar="#",type=float,default=1.0,
+#                        help="Column scaling value for size of subarea that will be used for alignment, default is 1.0")
 
 #		self.parser.add_option("--flp", dest="flp", type="int", default=0,
 #			help="Flip frames along Y axis. (0 = no flip, 1 = flip", metavar="INT")
@@ -142,14 +150,15 @@ class MakeFrameStackLoop(apDDLoop.DDStackLoop):
 		sessiondata = apDatabase.getSessionDataFromSessionName(self.params['sessionname'])
 		if sessiondata['frame path']:
 			# 3.0+
-			return ddinfo.getRawFrameType(sessiondata['frame path'])
+			print sessiondata
+			return ddinfo.getRawFrameType(sessiondata['image path'])
 		else:
 			# pre-3.0
 			return ddinfo.getRawFrameType(sessiondata['image path'])
 
 	#=======================
 	def preLoopFunctions(self):
-		self.dd = apDDprocessv2.initializeDDFrameprocess(self.params['sessionname'],self.params['wait'])
+		self.dd = apDDprocess.initializeDDFrameprocess(self.params['sessionname'],self.params['wait'])
 		self.dd.setUseGS(self.params['useGS'])
 		self.dd.setRunDir(self.params['rundir'])
 		self.dd.setTempDir(self.params['tempdir'])
@@ -162,7 +171,7 @@ class MakeFrameStackLoop(apDDLoop.DDStackLoop):
 		# keepstack is resolved for various cases in conflict check.  There should be no ambiguity by now
 		self.dd.setKeepStack(self.params['keepstack'])
 		self.dd.setCycleReferenceChannels(self.params['cyclechannels'])
-		self.dd.setNewNumRunningAverageFrames(self.params['Group'])
+		self.dd.setNewNumRunningAverageFrames(self.params['nrw'])
 #		self.dd.setNewFlipAlongYAxis(self.params['flp'])
 	
 		if self.params['refimgid']:
@@ -208,15 +217,23 @@ class MakeFrameStackLoop(apDDLoop.DDStackLoop):
 			self.dd.setAlignedSumFrameList(framelist)
 
 		### first remove any existing stack file
-		apFile.removeFile(self.dd.framestackpath)
-		apFile.removeFile(self.dd.tempframestackpath)
+#		apFile.removeFile(self.dd.framestackpath)
+#		apFile.removeFile(self.dd.tempframestackpath)
 		if self.dd.hasBadPixels() or not self.params['align']:
 			self.dd.setUseGPUFlat(False)
 			### make stack
-			self.dd.makeCorrectedFrameStack(self.params['rawarea'])
+#			self.dd.makeCorrectedFrameStack(self.params['rawarea'])
 		else:
 			self.dd.setUseGPUFlat(True)
-			self.dd.makeRawFrameStackForOneStepCorrectAlign(self.params['rawarea'])
+#			self.dd.makeRawFrameStackForOneStepCorrectAlign(self.params['rawarea'])
+
+		# parameters for alignment & dose fractionation
+		self.params['kv'] = self.dd.getKVFromImage(imgdata)
+		self.params['totalframes'] = self.dd.getNumberOfFrameSaved()
+		self.params['totaldose'] = apDatabase.getDoseFromImageData(imgdata)
+#		self.params['totaldose'] = apDatabase.getDoseFromSessionPresetNames(self.params['sessionname'], self.params['preset'])
+		self.params['FmDose'] = self.params['totaldose'] / self.params['totalframes']
+
 		# Align
 		if self.params['align']:
 			# make a fake log so that catchUpDDAlign will know that frame stack is done
@@ -227,24 +244,27 @@ class MakeFrameStackLoop(apDDLoop.DDStackLoop):
 			if self.params['defergpu']:
 				return
 			# Doing the alignment
+			aligner = apDDFrameAligner.MotionCorr2_UCSF()
 			if self.dd.getUseGPUFlat():
 				self.dd.gainCorrectAndAlignFrameStack()
 			else:
-				self.dd.alignCorrectedFrameStack()
+				aligner.setRunDir(self.params['rundir'])
+#				print self.dd.__dict__.keys()
+				aligner.setInputFrameStackPath(self.dd.framestackpath)
+				aligner.setFrameAlignOptions(self.params)
+				aligner.setAlignedSumPath()
+				aligner.alignFrameStack()
 			if os.path.isfile(self.dd.aligned_sumpath):
 				self.aligned_imagedata = self.dd.makeAlignedImageData(alignlabel=self.params['alignlabel'])
 				if os.path.isfile(self.dd.aligned_stackpath):
 					# aligned_stackpath exists either because keepstack is true
 					apDisplay.printMsg(' Replacing unaligned stack with the aligned one....')
-					apFile.removeFile(self.dd.framestackpath)
+#					apFile.removeFile(self.dd.framestackpath)
 					apDisplay.printMsg('Moving %s to %s' % (self.dd.aligned_stackpath,self.dd.framestackpath))
 					shutil.move(self.dd.aligned_stackpath,self.dd.framestackpath)
-			# Clean up tempdir in case of failed alignment
-			if self.dd.framestackpath != self.dd.tempframestackpath:
-				apFile.removeFile(self.dd.tempframestackpath)
-		if not self.params['keepstack']:
-			apFile.removeFile(self.dd.framestackpath)
 
+
+	#=======================
 	def insertFunctionRun(self):
 		if self.params['stackid']:
 			stackdata = apStack.getOnlyStackData(self.params['stackid'])
