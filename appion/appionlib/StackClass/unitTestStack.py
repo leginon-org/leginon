0095eb6bf933a759739fd3b64ec6330fa59b9793
Neil Voss <vossman77@yahoo.com>
2016-08-14 13:28:37 -0500

    created StackClass unit test unitTestStack.py, refs #4014

--- appion/appionlib/StackClass/unitTestStack.py
+++ appion/appionlib/StackClass/unitTestStack.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import math
 import numpy
 from appionlib.StackClass import stackTools
 from appionlib.StackClass import ProcessStack
@@ -15,8 +16,12 @@ for ext in extensions:
 	stackClass.removeStack(warn=False)
 	# 4 particles with box 128x128
 	a = numpy.random.random((4, 128, 128))
+	a = numpy.array(a, dtype=numpy.float64)
 	stackClass.writeParticlesToFile(a)
+	print "getNumberOfParticles", stackClass.getNumberOfParticles()
 	b = stackClass.readParticlesFromFile()
+	#print b.shape
+	#print b
 	squareError = ((a-b)**2).sum()
 	print "%s squareError: %.8f"%(ext, squareError)
 	average = stackTools.averageStack(filename, msg=False)
