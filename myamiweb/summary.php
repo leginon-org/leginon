b727b443d498815f6a3695420ab5e93a4de17121
Anchi Cheng <acheng@nysbc.org>
2016-09-15 22:40:59 -0400

    refs #4438 titles of the phase shift plot

--- myamiweb/summary.php
+++ myamiweb/summary.php
@@ -439,6 +439,8 @@ if ($particle->hasCtfData($sessionId)) {
 	
 	if ($bestctf) {
 		//phase shift progression
+		echo '</td></tr><tr><td>'."\n";
+		echo 'Phase shift by phase plate';
 		$phasegraph = '<a href="processing/phaseshiftgraph.php?expId='.$sessionId.'&hg=0'
 							.'&s=1'.$urlmconf.'">'
 							.'<img border="0" src="processing/phaseshiftgraph.php?w=300&s=1&expId='.$sessionId
@@ -448,6 +450,8 @@ if ($particle->hasCtfData($sessionId)) {
 		echo "<a href='processing/phaseshiftgraph.php?vd=1&s=1&hg=0&expId=$expId'>[data]</a>";
 		echo '</td></tr><tr><td>'."\n";
 
+		//Summary table
+		echo 'Summary';
 		foreach($bestctf as $field=>$data) {
 			foreach($data as $k=>$v) {
 				$preset = $bestctf[$field][$k]['name'];
