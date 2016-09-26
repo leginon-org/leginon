dc0cccb1b34500ae4fb085f2af726531e669b8f9
Anchi Cheng <acheng@nysbc.org>
2016-09-26 17:03:59 -0400

    Revert "refs #4472 get method, too" since apDDprocess rework is not on branch
    This reverts commit 9586696a67150d23f3a7f80515328f24b1d6cf3f.

--- appion/bin/makeDDRawFrameStack.py
+++ appion/bin/makeDDRawFrameStack.py
@@ -103,7 +103,7 @@ class MakeFrameStackLoop(apDDLoop.DDStackLoop):
 		self.dd.setRunDir(self.params['rundir'])
 		self.dd.setTempDir(self.params['tempdir'])
 		self.dd.setRawFrameType(self.getFrameType())
-		self.dd.setUseFrameAlignerFlat(True)
+		self.dd.getUseFrameAlignerFlat(True)
 		self.dd.setSquareOutputShape(self.params['square'])
 		self.dd.setTrimingEdge(self.params['trim'])
 		self.dd.setDoseFDriftCorrOptions(self.params)
@@ -160,11 +160,11 @@ class MakeFrameStackLoop(apDDLoop.DDStackLoop):
 		apFile.removeFile(self.dd.framestackpath)
 		apFile.removeFile(self.dd.tempframestackpath)
 		if self.dd.hasBadPixels() or not self.params['align']:
-			self.dd.setUseFrameAlignerFlat(False)
+			self.dd.getUseFrameAlignerFlat(False)
 			### make stack
 			self.dd.makeCorrectedFrameStack(self.params['rawarea'])
 		else:
-			self.dd.setUseFrameAlignerFlat(True)
+			self.dd.getUseFrameAlignerFlat(True)
 			self.dd.makeRawFrameStackForOneStepCorrectAlign(self.params['rawarea'])
 		# Align
 		if self.params['align']:
@@ -176,7 +176,7 @@ class MakeFrameStackLoop(apDDLoop.DDStackLoop):
 			if self.params['defergpu']:
 				return
 			# Doing the alignment
-			if self.dd.getUseFrameAlignerFlat():
+			if self.dd.getUseGPUFlat():
 				self.dd.gainCorrectAndAlignFrameStack()
 			else:
 				self.dd.alignCorrectedFrameStack()
