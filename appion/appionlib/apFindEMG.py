4a60e58c8f16df0067a1e36d9e78008765f66378
Neil Voss (Nysbc) <vossman77@yahoo.com>
2017-01-11 11:01:40 -0500

    set peak values to 1 not 0

--- appion/appionlib/apFindEMG.py
+++ appion/appionlib/apFindEMG.py
@@ -239,7 +239,7 @@ def getPeaksFromBoxFile(imgmapname):
 		tline = sline.replace("\t", " ")
 		rline = re.sub("  *", " ", tline)
 		bits = line.split(' ')
-		peakdict = {'peakarea':0,'peakstddev':0,'peakmoment':0}
+		peakdict = {'peakarea':1,'peakstddev':1,'peakmoment':1}
 		xboxsize = int(bit[2])
 		yboxsize = int(bit[3])
 		#WARNING: box files provide coordiates to corner of box not particle center
