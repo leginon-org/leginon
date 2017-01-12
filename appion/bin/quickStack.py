9c87df46a4d59a7d551b1cead5abdab54ed26fd1
Neil Voss <vossman77@yahoo.com>
2017-01-12 17:23:59 -0500

    fun quick stack project

--- appion/bin/quickStack.py
+++ appion/bin/quickStack.py
@@ -1,24 +1,25 @@
 #!/usr/bin/env python
 
 import os
+import sys
+import time
+import numpy
 import multiprocessing
-
 from appionlib import appionScript
 from appionlib import apDisplay
 from appionlib import apStack
 from appionlib import apDatabase
-from appionlib import appiondata
-import leginon.leginondata
+from appionlib import apParticle
+from appionlib import starFile
 
-def makeStack(tup):
-	imgid, selectid = tup
-	imgdata = leginon.leginondata.AcquisitionImageData.direct_query(imgid)
-	selectdata = appiondata.ApSelectionRunData.direct_query(selectid)
-	partq = appiondata.ApParticleData()
-	partq[image] = imgdata
-	partq[selectionrun] = selectdata
-	partdata = partq.query()
-	print "Found %d particles"%(len(partdata))
+import sinedon
+
+def makeStack(starfile):
+	star = starFile.StarFile(starfile)
+	star.read()
+	dataBlock = star.getDataBlock("data_images")
+	loopDict  = dataBlock.getLoopDict()
+	print "Found %d particles"%(len(loopDict))
 	return
 
 def start_process():
@@ -29,36 +30,73 @@ def start_process():
 class QuickStack(appionScript.AppionScript):
 	#=====================
 	def setupParserOptions(self):
-		self.parser.add_option("--sessionid", dest="sessionid", type="int",
-			help="Session ID", metavar="#")
 		self.parser.add_option("--selectid", dest="selectid", type="int",
 			help="Particle Selction ID", metavar="#")
+		self.parser.add_option("--preset", dest="preset",
+			help="Preset Name", metavar="#")
 
 	#=====================
 	def checkConflicts(self):
-		if self.params['sessionid'] is None:
-			apDisplay.printError("Please provide a session id, e.g. --sessionid=15")
+		if self.params['expid'] is None:
+			apDisplay.printError("Please provide a session id, e.g. --expid=15")
 		if self.params['selectid'] is None:
 			apDisplay.printError("Please provide a Particle Selction id, e.g. --selectid=15")
 
+	#==========AppionScript):
 	#=====================
-	def start(self):
-		self.imgtree = apDatabase.getImagesFromDB(self.params['sessionname'], self.params['preset'])
-		imgidlist = []
-		for imgdata in self.imgtree:
-			imgidlist.append(imgdata.dbid)
-		selectlist = numpy.ones((len(imgidlist)))*self.params['selectid']
-		inputs = numpy.hstack(imgidlist, selectlist)
+	def setupParserOptions(self):
+		self.parser.add_option("--selectid", dest="selectid", type="int",
+			help="Particle Selction ID", metavar="#")
+		self.parser.add_option("--preset", dest="preset",
+			help="Preset Name", metavar="#")
 
+	#=====================
+	def checkConflicts(self):
+		if self.params['expid'] is None:
+			apDisplay.printError("Please provide a session id, e.g. --expid=15")
+		if self.params['selectid'] is None:
+			apDisplay.printError("Please provide a Particle Selction id, e.g. --selectid=15")
+
+	#================================
+	def start(self):
+		self.params['sessionname'] = apDatabase.getSessionDataFromSessionId(self.params['expid'])['name']
+		self.imgtree = apDatabase.getImagesFromDB(self.params['sessionname'], self.params['preset'])
+		starlist = []
+		for imgdata in self.imgtree:
+			imgfile = os.path.join(imgdata['session']['image path'], imgdata['filename']+'.mrc')
+			sys.stderr.write(".")
+			starfile = self.writeStarFile(imgdata)
+			starlist.append(starfile)
+		print ""
+		del self.imgtree
 		nproc = 6
 		t0 = time.time()
 		print "nproc %d"%(nproc)
 		p = multiprocessing.Pool(processes=nproc, initializer=start_process)
-		p.map(makeStack, inputs)
+		p.map(makeStack, starlist)
 		p.close()
 		p.join()
 		p.terminate()
 		print "Complete %.3f"%((time.time()-t0)*1)
+
+	#================================
+	def writeStarFile(self, imgdata):
+		starfile = imgdata['filename']+".star"
+		stackfile = imgdata['filename']+".particles.mrcs"
+		micrograph = os.path.join(imgdata['session']['image path'], imgdata['filename']+'.mrc')
+
+		particles = apParticle.getParticles(imgdata, self.params['selectid'])
+		labels = ["_rlnCoordinateX", "_rlnCoordinateY", "_rlnImageName", "_rlnMicrographName"]
+		count = 0
+		valueSets = []
+		for part in particles:
+			count += 1
+			valueString = "%d %d %05d@%s %s"%(part['xcoord'], part['ycoord'], count, stackfile, micrograph)
+			valueSets.append(valueString)
+		star = starFile.StarFile(starfile)
+		star.buildLoopFile( "data_images", labels, valueSets )
+		star.write()
+		return starfile
 
 
 #=====================
