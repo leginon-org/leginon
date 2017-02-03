e48a4bc1ea51b705af53fe11b778aff350f58f88
sargis <sargis@nysbc.org>
2017-02-02 18:10:20 -0800

    Added Leginon Statistics Across Institutions.

--- myamiweb/project/runStat.php
+++ myamiweb/project/runStat.php
@@ -430,8 +430,32 @@ $totalCTF = $aceRun + $ace2Run + $ctfindRun;
 	</tr>
 
 	<tr>
+		<td colspan=2><h3>Leginon Statistics Across Institutions:</h3></td>
+	</tr>
+	<tr>
+		<table border="1"  cellpadding="5" cellspacing="0" width="100%">
+			<tr><td><b>Institution</b></td><td><b># Projects</b></td><td><b># Sessions</b></td></tr>
+			
+
+	<?php
+	$q = "SELECT userdetails.`institution`, count( DISTINCT projectexperiments.`REF|projects|project` ), count( DISTINCT projectexperiments.`REF|leginondata|SessionData|session` )
+FROM projectexperiments
+LEFT JOIN projectowners ON projectowners.`REF|projects|project` = projectexperiments.`REF|projects|project`
+LEFT JOIN userdetails ON userdetails.`REF|leginondata|UserData|user` = projectowners.`REF|leginondata|UserData|user`
+GROUP BY userdetails.`institution`";
+	$r = mysql_query($q) or die("Query error: " . mysql_error());
+	while ($totalProjectWithSessionsByInstitution =  mysql_fetch_row($r))
+	{
+		echo "<tr><td>$totalProjectWithSessionsByInstitution[0]</td><td>$totalProjectWithSessionsByInstitution[1]</td><td>$totalProjectWithSessionsByInstitution[2]</td></tr>"; 
+	}
+	?>
+
+		</table>
+	</tr>
+	<tr>
 		<td colspan=2><h3>Appion Statistics:</h3></td>
 	</tr>
+	
 	<tr>
 		<td># Projects:</td><td><?php echo number_format($numOfApProjects); ?></td>
 	</tr>
