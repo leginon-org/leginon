a67a7054532a880eb9f1930f55877b2d410a26da
Neil Voss <vossman77@yahoo.com>
2017-02-26 12:50:28 -0600

    docker thing

--- appion/docker/centos6/run.sh
+++ appion/docker/centos6/run.sh
@@ -4,8 +4,10 @@
 #  -v /Volumes/Downloads/000test/get-flash-videos/flvchats/emg/data/appion:/emg/data/appion \
 #  -v /Users/vosslab/emg/data:/emg/data \
 
+#  -v /Users/vosslab/myami:/emg/sw/myami \
+
 docker run -d -t \
-  -v /Users/vosslab/myami:/emg/sw/myami \
+  -v /Users/vosslab/myami/pyami:/emg/sw/myami/pyami \
   -w /emg/sw/myami/appion \
   -p 80:80 -p 5901:5901 -p 3306:3306 \
   vosslab/appion
