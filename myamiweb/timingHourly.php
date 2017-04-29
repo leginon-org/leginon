01a0c81f779169756a2b07b0f03dcac9c6a4bddf
sargis <dallakyans@gmail.com>
2017-04-28 17:26:35 -0700

    refs #4931 added pagination following http://www.freezecoders.com/2014/01/simple-pagination-using-php-mysql.html

--- myamiweb/timingHourly.php
+++ myamiweb/timingHourly.php
@@ -8,6 +8,41 @@
  */
 
 require_once "inc/leginon.inc";
+?>
+
+<style>
+#content
+{
+	width: 900px;
+	margin: 0 auto;
+	font-family:Arial, Helvetica, sans-serif;
+}
+.page
+{
+	float: right;
+	margin: 0;
+	padding: 0;
+}
+.page li
+{
+	list-style: none;
+	display:inline-block;
+}
+.page li a, .current
+{
+	display: block;
+	padding: 5px;
+	text-decoration: none;
+	color: #8A8A8A;
+}
+.current
+{
+	font-weight:bold;
+	color: #000;
+}
+
+</style>
+<?php
 
 login_header('Image Collection Timing Statistics per Hour', '', '');
 
@@ -16,10 +51,13 @@ if (privilege('groups') < 3 ){
 	exit;
 }
 ?>
+
+
 <br>
+<div id="content">
 <h1>Image Collection Timing Statistics</h1>
 
-<p>The following table lists how many high magnification images (en or enn) do we collect per hour (on average and max and min). </p>
+<p>The following table lists how many high magnification images (preset en or enn) do we collect per hour (on average and max and min). </p>
  
 <table>
   <tr>
@@ -31,14 +69,21 @@ if (privilege('groups') < 3 ){
   </tr>
   
 <?php
+
+if (isset($_GET["page"])) { $page  = $_GET["page"]; } else { $page=1; };
+if (isset($_GET["limit"])) { $page  = $_GET["limit"]; } else { $limit=50; };
+
+$start = ($page - 1) * $limit;
 $scopes = $leginondata->getScopesForSelection();
 $scopeId = (empty($scopeId)) ? false:$scopeId;
-
+$combinedData = array();
 $sessions = $leginondata->getSessions('description', false, '', $scopeId);
-for ($i = 1; $i <= 100; $i++) {
+
+$sessionsCount = count($sessions);
+$total_pages = ceil( $sessionsCount/ $limit);
+for ($i = $start; $i < $start+$limit; $i++) {
 	$session = $sessions[$i];
 
-	echo "<tr><td>".$session['name_org']."</td>";
 	$presets = $leginondata->getDataTypes($session['id']);
 	$presets = array_reverse($presets);
 	foreach ($presets as $preset) {
@@ -46,20 +91,42 @@ for ($i = 1; $i <= 100; $i++) {
 			continue;
 		if ($preset != 'en' and $preset != 'enn')
 			continue;
-		echo "<td>".$preset."</td>";
 		$timings = $leginondata->getTiming($session['id'], $preset);
 		$data = array(); 
+		global $combinedData;
 		foreach ($timings as $time) {
-			$data[] = intval($time['unix_timestamp']/ 3600);
+			$data[] = intval($time['unix_timestamp']/3600);
+			$combinedData[] = intval($time['unix_timestamp']/3600);
 		} 
 		$array = array_count_values($data);
 		$average = array_sum($array) / count($array);
+		echo "<tr><td>".$session['name_org']."</td>";
+		echo "<td>".$preset."</td>";
 		echo "<td>".round($average)."</td>";
 		echo "<td>".max($array)."</td>";
 		echo "<td>".min($array)."</td></tr>";
 	}
+	
 }
+
+echo "</table>";
+
+$combinedArray = array_count_values($combinedData);
+$average = array_sum($combinedArray) / count($combinedArray);
+echo "<p>Combined Average: ".round($average).'<br>Max: '.max($combinedArray).'<br>Min: '.min($combinedArray)."</p>";
+
+
+echo "<ul class='page'>";
+for($i=1;$i<=$total_pages;$i++)
+{
+	if($i==$page) { echo "<li class='current'>".$i."</li>"; }
+
+	else { echo "<li><a href='?page=".$i."'>".$i."</a></li>"; }
+}
+echo "</ul>";
+
 ?>
-</table>
+
+</div>
 </body>
 </html>
