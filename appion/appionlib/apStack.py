4470bc481af80f087660e3a5491a2c917103336a
Neil Voss <vossman77@yahoo.com>
2017-07-06 16:02:23 -0500

    clean up for apStack.makeNewStack() in various files, refs #5048

--- appion/appionlib/apStack.py
+++ appion/appionlib/apStack.py
@@ -47,8 +47,9 @@ def makeNewStack(oldstack, newstack, partlist=None, remove=False, bad=False):
 	apDisplay.printMsg("creating a new stack\n\t"+newstack+
 		"\nfrom the oldstack\n\t"+oldstack+"\n")
 
-	listfile = partlist
 	if isinstance(partlist, str) and os.path.exists(partlist):
+		listfile = partlist
+		#note: eman particle list files start at 0, appion particle lists start at 1
 		partlist = emanLstFileToPartList(listfile)
 
 	stackTools.createSubStack(oldstack, newstack, partlist, msg=True)
