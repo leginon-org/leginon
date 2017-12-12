5832a7f824a8138b767e86730621c285786dcd72
Anchi Cheng <acheng@nysbc.org>
2017-12-12 09:46:05 -0500

    refs #5359 remove comarray

--- comarray/py2.5/__init__.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-# use precompiled NumpySafeArray dll only if python 2.5
-v = sys.version_info
-if v[:2] == (2,5):
-	from NumpySafeArray import *
-else:
-	from NumpySafeArraySlow import *
