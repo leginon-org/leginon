3849624925436060a020ddc4b5082b9879299837
Carl <carlnegro@gmail.com>
2017-12-29 19:57:24 -0500

    Fix summary.php display of ice thickness data, refs #5384

--- myamiweb/zlp_icegraph.php
+++ myamiweb/zlp_icegraph.php
@@ -6,23 +6,22 @@
  *	For terms of the license agreement
  *	see  http://leginon.org
  */
-
 require_once "inc/leginon.inc";
 require_once "inc/graph.inc";
 
 $defaultId= 4828;
-######$defaultpreset='hl';
+//$defaultpreset='en';
 $sessionId= ($_GET[Id]) ? $_GET[Id] : $defaultId;
-######$preset = ($_GET[preset]) ? $_GET[preset] : $defaultpreset;
+//$preset = ($_GET[preset]) ? $_GET[preset] : $defaultpreset;
 $viewdata = ($_GET['vdata']==1) ? true : false;
-$viewsql = $_GET[vs];
+$viewsql = $_GET['vs'];
 
 $histogram= true;
 $histaxis=($_GET['haxis']) ? $_GET['haxis'] : 'y';
 $width=$_GET['w'];
 $height=$_GET['h'];
-
 $thicknessdata = $leginondata->getZeroLossIceThickness($sessionId);
+
 foreach($thicknessdata as $t) {
 	$data[] = $t['thickness'];
 }
@@ -37,11 +36,6 @@ if ($viewdata) {
 	exit;
 }
 
-if ($viewsql) {
-	$sql = $leginondata->mysql->getSQLQuery();
-	echo $sql;
-	exit;
-}
 $display_x = 'timestamp';
 $display_y = 'thickness';
 $axes = array($display_x,$display_y);
@@ -66,5 +60,5 @@ $dbemgraph->scaley(1e-3);
 $dbemgraph->dim($width,$height);
 $dbemgraph->graph();
 
-?>
+
 ?>
