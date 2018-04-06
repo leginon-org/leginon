c38414d7ee18942847cc9efdbff3798f8eb7d8bb
Anchi Cheng <acheng@nysbc.org>
2018-04-05 18:04:02 -0700

    refs #5593 add line breaks to help parsing

--- myamiweb/map.php
+++ myamiweb/map.php
@@ -89,7 +89,9 @@ MAP: <?php echo $filename; ?>
 <script language="javascript" src="js/scale.js"></script>
 <script type="text/javascript">
 var filename= "<?php echo $filename; ?>"
+
 var pixsize = <?php echo $display_pixelsize; ?>;
+
 var fftflag= <?php echo $fftflag; ?>
 
 var jssize= <?php echo $imgwidth; ?>
