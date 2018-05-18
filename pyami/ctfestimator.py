720d6601fa56c9aaa7b9f879b5dbb07236fdb961
Anchi Cheng <acheng@nysbc.org>
2018-05-18 13:12:32 -0700

    refs #5814 gctf implementation

---
+++ pyami/ctfestimator.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python
+import sys
+import os
+import glob
+import math
+import subprocess
+from pyami import mrc,imagefun,fftfun
+from leginon import leginondata,calibrationclient
+
+class GctfEstimator(object):
+	def __init__(self, gctfexe='gctfCurrent', dbids=[], ln_pattern='test'):
+		self.gctfexe = gctfexe
+		self.pcal = calibrationclient.CalibrationClient(None)
+		self.ln_pattern = ln_pattern
+		self.amp_contrast = 0.07 # amplitude contrast
+		self.reset()
+		if dbids:
+			self.setImagesFromIds(dbids)
+			self.setGlobalInputParams(self.images[0])
+
+	def reset(self):
+		self.images = []
+		self.cleanUp()
+
+	def addImage(self, imagedata):
+		self.images.append(imagedata)
+
+	def setImagesFromIds(self, dbids):
+		for dbid in dbids:
+			imagedata = leginondata.AcquisitionImageData().direct_query(dbid)
+			self.addImage(imagedata)
+
+	def setGlobalInputParams(self,imagedata):
+		scope= imagedata['scope']
+		self.ht = scope['high tension']
+		mag = scope['magnification']
+		tem = scope['tem']
+		self.cs = tem['cs']
+		self.nominal_defocus = scope['defocus']
+		ccdcamera = imagedata['camera']['ccdcamera']
+		pdata = leginondata.PixelSizeCalibrationData(magnification=mag,tem=tem, ccdcamera=ccdcamera).query(results=1)[0]
+		self.pixelsize = pdata['pixelsize']*imagedata['camera']['binning']['x']
+
+	def cleanUp(self):
+		# clean up
+		files = glob.glob('%s*.mrc' % (self.ln_pattern,))
+		for f in files:
+			os.remove(f)
+
+	def writeTestMrc(self,i, a):
+		mrc.write(a, '%s%03d.mrc' % (self.ln_pattern, i))
+
+	def runACE(self, pixelsize, defocus, ht, amp_contrast, cs,path_pattern='test*'):
+		cmd = "%s --apix %.5f --kV %d --Cs %.1f --ac %.3f %s.mrc" % (self.gctfexe, defocus*1e10, int(ht/1000), cs*1e3, amp_contrast, path_pattern)
+		print cmd
+		proc = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE,)
+		proc.stdin.write(cmd)
+		proc.communicate()
+
+	def getCTFResults(self):
+		filepath = 'micrographs_all_gctf.star'
+		from appionlib import starFile
+		star = starFile.StarFile(filepath)
+		star.read()
+		dataBlock = star.getDataBlock("data_")
+		loopDict  = dataBlock.getLoopDict() # returns a list with a dictionary for each line in the loop
+		all_ctfvalues = []
+		for i in range(len(loopDict)):
+			bits = loopDict[i]
+			ctfvalues = {
+						'imagenum' : self.images[i].dbid,
+						'defocus2' : float(bits['_rlnDefocusV'])*1e-10,
+						'defocus1' : float(bits['_rlnDefocusU'])*1e-10,
+						'angle_astigmatism' : float(bits['_rlnDefocusAngle']) + 90,  # see bug #4047 for astig conversion
+						'amplitude_contrast' : self.amp_contrast,
+						'do_EPA' : True,
+						'defocusinit' : None,
+						'cs' : self.cs,
+						'volts' : self.ht,
+						'do_local_refine' : False,
+					}
+			all_ctfvalues.append(ctfvalues)
+		return all_ctfvalues	
+
+	def saveBatchImages(self):
+		avg_defocus = 0
+		for i,imagedata in enumerate(self.images):
+			print imagedata['filename']
+			avg_defocus += imagedata['scope']['defocus']
+			self.writeTestMrc(i, imagedata['image'])
+		self.nominal_defocus = avg_defocus / len(self.images)
+
+	def runImages(self):
+		self.saveBatchImages()
+		self.runACE(self.pixelsize, self.nominal_defocus, self.ht, self.amp_contrast, self.cs,self.ln_pattern+'*')
+		ctfvalues = self.getCTFResults()
+		print ctfvalues
+		return ctfvalues
+
+	def runOneImageData(self, imagedata):
+		self.reset()
+		self.setGlobalInputParams(imagedata)
+		self.addImage(imagedata)
+		result = self.runImages()
+		return result[0]
+
+if __name__ == '__main__':
+	app = GctfEstimator('gctfCurrent')
+	imagedata = leginondata.AcquisitionImageData().query(results=1)[0]
+	app.runOneImageData(imagedata)
