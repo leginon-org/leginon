fca5cc36621ed550b016478c8f2fcd967528bdb4
Sargis Dallakyan <sargis@nimgs.com>
2018-07-17 13:00:05 -0700

    fixes for CentsOS 7 install

--- install/centos7AutoInstallation.py
+++ install/centos7AutoInstallation.py
@@ -299,7 +299,7 @@ class CentosInstallation(object):
 			if '[mysqld]' in l:
 				# just the minimal now. This is not tested.
 				outlines +='default_storage_engine = myisam\n'
-				outlines +='default_tmp_storage_engine = myisam\n'
+				#outlines +='default_tmp_storage_engine = myisam\n'
 		f = open('./my.cnf','w')
 		f.write(outlines)
 		f.close()
@@ -310,16 +310,18 @@ class CentosInstallation(object):
 		self.mariadbYumInstall()
 		self.writeToLog("--- MariaDB is installed through yum on CentOs 7")
 		# turn on auto mysql start
-		
+		self.runCommand("systemctl enable mariadb")
 		# stop mysql server (if it's running)
 		self.runCommand("systemctl stop mariadb")
 		# start mysql server
+		self.runCommand("chown -R mysql:mysql  /var/lib/mysql")
 		
 		#https://stackoverflow.com/questions/33510184/change-mysql-root-password-on-centos7
-		os.system('systemctl set-environment MYSQLD_OPTS="--skip-grant-tables"')
+		#os.system('systemctl set-environment MYSQLD_OPTS="--skip-grant-tables"')
 		# TO DO: Need to find a good my.cnf for mariadb
 		self.updateMyCnf()
-		os.system("systemctl start mariadb")
+		os.system("mysqld_safe --skip-grant-tables &")
+		#os.system("systemctl start mariadb")
 		mysql_is_active = False
 		t0 = time.time()
 		while not mysql_is_active and time.time() - t0 < 30.0:
@@ -830,7 +832,7 @@ endif
 
 	def processServerYumInstall(self):
 
-		packagelist = ['ImageMagick', 'MySQL-python', 'compat-gcc-34-g77', 'fftw3-devel', 'gcc-c++', 'gcc-gfortran', 'gcc-objc', 'gnuplot', 'grace', 'gsl-devel', 'libtiff-devel', 'netpbm-progs', 'numpy', 'openmpi-devel', 'opencv-python', 'python-devel', 'python-imaging', 'python-matplotlib', 'python-tools', 'scipy', 'wxPython', 'xorg-x11-server-Xvfb', 'libjpeg-devel', 'zlib-devel', ]
+		packagelist = ['ImageMagick', 'MySQL-python', 'compat-gcc-34-g77', 'fftw3-devel', 'gcc-c++', 'gcc-gfortran', 'gcc-objc', 'gnuplot', 'grace', 'gsl-devel', 'libtiff-devel', 'netpbm-progs', 'numpy', 'openmpi-devel', 'opencv-python', 'python-devel', 'python-imaging', 'python-matplotlib', 'python-tools', 'scipy', 'wxPython', 'xorg-x11-server-Xvfb', 'libjpeg-devel', 'zlib-devel', 'unzip']
 		self.yumInstall(packagelist)
 
 	def enableTorqueComputeNode(self):
