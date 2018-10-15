2915d04c08d150f03d012d412a425003449b8de2
Anchi Cheng <acheng@nysbc.org>
2018-10-15 17:04:22 -0400

    myami-3.4 set version

--- leginon/version.py
+++ leginon/version.py
@@ -110,7 +110,7 @@ def getSVNInfo(module_path=''):
 	return infodict
 
 def getTextVersion():
-	return 'beta'
+	return '3.4'
 
 def getSVNVersion(module_path=''):
 	svninfo = getSVNInfo(module_path)
