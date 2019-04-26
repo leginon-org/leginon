afeeff7af429c75680214cbcfb26e19258145cc4
Anchi Cheng <acheng@nysbc.org>
2019-04-25 18:18:58 -0700

    refs #7480 reorganize and correct formating

--- pyami/numsmv.py
+++ pyami/numsmv.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
+import sys
 import numpy
+import datetime
+
+# attempt to use weakattr
+try:
+	from pyami import weakattr
+except:
+	pass
 
 '''
 Write smv format image files given the numpy array.
@@ -9,22 +17,229 @@ https://strucbio.biologie.uni-konstanz.de/ccp4wiki/index.php/SMV_file_format
 This only modify the size1 and size2 from the standard header.
 '''
 
-def getHeaderText(shape):
-	# standard header must be 512 bytes
-	text='{\nHEADER_BYTES=  512;\nBEAM_CENTER_X=0;\nBEAM_CENTER_Y=0;\nBIN=1x1;\nBYTE_ORDER=little_endian;\nDATE=Thu Jan  1 00:00:00 1970;\nDETECTOR_SN=unknown;\nDIM=2;\nDISTANCE= 850;\nOSC_RANGE=0.868;\nOSC_START=nan;\nPHI=nan;\nSIZE1=2048;\nSIZE2=2048;\nIMAGE_PEDESTAL=0;\nPIXEL_SIZE=28;\nTIME=nan;\nWAVELENGTH=0.025;\nTWOTHETA=0;\nTYPE=unsigned_short;\n}\n                                                                                                                                                                                         \n'
-	size1_start=text.find('SIZE1=')+len('SIZE1=')
-	size2_start=text.find('SIZE2=')+len('SIZE2=')
-	# UNKNOWN which is width or height. all known smv are squares
-	size1='%4d' % min(shape)
-	size2='%4d' % min(shape)
-	text = text[:size1_start]+size1+text[size1_start+4:]
-	text = text[:size2_start]+size2+text[size2_start+4:]
-	return text
+# header (name, type, default, format
+HEADER_FIELDS = (
+	# required for reading data
+	('HEADER_BYTES', int, 512, '{:>5}'),
+	('TYPE', str, 'unsigned_short', '{0}'),
+	('BYTE_ORDER', str, 'little_endian', '{0}'),
+	('DIM', int, 2, '{:1}'),
+	('SIZE1', int, 2048, '{:>5}'),
+	('SIZE2', int, 2048, '{:>5}'),
+	# imaging geometry
+	('DISTANCE', float, 1000.0, '{:7.1f}'), #mm
+	('WAVELENGTH', float, 0.025014041, '{:12.8f}'), # angstrom
+	('PIXEL_SIZE', float, 0.014, '{:8.3f}'), # mm
+	('OSC_RANGE', float, 1.000, '{:8.3f}'), #degrees. Can this be negative ?
+	('OSC_START', float, 0.000, '{:8.3f}'), #degrees
+	('BEAM_CENTER_X', int, 0, '{:>4}'),
+	('BEAM_CENTER_Y', int, 0, '{:>4}'),
+	('PHI', float, 0.000, '{:8.3f}'), #spindle angle in degrees same as OSC_START ?
+	('TWOTHETA', float, 0.0, '{:8.3f}'), # detector angle to beam normal plane in degrees
+	# timing
+	('TIME', float, 1.0, '{0}'), # seconds
+	('ACC_TIME', int, 1000, '{0}'), # milliseconds
+	# detector identity
+	('BIN', tuple, (2,2), '{0[0]:1}x{0[1]:1}'),
+	('ADC', str, 'fast', '{0}'),
+	# ADSC info
+	('BIN_TYPE', str, 'HW', '{0}'),
+	('CREV', int, 1, '{:>3}'),
+	('DETECTOR_SN', int, 1, '{0}'),
+	# APS info
+	('BEAMLINE', str, 'nan', '{0}'),
+	# pthers
+	('DATE', datetime.datetime, datetime.datetime.today(), '{0}'), #UNIX date in current timezone
+#	('IMAGE_PEDESTAL', int, 0, '{:0}'), # where did I get this ?
+)
+
+# (header_value, numpy_dtype, number_of_bytes)
+TYPE_MAP = [('unsigned_short', numpy.uint16, 2),]
+
+VALID_KEYS = map((lambda x: x[0]),HEADER_FIELDS)
+
+def newHeader(header_fields=HEADER_FIELDS):
+	'''
+Return a new initialized header dictionary.
+All fields are initialized to default.
+	'''
+	headerdict = {}
+	for field in header_fields:
+		name = field[0]
+		default = field[2]
+		headerdict[name] = default
+	return headerdict
+
+def formatHeader(headerdict, set_default=False):
+	'''
+	Convert header dictionary which values are in the python type to a string.
+	Join the key and values to the required format.  The result is not yet
+	padded to header_bytes.
+	'''
+	names = map((lambda x: x[0]), HEADER_FIELDS)
+	header = ''
+	for i, name in enumerate(names):
+		try:
+			value = headerdict[name]
+		except KeyError:
+			if not set_default:
+				# ignore missing key
+				continue
+			else:
+				value = HEADER_FIELDS[i][2]
+		f_value = HEADER_FIELDS[i][3]
+		v_type = HEADER_FIELDS[i][1]
+		if v_type != str and type(value) == type('nan'):
+			# default nan is a string regardless of type.
+			str_value = value
+		elif name == 'BIN':
+			str_value = '%dx%d' % (value[0],value[1])
+		elif name == 'DATE':
+			str_value = value.strftime('%a %b %d %H:%M:%S %Y')
+		else:
+			str_value = f_value.format(value)
+		h_item = '%s=%s;\n' % (name, str_value)
+		header += h_item
+	header = '{\n' + header + '}\n'
+	filled_length = len(header)
+	header_bytes = headerdict['HEADER_BYTES']
+	spaces = ' '*(header_bytes-filled_length-1)
+	header += spaces + '\n'
+	return header
+
+def validate_and_convert(key, value):
+	try:
+		i = VALID_KEYS.index(key)
+	except:
+		raise ValueError('Key not valid: %s' % (key,))
+	valid_type = HEADER_FIELDS[i][1]
+	# Handle nan
+	if value == 'nan':
+		return value
+	if key == 'DETECTOR_SN' and value == 'unknown':
+		return 1
+	# Parsing Bin
+	if key == 'BIN':
+		bits = value.split('x')
+		if len(bits) != 2:
+			raise ValueError('BIN not in format like 1x1 but %s' % (value,))
+		try:
+			int(bits[0]) > 0
+			int(bits[1]) > 0
+		except:
+			raise ValueError('BIN not in format like 1x1 but %s' % (value,))
+		# Not sure this is XxY or YxX
+		return (int(bits[0]),int(bits[1]))
+	if key == 'DATE':
+		try:
+			return datetime.datetime.strptime(value, '%a %b %d %H:%M:%S %Y')
+		except:
+			print('Warning: Date not valid, use current date time')
+			return datetime.datetime.today()
+	# All other keys
+	try:
+	  return valid_type(value)
+	except:
+		raise
+
+def parseHeader(headerbytes):
+	'''
+	Parse the 512 byte SMV header into a header dictionary.
+	'''
+	curly_bracket_bits = headerbytes[1:].split('}')
+	bits = curly_bracket_bits[0].split(';')
+	headerdict = {}
+	for h in bits:
+		lines = h.split('\n')
+		for l in lines:
+			if '=' in l:
+				try:
+					key, value_str = l.split('=')
+					value = validate_and_convert(key, value_str)
+					headerdict[key] = value
+				except Exception as e:
+					print('Invalid parsing: "%s" with error-\n  %s' % (l,e))
+					sys.exit(1)
+	return headerdict
+
+def readHeaderFromFileObj(fobj):
+	fobj.seek(0)
+	headerbytes = fobj.read(512)  # read default size
+	headerdict = parseHeader(headerbytes)
+	if headerdict['HEADER_BYTES'] != 512:
+		# redo parse header
+		fobj.seek(0)
+		headerbytes = fobj.read(headerdict['HEADER_BYTES'])
+		headerdict = parseHeader(headerbytes)
+	return headerdict
+
+def updateHeader(headerdict, key, value):
+	'''
+	Update an item in header dict with validation
+	'''
+	try:
+		value = validate_and_convert(key, value)
+	except Exception as e:
+		print('Updating %s failed with value %s' % (key, value,))
+	headerdict[key] = value
+	return headerdict
+
+#-----END of Header functions
+#-----START of Data functions
+def getDataBytesPerPixel(header_type_string):
+	types = map((lambda x: x[0]),TYPE_MAP)
+	i = TYPE_MAP.index(header_type)
+	return TYPE_MAP[i][2]
+
+def getDataNumpyType(header_type_string):
+	types = map((lambda x: x[0]),TYPE_MAP)
+	i = TYPE_MAP.index(header_type)
+	return TYPE_MAP[i][1]
+
+def readDataFromFileObj(fobj, headerdict):
+	'''
+	Read data portion of SMV file from the file object fobj.
+	Both mrcmode and shape have been determined from the MRC header.
+	Returns a new numpy ndarray object.
+	'''
+	bytes_per_pixel = getDataBytesPerPixel(headerdict['TYPE'])
+	numpy_type = getDataNumpyType(headerdict['TYPE'])
+	header_bytes = headerdict['HEADER_BYTES']
+	start = header_bytes  # right after header
+	shape = (headerdict['SIZE1'],headerdict['SIZE2'])
+	datalen = numpy.prod(shape)
+	fobj.seek(start)
+	a = numpy.fromfile(fobj, dtype=numpy_type, count=datalen)
+	a.shape = shape
+	return a
+
+def setHeader(a, headerdict):
+	'''
+Attach an header to the array.
+	'''
+	try:
+		weakattr.set(a, 'smvheader', headerdict)
+	except:
+		pass
+
+# Functions to be called from outside.
+def read(filename):
+	'''
+	Read header and data of the filename. The header is saved in smvheader attribute
+	of the resulting numpy array if pyami is in the python path.
+	'''
+	fobj = open(filename, 'rb')
+	headerdict = readHeaderFromFileObj(fobj)
+	a = readDataFromFileObj(fobj, headerdict)
+	fobj.close()
+
+	## store keep header with image
+	setHeader(a, headerdict)
+	return a
 
 def write(a, imfile=None, offset=0, header_updates={}):
 	'''
 	Convert array to unsigned 16 bit gray scale and save to filename.
-	Format is determined from filename extension by PIL.
 	'''
 	if offset:
 		a = a+numpy.ones(a.shape)*offset
@@ -47,42 +262,50 @@ def write(a, imfile=None, offset=0, header_updates={}):
 			a = a[:,crop_start:crop_start+shape_min]
 	# known smv files are all uint16
 	a = a.astype(numpy.uint16)
-	new_head = getHeaderText(a.shape)
+	headerdict = newHeader()
+	header_updates['SIZE1']=a.shape[0]
+	header_updates['SIZE2']=a.shape[1]
 	for k in header_updates.keys():
 		v = header_updates[k]
-		new_head = updateHeader(new_head,k,v)
+		headerdict = updateHeader(headerdict,k,v)
+	# new header has complete default values
+	headbytes = formatHeader(headerdict)
 	outf = open(imfile,'wb')
-	outf.write(new_head)
+	outf.write(headbytes)
 	outf.write(a)
 	outf.close()
 
-def updateHeader(header, key, value):
-	try:
-		item_start = header.index(key)
-	except ValueError:
-		print('Key %s not in header' % (key,))
-		return
-	i = 0
-	while True:
-		if header[item_start+i] == ';':
-			item_end = item_start+i+1
-			break
-		i += 1
-	print(item_start, item_end)
-	item_length = item_end - item_start
-	item = header[item_start:item_start+item_length]
-	print(item)
-	allowed_length = item_length - len(key) -2 #take out '=' and ';'
-	if type(value) == type(0):
-		value = '%d' % value
-	elif typt(value) ==type(1.2):
-		value = '%5.3f' % value
-	value_length = len(value)
-	space_length = allowed_length - value_length
-	new_item='%s=%s%s;' % (key, ' '*space_length,value)
-	new_header = header.replace(item, new_item)
-	print('Updating %s to %s' % (item, new_item))
-	return new_header
+def readHeaderFromFile(filename):
+	fobj = open(filename)
+	h = readHeaderFromFileObj(fobj)
+	fobj.close()
+	return h
+
+def update_file_header(filename, headerdict, set_default=False):
+	'''
+	open the SMV file header, update the fields given by headerdict.
+	'''
+	# Check input
+	for k in headerdict.keys():
+		try:
+			v = headerdict[k]
+			v = validate_and_convert(k,headerdict[k])
+			headerdict[k] = v
+		except Exception as e:
+			raise ValueError('Updating %s failed with value %s' % (k, v,))
+	# Open header and update
+	f = open(filename, 'rb+')
+	oldheader = readHeaderFromFileObj(f)
+	oldheader.update(headerdict)
+	headerbytes = formatHeader(oldheader, set_default)
+	f.seek(0)
+	f.write(headerbytes)
+	f.close()
+
+def validate_file_length(filename):
+	h = readHeaderFromFile(filename)
+	fsize = os.path.getsize(filename)
+	return fsize == h['HEADER_BYTES']+headerdict['SIZE1']*headerdict['SIZE2']*getDataBytesPerPixel(headerdict['TYPE'])
 
 if __name__ == '__main__':
 	shape = (1000,1000)
@@ -90,3 +313,4 @@ if __name__ == '__main__':
 	a = numpy.zeros(shape)
 	a[0:inset_shape[0],0:inset_shape[1]] = numpy.ones(inset_shape)
 	write(a, 'test.smv')
+	print('Written test.smv')
