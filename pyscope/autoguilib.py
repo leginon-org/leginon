86f7bb9741f77f5bc2e46dc29317186af967b4a8
Anchi Cheng <acheng@nysbc.org>
2019-09-08 18:48:24 -0700

    refs #8014 debug

--- pyscope/autoguilib.py
+++ pyscope/autoguilib.py
@@ -8,28 +8,38 @@ class WindowAutoGui(object):
 	def __init__(self, origin):
 		self.origin = origin # (x,y)
 
-	def _toScreenPosition(position):
-		return (origin[0]+position[0],origin[1]+position[1])
+	def _toScreenPosition(self, position):
+		return (self.origin[0]+position[0],self.origin[1]+position[1])
 
 	def clickAtPosition(self, position=(0,0)):
 		screen_pos =self._toScreenPosition(position)
-		pyautogui.move(screen_pos)
+		pyautogui.moveTo(screen_pos)
 		pyautogui.click()
 
-class PullDownChoice(WindowAutoGui)
-	def __init__(self, origin, activate_position=(0,0),item0_position=(0,0),item_height=10,	item_count=1):
+	def activateWindow(self, position):
+		'''
+		activate window
+		'''
+		self.clickAtPosition(position)
+
+class PullDownChoice(WindowAutoGui):
+	def __init__(self, origin, pulldown_position=(0,0),item0_position=(0,0),item_height=10,	item_count=1):
 		super(PullDownChoice,self).__init__(origin)
+		self.pulldown_position = pulldown_position
+		self.item0_position = item0_position
+		self.item_height = item_height
+		self.item_count = item_count
 
 	def select(self, item_index):
-		if item_index >= item_count:
+		if item_index >= self.item_count:
 			raise ValueError('PullDownChoice index exceeds item count')
-		# activate
-		self.clickAtPosition(activate_position)
+		# show pull-down
+		self.clickAtPosition(self.pulldown_position)
 		# select
-		pos = (item0_position[0], item0_position[1]+item_index*item_height)
+		pos = (self.item0_position[0], self.item0_position[1]+item_index*self.item_height)
 		self.clickAtPosition(pos)
 
-class WindowTypeWrite(WindowAutoGui)
+class WindowTypeWrite(WindowAutoGui):
 	def write(self, position, text):
 		self.clickAtPosition(position)
 		pyautogui.typewrite(text)
