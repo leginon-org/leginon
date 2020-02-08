8adf6e92c4551f772b96b3e11b91c5adce9bb864
Anchi Cheng <acheng@nysbc.org>
2020-02-07 22:39:58 -0500

    refs #6443 test tilt in a time range

--- pyscope/testing/scope_tilt_test.py
+++ pyscope/testing/scope_tilt_test.py
@@ -3,40 +3,69 @@
 # change these for your testing
 #-----------------------
 # import the module of your scope
-from pyscope import fei
+#from pyscope import fei
+from pyscope import simtem
 # create instance of this tem
-c = fei.Glacios()
+#c = fei.Glacios()
+c = simtem.SimTEM300()
 # alpha angle to move in degrees
 delta_angle = 60.0
-# tilt speed degrees per second
-speed = 2.0
-
+# repeet test and average
+repeats = 1
 #------------------------
 # no change required below
 #------------------------
 import time
-display_scale = 180.0/3.14159
-display_unit = 'degrees'
-distance = delta_angle / display_scale
-repeats = 1
-functionname = 'StagePosition'
 
-getfunc = getattr(c,'get'+functionname)
-setfunc = getattr(c,'set'+functionname)
+def testTiltSpeed(tem_obj, delta_angle, speed):
+	display_scale = 180.0/3.14159
+	display_unit = 'degrees'
+	distance = delta_angle / display_scale
+	tem_obj.setStageSpeed(speed) # degrees per second
 
-p0 = getfunc()
+	functionname = 'StagePosition'
+	getfunc = getattr(tem_obj,'get'+functionname)
+	setfunc = getattr(tem_obj,'set'+functionname)
 
-c.setStageSpeed(speed) # degrees per second
+	p0 = getfunc()
 
-for i in range(repeats):
-	for d in (distance,):
-		p = getfunc()
-		p1 = {'a':p['a']+d}
-		t0 = time.time()
-		setfunc(p1)
-		t1 = time.time()
-		print '%s by %.2f %s took %.6f seconds' % (functionname, d*display_scale, display_unit, t1 -t0)
-	time.sleep(6.0)
-# reset back to the original
-c.setStageSpeed(50.0) #maximal speed since stage limit is about 29 degrees/sec.
-setfunc(p0)
+
+	move_times = []
+	calc_time = delta_angle / speed
+	print '-----'
+	print 'expected time for %.2f degrees/sec is\t %8.3f seconds' % (speed, calc_time)
+	for i in range(repeats):
+		for d in (distance,):
+			p = getfunc()
+			p1 = {'a':p['a']+d}
+			t0 = time.time()
+			setfunc(p1)
+			t1 = time.time()
+			move_time = t1 - t0
+			print '%s by %.2f %s took\t %8.3f seconds' % (functionname, d*display_scale, display_unit, move_time)
+			move_times.append(move_time)
+			time.sleep(3.0)
+			# reset back to the original
+			tem_obj.resetStageSpeed()
+			setfunc(p0)
+	avg_move_time = sum(move_times) / repeats
+	diff_time = avg_move_time - calc_time
+	print 'speed %.2f degrees/sec time_diff is\t %8.3f sec.' % (speed, diff_time)
+
+def loop(tem_obj, center_time, step_time, half_loop_number):
+	'''
+	Equally spaced time loop centered around a value.
+	'''
+	loop_number = half_loop_number*2 + 1
+	for i in range(loop_number):
+		target_time = (i-half_loop_number)*step_time + center_time
+		speed = delta_angle / target_time
+		if target_time >=180 or target_time < 1:
+			print 'invalid target time: %.2f seconds, ignored' %(target_time)
+			continue
+		testTiltSpeed(tem_obj, delta_angle, speed)
+
+if __name__ == '__main__':
+	print "Edit file to set instrument"
+	# Check from 40 s to 140 s target time
+	loop(c, 90, 10, 5)
