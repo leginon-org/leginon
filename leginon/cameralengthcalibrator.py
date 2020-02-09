2d70c941a93ba2aed369be81ffac6a8a79c0bbca
Anchi Cheng <acheng@nysbc.org>
2020-02-08 22:52:20 -0500

    refs #7781 add pixelsize calibration to be shown in webviewer

--- leginon/cameralengthcalibrator.py
+++ leginon/cameralengthcalibrator.py
@@ -8,7 +8,7 @@
 import calibrator
 import calibrationclient
 import event, leginondata
-from pyami import imagefun, fftfun
+from pyami import imagefun, fftfun, diffrfun
 import node
 import math
 import scipy
@@ -30,6 +30,8 @@ class CameraLengthCalibrator(calibrator.Calibrator):
 		calibrator.Calibrator.__init__(self, id, session, managerlocation, **kwargs)
 		self.mag = None
 		self.bin = None
+		self.is_diffr_tem = False
+		self.diffr_constant = None
 		self.image_camera_length = None
 		self.pixeldistance = None
 		self.calclient = calibrationclient.CameraLengthCalibrationClient(self)
@@ -49,8 +51,17 @@ class CameraLengthCalibrator(calibrator.Calibrator):
 		#imagedata = self.fakeImage()
 		if imagedata is None:
 			return
+		# check tem
 		scope = imagedata['scope']
+		self.is_diffr_tem = 'Diffr' in scope['tem']['name']
+		if not self.is_diffr_tem:
+			self.logger.error('image not acquired in diffraction mode,')
+			return
+		# check camera dimemsion
 		camera = imagedata['camera']
+		if camera['dimension']['x'] != camera['dimension']['y']:
+			self.logger.error('Only know how to handle square image right now.')
+			return
 		self.mag = scope['magnification']
 		self.bin = camera['binning']['x']
 		newimage = imagedata['image']
@@ -85,6 +96,10 @@ class CameraLengthCalibrator(calibrator.Calibrator):
 		camera_lengths = []
 		if self.initInstruments():
 			return camera_lengths
+		self.is_diffr_tem = 'Diffr' in self.instrument.getTEMName()
+		if not self.is_diffr_tem:
+			self.logger.error('image not acquired in diffraction mode,')
+			return camera_lengths
 		calibrations = self.calclient.retrieveLastCameraLengths(None, None)
 		mag, mags = self.getMagnification()
 		for calibration in calibrations:
@@ -105,30 +120,45 @@ class CameraLengthCalibrator(calibrator.Calibrator):
 			
 		return camera_lengths
 
-	def _store(self, mag, psize, comment):
+	def _store(self, mag, camlength, comment):
 		temdata = self.instrument.getTEMData()
 		camdata = self.instrument.getCCDCameraData()
 		caldata = leginondata.CameraLengthCalibrationData()
 		caldata['magnification'] = mag
-		caldata['camera length'] = psize
+		caldata['camera length'] = camlength
 		caldata['comment'] = comment
 		caldata['session'] = self.session
 		caldata['tem'] = temdata
 		caldata['ccdcamera'] = camdata
+		self.publish(caldata, database=True, dbforce=True)
+		self._storePixelSizeCalibration(caldata)
+
+	def _storePixelSizeCalibration(self, camlengthdata):
+		cdata = camlengthdata
+
+		rpsize_nm = 1e-18 * self.diffr_constant / cdata['camera length']
+		# fudge factor for images to be displayed as 1kx1k images in the viewer.
+		rpsize_nm_viewer = rpsize_nm / 4.0
+		caldata = leginondata.PixelSizeCalibrationData()
+		caldata['magnification'] = cdata['magnification']
+		caldata['pixelsize'] = rpsize_nm_viewer
+		caldata['comment'] = cdata['comment']+' assuming 2kx2k image'
+		caldata['session'] = self.session
+		caldata['tem'] = cdata['tem']
+		caldata['ccdcamera'] = cdata['ccdcamera']
 		self.publish(caldata, database=True, dbforce=True)
 
 	def calculateCameraLength(self):
 		if self.shape is None:
 			self.logger.error('need image to calculate camera length')
 			return
-		wavelength = fftfun.getElectronWavelength(self.instrument.tem.HighTension)
 		d_spacing = self.settings['d spacing'] #Angstrum
-		pixel_ring_diameter = self.settings['distance']*self.bin
-		# camera physical pixel size
-		meter_ring_diameter = pixel_ring_diameter*self.instrument.ccdcamera.getPixelSize()['x']
-		theta = math.asin(wavelength*1e10/(2*d_spacing))
-		camera_length = meter_ring_diameter/2.0/(math.tan(2*theta))
-		self.image_camera_length = camera_length # meters
+		radius = self.settings['distance']/2.0 #in binned pixel
+		ht = self.instrument.tem.HighTension # in Volt
+		cam_psize = self.instrument.ccdcamera.getPixelSize()['x'] # meters
+		image_bin = self.bin
+		camera_length, rpixel_m = diffrfun.calculateCameraLength(d_spacing, radius, ht, cam_psize,image_bin)
+		self.diffr_constant = camera_length * rpixel_m
 		return camera_length
 
 	def averageCameraLengths(self,measurements):
@@ -137,6 +167,7 @@ class CameraLengthCalibrator(calibrator.Calibrator):
 			for measurement in measurements:
 				sum = sum + measurement
 			average = sum / len(measurements)
+			self.logger.info('averaging %d measurements' % (len(measurements),))
 			self._store(self.mag,average,'averaged from %d values' % len(measurements))
 			self.image_camera_length = average
 			return average
