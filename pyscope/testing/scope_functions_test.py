87f7b0ef6393b28c49c16b612c654c08e7ed19f6
Anchi Cheng <acheng@nysbc.org>
2020-10-19 17:16:22 -0700

    refs #4066 #5743 better scope testing

--- pyscope/testing/scope_functions_test.py
+++ pyscope/testing/scope_functions_test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import time
+import inspect
 from pyami import moduleconfig
 from pyscope import instrumenttype
 search_for = 'TEM'
@@ -9,32 +10,51 @@ def test(tem_inst, attr_name, arg=None):
 		try:
 			result = getattr(tem_inst, attr_name)()
 		except Exception as e:
+			if 'adaExp' in e:
+				pass
 			raise RuntimeError('Error testing %s: %s' % (attr_name,e))
 	else:
 		try:
 			result = getattr(tem_inst, attr_name)(arg)
 		except Exception as e:
+			if 'adaExp' in e:
+				pass
 			raise RuntimeError('Error testing %s with %s: %s' % (attr_name, attr_name, e))			
+
+def _testGet(tem_inst, attr_name, count=False):
+	this_module = inspect.getmodule(tem_inst)
+	try:
+		if 'ApertureSelection' in attr_name or 'ApertureNames' in attr_name:
+			test(tem_inst, attr_name, 'objective')
+		elif attr_name.endswith('SlotState'):
+			test(tem_inst, attr_name, 1)
+		elif 'Film' in attr_name:
+			# don't test film functions
+			return None # skip
+		elif 'Config' in attr_name and hasattr(this_module,'configs'):
+			k = getattr(this_module,'configs').keys()[0]
+			test(tem_inst, attr_name, k)
+		else:
+			test(tem_inst, attr_name)
+		return True
+	except RuntimeError as e:
+		return False
 
 def testMethods(tem_inst):
 	capabilities = tem_inst.getCapabilities()
 	attr_names = dir(tem_inst)
 	exclusions = []
 	error_count = 0
+	good_get = []
 	# test all get methods
 	for a in attr_names:
-		try:
-			if a.startswith('get'):
-				attr_name = a
-				if 'ApertureSelection' in a:
-					test(tem_inst, attr_name, 'objective')
-				elif a.endswith('SlotState'):
-					test(tem_inst, attr_name, 1)
-				else:
-					test(tem_inst, attr_name)
-		except RuntimeError as e:
-			print(e)
-			error_count += 1
+		if a.startswith('get'):
+			is_success = _testGet(tem_inst,a)
+			if is_success == False:
+				print(e)
+				error_count += 1
+			elif is_success == True:
+				good_get.append(a)
 				
 	for c in capabilities:
 		if c['name'] in exclusions:
@@ -42,8 +62,15 @@ def testMethods(tem_inst):
 		impls = c['implemented']
 		if 'get' in impls:
 			attr_name = 'get'+c['name']
+			if attr_name not in good_get:
+				continue
 			try:
-				result = getattr(tem_inst, attr_name)()
+				if 'ApertureSelection' in a or 'ApertureNames' in a:
+					result = getattr(tem_inst, attr_name)('objective')
+				elif a.endswith('SlotState'):
+					result = getattr(tem_inst, attr_name)(1)
+				else:
+					result = getattr(tem_inst, attr_name)()
 				if 'set' in impls:
 					attr_name = 'set'+c['name']
 					t0 = time.time()
@@ -57,6 +84,7 @@ def testMethods(tem_inst):
 	print('Number of error found: %d' % (error_count,))
 
 t = instrumenttype.getInstrumentTypeInstance(search_for)
+global this_module 
 t.findMagnifications()
 testMethods(t)
 raw_input('Finished. Hit return to quit')
