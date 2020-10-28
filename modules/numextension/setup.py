907f5ca7a358dc668148ec90d6c9754abcac77a9
Jim Pulokas <jimpulokas@gmail.com>
2020-10-27 22:30:18 -0500

    numextension python3 conversion

--- modules/numextension/setup.py
+++ modules/numextension/setup.py
@@ -4,14 +4,12 @@ import numpy
 
 numpyinc = numpy.get_include()
 
-numextmod = Extension('numextension._numextension', sources = ['numextension.c', 'canny_edge.c', 'allstats.c'], include_dirs=[numpyinc])
+numextmod = Extension('numextension', sources = ['numextension.c', 'canny_edge.c', 'allstats.c'], include_dirs=[numpyinc])
 
 setup(
 	name='numextension',
 	description='Extension to numpy',
 	ext_modules=[numextmod],
-	packages=['numextension'],
-	package_dir={'numextension': ''},
-	version='svn',
+	version='git',
 )
 
