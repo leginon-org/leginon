c271bc2ea074967eb0a95a5130a7ff7b81051808
Anchi Cheng <acheng@nysbc.org>
2021-02-23 19:07:17 -0800

    refs #9915 hook from image viewer

--- myamiweb/sorttargets.php
+++ myamiweb/sorttargets.php
@@ -4,8 +4,8 @@
 	*/
 	require_once 'inc/leginon.inc';
 
-	$sessionId = $_GET['expid'];
-	$imgId = $_GET['id'];
+	// This script does not take preset, so the imgId needs to be exact.
+	$imgId = ($_GET['id']) ? (int)$_GET['id']:null;
 	$order_list = $_GET['order'];
 	$is_abort = $_GET['abort'];
 
@@ -17,19 +17,23 @@
 	}
 	$rtlist = $leginondata->getTargetListFromImage($imgId);
 	if ($rtlist) {
+		$sessionId = $rtlist['session'];
 		if ($rtlist['mosaic'] == 1) {
 			// given a gr image id and get targets on all tile images on the atlas
+			$is_mosaic = true;
 			$title = "Targets on atlas ".$rtlist['label'];
 			$targets = $leginondata->getNewTargetsFromAtlasImageList($rtlist['ilist']);
 		} else {
 			// given a non atlas image id and get targets on that image
-			$title = "Targets on image ";
+			$is_mosaic = false;
 			$targets = $leginondata->getNewTargetsOnImage($imgId); 
+			$filename = $leginondata->getFilename($imgId)[0]['filename'];
+			$title = "Targets on image ".$filename;
 		}
 		if (!is_null($order_list)) {
 			// save new order 
 			$order_str = "(".$order_list.")";
-			$order_array = explode(',', $order_list);
+			$order_array = explode(', ', $order_list);
 			$order_array = array_map('subtractOne', $order_array);
 			if ($targets) {
 				$leginondata->saveTargetOrder($sessionId, $targets[0]['tlist'], $order_str);
@@ -40,16 +44,15 @@
 				$leginondata->saveTargetOrder($sessionId, $targets[0]['tlist'], '()');
 				exit();
 			}
+		}
+	// retrieve current target order
+		$order_result = $leginondata->getTargetOrder($sessionId, $targets[0]['tlist']);
+		if (is_array($order_result) && array_key_exists('SEQ|order',$order_result)) {
+			$order_array = explode(',',substr($order_result['SEQ|order'],1,-1));
+			$order_array = array_map('subtractOne', $order_array);
 		} else {
-		// retrieve current target order
-			$order_result = $leginondata->getTargetOrder($sessionId, $targets[0]['tlist']);
-			if (is_array($order_result) && array_key_exists('SEQ|order',$order_result)) {
-				$order_array = explode(',',substr($order_result['SEQ|order'],1,-1));
-				$order_array = array_map('subtractOne', $order_array);
-			} else {
-				// make default
-				$order_array = range(0,count($targets)-1);
-			}
+			// make default
+			$order_array = range(0,count($targets)-1);
 		}
 	}
 
@@ -63,8 +66,8 @@
 		<title>Target Sorter</title>
 		<link rel="stylesheet" type='text/css' href='css/viewer.css'>
 		<style>
-			#sortable { list-style-type: none; margin: 0; padding: 0; width: 60%; }
-			#sortable li { margin: 0 3px 3px 3px; padding: 0.4em; padding-left: 1.5em; font-size: 1.0em; height: 18px; }
+			#sortable { list-style-type: none; margin: 0; padding: 0; width: 100%; }
+			#sortable li { margin: 0 3px 3px 3px; padding: 0.4em; padding-left: 1.5em; font-size: 0.8em; height: 15px; }
 			#sortable li span { position: absolute; margin-left: -1.3em; }
 		</style>
 
@@ -98,15 +101,18 @@
 		$str = '<ul id="sortable">';
 		for ($i = 0; $i < count($targets); $i++) {
 			$t_index = $order_array[$i];
-			//$str .= "<li class=\"ui-state-default\" id='".$targets[$t_index]['number']."'><span class=\"ui-icon ui-icon-arrowthick-".$t_index."-n-s\"></span>Target Number ".$targets[$t_index]['number']." on ".$targets[$t_index]['filename'].".mrc</li>".$crlf;
-			$str .= "<li class=\"tablebg\" id='".$targets[$t_index]['number']."'><img name=\"thumbtarget_btimg\" src=\"http://localhost/myamiweb/img/target_bt_on.gif\" alt=\"target_bt\" border=\"0\" height=\"15\" vspace=\"0\">Target Number ".$targets[$t_index]['number']." on ".$targets[$t_index]['filename'].".mrc</li>".$crlf;
-
+			$postfix = ($is_mosaic) ? 'on '.$targets[$t_index]['filename'].".mrc": '';
+			if (is_numeric($t_index)) {
+				$str .= "<li class=\"fittable\" id='".$targets[$t_index]['number']."'>";
+				$str .= "<img name=\"thumbtarget_btimg\" src=\"http://localhost/myamiweb/img/target_bt_on.gif\" alt=\"target_bt\" border=\"0\" height=\"15\" vspace=\"0\">";
+				$str .= "Target <b>".$targets[$t_index]['number']."</b> ".$postfix."</li>".$crlf;
+			}
 
 		}
 		$str .= '</ul>'.$crlf;
-		$str .= '<table><tbody><tr><td>';
+		$str .= '<table style="width:100%"><tbody><tr><td style="width:50%">';
 		$str .= "<input class='gobutton' type='button' value='Save new order' onclick=\"submit()\">"; 
-		$str .= '</td><td>'.$crlf;
+		$str .= '</td><td style="text-align:right">'.$crlf;
 		$str .= "<input class='stopbutton' type='button' value='Abort all targets' onclick=\"submit_abort()\">"; 
 		$str .= '</td></tr></tbody></table>'.$crlf;
 	} else {
