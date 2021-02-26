b5d85d8b0f51bf9a528a98d529680306333b6242
Anchi Cheng <acheng@nysbc.org>
2021-02-25 22:49:47 -0800

    refs #9915 show only undone targets in sorting

--- myamiweb/sorttargets.php
+++ myamiweb/sorttargets.php
@@ -15,45 +15,82 @@
 	function addOne($n) {
 		return $n+1;
 	}
+	function currentTargetOrder($leginondata, $sessionId, $targets) {
+		if (empty($targets)) return array();
+		// retrieve current target order tagged in target number
+		$order_result = $leginondata->getTargetOrder($sessionId, $targets[0]['tlist']);
+		if (is_array($order_result) && array_key_exists('SEQ|order',$order_result)) {
+			$order_array = explode(',',substr($order_result['SEQ|order'],1,-1));
+			if ($order_array[0]=="") return array(); //aborted
+		} else {
+			// make default all targets (base 1)
+			$order_array = range(1,count($targets)); 
+		}
+		// keeps only undone target numbers
+		$clean_array = array();
+		foreach ($order_array as $n) {
+			$status = $leginondata->getTargetStatus($targets[0]['tlist'],(int)$n);
+			if ($status && strcmp($status,'done')!=0 && strcmp($status,'aborted')!=0) $clean_array[]=$n;
+		}
+		$order_array = array_map('subtractOne', $clean_array);
+		return $order_array; //base 0
+	}
+
+	//Need version 0 so that targets are complete.
+	//Leginon only generates targets on images of newer versions if targets are not done.
+	$imgId = $leginondata->getVersionZeroImage($imgId);
+	if (empty($imgId)) exit();
 	$rtlist = $leginondata->getTargetListFromImage($imgId);
 	if ($rtlist) {
 		$sessionId = $rtlist['session'];
 		if ($rtlist['mosaic'] == 1) {
 			// given a gr image id and get targets on all tile images on the atlas
 			$is_mosaic = true;
-			$title = "Targets on atlas ".$rtlist['label'];
+			$title = "Targets on atlas ".$rtlist['label']."<br><hr>";
+			//Targets with new status. This is a complete list of targets necessary to
+			//map order_array later
 			$targets = $leginondata->getNewTargetsFromAtlasImageList($rtlist['ilist']);
 		} else {
 			// given a non atlas image id and get targets on that image
 			$is_mosaic = false;
+			//Targets with new status. This is a complete list of targets necessary to
+			//map order_array later
 			$targets = $leginondata->getNewTargetsOnImage($imgId); 
 			$filename = $leginondata->getFilename($imgId)[0]['filename'];
-			$title = "Targets on image ".$filename;
+			$title = "Targets on image <br> <ul><li>".$filename."</li></ul><hr>";
 		}
 		if (!is_null($order_list)) {
 			// save new order 
 			$order_str = "(".$order_list.")";
+			// order list from JQueryUI is base 1
 			$order_array = explode(', ', $order_list);
+			// convert to base 0
 			$order_array = array_map('subtractOne', $order_array);
 			if ($targets) {
 				$leginondata->saveTargetOrder($sessionId, $targets[0]['tlist'], $order_str);
 			//TODO close the dialogue.
 			}
 		} elseif ($is_abort == 1) {
+			// aborting
 			if ($targets) {
+				if ($is_mosaic) {
+					//targetlist on mosaic grid atlas is never done.
+					//Need to make individual target status done.
+					$order_array = currentTargetOrder($leginondata, $sessionId, $targets);
+					// the targets not in order_arry should be done already
+					for ($i = 0; $i < count($targets); $i++) {
+						$t_index = $order_array[$i];
+						$t = $targets[$t_index];
+						//Only do this on non-done/aborted targets
+						$status = $leginondata->getTargetStatus($t['tlist'],$t['number']);
+						if ($status && $status != 'done' && $status != 'aborted')
+							$leginondata->updateTargetStatus($t['tid'],'aborted');
+					}
+				}
 				$leginondata->saveTargetOrder($sessionId, $targets[0]['tlist'], '()');
-				exit();
 			}
 		}
-	// retrieve current target order
-		$order_result = $leginondata->getTargetOrder($sessionId, $targets[0]['tlist']);
-		if (is_array($order_result) && array_key_exists('SEQ|order',$order_result)) {
-			$order_array = explode(',',substr($order_result['SEQ|order'],1,-1));
-			$order_array = array_map('subtractOne', $order_array);
-		} else {
-			// make default
-			$order_array = range(0,count($targets)-1);
-		}
+		$order_array = currentTargetOrder($leginondata, $sessionId, $targets);
 	}
 
 	$crlf="\n";
@@ -97,14 +134,15 @@
 	<body>
  
 <?php
-	if (is_array($targets) && count($targets)) {
+	// Show target list
+	if (is_array($targets) && count($targets) && $order_array) {
 		$str = '<ul id="sortable">';
 		for ($i = 0; $i < count($targets); $i++) {
 			$t_index = $order_array[$i];
 			$postfix = ($is_mosaic) ? 'on '.$targets[$t_index]['filename'].".mrc": '';
 			if (is_numeric($t_index)) {
 				$str .= "<li class=\"fittable\" id='".$targets[$t_index]['number']."'>";
-				$str .= "<img name=\"thumbtarget_btimg\" src=\"http://localhost/myamiweb/img/target_bt_on.gif\" alt=\"target_bt\" border=\"0\" height=\"15\" vspace=\"0\">";
+				$str .= "<img name=\"thumbtarget_btimg\" src=\"img/target_bt_on.gif\" alt=\"target_bt\" border=\"0\" height=\"15\" vspace=\"0\">";
 				$str .= "Target <b>".$targets[$t_index]['number']."</b> ".$postfix."</li>".$crlf;
 			}
 
@@ -116,7 +154,7 @@
 		$str .= "<input class='stopbutton' type='button' value='Abort all targets' onclick=\"submit_abort()\">"; 
 		$str .= '</td></tr></tbody></table>'.$crlf;
 	} else {
-		$str = "No targets found";
+		$str = '<br><br>No active target found';
 	}
 	echo $str;
 ?>
