5d3b73907845a3eb2e8198e691c063191d5a7281
Anchi Cheng <acheng@nysbc.org>
2022-02-03 13:30:07 -0500

    refs #11992 use a quarter of image to get mean in als ice thickness

---
+++ leginon/bestice.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import numpy
+from pyami import arraystats
+
+def getBestHoleMeanIntensity(imarray):
+	imshape = imarray.shape
+	qshape = (imshape[0]//4, imshape[1]//4)
+	qstarts = []
+	start_fractions = (0.25, 0.375, 0.5)
+	for x in start_fractions:
+		for y in start_fractions:
+			starty = int(imshape[0]*y)
+			startx = int(imshape[1]*x)
+			qstarts.append((starty, startx))
+	means = []
+	stds = []
+	for s in qstarts:
+		qarray = imarray[s[0]:s[0]+qshape[0],s[1]:s[1]+qshape[1]]
+		means.append(arraystats.mean(qarray))		
+		stds.append(arraystats.std(qarray))
+	print 'means', means
+	print 'stds', stds
+	return max(means)	
