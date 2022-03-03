a524e18fb11c660e08374bd0bea1a6166cdd10cf
Anchi Cheng <acheng@nysbc.org>
2022-03-03 13:53:03 -0800

    refs #12172 use update method in case new values need to be changed.

--- dbschema/tools/buffer_host_setup.py
+++ dbschema/tools/buffer_host_setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import sys
 from leginon import leginondata
+from sinedon import directq
 
 buffer_hostname = raw_input('Buffer Hostname to setup ? ')
 if not buffer_hostname:
@@ -22,13 +23,32 @@ if answer.lower() == 'n':
 for c in cameras:
 	q = leginondata.BufferHostData(ccdcamera=c)
 	q['buffer hostname']=buffer_hostname
-	q['buffer base path']=buffer_base_path
-	results= q.query()
-	if results:
-		print('Buffer/Camera pair exists as row of DEF_id=%d of leginondb.BufferHostData.\n  You should use database admin tools to disable/enable' % (results[0].dbid,))
-		sys.exit(1)
-	q['disabled']=False
-	q.insert()
-	print('Camera %s is paired to Bufer host %s to be saved under %s' % (c['name'],buffer_hostname, buffer_base_path))
-
+	results= q.query(results=1)
+	if not results:
+		# User sinedon to do initial insert
+		q = leginondata.BufferHostData(ccdcamera=c)
+		q['buffer hostname']=buffer_hostname
+		q['buffer base path']=buffer_base_path
+		q['disabled']= not last_status
+		q.insert(force=True)
+		print('Camera %s is paired to Bufer host %s to be saved under %s' % (c['name'],buffer_hostname, buffer_base_path))
+	else:
+		r = results[0]
+		if r['buffer base path'] != buffer_base_path:
+			# update and set to active.
+			q = "update BufferHostData set `buffer base path`='%s', `disabled`='0' where `DEF_id`=%d;" % (buffer_base_path, r.dbid)
+			directq.complexMysqlQuery('leginondata',q)
+			print('Base path changed')
+		else:
+			enable_str = 'enabled'
+			if r['disabled']:
+				enable_str = 'disabled'
+			last_status = r['disabled']
+			print('Buffer/Camera pair is already correct as DEF_id=%d of leginondb.BufferHostData.\n  It is curently %s' % (r.dbid, enable_str))
+			answer = raw_input('Do you want to change it ? (Y/N/y/n) ')
+			if answer.lower() == 'n':
+				continue
+			q = "update BufferHostData set `disabled`='%d' where `DEF_id`=%d;" % (int(not last_status), r.dbid)
+			directq.complexMysqlQuery('leginondata',q)
+			print('Status changed')
 print('DONE') 
