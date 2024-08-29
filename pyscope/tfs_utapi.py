fdb328dc1431016fa896b4f569c24e47c8feefae
Anchi Cheng <anchi2c@gmail.com>
2024-08-29 22:38:27 +0200

    refs #33 working

--- pyscope/tfs_utapi.py
+++ pyscope/tfs_utapi.py
@@ -18,7 +18,7 @@ from optics.v1 import illumination_pb2_grpc as illu_pg
 from optics.v1 import magnification_pb2_grpc as mag_pg
 from optics.v1 import optics_pb2_grpc as optics_pg
 from optics.v1 import stigmator_pb2_grpc as stig_pg
-from optics.v1 import xlens_pb2_grpc as xlens_pg
+from optics.v1 import x_lens_alignments_pb2_grpc as xlens_pg
 from column.v1 import column_mode_pb2_grpc as cm_pg
 from column.v1 import normalization_pb2_grpc as norm_pg
 
@@ -34,10 +34,10 @@ mag_stub = mag_pg.MagnificationServiceStub(channel)
 optics_stub =  optics_pg.OpticsServiceStub(channel)
 stig_stub = stig_pg.StigmatorServiceStub(channel)
 xlens_stub = xlens_pg.XLensAlignmentsServiceStub(channel)
-cm_stub = cm_pg.ColumnServiceStub(channel)
-norm_stub = norm_pg.ServiceStub(channel)
+cm_stub = cm_pg.ColumnModeServiceStub(channel)
+norm_stub = norm_pg.NormalizationServiceStub(channel)
 
-cm_probe_mode_map = [('nano', 'PROBE_MODE_NANO_PROBE'), ('micro', 'PROBE_MODE_MICRO_PROBE')]
+cm_probe_mode_map = [('micro','PROBE_MODE_MICRO_PROBE'),('nano', 'PROBE_MODE_NANO_PROBE')]
 
 def handleRpcError(e):
 	if e.code() == grpc.StatusCode.ABORTED:
@@ -49,7 +49,7 @@ def handleRpcError(e):
 		raise
 
 def _response_to_dict(response):
-	return json_format.MessageToDict(response_dict)
+	return json_format.MessageToDict(response)
 
 def _get_by_request(stub,attr_name,request):
 	my_attr = getattr(stub,attr_name)
@@ -81,17 +81,20 @@ class Utapi(fei.Krios):
 		return True
 
 	def getBeamstopPosition(self):
-		state_dict = _get_by_request(bstop_stub,'GetState',self.beamstop_device_id)
-		state_short = state_dict['state']
-		state_map = [('inserted','in'),('retracted','out')]
-		if state:
+		try:
+			state_dict = _get_by_request(bstop_stub,'GetState',self.beamstop_device_id)
+			state_short = state_dict['state']
+			print(state_short)
+		except Exception as e:
+			print(e)
+			return 'unknown'
+		state_map = [('inserted','in'),('retracted','out'),('moving','moving'),('inserted_halfway','halfway'),('undefined','unknown')]
+		try:
 			result = state_map[list(map((lambda x: x[0].lower()),state_map)).index(state_short.lower())][1]
 			return result
-		else:
-			print('no state returned')
+		except Exception as e:
+			print(e)
 			return 'unknown'
-		# all counted as invalid state
-		return 'unknown'
 
 	def setBeamstopPosition(self, value):
 		"""
@@ -99,7 +102,7 @@ class Utapi(fei.Krios):
 		"""
 		if value == self.getBeamstopPosition():
 			return
-		state_map = [('Insert','in'),('Retract','out')]
+		state_map = [('Insert','in'),('Retract','out'),('InsertedHalfway','halfway')]
 		attr_name = state_map[list(map((lambda x: x[1].lower()),state_map)).index(value.lower())][0]
 		max_trials = 5
 		trial = 1
@@ -112,7 +115,7 @@ class Utapi(fei.Krios):
 				raise RuntimeError('Beamstop setting to %s failed %d times' % (value, trial))
 			trial += 1
 
-	def _getApertureMechanismId(self, value):
+	def _getApertureMechanismId(self, mechanism_name):
 		if mechanism_name == 'condenser':
 			# always look up condenser 2 value
 			mechanism_name = 'condenser_2'
@@ -138,8 +141,9 @@ class Utapi(fei.Krios):
 		#TODO check if the mechanism is retracted.
 		# Retracted mechanism will get error in the next statement
 		# return "open"
-		result = _get_by_request(apmc_stub,'GetSelectedAperture',mechanism_id))
-		return result['aperture_name']
+		result = _get_by_request(apmc_stub,'GetSelectedAperture',my_request)
+		print(result)
+		return result['apertureName']
 
 	def setApertureSelection(self, mechanism_name, name):
 		#TODO handle open position
@@ -147,22 +151,22 @@ class Utapi(fei.Krios):
 		if name not in all_names:
 			raise RuntimeError('Invalid aperture %s' % (name))
 		mechanism_id = self._getApertureMechanismId(mechanism_name)
-		my_request = apmc_p.SelectApertureRequest(device_id=dip.DeviceId(id=mechanism_id),name)
+		my_request = apmc_p.SelectApertureRequest(device_id=dip.DeviceId(id=mechanism_id),aperture_name=name)
 		_set_by_request(apmc_stub, 'SelectAperture', my_request)
 
 	# column modes
 	def _get_column_modes(self):
-		results = _get_by_request(cm_pg,'GetColumnModes',cm_p.ColumnModeRequest())
+		results = _get_by_request(cm_stub,'GetColumnModes',cm_p.ColumnModeRequest())
 		return results
 
 	def getProbeModes(self):
-		mode_names = list(map((lambda x[0]),cm_mode_map))
+		mode_names = list(map((lambda x: x[0]),cm_probe_mode_map))
 		return mode_names
 
 	def getProbeMode(self):
 		all_modes = self._get_column_modes()
 		try:
-			value = list(map((lambda x:x[1]), cm_probe_mode_map)).index(all_modes['probeMode'])
+			value = cm_probe_mode_map[list(map((lambda x:x[1]),cm_probe_mode_map)).index(all_modes['probeMode'])][0]
 		except:
 			raise
 		return value
@@ -172,5 +176,6 @@ class Utapi(fei.Krios):
 			my_index = self.getProbeModes().index(value)
 		except ValueError:
 			raise RuntimeError('Invalid probe mode %s' % (value))
-		my_request =getattr(cm_p, my_index)
-		_set_by_request(cm_stub, attr_name, my_request)
+		my_mode = getattr(cm_p,cm_probe_mode_map[my_index][1])
+		my_request =cm_p.ProbeModeRequest(probe_mode=my_mode)
+		_set_by_request(cm_stub, 'SetProbeMode', my_request)
