03349742b9b11e85380be6d031c04e16cd0bc756
Anchi Cheng <anchi2c@gmail.com>
2024-09-11 20:56:21 +0200

    refs #33 optics and source

--- pyscope/tfs_utapi.py
+++ pyscope/tfs_utapi.py
@@ -9,14 +9,20 @@ if not SIMULATION:
 	# requests builders
 	from utapi_types.v1 import device_id_pb2 as dip
 	from utapi_types.v1 import utapi_response_pb2 as urp
+	from utapi_types.v1 import vector_pb2 as vctr_p
 	from optics.v1 import aperture_mechanism_control_pb2 as apmc_p
-	from optics.v1 import deflectors_pb2_grpc as defl_pg
+	from optics.v1 import deflectors_pb2 as defl_p
 	from optics.v1 import focus_pb2 as foc_p
 	from optics.v1 import illumination_pb2 as illu_p
 	from optics.v1 import magnification_pb2 as mag_p
+	from optics.v1 import stigmator_pb2 as stig_p
+	from optics.v1 import x_lens_alignments_pb2 as xaln_p
 	from column.v1 import column_mode_pb2 as cm_p
 	from column.v1 import normalization_pb2 as norm_p
 	from column.v1 import beam_blanker_pb2 as bb_p
+	from source.v1 import high_tension_pb2 as ht_p
+	from source.v1 import feg_pb2 as feg_p
+	from source.v1 import feg_flashing_pb2 as flash_p
 
 	# used to create stub for access
 	from optics.v1 import beam_stopper_control_pb2_grpc as bstop_pg
@@ -27,10 +33,13 @@ if not SIMULATION:
 	from optics.v1 import magnification_pb2_grpc as mag_pg
 	from optics.v1 import optics_pb2_grpc as optics_pg
 	from optics.v1 import stigmator_pb2_grpc as stig_pg
-	from optics.v1 import x_lens_alignments_pb2_grpc as xlens_pg
+	from optics.v1 import x_lens_alignments_pb2_grpc as xaln_pg
 	from column.v1 import column_mode_pb2_grpc as cm_pg
 	from column.v1 import normalization_pb2_grpc as norm_pg
 	from column.v1 import beam_blanker_pb2_grpc as bb_pg
+	from source.v1 import high_tension_pb2_grpc as ht_pg
+	from source.v1 import feg_pb2_grpc as feg_pg
+	from source.v1 import feg_flashing_pb2_grpc as flash_pg
 
 	# Can we connect remotely ?
 	channel = grpc.insecure_channel('localhost:46699', options=[('grpc.max_receive_message_length', 200 * 1024 * 1024)])
@@ -44,10 +53,13 @@ if not SIMULATION:
 	mag_stub = mag_pg.MagnificationServiceStub(channel)
 	optics_stub =  optics_pg.OpticsServiceStub(channel)
 	stig_stub = stig_pg.StigmatorServiceStub(channel)
-	xlens_stub = xlens_pg.XLensAlignmentsServiceStub(channel)
+	xaln_stub = xaln_pg.XLensAlignmentsServiceStub(channel)
 	cm_stub = cm_pg.ColumnModeServiceStub(channel)
 	norm_stub = norm_pg.NormalizationServiceStub(channel)
 	bb_stub = bb_pg.BeamBlankerServiceStub(channel)
+	ht_stub = ht_pg.HighTensionServiceStub(channel)
+	feg_stub = feg_pg.FegServiceStub(channel)
+	flash_stub = flash_pg.FegFlashingServiceStub(channel)
 
 else:
 	from pyscope import simtem
@@ -91,6 +103,68 @@ def _set_by_request(stub, attr_name, request):
 	except grpc.RpcError as rpc_error:
 		handleRpcError(rpc_error)
 
+def _get_vector_xy(msg_dict, key):
+	if not msg_dict or key not in msg_dict.keys():
+		# error state
+		return {'x':None,'y':None}
+	r = {'x':0.0, 'y':0.0}
+	for k in r.keys():
+		if k in msg_dict[key].keys():
+			r[k] = msg_dict[key][k]
+	return r
+
+def _make_vector(vector, original_vector, relative = 'absolute'):
+	"""
+	Return vector xy dict either relative to original or absolute values.
+	"""
+	if relative == 'relative':
+		try:
+			vector['x'] += original_vector['x']
+		except KeyError:
+			pass
+		try:
+			vector['y'] += original_vector['y']
+		except KeyError:
+			pass
+	elif relative == 'absolute':
+		pass
+	else:
+		raise ValueError
+	# handle missing key that means no change.
+	if 'x' not in vector.keys():
+		vector['x'] = original_vector['x']
+	if 'y' not in vector.keys():
+		vector['y'] = original_vector['y']
+	if 'x' not in original_vector.keys():
+		vector['x'] = vector['x']
+	if 'y' not in original_vector.keys():
+		vector['y'] = vector['y']
+	return vector
+
+def underscore_to_camelcase(value,capitalize_first=False):
+	"""
+	Copied from https://stackoverflow.com/questions/4303492/how-can-i-simplify-this-conversion-from-underscore-to-camelcase-in-python	
+	"""
+	def camelcase():
+		yield str.lower
+		while True:
+			yield str.capitalize
+	c = camelcase()
+	r = "".join(c.__next__()(x) if x else '_' for x in value.split("_"))
+	if capitalize_first:
+		r = r.replace(r[0],r[0].upper(),1)
+	return r
+
+def camelcase_to_underscore(value):
+	def underscore():
+		yield '_'+str.lower
+	indices = [i for i,l in enumerate(value) if l.isupper()]
+	if indices[0] == 0:
+		indices.pop(0)
+	for i, x in enumerate(indices):
+		offset = i+x
+		value = value.replace(value[offset], '_'+value[offset].lower()) 
+	return value.lower()
 
 import time
 
@@ -147,18 +221,21 @@ class Utapi(fei.Krios):
 						('out','Retract'),
 						('halfway','InsertedHalfway')
 	]
+	cold_feg_flash_types = {'low':1,'high':2}
+
 	def __init__(self):
 		super(Utapi, self).__init__()
+		self.cold_feg_flash_types = {'low':1,'high':2}
 		self.beamstop_device_id = dip.DeviceIdRequest(id='BeamStopper')
 		# default to let the scope control auto normalization.
 		self.noramlize_all_after_setting = False
-		self.need_noramlize_all = False
+		self.need_normalize_all = False
 		self.sup_mag_data = {}
 		self.logger = Logger()
 		self.stage_logger = Logger()
 		if self.getDebugAll():
-			self.logger.setLevel(3)
-			self.stage_logger.setLevel(3)
+			self.logger.setLevel(0)
+			self.stage_logger.setLevel(0)
 
 	def getDebugAll(self):
 		return True
@@ -242,6 +319,101 @@ class Utapi(fei.Krios):
 		mechanism_id = self._getApertureMechanismId(mechanism_name)
 		my_request = apmc_p.SelectApertureRequest(device_id=dip.DeviceId(id=mechanism_id),aperture_name=name)
 		_set_by_request(apmc_stub, 'SelectAperture', my_request)
+
+	#source
+	def _getHighTensionSettings(self):
+		my_request = getattr(ht_p,'HighTensionSettingsRequest')()
+		return _get_by_request(ht_stub, 'GetHighTensionSettings', my_request)
+	
+	def getHighTension(self):
+		r = self._getHighTensionSettings()
+		# ht_offset returns 0.0 if not available and therefore not transfered
+		return r['htValue']
+	
+	def _getFegValue(self, my_device):
+		result_key = my_device.replace(my_device[0],my_device[0].lower())
+		my_request = getattr(feg_p,'%sRequest' % my_device)()
+		r = _get_by_request(feg_stub, 'Get%s' % my_device, my_request)
+		try:
+			return r[result_key]
+		except:
+			return -1.0
+
+	def getGunLens(self):
+		my_device = 'FocusIndex'
+		my_request = getattr(feg_p,'%sRequest' % my_device)()
+		r = _get_by_request(feg_stub, 'Get%s' % my_device, my_request)
+		return r['coarse'] + r['fine']*0.1 if 'fine' in r.keys() else float(r['coarse'])
+
+	def getColdFegBeamCurrent(self):
+		my_device = 'BeamCurrent'
+		return set._getFegValue(my_device)
+
+	def getExtractorVoltage(self):
+		my_device = 'ExtractorVoltage'
+		return set._getFegValue(my_device)
+	
+	def hasColdFeg(self):
+		try:
+			flash_type_constant = self.cold_feg_flash_types['low']
+			r = self._getFlashingAdvised(flash_type_constant)
+			return True
+		except Exception as e:
+			print(e)
+			return False
+
+	def _getFlashingAdvised(self,flash_type_constant):
+		my_device = 'FegFlashing'
+		my_request = getattr(flash_p,'%sRequest' % my_device)(flashing_type=flash_type_constant)
+		r = _get_by_request(flash_stub, 'GetFlashingAdvised', my_request)
+
+	def getFlashingAdvised(self, flash_type):
+		advised_only = self.getFeiConfig('source','flash_cold_feg_only_if_advised')
+		try:
+			flash_type_constant = self.cold_feg_flash_types[flash_type]
+			r = self._getFlashingAdvised(flash_type_constant)
+			# r can either be {} or {'flashing_advised': True}
+			should_flash = bool(r)
+		except AttributeError as e:
+			return False
+		except KeyError:
+			self.logger.debug('flash type can only be %s' % list(self.cold_feg_flash_types.keys()))
+			return False
+		except Exception as e:
+			self.logger.debug('other getFlashAdvised exception %s' % e)
+			return False
+		if advised_only:
+			return should_flash
+		else:
+			if flash_type == 'low':
+				# only low temp can flash without advised.
+				return True
+		return should_flash
+
+	def _performColdFegFlashing(self, flash_type_constant):
+		my_device = 'FegFlashing'
+		my_request = getattr(flash_p,'%sRequest' % my_device)(flashing_type=flash_type_constant)
+		r = _set_by_request(flash_stub, 'PerformFlashing', my_request)
+
+	def setColdFegFlashing(self,state):
+		# 'on' starts flashing, 'off' stops flashing
+		# tfs flashing can not be stopped.
+		if not self.hasColdFeg():
+			return
+		# low temperature (lowT) flashing can be done any time even if not advised.
+		# highT flashing can only be done if advised.
+		if state != 'on':
+			# tfs flashing can not be stopped.
+			return
+		for flash_type in ('high','low'):
+			if self.getFlashingAdvised(flash_type):
+				flash_type_constant = self.cold_feg_flash_types[flash_type]
+				try:
+					self._performColdFegFlashing(flash_type_constant)
+					# no need to do lowT flashing if highT is done
+					break
+				except Exception as e:
+					raise RuntimeError(e)
 
 	# column modes
 	def _getColumnModes(self):
@@ -398,6 +570,8 @@ class Utapi(fei.Krios):
 
 	def setMagnification(self, int_value):
 		obj_mode = self.getObjectiveMode()
+		if not self.projection_submode_map:
+			raise ValueError('objective mode and submode mapping not set')
 		if int_value in self.projection_submode_map.keys():
 			mode_name, mode_id, obj_mode_name = self.projection_submode_map[int_value]
 			obj_mode_name = obj_mode_name.lower()
@@ -406,6 +580,8 @@ class Utapi(fei.Krios):
 			mag_float = self._getSupportedMagnifications()['supportedMagnifications'][index]
 			my_request = getattr(mag_p,'SetMagnificationRequest')(magnification=mag_float)
 			_set_by_request(mag_stub, 'SetMagnification', my_request)
+		else:
+			raise ValueError('Magnification %d not in mapping. Can not determine how to set the value ' % (int_value))
 
 	def _addSupportedMagData(self,obj_mode_name):
 		om = obj_mode_name.lower()
@@ -435,6 +611,33 @@ class Utapi(fei.Krios):
 		self.setObjectiveMode(saved_mode)
 		self.setMagnification(saved_mag)
 
+	def _getIlluminationSettings(self):
+		my_request = getattr(illu_p,'IlluminationSettingsRequest')()
+		return _get_by_request(illu_stub, 'GetIlluminationSettings', my_request)
+	
+	def getSpotSize(self):
+		return self._getIlluminationSettings()['spotSizeIndex']
+
+	def getIntensity(self):
+		return self._getIlluminationSettings()['illuminatedAreaDiameter']
+
+	def _setIllumination(self, req_key_name, value):
+		req_key_name = 'illuminated_area_diameter'
+		my_device = underscore_to_camelcase(req_key_name,True)
+		kwargs = {}
+		kwargs[req_key_name] = value
+		my_request = getattr(illu_p,'%sRequest' % my_device)
+		my_request = my_request(**kwargs)
+		return _get_by_request(illu_stub, 'Set%s' % my_device, my_request)
+
+	def setSpotSize(self, value):
+		req_key_name = 'spot_size_index'
+		return self._setIllumination(req_key_name, value)
+
+	def setIntensity(self, value):
+		req_key_name = 'illuminated_area_diamter'
+		return self._setIllumination(req_key_name, value)
+
 	def getBeamBlank(self):
 		attr_name = 'GetBeamBlankerState'
 		my_map = self.bb_map
@@ -457,9 +660,9 @@ class Utapi(fei.Krios):
 		# until done. see example. The completion is very fast, though.
 
 	def normalizeLens(self, value='all'):
-		# constant values not exposed as attribute anywhere. see proto file
+		# See porto file for enum values (norm_p.Normalization.Normalize)
 		norm_map = [
-					('spotsize',1),  # 4 s on simulator
+					('spotsize',1),  # takes 4 s on simulator to complete.
 					('intensity',2), # 4 s
 					('objective',3), # 4 s
 					('projector',4), # 4 s
@@ -483,8 +686,10 @@ class Utapi(fei.Krios):
 	def getAutoNormalizeEnabled(self, value):
 		try:
 			my_request = norm_p.GetAtutoNormalizeEnabledRequest()
+
 			result = _get_by_request(norm_stub,'GetAutoNormalizeEnabled', my_request)
 			if 'enabled' not in result.keys():
+				# protobuf message parsing drops the item if the value is default of the type.
 				return False
 			else:
 				return result['enabled']
@@ -500,3 +705,222 @@ class Utapi(fei.Krios):
 		else:
 			# not affected by the value
 			self.need_normalize_all = False
+
+	def _getAllDeflectors(self):
+		try:
+			my_request = getattr(defl_p,'DeflectorsSettingsRequest')()
+			return _get_by_request(defl_stub, 'GetDeflectorsSettings', my_request)
+		except Exception as e:
+			self.logger.error('Error getting deflector values: %s' %e)
+			return {}
+
+	def _setDeflector(self, req_key_name, vector, original_vector, min_move, relative = 'absolute'):
+		my_device = underscore_to_camelcase(req_key_name,True)
+		#
+		vector = _make_vector(vector, original_vector, relative)
+		vec = original_vector
+		if abs(vec['x']-vector['x'])+abs(vec['x']-vector['y']) < min_move:
+			# small move is ignored.
+			return
+		v_req = vctr_p.Vector(x=vector['x'],y=vector['y'])
+		if req_key_name.startswith('x_'):
+			req_attr_name = 'Set%sRequest' % my_device
+		else:
+			req_attr_name = '%sRequest' % my_device
+		my_request = getattr(defl_p,req_attr_name)
+		kwargs = {}
+		kwargs[req_key_name]=v_req
+		my_request = my_request(**kwargs)
+		_set_by_request(defl_stub,'Set%s' % my_device, my_request)
+
+	def getBeamTilt(self):
+		"""
+		Beam Tilt in radians for vector axes x,y.
+		"""
+		#TODO: this might need to be mapped to ImageBeamTilt, not BeamTilt
+		r = self._getAllDeflectors()
+		return _get_vector_xy(r,'beamTilt')
+
+	def setBeamTilt(self, vector, relative = 'absolute'):
+		my_device = 'BeamTilt'
+		original_vector = getattr(self,'get%s' % my_device)()
+		min_move = 1e-6
+
+		req_key_name = camelcase_to_underscore(my_device)
+		self._setDeflector(req_key_name, vector, original_vector, min_move, relative)
+
+	def getBeamShift(self):
+		"""
+		Beam Tilt in meters for vector axes x,y.
+		"""
+		r = self._getAllDeflectors()
+		return _get_vector_xy(r,'beamShift')
+
+	def setBeamShift(self, vector, relative = 'absolute'):
+		my_device = 'BeamShift'
+		original_vector = getattr(self,'get%s' % my_device)()
+		min_move = 1e-9
+
+		req_key_name = camelcase_to_underscore(my_device)
+		self._setDeflector(req_key_name, vector, original_vector, min_move, relative)
+
+	def getImageShift(self):
+		"""
+		ImageBeamShift in meters for vector axes x,y.
+		"""
+		r = self._getAllDeflectors()
+		return _get_vector_xy(r,'imageBeamShift')
+
+	def setImageShift(self, vector, relative = 'absolute'):
+		my_device = 'ImageBeamShift'
+		original_vector = getattr(self,'get%s' % 'ImageShift')()
+		min_move = 1e-9
+
+		req_key_name = camelcase_to_underscore(my_device)
+		self._setDeflector(req_key_name, vector, original_vector, min_move, relative)
+
+	def getDiffractionShift(self):
+		"""
+		Diffraction shift in meters for vector axes x,y.
+		"""
+		r = self._getAllDeflectors()
+		result = _get_vector_xy(r,'difffractionShift')
+		if result['x'] is None:
+			raise ValueError('Diffraction shift not available in this mode')
+
+	def setDiffractionShift(self, vector, relative = 'absolute'):
+		my_device = 'DiffractionShift'
+		original_vector = getattr(self,'get%s' % my_device)()
+		min_move = 1e-9
+
+		req_key_name = camelcase_to_underscore(my_device)
+		self._setDeflector(req_key_name, vector, original_vector, min_move, relative)
+
+	def getXDeflectorTilt(self):
+		"""
+		X lens Tilt in radians for vector axes x,y.
+		"""
+		r = self._getAllDeflectors()
+		return _get_vector_xy(r,'xDeflectorTilt')
+
+	def setXDeflectorTilt(self, vector, relative = 'absolute'):
+		my_device = 'XDeflectorTilt'
+		original_vector = getattr(self,'get%s' % my_device)()
+		min_move = 1e-6
+
+		req_key_name = camelcase_to_underscore(my_device)
+		self._setDeflector(req_key_name, vector, original_vector, min_move, relative)
+
+	def getXDeflectorShift(self):
+		"""
+		X lens Shift in meters for vector axes x,y.
+		"""
+		r = self._getAllDeflectors()
+		return _get_vector_xy(r,'xDeflectorShift')
+
+	def setXDeflectorShift(self, vector, relative = 'absolute'):
+		my_device = 'XDeflectorShift'
+		original_vector = getattr(self,'get%s' % my_device)()
+		min_move = 1e-9
+		#
+		req_key_name = camelcase_to_underscore(my_device)
+		self._setDeflector(req_key_name, vector, original_vector, min_move, relative)
+
+	def _getAllStigmators(self):
+		try:
+			my_request = getattr(stig_p,'StigmatorSettingsRequest')()
+			return _get_by_request(stig_stub, 'GetStigmatorSettings', my_request)
+		except Exception as e:
+			self.logger.error('Error getting stigmator values: %s' %e)
+			return {}
+
+	def getStigmator(self):
+		r = self._getAllStigmators()
+		# new_r uses pyscope keys
+		new_r = {}
+		for k in r.keys():
+			new_k = camelcase_to_underscore(k).split('_')[0]
+			new_r[new_k] = {}
+			for axis in ('x','y'):
+				if axis not in r[k]:
+					r[k][axis] = 0.0
+			new_r[new_k] = r[k]
+		return new_r
+
+	def setStigmator(self, stigs, relative = 'absolute'):
+		original_stigs = getattr(self,'getStigmator')()
+		for device_type in stigs.keys():
+			req_key_name = device_type+'_stigmator'
+			original_vector = original_stigs[device_type]
+			min_move = 1e-9 # TODO: what is the good value ?
+			self._setStigmator(req_key_name, stigs[device_type], original_vector, min_move, relative)
+
+	def _setStigmator(self, req_key_name, vector, original_vector, min_move, relative = 'absolute'):
+		my_device = underscore_to_camelcase(req_key_name,True)
+		#
+		vector = _make_vector(vector, original_vector, relative)
+		vec = original_vector
+		if abs(vec['x']-vector['x'])+abs(vec['x']-vector['y']) < min_move:
+			# small move is ignored.
+			return
+		v_req = vctr_p.Vector(x=vector['x'],y=vector['y'])
+		my_request = getattr(stig_p,'%sRequest' % my_device)
+		kwargs = {}
+		kwargs[req_key_name]=v_req
+		my_request = my_request(**kwargs)
+		_set_by_request(stig_stub,'Set%s' % my_device, my_request)
+
+	def _getFocusSettings(self):
+		my_request = getattr(foc_p,'FocusSettingsRequest')()
+		return _get_by_request(foc_stub, 'GetFocusSettings', my_request)
+	
+	def getFocus(self):
+		return self._getFocusSettings()['focus']
+
+	def setFocus(self, value):
+		my_request = getattr(foc_p,'FocusRequest')(focus=value)
+		return _get_by_request(foc_stub, 'SetFocus', my_request)
+
+	def getDefocus(self):
+		return self._getFocusSettings()['defocus']
+
+	def setDefocus(self, defocus, relative = 'absolute'):
+		old_defocus = self.getDefocus
+		if relative == 'relative':
+			defocus += old_defocus
+		elif relative == 'absolute':
+			pass
+		else:
+			raise ValueError
+		# normalize by always sending 0 focus first
+		self.setFocus(0.0)
+		my_request = getattr(foc_p,'DefocusRequest')(defocus=defocus)
+		return _get_by_request(foc_stub, 'SetDefocus', my_request)
+	
+	def resetDefocus(self):
+		my_request = getattr(foc_p,'ResetDefocusRequest')()
+		return _get_by_request(foc_stub, 'ResetDefocus', my_request)
+
+	def getXLensAlignmentRange(self, lens_id):
+		my_request = getattr(xaln_p,'GetXLensPresetRangeRequest')(x_lens_id=lens_id)
+		r = _get_by_request(xaln_stub, 'GetXLensPresetRange', my_request)
+		return (r['minXLensPreset'],r['maxXLensPreset'])
+
+	def getXLensAlignment(self, lens_id=1):
+		my_request = getattr(xaln_p,'GetXLensPresetRequest')(x_lens_id=lens_id)
+		return _get_by_request(xaln_stub, 'GetXLensPreset', my_request)['xLensPreset']
+		
+	def setXLensAlignment(self, lens_id, value, relative = 'absolute'):
+		#TODO: Check on real scope. Simulator only has valid results with lens_id=1
+		old_value = self.getXLensAlignment(lens_id)
+		if relative == 'relative':
+			value += old_value
+		elif relative == 'absolute':
+			pass
+		else:
+			raise ValueError('XLensAlignment option %s not assigned' % relative)
+		my_min, my_max = self.getXLensAlignmentRange(lens_id)
+		if value > my_max or value < my_min:
+			raise ValueError('preset %d set value %.3f out of range' % (lens_id, value))
+		my_request = getattr(xaln_p,'SetXLensPresetRequest')(x_lens_id=lens_id, x_lens_preset=value)
+		return _get_by_request(xaln_stub, 'SetXLensPreset', my_request)
