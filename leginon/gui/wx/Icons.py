26d8befe722fdc5a490937cd8deb598b9a71ab2d
Anchi Cheng <acheng@czii.org>
2025-08-13 16:24:13 -0700

    refs redmine #12958 still an issue on newer wxpython

--- leginon/gui/wx/Icons.py
+++ leginon/gui/wx/Icons.py
@@ -33,8 +33,8 @@ def icon(name):
 	return bitmap
 
 def _icon(name):
-	# workaround for wx 4.1.1 bug showing bitmap alpha as black instead of transparency.
-	if name == 'null' and wx.__version__=='4.1.1':
+	# workaround for wx 4.1.1 bug showing bitmap alpha as black instead of transparent.
+	if name == 'null' and wx.__version__ >= '4.1.1':
 		name = 'null_white'
 	filename = '%s.png' % name
 	path = leginon.icons.getPath(filename)
